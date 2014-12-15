bdea604bf0a62956da89b03b12ff54067ab8b270
Michael Vogt <michael.vogt@ubuntu.com>
2014-12-15 16:25:47 +0100

    POC for search

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"net/http"
+	"encoding/json"
 
 	// my own stuff
 	"snappy"
 )
 
-const APPS_ROOT = "/tmp/apps"
+const APPS_ROOT = "/apps"
 
 func audit_snap(snap string) bool {
 	// FIXME: we want a bit more here ;)
@@ -208,6 +210,46 @@ func snap_build(dir string) error {
 	return nil
 }
 
+func search_snap(search_term string) error {
+	const SEARCH_URI = "https://search.apps.ubuntu.com/api/v1/search?q=%s"
+
+	url := fmt.Sprintf(SEARCH_URI, search_term)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	// set headers
+	req.Header.Set("Accept", "application/hal+json")
+	//FIXME: hardcoded
+	req.Header.Set("X-Ubuntu-Frameworks", "ubuntu-core-15.04-dev1")
+	req.Header.Set("X-Ubuntu-Architecture", "amd64")
+	
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	searchData := make(map[string]interface{})
+	body, err := ioutil.ReadAll(resp.Body)
+	//log.Print(string(body))
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &searchData)
+	if err != nil {
+		return nil
+	}
+	embedded := searchData["_embedded"].(map[string]interface{})
+	packages :=  embedded["clickindex:package"].([]interface{})
+	for _, raw := range(packages) {
+		pkg := raw.(map[string]interface{})
+		fmt.Printf("%s (%s) - %s \n", pkg["name"], pkg["version"], pkg["title"])
+	}
+
+	return nil;
+}
+
 func main() {
 
 	switch os.Args[1] {
@@ -219,6 +261,11 @@ func main() {
 		snap := os.Args[2]
 		if err := install_snap(snap, APPS_ROOT); err != nil {
 			log.Fatal("install_snap failed: ", err)
+		}
+        case "search":
+		arg := os.Args[2]
+		if err := search_snap(arg); err != nil {
+			log.Fatal("search_snap failed: ", err)
 		}
 	}
 }
