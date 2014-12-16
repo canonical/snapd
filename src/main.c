12ba47ff777ccd8441d022fb71fb0ce62d370df6
Michael Vogt <michael.vogt@ubuntu.com>
2014-12-16 08:36:26 +0100

    load apparmor profile

--- src/main.c
+++ src/main.c
@@ -11,18 +11,17 @@ int main(int argc, char **argv)
 {
     int i = 0;
 
-    if(argc < 3)
+    if(argc < 4)
     {
-       fprintf(stderr, "Usage: %s <rootdir> <binary>\n", argv[0]);
+       fprintf(stderr, "Usage: %s <rootdir> <binary> <apparmor>\n", argv[0]);
        exit(1);
     }
 
     const char *rootdir = argv[1];
     const char *binary = argv[2];
+    const char *apparmor = argv[3];
 
     //https://wiki.ubuntu.com/SecurityTeam/Specifications/SnappyConfinement#ubuntu-snapp-launch
-
-    // FIXME: set apparmor rules
 
     // setup env
     setenv("SNAP_APP_DIR", rootdir, 1);
@@ -35,6 +34,7 @@ int main(int argc, char **argv)
     // FIXME: we need to add all frameworks that need to be overlayed here
     const char* OVERLAY_DIRS[] = {
        "/",
+       rootdir,
        NULL,
     };
     if (!make_overlay(OVERLAY_DIRS))
@@ -42,13 +42,16 @@ int main(int argc, char **argv)
     if (!make_private_tmp())
        die("failed to create private /tmp dir");
 
-   // FIXME: setup cgroup
+   // FIXME: setup cgroup for net_cls
 
-   // FIXME: setup iptables
+   // FIXME: setup iptables security table
 
-   // FIXME: seccomp
+   // FIXME: port binding restriction (seccomp?)
 
    // FIXME: ensure user specific data dir is availble (create if needed)
+
+    // set apparmor rules
+    aa_change_onexec(apparmor);
 
    // run the app
    chdir(rootdir);
