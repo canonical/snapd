543321b333c9b6aa861e1a97c78cbb2a6c11ae6a
James Hunt <james.hunt@ubuntu.com>
2014-12-17 22:54:08 +0000

    * Reworked to use dispatcher model for handling snappy commands.

--- main.go
+++ main.go
@@ -261,6 +261,8 @@ func search_snap(search_term string) error {
 	return nil;
 }
 
+// FIXME: rework to use dispatcher logic
+/*
 func main() {
 
 	switch os.Args[1] {
@@ -279,4 +281,24 @@ func main() {
 			log.Fatal("search_snap failed: ", err)
 		}
 	}
+
+}
+*/
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <command>\n", os.Args[0])
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+
+	err := snappy.CommandDispatch(cmd, os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+		"ERROR: command %s failed: %s\n",
+		cmd, err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
---
+++ src/snappy/commands.go
@@ -0,0 +1,93 @@
+package snappy
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+type SnappyFunction func(args []string) error
+
+type SnappyCommand struct {
+
+	fp SnappyFunction
+	description string
+}
+
+// jump table
+var commands map[string]SnappyCommand
+
+func init() {
+	// create the jump table
+	commands = make(map[string]SnappyCommand)
+}
+
+// Add a function to the dispatcher
+func registerCommand(cmd string, desc string, f SnappyFunction) {
+	commands[cmd] = SnappyCommand{fp: f, description: desc}
+}
+
+func showUsage() {
+	var keys []string
+
+	// create a slice of command names
+	for cmd := range commands {
+		keys = append(keys, cmd)
+	}
+
+	sort.Strings(keys)
+
+	fmt.Println("Available commands:\n")
+
+	for _, cmd := range keys {
+		help := commands[cmd].description
+		fmt.Printf("  %s (%s)\n", cmd, help)
+	}
+	fmt.Println("")
+}
+
+func CommandDispatch(cmd string, args []string) (err error) {
+	registerCommands()
+
+	return handleCommand(cmd, args)
+}
+
+func handleCommand(cmd string, args []string) (err error) {
+
+	// Special-case
+	if cmd == "help" {
+		showUsage()
+		os.Exit(0)
+	}
+
+	command, exists := commands[cmd]
+
+	if !exists {
+		fmt.Printf("ERROR: Invalid command: %s\n", cmd)
+		showUsage()
+		os.Exit(1)
+	}
+
+	return command.fp(args)
+}
+
+// register all dispatcher functions
+func registerCommands() {
+
+	registerCommand("versions", "display versions of installed parts", cmdVersions)
+	registerCommand("update", "update installed parts", cmdUpdate)
+}
+
+func cmdVersions(args []string) (err error) {
+	// FIXME: implement
+	fmt.Printf("FIXME: implement versions\n")
+
+	return err
+}
+
+func cmdUpdate(args []string) (err error) {
+	// FIXME: implement
+	fmt.Printf("FIXME: implement update\n")
+	return err
+}
+
