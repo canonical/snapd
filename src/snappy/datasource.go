e72f30bd2b1f20886272e84db62d4855ab53e6e4
James Hunt <james.hunt@ubuntu.com>
2014-12-18 10:44:12 +0000

    * Comments.

--- src/snappy/datasource.go
+++ src/snappy/datasource.go
@@ -38,7 +38,15 @@ type DataSource interface {
 	// compare two parts; return true if (a < b)
 	Less(a, b Part) bool
 
-	// true if manipulating the DS requires root
+	// true if manipulating the DS requires root.
+	//
+	// This is used to speed up command invocation: rather than
+	// running a particular snappy command for all available
+	// datasources and ultimately finding that the _last_ datasource
+	// requires root, we check upfront to avoid running any
+	// commands. This gives a better user experience since some
+	// datasource commands are inherantly slow (they require network
+	// access for example).
 	//
 	// This is rather crude since ideally we'd allow a DS's
 	// methods to individually specify if they are privileged.
