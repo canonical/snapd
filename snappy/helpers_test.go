2fd5fd20f93f7457fa81513bf07c02477bb7d8be
Michael Vogt <michael.vogt@ubuntu.com>
2015-01-08 11:28:51 +0100

    convert helpers_test.go to gocheck

--- snappy/helpers_test.go
+++ snappy/helpers_test.go
@@ -9,15 +9,17 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	. "launchpad.net/gocheck"
 )
 
-func TestUnpack(t *testing.T) {
+type TestSuite struct {}
+
+func (ts *TestSuite) TestUnpack(c *C) {
 
 	// setup tmpdir
 	tmpdir, err := ioutil.TempDir(os.TempDir(), "meep")
-	if err != nil {
-		t.Error("tmpdir failed")
-	}
+	c.Assert(err, IsNil)
 	defer os.RemoveAll(tmpdir)
 	tmpfile := filepath.Join(tmpdir, "foo.tar.gz")
 
@@ -27,40 +29,30 @@ func TestUnpack(t *testing.T) {
 	// create test data
 	cmd := exec.Command("tar", "cvzf", tmpfile, path)
 	output, err := cmd.CombinedOutput()
+	c.Assert(err, IsNil)
 	if !strings.Contains(string(output), "/etc/fstab") {
-		t.Error("Can not find expected output from tar")
-	}
-	if err != nil {
-		t.Error("failed to create tmp archive")
+		c.Error("Can not find expected output from tar")
 	}
 
 	// unpack
 	unpackdir := filepath.Join(tmpdir, "t")
 	err = unpackTar(tmpfile, unpackdir)
-	if err != nil {
-		t.Error("unpack failed: %v", err)
-	}
+	c.Assert(err, IsNil)
 
 	_, err = os.Open(filepath.Join(tmpdir, "t/etc/fstab"))
-	if err != nil {
-		t.Error("can not find expected file in unpacked dir")
-	}
+	c.Assert(err, IsNil)
 }
 
-func TestGetMapFromValidYaml(t *testing.T) {
+func (ts *TestSuite) TestGetMapFromValidYaml(c *C) {
 	m, err := getMapFromYaml([]byte("name: value"))
-	if err != nil {
-		t.Error("Failed to convert yaml")
-	}
+	c.Assert(err, IsNil)
 	me := map[string]interface{}{"name": "value"}
 	if !reflect.DeepEqual(m, me) {
-		t.Error(fmt.Sprintf("Unexpected map %v != %v", m, me))
+		c.Error(fmt.Sprintf("Unexpected map %v != %v", m, me))
 	}
 }
 
-func TestGetMapFromInvalidYaml(t *testing.T) {
+func (ts *TestSuite) TestGetMapFromInvalidYaml(c *C) {
 	_, err := getMapFromYaml([]byte("%lala%"))
-	if err == nil {
-		t.Error("invalid yaml is a error")
-	}
+	c.Assert(err, NotNil)
 }
