87d3c744fa20da266b93e7e7d6ab6bebfa7186a4
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-06 08:31:49 +0100

    add support for local snap installs

--- snappy/install.go
+++ snappy/install.go
@@ -2,13 +2,51 @@ package snappy
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
+
+var cloudMetaDataFile = "/var/lib/cloud/seed/nocloud-net/meta-data"
+
+// check if the image is in developer mode
+// FIXME: this is a bit crude right now, but it seems like there is not more
+//        meta-data to check right now
+func inDeveloperMode() bool {
+	f, err := os.Open(cloudMetaDataFile)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return false
+	}
+	needle := "public-keys:\n"
+	if strings.Contains(string(data), needle) {
+		return true
+	}
+	return false
+}
 
 func Install(args []string) (err error) {
 	didSomething := false
 	m := NewMetaRepository()
 	for _, name := range args {
+
+		// consume local parts
+		if _, err := os.Stat(name); err == nil {
+			// we allow unauthenticated package when in developer
+			// mode
+			allowUnauthenticated := inDeveloperMode()
+			if err := installClick(name, allowUnauthenticated); err != nil {
+				return err
+			}
+			didSomething = true
+			continue
+		}
+
+		// check repos next
 		found, _ := m.Details(name)
 		for _, part := range found {
 			// act only on parts that are downloadable
