5b89a525a7f770eb5e735fcecdadeae6af765aac
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-10 14:32:56 +0100

    snappy/remove.go: return a error if no snap can be found for removal

--- snappy/remove.go
+++ snappy/remove.go
@@ -1,12 +1,15 @@
 package snappy
 
+import "fmt"
+
 func Remove(partName string) error {
 	part := InstalledSnapByName(partName)
-	if part != nil {
-		if err := part.Uninstall(); err != nil {
-			return err
-		}
+	if part == nil {
+		return fmt.Errorf("Can not find snap %s", partName)
 	}
-
+	if err := part.Uninstall(); err != nil {
+		return err
+	}
+	
 	return nil
 }
