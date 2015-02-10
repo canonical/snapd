2a59fdec878f26cf57cccbcc4d80a1c57ddff92a
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-10 08:02:41 +0100

    use ActiveSnapByName,FindSnapsByName names as suggested by Sergio

--- snappy/parts.go
+++ snappy/parts.go
@@ -155,7 +155,7 @@ var InstalledSnapNamesByType = func(snapTs ...SnapType) (res []string, err error
 	return
 }
 
-func ActivePartByName(needle string) Part {
+func ActiveSnapByName(needle string) Part {
 	m := NewMetaRepository()
 	installed, err := m.Installed()
 	if err != nil {
@@ -172,11 +172,11 @@ func ActivePartByName(needle string) Part {
 	return nil
 }
 
-func FindPartByName(needle string, haystack []Part) *Part {
+func FindSnapsByName(needle string, haystack []Part) (res []Part) {
 	for _, part := range haystack {
 		if part.Name() == needle {
-			return &part
+			res = append(res, part)
 		}
 	}
-	return nil
+	return res
 }
--- snappy/remove.go
+++ snappy/remove.go
@@ -1,7 +1,7 @@
 package snappy
 
 func Remove(partName string) error {
-	part := ActivePartByName(partName)
+	part := ActiveSnapByName(partName)
 	if part != nil {
 		if err := part.Uninstall(); err != nil {
 			return err
