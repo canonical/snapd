65d099bfe81dbfcdd6ac6884cd424b258f8d213d
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-10 17:32:13 +0100

    Make set-active error when trying to set a unknown property and add support for implicit "ubuntu-core" adding when no packagename is specificed in "snappy set"

--- cmd/snappy-go/cmd_set.go
+++ cmd/snappy-go/cmd_set.go
@@ -23,25 +23,45 @@ func (x *CmdSet) Execute(args []string) (err error) {
 	return set(args)
 }
 
-func set(args []string) (err error) {
+func setActive(pkg, ver string) (err error) {
 	m := snappy.NewMetaRepository()
 	installed, err := m.Installed()
 	if err != nil {
 		return err
 	}
 
+	part := snappy.FindPartByNameAndVersion(pkg, ver, installed)
+	if part == nil {
+		return fmt.Errorf("Can not find %s with version %s", pkg, ver)
+	}
+	fmt.Printf("Setting %s to active version %s\n", pkg, ver)
+	return part.SetActive()
+}
+
+func set(args []string) (err error) {
+	// map from
+	setFuncs := map[string]func(k, v string) error{
+		"active": setActive,
+	}
+
+	// check if the first argument is of the form property=value,
+	// if so, the spec says we need to put "ubuntu-core" here
+	if strings.Contains(args[0], "=") {
+		// go version of prepend()
+		args = append([]string{"ubuntu-core"}, args...)
+	}
+
 	pkg := args[0]
 	for _, propVal := range args[1:] {
 		s := strings.Split(propVal, "=")
-		if s[0] == "active" {
-			ver := s[1]
-			part := snappy.FindPartByNameAndVersion(pkg, ver, installed)
-			if part != nil {
-				fmt.Printf("Setting %s to active version %s\n", pkg, ver)
-				return part.SetActive()
-			} else {
-				return fmt.Errorf("Can not find %s with version %s", pkg, ver)
-			}
+		prop := s[0]
+		f, ok := setFuncs[prop]
+		if !ok {
+			return fmt.Errorf("Unknown property %s", prop)
+		}
+		err := f(pkg, s[1])
+		if err != nil {
+			return err
 		}
 	}
 
