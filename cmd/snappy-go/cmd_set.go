7a7592cd41efcf832862fdcb7953fc72fb075a3b
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-10 21:19:27 +0100

    move SetProperty into "snappy" add test"

--- cmd/snappy-go/cmd_set.go
+++ cmd/snappy-go/cmd_set.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-
-	"launchpad.net/snappy/snappy"
-)
+import "launchpad.net/snappy/snappy"
 
 type CmdSet struct {
 }
@@ -23,47 +18,6 @@ func (x *CmdSet) Execute(args []string) (err error) {
 	return set(args)
 }
 
-func setActive(pkg, ver string) (err error) {
-	m := snappy.NewMetaRepository()
-	installed, err := m.Installed()
-	if err != nil {
-		return err
-	}
-
-	part := snappy.FindPartByNameAndVersion(pkg, ver, installed)
-	if part == nil {
-		return fmt.Errorf("Can not find %s with version %s", pkg, ver)
-	}
-	fmt.Printf("Setting %s to active version %s\n", pkg, ver)
-	return part.SetActive()
-}
-
 func set(args []string) (err error) {
-	// map from
-	setFuncs := map[string]func(k, v string) error{
-		"active": setActive,
-	}
-
-	// check if the first argument is of the form property=value,
-	// if so, the spec says we need to put "ubuntu-core" here
-	if strings.Contains(args[0], "=") {
-		// go version of prepend()
-		args = append([]string{"ubuntu-core"}, args...)
-	}
-
-	pkg := args[0]
-	for _, propVal := range args[1:] {
-		s := strings.Split(propVal, "=")
-		prop := s[0]
-		f, ok := setFuncs[prop]
-		if !ok {
-			return fmt.Errorf("Unknown property %s", prop)
-		}
-		err := f(pkg, s[1])
-		if err != nil {
-			return err
-		}
-	}
-
-	return err
+	return snappy.SetProperty(args)
 }
---
+++ snappy/set.go
@@ -0,0 +1,52 @@
+package snappy
+
+import (
+	"fmt"
+	"strings"
+)
+
+func setActive(pkg, ver string) (err error) {
+	m := NewMetaRepository()
+	installed, err := m.Installed()
+	if err != nil {
+		return err
+	}
+
+	part := FindPartByNameAndVersion(pkg, ver, installed)
+	if part == nil {
+		return fmt.Errorf("Can not find %s with version %s", pkg, ver)
+	}
+	fmt.Printf("Setting %s to active version %s\n", pkg, ver)
+	return part.SetActive()
+}
+
+// map from
+var setFuncs = map[string]func(k, v string) error{
+	"active": setActive,
+}
+
+func SetProperty(args []string) (err error) {
+
+	// check if the first argument is of the form property=value,
+	// if so, the spec says we need to put "ubuntu-core" here
+	if strings.Contains(args[0], "=") {
+		// go version of prepend()
+		args = append([]string{"ubuntu-core"}, args...)
+	}
+
+	pkg := args[0]
+	for _, propVal := range args[1:] {
+		s := strings.Split(propVal, "=")
+		prop := s[0]
+		f, ok := setFuncs[prop]
+		if !ok {
+			return fmt.Errorf("Unknown property %s", prop)
+		}
+		err := f(pkg, s[1])
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
