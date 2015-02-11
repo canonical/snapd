9b73631d3685ab5a242268a96290aa802be8b737
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-11 08:26:11 +0100

    allow removal of not-active snaps via "snappy remove $name=$version"

--- snappy/remove.go
+++ snappy/remove.go
@@ -1,11 +1,28 @@
 package snappy
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func Remove(partName string) error {
-	part := ActiveSnapByName(partName)
+// Remove a part by a partSpec string, this can be "name" or "name=version"
+func Remove(partSpec string) error {
+	var part Part
+	// Note that "=" is not legal in a snap name
+	if strings.Count(partSpec, "=") == 1 {
+		name := strings.Split(partSpec, "=")[0]
+		ver := strings.Split(partSpec, "=")[1]
+		installed, err := NewMetaRepository().Installed()
+		if err != nil {
+			return err
+		}
+		part = FindSnapByNameAndVersion(name, ver, installed)
+	} else {
+		part = ActiveSnapByName(partSpec)
+	}
+
 	if part == nil {
-		return fmt.Errorf("Can not find snap %s", partName)
+		return fmt.Errorf("Can not find snap %s", partSpec)
 	}
 	if err := part.Uninstall(); err != nil {
 		return err
--- snappy/remove_test.go
+++ snappy/remove_test.go
@@ -12,3 +12,42 @@ func (s *SnapTestSuite) TestRemoveNonExistingRaisesError(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(err.Error(), Equals, fmt.Sprintf("Can not find snap %s", pkgName))
 }
+
+func (s *SnapTestSuite) makeTwoTestSnaps(c *C) {
+	packageYaml := `name: foo
+icon: foo.svg
+vendor: Foo Bar <foo@example.com>
+`
+	snapFile := s.makeTestSnap(c, packageYaml+"version: 1.0")
+	c.Assert(installClick(snapFile, true), IsNil)
+
+	snapFile = s.makeTestSnap(c, packageYaml+"version: 2.0")
+	c.Assert(installClick(snapFile, true), IsNil)
+
+	m := NewMetaRepository()
+	installed, err := m.Installed()
+	c.Assert(err, IsNil)
+	c.Assert(len(installed), Equals, 2)
+}
+
+func (s *SnapTestSuite) TestSnapRemoveByVersion(c *C) {
+	s.makeTwoTestSnaps(c)
+
+	err := Remove("foo=1.0")
+
+	m := NewMetaRepository()
+	installed, err := m.Installed()
+	c.Assert(err, IsNil)
+	c.Assert(installed[0].Version(), Equals, "2.0")
+}
+
+func (s *SnapTestSuite) TestSnapRemoveActive(c *C) {
+	s.makeTwoTestSnaps(c)
+
+	err := Remove("foo")
+
+	m := NewMetaRepository()
+	installed, err := m.Installed()
+	c.Assert(err, IsNil)
+	c.Assert(installed[0].Version(), Equals, "1.0")
+}
