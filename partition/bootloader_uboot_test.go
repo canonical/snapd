a20524248bc3fcf941d38b251df91c28d5daa1d5
Michael Vogt <michael.vogt@ubuntu.com>
2015-02-11 13:09:11 +0100

    add (failing) test for TestUbootToggleRootFS

--- partition/bootloader_uboot_test.go
+++ partition/bootloader_uboot_test.go
@@ -8,6 +8,9 @@ import (
 	. "launchpad.net/gocheck"
 )
 
+const fakeUbootEnvData = `
+`
+
 func (s *PartitionTestSuite) makeFakeUbootEnv(c *C) {
 	bootloaderUbootDir = filepath.Join(s.tempdir, "boot", "uboot")
 	err := os.MkdirAll(bootloaderUbootDir, 0755)
@@ -15,6 +18,10 @@ func (s *PartitionTestSuite) makeFakeUbootEnv(c *C) {
 	// this file just needs to exist
 	bootloaderUbootConfigFile = filepath.Join(bootloaderUbootDir, "uEnv.txt")
 	err = ioutil.WriteFile(bootloaderUbootConfigFile, []byte(""), 0644)
+	c.Assert(err, IsNil)
+
+	bootloaderUbootEnvFile = filepath.Join(bootloaderUbootDir, "uEnv.txt")
+	err = ioutil.WriteFile(bootloaderUbootEnvFile, []byte(fakeUbootEnvData), 0644)
 	c.Assert(err, IsNil)
 }
 
@@ -30,4 +37,18 @@ func (s *PartitionTestSuite) TestNewUboot(c *C) {
 	partition := New()
 	u := NewUboot(partition)
 	c.Assert(u, NotNil)
+}
+
+func (s *PartitionTestSuite) TestUbootToggleRootFS(c *C) {
+	s.makeFakeUbootEnv(c)
+
+	partition := New()
+	u := NewUboot(partition)
+	c.Assert(u, NotNil)
+	err := u.ToggleRootFS()
+	c.Assert(err, IsNil)
+
+	nextBoot, err := u.GetBootVar("snappy_ab")
+	c.Assert(err, IsNil)
+	c.Assert(nextBoot, Equals, "system-b")
 }
