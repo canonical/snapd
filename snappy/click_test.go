4bd3cd8652b3a97ed0b3c5c4a7ea8ab4e9ba6960
Michael Vogt <michael.vogt@ubuntu.com>
2015-03-12 11:26:32 +0100

    move test into deb_test.go

--- snappy/click_test.go
+++ snappy/click_test.go
@@ -483,20 +483,3 @@ func (s *SnapTestSuite) TestSnappyGenerateSnapServicesFile(c *C) {
 	generated := generateSnapServicesFile(service, pkgPath, aaProfile, &m)
 	c.Assert(generated, Equals, expectedService)
 }
-
-func (s *SnapTestSuite) TestClickVerifyContentFnSimple(c *C) {
-	newPath, err := clickVerifyContentFn("foo")
-	c.Assert(err, IsNil)
-	c.Assert(newPath, Equals, "foo")
-}
-
-func (s *SnapTestSuite) TestClickVerifyContentFnStillOk(c *C) {
-	newPath, err := clickVerifyContentFn("./foo/bar/../baz")
-	c.Assert(err, IsNil)
-	c.Assert(newPath, Equals, "foo/baz")
-}
-
-func (s *SnapTestSuite) TestClickVerifyContentFnNotOk(c *C) {
-	_, err := clickVerifyContentFn("./foo/../../baz")
-	c.Assert(err, Equals, ErrSnapInvalidContent)
-}
---
+++ snappy/deb_test.go
@@ -0,0 +1,23 @@
+package snappy
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+
+func (s *SnapTestSuite) TestClickVerifyContentFnSimple(c *C) {
+	newPath, err := clickVerifyContentFn("foo")
+	c.Assert(err, IsNil)
+	c.Assert(newPath, Equals, "foo")
+}
+
+func (s *SnapTestSuite) TestClickVerifyContentFnStillOk(c *C) {
+	newPath, err := clickVerifyContentFn("./foo/bar/../baz")
+	c.Assert(err, IsNil)
+	c.Assert(newPath, Equals, "foo/baz")
+}
+
+func (s *SnapTestSuite) TestClickVerifyContentFnNotOk(c *C) {
+	_, err := clickVerifyContentFn("./foo/../../baz")
+	c.Assert(err, Equals, ErrSnapInvalidContent)
+}
