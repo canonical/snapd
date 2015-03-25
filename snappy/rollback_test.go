07a78f4a152a2a0f1a911776ff126f4829d1ce1c
Michael Vogt <michael.vogt@ubuntu.com>
2015-03-25 10:06:46 +0100

    add tests for rollback

---
+++ snappy/rollback_test.go
@@ -0,0 +1,27 @@
+package snappy
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+func (s *SnapTestSuite) TestRollbackWithVersion(c *C) {
+	makeTwoTestSnaps(c, SnapTypeApp)
+	c.Assert(ActiveSnapByName("foo").Version(), Equals, "2.0")
+
+	// rollback with version
+	err := Rollback("foo", "1.0")
+	c.Assert(err, IsNil)
+
+	c.Assert(ActiveSnapByName("foo").Version(), Equals, "1.0")
+}
+
+func (s *SnapTestSuite) TestRollbackFindVersion(c *C) {
+	makeTwoTestSnaps(c, SnapTypeApp)
+	c.Assert(ActiveSnapByName("foo").Version(), Equals, "2.0")
+
+	// rollback without version
+	err := Rollback("foo", "")
+	c.Assert(err, IsNil)
+
+	c.Assert(ActiveSnapByName("foo").Version(), Equals, "1.0")
+}
