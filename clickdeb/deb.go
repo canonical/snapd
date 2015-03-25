c7190ba141e3da48de19a2260704b396da5e6b49
John R. Lenton <jlenton@gmail.com>
2015-03-25 21:48:44 +0000

    oops, commited printlns

--- clickdeb/deb.go
+++ clickdeb/deb.go
@@ -81,7 +81,6 @@ func (d *ClickDeb) member(arMember, tarMember string) (content []byte, err error
 	defer file.Close()
 
 	arReader := ar.NewReader(file)
-	println("***", arMember)
 	dataReader, err := skipToArMember(arReader, arMember)
 	if err != nil {
 		return nil, err
@@ -336,7 +335,6 @@ func skipToArMember(arReader *ar.Reader, memberPrefix string) (io.Reader, error)
 		if err != nil {
 			return nil, err
 		}
-		println("`--", header.Name, "...", memberPrefix)
 		if strings.HasPrefix(header.Name, memberPrefix) {
 			break
 		}
