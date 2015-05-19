f901bfb39ceb8faf14fd17a5cf626cea5fa9a38a
Michael Vogt <michael.vogt@ubuntu.com>
2015-05-19 16:08:42 +0200

    mdlint.py: add exit code

--- mdlint.py
+++ mdlint.py
@@ -8,20 +8,26 @@ import sys
 
 def lint_li(fname, text):
     """Ensure that the list-items are multiplies of 4"""
+    is_clean = True
     for i, line in enumerate(text.splitlines()):
         if line.lstrip().startswith("*") and line.index("*") % 4 != 0:
             print("%s: line %i list has non-4 spaces indent" % (fname, i))
+            is_clean = False
+    return is_clean
 
 
 def lint(md_files):
     """lint all md files"""
+    all_clean = True
     for md in md_files:
         with open(md) as f:
             buf = f.read()
             for fname, func in globals().items():
                 if fname.startswith("lint_"):
-                    func(md, buf)
-        
+                    all_clean &= func(md, buf)
+    return all_clean
+
 
 if __name__ == "__main__":
-    lint(sys.argv)
+    if not lint(sys.argv):
+        sys.exit(1)
