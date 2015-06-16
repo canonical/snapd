12e3407792d6bf647a590f3e2159b9f6f6c85ae7
Leonardo Arias Fonseca <leo.arias@canonical.com>
2015-06-15 22:47:40 -0600

    Added a tear down that removes the test snap.

--- _integration-tests/tests/snappy_test.go
+++ _integration-tests/tests/snappy_test.go
@@ -14,6 +14,12 @@ var _ = Suite(&InstallSuite{})
 
 type InstallSuite struct{}
 
+func (s *InstallSuite) TearDownTest(c *C) {
+	uninstallCommand := exec.Command("sudo", "snappy", "hello-world")
+	_, uninstallErr := installCommand.CombinedOutput()
+	c.Assert(uninstallErr, IsNil)
+}
+
 func (s *InstallSuite) TestInstallSnapp(c *C) {
 	installCommand := exec.Command("sudo", "snappy", "install", "hello-world")
 	_, installErr := installCommand.CombinedOutput()
