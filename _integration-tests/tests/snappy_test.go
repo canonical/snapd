008331d7a5b9cefeaef07cd641d3f4d14c0b826e
Leonardo Arias Fonseca <leo.arias@canonical.com>
2015-06-15 23:25:34 -0600

    Stop autopilot.

--- _integration-tests/tests/snappy_test.go
+++ _integration-tests/tests/snappy_test.go
@@ -14,21 +14,26 @@ var _ = Suite(&InstallSuite{})
 
 type InstallSuite struct{}
 
+func (s *InstallSuite) execCommand(c *C, cmds ...string) []byte {
+	cmd := exec.Command(cmds[0], cmds[1:len(cmds)]...)
+	output, err := cmd.CombinedOutput()
+	c.Assert(err, IsNil, Commentf("Error: %v", output))
+	return output
+}
+
+func (s *InstallSuite) SetUpSuite(c *C) {
+	s.execCommand(c, "sudo", "systemctl", "stop", "snappy-autopilot.timer")
+}
+
 func (s *InstallSuite) TearDownTest(c *C) {
-	uninstallCommand := exec.Command("sudo", "snappy", "hello-world")
-	_, uninstallErr := uninstallCommand.CombinedOutput()
-	c.Assert(uninstallErr, IsNil)
+	s.execCommand(c, "sudo", "snappy", "hello-world")
 }
 
 func (s *InstallSuite) TestInstallSnapp(c *C) {
-	installCommand := exec.Command("sudo", "snappy", "install", "hello-world")
-	_, installErr := installCommand.CombinedOutput()
+	s.execCommand(c, "sudo", "snappy", "install", "hello-world")
 
-	c.Assert(installErr, IsNil)
+	echoOutput := s.execCommand(c, "hello-world.echo")
 
-	echoCommand := exec.Command("hello-world.echo")
-	echoOutput, echoErr := echoCommand.CombinedOutput()
-
-	c.Assert(echoErr, IsNil)
+	// Assert the output of the hello-world.echo command.
 	c.Assert(string(echoOutput), Equals, "Hello World!\n")
 }
