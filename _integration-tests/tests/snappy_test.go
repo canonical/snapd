ab001d8fb182d6bd9c6fa28601312e80fb219872
Federico Gimenez <fgimenez@canonical.com>
2015-06-17 13:12:08 +0200

    Changes for adding failover_test file

--- _integration-tests/tests/snappy_test.go
+++ _integration-tests/tests/snappy_test.go
@@ -1,33 +1,8 @@
-package snappy
+package tests
 
-import (
-	"os/exec"
-	"testing"
-
-	. "gopkg.in/check.v1"
-)
-
-// Hook up gocheck into the "go test" runner
-func Test(t *testing.T) { TestingT(t) }
+import . "gopkg.in/check.v1"
 
 var _ = Suite(&InstallSuite{})
-
-type InstallSuite struct{}
-
-func (s *InstallSuite) installSnap(c *C, packageName string) []byte {
-	return s.execCommand(c, "sudo", "snappy", "install", packageName)
-}
-
-func (s *InstallSuite) execCommand(c *C, cmds ...string) []byte {
-	cmd := exec.Command(cmds[0], cmds[1:len(cmds)]...)
-	output, err := cmd.CombinedOutput()
-	c.Assert(err, IsNil, Commentf("Error: %v", output))
-	return output
-}
-
-func (s *InstallSuite) SetUpSuite(c *C) {
-	s.execCommand(c, "sudo", "systemctl", "stop", "snappy-autopilot.timer")
-}
 
 func (s *InstallSuite) TearDownTest(c *C) {
 	s.execCommand(c, "sudo", "snappy", "remove", "hello-world")
