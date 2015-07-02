1b12ab9db4ec1362e022413335ceed66ee5793e5
Michael Vogt <michael.vogt@ubuntu.com>
2015-07-02 14:25:19 +0200

    use xgettext-go by default, refactor xgettext-go to be more modular

--- i18n/xgettext-go/main.go
+++ i18n/xgettext-go/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -13,8 +13,7 @@ import (
 var gettextSelector = "i18n"
 var gettextFuncName = "G"
 
-const header = `
-# SOME DESCRIPTIVE TITLE.
+const header = `# SOME DESCRIPTIVE TITLE.
 # Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
 # This file is distributed under the same license as the PACKAGE package.
 # FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
@@ -34,51 +33,66 @@ msgstr  "Project-Id-Version: snappy\n"
 
 `
 
-func main() {
-	fname := os.Args[1]
+func findCommentsForTranslation(fset *token.FileSet, f *ast.File, posCall token.Position) string {
+	com := ""
+	for _, cg := range f.Comments {
+		// search for all comments in the previous line
+		for i := len(cg.List) - 1; i >= 0; i-- {
+			c := cg.List[i]
+			posComment := fset.Position(c.Slash)
+			//println(posCall.Line, posComment.Line, c.Text)
+			if posCall.Line == posComment.Line+1 {
+				posCall = posComment
+				com = fmt.Sprintf("#. %s\n%s", c.Text, com)
+			}
+		}
+	}
+	return com
+}
+
+func inspectNodeForTranslations(fset *token.FileSet, f *ast.File, n ast.Node) bool {
+	switch x := n.(type) {
+	case *ast.CallExpr:
+		if sel, ok := x.Fun.(*ast.SelectorExpr); ok {
+			if sel.Sel.Name == gettextFuncName && sel.X.(*ast.Ident).Name == gettextSelector {
+				i18nStr := x.Args[0].(*ast.BasicLit).Value
+				// strip " (or `)
+				i18nStr = i18nStr[1 : len(i18nStr)-1]
+				posCall := fset.Position(n.Pos())
+
+				fmt.Printf("#: %s:%d\n", posCall.Filename, posCall.Line)
+				fmt.Printf("%s", findCommentsForTranslation(fset, f, posCall))
+				fmt.Printf("msgid \"%v\"\n", strings.Replace(i18nStr, "\n", "\\n", -1))
+				fmt.Printf("msgstr \"\"\n\n")
+			}
+		}
+	}
+	return true
+}
+
+func processSingleGoSource(fset *token.FileSet, fname string) {
 	fnameContent, err := ioutil.ReadFile(fname)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Create the AST by parsing src.
-	fset := token.NewFileSet() // positions are relative to fset
 	f, err := parser.ParseFile(fset, fname, fnameContent, parser.ParseComments)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(header)
-	
 	ast.Inspect(f, func(n ast.Node) bool {
-		var i18nStr string
-		switch x := n.(type) {
-		case *ast.CallExpr:
-			if sel, ok := x.Fun.(*ast.SelectorExpr); ok {
-				if sel.Sel.Name == gettextFuncName && sel.X.(*ast.Ident).Name == gettextSelector {
-					i18nStr = x.Args[0].(*ast.BasicLit).Value
-					// strip " (or `)
-					i18nStr = i18nStr[1:len(i18nStr)-1]
-					posCall := fset.Position(n.Pos())
-					com := ""
-					for _, cg := range(f.Comments) {
-						for i := len(cg.List)-1; i >= 0; i-- {
-							c := cg.List[i]
-							posComment := fset.Position(c.Slash)
-							//println(posCall.Line, posComment.Line, c.Text)
-							if posCall.Line == posComment.Line+1 {
-								posCall = posComment
-								com = fmt.Sprintf("#. %s\n%s", c.Text, com)
-							}
-						}
-					}
-					fmt.Printf("%s", com)
-					fmt.Printf("msgid \"%v\"\n", strings.Replace(i18nStr, "\n", "\\n", -1))
-					fmt.Printf("msgstr \"\"\n\n")
-				}
-			}
-		}
-		return true
+		return inspectNodeForTranslations(fset, f, n)
 	})
 
+}
+
+func main() {
+	fmt.Println(header)
+
+	fset := token.NewFileSet() // positions are relative to fset
+	for _, fname := range os.Args[1:] {
+		processSingleGoSource(fset, fname)
+	}
 }
