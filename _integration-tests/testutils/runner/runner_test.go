7e2d56dd1b859650b8297cf481fcbf8aaabeb7aa
Federico Gimenez <fgimenez@canonical.com>
2015-08-13 10:22:35 +0200

    Using bytes.Buffer instead of the custom String type

--- _integration-tests/testutils/runner/runner_test.go
+++ _integration-tests/testutils/runner/runner_test.go
@@ -20,27 +20,13 @@
 package runner
 
 import (
+	"bytes"
 	"flag"
 	"regexp"
 	"testing"
 
 	check "gopkg.in/check.v1"
 )
-
-// -----------------------------------------------------------------------
-// Helper type implementing a basic io.Writer for testing output.
-
-// Type implementing the io.Writer interface for analyzing output.
-type String struct {
-	value string
-}
-
-// The only function required by the io.Writer interface.  Will append
-// written data to the String.value string.
-func (s *String) Write(p []byte) (n int, err error) {
-	s.value += string(p)
-	return len(p), nil
-}
 
 var _ = check.Suite(&GenericTestSuite{})
 
@@ -59,17 +45,17 @@ func TestRunnerPipesOutput(t *testing.T) {
 	flag.Set("check.v", "true")
 	defer flag.Set("check.v", "false")
 
-	output := &String{}
+	var output bytes.Buffer
 	// passing here a different *testing.T so that the results
 	// of the tests in the target suite do not pollute the results of
 	// this test
-	TestingT(new(testing.T), output)
+	TestingT(new(testing.T), &output)
 
 	expectedOutput := `(?msi).*FAIL: runner_test.go:.*: GenericTestSuite.TestFail.*PASS: runner_test.go:.*: GenericTestSuite.TestSuccess.*`
 
-	if match, _ := regexp.MatchString(expectedOutput, output.value); !match {
+	if match, _ := regexp.MatchString(expectedOutput, output.String()); !match {
 		t.Errorf("Expected value not obtained in the output writer!! Expected: %s, Actual: %s",
-			expectedOutput, output.value)
+			expectedOutput, output.String())
 	}
 }
 
@@ -77,17 +63,17 @@ func TestRunnerAcceptsStreamFlag(t *testing.T) {
 	flag.Set("check.vv", "true")
 	defer flag.Set("check.vv", "false")
 
-	output := &String{}
-	TestingT(new(testing.T), output)
+	var output bytes.Buffer
+	TestingT(new(testing.T), &output)
 
 	expectedOutput := `(?msi)START: runner_test.go:.*: GenericTestSuite.TestFail.*
 FAIL: runner_test.go:.*: GenericTestSuite.TestFail.*
 START: runner_test.go:.*: GenericTestSuite.TestSuccess
 PASS: runner_test.go:.*: GenericTestSuite.TestSuccess.*`
 
-	if match, _ := regexp.MatchString(expectedOutput, output.value); !match {
+	if match, _ := regexp.MatchString(expectedOutput, output.String()); !match {
 		t.Errorf("Expected value not obtained in the output writer!! Expected: %s, Actual: %s",
-			expectedOutput, output.value)
+			expectedOutput, output.String())
 	}
 }
 
@@ -95,13 +81,13 @@ func TestRunnerAcceptsFilterFlag(t *testing.T) {
 	flag.Set("check.f", "GenericTestSuite.TestSuccess")
 	defer flag.Set("check.f", "")
 
-	output := &String{}
-	TestingT(new(testing.T), output)
+	var output bytes.Buffer
+	TestingT(new(testing.T), &output)
 
 	unExpectedOutput := `(?msi).*FAIL: runner_test.go:.*: GenericTestSuite.TestFail.*`
 
-	if match, _ := regexp.MatchString(unExpectedOutput, output.value); match {
+	if match, _ := regexp.MatchString(unExpectedOutput, output.String()); match {
 		t.Errorf("Unexpected value obtained in the output writer!! Unexpected: %s, Actual: %s",
-			unExpectedOutput, output.value)
+			unExpectedOutput, output.String())
 	}
 }
