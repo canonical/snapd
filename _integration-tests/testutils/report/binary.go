0e913981d6fe9364fff071f6f3568ac1b47259ec
Federico Gimenez <fgimenez@canonical.com>
2015-08-14 13:49:02 +0200

    binary initial files

---
+++ _integration-tests/testutils/report/binary.go
@@ -0,0 +1,29 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2015 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package report
+
+import "io"
+
+// BinaryReporter is a type implementing io.Writer that
+// sends to its Next member the given data encoded according
+// to the subunit binary standard
+type BinaryReporter struct {
+	Next io.Writer
+}
