28a9734139761cfa8450edeb4dcdbc536af592c5
Federico Gimenez <fgimenez@canonical.com>
2015-09-24 16:17:28 +0200

    modprobe test

---
+++ _integration-tests/tests/config_test.go
@@ -0,0 +1,79 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2015 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package tests
+
+import (
+	"io/ioutil"
+	"os"
+
+	"launchpad.net/snappy/_integration-tests/testutils/common"
+
+	"gopkg.in/check.v1"
+)
+
+var _ = check.Suite(&configSuite{})
+
+type configSuite struct {
+	common.SnappySuite
+	backConfig string
+}
+
+func (s *configSuite) SetUpTest(c *check.C) {
+	s.backConfig = actualConfig(c)
+}
+
+func (s *configSuite) TearDownTest(c *check.C) {
+	s.setConfig(c, s.backConfig)
+}
+
+func (s *configSuite) TestModprobe(c *check.C) {
+	targetCfg := `modprobe: blacklist floppy`
+	modprobeConfig := configString(targetCfg)
+
+	err := s.setConfig(c, modprobeConfig)
+	c.Assert(err, check.IsNil)
+
+	config := actualConfig(c)
+
+	c.Assert(config, check.Matches, "(?Usm).*"+targetCfg+".*")
+}
+
+func (s *configSuite) setConfig(c *check.C, config string) (err error) {
+	configFile, err := ioutil.TempFile("", "snappy-cfg")
+	defer func() { configFile.Close(); os.Remove(configFile.Name()) }()
+	if err != nil {
+		return
+	}
+	_, err = configFile.Write([]byte(config))
+
+	common.ExecCommand(c, "sudo", "snappy", "config", "ubuntu-core", configFile.Name())
+
+	return
+}
+
+func actualConfig(c *check.C) string {
+	return common.ExecCommand(c, "snappy", "config", "ubuntu-core")
+}
+
+func configString(cfg string) string {
+	return `config:
+  ubuntu-core:
+    ` + cfg
+}
