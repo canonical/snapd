366633cea51b082f1256dcff4187e5e6f4f1e29e
Leo Arias <leo.arias@canonical.com>
2015-10-14 19:34:46 -0600

    Added error messages to all the assertions and check of the integration tests.

--- _integration-tests/tests/rollback_test.go
+++ _integration-tests/tests/rollback_test.go
@@ -46,14 +46,16 @@ func (s *rollbackSuite) TestRollbackMustRebootToOtherVersion(c *check.C) {
 		// TODO remove once the bug is fixed. --elopio - 2015-09-30
 		wait.ForFunction(c, "regular", partition.Mode)
 		currentVersion := common.GetCurrentUbuntuCoreVersion(c)
-		c.Assert(currentVersion > common.GetSavedVersion(c), check.Equals, true)
+		c.Assert(currentVersion > common.GetSavedVersion(c), check.Equals, true,
+			check.Commentf("Rebooted to the wrong version: %d", currentVersion))
 		cli.ExecCommand(c, "sudo", "snappy", "rollback", "ubuntu-core",
 			strconv.Itoa(common.GetSavedVersion(c)))
 		common.SetSavedVersion(c, currentVersion)
 		common.RebootWithMark(c, c.TestName()+"-rollback")
 	} else if common.CheckRebootMark(c.TestName() + "-rollback") {
 		common.RemoveRebootMark(c)
-		c.Assert(
-			common.GetCurrentUbuntuCoreVersion(c) < common.GetSavedVersion(c), check.Equals, true)
+		currentVersion := common.GetCurrentUbuntuCoreVersion(c)
+		c.Assert(currentVersion < common.GetSavedVersion(c), check.Equals, true,
+			check.Commentf("Rebooted to the wrong version: %d", currentVersion))
 	}
 }
