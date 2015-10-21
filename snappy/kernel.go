2ebd1d2c9182c92d24114f7fb095bea28a563b0c
Michael Vogt <michael.vogt@ubuntu.com>
2015-10-21 14:39:57 +0200

    snappy/kernel.go: use GlobalRootDir in unpackKernel

--- snappy/kernel.go
+++ snappy/kernel.go
@@ -23,12 +23,14 @@ import (
 	"os"
 	"path/filepath"
 
+	"launchpad.net/snappy/dirs"
 	"launchpad.net/snappy/partition"
 	"launchpad.net/snappy/pkg/snapfs"
 )
 
 func unpackKernel(s *SnapPart) error {
-	bootdir := partition.BootloaderDir()
+	bootdir := filepath.Join(dirs.GlobalRootDir, partition.BootloaderDir())
+		
 	if err := os.MkdirAll(filepath.Join(bootdir, s.Version()), 0755); err !=
 		nil {
 		return err
