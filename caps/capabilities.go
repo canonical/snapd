beebd709d62dbbc645c21acca100c5ff5f7d355d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-11-16 11:35:56 +0100

    Tweak comments on caps.Capability
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- caps/capabilities.go
+++ caps/capabilities.go
@@ -25,24 +25,23 @@ import (
 	"sort"
 )
 
-// Type defines the "kind" of a capability.
-// Each type defines the behavior allowed and expected from providers
-// and consumers of that capability, and also which information should
-// be exchanged by these parties.
+// Type is the name of a capability type.
 type Type string
 
 // Capability holds information about a capability that a snap may request
 // from a snappy system to do its job while running on it.
 type Capability struct {
-	// Name is a key that identifies the capability. It must be unique within its
-	// context, which may be either a snap or a snappy runtime.
+	// Name is a key that identifies the capability. It must be unique within
+	// its context, which may be either a snap or a snappy runtime.
 	Name string
 	// Label provides an optional title for the capability to help a human tell
 	// which physical device this capability is referring to. It might say
 	// "Front USB", or "Green Serial Port", for example.
 	Label string
-	// CapabilityType describes a group of capabilities sharing some common
-	// traits. In particular, this is where security bits are coming from.
+	// Type defines the type of this capability. The capability type defines
+	// the behavior allowed and expected from providers and consumers of that
+	// capability, and also which information should be exchanged by these
+	// parties.
 	Type Type
 }
 
