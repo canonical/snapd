fb9c3a9d7ac4f39caea440e34dc601c2ac795a47
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-11-17 17:07:54 +0100

    Compact the text of sorting.Interface for caps.Repository
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- caps/capabilities.go
+++ caps/capabilities.go
@@ -126,17 +126,9 @@ func (c Capability) String() string {
 
 type byName []Capability
 
-func (c byName) Len() int {
-	return len(c)
-}
-
-func (c byName) Swap(i, j int) {
-	c[i], c[j] = c[j], c[i]
-}
-
-func (c byName) Less(i, j int) bool {
-	return c[i].Name < c[j].Name
-}
+func (c byName) Len() int           { return len(c) }
+func (c byName) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c byName) Less(i, j int) bool { return c[i].Name < c[j].Name }
 
 // All capabilities, sorted by name
 func (r *Repository) All() []Capability {
