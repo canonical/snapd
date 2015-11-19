canonical/snapd

commit 8b04bd2dce7915231b91ceef9711c0fcb90ebd57

fix bug in CompareGeneratePolicyFromFile for sideloaded pkgs

snappy/security.go
@@ -827,10 +827,11 @@ func compareSinglePolicyToCurrent(oldPolicyFn, newPolicy string) error {
 // CompareGeneratePolicyFromFile is used to simulate security policy
 // generation and returns if the policy would have changed
 func CompareGeneratePolicyFromFile(fn string) error {
-	m, err := parsePackageYamlFile(fn)
+	m, err := parsePackageYamlFileWithVersion(fn)
 	if err != nil {
 		return err
 	}
+
 	baseDir := filepath.Dir(filepath.Dir(fn))
 
 	for _, service := range m.ServiceYamls {
@@ -872,18 +873,25 @@ func CompareGeneratePolicyFromFile(fn string) error {
 	return nil
 }
 
+// FIXME: refactor so that we don't need this
+func parsePackageYamlFileWithVersion(fn string) (*packageYaml, error) {
+	m, err := parsePackageYamlFile(fn)
+
+	// FIXME: duplicated code from snapp.go:NewSnapPartFromYaml,
+	//        version is overriden by sideloaded versions
+	m.Version = filepath.Base(filepath.Dir(filepath.Dir(fn)))
+
+	return m, err
+}
+
 // GeneratePolicyFromFile is used to generate security policy on the system
 // from the specified manifest file name
 func GeneratePolicyFromFile(fn string, force bool) error {
 	// FIXME: force not used yet
-
-	m, err := parsePackageYamlFile(fn)
+	m, err := parsePackageYamlFileWithVersion(fn)
 	if err != nil {
 		return err
 	}
-	// FIXME: duplicated code from snapp.go:NewSnapPartFromYaml,
-	//        version is overriden by sideloaded versions
-	m.Version = filepath.Base(filepath.Dir(filepath.Dir(fn)))
 
 	if m.Type == "" || m.Type == pkg.TypeApp {
 		_, err = originFromYamlPath(fn)

snappy/security_test.go
@@ -931,11 +931,7 @@ func (a *SecurityTestSuite) TestSecurityWarnsOnDeprecatedSeccomp(c *C) {
 	}
 }
 
-func (a *SecurityTestSuite) TestSecurityGeneratePolicyFromFileSideload(c *C) {
-	// we need to create some fake data
-	makeMockApparmorTemplate(c, "default", []byte(``))
-	makeMockSeccompTemplate(c, "default", []byte(``))
-
+func makeInstalledMockSnapSideloaded(c *C) string {
 	mockPackageYamlFn, err := makeInstalledMockSnap(dirs.GlobalRootDir, mockSecurityPackageYaml)
 	c.Assert(err, IsNil)
 	// pretend its sideloaded
@@ -945,8 +941,18 @@ func (a *SecurityTestSuite) TestSecurityGeneratePolicyFromFileSideload(c *C) {
 	err = os.Rename(oldPath, newPath)
 	mockPackageYamlFn = filepath.Join(basePath, "IsSideloadVer", "meta", "package.yaml")
 
+	return mockPackageYamlFn
+}
+
+func (a *SecurityTestSuite) TestSecurityGeneratePolicyFromFileSideload(c *C) {
+	// we need to create some fake data
+	makeMockApparmorTemplate(c, "default", []byte(``))
+	makeMockSeccompTemplate(c, "default", []byte(``))
+
+	mockPackageYamlFn := makeInstalledMockSnapSideloaded(c)
+
 	// the acutal thing that gets tested
-	err = GeneratePolicyFromFile(mockPackageYamlFn, false)
+	err := GeneratePolicyFromFile(mockPackageYamlFn, false)
 	c.Assert(err, IsNil)
 
 	// ensure the apparmor policy got loaded
@@ -959,6 +965,21 @@ func (a *SecurityTestSuite) TestSecurityGeneratePolicyFromFileSideload(c *C) {
 	// ... and seccomp
 	generatedProfileFn = filepath.Join(dirs.SnapSeccompDir, fmt.Sprintf("hello-world.%s_binary1_IsSideloadVer", testOrigin))
 	c.Assert(helpers.FileExists(generatedProfileFn), Equals, true)
+}
+
+func (a *SecurityTestSuite) TestSecurityCompareGeneratePolicyFromFileSideload(c *C) {
+	// we need to create some fake data
+	makeMockApparmorTemplate(c, "default", []byte(``))
+	makeMockSeccompTemplate(c, "default", []byte(``))
+
+	mockPackageYamlFn := makeInstalledMockSnapSideloaded(c)
+	// generate policy
+	err := GeneratePolicyFromFile(mockPackageYamlFn, false)
+	c.Assert(err, IsNil)
+
+	// nothing changed, ensure compare is happy even for sideloaded pkgs
+	err = CompareGeneratePolicyFromFile(mockPackageYamlFn)
+	c.Assert(err, IsNil)
 }
 
 func (a *SecurityTestSuite) TestSecurityGeneratePolicyForServiceBinaryFramework(c *C) {

commit f61d7ad2b4faeb1c8e95bb44157c4ae6e7955e56

use EvalSymlinks to resolve 'current' to the correct version
'snappy policygen' takes a path to a yaml file as an argument. If the user
specifies /apps/foo/current/meta/package.yaml then the version is incorrectly
calculated to be 'current'. This issue was introduced in 8b04bd2d when fixing
CompareGeneratePolicyFromFile for sideloaded pkgs.

The fix adjusts parsePackageYamlFileWithVersion() to simply call EvalSymlinks()
to resolve the current symlink. Tested with store and sideloaded apps.

snappy/security.go
@@ -882,7 +882,13 @@ func parsePackageYamlFileWithVersion(fn string) (*packageYaml, error) {
 
 	// FIXME: duplicated code from snapp.go:NewSnapPartFromYaml,
 	//        version is overriden by sideloaded versions
-	m.Version = filepath.Base(filepath.Dir(filepath.Dir(fn)))
+
+	// use EvalSymlinks to resolve 'current' to the correct version
+	dir, err := filepath.EvalSymlinks(filepath.Dir(filepath.Dir(fn)))
+	if err != nil {
+		return nil, err
+	}
+	m.Version = filepath.Base(dir)
 
 	return m, err
 }

snappy/security_test.go
@@ -35,6 +35,7 @@ import (
 )
 
 type SecurityTestSuite struct {
+	tempDir               string
 	buildDir              string
 	m                     *packageYaml
 	scFilterGenCall       []string
@@ -47,6 +48,7 @@ var _ = Suite(&SecurityTestSuite{})
 
 func (a *SecurityTestSuite) SetUpTest(c *C) {
 	a.buildDir = c.MkDir()
+	a.tempDir = c.MkDir()
 	os.MkdirAll(filepath.Join(a.buildDir, "meta"), 0755)
 
 	// set global sandbox
@@ -1016,4 +1018,34 @@ func (a *SecurityTestSuite) TestSecurityGeneratePolicyForServiceBinaryErrors(c *
 	// ensure invalid packages generate an error
 	err := sd.generatePolicyForServiceBinary(m, "binary", "/apps/app-no-origin/1.0")
 	c.Assert(err, ErrorMatches, "invalid package on system")
+}
+
+func (s *SecurityTestSuite) TestParsePackageYamlWithVersion(c *C) {
+	testVersion := "1.0"
+	dir := filepath.Join(s.tempDir, "foo", testVersion, "meta")
+	os.MkdirAll(dir, 0755)
+	y := filepath.Join(dir, "package.yaml")
+	ioutil.WriteFile(y, []byte(`
+name: foo
+version: 123456789
+`), 0644)
+	m, err := parsePackageYamlFileWithVersion(y)
+	c.Assert(err, IsNil)
+	c.Assert(m.Version, Equals, testVersion)
+}
+
+func (s *SecurityTestSuite) TestParsePackageYamlWithVersionSymlink(c *C) {
+	testVersion := "1.0"
+	verDir := filepath.Join(s.tempDir, "foo", testVersion)
+	symDir := filepath.Join(s.tempDir, "foo", "current")
+	os.MkdirAll(filepath.Join(verDir, "meta"), 0755)
+	os.Symlink(verDir, symDir)
+	y := filepath.Join(symDir, "meta", "package.yaml")
+	ioutil.WriteFile(y, []byte(`
+name: foo
+version: 123456789
+`), 0644)
+	m, err := parsePackageYamlFileWithVersion(y)
+	c.Assert(err, IsNil)
+	c.Assert(m.Version, Equals, testVersion)
 }