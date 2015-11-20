5613b1badcfa4d4c571b4c7fd1f7869a5be2415f
Samuele Pedroni <pedronis@lucediurna.net>
2015-11-20 10:55:22 +0100

    use the style recommended in a different review

--- asserts/asserts_test.go
+++ asserts/asserts_test.go
@@ -114,15 +114,15 @@ func (as *AssertsSuite) TestDecodeNoSignatureSplit(c *C) {
 }
 
 func (as *AssertsSuite) TestDecodeHeaderParsingErrors(c *C) {
-	for _, scen := range []struct {
-		encoded, expectedErr string
-	}{
+	headerParsingErrorsTests := []struct{ encoded, expectedErr string }{
 		{string([]byte{255, '\n', '\n'}), "header is not utf8"},
 		{"foo: a\nbar\n\n", "header entry missing name value ': ' separation: \"bar\""},
 		{"TYPE: foo\n\n", `invalid header name: "TYPE"`},
-	} {
-		_, err := asserts.Decode([]byte(scen.encoded))
-		c.Check(err, ErrorMatches, "parsing assertion headers: "+scen.expectedErr)
+	}
+
+	for _, test := range headerParsingErrorsTests {
+		_, err := asserts.Decode([]byte(test.encoded))
+		c.Check(err, ErrorMatches, "parsing assertion headers: "+test.expectedErr)
 	}
 }
 
@@ -136,9 +136,7 @@ func (as *AssertsSuite) TestDecodeInvalid(c *C) {
 		"\n\n" +
 		"openpgp c2ln"
 
-	for _, scen := range []struct {
-		original, invalid, expectedErr string
-	}{
+	invalidAssertTests := []struct{ original, invalid, expectedErr string }{
 		{"body-length: 5", "body-length: z", "assertion body-length is not an integer: z"},
 		{"body-length: 5", "body-length: 3", "assertion body length and declared body-length don't match: 5 != 3"},
 		{"authority-id: auth-id\n", "", "assertion authority-id header is mandatory"},
@@ -148,12 +146,13 @@ func (as *AssertsSuite) TestDecodeInvalid(c *C) {
 		{"type: test-only\n", "type: unknown\n", "cannot build assertion of unknown type: unknown"},
 		{"revision: 0\n", "revision: Z\n", "assertion revision is not an integer: Z"},
 		{"revision: 0\n", "revision: -10\n", "assertion revision should be positive: -10"},
-	} {
-		invalid := strings.Replace(encoded, scen.original, scen.invalid, 1)
-		_, err := asserts.Decode([]byte(invalid))
-		c.Check(err, ErrorMatches, scen.expectedErr)
 	}
 
+	for _, test := range invalidAssertTests {
+		invalid := strings.Replace(encoded, test.original, test.invalid, 1)
+		_, err := asserts.Decode([]byte(invalid))
+		c.Check(err, ErrorMatches, test.expectedErr)
+	}
 }
 
 func (as *AssertsSuite) TestEncode(c *C) {
