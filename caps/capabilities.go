fb42b48bb9e566b2c5c76956fce0c7d09bd116e3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-11-22 13:09:06 +0100

    Add to-from JSON converters for caps.Capability
    This patch adds two things.
    
    First is the new type caps.CapabilityRepr which represents a capability
    in a manner that is friendly to JSON serialization. In particular the
    caps.Capability.Type field is replaced with TypeName (string). Struct
    Tags are used to make JSON representation of CapabilityRepr look nice
    and friendly.
    
    The second thing is a pair of converter functions that assist in making
    the change from one to the other.
    
    Both are essential to the upcoming REST API.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- caps/capabilities.go
+++ caps/capabilities.go
@@ -51,3 +51,45 @@ var (
 func (c Capability) String() string {
 	return c.Name
 }
+
+// CapabilityRepr is the JSON representation of a capability.
+// It exists so that Type can be replaced by Type.Name as we don't want to
+// create or describe capabilities fully, just to refer to them.
+type CapabilityRepr struct {
+	// Name is a key that identifies the capability. It must be unique within
+	// its context, which may be either a snap or a snappy runtime.
+	Name string `json:"name"`
+	// Label provides an optional title for the capability to help a human tell
+	// which physical device this capability is referring to. It might say
+	// "Front USB", or "Green Serial Port", for example.
+	Label string `json:"label"`
+	// Type defines the type of this capability. The capability type defines
+	// the behavior allowed and expected from providers and consumers of that
+	// capability, and also which information should be exchanged by these
+	// parties.
+	TypeName string `json:"type_name"`
+	// Attrs are key-value pairs that provide type-specific capability details.
+	Attrs map[string]string `json:"attrs"`
+}
+
+// ConvertToRepr makes a CapabilityRepr from a Capability.
+// This function is useful for creating JSON representation of a capability
+// that abbreviates the full definition of a Type to just the Type.Name.
+func (c *Capability) ConvertToRepr() *CapabilityRepr {
+	return &CapabilityRepr{
+		Name:     c.Name,
+		Label:    c.Label,
+		TypeName: c.Type.Name,
+		Attrs:    c.Attrs,
+	}
+}
+
+// ConvertToCap makes a Capability from a CapabilityRepr.
+func (r *CapabilityRepr) ConvertToCap(typeLookupFn TypeLookupFn) *Capability {
+	return &Capability{
+		Name:  r.Name,
+		Label: r.Label,
+		Type:  typeLookupFn(r.TypeName),
+		Attrs: r.Attrs,
+	}
+}
