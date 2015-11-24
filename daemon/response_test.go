3f12160cc31171fa499624568191d330945ce202
John R. Lenton <jlenton@gmail.com>
2015-11-24 11:34:30 +0000

    key off of Status instead of Type to decide whether to set Location; support setting location on Created as well as (ResponseTypeAsync's) Accepted. Opens the path for sync POST that create an element in a collection to behave properly.

---
+++ daemon/response_test.go
@@ -0,0 +1,76 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2014-2015 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package daemon
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"gopkg.in/check.v1"
+)
+
+type responseSuite struct{}
+
+var _ = check.Suite(&responseSuite{})
+
+func (s *responseSuite) TestRespSetsLocationIfAccepted(c *check.C) {
+	rec := httptest.NewRecorder()
+
+	rsp := &resp{
+		Status: http.StatusAccepted,
+		Result: map[string]interface{}{
+			"resource": "foo/bar",
+		},
+	}
+
+	rsp.ServeHTTP(rec, nil)
+	hdr := rec.Header()
+	c.Check(hdr.Get("Location"), check.Equals, "foo/bar")
+}
+
+func (s *responseSuite) TestRespSetsLocationIfCreated(c *check.C) {
+	rec := httptest.NewRecorder()
+
+	rsp := &resp{
+		Status: http.StatusCreated,
+		Result: map[string]interface{}{
+			"resource": "foo/bar",
+		},
+	}
+
+	rsp.ServeHTTP(rec, nil)
+	hdr := rec.Header()
+	c.Check(hdr.Get("Location"), check.Equals, "foo/bar")
+}
+
+func (s *responseSuite) TestRespDoesNotSetLocationIfOther(c *check.C) {
+	rec := httptest.NewRecorder()
+
+	rsp := &resp{
+		Status: http.StatusTeapot,
+		Result: map[string]interface{}{
+			"resource": "foo/bar",
+		},
+	}
+
+	rsp.ServeHTTP(rec, nil)
+	hdr := rec.Header()
+	c.Check(hdr.Get("Location"), check.Equals, "")
+}
