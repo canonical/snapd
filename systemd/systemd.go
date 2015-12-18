5c95856e37f53b4e1343d649162049cd000c37f4
Kyle Fazzari <kyle@canonical.com>
2015-12-18 15:17:59 +0000

    Remap user-level environment to system-level for services.
    Currently Snappy services define $SNAP_APP_USER_DATA_PATH to be
    within /root/apps, however, there is no logic to create the
    $SNAP_APP_USER_DATA_PATH like there is within binary wrappers,
    which means any service that tries to use $SNAP_APP_USER_DATA_PATH
    is trying to use a non-existing directory.
    
    Having services write caches or logs into /root doesn't really make
    sense anyway, so this commit remaps the user-level environment into
    the system-level environment (i.e. $SNAP_APP_USER_DATA_PATH now =
    $SNAP_APP_DATA_PATH) for services.
    
    Fixes LP: #1527612
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -359,7 +359,9 @@ WantedBy={{.ServiceSystemdTarget}}
 		servicesSystemdTarget,
 		origin,
 		helpers.UbuntuArchitecture(),
-		"%h",
+		// Remap user-level environment to system-level for services. The code
+		// below will append "apps/$appname/$version" to this path.
+		"/var/lib",
 		"",
 		desc.SocketFileName,
 	}
