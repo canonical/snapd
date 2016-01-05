890a914513507158e07f0ed099df4954165d9767
Michael Vogt <mvo@ubuntu.com>
2016-01-05 14:42:33 +0100

    Use withMutexAndRetry() for {enable,disable}-classic
    Thanks John!

--- cmd/snappy/cmd_classic.go
+++ cmd/snappy/cmd_classic.go
@@ -21,13 +21,11 @@ package main
 
 import (
 	"fmt"
-	sys "syscall"
 
 	"github.com/ubuntu-core/snappy/classic"
 	"github.com/ubuntu-core/snappy/i18n"
 	"github.com/ubuntu-core/snappy/logger"
 	"github.com/ubuntu-core/snappy/progress"
-	"github.com/ubuntu-core/snappy/snappy"
 )
 
 type cmdEnableClassic struct{}
@@ -52,12 +50,12 @@ func init() {
 }
 
 func (x *cmdEnableClassic) Execute(args []string) (err error) {
+	return withMutexAndRetry(x.doEnable)
+}
+
+func (x *cmdEnableClassic) doEnable() (err error) {
 	if classic.Enabled() {
 		return fmt.Errorf(i18n.G("Classic dimension is already enabled."))
-	}
-
-	if sys.Getuid() != 0 {
-		return snappy.ErrNeedRoot
 	}
 
 	pbar := progress.NewTextProgress()
@@ -71,12 +69,12 @@ Use “snappy shell classic” to enter the classic dimension.`))
 }
 
 func (x *cmdDestroyClassic) Execute(args []string) (err error) {
+	return withMutexAndRetry(x.doDisable)
+}
+
+func (x *cmdDestroyClassic) doDisable() (err error) {
 	if !classic.Enabled() {
 		return fmt.Errorf(i18n.G("Classic dimension is not enabled."))
-	}
-
-	if sys.Getuid() != 0 {
-		return snappy.ErrNeedRoot
 	}
 
 	if err := classic.Destroy(); err != nil {
