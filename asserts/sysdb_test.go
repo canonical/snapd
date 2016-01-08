1157cfd57bad0fe6568a94e2bc17433a8be68657
Samuele Pedroni <pedronis@lucediurna.net>
2016-01-08 15:36:10 +0100

    wip: simpler leaner test setup in sysdb_test.go

--- asserts/sysdb_test.go
+++ asserts/sysdb_test.go
@@ -31,6 +31,7 @@ import (
 )
 
 type sysDBSuite struct {
+	fakeRoot    string
 	probeAssert asserts.Assertion
 }
 
@@ -38,35 +39,31 @@ var _ = Suite(&sysDBSuite{})
 
 func (sdbs *sysDBSuite) SetUpTest(c *C) {
 	tmpdir := c.MkDir()
-	cfg0 := &asserts.DatabaseConfig{Path: filepath.Join(tmpdir, "asserts-db0")}
-	db0, err := asserts.OpenDatabase(cfg0)
-	c.Assert(err, IsNil)
-	pk := asserts.OpenPGPPrivateKey(testPrivKey0)
-	err = db0.ImportKey("canonical", pk)
-	c.Assert(err, IsNil)
-	trustedFingerp := pk.PublicKey().Fingerprint()
-	trustedKeyID := pk.PublicKey().ID()
 
-	trustedPubKey, err := db0.PublicKey("canonical", trustedKeyID)
-	c.Assert(err, IsNil)
+	pk := asserts.OpenPGPPrivateKey(testPrivKey0)
+	trustedPubKey := pk.PublicKey()
 	trustedPubKeyEncoded, err := asserts.EncodePublicKey(trustedPubKey)
 	c.Assert(err, IsNil)
 	// self-signed
 	headers := map[string]string{
 		"authority-id":           "canonical",
 		"account-id":             "canonical",
-		"public-key-id":          trustedKeyID,
-		"public-key-fingerprint": trustedFingerp,
+		"public-key-id":          trustedPubKey.ID(),
+		"public-key-fingerprint": trustedPubKey.Fingerprint(),
 		"since":                  "2015-11-20T15:04:00Z",
 		"until":                  "2500-11-20T15:04:00Z",
 	}
-	trustedAccKey, err := db0.Sign(asserts.AccountKeyType, headers, trustedPubKeyEncoded, trustedKeyID)
+	trustedAccKey, err := asserts.AssembleAndSignInTest(asserts.AccountKeyType, headers, trustedPubKeyEncoded, pk)
 	c.Assert(err, IsNil)
 
 	fakeRoot := filepath.Join(tmpdir, "root")
 	err = os.Mkdir(fakeRoot, os.ModePerm)
 	c.Assert(err, IsNil)
-	dirs.SetRootDir(fakeRoot)
+
+	sdbs.fakeRoot = fakeRoot
+
+	dirs.SetRootDir(sdbs.fakeRoot)
+	defer dirs.SetRootDir("/")
 
 	err = os.MkdirAll(filepath.Dir(dirs.SnapTrustedAccountKey), os.ModePerm)
 	c.Assert(err, IsNil)
@@ -77,7 +74,7 @@ func (sdbs *sysDBSuite) SetUpTest(c *C) {
 		"authority-id": "canonical",
 		"primary-key":  "0",
 	}
-	sdbs.probeAssert, err = db0.Sign(asserts.AssertionType("test-only"), headers, nil, trustedKeyID)
+	sdbs.probeAssert, err = asserts.AssembleAndSignInTest(asserts.AssertionType("test-only"), headers, nil, pk)
 	c.Assert(err, IsNil)
 }
 
@@ -86,6 +83,21 @@ func (sdbs *sysDBSuite) TearDownTest(c *C) {
 }
 
 func (sdbs *sysDBSuite) TestOpenSysDatabase(c *C) {
+	dirs.SetRootDir(sdbs.fakeRoot)
+	defer dirs.SetRootDir("/")
+
+	db, err := asserts.OpenSysDatabase()
+	c.Assert(err, IsNil)
+	c.Check(db, NotNil)
+
+	err = db.Check(sdbs.probeAssert)
+	c.Check(err, IsNil)
+}
+
+func (sdbs *sysDBSuite) TestOpenSysDatabase(c *C) {
+	dirs.SetRootDir(sdbs.fakeRoot)
+	defer dirs.SetRootDir("/")
+
 	db, err := asserts.OpenSysDatabase()
 	c.Assert(err, IsNil)
 	c.Check(db, NotNil)
