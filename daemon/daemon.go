e72b63ac00ae06066b314016bb53a7c604ddd827
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-01 23:40:12 +0100

    daemon: don't log confusing error message
    The API dispatcher always logs a message like:
    
        2016/02/01 22:25:18.196328 response.go:118: method "GET" not allowed
    
    This is caused by the fact that the message is logged before we even
    determine the method type and check if it is valid or not. This patch
    moves error construction to the spot when it is really needed.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -106,7 +106,6 @@ func (c *Command) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var rspf ResponseFunc
-	var rsp = BadMethod("method %q not allowed", r.Method)
 
 	switch r.Method {
 	case "GET":
@@ -120,10 +119,10 @@ func (c *Command) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if rspf != nil {
-		rsp = rspf(c, r)
+		rspf(c, r).ServeHTTP(w, r)
+	} else {
+		BadMethod("method %q not allowed", r.Method).ServeHTTP(w, r)
 	}
-
-	rsp.ServeHTTP(w, r)
 }
 
 type wrappedWriter struct {
