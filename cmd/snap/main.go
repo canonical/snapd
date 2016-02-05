b963ec47fa019672b487240b1bc771aa0ce431bb
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-05 10:38:37 +0100

    cmd: use variables for Stdout and Stderr
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap/main.go
+++ cmd/snap/main.go
@@ -28,6 +28,12 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
+// Stdout is the standard output stream, it is redirected for testing.
+var Stdout io.Writer = os.Stdout
+
+// Stderr is the standard error stream, it is redirected for testing.
+var Stderr io.Writer = os.Stderr
+
 type options struct {
 	// No global options yet
 }
