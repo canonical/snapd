89aa2bde8e73785a8bd3689e1ef076f25c0fea6d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-05 10:42:53 +0100

    cmd: capture command output during tests
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap/main_test.go
+++ cmd/snap/main_test.go
@@ -20,8 +20,11 @@
 package main_test
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
+	. "github.com/ubuntu-core/snappy/cmd/snap"
 	"github.com/ubuntu-core/snappy/testutil"
 	. "gopkg.in/check.v1"
 )
@@ -31,6 +34,18 @@ func Test(t *testing.T) { TestingT(t) }
 
 type SnapSuite struct {
 	testutil.BaseTest
+	stdout *bytes.Buffer
+	stderr *bytes.Buffer
 }
 
 var _ = Suite(&SnapSuite{})
+
+func (s *SnapSuite) SetUpTest(c *C) {
+	s.BaseTest.SetUpTest(c)
+	s.stdout = bytes.NewBuffer(nil)
+	s.stderr = bytes.NewBuffer(nil)
+	Stdout = s.stdout
+	Stderr = s.stderr
+	s.BaseTest.AddCleanup(func() { Stdout = os.Stdout })
+	s.BaseTest.AddCleanup(func() { Stderr = os.Stderr })
+}
