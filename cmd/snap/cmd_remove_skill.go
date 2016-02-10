f70c8702676b2dc8a2136859a2848a5edecde87d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-10 12:33:29 +0100

    cmd/snap: use upper-case argument descriptions
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap/cmd_remove_skill.go
+++ cmd/snap/cmd_remove_skill.go
@@ -25,8 +25,8 @@ import (
 
 type cmdRemoveSkill struct {
 	Positionals struct {
-		Snap string `positional-arg-name:"<snap name>" description:"name of the snap containing the skill"`
-		Name string `positional-arg-name:"<skill name>" description:"name of the skill slot within the snap"`
+		Snap string `positional-arg-name:"<snap name>" description:"Name of the snap containing the skill"`
+		Name string `positional-arg-name:"<skill name>" description:"Name of the skill slot within the snap"`
 	} `positional-args:"true" required:"true"`
 }
 
