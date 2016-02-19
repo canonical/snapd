24da55ceba3509e0237875197a3b31ac22349994
Michael Vogt <mvo@debian.org>
2016-02-19 08:55:15 -0800

    remove msg = msg[:]

--- cmd/snap/cmd_remove_skill_slot_test.go
+++ cmd/snap/cmd_remove_skill_slot_test.go
@@ -45,7 +45,6 @@ Help Options:
   <skill slot>:           Name of the skill slot within the snap
 `
 	rest, err := Parser().ParseArgs([]string{"experimental", "remove-skill-slot", "--help"})
-	msg = msg[:]
 	c.Assert(err.Error(), Equals, msg)
 	c.Assert(rest, DeepEquals, []string{})
 }
