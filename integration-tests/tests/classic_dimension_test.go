6c300f8de2eb8fbf88589e929948052508e805ed
Leonardo Arias Fonseca (¡paz y baile!) <yo@elopio.net>
2016-02-25 23:52:42 -0600

    Pass the proxy to the sudo command when creating the classic dimension.

--- integration-tests/tests/classic_dimension_test.go
+++ integration-tests/tests/classic_dimension_test.go
@@ -21,6 +21,8 @@
 package tests
 
 import (
+	"os"
+
 	"github.com/ubuntu-core/snappy/integration-tests/testutils/cli"
 	"github.com/ubuntu-core/snappy/integration-tests/testutils/common"
 
@@ -34,7 +36,7 @@ type classicDimensionSuite struct {
 }
 
 func (s *classicDimensionSuite) enableClassic(c *check.C) {
-	output := cli.ExecCommand(c, "sudo", "snappy", "enable-classic")
+	output := cli.ExecCommand(c, "sudo", "env", "https_proxy="+os.Getenv("https_proxy"), "snappy", "enable-classic")
 
 	expected := "(?ms)" +
 		".*" +
