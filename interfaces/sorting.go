5372a90eadeb0700a9d6d18e6806884536cc2d99
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-29 14:19:48 +0100

    interfaces: add implementation of sorting for PlugRef and SlotRef
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ interfaces/sorting.go
@@ -0,0 +1,42 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package interfaces
+
+type bySlotRef []SlotRef
+
+func (c bySlotRef) Len() int      { return len(c) }
+func (c bySlotRef) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c bySlotRef) Less(i, j int) bool {
+	if c[i].Snap != c[j].Snap {
+		return c[i].Snap < c[j].Snap
+	}
+	return c[i].Slot < c[j].Slot
+}
+
+type byPlugRef []PlugRef
+
+func (c byPlugRef) Len() int      { return len(c) }
+func (c byPlugRef) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c byPlugRef) Less(i, j int) bool {
+	if c[i].Snap != c[j].Snap {
+		return c[i].Snap < c[j].Snap
+	}
+	return c[i].Plug < c[j].Plug
+}
