6d9c09c1e0a50ba369811aee4713e12c5a446cdf
Michael Vogt <mvo@ubuntu.com>
2016-02-29 17:34:04 +0100

    add missing tests

---
+++ osutil/exitcode_test.go
@@ -0,0 +1,55 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2014-2015 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package osutil
+
+import (
+	"os"
+	"os/exec"
+
+	. "gopkg.in/check.v1"
+)
+
+type ExitCodeTestSuite struct{}
+
+var _ = Suite(&ExitCodeTestSuite{})
+
+func (ts *ExitCodeTestSuite) TestExitCode(c *C) {
+	cmd := exec.Command("true")
+	err := cmd.Run()
+	c.Assert(err, IsNil)
+
+	cmd = exec.Command("false")
+	err = cmd.Run()
+	c.Assert(err, NotNil)
+	e, err := ExitCode(err)
+	c.Assert(err, IsNil)
+	c.Assert(e, Equals, 1)
+
+	cmd = exec.Command("sh", "-c", "exit 7")
+	err = cmd.Run()
+	e, err = ExitCode(err)
+	c.Assert(e, Equals, 7)
+
+	// ensure that non exec.ExitError values give a error
+	_, err = os.Stat("/random/file/that/is/not/there")
+	c.Assert(err, NotNil)
+	_, err = ExitCode(err)
+	c.Assert(err, NotNil)
+}
