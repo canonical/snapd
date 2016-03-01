46044c45d0cdde6e740cf5a2d73df03af1dcc679
Michael Vogt <mvo@ubuntu.com>
2016-03-01 10:55:57 +0100

    remove dead code

--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -20,11 +20,6 @@
 package helpers
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -56,13 +51,6 @@ func (ts *HTestSuite) TestMakeMapFromEnvListInvalidInput(c *C) {
 	c.Assert(envMap, DeepEquals, map[string]string(nil))
 }
 
-func skipOnMissingDevKmsg(c *C) {
-	_, err := os.Stat("/dev/kmsg")
-	if err != nil {
-		c.Skip("Can not stat /dev/kmsg")
-	}
-}
-
 func (ts *HTestSuite) TestGetattr(c *C) {
 	T := struct {
 		S string
@@ -77,40 +65,4 @@ func (ts *HTestSuite) TestGetattr(c *C) {
 	// works for pointers too
 	c.Assert(Getattr(&T, "S").(string), Equals, "foo")
 	c.Assert(Getattr(&T, "I").(int), Equals, 42)
-}
-
-func makeTestFiles(c *C, srcDir, destDir string) {
-	// a new file
-	err := ioutil.WriteFile(filepath.Join(srcDir, "new"), []byte(nil), 0644)
-	c.Assert(err, IsNil)
-
-	// a existing file that needs update
-	err = ioutil.WriteFile(filepath.Join(destDir, "existing-update"), []byte("old-content"), 0644)
-	c.Assert(err, IsNil)
-	err = ioutil.WriteFile(filepath.Join(srcDir, "existing-update"), []byte("some-new-content"), 0644)
-	c.Assert(err, IsNil)
-
-	// existing file that needs no update
-	err = ioutil.WriteFile(filepath.Join(srcDir, "existing-unchanged"), []byte(nil), 0644)
-	c.Assert(err, IsNil)
-	err = exec.Command("cp", "-a", filepath.Join(srcDir, "existing-unchanged"), filepath.Join(destDir, "existing-unchanged")).Run()
-	c.Assert(err, IsNil)
-
-	// a file that needs removal
-	err = ioutil.WriteFile(filepath.Join(destDir, "to-be-deleted"), []byte(nil), 0644)
-	c.Assert(err, IsNil)
-}
-
-func compareDirs(c *C, srcDir, destDir string) {
-	d1, err := exec.Command("ls", "-al", srcDir).CombinedOutput()
-	c.Assert(err, IsNil)
-	d2, err := exec.Command("ls", "-al", destDir).CombinedOutput()
-	c.Assert(err, IsNil)
-	c.Assert(string(d1), Equals, string(d2))
-	// ensure content got updated
-	c1, err := exec.Command("sh", "-c", fmt.Sprintf("find %s -type f |xargs cat", srcDir)).CombinedOutput()
-	c.Assert(err, IsNil)
-	c2, err := exec.Command("sh", "-c", fmt.Sprintf("find %s -type f |xargs cat", destDir)).CombinedOutput()
-	c.Assert(err, IsNil)
-	c.Assert(string(c1), Equals, string(c2))
 }
