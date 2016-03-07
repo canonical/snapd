63b28787d516b745285e57cf0019808a9fb533ec
Zygmunt Krynicki <zkrynicki@gmail.com>
2016-03-07 12:41:26 +0000

    cmd/snap: fix test failure caused by wrapping lines
    Signed-off-by: Zygmunt Krynicki <zkrynicki@gmail.com>

--- cmd/snap/cmd_interfaces.go
+++ cmd/snap/cmd_interfaces.go
@@ -38,7 +38,7 @@ var longInterfacesHelp = i18n.G(`
 The interfaces command lists interfaces available in the system.
 
 By default all slots and plugs, used and offered by all snaps, are displayed.
- 
+
 $ snap interfaces <snap>:<slot or plug>
 
 Lists only the specified slot or plug.
@@ -49,7 +49,8 @@ Lists the slots offered and plugs used by the specified snap.
 
 $ snap interfaces --i=<interface> [<snap>]
 
-Filters the complete output so only plugs and/or slots matching the provided details are listed.
+Filters the complete output so only plugs and/or slots matching the provided
+details are listed.
 `)
 
 func init() {
