db54c5f34c99cfe714157771eed1d78b5539c07d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-03-08 12:17:56 +0100

    snap,snappy,systemd: kill SNAP_FULLNAME
    The SNAP_FULLNAME variable was defined as Snap.Name + "." + Snap.Origin
    (except for frameworks where .Origin was discarded). This patch removes
    this variable entirely.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- snap/snapenv/snapenv.go
+++ snap/snapenv/snapenv.go
@@ -66,7 +66,6 @@ func GetBasicSnapEnvVars(desc interface{}) []string {
 		"SNAP_NAME={{.SnapName}}",
 		"SNAP_VERSION={{.Version}}",
 		"SNAP_ORIGIN={{.Origin}}",
-		"SNAP_FULLNAME={{.UdevAppName}}",
 		"SNAP_ARCH={{.SnapArch}}",
 	})
 }
