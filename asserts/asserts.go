832e5c2d5efef86f1780bd42f27108b9fc29d915
Matt Goodall <matt.goodall@gmail.com>
2016-03-08 11:02:41 +0000

    Move series in model assertion's primary key to be first.
    Includes some minor ordering changes for consistency.

--- asserts/asserts.go
+++ asserts/asserts.go
@@ -48,7 +48,7 @@ var (
 	DeviceSerialType = &AssertionType{"device-serial", []string{"brand-id", "model", "serial"}, assembleDeviceSerial}
 	IdentityType     = &AssertionType{"identity", []string{"account-id"}, assembleIdentity}
 	// XXX: is series actually part of the primary key?
-	ModelType           = &AssertionType{"model", []string{"brand-id", "model", "series"}, assembleModel}
+	ModelType           = &AssertionType{"model", []string{"series", "brand-id", "model"}, assembleModel}
 	SnapDeclarationType = &AssertionType{"snap-declaration", []string{"series", "snap-id"}, assembleSnapDeclaration}
 	SnapBuildType       = &AssertionType{"snap-build", []string{"snap-id", "snap-digest"}, assembleSnapBuild}
 	SnapRevisionType    = &AssertionType{"snap-revision", []string{"snap-id", "snap-digest"}, assembleSnapRevision}
