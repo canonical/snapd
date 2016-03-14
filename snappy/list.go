7b0092209de4ab592219841a990799fec3d1d97d
Michael Vogt <mvo@ubuntu.com>
2016-03-14 09:56:36 +0100

    remove use of snappy.Part from cmd.Snappy

--- snappy/list.go
+++ snappy/list.go
@@ -20,11 +20,11 @@
 package snappy
 
 // ListInstalled returns all installed snaps
-func ListInstalled() ([]Part, error) {
-	return NewLocalSnapRepository().Installed()
+func ListInstalled() ([]*Snap, error) {
+	return NewLocalSnapRepository().AllSnaps()
 }
 
 // ListUpdates returns all snaps with updates
-func ListUpdates() ([]Part, error) {
-	return NewUbuntuStoreSnapRepository().Updates()
+func ListUpdates() ([]*RemoteSnap, error) {
+	return NewUbuntuStoreSnapRepository().SnapUpdates()
 }
