c7261cec4241aedfdff7802a2e5cc5111ed1f6d8
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-03-14 22:22:13 +0100

    osutil: use AtomicWriteFile in EnsureDirState()
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- osutil/sync_dir.go
+++ osutil/sync_dir.go
@@ -93,13 +93,13 @@ func EnsureDirState(dir, glob string, content map[string]*FileState) ([]string, 
 				return created, corrected, removed, err
 			}
 			if !bytes.Equal(content, expected.Content) {
-				if err := writeFile(file, expected); err != nil {
+				if err := AtomicWriteFile(file.Name(), expected.Content, expected.Mode, 0); err != nil {
 					return created, corrected, removed, err
 				}
 				changed = true
 			}
 		} else {
-			if err := writeFile(file, expected); err != nil {
+			if err := AtomicWriteFile(file.Name(), expected.Content, expected.Mode, 0); err != nil {
 				return created, corrected, removed, err
 			}
 			changed = true
@@ -150,17 +150,4 @@ func EnsureDirState(dir, glob string, content map[string]*FileState) ([]string, 
 		created = append(created, baseName)
 	}
 	return created, corrected, removed, nil
-}
-
-func writeFile(file *os.File, state *FileState) error {
-	if _, err := file.Seek(0, 0); err != nil {
-		return err
-	}
-	if err := file.Truncate(0); err != nil {
-		return err
-	}
-	if _, err := file.Write(state.Content); err != nil {
-		return err
-	}
-	return nil
 }
