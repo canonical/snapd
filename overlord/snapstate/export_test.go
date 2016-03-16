c16aca55147bceaacacce7db3827f5e064f235f7
Michael Vogt <mvo@ubuntu.com>
2016-03-16 17:20:22 +0100

    address review comments

--- overlord/snapstate/export_test.go
+++ overlord/snapstate/export_test.go
@@ -20,14 +20,6 @@
 // Package snapstate implements the manager and state aspects responsible for the installation and removal of snaps.
 package snapstate
 
-import (
-	"github.com/ubuntu-core/snappy/overlord/state"
-)
-
-func SnapManagerRunner(s *SnapManager) *state.TaskRunner {
-	return s.runner
-}
-
 type BackendIF backendIF
 
 func SetSnapManagerBackend(s *SnapManager, b BackendIF) {
