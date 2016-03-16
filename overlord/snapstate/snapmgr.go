f929c0c6b37ee60bc17f14077e34d94df1389a54
Michael Vogt <mvo@ubuntu.com>
2016-03-16 09:48:07 +0100

    add initial snapmgr with very coarse tasks

--- overlord/snapstate/snapmgr.go
+++ overlord/snapstate/snapmgr.go
@@ -21,38 +21,80 @@
 package snapstate
 
 import (
+	"fmt"
+
 	"github.com/ubuntu-core/snappy/overlord/state"
+	"github.com/ubuntu-core/snappy/progress"
+	"github.com/ubuntu-core/snappy/snappy"
 )
 
+// Install initiates a change installing snap.
+func Install(change *state.Change, snap string) error {
+	change.State().Lock()
+	defer change.State().Unlock()
+
+	tIns := change.NewTask("install-snap", fmt.Sprintf("Installing %q", snap))
+	tIns.Set("name", snap)
+
+	return nil
+}
+
+// Remove initiates a change removing snap.
+func Remove(change *state.Change, snap string) error {
+	change.State().Lock()
+	defer change.State().Unlock()
+
+	t := change.NewTask("remove-snap", fmt.Sprintf("Removing %q", snap))
+	t.Set("name", snap)
+
+	return nil
+}
+
 // SnapManager is responsible for the installation and removal of snaps.
-type SnapManager struct{}
+type SnapManager struct {
+	state *state.State
+
+	runner *state.TaskRunner
+}
 
 // Manager returns a new snap manager.
 func Manager() (*SnapManager, error) {
 	return &SnapManager{}, nil
 }
 
-// Install initiates a change installing snap.
-func (m *SnapManager) Install(snap string) error {
-	return nil
+func (m *SnapManager) doInstallSnap(t *state.Task) error {
+	var name string
+	t.Get("name", &name)
+	channel := ""
+	_, err := snappy.Install(name, channel, 0, &progress.NullProgress{})
+	return err
 }
 
-// Remove initiates a change removing snap.
-func (m *SnapManager) Remove(snap string) error {
-	return nil
+func (m *SnapManager) doRemoveSnap(t *state.Task) error {
+	var name string
+	t.Get("name", &name)
+	return snappy.Remove(name, 0, &progress.NullProgress{})
 }
 
 // Init implements StateManager.Init.
 func (m *SnapManager) Init(s *state.State) error {
+	m.state = s
+	m.runner = state.NewTaskRunner(s)
+
+	m.runner.AddHandler("install-snap", m.doInstallSnap)
+	m.runner.AddHandler("remove-snap", m.doRemoveSnap)
+
 	return nil
 }
 
 // Ensure implements StateManager.Ensure.
 func (m *SnapManager) Ensure() error {
+	m.runner.Ensure()
 	return nil
 }
 
 // Stop implements StateManager.Stop.
 func (m *SnapManager) Stop() error {
+	m.runner.Stop()
 	return nil
 }
