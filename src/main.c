fd6ad34e89305c4c1176c456c7116d9b4abb9a74
Jamie Strandboge <jamie@ubuntu.com>
2016-03-18 13:42:33 -0500

    adjust checks to make sure we have everything we require for multi-instance devpts add comments on what we require and why

--- src/main.c
+++ src/main.c
@@ -259,28 +259,36 @@ void setup_private_mount(const char* appname) {
 }
 
 void setup_private_pts() {
+    // See https://www.kernel.org/doc/Documentation/filesystems/devpts.txt
+    //
+    // Ubuntu by default uses devpts 'single-instance' mode where /dev/pts/ptmx
+    // is mounted with ptmxmode=0000. We don't want to change the startup
+    // scripts though, so we follow the instructions in point '4' of
+    // 'User-space changes' in the above doc. In other words, after
+    // unshare(CLONE_NEWNS), we mount devpts with -o newinstance,ptmxmode=0666
+    // and then bind mount /dev/pts/ptmx onto /dev/ptmx
+
     struct stat st;
-    if (stat("/dev/pts", &st) != 0 || !S_ISDIR(st.st_mode)) {
-        die("/dev/pts doesn't exist or is not a directory");
+
+    // Make sure /dev/pts/ptmx exists, otherwise we are in legacy mode which
+    // doesn't provide the isolation we require.
+    if (stat("/dev/pts/ptmx", &st) != 0) {
+        die("/dev/pts/ptmx does not exist");
+    }
+    // Make sure /dev/ptmx exists so we can bind mount over it
+    if (stat("/dev/ptmx", &st) != 0) {
+        die("/dev/ptmx does not exist");
     }
 
-    // https://www.kernel.org/doc/Documentation/filesystems/devpts.txt
-    // with newinstance, use ptmxmode=0666. The other options are copied from
-    // /etc/default/devpts
+    // Since multi-instance, use ptmxmode=0666. The other options are copied
+    // from /etc/default/devpts
     if (mount("devpts", "/dev/pts", "devpts", MS_MGC_VAL,
               "newinstance,ptmxmode=0666,mode=0620,gid=5")) {
         die("unable to mount a new instance of '/dev/pts'");
     }
 
-    // if /dev/ptmx exists, bind mount over it otherwise, create a symlink
-    if (stat("/dev/ptmx", &st) == 0) {
-	if (mount("/dev/pts/ptmx", "/dev/ptmx", "none", MS_BIND | MS_NOSUID | MS_NOEXEC, 0)) {
-            die("unable to mount '/dev/pts/ptmx'->'/dev/ptmx'");
-	}
-    } else {
-        if (!symlink("/dev/pts/ptmx", "/dev/ptmx")) {
-            die("unable to symlink '/dev/pts/ptmx'->'/dev/ptmx'");
-	}
+    if (mount("/dev/pts/ptmx", "/dev/ptmx", "none", MS_BIND, 0)) {
+        die("unable to mount '/dev/pts/ptmx'->'/dev/ptmx'");
     }
 }
 
