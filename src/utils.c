a4efb18ede570b1a56ef690db283895d8c11acfe
Jamie Strandboge <jamie@ubuntu.com>
2016-03-22 07:17:03 -0500

    * enforce coding style:   - add syntax-check and fmt Makefile targets   - use 'indent -linux'   - debian/control: Build-Depends on indent

--- src/utils.c
+++ src/utils.c
@@ -24,64 +24,66 @@
 
 void die(const char *msg, ...)
 {
-   va_list va;
-   va_start(va, msg);
-   vfprintf(stderr, msg, va);
-   va_end(va);
+	va_list va;
+	va_start(va, msg);
+	vfprintf(stderr, msg, va);
+	va_end(va);
 
-   if (errno != 0) {
-     perror(". errmsg");
-   } else {
-     fprintf(stderr, "\n");
-   }
-   exit(1);
+	if (errno != 0) {
+		perror(". errmsg");
+	} else {
+		fprintf(stderr, "\n");
+	}
+	exit(1);
 }
 
 bool error(const char *msg, ...)
 {
-   va_list va;
-   va_start(va, msg);
-   vfprintf(stderr, msg, va);
-   va_end(va);
+	va_list va;
+	va_start(va, msg);
+	vfprintf(stderr, msg, va);
+	va_end(va);
 
-   return false;
+	return false;
 }
 
 void debug(const char *msg, ...)
 {
-   if(getenv("UBUNTU_CORE_LAUNCHER_DEBUG") == NULL)
-      return;
+	if (getenv("UBUNTU_CORE_LAUNCHER_DEBUG") == NULL)
+		return;
 
-   va_list va;
-   va_start(va, msg);
-   fprintf(stderr, "DEBUG: ");
-   vfprintf(stderr, msg, va);
-   fprintf(stderr, "\n");
-   va_end(va);
+	va_list va;
+	va_start(va, msg);
+	fprintf(stderr, "DEBUG: ");
+	vfprintf(stderr, msg, va);
+	fprintf(stderr, "\n");
+	va_end(va);
 }
 
-void write_string_to_file(const char *filepath, const char *buf) {
-   debug("write_string_to_file %s %s", filepath, buf);
-   FILE *f = fopen(filepath, "w");
-   if (f == NULL)
-      die("fopen %s failed", filepath);
-   if (fwrite(buf, strlen(buf), 1, f) != 1)
-      die("fwrite failed");
-   if (fflush(f) != 0)
-      die("fflush failed");
-   fclose(f);
+void write_string_to_file(const char *filepath, const char *buf)
+{
+	debug("write_string_to_file %s %s", filepath, buf);
+	FILE *f = fopen(filepath, "w");
+	if (f == NULL)
+		die("fopen %s failed", filepath);
+	if (fwrite(buf, strlen(buf), 1, f) != 1)
+		die("fwrite failed");
+	if (fflush(f) != 0)
+		die("fflush failed");
+	fclose(f);
 }
 
-int must_snprintf(char *str, size_t size, const char *format, ...) {
-   int n = -1;
+int must_snprintf(char *str, size_t size, const char *format, ...)
+{
+	int n = -1;
 
-   va_list va;
-   va_start(va, format);
-   n = vsnprintf(str, size, format, va);
-   va_end(va);
+	va_list va;
+	va_start(va, format);
+	n = vsnprintf(str, size, format, va);
+	va_end(va);
 
-   if(n < 0 || n >= size)
-      die("failed to snprintf %s", str);
+	if (n < 0 || n >= size)
+		die("failed to snprintf %s", str);
 
-   return n;
+	return n;
 }
