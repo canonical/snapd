f2be69d767ea3ae693072769e6622f108934c93c
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-03-31 12:53:37 +0200

    interfaces/apparmor: rename customTemplate to legacyTemplate
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/apparmor/template_vars.go
+++ interfaces/apparmor/template_vars.go
@@ -95,7 +95,7 @@ var (
 // profile. That same content also decides if the profile is enforcing or
 // advisory (complain). This is used to implement developer mode.
 func (b *Backend) aaHeader(appInfo *snap.AppInfo, developerMode bool) []byte {
-	header := b.customTemplate
+	header := b.legacyTemplate
 	if header == nil {
 		header = []byte(defaultTemplate)
 	}
