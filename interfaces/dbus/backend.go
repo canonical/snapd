f19d451d2046ad2d750a3444902fcd5c655262b0
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-04-01 12:42:40 +0200

    interfaces/dbus: tweak comment, thanks to jdstrand
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/dbus/backend.go
+++ interfaces/dbus/backend.go
@@ -42,7 +42,7 @@ type Backend struct{}
 
 // Configure creates dbus configuration files specific to a given snap.
 //
-// DBus has no concept of a complain mode, developerMode is ignored.
+// DBus has no concept of a complain mode so developerMode is not supported
 func (b *Backend) Configure(snapInfo *snap.Info, developerMode bool, repo *interfaces.Repository) error {
 	// Get the snippets that apply to this snap
 	snippets, err := repo.SecuritySnippetsForSnap(snapInfo.Name, interfaces.SecurityDBus)
