5b4eb85040d8112f70e3f48dab331efede9cd089
Samuele Pedroni <pedronis@lucediurna.net>
2016-04-06 10:33:18 +0200

    actually start implementing ValidateApp

--- snap/validate_test.go
+++ snap/validate_test.go
@@ -60,3 +60,32 @@ func (s *ValidateSuite) TestValidateName(c *C) {
 		c.Assert(err, ErrorMatches, `invalid snap name: ".*"`)
 	}
 }
+
+// ValidateApp
+
+func (s *ValidateSuite) TestServiceWhitelistSimple(c *C) {
+	c.Check(ValidateApp(&AppInfo{Name: "foo"}), IsNil)
+	c.Check(ValidateApp(&AppInfo{Description: "foo"}), IsNil)
+	c.Check(ValidateApp(&AppInfo{Command: "foo"}), IsNil)
+	c.Check(ValidateApp(&AppInfo{Stop: "foo"}), IsNil)
+	c.Check(ValidateApp(&AppInfo{PostStop: "foo"}), IsNil)
+}
+
+func (s *ValidateSuite) TestServiceWhitelistIllegal(c *C) {
+	c.Check(ValidateApp(&AppInfo{Name: "x\n"}), NotNil)
+	c.Check(ValidateApp(&AppInfo{Name: "test!me"}), NotNil)
+	c.Check(ValidateApp(&AppInfo{Command: "foo\n"}), NotNil)
+	c.Check(ValidateApp(&AppInfo{Stop: "foo\n"}), NotNil)
+	c.Check(ValidateApp(&AppInfo{PostStop: "foo\n"}), NotNil)
+}
+
+func (s *ValidateSuite) TestVerifyAppDaemonValue(c *C) {
+	c.Check(ValidateApp(&AppInfo{Daemon: "oneshot"}), IsNil)
+	c.Check(ValidateApp(&AppInfo{Daemon: "nono"}), ErrorMatches, `"daemon" field contains invalid value "nono"`)
+}
+
+func (s *ValidateSuite) TestServiceWhitelistError(c *C) {
+	err := ValidateApp(&AppInfo{Name: "x\n"})
+	c.Assert(err, NotNil)
+	c.Check(err.Error(), Equals, `app description field 'name' contains illegal "x\n" (legal: '^[A-Za-z0-9/. _#:-]*$')`)
+}
