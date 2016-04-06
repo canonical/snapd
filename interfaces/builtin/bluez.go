d8ae66a8250938dc101bc384dfbb81d9d7fd5845
Scott Sweeny <scott.sweeny@canonical.com>
2016-04-06 08:48:58 -0400

    Add bluez interface
    First pass at adding the interface for bluez to make itself available
    from a snap.

---
+++ interfaces/builtin/bluez.go
@@ -0,0 +1,64 @@
+package builtin
+
+import (
+    "github.com/ubuntu-core/snappy/interfaces"
+)
+
+type BluezInterface struct { }
+
+func (iface *BluezInterface) Name() string {
+    return "bluez"
+}
+
+func (iface *BluezInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+    switch securitySystem {
+    case interfaces.SecurityDBus:
+        return []byte(`
+<policy user="root">
+    <allow own="org.bluez"/>
+    <allow send_destination="org.bluez"/>
+    <allow send_interface="org.bluez.Agent1"/>
+    <allow send_interface="org.bluez.ThermometerWatcher1"/>
+    <allow send_interface="org.bluez.AlertAgent1"/>
+    <allow send_interface="org.bluez.Profile1"/>
+    <allow send_interface="org.bluez.HeartRateWatcher1"/>
+    <allow send_interface="org.bluez.CyclingSpeedWatcher1"/>
+    <allow send_interface="org.bluez.GattCharacteristic1"/>
+    <allow send_interface="org.bluez.GattDescriptor1"/>
+    <allow send_interface="org.freedesktop.DBus.ObjectManager"/>
+    <allow send_interface="org.freedesktop.DBus.Properties"/>
+  </policy>
+  <policy at_console="true">
+    <allow send_destination="org.bluez"/>
+  </policy>
+  <policy context="default">
+    <deny send_destination="org.bluez"/>
+  </policy>`), nil
+    case interfaces.SecurityAppArmor, interfaces.SecuritySecComp,
+interfaces.SecurityUDev:
+        return nil, nil
+    default:
+        return nil, interfaces.ErrUnknownSecurity
+    }
+}
+
+
+func (iface *BluezInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+    return nil, nil
+}
+
+func (iface *BluezInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+    return nil, nil
+}
+
+func (iface *BluezInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+    return nil, nil
+}
+
+func (iface *BluezInterface) SanitizePlug(slot *interfaces.Plug) error {
+    return nil
+}
+
+func (iface *BluezInterface) SanitizeSlot(slot *interfaces.Slot) error {
+    return nil
+}
