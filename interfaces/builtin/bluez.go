9c46899f3c27088ebc724473e608e7ba5966a22f
Scott Sweeny <scott.sweeny@canonical.com>
2016-04-06 09:28:32 -0400

    Add copyright header

--- interfaces/builtin/bluez.go
+++ interfaces/builtin/bluez.go
@@ -1,3 +1,22 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package builtin
 
 import (
@@ -41,7 +60,6 @@ interfaces.SecurityUDev:
         return nil, interfaces.ErrUnknownSecurity
     }
 }
-
 
 func (iface *BluezInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
     return nil, nil
