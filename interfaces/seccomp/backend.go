fe34d1fea4091eb32dd61633cedf2c10fb98c017
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-04-08 15:20:54 +0200

    interfaces: add .String() method to each backend (for debugging)
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/seccomp/backend.go
+++ interfaces/seccomp/backend.go
@@ -45,6 +45,10 @@ import (
 // Backend is responsible for maintaining seccomp profiles for ubuntu-core-launcher.
 type Backend struct{}
 
+func (b *Backend) String() string {
+	return "seccomp"
+}
+
 // Setup creates seccomp profiles specific to a given snap.
 // The snap can be in developer mode to make security violations non-fatal to
 // the offending application process.
