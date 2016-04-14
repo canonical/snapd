601209adacfd1df1e3b0ada8d61ee29b9aec8e82
Samuele Pedroni <pedronis@lucediurna.net>
2016-04-15 00:39:40 +0200

    get rid of Remove but preserve coverage and flags for now

--- snappy/remove.go
+++ snappy/remove.go
@@ -19,12 +19,6 @@
 
 package snappy
 
-import (
-	"strings"
-
-	"github.com/ubuntu-core/snappy/progress"
-)
-
 // RemoveFlags can be used to pass additional flags to the snap removal request
 type RemoveFlags uint
 
@@ -33,41 +27,3 @@ const (
 	// version is specified.
 	DoRemoveGC RemoveFlags = 1 << iota
 )
-
-// Remove a snap by a snapSpec string, name[.developer][=version]
-func Remove(snapSpec string, flags RemoveFlags, meter progress.Meter) error {
-	var snaps BySnapVersion
-
-	installed, err := (&Overlord{}).Installed()
-	if err != nil {
-		return err
-	}
-	// Note that "=" is not legal in a snap name or a snap version
-	l := strings.Split(snapSpec, "=")
-	if len(l) == 2 {
-		name := l[0]
-		version := l[1]
-		snaps = FindSnapsByNameAndVersion(name, version, installed)
-	} else {
-		if (flags & DoRemoveGC) == 0 {
-			if snap := ActiveSnapByName(snapSpec); snap != nil {
-				snaps = append(snaps, snap)
-			}
-		} else {
-			snaps = FindSnapsByName(snapSpec, installed)
-		}
-	}
-
-	if len(snaps) == 0 {
-		return ErrPackageNotFound
-	}
-
-	overlord := &Overlord{}
-	for _, snap := range snaps {
-		if err := overlord.Uninstall(snap, meter); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
