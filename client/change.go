f29071fecf825f7e9069e891090dd2818fdb17f1
Gustavo Niemeyer <gustavo@niemeyer.net>
2016-04-13 21:18:23 -0300

    Fix test breakages due to merge.

--- client/change.go
+++ client/change.go
@@ -63,7 +63,7 @@ func (tp *TaskProgress) UnmarshalJSON(buf []byte) error {
 // Change fetches information about a Change given its ID
 func (client *Client) Change(id string) (*Change, error) {
 	var chg Change
-	err := client.doSync("GET", "/v2/changes/"+id, nil, nil, &chg)
+	_, err := client.doSync("GET", "/v2/changes/"+id, nil, nil, &chg)
 
 	return &chg, err
 }
@@ -94,7 +94,7 @@ func (client *Client) Changes(which ChangeSelector) ([]*Change, error) {
 	query.Set("select", which.String())
 
 	var chgs []*Change
-	err := client.doSync("GET", "/v2/changes", query, nil, &chgs)
+	_, err := client.doSync("GET", "/v2/changes", query, nil, &chgs)
 
 	return chgs, err
 }
