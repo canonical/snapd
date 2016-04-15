6b0ecb1e21be55bea84c1518bb38da2aaa55ee94
Simon Fels <simon.fels@canonical.com>
2016-04-15 16:33:38 +0200

    interfaces: first version of the network-manager interface
    Signed-off-by: Simon Fels <simon.fels@canonical.com>

--- interfaces/builtin/all.go
+++ interfaces/builtin/all.go
@@ -26,6 +26,7 @@ import (
 var allInterfaces = []interfaces.Interface{
 	&BoolFileInterface{},
 	&BluezInterface{},
+	&NetworkManagerInterface{},
 	NewFirewallControlInterface(),
 	NewHomeInterface(),
 	NewLocaleControlInterface(),
