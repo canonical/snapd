4aa96b6b0dfaf64e8f7196e26f724b763b88e9b3
Samuele Pedroni <pedronis@lucediurna.net>
2016-04-26 13:50:00 +0200

    make remove actually remove all revisions

--- overlord/snapstate/snapmgr_test.go
+++ overlord/snapstate/snapmgr_test.go
@@ -228,33 +228,7 @@ func (s *snapmgrTestSuite) TestRemoveTasks(c *C) {
 		Active: true,
 		Sequence: []*snap.SideInfo{
 			{OfficialName: "foo"},
-			{OfficialName: "foo"},
 		},
-	})
-
-	ts, err := snapstate.Remove(s.state, "foo", 0)
-	c.Assert(err, IsNil)
-
-	i := 0
-	c.Assert(ts.Tasks(), HasLen, 4)
-	// all tasks are accounted
-	c.Assert(s.state.NumTask(), Equals, 4)
-	c.Assert(ts.Tasks()[i].Kind(), Equals, "unlink-snap")
-	i++
-	c.Assert(ts.Tasks()[i].Kind(), Equals, "remove-profiles")
-	i++
-	c.Assert(ts.Tasks()[i].Kind(), Equals, "clear-snap")
-	i++
-	c.Assert(ts.Tasks()[i].Kind(), Equals, "discard-snap")
-}
-
-func (s *snapmgrTestSuite) TestRemoveLast(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	snapstate.Set(s.state, "foo", &snapstate.SnapState{
-		Active:   true,
-		Sequence: []*snap.SideInfo{{OfficialName: "foo"}},
 	})
 
 	ts, err := snapstate.Remove(s.state, "foo", 0)
@@ -977,24 +951,145 @@ func (s *snapmgrTestSuite) TestRemoveIntegration(c *C) {
 			name: "/snap/some-snap/7",
 		},
 		fakeOp{
-			op:    "discard-conns:Doing",
-			name:  "some-snap",
-			revno: 7,
+			op:   "discard-conns:Doing",
+			name: "some-snap",
 		},
 	}
 	c.Assert(s.fakeBackend.ops, DeepEquals, expected)
 
 	// verify snapSetup info
 	tasks := ts.Tasks()
-	// snap-setup is in discard-snap above discard-conns.
-	task := tasks[len(tasks)-2]
-	var ss snapstate.SnapSetup
-	err = task.Get("snap-setup", &ss)
-	c.Assert(err, IsNil)
-	c.Assert(ss, DeepEquals, snapstate.SnapSetup{
-		Name:     "some-snap",
-		Revision: 7,
+	for _, t := range tasks {
+		ss, err := snapstate.TaskSnapSetup(t)
+		c.Assert(err, IsNil)
+
+		var expSnapSetup *snapstate.SnapSetup
+		if t.Kind() == "discard-conns" {
+			expSnapSetup = &snapstate.SnapSetup{
+				Name: "some-snap",
+			}
+		} else {
+			expSnapSetup = &snapstate.SnapSetup{
+				Name:     "some-snap",
+				Revision: 7,
+			}
+		}
+		c.Check(ss, DeepEquals, expSnapSetup, Commentf(t.Kind()))
+	}
+
+	// verify snaps in the system state
+	var snapst snapstate.SnapState
+	err = snapstate.Get(s.state, "some-snap", &snapst)
+	c.Assert(err, Equals, state.ErrNoState)
+}
+
+func (s *snapmgrTestSuite) TestRemoveWithManyRevisionsIntegration(c *C) {
+	si3 := snap.SideInfo{
+		OfficialName: "some-snap",
+		Revision:     3,
+	}
+
+	si5 := snap.SideInfo{
+		OfficialName: "some-snap",
+		Revision:     5,
+	}
+
+	si7 := snap.SideInfo{
+		OfficialName: "some-snap",
+		Revision:     7,
+	}
+
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	snapstate.Set(s.state, "some-snap", &snapstate.SnapState{
+		Active:   true,
+		Sequence: []*snap.SideInfo{&si5, &si3, &si7},
 	})
+
+	chg := s.state.NewChange("remove", "remove a snap")
+	ts, err := snapstate.Remove(s.state, "some-snap", 0)
+	c.Assert(err, IsNil)
+	chg.AddAll(ts)
+
+	s.state.Unlock()
+	defer s.snapmgr.Stop()
+	s.settle()
+	s.state.Lock()
+
+	c.Assert(s.fakeBackend.ops, HasLen, 10)
+	expected := []fakeOp{
+		{
+			op:     "can-remove",
+			name:   "/snap/some-snap/7",
+			active: true,
+		},
+		{
+			op:   "unlink-snap",
+			name: "/snap/some-snap/7",
+		},
+		{
+			op:    "remove-profiles:Doing",
+			name:  "some-snap",
+			revno: 7,
+		},
+		{
+			op:   "remove-snap-data",
+			name: "/snap/some-snap/7",
+		},
+		{
+			op:   "remove-snap-files",
+			name: "/snap/some-snap/7",
+		},
+		{
+			op:   "remove-snap-data",
+			name: "/snap/some-snap/3",
+		},
+		{
+			op:   "remove-snap-files",
+			name: "/snap/some-snap/3",
+		},
+		{
+			op:   "remove-snap-data",
+			name: "/snap/some-snap/5",
+		},
+		{
+			op:   "remove-snap-files",
+			name: "/snap/some-snap/5",
+		},
+		{
+			op:   "discard-conns:Doing",
+			name: "some-snap",
+		},
+	}
+	c.Assert(s.fakeBackend.ops, DeepEquals, expected)
+
+	// verify snapSetup info
+	tasks := ts.Tasks()
+	revnos := []int{7, 3, 5}
+	whichRevno := 0
+	for _, t := range tasks {
+		ss, err := snapstate.TaskSnapSetup(t)
+		c.Assert(err, IsNil)
+
+		var expSnapSetup *snapstate.SnapSetup
+		if t.Kind() == "discard-conns" {
+			expSnapSetup = &snapstate.SnapSetup{
+				Name: "some-snap",
+			}
+		} else {
+			expSnapSetup = &snapstate.SnapSetup{
+				Name:     "some-snap",
+				Revision: revnos[whichRevno],
+			}
+		}
+
+		c.Check(ss, DeepEquals, expSnapSetup, Commentf(t.Kind()))
+
+		if t.Kind() == "discard-snap" {
+			whichRevno++
+		}
+	}
 
 	// verify snaps in the system state
 	var snapst snapstate.SnapState
