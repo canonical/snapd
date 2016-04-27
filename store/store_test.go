b8eade2269cd2c9f8a8b0284552c31a802ae4460
Samuele Pedroni <pedronis@lucediurna.net>
2016-04-27 17:42:48 +0200

    readjust test

--- store/store_test.go
+++ store/store_test.go
@@ -309,7 +309,7 @@ func (t *remoteRepoTestSuite) TestUbuntuStoreRepositoryDetailsOopses(c *C) {
 		c.Check(r.URL.Path, Equals, "/search")
 
 		q := r.URL.Query()
-		c.Check(q.Get("q"), Equals, "package_name:hello-world")
+		c.Check(q.Get("q"), Equals, `package_name:"hello-world"`)
 		c.Check(r.Header.Get("X-Ubuntu-Device-Channel"), Equals, "edge")
 
 		w.Header().Set("X-Oops-Id", "OOPS-d4f46f75a5bcc10edcacc87e1fc0119f")
