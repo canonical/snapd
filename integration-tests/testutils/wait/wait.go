8be35ee7035e527101f925cc51b1ad4097575cc3
Michael Vogt <mvo@ubuntu.com>
2016-04-29 09:47:29 +0200

    Update integration tests for new output

--- integration-tests/testutils/wait/wait.go
+++ integration-tests/testutils/wait/wait.go
@@ -93,7 +93,7 @@ func forFunction(c *check.C, outputPattern string, inputFunc func() (string, err
 			retries++
 			if retries >= maxWaitRetries {
 				ticker.Stop()
-				return fmt.Errorf("Pattern not found in function output")
+				return fmt.Errorf("Pattern not found in function output, want %q got %q", outputPattern, output)
 			}
 		}
 	}
