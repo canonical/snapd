4986b2aeea33b27e015522a1bc691e8a267372ca
Michael Vogt <mvo@ubuntu.com>
2016-05-02 08:56:59 +0200

    provide new ErrorKindLoginRequired

--- cmd/snap/main.go
+++ cmd/snap/main.go
@@ -137,7 +137,7 @@ func main() {
 		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
 			fmt.Fprintf(Stdout, "%v\n", err)
 			os.Exit(0)
-		} else if e, ok := err.(*client.Error); ok && e.StatusCode == 401 && e.Message == "access denied" {
+		} else if e, ok := err.(*client.Error); ok && e.StatusCode == 401 && e.Kind == client.ErrorKindLoginRequired {
 			fmt.Fprintf(Stderr, missingLoginHelp)
 			os.Exit(1)
 		} else {
