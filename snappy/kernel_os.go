6dc417b267498421a32c4e7a59a9720b1157ef82
Michael Vogt <mvo@ubuntu.com>
2016-05-04 17:33:54 +0200

    adress review feedback

--- snappy/kernel_os.go
+++ snappy/kernel_os.go
@@ -57,7 +57,7 @@ func removeKernelAssets(s snap.PlaceInfo, inter interacter) error {
 
 func copyAll(src, dst string) error {
 	if output, err := exec.Command("cp", "-a", src, dst).CombinedOutput(); err != nil {
-		return fmt.Errorf("failed to copy %q -> %q: %s (%q)", src, dst, err, output)
+		return fmt.Errorf("cannot copy %q -> %q: %s (%s)", src, dst, err, output)
 	}
 	return nil
 }
@@ -67,7 +67,7 @@ func copyAll(src, dst string) error {
 // can use it.
 func extractKernelAssets(s *snap.Info, snapf snap.File, flags InstallFlags, inter progress.Meter) error {
 	if s.Type != snap.TypeKernel {
-		return fmt.Errorf("can not extract kernel assets from snap type %q", s.Type)
+		return fmt.Errorf("cannot extract kernel assets from snap type %q", s.Type)
 	}
 
 	// sanity check that we have the new kernel format
@@ -78,7 +78,7 @@ func extractKernelAssets(s *snap.Info, snapf snap.File, flags InstallFlags, inte
 
 	bootloader, err := findBootloader()
 	if err != nil {
-		return fmt.Errorf("can not extract kernel assets: %s", err)
+		return fmt.Errorf("cannot extract kernel assets: %s", err)
 	}
 
 	if bootloader.Name() == "grub" {
@@ -97,7 +97,6 @@ func extractKernelAssets(s *snap.Info, snapf snap.File, flags InstallFlags, inte
 	}
 	defer dir.Close()
 
-	// TODO: hardcoded names until this is fully speced
 	for _, src := range []string{
 		filepath.Join(s.MountDir(), "vmlinuz"),
 		filepath.Join(s.MountDir(), "initrd.img"),
@@ -110,7 +109,6 @@ func extractKernelAssets(s *snap.Info, snapf snap.File, flags InstallFlags, inte
 		}
 	}
 
-	// TODO: hardcoded names until this is fully speced
 	dtbsDir := filepath.Join(s.MountDir(), "dtbs")
 	if osutil.IsDirectory(dtbsDir) {
 		src := filepath.Join(dtbsDir, "*")
@@ -131,7 +129,7 @@ func setNextBoot(s *snap.Info) error {
 
 	bootloader, err := findBootloader()
 	if err != nil {
-		return fmt.Errorf("can not set next boot: %s", err)
+		return fmt.Errorf("cannot set next boot: %s", err)
 	}
 
 	var bootvar string
@@ -160,7 +158,7 @@ func kernelOrOsRebootRequired(s *snap.Info) bool {
 
 	bootloader, err := findBootloader()
 	if err != nil {
-		logger.Noticef("can not get boot settings: %s", err)
+		logger.Noticef("cannot get boot settings: %s", err)
 		return false
 	}
 
@@ -211,7 +209,7 @@ func nameAndRevnoFromSnap(snap string) (string, int) {
 func SyncBoot() error {
 	bootloader, err := findBootloader()
 	if err != nil {
-		return fmt.Errorf("can not run SyncBoot: %s", err)
+		return fmt.Errorf("cannot run SyncBoot: %s", err)
 	}
 
 	kernelSnap, _ := bootloader.GetBootVar("snappy_kernel")
@@ -219,7 +217,7 @@ func SyncBoot() error {
 
 	installed, err := (&Overlord{}).Installed()
 	if err != nil {
-		return fmt.Errorf("failed to run SyncBoot: %s", err)
+		return fmt.Errorf("cannot run SyncBoot: %s", err)
 	}
 
 	overlord := &Overlord{}
@@ -227,10 +225,10 @@ func SyncBoot() error {
 		name, revno := nameAndRevnoFromSnap(snap)
 		found := FindSnapsByNameAndRevision(name, revno, installed)
 		if len(found) != 1 {
-			return fmt.Errorf("can not SyncBoot, expected 1 snap %q (revno=%d) found %d", snap, revno, len(found))
+			return fmt.Errorf("cannot SyncBoot, expected 1 snap %q (revno=%d) found %d", snap, revno, len(found))
 		}
 		if err := overlord.SetActive(found[0], true, nil); err != nil {
-			return fmt.Errorf("can not SyncBoot, failed to make %s active: %s", found[0].Name(), err)
+			return fmt.Errorf("cannot SyncBoot, cannot make %s active: %s", found[0].Name(), err)
 		}
 	}
 
