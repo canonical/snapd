7f1afeafa0a3b1720b49e1cd08126dda33f1cec0
Michael Vogt <mvo@ubuntu.com>
2016-05-04 16:51:22 +0200

    remove snap.File.Unpack()

--- snap/squashfs/squashfs_test.go
+++ snap/squashfs/squashfs_test.go
@@ -30,8 +30,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ubuntu-core/snappy/osutil"
-
 	. "gopkg.in/check.v1"
 )
 
@@ -89,23 +87,6 @@ func (s *SquashfsTestSuite) TestReadFile(c *C) {
 	content, err := snap.ReadFile("meta/snap.yaml")
 	c.Assert(err, IsNil)
 	c.Assert(string(content), Equals, "name: foo")
-}
-
-func (s *SquashfsTestSuite) TestUnpackGlob(c *C) {
-	data := "some random data"
-	snap := makeSnap(c, "", data)
-
-	outputDir := c.MkDir()
-	err := snap.Unpack("data*", outputDir)
-	c.Assert(err, IsNil)
-
-	// this is the file we expect
-	content, err := ioutil.ReadFile(filepath.Join(outputDir, "data.bin"))
-	c.Assert(err, IsNil)
-	c.Assert(string(content), Equals, data)
-
-	// ensure glob was honored
-	c.Assert(osutil.FileExists(filepath.Join(outputDir, "meta/snap.yaml")), Equals, false)
 }
 
 func (s *SquashfsTestSuite) TestBuild(c *C) {
