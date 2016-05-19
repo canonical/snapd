4c471cc3117c2756851173fd22ceb26722c2c95c
John R. Lenton <jlenton@gmail.com>
2016-05-19 09:53:49 +0100

    daemon, integration-tests: address comments from review

--- integration-tests/tests/snap_op_test.go
+++ integration-tests/tests/snap_op_test.go
@@ -108,7 +108,7 @@ func (s *snapOpSuite) TestRemoveBusyRetries(c *check.C) {
 		wait.ForCommand(c, needle, "snap", "changes")
 
 		// find change id of the remove
-		output := cli.ExecCommand(c, "snap", "changes")
+		output := cli.ExecCommand(c, "snap", "changes", data.BasicBinariesSnapName)
 		id := regexp.MustCompile(`(?m)([0-9]+).*Doing.*Remove.*"`).FindStringSubmatch(output)[1]
 		needle = `will retry: `
 		wait.ForCommand(c, needle, "snap", "change", id)
@@ -153,7 +153,7 @@ func (s *snapOpSuite) TestInstallFailedIsUndone(c *check.C) {
 	c.Assert(err, check.NotNil)
 
 	// check undone and error in tasks
-	output := cli.ExecCommand(c, "snap", "changes")
+	output := cli.ExecCommand(c, "snap", "changes", snapName)
 	expected := fmt.Sprintf(`(?ms).*\n(\d+) +Error.*Install "%s" snap\n$`, snapName)
 	id := regexp.MustCompile(expected).FindStringSubmatch(output)[1]
 
