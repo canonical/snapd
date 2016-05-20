5c4361812a8ef02ec8a71eab3d01e92d8d3c6e97
Michael Vogt <mvo@ubuntu.com>
2016-05-20 11:39:07 +0200

    add (failing) rollback integration test

---
+++ integration-tests/tests/rollbackApp_test.go
@@ -0,0 +1,78 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+// +build !excludeintegration
+
+/*
+ * Copyright (C) 2015, 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package tests
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/ubuntu-core/snappy/integration-tests/testutils/cli"
+	"github.com/ubuntu-core/snappy/integration-tests/testutils/common"
+	"github.com/ubuntu-core/snappy/integration-tests/testutils/config"
+	"github.com/ubuntu-core/snappy/integration-tests/testutils/store"
+	"github.com/ubuntu-core/snappy/integration-tests/testutils/updates"
+	"github.com/ubuntu-core/snappy/testutil"
+
+	"gopkg.in/check.v1"
+)
+
+var _ = check.Suite(&rollbackAppSuite{})
+
+type rollbackAppSuite struct {
+	common.SnappySuite
+}
+
+func (s *rollbackAppSuite) TestInstallUpdateRollback(c *check.C) {
+	snap := "hello-world"
+
+	// install edge version from the store (which is squashfs)
+	cli.ExecCommand(c, "sudo", "snap", "install", snap)
+	defer cli.ExecCommand(c, "sudo", "snap", "remove", snap)
+
+	// make a fakestore and make it available to snapd
+
+	// use /var/tmp is not a tempfs for space reasons
+	blobDir, err := ioutil.TempDir("/var/tmp", "snap-fake-store-blobs-")
+	c.Assert(err, check.IsNil)
+	defer cli.ExecCommand(c, "sudo", "rm", "-rf", blobDir)
+
+	fakeStore := store.NewStore(blobDir)
+	err = fakeStore.Start()
+	c.Assert(err, check.IsNil)
+	defer fakeStore.Stop()
+
+	env := fmt.Sprintf(`SNAPPY_FORCE_CPI_URL=%s`, fakeStore.URL())
+	cfg, _ := config.ReadConfig(config.DefaultFileName)
+
+	tearDownSnapd(c)
+	defer setUpSnapd(c, cfg.FromBranch, "")
+	setUpSnapd(c, cfg.FromBranch, env)
+	defer tearDownSnapd(c)
+
+	// run the fake update
+	output := updates.CallFakeSnapRefresh(c, snap, updates.NoOp, fakeStore)
+	c.Assert(output, check.Matches, "(?ms).*^hello-world.*fake1.*")
+
+	// NOW do the rollback
+	output = cli.ExecCommand(c, "sudo", "snap", "rollback", snap)
+	c.Assert(output, check.Matches, "(?ms).*^hello-world.*")
+	c.Assert(output, check.Not(testutil.Contains), "fake1")
+}
