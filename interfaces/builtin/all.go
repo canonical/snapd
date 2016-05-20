30ddc1f9860e6e1f5b7b23b60e7e58f3737b6291
Scott Sweeny <scott.sweeny@canonical.com>
2016-05-20 16:08:47 -0400

    interfaces: builtin: add location-control interface

--- interfaces/builtin/all.go
+++ interfaces/builtin/all.go
@@ -26,6 +26,7 @@ import (
 var allInterfaces = []interfaces.Interface{
 	&BoolFileInterface{},
 	&BluezInterface{},
+	&LocationControlInterface{},
 	&LocationObserveInterface{},
 	&NetworkManagerInterface{},
 	NewFirewallControlInterface(),
