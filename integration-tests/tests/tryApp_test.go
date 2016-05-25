a4e55c5eedaf478180ce0f8a5df7e4f48b644084
Michael Vogt <mvo@ubuntu.com>
2016-05-25 08:02:11 +0200

    add integration tests for the confinement

--- integration-tests/tests/tryApp_test.go
+++ integration-tests/tests/tryApp_test.go
@@ -21,11 +21,14 @@
 package tests
 
 import (
+	"os"
 	"path/filepath"
 
+	"github.com/snapcore/snapd/integration-tests/testutils/build"
 	"github.com/snapcore/snapd/integration-tests/testutils/cli"
 	"github.com/snapcore/snapd/integration-tests/testutils/common"
 	"github.com/snapcore/snapd/integration-tests/testutils/data"
+	"github.com/snapcore/snapd/testutil"
 
 	"gopkg.in/check.v1"
 )
@@ -51,4 +54,56 @@ func (s *trySuite) TestTryBasicBinaries(c *check.C) {
 
 	// can run commands from the snap-try binary
 	cli.ExecCommand(c, "basic-binaries.success")
+
+	// commands can read stuff in their own dir
+	output := cli.ExecCommand(c, "basic-binaries.cat")
+	c.Check(output, testutil.Contains, "I output myself")
+}
+
+func (s *trySuite) TestTryConfinmentDenies(c *check.C) {
+	tryPath, err := filepath.Abs(filepath.Join(data.BaseSnapPath, data.DevKmsg))
+	c.Assert(err, check.IsNil)
+
+	cli.ExecCommand(c, "sudo", "snap", "try", tryPath)
+	defer common.RemoveSnap(c, data.DevKmsg)
+
+	// confinment works in try mode:
+	//  dev-kmsg.reader can not read /dev/kmsg out of the box
+	output, err := cli.ExecCommandErr("dev-kmsg.reader")
+	c.Check(err, check.NotNil)
+	c.Check(output, check.Matches, `(?ms)dd: failed to open '/dev/kmsg': Permission denied`)
+}
+
+func (s *trySuite) TestTryConfinmentDevModeAllows(c *check.C) {
+	tryPath, err := filepath.Abs(filepath.Join(data.BaseSnapPath, data.DevKmsg))
+	c.Assert(err, check.IsNil)
+
+	cli.ExecCommand(c, "sudo", "snap", "try", "--devmode", tryPath)
+	defer common.RemoveSnap(c, data.DevKmsg)
+
+	// dev-mode disables confinement and we can read /dev/kmsg
+	output, err := cli.ExecCommandErr("dev-kmsg.reader")
+	c.Check(err, check.IsNil)
+	c.Check(output, check.Not(check.HasLen), 0)
+}
+
+func (s *trySuite) TestTryConfinmentAllows(c *check.C) {
+	// provide a server for the test
+	snapPath, err := build.LocalSnap(c, data.NetworkBindConsumerSnapName)
+	c.Assert(err, check.IsNil)
+	defer os.Remove(snapPath)
+	common.InstallSnap(c, snapPath)
+	defer common.RemoveSnap(c, data.NetworkBindConsumerSnapName)
+
+	// "try" client confinment (network auto-connects)
+	tryPath, err := filepath.Abs(filepath.Join(data.BaseSnapPath, data.NetworkConsumerSnapName))
+	c.Assert(err, check.IsNil)
+
+	cli.ExecCommand(c, "sudo", "snap", "try", tryPath)
+	defer common.RemoveSnap(c, data.NetworkConsumerSnapName)
+
+	// confinment works in try mode:
+	providerURL := "http://127.0.0.1:8081"
+	output := cli.ExecCommand(c, "network-consumer", providerURL)
+	c.Assert(output, check.Equals, "ok\n")
 }
