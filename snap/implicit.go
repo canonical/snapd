dd72e4d6c66b3a282adf37ddf183f9f36a6a8d2a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-05-30 12:01:26 +0200

    snap: sort implicit slot lists
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- snap/implicit.go
+++ snap/implicit.go
@@ -38,12 +38,12 @@ var implicitSlots = []string{
 }
 
 var implicitClassicSlots = []string{
+	"cups-control",
+	"network-manager",
+	"opengl",
+	"pulseaudio",
 	"unity7",
 	"x11",
-	"opengl",
-	"network-manager",
-	"pulseaudio",
-	"cups-control",
 }
 
 // AddImplicitSlots adds implicitly defined slots to a given snap.
