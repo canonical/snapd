63b54d46a3dcb6fb75b4bddefcb863898f4b7569
Kyle Fazzari <kyle@canonical.com>
2016-06-17 14:15:21 +0000

    snap: stop testing addImplicitHooks directly.
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- snap/export_test.go
+++ snap/export_test.go
@@ -20,9 +20,7 @@
 package snap
 
 var (
-	ImplicitSlotsForTests         = implicitSlots
-	ImplicitClassicSlotsForTests  = implicitClassicSlots
-	RelativeHooksDir              = relativeHooksDir
-	AddImplicitHooks              = addImplicitHooks
-	AddImplicitHooksFromContainer = addImplicitHooksFromContainer
+	ImplicitSlotsForTests        = implicitSlots
+	ImplicitClassicSlotsForTests = implicitClassicSlots
+	RelativeHooksDir             = relativeHooksDir
 )
