5786a0961a44d034d27c5633faa3ae356be17903
Michael Vogt <mvo@ubuntu.com>
2016-06-20 16:19:01 +0200

    add "content" interface

---
+++ interfaces/builtin/content.go
@@ -0,0 +1,141 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/snapcore/snapd/interfaces"
+)
+
+type ContentInterface struct{}
+
+func (iface *ContentInterface) Name() string {
+	return "content"
+}
+
+func (iface *ContentInterface) SanitizeSlot(slot *interfaces.Slot) error {
+	if iface.Name() != slot.Interface {
+		panic(fmt.Sprintf("slot is not of interface %q", iface))
+	}
+
+	// check that we have either a read or write path
+	rpath, rok := slot.Attrs["read"].([]interface{})
+	wpath, wok := slot.Attrs["write"].([]interface{})
+	if !rok && !wok {
+		return fmt.Errorf("content must contain a read or write path")
+	}
+	if len(rpath) == 0 && len(wpath) == 0 {
+		return fmt.Errorf("read or write path must be set")
+	}
+
+	// go over both paths
+	paths := rpath
+	paths = append(paths, wpath...)
+	for _, p := range paths {
+		if strings.Contains(p.(string), "..") {
+			return fmt.Errorf("relative path not allowed")
+		}
+	}
+
+	return nil
+}
+
+func (iface *ContentInterface) SanitizePlug(plug *interfaces.Plug) error {
+	if iface.Name() != plug.Interface {
+		panic(fmt.Sprintf("plug is not of interface %q", iface))
+	}
+	target, ok := plug.Attrs["target"].(string)
+	if !ok || len(target) == 0 {
+		return fmt.Errorf("content plug must contain target path")
+	}
+	if strings.Contains(target, "..") {
+		return fmt.Errorf("relative path not allowed")
+	}
+
+	return nil
+}
+
+func (iface *ContentInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityDBus, interfaces.SecurityUDev, interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+func (iface *ContentInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+
+	switch securitySystem {
+	case interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityDBus, interfaces.SecurityUDev, interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+func (iface *ContentInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	contentSnippet := bytes.NewBuffer(nil)
+	dst := plug.Attrs["target"].(string)
+	dst = filepath.Join(plug.Snap.MountDir(), dst)
+
+	// read
+	if readPaths, ok := slot.Attrs["read"].([]interface{}); ok {
+		for _, r := range readPaths {
+			src := filepath.Join(slot.Snap.MountDir(), r.(string))
+			fmt.Fprintf(contentSnippet, "%s %s none bind,ro 0 0\n", src, dst)
+		}
+	}
+
+	// write
+	if writePaths, ok := slot.Attrs["write"].([]interface{}); ok {
+		for _, r := range writePaths {
+			src := filepath.Join(slot.Snap.MountDir(), r.(string))
+			fmt.Fprintf(contentSnippet, "%s %s none bind 0 0\n", src, dst)
+		}
+	}
+
+	switch securitySystem {
+	case interfaces.SecurityMount:
+		return contentSnippet.Bytes(), nil
+	case interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityDBus, interfaces.SecurityUDev:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+func (iface *ContentInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityDBus, interfaces.SecurityUDev, interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+func (iface *ContentInterface) AutoConnect() bool {
+	// FIXME: really?
+	return true
+}
---
+++ interfaces/builtin/content_test.go
@@ -0,0 +1,189 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/snap"
+)
+
+type ContentSuite struct {
+	iface interfaces.Interface
+}
+
+var _ = Suite(&ContentSuite{
+	iface: &builtin.ContentInterface{},
+})
+
+func (s *ContentSuite) TestName(c *C) {
+	c.Assert(s.iface.Name(), Equals, "content")
+}
+
+func (s *ContentSuite) TestSanitizeSlotSimple(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+slots:
+ content-slot:
+  interface: content
+  read:
+   - ./shared/read
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	slot := &interfaces.Slot{SlotInfo: info.Slots["content-slot"]}
+	err = s.iface.SanitizeSlot(slot)
+	c.Assert(err, IsNil)
+}
+
+func (s *ContentSuite) TestSanitizeSlotNoPaths(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+slots:
+ content-slot:
+  interface: content
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+	slot := &interfaces.Slot{SlotInfo: info.Slots["content-slot"]}
+
+	err = s.iface.SanitizeSlot(slot)
+	c.Assert(err, ErrorMatches, "content must contain a read or write path")
+}
+
+func (s *ContentSuite) TestSanitizeSlotEmptyPaths(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+slots:
+ content-slot:
+  interface: content
+  read: []
+  write: []
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	slot := &interfaces.Slot{SlotInfo: info.Slots["content-slot"]}
+	err = s.iface.SanitizeSlot(slot)
+	c.Assert(err, ErrorMatches, "read or write path must be set")
+}
+
+func (s *ContentSuite) TestSanitizeSlotHasRealtivePath(c *C) {
+	mockSnapYaml := `name: content-slot-snap
+version: 1.0
+slots:
+ content-slot:
+  interface: content
+`
+	for _, rw := range []string{"read: [../foo]", "write: [../bar]"} {
+		info, err := snap.InfoFromSnapYaml([]byte(mockSnapYaml + "  " + rw))
+		c.Assert(err, IsNil)
+
+		slot := &interfaces.Slot{SlotInfo: info.Slots["content-slot"]}
+		err = s.iface.SanitizeSlot(slot)
+		c.Assert(err, ErrorMatches, "relative path not allowed")
+	}
+}
+
+func (s *ContentSuite) TestSanitizePlugSimple(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+plugs:
+ content-plug:
+  interface: content
+  target: ./import
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	plug := &interfaces.Plug{PlugInfo: info.Plugs["content-plug"]}
+	err = s.iface.SanitizePlug(plug)
+	c.Assert(err, IsNil)
+}
+
+func (s *ContentSuite) TestSanitizePlugSimpleNoTarget(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+plugs:
+ content-plug:
+  interface: content
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	plug := &interfaces.Plug{PlugInfo: info.Plugs["content-plug"]}
+	err = s.iface.SanitizePlug(plug)
+	c.Assert(err, ErrorMatches, "content plug must contain target path")
+}
+
+func (s *ContentSuite) TestSanitizePlugSimpleTargetRelative(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+plugs:
+ content-plug:
+  interface: content
+  target: ../foo
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	plug := &interfaces.Plug{PlugInfo: info.Plugs["content-plug"]}
+	err = s.iface.SanitizePlug(plug)
+	c.Assert(err, ErrorMatches, "relative path not allowed")
+}
+
+func (s *ContentSuite) TestConnectedPlugSnippetSimple(c *C) {
+	var mockSnapYaml = []byte(`name: content-slot-snap
+version: 1.0
+slots:
+ content-slot:
+  interface: content
+  read:
+   - ./shared/read
+  write:
+   - ./shared/write
+plugs:
+ content-plug:
+  interface: content
+  target: ./import
+`)
+
+	info, err := snap.InfoFromSnapYaml(mockSnapYaml)
+	c.Assert(err, IsNil)
+
+	slot := &interfaces.Slot{SlotInfo: info.Slots["content-slot"]}
+	plug := &interfaces.Plug{PlugInfo: info.Plugs["content-plug"]}
+	content, err := s.iface.ConnectedPlugSnippet(plug, slot, interfaces.SecurityMount)
+	c.Assert(err, IsNil)
+
+	expected := `/snap/content-slot-snap/unset/shared/read /snap/content-slot-snap/unset/import none bind,ro 0 0
+/snap/content-slot-snap/unset/shared/write /snap/content-slot-snap/unset/import none bind 0 0
+`
+	c.Assert(string(content), DeepEquals, expected)
+}
