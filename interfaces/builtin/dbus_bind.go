34dd46f0cc3aed8757e4cd137c44f9f6fa71596d
Jamie Strandboge <jamie@ubuntu.com>
2016-06-27 13:39:12 -0500

    add connected plug and connected slot policy. improve tests

--- interfaces/builtin/dbus_bind.go
+++ interfaces/builtin/dbus_bind.go
@@ -26,6 +26,7 @@ import (
 	"strings"
 
 	"github.com/snapcore/snapd/interfaces"
+	//"github.com/luci/go-render/render"
 )
 
 var dbusBindPermanentSlotAppArmor = []byte(`
@@ -33,6 +34,7 @@ var dbusBindPermanentSlotAppArmor = []byte(`
 # Usage: common
 
 # DBus accesses
+# FIXME
 #include <abstractions/dbus-session-strict>
 
 # register and bind to a well-known DBus name
@@ -54,7 +56,7 @@ dbus (bind)
     bus=###DBUS_BIND_BUS###
     name=###DBUS_BIND_NAME###,
 
-# Allow unconfined clients to interact with this service
+# Allow us to talk to dbus-daemon
 dbus (receive)
     bus=###DBUS_BIND_BUS###
     path=###DBUS_BIND_PATH###
@@ -64,6 +66,8 @@ dbus (send)
     path=###DBUS_BIND_PATH###
     interface=org.freedesktop.DBus.Properties
     peer=(name=org.freedesktop.DBus, label=unconfined),
+
+# TODO: allow unconfined clients talk to us
 `)
 
 var dbusBindPermanentSlotSecComp = []byte(`
@@ -75,6 +79,41 @@ recvmsg
 sendmsg
 sendto
 `)
+
+var dbusBindConnectedSlotAppArmor = []byte(`
+# Description: Allow DBus consumer to connect to us
+# FIXME
+#include <abstractions/dbus-session-strict>
+
+# Communicate with the well-known named DBus service
+dbus (receive, send)
+    bus=###DBUS_BIND_BUS###
+    path=###DBUS_BIND_PATH###
+    peer=(label=###PLUG_SECURITY_TAGS###),
+`)
+
+
+var dbusBindConnectedPlugAppArmor = []byte(`
+# Description: Allow connecting to DBus service on well-known name
+# Usage: common
+
+# DBus accesses
+#include <abstractions/dbus-session-strict>
+
+# Communicate with the well-known named DBus service
+dbus (receive, send)
+    bus=###DBUS_BIND_BUS###
+    path=###DBUS_BIND_PATH###
+    peer=(label=###SLOT_SECURITY_TAGS###),
+`)
+
+var dbusBindConnectedPlugSecComp = []byte(`
+getsockname
+recvmsg
+sendmsg
+sendto
+`)
+
 
 type DbusBindInterface struct{}
 
@@ -93,7 +132,35 @@ func (iface *DbusBindInterface) PermanentPlugSnippet(plug *interfaces.Plug, secu
 
 func (iface *DbusBindInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
+	case interfaces.SecurityAppArmor:
+		old := []byte("###DBUS_BIND_BUS###")
+		new := []byte(slot.Attrs["bus"].(string))
+		snippet := bytes.Replace(dbusBindConnectedPlugAppArmor, old, new, -1)
+
+		old = []byte("###DBUS_BIND_NAME###")
+		new = []byte(slot.Attrs["name"].(string))
+		snippet = bytes.Replace(snippet, old, new, -1)
+
+		// convert name to AppArmor dbus path
+		dot_re := regexp.MustCompile("\\.")
+		var path_buf bytes.Buffer
+		path_buf.WriteString(`"/`)
+		path_buf.WriteString(dot_re.ReplaceAllString(slot.Attrs["name"].(string), "/"))
+		path_buf.WriteString(`{,/**}"`)
+
+		old = []byte("###DBUS_BIND_PATH###")
+		new = path_buf.Bytes()
+		snippet = bytes.Replace(snippet, old, new, -1)
+
+		old = []byte("###SLOT_SECURITY_TAGS###")
+		new = slotAppLabelExpr(slot)
+		snippet = bytes.Replace(snippet, old, new, -1)
+
+		//fmt.Printf("CONNECTED PLUG:\n %s\n", snippet)
+		return snippet, nil
+	case interfaces.SecuritySecComp:
+		return dbusBindPermanentSlotSecComp, nil
+	case interfaces.SecurityDBus, interfaces.SecurityUDev, interfaces.SecurityMount:
 		return nil, nil
 	default:
 		return nil, interfaces.ErrUnknownSecurity
@@ -122,6 +189,7 @@ func (iface *DbusBindInterface) PermanentSlotSnippet(slot *interfaces.Slot, secu
 		new = path_buf.Bytes()
 		snippet = bytes.Replace(snippet, old, new, -1)
 
+		//fmt.Printf("PERMANENT SLOT:\n %s\n", snippet)
 		return snippet, nil
 	case interfaces.SecuritySecComp:
 		return dbusBindPermanentSlotSecComp, nil
@@ -134,7 +202,30 @@ func (iface *DbusBindInterface) PermanentSlotSnippet(slot *interfaces.Slot, secu
 
 func (iface *DbusBindInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
+	case interfaces.SecurityAppArmor:
+		old := []byte("###DBUS_BIND_BUS###")
+		new := []byte(slot.Attrs["bus"].(string))
+		snippet := bytes.Replace(dbusBindConnectedSlotAppArmor, old, new, -1)
+
+		// TODO: break this out
+		// convert name to AppArmor dbus path
+		dot_re := regexp.MustCompile("\\.")
+		var path_buf bytes.Buffer
+		path_buf.WriteString(`"/`)
+		path_buf.WriteString(dot_re.ReplaceAllString(slot.Attrs["name"].(string), "/"))
+		path_buf.WriteString(`{,/**}"`)
+
+		old = []byte("###DBUS_BIND_PATH###")
+		new = path_buf.Bytes()
+		snippet = bytes.Replace(snippet, old, new, -1)
+
+		old = []byte("###PLUG_SECURITY_TAGS###")
+		new = plugAppLabelExpr(plug)
+		snippet = bytes.Replace(snippet, old, new, -1)
+
+		//fmt.Printf("CONNECTED SLOT:\n %s\n", snippet)
+		return snippet, nil
+	case interfaces.SecurityDBus, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
 		return nil, nil
 	default:
 		return nil, interfaces.ErrUnknownSecurity
