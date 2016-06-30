a095ca32c03d37e34a30d5aa16f65722fc19557a
Federico Gimenez <fgimenez@canonical.com>
2016-06-30 11:36:20 +0200

    port the op remove retry test to spread

--- integration-tests/tests/snap_op_test.go
+++ integration-tests/tests/snap_op_test.go
@@ -25,7 +25,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"time"
 
 	"gopkg.in/check.v1"
 
@@ -34,7 +33,6 @@ import (
 	"github.com/snapcore/snapd/integration-tests/testutils/cli"
 	"github.com/snapcore/snapd/integration-tests/testutils/common"
 	"github.com/snapcore/snapd/integration-tests/testutils/data"
-	"github.com/snapcore/snapd/integration-tests/testutils/wait"
 	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/testutil"
 )
@@ -81,67 +79,6 @@ func (s *snapOpSuite) TestRemoveRemovesAllRevisions(c *check.C) {
 	revnos, err = filepath.Glob(filepath.Join(dirs.SnapSnapsDir, data.BasicSnapName, "1*"))
 	c.Assert(err, check.IsNil)
 	c.Check(revnos, check.HasLen, 0)
-}
-
-// TestRemoveBusyRetries is a regression test for LP:#1571721
-func (s *snapOpSuite) TestRemoveBusyRetries(c *check.C) {
-	// install the binaries snap
-	snapPath, err := build.LocalSnap(c, data.BasicBinariesSnapName)
-	defer os.Remove(snapPath)
-	c.Assert(err, check.IsNil, check.Commentf("Error building local snap: %s", err))
-	installOutput := common.InstallSnap(c, snapPath)
-	c.Assert(installOutput, testutil.Contains, data.BasicBinariesSnapName)
-
-	// run a command that keeps the mount point busy in the background
-	blockerBin := fmt.Sprintf("/snap/bin/%s.block", data.BasicBinariesSnapName)
-	blockerSrv := "umount-blocker"
-	cli.ExecCommand(c, "sudo", "systemd-run", "--unit", blockerSrv, blockerBin)
-	c.Assert(err, check.IsNil, check.Commentf("cannot start %q:%s", blockerBin, err))
-	wait.ForActiveService(c, blockerSrv)
-
-	// snap remove will block (and try to retry) while the umount-block
-	// service is running. so we need to do stuff in a go-routine
-	ch := make(chan int)
-	go func() {
-		// wait until snappy to show that it is retrying
-		needle := `Doing.*Remove`
-		wait.ForCommand(c, needle, "snap", "changes")
-
-		// find change id of the remove
-		pattern := `(?m)([0-9]+).*Doing.*Remove.*"`
-		err := wait.ForCommand(c, pattern, "snap", "changes", data.BasicBinariesSnapName)
-		c.Assert(err, check.IsNil)
-
-		output := cli.ExecCommand(c, "snap", "changes", data.BasicBinariesSnapName)
-		id := regexp.MustCompile(pattern).FindStringSubmatch(output)[1]
-		needle = `will retry: `
-		wait.ForCommand(c, needle, "snap", "change", id)
-
-		// now stop the service that blocks the umount
-		cli.ExecCommand(c, "sudo", "systemctl", "stop", blockerSrv)
-		wait.ForInactiveService(c, blockerSrv)
-
-		// this triggers an Ensure in the overlord
-		cli.ExecCommandErr("sudo", "snap", "refresh", "ubuntu-core")
-		ch <- 1
-	}()
-
-	// try to remove, this will block and eventually succeed
-	output, err := cli.ExecCommandErr("sudo", "snap", "remove", data.BasicBinariesSnapName)
-	c.Check(err, check.IsNil)
-	c.Check(output, testutil.Contains, `will retry: `)
-
-	// wait for the goroutine to finish so that we can do the final
-	// check that we have no pending changes
-	select {
-	case <-ch:
-	case <-time.After(5 * time.Minute):
-		c.Errorf("busy retry test timed out after 5 minutes")
-	}
-
-	// ensure no changes are left in Doing state
-	output = cli.ExecCommand(c, "snap", "changes")
-	c.Check(output, check.Not(testutil.Contains), "Doing")
 }
 
 func (s *snapOpSuite) TestInstallFailedIsUndone(c *check.C) {
