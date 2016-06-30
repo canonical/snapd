f5a6a1dbebc342f86fac1caca0bc089ee1532d3a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-06-30 13:32:58 +0200

    Add get_nextpath() from lxd
    This patch is a copy of a function found in
    https://github.com/lxc/lxc/commit/592fd47a6245508b79fe6ac819fe6d3b2c1289
    by Serge Hallyn <serge.hallyn@ubuntu.com>.
    
    The committer at the time was a Canonical employee. The code is
    implicitly relicensed to match the license in snap-confine

--- src/mount-support.c
+++ src/mount-support.c
@@ -368,3 +368,26 @@ void sc_setup_mount_profiles(const char *appname)
 		}
 	}
 }
+
+/**
+ * @path:    a pathname where / replaced with '\0'.
+ * @offsetp: pointer to int showing which path segment was last seen.
+ *           Updated on return to reflect the next segment.
+ * @fulllen: full original path length.
+ * Returns a pointer to the next path segment, or NULL if done.
+ */
+static char *get_nextpath(char *path, int *offsetp, int fulllen)
+{
+	int offset = *offsetp;
+
+	if (offset >= fulllen)
+		return NULL;
+
+	while (path[offset] != '\0' && offset < fulllen)
+		offset++;
+	while (path[offset] == '\0' && offset < fulllen)
+		offset++;
+
+	*offsetp = offset;
+	return (offset < fulllen) ? &path[offset] : NULL;
+}
