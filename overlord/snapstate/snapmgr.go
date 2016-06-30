0b72590be2a7ad52beb9332dab93022a0e34acab
Michael Vogt <mvo@ubuntu.com>
2016-06-30 14:08:50 +0200

    use "had-candidate-in-seq" instead of "old-seqlen"

--- overlord/snapstate/snapmgr.go
+++ overlord/snapstate/snapmgr.go
@@ -734,9 +734,10 @@ func (m *SnapManager) doLinkSnap(t *state.Task, _ *tomb.Tomb) error {
 
 	cand := snapst.Candidate
 	m.backend.Candidate(snapst.Candidate)
-	oldSeqLen := len(snapst.Sequence)
+	hadCandidateInSeq := false
 	if snapst.findIndex(snapst.Candidate.Revision) < 0 {
 		snapst.Sequence = append(snapst.Sequence, snapst.Candidate)
+		hadCandidateInSeq = true
 	}
 	oldCurrent := snapst.Current
 	snapst.Current = snapst.Candidate.Revision
@@ -779,7 +780,7 @@ func (m *SnapManager) doLinkSnap(t *state.Task, _ *tomb.Tomb) error {
 	t.Set("old-trymode", oldTryMode)
 	t.Set("old-channel", oldChannel)
 	t.Set("old-current", oldCurrent)
-	t.Set("old-seqlen", oldSeqLen)
+	t.Set("had-candidate-in-seq", hadCandidateInSeq)
 	// Do at the end so we only preserve the new state if it worked.
 	Set(st, ss.Name, snapst)
 	// Make sure if state commits and snapst is mutated we won't be rerun
@@ -823,15 +824,15 @@ func (m *SnapManager) undoLinkSnap(t *state.Task, _ *tomb.Tomb) error {
 	if err != nil {
 		return err
 	}
-	var oldSeqLen int
-	err = t.Get("old-seqlen", &oldSeqLen)
+	var hadCandidateInSeq bool
+	err = t.Get("had-candidate-in-seq", &hadCandidateInSeq)
 	if err != nil {
 		return err
 	}
 
 	// relinking of the old snap is done in the undo of unlink-current-snap
 	snapst.Candidate = snapst.Sequence[snapst.findIndex(snapst.Current)]
-	if len(snapst.Sequence) != oldSeqLen {
+	if hadCandidateInSeq {
 		snapst.Sequence = snapst.Sequence[:len(snapst.Sequence)-1]
 	}
 	snapst.Current = oldCurrent
