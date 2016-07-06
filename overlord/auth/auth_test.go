3bee73c03a6cab491bae3b9812695d742e940e4b
William Grant <william.grant@canonical.com>
2016-07-06 15:33:11 +1000

    add Device/SetDevice to persist device identity in state

--- overlord/auth/auth_test.go
+++ overlord/auth/auth_test.go
@@ -300,6 +300,37 @@ func (as *authSuite) TestLoginCaveatIDMacaroonMissingCaveat(c *C) {
 	c.Check(caveat, Equals, "")
 }
 
+func (as *authSuite) TestSetDevice(c *C) {
+	as.state.Lock()
+	device, err := auth.Device(as.state)
+	as.state.Unlock()
+	c.Check(err, IsNil)
+	c.Check(device, DeepEquals, &auth.DeviceState{})
+
+	as.state.Lock()
+	err = auth.SetDevice(as.state, &auth.DeviceState{Brand: "some-brand"})
+	c.Check(err, IsNil)
+	device, err = auth.Device(as.state)
+	as.state.Unlock()
+	c.Check(err, IsNil)
+	c.Check(device, DeepEquals, &auth.DeviceState{Brand: "some-brand"})
+}
+
+func (as *authSuite) TestSetDeviceIdentity(c *C) {
+	as.state.Lock()
+	err := auth.SetDeviceIdentity(as.state, "the-brand", "the-model", "the-serial")
+	c.Check(err, IsNil)
+	device, err := auth.Device(as.state)
+	as.state.Unlock()
+	expected := &auth.DeviceState{
+		Brand:  "the-brand",
+		Model:  "the-model",
+		Serial: "the-serial",
+	}
+	c.Check(err, IsNil)
+	c.Check(device, DeepEquals, expected)
+}
+
 func (as *authSuite) TestGetAuthenticatorFromUser(c *C) {
 	as.state.Lock()
 	user, err := auth.NewUser(as.state, "username", "macaroon", []string{"discharge"})
