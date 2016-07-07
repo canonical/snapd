588d439da724c0fae9b76337665e1110c6a3e381
Michael Vogt <mvo@ubuntu.com>
2016-07-07 17:13:23 +0200

    address review feedback

--- overlord/snapstate/export_test.go
+++ overlord/snapstate/export_test.go
@@ -67,8 +67,9 @@ func (m *SnapManager) AddForeignTaskHandlers(tracker ForeignTaskTracker) {
 }
 
 func MockReadInfo(mock func(name string, si *snap.SideInfo) (*snap.Info, error)) (restore func()) {
+	old := readInfo
 	readInfo = mock
-	return func() { readInfo = readInfoImpl }
+	return func() { readInfo = old }
 }
 
 func MockOpenSnapFile(mock func(path string, si *snap.SideInfo) (*snap.Info, snap.Container, error)) (restore func()) {
