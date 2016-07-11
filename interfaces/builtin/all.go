bc68a6911bd5ef02ab90d529231d1b7c29bddaed
Scott Sweeny <scott.sweeny@canonical.com>
2016-07-11 08:58:11 -0400

    Add pluggable-storage interface

--- interfaces/builtin/all.go
+++ interfaces/builtin/all.go
@@ -45,6 +45,7 @@ var allInterfaces = []interfaces.Interface{
 	NewNetworkBindInterface(),
 	NewNetworkControlInterface(),
 	NewNetworkObserveInterface(),
+	NewPluggableStorageInterface(),
 	NewSnapdControlInterface(),
 	NewSystemObserveInterface(),
 	NewTimeserverControlInterface(),
--- snap/implicit.go
+++ snap/implicit.go
@@ -38,6 +38,7 @@ var implicitSlots = []string{
 	"network-bind",
 	"network-control",
 	"network-observe",
+	"pluggable-storage",
 	"ppp",
 	"snapd-control",
 	"system-observe",
