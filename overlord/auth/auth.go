matiasb/snappy

many: add authcontext for user updates to state

overlord/auth/auth.go
@@ -183,3 +183,36 @@ NextUser:
 	}
 	return nil, ErrInvalidAuth
 }
+
+// AuthContext helps keeping track and updating users in the state.
+type AuthContext struct {
+	state *state.State
+}
+
+// NewAuthContext returns an AuthContext for state.
+func NewAuthContext(st *state.State) *AuthContext {
+	return &AuthContext{state: st}
+}
+
+// UpdateUser updates user in state.
+func (ac *AuthContext) UpdateUser(user *UserState) error {
+	var authStateData AuthState
+
+	ac.state.Lock()
+	defer ac.state.Unlock()
+
+	err := ac.state.Get("auth", &authStateData)
+	if err != nil {
+		return err
+	}
+
+	for i := range authStateData.Users {
+		if authStateData.Users[i].ID == user.ID {
+			authStateData.Users[i] = *user
+			ac.state.Set("auth", authStateData)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid user")
+}

overlord/auth/auth_test.go
@@ -241,3 +241,38 @@ func (as *authSuite) TestSetDevice(c *C) {
 	c.Check(err, IsNil)
 	c.Check(device, DeepEquals, &auth.DeviceState{Brand: "some-brand"})
 }
+
+func (as *authSuite) TestAuthContextUpdateUser(c *C) {
+	as.state.Lock()
+	user, _ := auth.NewUser(as.state, "username", "macaroon", []string{"discharge"})
+	as.state.Unlock()
+
+	user.Username = "different"
+	user.StoreDischarges = []string{"updated-discharge"}
+
+	authContext := auth.NewAuthContext(as.state)
+	err := authContext.UpdateUser(user)
+	c.Check(err, IsNil)
+
+	as.state.Lock()
+	userFromState, err := auth.User(as.state, 1)
+	as.state.Unlock()
+	c.Check(err, IsNil)
+	c.Check(userFromState, DeepEquals, user)
+}
+
+func (as *authSuite) TestAuthContextUpdateUserInvalid(c *C) {
+	as.state.Lock()
+	_, _ = auth.NewUser(as.state, "username", "macaroon", []string{"discharge"})
+	as.state.Unlock()
+
+	user := &auth.UserState{
+		ID:       102,
+		Username: "username",
+		Macaroon: "macaroon",
+	}
+
+	authContext := auth.NewAuthContext(as.state)
+	err := authContext.UpdateUser(user)
+	c.Assert(err, ErrorMatches, "invalid user")
+}

overlord/snapstate/snapmgr.go
@@ -371,7 +371,8 @@ func Store(s *state.State) StoreService {
 		storeID = cand
 	}
 
-	s.Cache(cachedStoreKey{}, store.NewUbuntuStoreSnapRepository(nil, storeID))
+	authContext := auth.NewAuthContext(s)
+	s.Cache(cachedStoreKey{}, store.NewUbuntuStoreSnapRepository(nil, storeID, authContext))
 	return cachedStore(s)
 }
 

canonical/snapd

overlord/auth/auth.go
@@ -183,3 +183,41 @@ NextUser:
 	}
 	return nil, ErrInvalidAuth
 }
+
+// An AuthContext handles user updates.
+type AuthContext interface {
+	UpdateUser(user *UserState) error
+}
+
+// AuthStateContext helps keeping track and updating users in the state.
+type AuthStateContext struct {
+	state *state.State
+}
+
+// NewAuthContext returns an AuthContext for state.
+func NewAuthStateContext(st *state.State) *AuthStateContext {
+	return &AuthStateContext{state: st}
+}
+
+// UpdateUser updates user in state.
+func (asc *AuthStateContext) UpdateUser(user *UserState) error {
+	var authStateData AuthState
+
+	asc.state.Lock()
+	defer asc.state.Unlock()
+
+	err := asc.state.Get("auth", &authStateData)
+	if err != nil {
+		return err
+	}
+
+	for i := range authStateData.Users {
+		if authStateData.Users[i].ID == user.ID {
+			authStateData.Users[i] = *user
+			asc.state.Set("auth", authStateData)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid user")
+}

overlord/auth/auth_test.go
@@ -241,3 +241,38 @@ func (as *authSuite) TestSetDevice(c *C) {
 	c.Check(err, IsNil)
 	c.Check(device, DeepEquals, &auth.DeviceState{Brand: "some-brand"})
 }
+
+func (as *authSuite) TestAuthStateContextUpdateUser(c *C) {
+	as.state.Lock()
+	user, _ := auth.NewUser(as.state, "username", "macaroon", []string{"discharge"})
+	as.state.Unlock()
+
+	user.Username = "different"
+	user.StoreDischarges = []string{"updated-discharge"}
+
+	authContext := auth.NewAuthStateContext(as.state)
+	err := authContext.UpdateUser(user)
+	c.Check(err, IsNil)
+
+	as.state.Lock()
+	userFromState, err := auth.User(as.state, 1)
+	as.state.Unlock()
+	c.Check(err, IsNil)
+	c.Check(userFromState, DeepEquals, user)
+}
+
+func (as *authSuite) TestAuthStateContextUpdateUserInvalid(c *C) {
+	as.state.Lock()
+	_, _ = auth.NewUser(as.state, "username", "macaroon", []string{"discharge"})
+	as.state.Unlock()
+
+	user := &auth.UserState{
+		ID:       102,
+		Username: "username",
+		Macaroon: "macaroon",
+	}
+
+	authContext := auth.NewAuthStateContext(as.state)
+	err := authContext.UpdateUser(user)
+	c.Assert(err, ErrorMatches, "invalid user")
+}

overlord/snapstate/snapmgr.go
@@ -403,7 +403,8 @@ func Store(s *state.State) StoreService {
 		storeID = cand
 	}
 
-	s.Cache(cachedStoreKey{}, store.NewUbuntuStoreSnapRepository(nil, storeID))
+	authContext := auth.NewAuthStateContext(s)
+	s.Cache(cachedStoreKey{}, store.NewUbuntuStoreSnapRepository(nil, storeID, authContext))
 	return cachedStore(s)
 }
 