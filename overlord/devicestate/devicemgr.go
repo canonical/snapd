affc9a41433dd2adecd657d46dabe1cce77ed5e0
Samuele Pedroni <pedronis@lucediurna.net>
2016-08-01 17:16:20 +0200

    first pass at device registration logic (all in snapd, assume classic canonical/pc (needs more first-boot work for the full story), dummy URL for now)

---
+++ overlord/devicestate/devicemgr.go
@@ -0,0 +1,384 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package devicestate implements the manager and state aspects responsible
+// for the device identity and policies.
+package devicestate
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/crypto/openpgp/packet"
+	"gopkg.in/tomb.v2"
+
+	"github.com/snapcore/snapd/asserts"
+	"github.com/snapcore/snapd/i18n"
+	"github.com/snapcore/snapd/overlord/auth"
+	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/release"
+)
+
+// DeviceManager is responsible for managing the device identity and device
+// policies.
+type DeviceManager struct {
+	state  *state.State
+	runner *state.TaskRunner
+}
+
+// Manager returns a new device manager.
+func Manager(s *state.State) (*DeviceManager, error) {
+	runner := state.NewTaskRunner(s)
+
+	runner.AddHandler("generate-device-key", doGenerateDeviceKey, nil)
+	runner.AddHandler("deliver-serial-request", doDeliverSerialRequest, nil)
+	runner.AddHandler("retrieve-n-install-serial", doRetrieveAndInstallSerial, nil)
+
+	return &DeviceManager{state: s, runner: runner}, nil
+}
+
+func (m *DeviceManager) ensureOperational() error {
+	m.state.Lock()
+	defer m.state.Unlock()
+	// XXX: auth.Device/SetDevice should probably move to devicestate
+	// they are not quite just about auth and also we risk circular imports
+	// (auth will need to mediate bits from devicestate soon)
+	devIdentity, err := auth.Device(m.state)
+	if err != nil {
+		return err
+	}
+
+	if devIdentity.Serial != "" {
+		// serial is set, we are all set
+		return nil
+	}
+
+	if devIdentity.Brand == "" || devIdentity.Model == "" {
+		// need first-boot, loading of model assertion info
+		if release.OnClassic {
+			// XXX: cheat for now to get us started somewhere
+			devIdentity.Brand = "canonical"
+			devIdentity.Model = "pc"
+			err := auth.SetDevice(m.state, devIdentity)
+			if err != nil {
+				return err
+			}
+			m.state.Unlock()
+			m.state.Lock()
+		} else {
+			// full first-boot stuff!
+			// TODO: move first boot setup/invocation here!
+			panic("need full first-boot to initialize brand and model of device")
+		}
+	}
+
+	for _, chg := range m.state.Changes() {
+		if chg.Kind() == "become-operational" && !chg.Status().Ready() {
+			// change already in motion
+			return nil
+		}
+	}
+
+	// XXX: some of these will need to be split and use hooks
+	// retries might need to embrace more than one "task" then,
+	// need to be careful
+
+	genKey := m.state.NewTask("generate-device-key", i18n.G("Generate device key"))
+	serialReq := m.state.NewTask("deliver-serial-request", i18n.G("Deliver device serial request"))
+	serialReq.WaitFor(genKey)
+	retrieveSerial := m.state.NewTask("retrieve-n-install-serial", i18n.G("Retrieve and install device serial"))
+	retrieveSerial.WaitFor(serialReq)
+	serialReq.Set("serial-setup-task", retrieveSerial.ID())
+
+	chg := m.state.NewChange("become-operational", i18n.G("Setting up device identity"))
+	chg.AddAll(state.NewTaskSet(genKey, serialReq, retrieveSerial))
+
+	return nil
+}
+
+// Ensure implements StateManager.Ensure.
+func (m *DeviceManager) Ensure() error {
+	err := m.ensureOperational()
+	if err != nil {
+		return err
+	}
+	m.runner.Ensure()
+	return nil
+}
+
+// Wait implements StateManager.Wait.
+func (m *DeviceManager) Wait() {
+	m.runner.Wait()
+}
+
+// Stop implements StateManager.Stop.
+func (m *DeviceManager) Stop() {
+	m.runner.Stop()
+}
+
+var (
+	keyLength = 4096
+	// XXX: a 2nd different URL for nonce?
+	// TODO: this will come as config from the gadget snap
+	serialRequestURL = "https://serial.request"  // XXX dummy value!
+)
+
+func doGenerateDeviceKey(t *state.Task, _ *tomb.Tomb) error {
+	st := t.State()
+	st.Lock()
+	defer st.Unlock()
+	// XXX: where to store device private key ultimately? use asserts support?
+	var encoded string
+	err := st.Get("device-keypair", &encoded)
+	if err == nil {
+		// nothing to do
+		return nil
+	}
+	if err != state.ErrNoState {
+		return err
+	}
+	keyPair, err := rsa.GenerateKey(rand.Reader, keyLength)
+	if err != nil {
+		return fmt.Errorf("cannot generate device key pair: %v", err)
+	}
+
+	encoded = base64.StdEncoding.EncodeToString(x509.MarshalPKCS1PrivateKey(keyPair))
+	st.Set("device-keypair", encoded)
+	return nil
+}
+
+func keyPair(st *state.State) (*rsa.PrivateKey, error) {
+	var encoded string
+	err := st.Get("device-keypair", &encoded)
+	if err == state.ErrNoState {
+		return nil, fmt.Errorf("internal error: cannot find device key pair")
+	}
+	if err != nil {
+		return nil, err
+	}
+	b, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return nil, fmt.Errorf("internal error: cannot decode device key pair: %v", err)
+	}
+	pk, err := x509.ParsePKCS1PrivateKey(b)
+	if err != nil {
+		return nil, fmt.Errorf("internal error: cannot decode device key pair: %v", err)
+	}
+	return pk, nil
+}
+
+type serialSetup struct {
+	Serial        string `json:"serial"`
+	SerialRequest string `json:"serial-request"`
+}
+
+type nonceTicketResp struct {
+	NonceTicket string `json:"nonce-ticket"`
+}
+
+var errPoll = errors.New("serial-request accepted, poll later")
+
+func submitSerialRequest(cli *http.Client, encodedSerialRequest []byte) (*asserts.Serial, error) {
+	resp, err := cli.Post(serialRequestURL, asserts.MediaType, bytes.NewBuffer(encodedSerialRequest))
+	if err != nil {
+		return nil, &state.Retry{After: 60 * time.Second}
+	}
+	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+		return nil, fmt.Errorf("unexpected status code %d trying to post serial request to %s)", resp.StatusCode, serialRequestURL)
+	}
+
+	switch resp.StatusCode {
+	case 200, 201:
+	case 202:
+		return nil, errPoll
+	default:
+		return nil, &state.Retry{After: 60 * time.Second}
+	}
+
+	// decode body with serial assertion
+	dec := asserts.NewDecoder(resp.Body)
+	got, err := dec.Decode()
+	if err != nil { // assume broken i/o
+		return nil, &state.Retry{After: 60 * time.Second}
+	}
+
+	serial, ok := got.(*asserts.Serial)
+	if !ok {
+		return nil, fmt.Errorf("serial-request response did not return a serial assertion")
+	}
+
+	return serial, nil
+}
+
+func doDeliverSerialRequest(t *state.Task, _ *tomb.Tomb) error {
+	st := t.State()
+	st.Lock()
+	defer st.Unlock()
+
+	devIdentity, err := auth.Device(st)
+	if err != nil {
+		return err
+	}
+
+	var retrieveSerialID string
+	err = t.Get("serial-setup-task", &retrieveSerialID)
+	if err != nil {
+		return err
+	}
+	retrieveSerial := st.Task(retrieveSerialID)
+	if retrieveSerial == nil {
+		return fmt.Errorf("internal error: cannot find retrieve serial task")
+	}
+
+	pk, err := keyPair(st)
+	if err != nil {
+		return err
+	}
+
+	cli := &http.Client{Timeout: 30 * time.Second}
+
+	st.Unlock()
+	resp, err := cli.Get(serialRequestURL)
+	st.Lock()
+	if err != nil {
+		return &state.Retry{After: 60 * time.Second}
+	}
+	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+		return fmt.Errorf("unexpected status code %d trying to get serial request nonce/ticket (from %s)", resp.StatusCode, serialRequestURL)
+	}
+	if resp.StatusCode != 200 {
+		return &state.Retry{After: 60 * time.Second}
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	var nonceTicket nonceTicketResp
+	err = dec.Decode(&nonceTicket)
+	if err != nil { // assume broken i/o
+		return &state.Retry{After: 60 * time.Second}
+	}
+
+	// XXX: do it the hard way for now (should be much simpler soon)
+	privKey := asserts.OpenPGPPrivateKey(packet.NewRSAPrivateKey(time.Unix(1, 0), pk))
+	encodedPubKey, err := asserts.EncodePublicKey(privKey.PublicKey())
+	if err != nil {
+		return fmt.Errorf("internal error: cannot encode device public key: %v", err)
+
+	}
+
+	serialReq, err := asserts.FreestandingSign(asserts.SerialRequestType, map[string]interface{}{
+		"brand-id":     devIdentity.Brand,
+		"model":        devIdentity.Model,
+		"nonce-ticket": nonceTicket.NonceTicket,
+		"device-key":   string(encodedPubKey),
+	}, nil, privKey) // XXX: fill body with some agreed hardware details
+	if err != nil {
+		return err
+	}
+
+	encodedSerialReq := asserts.Encode(serialReq)
+
+	// NB: until we get at least an Accepted (202) we need to
+	// retry from scratch creating a new nonce because the
+	// previous one used could have expired
+
+	st.Unlock()
+	serial, err := submitSerialRequest(cli, encodedSerialReq)
+	st.Lock()
+	if err == errPoll {
+		retrieveSerial.Set("serial-setup", serialSetup{
+			SerialRequest: string(encodedSerialReq),
+		})
+		// TODO: delay next task?
+		t.SetStatus(state.DoneStatus)
+		return nil
+	}
+	if err != nil { // errors and retries
+		return err
+	}
+
+	retrieveSerial.Set("serial-setup", serialSetup{
+		Serial: string(asserts.Encode(serial)),
+	})
+	t.SetStatus(state.DoneStatus)
+	return nil
+}
+
+func doRetrieveAndInstallSerial(t *state.Task, _ *tomb.Tomb) error {
+	st := t.State()
+	st.Lock()
+	defer st.Unlock()
+
+	devIdentity, err := auth.Device(st)
+	if err != nil {
+		return err
+	}
+
+	var serialSup serialSetup
+	err = t.Get("serial-setup", &serialSup)
+	if err != nil {
+		return err
+	}
+
+	var serial *asserts.Serial
+
+	if serialSup.SerialRequest != "" {
+		// delivery of serial-request was asked to poll
+		cli := &http.Client{Timeout: 30 * time.Second}
+		var err error
+		st.Unlock()
+		serial, err = submitSerialRequest(cli, []byte(serialSup.SerialRequest))
+		st.Lock()
+		if err == errPoll {
+			// TODO: what poll interval?
+			return &state.Retry{After: 60 * time.Second}
+
+		}
+		if err != nil { // errors and retries
+			return err
+		}
+
+	} else {
+		// delivery of serial-request got directly a result
+		a, err := asserts.Decode([]byte(serialSup.Serial))
+		if err != nil {
+			return fmt.Errorf("internal error: cannot decode retrieved serial assertion: %v", err)
+		}
+		var ok bool
+		serial, ok = a.(*asserts.Serial)
+		if !ok {
+			return fmt.Errorf("internal error: retrieved serial assertion has wrong type")
+		}
+	}
+
+	// TODO: (possibly refetch brand key and) verify serial assertion!
+	// TODO: double check brand, model and key hash
+
+	devIdentity.Serial = serial.Serial()
+	auth.SetDevice(st, devIdentity)
+	t.SetStatus(state.DoneStatus)
+	return nil
+}
---
+++ overlord/devicestate/devicemgr_test.go
@@ -0,0 +1,142 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package devicestate_test
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/asserts"
+	"github.com/snapcore/snapd/asserts/assertstest"
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/overlord/auth"
+	"github.com/snapcore/snapd/overlord/devicestate"
+	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/release"
+)
+
+func TestDeviceManager(t *testing.T) { TestingT(t) }
+
+type deviceMgrSuite struct {
+	state *state.State
+	mgr   *devicestate.DeviceManager
+
+	storeSigning *assertstest.StoreStack
+}
+
+var _ = Suite(&deviceMgrSuite{})
+
+func (s *deviceMgrSuite) SetUpTest(c *C) {
+	dirs.SetRootDir(c.MkDir())
+
+	rootPrivKey, _ := assertstest.GenerateKey(1024)
+	storePrivKey, _ := assertstest.GenerateKey(752)
+	s.storeSigning = assertstest.NewStoreStack("canonical", rootPrivKey, storePrivKey)
+
+	s.state = state.New(nil)
+	mgr, err := devicestate.Manager(s.state)
+	c.Assert(err, IsNil)
+
+	s.mgr = mgr
+}
+
+func (s *deviceMgrSuite) TearDownTest(c *C) {
+	dirs.SetRootDir("")
+}
+
+func (s *deviceMgrSuite) settle() {
+	for i := 0; i < 50; i++ {
+		s.mgr.Ensure()
+		s.mgr.Wait()
+	}
+}
+
+func (s *deviceMgrSuite) TestFullDeviceRegistrationHappyOnClassic(c *C) {
+	r1 := release.MockOnClassic(true)
+	defer r1()
+	r2 := devicestate.MockKeyLength(752)
+	defer r2()
+
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			w.WriteHeader(http.StatusOK)
+			io.WriteString(w, `{"nonce-ticket": "NONCE-TICKET-1"}`)
+		case "POST":
+			b, err := ioutil.ReadAll(r.Body)
+			c.Assert(err, IsNil)
+			a, err := asserts.Decode(b)
+			c.Assert(err, IsNil)
+			serialReq, ok := a.(*asserts.SerialRequest)
+			c.Assert(ok, Equals, true)
+			err = asserts.SignatureCheck(serialReq, serialReq.DeviceKey())
+			c.Assert(err, IsNil)
+			c.Check(serialReq.BrandID(), Equals, "canonical")
+			c.Check(serialReq.Model(), Equals, "pc")
+			serial, err := s.storeSigning.Sign(asserts.SerialType, map[string]interface{}{
+				"brand-id":   "canonical",
+				"model":      "pc",
+				"serial":     "9999",
+				"device-key": serialReq.HeaderString("device-key"),
+				"timestamp":  time.Now().Format(time.RFC3339),
+			}, nil, "")
+			c.Assert(err, IsNil)
+			w.Header().Set("Content-Type", asserts.MediaType)
+			w.WriteHeader(http.StatusOK)
+			w.Write(asserts.Encode(serial))
+		}
+	}))
+	defer mockServer.Close()
+
+	r3 := devicestate.MockSerialRequestURL(mockServer.URL)
+	defer r3()
+
+	// runs the whole device registration process
+	s.settle()
+
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	var becomeOperational *state.Change
+	for _, chg := range s.state.Changes() {
+		if chg.Kind() == "become-operational" {
+			becomeOperational = chg
+			break
+		}
+	}
+	c.Assert(becomeOperational, NotNil)
+
+	c.Check(becomeOperational.Status().Ready(), Equals, true)
+	c.Check(becomeOperational.Err(), IsNil)
+
+	devId, err := auth.Device(s.state)
+	c.Assert(err, IsNil)
+	c.Check(devId.Brand, Equals, "canonical")
+	c.Check(devId.Model, Equals, "pc")
+	c.Check(devId.Serial, Equals, "9999")
+
+	// TODO: check we now have a device key and serial assertion
+}
