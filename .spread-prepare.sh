e3471650d6177a6310ff703c71e0b6ddbb645ab5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-08-11 16:21:54 +0200

    Use downstream packaging in spread tests
    This patch changes spread setup phase to build the native package (for
    Ubuntu and soon for Debian) in a way that is more consistent with the
    actual release process. This should help us release with confidence as
    all CI tests will run with real downstream packaging applied to real
    upstream tarball and build in an isolated manner with sbuild.
    
    This also paves the way towards the removal of the debian/ packaging
    from the upstream repository. This will be done in a separate pull
    request to avoid clutter.
    
    The spread prepare stage was moved to .spread-prepare.sh so that it can
    be shell-checked more easily.
    
    The debian-8 spread target was removed because currently the downstream
    packaging is not compatible. Debian maintainers were notified to the
    issue so that we should be able to re-enable Debian (sid) support soon.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ .spread-prepare.sh
@@ -0,0 +1,226 @@
+#!/bin/sh
+# This script is started by spread to prepare the execution environment
+set -x
+set -u
+set -e
+
+# Sanity check, are we in the top-level directory of the tree?
+test -f configure.ac || ( echo 'this script must be executed from the top-level of the tree' && exit 1)
+
+# Record where the top level directory is
+top_dir=$(pwd)
+
+# Record the current distribution release data to know what to do
+release_ID="$( . /etc/os-release && echo "${ID:-linux}" )"
+release_VERSION_ID="$( . /etc/os-release && echo "${VERSION_ID:-}" )"
+
+
+# Create source distribution tarball and place it in the top-level directory.
+create_dist_tarball() {
+    # Load the version number from a dedicated file
+    local pkg_version=
+    pkg_version="$(cat "$top_dir/VERSION")"
+
+    # Ensure that build system is up-to-date and ready
+    autoreconf -i
+    # XXX: This fixes somewhat odd error when configure below (in an empty directory) fails with:
+    # configure: error: source directory already configured; run "make distclean" there first
+    test -f Makefile && make distclean
+
+    # Create a scratch space to run configure
+    scratch_dir="$(mktemp -d)"
+    trap 'rm -rf "$scratch_dir"' EXIT
+
+    # Configure the project in a scratch directory
+    cd "$scratch_dir"
+    "$top_dir/configure" --prefix=/usr
+
+    # Create the distribution tarball
+    make dist
+
+    # Ensure we got the tarball we were expecting to see
+    test -f "snap-confine-$pkg_version.tar.gz"
+
+    # Move it to the top-level directory
+    mv "snap-confine-$pkg_version.tar.gz" "$top_dir/"
+}
+
+
+# build Ubuntu binary package and place it in $top_dir
+# $1 = /etc/os-release ID field
+# $2 = /etc/os-release VERSION_ID field (possibly empty!)
+build_debian_or_ubuntu_package() {
+    local pkg_version=
+    local distro_codename=
+    local distro_archive=
+    local distro_packaging_git=
+    local distro_packaging_git_branch=
+    local sbuild_args=
+    
+    pkg_version="$(cat "$top_dir/VERSION")"
+
+    case "$1" in
+        ubuntu)
+            distro_archive=http://archive.ubuntu.com/ubuntu
+            distro_packaging_git=https://git.launchpad.net/snap-confine
+            case "$2" in
+                14.04)
+                    distro_codename=trusty
+                    distro_packaging_git_branch=14.04
+                    ;;
+                16.04)
+                    distro_codename=xenial
+                    distro_packaging_git_branch=16.04
+                    ;;
+                16.10)
+                    distro_codename=yakkety
+                    distro_packaging_git_branch=16.10
+                    ;;
+                *)
+                    echo "unsupported Ubuntu VERSION_ID: $2"
+                    exit 1
+                    ;;
+            esac
+            # NOTE: universe has to be enabled as it is not enabled by default
+            sbuild_args="--extra-repository=deb http://archive.ubuntu.com/ubuntu/ ${distro_codename} universe"
+            ;;
+        debian)
+            distro_archive=http://ftp.debian.org/debian
+            # NOTE: Debian packaging needs to be updated. I sent a mail to the
+            # debian maintainer with instructions on what needs to happen and
+            # how it fits into the CI system.
+            #
+            # For now all builds on debian will fail as they still contains
+            # debian/patches that are now applied upstream.
+            distro_packaging_git=git://anonscm.debian.org/collab-maint/snap-confine.git
+            case "$2" in
+                '')  # sid
+                    distro_codename=sid
+                    distro_packaging_git_branch=debian
+                    ;;
+                *)
+                    echo "unsupported Debian VERSION_ID: $2"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            echo "unsupported distribution ID: $1"
+            exit 1
+            ;;
+    esac
+
+    # Ensure that we have a sbuild chroot ready
+    if ! schroot -l | grep "chroot:${distro_codename}-.*-sbuild"; then
+        sbuild-createchroot \
+            --include=eatmydata \
+            "--make-sbuild-tarball=/var/lib/sbuild/${distro_codename}-amd64.tar.gz" \
+            "$distro_codename" "$(mktemp -d)" \
+            "$distro_archive"
+    fi
+
+    # Create a scratch space 
+    scratch_dir="$(mktemp -d)"
+    trap 'rm -rf "$scratch_dir"' EXIT
+
+    # Do everything in the scratch directory
+    cd "$scratch_dir"
+
+    # Fetch the current Ubuntu packaging for the appropriate release
+    git clone -b "$distro_packaging_git_branch" "$distro_packaging_git" distro-packaging
+
+    # Install all the build dependencies declared by the package.
+    apt build-dep -y ./distro-packaging/
+
+    # Generate a new upstream tarball from the current state of the tree
+    ( cd "$top_dir" && create_dist_tarball )
+
+    # Prepare the .orig tarball and unpackaged source tree
+    cp "$top_dir/snap-confine-$pkg_version.tar.gz" "snap-confine_$pkg_version.orig.tar.gz"
+    tar -zxf "snap-confine_$pkg_version.orig.tar.gz" 
+
+    # Apply the debian directory from downstream packaging to form a complete source package
+    mv "distro-packaging/debian" "snap-confine-$pkg_version/debian"
+    rm -rf distro-packaging
+
+    # Add an automatically-generated changelog entry
+    # The --controlmaint takes the maintainer details from debian/control
+    ( cd "snap-confine-$pkg_version" && dch --controlmaint --newversion "${pkg_version}-1" "Automatic CI build")
+
+    # Build an unsigned source package
+    ( cd "snap-confine-$pkg_version" && dpkg-buildpackage -uc -us -S )
+
+    # Copy source package files to the top-level directory (this helps for
+    # interactive debugging since the package is available right there)
+    cp ./*.dsc ./*.debian.tar.* ./*.orig.tar.gz "$top_dir/"
+
+    # Build a binary package in a clean chroot.
+    # NOTE: nocheck is because the package still includes old unit tests that
+    # are deeply integrated into how ubuntu apparmor denials are logged. This
+    # should be removed once those test are migrated to spread testes.
+    DEB_BUILD_OPTIONS=nocheck sbuild \
+        --arch-all \
+        --dist="$distro_codename" \
+        --batch \
+        "$sbuild_args" \
+        "snap-confine_${pkg_version}-1.dsc"
+
+    # Copy all binary packages to the top-level directory
+    cp ./*.deb "$top_dir/"
+}
+
+
+# Apply tweaks
+case "$release_ID" in
+    ubuntu)
+        # apt update is hanging on security.ubuntu.com with IPv6.
+        sysctl -w net.ipv6.conf.all.disable_ipv6=1
+        trap "sysctl -w net.ipv6.conf.all.disable_ipv6=0" EXIT
+        ;;
+esac
+
+
+# Install all the build dependencies
+case "$release_ID" in
+    ubuntu|debian)
+        apt-get update
+        # On Debian and derivatives we need the following things:
+        # - sbuild -- to build the binary package with extra hygiene
+        # - devscripts -- to modify the changelog automatically
+        # - git -- to clone native downstream packaging
+        apt-get install --quiet -y sbuild devscripts git
+        # XXX: Taken from https://wiki.debian.org/sbuild
+        mkdir -p /root/.gnupg
+        # NOTE: We cannot use sbuild-update --keygen as virtual machines lack
+        # the necessary entropy to generate keys before the spread timeout
+        # kicks in. Instead we just copy pre-made, insecure keys from the
+        # source repository.
+        mkdir -p /var/lib/sbuild/apt-keys/
+        cp -a .spread-data/apt-keys/* /var/lib/sbuild/apt-keys/
+        sbuild-adduser "$LOGNAME"
+        ;;
+    *)
+        echo "unsupported distribution: $release_ID"
+        echo "patch spread-prepare to teach it about how to install build dependencies"
+        exit 1
+        ;;
+esac
+
+# Build and install the native package using downstream packaging and the fresh upstream tarball
+case "$release_ID" in
+    ubuntu|debian)
+        build_debian_or_ubuntu_package "$release_ID" "$release_VERSION_ID"
+        # Install the freshly-built packages
+        dpkg -i snap-confine_*.deb || apt-get -f install -y
+        dpkg -i ubuntu-core-launcher_*.deb || apt-get -f install -y
+        # Install snapd (testes require it)
+        apt-get install -y snapd
+        ;;
+    *)
+        echo "unsupported distribution: $release_ID"
+        exit 1
+        ;;
+esac
+
+# Install the core snap
+snap list | grep -q ubuntu-core || snap install ubuntu-core
