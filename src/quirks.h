5eb877d8b41f8c04cb05ab1b2e560ae6191a0f72
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-08-16 23:56:00 +0200

    Add quirk for /var/lib/lxd
    This patch adds a quirk that ensures /var/lib/lxd is bind mounted from
    the hostfs if such directory is available there.
    
    Fixes: https://bugs.launchpad.net/snap-confine/+bug/1613845
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- src/quirks.h
+++ src/quirks.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Canonical Ltd
+ * Copyright (C) 2016 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -15,37 +15,16 @@
  *
  */
 
-#include "cleanup-funcs.h"
+#ifndef SNAP_QUIRKS_H
+#define SNAP_QUIRKS_H
 
-#include <mntent.h>
+/**
+ * Setup various quirks that have to exists for now.
+ *
+ * This function applies non-standard tweaks that are required
+ * because of requirement to stay compatible with certain snaps
+ * that were tested with pre-chroot layout.
+ **/
+void sc_setup_quirks();
 
-void sc_cleanup_string(char **ptr)
-{
-	free(*ptr);
-}
-
-void sc_cleanup_file(FILE ** ptr)
-{
-	if (*ptr != NULL)
-		fclose(*ptr);
-}
-
-void sc_cleanup_endmntent(FILE ** ptr)
-{
-	if (*ptr != NULL)
-		endmntent(*ptr);
-}
-
-#ifdef HAVE_SECCOMP
-void sc_cleanup_seccomp_release(scmp_filter_ctx * ptr)
-{
-	seccomp_release(*ptr);
-}
-#endif				// HAVE_SECCOMP
-
-void sc_cleanup_closedir(DIR ** ptr)
-{
-	if (*ptr != NULL) {
-		closedir(*ptr);
-	}
-}
+#endif
