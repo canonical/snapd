822255a53d9efae139243f8cf203cb2407857f5c
Michael Vogt <mvo@ubuntu.com>
2016-08-19 09:43:06 +0200

    Move to a single suite and setup based on SPREAD_SYSTEM

--- tests/lib/reset.sh
+++ tests/lib/reset.sh
@@ -2,15 +2,16 @@
 
 set -e -x
 
-systemctl stop snapd.service snapd.socket
+reset_classic() {
+    systemctl stop snapd.service snapd.socket
 
-# purge all state
-sh ${SPREAD_PATH}/debian/snapd.postrm purge
-mkdir -p /snap /var/snap /var/lib/snapd
-
-rm -f /tmp/ubuntu-core*
-
-if [ "$1" = "--reuse-core" ]; then
+    # purge all state
+    sh ${SPREAD_PATH}/debian/snapd.postrm purge
+    mkdir -p /snap /var/snap /var/lib/snapd
+    
+    rm -f /tmp/ubuntu-core*
+    
+    if [ "$1" = "--reuse-core" ]; then
 	$(cd / && tar xzf $SPREAD_PATH/snapd-state.tar.gz)
 	mounts="$(systemctl list-unit-files | grep '^snap[-.].*\.mount' | cut -f1 -d ' ')"
 	services="$(systemctl list-unit-files | grep '^snap[-.].*\.service' | cut -f1 -d ' ')"
@@ -18,8 +19,23 @@ if [ "$1" = "--reuse-core" ]; then
 	for unit in $mounts $services; do
 	    systemctl start $unit
 	done
-fi
-systemctl start snapd.socket
+    fi
+    systemctl start snapd.socket
 
-# wait for snapd listening
-while ! printf "GET / HTTP/1.0\r\n\r\n" | nc -U -q 1 /run/snapd.socket; do sleep 0.5; done
+    # wait for snapd listening
+    while ! printf "GET / HTTP/1.0\r\n\r\n" | nc -U -q 1 /run/snapd.socket; do sleep 0.5; done
+}
+
+reset_all_snap() {
+    systemctl stop snapd.socket
+    rm -rf /var/lib/snapd/*
+    rm -rf /snap/bin/*
+    $(cd / && tar xzf $SPREAD_PATH/snapd-state.tar.gz)
+    systemctl start snapd.socket
+}
+
+if [ "$SPREAD_SYSTEM" = "ubuntu-core-16" ]; then
+    reset_all_snap "$@"
+else
+    reset_classic "$@"
+fi
