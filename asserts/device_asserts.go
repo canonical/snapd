6717dd8bdb2ae7451be4ddccb94f66903fcf567a
Samuele Pedroni <pedronis@lucediurna.net>
2016-08-25 21:32:48 +0200

    remove unsupported/postponed/reserved headers and make sure they don't sneak in for now in prepare-image, make store optional, defaulting to the ubuntu store if left out

--- asserts/device_asserts.go
+++ asserts/device_asserts.go
@@ -24,13 +24,10 @@ import (
 	"time"
 )
 
-// TODO: model assertion still needs final design review!
-
 // Model holds a model assertion, which is a statement by a brand
 // about the properties of a device model.
 type Model struct {
 	assertionBase
-	allowedModes  []string
 	requiredSnaps []string
 	timestamp     time.Time
 }
@@ -48,11 +45,6 @@ func (mod *Model) Model() string {
 // Series returns the series of the core software the model uses.
 func (mod *Model) Series() string {
 	return mod.HeaderString("series")
-}
-
-// Core returns the core snap the model uses.
-func (mod *Model) Core() string {
-	return mod.HeaderString("core")
 }
 
 // Architecture returns the archicteture the model is based on.
@@ -75,20 +67,9 @@ func (mod *Model) Store() string {
 	return mod.HeaderString("store")
 }
 
-// AllowedModes returns which ones of the "classic" and "developer" modes are allowed for the model.
-func (mod *Model) AllowedModes() []string {
-	return mod.allowedModes
-}
-
 // RequiredSnaps returns the snaps that must be installed at all times and cannot be removed for this model.
 func (mod *Model) RequiredSnaps() []string {
 	return mod.requiredSnaps
-}
-
-// Class returns which class the model belongs to defining policies for
-// additional software installation.
-func (mod *Model) Class() string {
-	return mod.HeaderString("class")
 }
 
 // Timestamp returns the time when the model assertion was issued.
@@ -115,7 +96,7 @@ func checkAuthorityMatchesBrand(a Assertion) error {
 	return nil
 }
 
-var modelMandatory = []string{"core", "architecture", "gadget", "kernel", "store", "class"}
+var modelMandatory = []string{"architecture", "gadget", "kernel"}
 
 func assembleModel(assert assertionBase) (Assertion, error) {
 	err := checkAuthorityMatchesBrand(&assert)
@@ -129,18 +110,33 @@ func assembleModel(assert assertionBase) (Assertion, error) {
 		}
 	}
 
-	// TODO: check 'class' value already here? fundamental policy derives from it
+	// store is optional but must be a string, defaults to the ubuntu store
+	_, err = checkShouldBeString(assert.headers, "store")
+	if err != nil {
+		return nil, err
+	}
+
+	reqSnaps, err := checkStringList(assert.headers, "required-snaps")
+	if err != nil {
+		return nil, err
+	}
 
 	timestamp, err := checkRFC3339Date(assert.headers, "timestamp")
 	if err != nil {
 		return nil, err
 	}
 
+	// NB:
+	// * core is not supported at this time, it defaults to ubuntu-core
+	// in prepare-image until rename and/or introduction of the header.
+	// * some form of allowed-modes, class are postponed,
+	//
+	// prepare-image takes care of not allowing them for now
+
 	// ignore extra headers and non-empty body for future compatibility
 	return &Model{
 		assertionBase: assert,
-		allowedModes:  nil, // XXX: empty for now
-		requiredSnaps: nil, // XXX: empty for now
+		requiredSnaps: reqSnaps,
 		timestamp:     timestamp,
 	}, nil
 }
