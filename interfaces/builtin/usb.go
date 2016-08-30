85ca0f886ca63ba2b2f7a874c01be95640765a21
axelebas <axel.lebas@gmail.com>
2016-08-30 22:05:32 +0200

    Update usb.go

--- interfaces/builtin/usb.go
+++ interfaces/builtin/usb.go
@@ -21,7 +21,7 @@ package builtin
 
 import (
 	"github.com/snapcore/snapd/interfaces"
-        "github.com/snapcore/snapd/release"
+       "github.com/snapcore/snapd/release"
 )
 
 // http://bazaar.launchpad.net/~ubuntu-security/ubuntu-core-security/trunk/view/head:/data/apparmor/policygroups/ubuntu-core/16.04/home
