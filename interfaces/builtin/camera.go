878b62ea9510eead7f6a0b078be3bb738efa6b84
Jamie Strandboge <jamie@ubuntu.com>
2016-08-30 14:59:30 -0500

    interfaces: all access to /dev/video[0-9]* in camera interface

--- interfaces/builtin/camera.go
+++ interfaces/builtin/camera.go
@@ -24,7 +24,8 @@ import (
 )
 
 const cameraConnectedPlugAppArmor = `
-/dev/video0 rw,
+# Until we have proper device assignment, allow access to all cameras
+/dev/video[0-9]* rw,
 
 # Allow detection of cameras. Leaks plugged in USB device info
 /sys/bus/usb/devices/ r,
