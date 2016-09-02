00903838cfcd1d8fbf6228baf5c082c508756666
Michael Hudson-Doyle <michael.hudson@canonical.com>
2016-09-02 12:51:19 +1200

    configure eth* as well as en*

--- firstboot/firstboot.go
+++ firstboot/firstboot.go
@@ -54,9 +54,13 @@ var netplanConfigData = `
 network:
  version: 2
  ethernets:
-   all:
+   all-en:
     match:
      name: "en*"
+    dhcp4: true
+   all-eth:
+    match:
+     name: "eth*"
     dhcp4: true
 `
 
