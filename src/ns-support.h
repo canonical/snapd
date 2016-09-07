9203b37586cb5476424b66d431b02a9f0e83097d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-09-07 12:30:08 +0200

    Trim whitespace
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- src/ns-support.h
+++ src/ns-support.h
@@ -102,7 +102,7 @@ void sc_unlock_ns_mutex(struct sc_ns_group *group);
  * can be done by calling sc_preserve_populated_ns_group()). In the meantime
  * the parent process unshares the mount namespace() and sets a flag so that
  * sc_should_populate_ns_group() returns true.
- * 
+ *
  * @returns true if the mount namespace needs to be populated
  **/
 void sc_create_or_join_ns_group(struct sc_ns_group *group);
