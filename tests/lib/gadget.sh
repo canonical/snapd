edcb2132caa48f5402f609e9b53cf154ebaaa9c6
Federico Gimenez <fgimenez@canonical.com>
2016-09-09 17:39:11 +0200

    gadget list completed

--- tests/lib/gadget.sh
+++ tests/lib/gadget.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 get_gadget_name(){
-    snap list | grep '^pc \|^pi2 ' | head -n 1 | cut -d ' ' -f 1
+    snap list | grep '^pc \|^pi2 \|^pi3 \|^dragonboard ' | head -n 1 | cut -d ' ' -f 1
 }
