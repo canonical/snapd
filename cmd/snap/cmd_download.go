fa00dd7711bc245dc1042ecd9a17d031cda2f384
Samuele Pedroni <pedronis@lucediurna.net>
2016-09-09 17:26:44 +0200

    start sharing some code between snap download and prepare-image

--- cmd/snap/cmd_download.go
+++ cmd/snap/cmd_download.go
@@ -29,9 +29,8 @@ import (
 	"github.com/snapcore/snapd/asserts"
 	"github.com/snapcore/snapd/asserts/sysdb"
 	"github.com/snapcore/snapd/i18n"
-	"github.com/snapcore/snapd/osutil"
+	"github.com/snapcore/snapd/image"
 	"github.com/snapcore/snapd/overlord/auth"
-	"github.com/snapcore/snapd/progress"
 	"github.com/snapcore/snapd/snap"
 	"github.com/snapcore/snapd/store"
 )
@@ -56,47 +55,32 @@ func init() {
 	})
 }
 
-func fetchSnapAssertions(sto *store.Store, snapFn string) (string, error) {
-	// TODO: share some of this code
+func fetchSnapAssertions(sto *store.Store, snapPath string, snapInfo *snap.Info, dlOpts *image.DownloadOptions) error {
 	db, err := asserts.OpenDatabase(&asserts.DatabaseConfig{
-		KeypairManager: asserts.NewMemoryKeypairManager(),
-		Backstore:      asserts.NewMemoryBackstore(),
-		Trusted:        sysdb.Trusted(),
+		Backstore: asserts.NewMemoryBackstore(),
+		Trusted:   sysdb.Trusted(),
 	})
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	assertsFn := snapFn + ".asserts"
+	assertsFn := filepath.Join(filepath.Dir(snapPath), filepath.Base(snapInfo.MountFile())+".asserts")
 	w, err := os.Create(assertsFn)
 	if err != nil {
-		return "", fmt.Errorf("cannot create assertions file: %s", err)
+		return fmt.Errorf("cannot create assertions file: %v", err)
 	}
 	defer w.Close()
 
 	encoder := asserts.NewEncoder(w)
 	retrieve := func(ref *asserts.Ref) (asserts.Assertion, error) {
-		return sto.Assertion(ref.Type, ref.PrimaryKey, nil)
+		return sto.Assertion(ref.Type, ref.PrimaryKey, dlOpts.User)
 	}
 	save := func(a asserts.Assertion) error {
 		return encoder.Encode(a)
 	}
 	f := asserts.NewFetcher(db, retrieve, save)
 
-	sha3_384, _, err := asserts.SnapFileSHA3_384(snapFn)
-	if err != nil {
-		return "", err
-	}
-	ref := &asserts.Ref{
-		Type:       asserts.SnapRevisionType,
-		PrimaryKey: []string{sha3_384},
-	}
-	if err := f.Fetch(ref); err != nil {
-		return "", fmt.Errorf("cannot fetch assertion %s: %s", ref, err)
-	}
-	// FIXME: cross check assertions here?
-
-	return assertsFn, nil
+	return image.FetchSnapAssertions(snapPath, snapInfo, f, db)
 }
 
 func (x *cmdDownload) Execute(args []string) error {
@@ -128,30 +112,25 @@ func (x *cmdDownload) Execute(args []string) error {
 	sto := store.New(nil, authContext)
 	// we always allow devmode for downloads
 	devMode := true
-	snap, err := sto.Snap(snapName, x.Channel, devMode, revision, user)
-	if err != nil {
-		return err
+
+	dlOpts := image.DownloadOptions{
+		TargetDir: "", // cwd
+		DevMode:   devMode,
+		Channel:   x.Channel,
+		User:      user,
 	}
 
 	fmt.Printf("Fetching snap %s\n", snapName)
-	pb := progress.NewTextProgress()
-	tmpName, err := sto.Download(snapName, &snap.DownloadInfo, pb, user)
+	snapPath, snapInfo, err := image.DownloadSnap(sto, snapName, revision, &dlOpts)
 	if err != nil {
 		return err
 	}
-	defer os.Remove(tmpName)
 
 	fmt.Printf("Fetching assertions for %s\n", snapName)
-	tmpAssertsFn, err := fetchSnapAssertions(sto, tmpName)
+	err = fetchSnapAssertions(sto, snapPath, snapInfo, &dlOpts)
 	if err != nil {
 		return err
 	}
-	defer os.Remove(tmpAssertsFn)
 
-	// copy files into the working directory
-	targetFn := filepath.Base(snap.MountFile())
-	if err := osutil.CopyFile(tmpAssertsFn, targetFn+".asserts", 0); err != nil {
-		return err
-	}
-	return osutil.CopyFile(tmpName, targetFn, 0)
+	return nil
 }
