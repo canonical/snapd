4b82b88fe832897d0348f233c2f63125b7c4ecf1
Jamie Strandboge <jamie@ubuntu.com>
2016-09-13 13:06:56 -0500

    interfaces: allow ixr of sha*sum binaries

--- interfaces/apparmor/template.go
+++ interfaces/apparmor/template.go
@@ -149,6 +149,7 @@ var defaultTemplate = []byte(`
   /{,usr/}bin/rmdir ixr,
   /{,usr/}bin/sed ixr,
   /{,usr/}bin/seq ixr,
+  /{,usr/}bin/sha{1,224,256,384,512}sum ixr,
   /{,usr/}bin/shuf ixr,
   /{,usr/}bin/sleep ixr,
   /{,usr/}bin/sort ixr,
