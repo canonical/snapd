faca41af50c70fa784fecea6dea341ae4290f6e0
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-09-16 14:34:33 +0200

    Quote command argument.

--- tests/lib/systemd.sh
+++ tests/lib/systemd.sh
@@ -2,7 +2,7 @@
 
 # Use like systemd_create_and_start_unit(fakestore, "$(which fakestore) -start -dir $top_dir -addr localhost:11028 $@")
 systemd_create_and_start_unit() {
-    printf "[Unit]\nDescription=For testing purposes\n[Service]\nType=simple\nExecStart=%s" $2 > /run/systemd/system/$1.service
+    printf "[Unit]\nDescription=For testing purposes\n[Service]\nType=simple\nExecStart=%s" "$2" > /run/systemd/system/$1.service
     systemctl daemon-reload
     systemctl start $1
 }
