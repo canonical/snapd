de059b47bcaddb7899a79cdfea84f6f42ceef480
Federico Gimenez <fgimenez@canonical.com>
2016-09-19 14:19:50 +0200

    remove /home/fgimenez/workspace/gocode/src on backend restore

--- tests/lib/adhoc-ubuntu-core/prepare-instance.sh
+++ tests/lib/adhoc-ubuntu-core/prepare-instance.sh
@@ -9,7 +9,6 @@ execute_remote(){
     ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $INSTANCE_PORT $USER@$INSTANCE_IP "$@"
 }
 
-execute_remote "sudo rm -rf /home/gopath/src"
 execute_remote "sudo adduser --extrausers --quiet --disabled-password --gecos '' test"
 execute_remote "(echo test:ubuntu | sudo chpasswd)"
 execute_remote "(echo 'test ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-test)"
