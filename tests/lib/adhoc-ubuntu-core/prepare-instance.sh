317f5d1fe8b95157a4ee06b0ada702b6ffb0c322
Federico Gimenez <fgimenez@canonical.com>
2016-09-19 11:41:49 +0200

    build test binaries on target

--- tests/lib/adhoc-ubuntu-core/prepare-instance.sh
+++ tests/lib/adhoc-ubuntu-core/prepare-instance.sh
@@ -4,16 +4,12 @@ set -x
 INSTANCE_IP="${1:-localhost}"
 INSTANCE_PORT="${2:-8022}"
 USER="${3:-$(whoami)}"
-SNAPBUILD_BINARY="${4:-$(which snapbuild)}"
 
 execute_remote(){
     ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $INSTANCE_PORT $USER@$INSTANCE_IP "$@"
 }
 
-scp -P $INSTANCE_PORT "$SNAPBUILD_BINARY" $USER@$INSTANCE_IP:/home/$USER
-
-execute_remote "sudo rm -rf /home/gopath"
+execute_remote "sudo rm -rf /home/gopath/src"
 execute_remote "sudo adduser --extrausers --quiet --disabled-password --gecos '' test"
 execute_remote "(echo test:ubuntu | sudo chpasswd)"
 execute_remote "(echo 'test ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/create-user-test)"
-execute_remote "sudo cp /home/$USER/snapbuild /home/test"
