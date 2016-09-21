cdb39e5263cc87b50272b0948ec62b6ff8cfebeb
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-09-21 22:06:27 +0200

    Less documentation, more strictness for apt helper.

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -2,7 +2,6 @@
 
 set -eux
 
-# pull in apt helpers
 . $TESTSLIB/apt.sh
 
 update_core_snap_with_snap_exec_snapctl() {
@@ -41,7 +40,7 @@ update_core_snap_with_snap_exec_snapctl() {
 }
 
 prepare_classic() {
-    apt_install_local_packages ${SPREAD_PATH}/../snapd_*.deb
+    apt_install_local_package ${SPREAD_PATH}/../snapd_*.deb
 
     # Snapshot the state including core.
     if [ ! -f $SPREAD_PATH/snapd-state.tar.gz ]; then
@@ -81,7 +80,7 @@ prepare_classic() {
 setup_reflash_magic() {
         # install the stuff we need
         apt-get install -y kpartx busybox-static
-        apt_install_local_packages ${SPREAD_PATH}/../snapd_*.deb
+        apt_install_local_package ${SPREAD_PATH}/../snapd_*.deb
 
         snap install --edge ubuntu-core
 
