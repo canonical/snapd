4d35b1307562b1990fb9df5cdd974e91a987ecbc
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-09-21 22:06:27 +0200

    Less documentation, more strictness for apt helper.

--- tests/lib/apt.sh
+++ tests/lib/apt.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-apt_install_local_packages() {
+apt_install_local_package() {
     if [ "$SPREAD_SYSTEM" = "ubuntu-14.04-64" ]; then
         # relying on dpkg as apt(-get) does not support installation from local files in trusty.
-        dpkg -i "$@" || apt-get -f install -y
+        dpkg -i --force-depends --force-depends-version "$@"
+        apt-get -f install -y
     else
         apt install -y "$@"
     fi
