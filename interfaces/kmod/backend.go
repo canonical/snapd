534c72f658d8a34f66fb65a5d0a1bde57b1b675c
Pawel Stolowski <stolowski@gmail.com>
2016-09-23 11:27:03 +0200

    No need for sorting functions

--- interfaces/kmod/backend.go
+++ interfaces/kmod/backend.go
@@ -48,12 +48,6 @@ import (
 	"github.com/snapcore/snapd/snap"
 )
 
-type byAppName []*snap.AppInfo
-
-func (c byAppName) Len() int           { return len(c) }
-func (c byAppName) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c byAppName) Less(i, j int) bool { return c[i].Name < c[j].Name }
-
 // Backend is responsible for maintaining kernel modules
 type Backend struct{}
 
