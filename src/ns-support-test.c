6f3de3af1b6abee8ded705ccb6d0f2b52fdaaefc
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-09-23 11:23:06 +0200

    Add test for graceful sc_open_ns_group failure
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- src/ns-support-test.c
+++ src/ns-support-test.c
@@ -185,6 +185,15 @@ static void test_sc_open_ns_group()
 		      (lock_file, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_REGULAR));
 }
 
+static void test_sc_open_ns_group_graceful()
+{
+	sc_set_ns_dir("/nonexistent");
+	g_test_queue_destroy((GDestroyNotify) sc_set_ns_dir, SC_NS_DIR);
+	struct sc_ns_group *group =
+	    sc_open_ns_group("foo", SC_NS_FAIL_GRACEFULLY);
+	g_assert_null(group);
+}
+
 static void test_sc_lock_ns_mutex_precondition()
 {
 	sc_test_use_fake_ns_dir();
@@ -361,7 +370,9 @@ static void __attribute__ ((constructor)) init()
 	g_test_add_func("/ns/sc_enable_sanity_timeout",
 			test_sc_enable_sanity_timeout);
 	g_test_add_func("/ns/sc_alloc_ns_group", test_sc_alloc_ns_group);
-	g_test_add_func("/ns/sc_init_ns_group", test_sc_open_ns_group);
+	g_test_add_func("/ns/sc_open_ns_group", test_sc_open_ns_group);
+	g_test_add_func("/ns/sc_open_ns_group/graceful",
+			test_sc_open_ns_group_graceful);
 	g_test_add_func("/ns/sc_lock_unlock_ns_mutex",
 			test_sc_lock_unlock_ns_mutex);
 	g_test_add_func("/ns/sc_lock_ns_mutex/precondition",
