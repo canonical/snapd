599e6bd7948a08de1f807d0784717a6e6bcb6ab6
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-10-11 11:29:26 +0200

    Add spread tests for mount namespace layout
    This patch adds a simple test that looks at /proc/self/mountinfo within
    confined applications. The test discards some information that is too
    variable to be useful but retains the most essential facts.
    
    This test will be changed by the upcoming /media sharing patches. It is
    intended to be useful for before/after comparison.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ spread-tests/main/mount-ns-layout/process.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import sys
+import json
+import re
+
+_boring_fs = set(['cgroup', 'fusectl', 'debugfs', 'pstore', 'securityfs', 'mqueue', 'hugetlbfs'])
+
+def line2mountinfo(lines, boring_fs=_boring_fs):
+    seen = {}
+    for line in lines:
+        parts = line.split()
+        fs_type = parts[-3]
+        if fs_type in boring_fs:
+            continue
+        mount_src = parts[-2]
+        root_dir = re.sub('_\w{6}', '_XXXXXX', parts[3])
+        mount_point = re.sub('/\d+$', '/NUMBER', parts[4])
+        mount_point = re.sub('_\w{6}', '_XXXXXX', mount_point)
+        mount_opts = parts[5]
+        opt_fields = []
+        i = 6
+        while parts[i] != '-':
+            opt = parts[i]
+            # renumber options
+            if opt not in seen:
+                opt_id = len(seen)
+                seen[opt] = opt_id
+            else:
+                opt_id = seen[opt]
+            opt_fields.append(re.sub(':\d+$', lambda m: ':renumbered/{}'.format(opt_id), parts[i]))
+            i += 1
+        yield {
+            'root_dir': root_dir,
+            'mount_point': mount_point,
+            'mount_opts': mount_opts,
+            'opt_fields': opt_fields,
+            'fs_type': fs_type,
+            'mount_src': mount_src,
+        }
+
+
+def main():
+    entries = list(line2mountinfo(sorted(sys.stdin)))
+    entries.sort(key=lambda obj: obj['mount_point'])
+    json.dump(entries, sys.stdout, sort_keys=True,
+              indent=2, separators=(',', ': '))
+    sys.stdout.write('\n')
+
+
+if __name__ == '__main__':
+    main()
