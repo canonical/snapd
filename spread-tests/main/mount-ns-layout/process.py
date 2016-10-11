324a83a7259761b0addbffc0fbeb5668d4526123
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-10-11 20:17:45 +0200

    Fix the remapping script
    So, the remapping script needed some love.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- spread-tests/main/mount-ns-layout/process.py
+++ spread-tests/main/mount-ns-layout/process.py
@@ -3,47 +3,97 @@ import sys
 import json
 import re
 
-_boring_fs = set(['cgroup', 'fusectl', 'debugfs', 'pstore', 'securityfs', 'mqueue', 'hugetlbfs'])
 
-def line2mountinfo(lines, boring_fs=_boring_fs):
-    seen = {}
-    for line in lines:
+class mountinfo_entry:
+
+    def __init__(self, fs_type, mount_id, mount_opts, mount_point, mount_src, opt_fields, root_dir):
+        self.fs_type = fs_type
+        self.mount_id = mount_id
+        self.mount_opts = mount_opts
+        self.mount_point = mount_point
+        self.mount_src = mount_src
+        self.opt_fields = opt_fields
+        self.root_dir = root_dir
+
+    @classmethod
+    def parse(cls, line):
         parts = line.split()
         fs_type = parts[-3]
-        if fs_type in boring_fs:
-            continue
-        mount_src = parts[-2]
-        root_dir = re.sub('_\w{6}', '_XXXXXX', parts[3])
-        mount_point = re.sub('/\d+$', '/NUMBER', parts[4])
-        mount_point = re.sub('_\w{6}', '_XXXXXX', mount_point)
+        mount_id = parts[0]
         mount_opts = parts[5]
+        mount_point = parts[4]
+        mount_src = parts[-2]
+        root_dir = parts[3]
         opt_fields = []
         i = 6
         while parts[i] != '-':
             opt = parts[i]
-            # renumber options
+            opt_fields.append(opt)
+            i += 1
+        opt_fields.sort()
+        return cls(fs_type, mount_id, mount_opts, mount_point, mount_src,
+                   opt_fields, root_dir)
+
+    def _fix_nondeterministic_strings(self):
+        self.root_dir = re.sub('_\w{6}', '_XXXXXX', self.root_dir)
+        self.mount_point = re.sub('_\w{6}', '_XXXXXX', self.mount_point)
+        self.mount_point = re.sub('/\d+$', '/NUMBER', self.mount_point)
+        self.mount_src = re.sub('/dev/[sv]da', '/dev/BLOCK', self.mount_src)
+
+    def _fix_nondeterministic_opt_fields(self, seen):
+        fixed = []
+        for opt in self.opt_fields:
             if opt not in seen:
                 opt_id = len(seen)
                 seen[opt] = opt_id
             else:
                 opt_id = seen[opt]
-            opt_fields.append(re.sub(':\d+$', lambda m: ':renumbered/{}'.format(opt_id), parts[i]))
-            i += 1
-        yield {
-            'root_dir': root_dir,
-            'mount_point': mount_point,
-            'mount_opts': mount_opts,
-            'opt_fields': opt_fields,
-            'fs_type': fs_type,
-            'mount_src': mount_src,
+            remapped_opt = re.sub(':\d+$', lambda m: ':renumbered/{}'.format(opt_id), opt)
+            fixed.append(remapped_opt)
+        self.opt_fields = fixed
+
+    def _fix_nondeterministic_loop(self, seen):
+        if not self.mount_src.startswith("/dev/loop"):
+            return
+        if self.mount_src not in seen:
+            loop_id = len(seen)
+            seen[self.mount_src] = loop_id
+        else:
+            loop_id = seen[self.mount_src]
+        self.mount_src = re.sub('loop\d+$', lambda m: 'remapped-loop{}'.format(loop_id), self.mount_src)
+
+    def as_json(self):
+        return {
+            "fs_type": self.fs_type,
+            "mount_opts": self.mount_opts,
+            "mount_point": self.mount_point,
+            "mount_src": self.mount_src,
+            "opt_fields": self.opt_fields,
+            "root_dir": self.root_dir,
         }
 
+def parse_mountinfo(lines): 
+    return [mountinfo_entry.parse(line) for line in lines]
+
+def fix_nondeterminism(entries):
+    seen_opt_fields = {}
+    seen_loops = {}
+    for entry in entries:
+        entry._fix_nondeterministic_strings()
+        entry._fix_nondeterministic_opt_fields(seen_opt_fields)
+        entry._fix_nondeterministic_loop(seen_loops)
 
 def main():
-    entries = list(line2mountinfo(sorted(sys.stdin)))
-    entries.sort(key=lambda obj: obj['mount_point'])
-    json.dump(entries, sys.stdout, sort_keys=True,
-              indent=2, separators=(',', ': '))
+    entries = parse_mountinfo(sys.stdin)
+    # Get rid of the core snap as it is not certain that we'll see one and we want determinism
+    entries = [entry for entry in entries if not re.match("/snap/core/\d+", entry.mount_point)]
+    # Make entries nicely deterministic, by sorting them by mount location
+    entries.sort(key=lambda entry: (entry.mount_point, entry.mount_src, entry.root_dir))
+
+    # Fix random directories and nondeterministic revisions
+    fix_nondeterminism(entries)
+    json.dump([entry.as_json() for entry in entries],
+               sys.stdout, sort_keys=True, indent=2, separators=(',', ': '))
     sys.stdout.write('\n')
 
 
