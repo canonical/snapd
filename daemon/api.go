56592b52bd44e11062f443150f5848f32346b701
Michael Vogt <michael.vogt@gmail.com>
2016-10-11 22:07:28 +0200

    tests: add spread test for `snap auto-import` (#2126)

--- daemon/api.go
+++ daemon/api.go
@@ -1737,7 +1737,7 @@ func createAllKnownSystemUsers(st *state.State, createData *postUserCreateData) 
 	st.Lock()
 	assertions, err := db.FindMany(asserts.SystemUserType, headers)
 	st.Unlock()
-	if err != nil {
+	if err != nil && err != asserts.ErrNotFound {
 		return BadRequest("cannot find system-user assertion: %s", err)
 	}
 
