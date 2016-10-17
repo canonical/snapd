70fa257b78a0d3c064626e2e86636f66eaa583a7
John R. Lenton <jlenton@gmail.com>
2016-10-17 17:12:19 +0200

    * overlord/snapstate: introduce `KeepSnapPath` flag to skip removing a snap on install. * overlord/devicestate: use `KeepSnapPath` to not remove seed snaps on install. * tests/main/ubuntu-core-custom-device-reg: remove workaround for the bug this branch addresses * tests/main/firstboot-snaps: check we don't have this bug

--- overlord/snapstate/flags.go
+++ overlord/snapstate/flags.go
@@ -32,6 +32,9 @@ type Flags struct {
 	// Revert flags the SnapSetup as coming from a revert
 	Revert bool `json:"revert,omitempty"`
 
+	// KeepSnapPath is used via InstallPath to flag that the file passed in is not temporary and should not be removed
+	KeepSnapPath bool `json:"keep-snap-path,omitempty"`
+
 	// IgnoreValidation is set when the user requested as one-off
 	// to ignore refresh control validation.
 	IgnoreValidation bool `json:"ignore-validation,omitempty"`
