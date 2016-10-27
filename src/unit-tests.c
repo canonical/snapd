ae285c1bbae39af77ab744145c9f702902dc8acd
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-10-27 09:12:49 +0200

    Remove the dummy test case
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- src/unit-tests.c
+++ src/unit-tests.c
@@ -21,16 +21,9 @@
 #include "unit-tests.h"
 #include <glib.h>
 
-static void simple_test_case(void)
-{
-	g_assert(g_bit_storage(1) == 1);
-	g_assert_cmpint(g_bit_storage(1), ==, 1);
-}
-
 int sc_run_unit_tests(int *argc, char ***argv)
 {
 	g_test_init(argc, argv, NULL);
 	g_test_set_nonfatal_assertions();
-	g_test_add_func("/Simple Test Case", simple_test_case);
 	return g_test_run();
 }
