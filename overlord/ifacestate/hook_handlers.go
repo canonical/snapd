f1ad89b4fe6db6d1f4c9a8e760f87fb4bafb1901
Pawel Stolowski <stolowski@gmail.com>
2016-10-27 20:27:39 +0200

    Fixed yamls

--- overlord/ifacestate/hook_handlers.go
+++ overlord/ifacestate/hook_handlers.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/snapcore/snapd/overlord/hookstate"
 	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/logger"
 )
 
 type collectAttrHandler struct {
@@ -86,6 +87,7 @@ func copyAttributesToConnectTask(context *hookstate.Context) error {
 }
 
 func (h *collectAttrHandler) Before() error {
+	logger.Debugf("collect attr handler !!!")
 	h.context.Lock()
 	defer h.context.Unlock()
 	return copyAttributesFromConnectTask(h.context)
