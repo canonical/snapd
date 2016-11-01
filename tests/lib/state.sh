50d2477f51af6855b5d1b152fa3e3324d0f88b4e
Federico Gimenez <fgimenez@canonical.com>
2016-11-01 13:13:23 +0100

    stop and start daemon for actually changing state

--- tests/lib/state.sh
+++ tests/lib/state.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 remove_users_from_state(){
+    systemctl stop snapd.service
     cp /var/lib/snapd/state.json ./state.json.back
     sed -i 's/"users":\[[^]]*\]/"users":null/' /var/lib/snapd/state.json
+    systemctl start snapd.service
 }
 
 restore_state(){
     if [ -f ./state.json.back ]; then
+        systemctl stop snapd.service
         mv ./state.json.back /var/lib/snapd/state.json
+        systemctl start snapd.service
     fi
 }
