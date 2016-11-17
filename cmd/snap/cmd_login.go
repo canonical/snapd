6af047edb3de801363149a3f4d1ad3bb3582b10a
Michael Vogt <mvo@ubuntu.com>
2016-11-17 16:57:27 +0100

    use Store Password (thanks to Gustavo)

--- cmd/snap/cmd_login.go
+++ cmd/snap/cmd_login.go
@@ -92,7 +92,7 @@ func requestLoginWith2faRetry(email, password string) error {
 }
 
 func requestLogin(email string) error {
-	fmt.Fprint(Stdout, i18n.G("SSO Password: "))
+	fmt.Fprint(Stdout, i18n.G("Store password: "))
 	password, err := terminal.ReadPassword(Terminal)
 	fmt.Fprint(Stdout, "\n")
 	if err != nil {
