60f1116900c46398311e34b620e48b9e4733d5f7
Federico Gimenez <fgimenez@canonical.com>
2016-11-18 11:50:56 +0100

    Move files to be accessible to the snap

--- tests/lib/state.sh
+++ tests/lib/state.sh
@@ -3,11 +3,12 @@ save_state(){
     cp /var/lib/snapd/state.json $SPREAD_PATH/state.json.save
 }
 remove_users_from_state(){
-    snap install jq-cprov.jq
+    snap install jq-cprov
     systemctl stop snapd.service
-    mv /var/lib/snapd/state.json /var/lib/snapd/state.json~
-    jq-cprov.jq 'del(.data.auth.users)|.' /var/lib/snapd/state.json~ > /var/lib/snapd/state.json
-    rm /var/lib/snapd/state.json~
+    cp /var/lib/snapd/state.json /var/snap/jq-cprov/state.json~
+    jq-cprov.jq 'del(.data.auth.users)|.' /var/snap/jq-cprov/state.json~ > /var/snap/jq-cprov/state.json
+    cp /var/snap/jq-cprov/state.json /var/lib/snapd/state.json
+    rm /var/snap/jq-cprov/state.json~
     systemctl start snapd.service
     snap remove jq-cprov
 }
