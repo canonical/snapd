c7c1db8e4598f77e6a5c1d7e6b7b01b5688bc8de
Federico Gimenez <fgimenez@canonical.com>
2016-11-18 09:46:56 +0100

    save state as a separate action

--- tests/lib/state.sh
+++ tests/lib/state.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+save_state(){
+    cp /var/lib/snapd/state.json $SPREAD_PATH/state.json.save
+}
 remove_users_from_state(){
     snap install jq-cprov.jq
     systemctl stop snapd.service
-    cp /var/lib/snapd/state.json ./state.json.back
     mv /var/lib/snapd/state.json /var/lib/snapd/state.json~
     jq-cprov.jq 'del(.data.auth.users)|.' /var/lib/snapd/state.json~ > /var/lib/snapd/state.json
     rm /var/lib/snapd/state.json~
@@ -13,7 +15,7 @@ remove_users_from_state(){
 restore_state(){
     if [ -f ./state.json.back ]; then
         systemctl stop snapd.service
-        mv ./state.json.back /var/lib/snapd/state.json
+        mv $SPREAD_PATH/state.json.save /var/lib/snapd/state.json
         systemctl start snapd.service
     fi
 }
