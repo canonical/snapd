efdb5ae7779e13c490bf9132550e5f8df44339a3
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-11-18 16:27:41 +0100

    Fix up tests

--- tests/lib/reset.sh
+++ tests/lib/reset.sh
@@ -6,7 +6,6 @@ reset_classic() {
     systemctl stop snapd.service snapd.socket
 
     # purge all state
-    sh -x ${SPREAD_PATH}/debian/snapd.prerm
     sh -x ${SPREAD_PATH}/debian/snapd.postrm purge
     # extra purge
     rm -rvf /var/snap
@@ -29,7 +28,7 @@ reset_classic() {
             systemctl start $unit
         done
     fi
-    systemctl start snap.mount snapd.socket
+    systemctl start snapd.socket
 
     # wait for snapd listening
     while ! printf "GET / HTTP/1.0\r\n\r\n" | nc -U -q 1 /run/snapd.socket; do sleep 0.5; done
