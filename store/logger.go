26c216982a31adc61cf79b8af3bb8d8b8bd2c415
Michael Vogt <mvo@ubuntu.com>
2016-11-21 12:10:39 +0100

    add basic certificate pinning

--- store/logger.go
+++ store/logger.go
@@ -20,6 +20,12 @@
 package store
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"crypto/tls"
+	"encoding/hex"
+	"fmt"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -85,12 +91,51 @@ func (tr *LoggedTransport) getFlags() debugflag {
 	return debugflag(flags)
 }
 
+var validCertFps [2][]byte
+
+func init() {
+	var err error
+
+	// obtained via:
+	// python3 -c 'import ssl,socket,hashlib,binascii; conn=ssl.create_default_context().wrap_socket(socket.socket(socket.AF_INET), server_hostname="search.apps.ubuntu.com"); conn.connect(("search.apps.ubuntu.com", 443));sha256=hashlib.sha256();sha256.update(conn.getpeercert(binary_form=True));print(binascii.hexlify(sha256.digest()))'
+	// python3 -c 'import ssl,socket,hashlib,binascii; conn=ssl.create_default_context().wrap_socket(socket.socket(socket.AF_INET), server_hostname="assertions.ubuntu.com"); conn.connect(("assertions.ubuntu.com", 443));sha256=hashlib.sha256();sha256.update(conn.getpeercert(binary_form=True));print(binascii.hexlify(sha256.digest()))'
+	for i, h := range []string{
+		"12b4681522999af0f46a1965cdacbbfa376370644a0606f441a33fb26952d3ef",
+		"be1294e0682fe95ea1b692bb002e642763d6cba61f48d8437f4ed6f87a6c0e7d",
+	} {
+		validCertFps[i], err = hex.DecodeString(h)
+		if err != nil {
+			panic("internal error cannot decode validCertFp")
+		}
+	}
+}
+
+func pinnedDialTLS(network, addr string) (net.Conn, error) {
+	c, err := tls.Dial(network, addr, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, peercert := range c.ConnectionState().PeerCertificates {
+		fp := sha256.Sum256(peercert.Raw)
+		for _, validCertFp := range validCertFps {
+			if bytes.Equal(fp[0:], validCertFp) {
+				return c, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("cannot find expected security cert for %v %v: valid %v", network, addr, validCertFps)
+}
+
 // returns a new http.Client with a LoggedTransport and a Timeout
 func newHTTPClient() *http.Client {
 	return &http.Client{
 		Transport: &LoggedTransport{
-			Transport: http.DefaultTransport,
-			Key:       "SNAPD_DEBUG_HTTP",
+			Transport: &http.Transport{
+				DialTLS: pinnedDialTLS,
+			},
+			Key: "SNAPD_DEBUG_HTTP",
 		},
 		Timeout: 10 * time.Second,
 	}
