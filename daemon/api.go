f1b883389c0786241675ffb3c08d81b1dfcbdbf1
Pawel Stolowski <stolowski@gmail.com>
2016-11-28 15:58:37 +0100

    Set RemoveSnapPath flag to false in try mode.

--- daemon/api.go
+++ daemon/api.go
@@ -1237,14 +1237,16 @@ func postSnaps(c *Command, r *http.Request, user *auth.UserState) Response {
 	if err != nil {
 		return BadRequest(err.Error())
 	}
-	flags.RemoveSnapPath = true
 
 	if len(form.Value["action"]) > 0 && form.Value["action"][0] == "try" {
+		flags.RemoveSnapPath = false
 		if len(form.Value["snap-path"]) == 0 {
 			return BadRequest("need 'snap-path' value in form")
 		}
 		return trySnap(c, r, user, form.Value["snap-path"][0], flags)
 	}
+
+	flags.RemoveSnapPath = true
 
 	// find the file for the "snap" form field
 	var snapBody multipart.File
