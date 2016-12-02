f92701a172e2bc110c1be27677b3d7d43d00a9ef
Samuele Pedroni <pedronis@lucediurna.net>
2016-12-02 15:28:57 +0100

    support for parsing and exposing on snap.Info aliases

--- snap/info_snap_yaml.go
+++ snap/info_snap_yaml.go
@@ -50,6 +50,8 @@ type snapYaml struct {
 }
 
 type appYaml struct {
+	Aliases []string `yaml:"aliases,omitempty"`
+
 	Command string `yaml:"command"`
 
 	Daemon string `yaml:"daemon"`
@@ -107,8 +109,10 @@ func InfoFromSnapYaml(yamlData []byte) (*Info, error) {
 		globalSlotNames = append(globalSlotNames, slotName)
 	}
 
-	// Collect all apps and hooks
-	setAppsFromSnapYaml(y, snap)
+	// Collect all apps, their aliases and hooks
+	if err := setAppsAndAliasesFromSnapYaml(y, snap); err != nil {
+		return nil, err
+	}
 	setHooksFromSnapYaml(y, snap)
 
 	// Bind unbound plugs to all apps and hooks
@@ -156,6 +160,7 @@ func infoSkeletonFromSnapYaml(y snapYaml) *Info {
 		Epoch:               epoch,
 		Confinement:         confinement,
 		Apps:                make(map[string]*AppInfo),
+		Aliases:             make(map[string]*AppInfo),
 		Hooks:               make(map[string]*HookInfo),
 		Plugs:               make(map[string]*PlugInfo),
 		Slots:               make(map[string]*SlotInfo),
@@ -218,12 +223,13 @@ func setSlotsFromSnapYaml(y snapYaml, snap *Info) error {
 	return nil
 }
 
-func setAppsFromSnapYaml(y snapYaml, snap *Info) {
+func setAppsAndAliasesFromSnapYaml(y snapYaml, snap *Info) error {
 	for appName, yApp := range y.Apps {
 		// Collect all apps
 		app := &AppInfo{
 			Snap:            snap,
 			Name:            appName,
+			Aliases:         yApp.Aliases,
 			Command:         yApp.Command,
 			Daemon:          yApp.Daemon,
 			StopTimeout:     yApp.StopTimeout,
@@ -243,6 +249,12 @@ func setAppsFromSnapYaml(y snapYaml, snap *Info) {
 			app.Slots = make(map[string]*SlotInfo)
 		}
 		snap.Apps[appName] = app
+		for _, alias := range app.Aliases {
+			if snap.Aliases[alias] != nil {
+				return fmt.Errorf("cannot set %q as alias for both %q and %q", alias, snap.Aliases[alias].Name, appName)
+			}
+			snap.Aliases[alias] = app
+		}
 		// Bind all plugs/slots listed in this app
 		for _, plugName := range yApp.PlugNames {
 			plug, ok := snap.Plugs[plugName]
@@ -274,6 +286,7 @@ func setAppsFromSnapYaml(y snapYaml, snap *Info) {
 			slot.Apps[appName] = app
 		}
 	}
+	return nil
 }
 
 func setHooksFromSnapYaml(y snapYaml, snap *Info) {
