40fef6150aff6f1dc76797772a4c7068e23bb6d0
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2016-12-06 11:27:58 -0300

    Renamed Unity8Pim internal interface to Unity8PimCommon interface.
    Split apparmor policy into ConnectedSlotAppArmor.

--- interfaces/builtin/unity8_pim_common.go
+++ interfaces/builtin/unity8_pim_common.go
@@ -24,9 +24,10 @@ import (
 	"fmt"
 
 	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/release"
 )
 
-var unity8PimPermanentSlotAppArmor = []byte(`
+var unity8PimCommonPermanentSlotAppArmor = []byte(`
 # Description: Allow operating as the EDS service. Reserved because this
 #  gives privileged access to the system.
 # Usage: reserved
@@ -38,7 +39,7 @@ dbus (send)
 	path=/org/freedesktop/DBus
 	interface=org.freedesktop.DBus
 	member={Request,Release}Name
-	peer=(name=org.freedesktop.DBus),
+	peer=(name=org.freedesktop.DBus, label=unconfined),
 
 dbus (send)
 	bus=session
@@ -55,23 +56,31 @@ dbus (bind)
 	bus=session
 	name="org.gnome.evolution.dataserver.Sources5",
 
+`)
+
+var unity8PimCommonConnectedSlotAppArmor = []byte(`
 ########################
 # SourceManager
 ########################
 dbus (receive, send)
 	bus=session
 	path=/org/gnome/evolution/dataserver/SourceManager{,/**},
+	peer=(label=###PLUG_SECURITY_TAGS###),
+
 dbus (receive, send)
 	bus=session
 	path=/org/gnome/evolution/dataserver/SourceManager{,/**}
 	interface=org.gnome.evolution.dataserver.Source{,.*},
+	peer=(label=###PLUG_SECURITY_TAGS###),
+
 dbus (receive, send)
 	bus=session
 	path=/org/gnome/evolution/dataserver/SourceManager{,/**}
 	interface=org.freedesktop.DBus.*,
+	peer=(label=###PLUG_SECURITY_TAGS###),
 `)
 
-var unity8PimConnectedPlugAppArmor = []byte(`
+var unity8PimCommonConnectedPlugAppArmor = []byte(`
 # DBus accesses
 #include <abstractions/dbus-session-strict>
 
@@ -85,21 +94,24 @@ dbus (send)
      path=/org/freedesktop/DBus
      interface=org.freedesktop.DBus
      member={Request,Release}Name
-     peer=(name=org.freedesktop.DBus),
+     peer=(label=###SLOT_SECURITY_TAGS###),
+
 dbus (send)
      bus=session
      path=/org/freedesktop/*
      interface=org.freedesktop.DBus.Properties
-     peer=(label=unconfined),
+	 peer=(label=###SLOT_SECURITY_TAGS###),
 
+########################
 # SourceManager
+########################
 dbus (receive, send)
      bus=session
      path=/org/gnome/evolution/dataserver/SourceManager{,/**}
-     peer=(label=unconfined),
+     peer=(label=###SLOT_SECURITY_TAGS###),
 `)
 
-var unity8PimPermanentSlotSecComp = []byte(`
+var unity8PimCommonPermanentSlotSecComp = []byte(`
 # Description: Allow operating as the EDS service. Reserved because this
 # gives
 #  privileged access to the system.
@@ -126,7 +138,7 @@ socketpair
 socket
 `)
 
-var unity8PimConnectedPlugSecComp = []byte(`
+var unity8PimCommonConnectedPlugSecComp = []byte(`
 # Description: Allow using EDS service. Reserved because this gives
 #  privileged access to the eds service.
 # Usage: reserved
@@ -142,7 +154,7 @@ sendmsg
 socket
 `)
 
-var unity8PimPermanentSlotDBus = []byte(`
+var unity8PimCommonPermanentSlotDBus = []byte(`
 <policy user="root">
 	<allow own="org.gnome.evolution.dataserver.Sources5"/>
 	<allow send_destination="org.gnome.evolution.dataserver.Sources5"/>
@@ -159,49 +171,59 @@ var unity8PimPermanentSlotDBus = []byte(`
 </policy>
 `)
 
-type unity8PimInterface struct {
+type unity8PimCommonInterface struct {
 	name                  string
 	permanentSlotAppArmor string
 	connectedPlugAppArmor string
+	connectedSlotAppArmor string
 	permanentSlotDBus     string
 }
 
-func (iface *unity8PimInterface) Name() string {
+func (iface *unity8PimCommonInterface) Name() string {
 	return iface.name
 }
 
-func (iface *unity8PimInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+func (iface *unity8PimCommonInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	return nil, nil
 }
 
-func (iface *unity8PimInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+func (iface *unity8PimCommonInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
 	case interfaces.SecurityAppArmor:
 		old := []byte("###SLOT_SECURITY_TAGS###")
 		new := slotAppLabelExpr(slot)
-		snippet := []byte(unity8PimConnectedPlugAppArmor)
-		snippet = append(snippet, iface.connectedPlugAppArmor...)
-		snippet = bytes.Replace(snippet, old, new, -1)
+
+		originalSnippet := []byte(unity8PimCommonConnectedPlugAppArmor)
+		originalSnippet = append(originalSnippet, iface.connectedPlugAppArmor...)
+
+		snippet := bytes.Replace(originalSnippet, old, new, -1)
+
+		if release.OnClassic {
+			// Let confined apps access unconfined service on classic
+			classicSnippet := bytes.Replace(originalSnippet, old, []byte("unconfined"), -1)
+			snippet = append(snippet, classicSnippet...)
+		}
+
 		return snippet, nil
 	case interfaces.SecuritySecComp:
-		return unity8PimConnectedPlugSecComp, nil
+		return unity8PimCommonConnectedPlugSecComp, nil
 	default:
 		return nil, nil
 	}
 }
 
-func (iface *unity8PimInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+func (iface *unity8PimCommonInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
 	case interfaces.SecurityAppArmor:
-		snippet := []byte(unity8PimConnectedPlugAppArmor)
+		snippet := []byte(unity8PimCommonConnectedPlugAppArmor)
 		snippet = append(snippet, iface.permanentSlotAppArmor...)
 		return snippet, nil
 	case interfaces.SecuritySecComp:
-		return unity8PimPermanentSlotSecComp, nil
+		return unity8PimCommonPermanentSlotSecComp, nil
 	case interfaces.SecurityDBus:
 		old := []byte("###SLOT_DBUS_SERVICE_TAGS###")
 		new := []byte(iface.permanentSlotDBus)
-		snippet := []byte(unity8PimPermanentSlotDBus)
+		snippet := []byte(unity8PimCommonPermanentSlotDBus)
 		snippet = bytes.Replace(snippet, old, new, -1)
 		return snippet, nil
 	default:
@@ -209,11 +231,20 @@ func (iface *unity8PimInterface) PermanentSlotSnippet(slot *interfaces.Slot, sec
 	}
 }
 
-func (iface *unity8PimInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+func (iface *unity8PimCommonInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		old := []byte("###PLUG_SECURITY_TAGS###")
+		new := plugAppLabelExpr(plug)
+		snippet := []byte(unity8PimCommonConnectedSlotAppArmor)
+		snippet = append(snippet, iface.connectedSlotAppArmor...)
+		snippet = bytes.Replace(snippet, old, new, -1)
+		return snippet, nil
+	}
 	return nil, nil
 }
 
-func (iface *unity8PimInterface) SanitizePlug(plug *interfaces.Plug) error {
+func (iface *unity8PimCommonInterface) SanitizePlug(plug *interfaces.Plug) error {
 	if iface.Name() != plug.Interface {
 		panic(fmt.Sprintf("plug is not of interface \"%s\"", iface.Name()))
 	}
@@ -221,18 +252,18 @@ func (iface *unity8PimInterface) SanitizePlug(plug *interfaces.Plug) error {
 	return nil
 }
 
-func (iface *unity8PimInterface) SanitizeSlot(slot *interfaces.Slot) error {
+func (iface *unity8PimCommonInterface) SanitizeSlot(slot *interfaces.Slot) error {
 	if iface.Name() != slot.Interface {
 		panic(fmt.Sprintf("slot is not of interface \"%s\"", iface.Name()))
 	}
 	return nil
 }
 
-func (iface *unity8PimInterface) LegacyAutoConnect() bool {
+func (iface *unity8PimCommonInterface) LegacyAutoConnect() bool {
 	return false
 }
 
-func (iface *unity8PimInterface) AutoConnect(*interfaces.Plug, *interfaces.Slot) bool {
+func (iface *unity8PimCommonInterface) AutoConnect(*interfaces.Plug, *interfaces.Slot) bool {
 	// allow what declarations allowed
 	return true
 }
