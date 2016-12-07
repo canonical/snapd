309cb90c86e54a8b6dbe3d00f71f309a982a380a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-12-07 18:42:41 +0100

    debian: add split ubuntu-core-launcher and snap-confine packages
    This patch changes debian packaging for snapd to generate two additional
    binary packages, snap-confine and ubuntu-core-launcher. They are now
    generated separately so that apt logic can give everyone a better
    upgrade experience as snap-confine and ubuntu-core-launcher packages
    will no longer have to be removed during the upgrade operation.
    
    To ensure that everything is in sync there's now a strong dependency on
    the exact source package version among snapd and snap-confine and also
    among snap-confine and ubunut-core-launcher. This should ensure that
    those packages are always updated in lockstep, to the extent possible in
    dpkg.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -40,7 +40,7 @@ update_core_snap_with_snap_exec_snapctl() {
 }
 
 prepare_classic() {
-    apt_install_local ${SPREAD_PATH}/../snapd_*.deb
+    apt_install_local ${SPREAD_PATH}/../snapd_*.deb ${SPREAD_PATH}/../snap-confine*.deb ${SPREAD_PATH}/../ubuntu-core-launcher_*.deb
 
     # Snapshot the state including core.
     if [ ! -f $SPREAD_PATH/snapd-state.tar.gz ]; then
@@ -81,7 +81,7 @@ prepare_classic() {
 setup_reflash_magic() {
         # install the stuff we need
         apt-get install -y kpartx busybox-static
-        apt_install_local ${SPREAD_PATH}/../snapd_*.deb
+        apt_install_local ${SPREAD_PATH}/../snapd_*.deb ${SPREAD_PATH}/../snap-confine_*.deb ${SPREAD_PATH}/../ubuntu-core-launcher_*.deb
         apt-get clean
 
         snap install --${CORE_CHANNEL} core
