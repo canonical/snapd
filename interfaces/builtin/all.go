8cda53ac40cb752ad24359e42c4d2c0eea435d5d
Christian <christian@twotoasts.de>
2016-12-08 14:44:11 +0100

    Implement 'shadow' interface for mounting snap folders

--- interfaces/builtin/all.go
+++ interfaces/builtin/all.go
@@ -47,6 +47,7 @@ var allInterfaces = []interfaces.Interface{
 	&PppInterface{},
 	&PulseAudioInterface{},
 	&SerialPortInterface{},
+	&ShadowInterface{},
 	&UDisks2Interface{},
 	NewAlsaInterface(),
 	NewAvahiObserveInterface(),
