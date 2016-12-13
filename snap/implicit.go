0d47b6d741ab88f453b98fc7d2f8a433e22e0433
Thomas Voß <thomas.voss.bochum@gmail.com>
2016-12-13 23:55:55 +0100

    Address reviewer comments.

--- snap/implicit.go
+++ snap/implicit.go
@@ -101,6 +101,9 @@ func AddImplicitSlots(snapInfo *Info) {
 			snapInfo.Slots[ifaceName] = makeImplicitSlot(snapInfo, ifaceName)
 		}
 	}
+	// fuse-support is disabled on trusty due to usage of fuse requiring access to mount.
+	// we do not want to widen the apparmor profile defined in fuse-support to support trusty
+	// right now.
 	if !(release.ReleaseInfo.ID == "ubuntu" && release.ReleaseInfo.VersionID == "14.04") {
 		snapInfo.Slots["fuse-support"] = makeImplicitSlot(snapInfo, "fuse-support")
 	}
