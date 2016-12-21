7661ec6b6174f0e93afb5a98bc9deb4e02f960d5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-12-21 14:18:07 +0100

    tests: add install_local_devmode
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/snaps.sh
+++ tests/lib/snaps.sh
@@ -8,3 +8,12 @@ install_local() {
     fi
     snap install --dangerous "$SNAP_FILE"
 }
+
+install_local_devmode() {
+    local SNAP_FILE="$TESTSLIB/snaps/$1/$1_1.0_all.snap"
+    local SNAP_DIR=$(dirname "$SNAP_FILE")
+    if [ ! -f "$SNAP_FILE" ]; then
+        snapbuild "$SNAP_DIR" "$SNAP_DIR"
+    fi
+    snap install --devmode --dangerous "$SNAP_FILE"
+}
