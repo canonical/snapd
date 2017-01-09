8a701d35ab9907a95db75053717c32e8e8ae8882
Samuele Pedroni <pedronis@lucediurna.net>
2017-01-09 14:47:59 +0100

    some explanation about undefined in the help

--- cmd/snap/cmd_aliases.go
+++ cmd/snap/cmd_aliases.go
@@ -43,6 +43,10 @@ The aliases command lists all aliases available in the system and their status.
 $ snap aliases <snap>
 
 Lists only the aliases defined by the specified snap.
+
+An alias noted as undefined means it was explicitly enabled or disabled but is
+not defined in the current revision of the snap; possibly temporarely (e.g
+because of a revert), if not this can be cleared with snap alias --reset.
 `)
 
 func init() {
