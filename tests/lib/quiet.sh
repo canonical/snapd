21ab3f0dc04951bf45a2151a37631c316bd3f48a
John R. Lenton <jlenton@gmail.com>
2017-01-16 16:59:38 +0000

    address review feedback

--- tests/lib/quiet.sh
+++ tests/lib/quiet.sh
@@ -1,18 +1,30 @@
+#!/bin/bash
+
 # use "quiet foo" when you expect "foo" to produce a lot of output
 # that isn't useful unless foo itself fails.
 quiet() (
     # note this is a subshell (parens instead of braces around the function)
-    # so this set only affects this function itself
+    # so this set only affects this function and not the caller
     { set +x; } >&/dev/null
 
+    # not strictly needed because it's a subshell, but good practice
+    local tf retval
+
     tf="$(tempfile)"
-    if "$@" >& "$tf"; then
-        rm -f -- "$tf"
-        return 0
-    else
-        echo "Q: $*" >&2
+
+    set +e
+    "$@" >& "$tf"
+    retval=$?
+    set -e
+
+    if [ "$retval" != "0" ]; then
+        echo "quiet: $*" >&2
+        echo "quiet: exit status $retval. Output follows:" >&2
         cat "$tf" >&2
-        rm -f -- "$tf"
-        return 1
+        echo "quiet: end of output." >&2
     fi
+
+    rm -f -- "$tf"
+
+    return $retval
 )
