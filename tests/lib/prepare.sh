75257179b432a95c19992f5d5dfe32f310a774ab
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-01-16 13:40:33 +0100

    tests: fix typo
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -49,7 +49,7 @@ update_core_snap_for_classic_reexec() {
     mount "$snap" "$core"
 
     # Make sure we're running with the correct copied bits
-    for p in /usr/lib/snapd/snap-exec /usr/lib/snapd/snap-confine /usr/lib/snapd/snap-disacrd-ns /usr/bin/snapctl /usr/lib/snapd/snapd /usr/bin/snap; do
+    for p in /usr/lib/snapd/snap-exec /usr/lib/snapd/snap-confine /usr/lib/snapd/snap-discard-ns /usr/bin/snapctl /usr/lib/snapd/snapd /usr/bin/snap; do
         if ! cmp ${p} ${core}${p}; then
             echo "$p in tree and $p in core snap are unexpectedly not the same"
             exit 1
