2194aa5f724e6ee7de653020c636d181d754d9aa
John R. Lenton <jlenton@gmail.com>
2017-01-17 17:00:30 +0000

    small tweaks / cleanups to prepare-project.sh

--- tests/lib/prepare-project.sh
+++ tests/lib/prepare-project.sh
@@ -3,13 +3,16 @@
 # Set REUSE_PROJECT to reuse the previous prepare when also reusing the server.
 [ "$REUSE_PROJECT" != 1 ] || exit 0
 echo "Running with SNAP_REEXEC: $SNAP_REEXEC"
+
 # check that we are not updating
 . "$TESTSLIB/boot.sh"
-. "$TESTSLIB/quiet.sh"
 if [ "$(bootenv snap_mode)" = "try" ]; then
    echo "Ongoing reboot upgrade process, please try again when finished"
    exit 1
 fi
+
+# declare the "quiet" wrapper
+. "$TESTSLIB/quiet.sh"
 
 if [ "$SPREAD_BACKEND" = external ]; then
    # build test binaries
@@ -32,12 +35,6 @@ if [ "$SPREAD_BACKEND" = qemu ]; then
    fi
 fi
 
-quiet apt-get update
-
-# XXX: build-essential seems to be required. Otherwise package build
-# fails with unmet dependency on "build-essential:native"
-quiet apt-get install -y build-essential software-properties-common
-
 if [[ "$SPREAD_SYSTEM" == ubuntu-14.04-* ]]; then
     if [ ! -d debian-ubuntu-14.04 ]; then
         echo "no debian-ubuntu-14.04/ directory "
@@ -49,19 +46,22 @@ if [[ "$SPREAD_SYSTEM" == ubuntu-14.04-* ]]; then
     rm -rf debian
     mv debian-ubuntu-14.04 debian
 
+    quiet apt-get update
+    quiet apt-get install -y software-properties-common
+
     echo 'deb http://archive.ubuntu.com/ubuntu/ trusty-proposed main universe' >> /etc/apt/sources.list
     quiet add-apt-repository ppa:snappy-dev/image
     quiet apt-get update
-fi
 
-if [[ "$SPREAD_SYSTEM" == ubuntu-14.04-* ]]; then
     quiet apt-get install -y --install-recommends linux-generic-lts-xenial
     quiet apt-get install -y --force-yes apparmor libapparmor1 seccomp libseccomp2 systemd cgroup-lite util-linux
 fi
 
 quiet apt-get purge -y snapd snap-confine ubuntu-core-launcher
 # utilities
-quiet apt-get install -y curl devscripts expect gdebi-core jq rng-tools git
+# XXX: build-essential seems to be required. Otherwise package build
+# fails with unmet dependency on "build-essential:native"
+quiet apt-get install -y build-essential curl devscripts expect gdebi-core jq rng-tools git
 
 # in 16.04: apt build-dep -y ./
 quiet apt-get install -y $(gdebi --quiet --apt-line ./debian/control)
