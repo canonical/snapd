8dbde9bedab97ee1985ec86282e6547cf6b611d0
Matteo Croce <matteo.croce@canonical.com>
2017-01-23 14:25:49 +0100

    interfaces: add accounts-control interface
    accounts-control interface allows to handle the user accounts.

--- interfaces/builtin/all.go
+++ interfaces/builtin/all.go
@@ -56,6 +56,7 @@ var allInterfaces = []interfaces.Interface{
 	&UbuntuDownloadManagerInterface{},
 	&UpowerObserveInterface{},
 	&UhidInterface{},
+	NewAccountsControlInterface(),
 	NewAlsaInterface(),
 	NewAvahiObserveInterface(),
 	NewBluetoothControlInterface(),
