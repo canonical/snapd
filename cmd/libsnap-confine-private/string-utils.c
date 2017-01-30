3673186e2073714ace599acd70330dc06654e550
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-01-30 17:01:57 +0100

    cmd: add sc_must_stpcpy
    This patch adds a security-paranoid version of stpcpy that is more
    sensitive to buffer overflows and other misuse of common string
    utilities.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/libsnap-confine-private/string-utils.c
+++ cmd/libsnap-confine-private/string-utils.c
@@ -17,6 +17,7 @@
 
 #include "string-utils.h"
 
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -68,4 +69,46 @@ int sc_must_snprintf(char *str, size_t size, const char *format, ...)
 		die("cannot format string: %s", str);
 
 	return n;
+}
+
+char *sc_must_stpcpy(char *buf, size_t buf_size, char *dest, const char *src)
+{
+	// Set errno in case we die.
+	errno = 0;
+	if (buf == NULL) {
+		die("cannot append string: buffer is NULL");
+	}
+	if (dest == NULL) {
+		die("cannot append string: destination is NULL");
+	}
+	if (src == NULL) {
+		die("cannot append string: source is NULL");
+	}
+	// Sanity check, the code doesn't need buffers larger than a few KBs so
+	// prevent corrupted or otherwise huge buffers from seeming "valid".
+	if (buf_size >= 0xFFFF) {
+		die("cannot append string: buffer size (%zu) exceeds internal limit", buf_size);
+
+	}
+	size_t src_len = strlen(src);
+	// Sanity check, dest points to the inside of the buffer.
+	if (dest == &buf[buf_size]) {
+		die("cannot append string: destination points"
+		    " to the end of the buffer");
+	}
+	if (dest > &buf[buf_size] && src_len > 0) {
+		die("cannot append string: destination points"
+		    " %td byte(s) beyond the buffer", dest - &buf[buf_size]);
+	}
+	if (dest < buf) {
+		die("cannot append string: destination points"
+		    " %td byte(s) in front of the buffer", buf - dest);
+	}
+	// Sanity check the new content fits the buffer.
+	if (&dest[src_len] >= &buf[buf_size]) {
+		die("cannot append string: buffer overflow of %td byte(s)",
+		    &dest[src_len] - &buf[buf_size] + 1);
+	}
+	memcpy(dest, src, src_len + 1);
+	return &dest[src_len + 1];
 }
--- cmd/libsnap-confine-private/string-utils.h
+++ cmd/libsnap-confine-private/string-utils.h
@@ -39,4 +39,28 @@ bool sc_endswith(const char *str, const char *suffix);
 __attribute__ ((format(printf, 3, 4)))
 int sc_must_snprintf(char *str, size_t size, const char *format, ...);
 
+/**
+ * Safer version of stpcpy.
+ *
+ * This version is fully aware of the output buffer and is extra careful not to
+ * overflow it. If any argument is NULL, buffer size is unrealistic (see below)
+ * or a buffer overflow is detected then the function dies.
+ *
+ * Since snap-confine has very modest requirements buffers larger than 0xFFFF
+ * are not allowed. This is meant as an extra sanity check to prevent
+ * '-1'-sized buffers from allowing memory corruption to go on unnoticed.
+ *
+ * As a tip, the function should be used like this:
+ *
+ *   char buf[100];
+ *   char *to = buf;
+ *
+ *   to = sc_must_stpcpy(buf, sizeof buf, to, "hello");
+ *   to = sc_must_stpcpy(buf, sizeof buf, to, " ");
+ *   sc_must_stpcpy(buf, sizeof buf, to, "world");
+ *
+ * The return value can be discarded when no more appending is necessary.
+ **/
+char *sc_must_stpcpy(char *buf, size_t buf_size, char *dest, const char *src);
+
 #endif
