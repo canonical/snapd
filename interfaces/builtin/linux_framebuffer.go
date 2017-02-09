e10ea56054a5238cc9b65c4c4a8274aac9bca6d3
Renat Galimov <renat2017@gmail.com>
2017-02-09 11:42:04 +0300

    Added a linux framebuffer interface

---
+++ interfaces/builtin/linux_framebuffer.go
@@ -0,0 +1,41 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+import "github.com/snapcore/snapd/interfaces"
+
+const linuxFramebufferConnectedPlugAppArmor = `
+# Description: for those who need to talk to the system TPM chip over /dev/tpm0
+# Usage: reserved
+
+/dev/fb0 rw,
+/dev/graphics/0 rw,
+/dev/tty0 rw, 
+/dev/tty rw,
+/dev/console rw,
+`
+
+func NewLinuxFramebufferInterface() interfaces.Interface {
+	return &commonInterface{
+		name: "linux-framebuffer",
+		connectedPlugAppArmor: linuxFramebufferConnectedPlugAppArmor,
+		reservedForOS:         false,
+	}
+}
