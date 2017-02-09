21bc6b9ffc327a726c790b21ecc4dc4fbc427c14
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-02-09 12:44:04 +0100

    interfaces: use seccomp profile syntax readable by stable snap-confine
    This patch reverts useful security hardening patches done by jdstrand in
    order to unbreak people tracking the edge channel on core. Because
    snap-confine is not re-executed yet we don't have a way to introduce new
    seccomp syntax in a compatible way.
    
    For the time being (before we allow snap-confine to run from core snap
    directly) we need this to generate profiles that actually work.
    
    This patch is designed to be reverted easily. It does not undo changes
    to snap-confine that were made along the way as they are not the
    problem.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/seccomp/template.go
+++ interfaces/seccomp/template.go
@@ -163,7 +163,10 @@ inotify_rm_watch
 
 # TIOCSTI allows for faking input (man tty_ioctl)
 # TODO: this should be scaled back even more
-ioctl - !TIOCSTI
+#ioctl - !TIOCSTI
+# FIXME: replace this with the filter of TIOCSTI once snap-confine can read this syntax
+# See LP:#1662489 for context.
+ioctl
 
 io_cancel
 io_destroy
