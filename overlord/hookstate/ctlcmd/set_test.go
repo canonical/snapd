bf68b560f25cf20c09df83eeeefc9ce61fa3c881
Pawel Stolowski <stolowski@gmail.com>
2017-02-10 13:04:36 +0100

    Merged set interface attributes tests into set_test.

--- overlord/hookstate/ctlcmd/set_test.go
+++ overlord/hookstate/ctlcmd/set_test.go
@@ -20,6 +20,7 @@
 package ctlcmd_test
 
 import (
+	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/overlord/configstate/config"
 	"github.com/snapcore/snapd/overlord/hookstate"
 	"github.com/snapcore/snapd/overlord/hookstate/ctlcmd"
@@ -35,7 +36,13 @@ type setSuite struct {
 	mockHandler *hooktest.MockHandler
 }
 
+type setAttrSuite struct {
+	mockContext *hookstate.Context
+	mockHandler *hooktest.MockHandler
+}
+
 var _ = Suite(&setSuite{})
+var _ = Suite(&setAttrSuite{})
 
 func (s *setSuite) SetUpTest(c *C) {
 	s.mockHandler = hooktest.NewMockHandler()
@@ -119,4 +126,73 @@ func (s *setSuite) TestCommandSavesDeltasOnly(c *C) {
 func (s *setSuite) TestCommandWithoutContext(c *C) {
 	_, _, err := ctlcmd.Run(nil, []string{"set", "foo=bar"})
 	c.Check(err, ErrorMatches, ".*cannot set without a context.*")
+}
+
+func (s *setAttrSuite) SetUpTest(c *C) {
+	s.mockHandler = hooktest.NewMockHandler()
+
+	state := state.New(nil)
+	state.Lock()
+
+	ch := state.NewChange("mychange", "mychange")
+	attrsTask := state.NewTask("connect-task", "my connect task")
+	attrsTask.Set("plug", &interfaces.PlugRef{Snap: "a", Name: "aplug"})
+	attrsTask.Set("slot", &interfaces.SlotRef{Snap: "b", Name: "bslot"})
+	attrs := make(map[string]interface{})
+	attrs["foo"] = "bar"
+	attrs["baz"] = []string{"a", "b"}
+	attrsTask.Set("plug-attrs", attrs)
+	attrsTask.Set("slot-attrs", make(map[string]interface{}))
+
+	hookTask := state.NewTask("run-hook", "my test task")
+	setup := &hookstate.HookSetup{Snap: "test-snap", Revision: snap.R(1), Hook: "prepare-plug-aplug"}
+	ch.AddTask(attrsTask)
+	state.Unlock()
+
+	var err error
+	s.mockContext, err = hookstate.NewContext(hookTask, setup, s.mockHandler)
+	c.Assert(err, IsNil)
+
+	s.mockContext.Lock()
+	s.mockContext.Set("attrs-task", attrsTask.ID())
+	defer s.mockContext.Unlock()
+	ch.AddTask(hookTask)
+}
+
+func (s *setAttrSuite) TestCommand(c *C) {
+	stdout, stderr, err := ctlcmd.Run(s.mockContext, []string{"set", ":aplug", "foo=bar", `baz=["a", "b"]`})
+	c.Check(err, IsNil)
+	c.Check(string(stdout), Equals, "")
+	c.Check(string(stderr), Equals, "")
+
+	attrsTask, err := ctlcmd.AttributesTask(s.mockContext)
+	c.Assert(err, IsNil)
+	st := s.mockContext.State()
+	st.Lock()
+	defer st.Unlock()
+	attrs := make(map[string]interface{})
+	err = attrsTask.Get("plug-attrs", &attrs)
+	c.Assert(err, IsNil)
+	c.Check(attrs["foo"], Equals, "bar")
+	c.Check(attrs["baz"], DeepEquals, []interface{}{"a", "b"})
+}
+
+func (s *setAttrSuite) TestCommandFailsOutsideOfValidContext(c *C) {
+	var err error
+	var mockContext *hookstate.Context
+
+	state := state.New(nil)
+	state.Lock()
+	defer state.Unlock()
+
+	task := state.NewTask("test-task", "my test task")
+	setup := &hookstate.HookSetup{Snap: "test-snap", Revision: snap.R(1), Hook: "not-a-connect-hook"}
+	mockContext, err = hookstate.NewContext(task, setup, s.mockHandler)
+	c.Assert(err, IsNil)
+
+	stdout, stderr, err := ctlcmd.Run(mockContext, []string{"set", ":aplug", "foo=bar"})
+	c.Check(err, NotNil)
+	c.Check(err.Error(), Equals, `interface attributes can only be set during the execution of prepare hooks`)
+	c.Check(string(stdout), Equals, "")
+	c.Check(string(stderr), Equals, "")
 }
