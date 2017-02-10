4ddf21ca2bfed4cc157c08262a74bc1e2d1da60e
Michael Vogt <mvo@ubuntu.com>
2017-02-10 08:54:10 +0100

    add support for seconds in TimeOfDay

--- timeutil/schedule_test.go
+++ timeutil/schedule_test.go
@@ -67,16 +67,18 @@ func (ts *timeutilSuite) TestParseSchedule(c *C) {
 
 func (ts *timeutilSuite) TestParseTime(c *C) {
 	for _, t := range []struct {
-		timeStr      string
-		hour, minute int
-		errStr       string
+		timeStr              string
+		hour, minute, second int
+		errStr               string
 	}{
-		{"8:59", 8, 59, ""},
-		{"08:59", 8, 59, ""},
-		{"12:00", 12, 0, ""},
-		{"xx", 0, 0, `cannot parse "xx"`},
-		{"11:61", 0, 0, `cannot parse "11:61"`},
-		{"25:00", 0, 0, `cannot parse "25:00"`},
+		{"8:59", 8, 59, 0, ""},
+		{"8:59:12", 8, 59, 12, ""},
+		{"08:59", 8, 59, 0, ""},
+		{"12:00", 12, 0, 0, ""},
+		{"xx", 0, 0, 0, `cannot parse "xx"`},
+		{"11:61", 0, 0, 0, `cannot parse "11:61"`},
+		{"25:00", 0, 0, 0, `cannot parse "25:00"`},
+		{"11:59:61", 0, 0, 0, `cannot parse "11:59:61"`},
 	} {
 		ti, err := timeutil.ParseTime(t.timeStr)
 		if t.errStr != "" {
@@ -85,6 +87,7 @@ func (ts *timeutilSuite) TestParseTime(c *C) {
 			c.Check(err, IsNil)
 			c.Check(ti.Hour, Equals, t.hour)
 			c.Check(ti.Minute, Equals, t.minute)
+			c.Check(ti.Second, Equals, t.second)
 		}
 	}
 }
