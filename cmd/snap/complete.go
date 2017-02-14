4ed3e6a6c0520a29eb0b7e818ad76ded5f3d3a84
Zygmunt Krynicki <me@zygoon.pl>
2017-02-14 12:53:11 +0100

    cmd/snap: add shell completion to connect (#2588)

--- cmd/snap/complete.go
+++ cmd/snap/complete.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/jessevdk/go-flags"
@@ -97,4 +98,73 @@ func (s keyName) Complete(match string) []flags.Completion {
 		return nil
 	})
 	return res
+}
+
+type connectPlugSpec struct {
+	SnapAndName
+}
+
+func (cps connectPlugSpec) Complete(match string) []flags.Completion {
+	// Parse what the user typed so far, it can be either
+	// nothing (""), a "snap", a "snap:" or a "snap:name".
+	parts := strings.SplitN(match, ":", 2)
+
+	// Ask snapd about available interfaces.
+	cli := Client()
+	ifaces, err := cli.Interfaces()
+	if err != nil {
+		return nil
+	}
+
+	snaps := make(map[string]bool)
+
+	var ret []flags.Completion
+
+	var snapPrefix, plugPrefix string
+	if len(parts) == 2 {
+		// The user typed the colon, means they know the snap they want;
+		// go with that.
+		plugPrefix = parts[1]
+		snaps[parts[0]] = true
+	} else {
+		// The user is about to or has started typing a snap name but didn't
+		// reach the colon yet. Offer plugs for snaps with names that start
+		// like that.
+		snapPrefix = parts[0]
+		for _, plug := range ifaces.Plugs {
+			if strings.HasPrefix(plug.Snap, snapPrefix) {
+				snaps[plug.Snap] = true
+			}
+		}
+	}
+
+	if len(snaps) == 1 {
+		for snapName := range snaps {
+			for _, plug := range ifaces.Plugs {
+				if plug.Snap == snapName && strings.HasPrefix(plug.Name, plugPrefix) {
+					// TODO: in the future annotate plugs that can take
+					// multiple connection sensibly and don't skip those even
+					// if they have connections already.
+					if len(plug.Connections) == 0 {
+						ret = append(ret, flags.Completion{Item: fmt.Sprintf("%s:%s", plug.Snap, plug.Name)})
+					}
+				}
+			}
+		}
+	} else {
+		for snapName := range snaps {
+			for _, plug := range ifaces.Plugs {
+				if plug.Snap == snapName {
+					if len(plug.Connections) == 0 {
+						// TODO: in the future annotate plugs that can take
+						// multiple connection sensibly and don't skip those
+						// even if they have connections already.
+						ret = append(ret, flags.Completion{Item: fmt.Sprintf("%s:", snapName)})
+					}
+				}
+			}
+		}
+	}
+
+	return ret
 }
