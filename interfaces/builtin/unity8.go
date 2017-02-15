5f7e7a3a93136cee2a4874b4fe99d9f492585da6
Michael Terry <mike@mterry.name>
2017-02-15 12:34:31 -0500

    Restrict content hub access to pasteboard, drop a11y bits, add glib schema read access

--- interfaces/builtin/unity8.go
+++ interfaces/builtin/unity8.go
@@ -24,79 +24,82 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"unicode"
 
 	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/dbus"
 	"github.com/snapcore/snapd/snap"
 )
 
 var unity8ConnectedPlugAppArmor = []byte(`
 # Description: Can access unity8 desktop services
 
-  #include <abstractions/dbus-session-strict>
+#include <abstractions/dbus-session-strict>
 
-  # Fonts
-  #include <abstractions/fonts>
-  /var/cache/fontconfig/   r,
-  /var/cache/fontconfig/** mr,
+# Fonts
+#include <abstractions/fonts>
+/var/cache/fontconfig/   r,
+/var/cache/fontconfig/** mr,
 
-  # Accessibility
-  #include <abstractions/dbus-accessibility-strict>
-  dbus (send)
-       bus=session
-       path=/org/a11y/bus
-       interface=org.a11y.Bus
-       member=GetAddress
-       peer=(name=org.a11y.Bus,label=unconfined),
-  dbus (receive, send)
-       bus=accessibility
-       path=/org/a11y/atspi/**
-       peer=(label=unconfined),
+# The snapcraft desktop part may look for schema files in various locations, so
+# allow reading system installed schemas.
+/usr/share/glib*/schemas/{,*}              r,
+/usr/share/gnome/glib*/schemas/{,*}        r,
+/usr/share/ubuntu/glib*/schemas/{,*}       r,
 
-  # URL dispatcher. All apps can call this since:
-  # a) the dispatched application is launched out of process and not
-  #    controllable except via the specified URL
-  # b) the list of url types is strictly controlled
-  # c) the dispatched application will launch in the foreground over the
-  #    confined app
-  dbus (send)
-       bus=session
-       path=/com/canonical/URLDispatcher
-       interface=com.canonical.URLDispatcher
-       member=DispatchURL
-       peer=(name=com.canonical.URLDispatcher,label=###SLOT_SECURITY_TAGS###),
+# URL dispatcher. All apps can call this since:
+# a) the dispatched application is launched out of process and not
+#    controllable except via the specified URL
+# b) the list of url types is strictly controlled
+# c) the dispatched application will launch in the foreground over the
+#    confined app
+dbus (send)
+     bus=session
+     path=/com/canonical/URLDispatcher
+     interface=com.canonical.URLDispatcher
+     member=DispatchURL
+     peer=(name=com.canonical.URLDispatcher,label=###SLOT_SECURITY_TAGS###),
 
-  # This is needed when the app is already running and needs to be passed in
-  # a URL to open. This is most often used with content-hub providers and
-  # url-dispatcher, but is actually supported by Qt generally.
-  dbus (receive)
-       bus=session
-       path=/###PLUG_DBUS_APPIDS###
-       interface=org.freedesktop.Application
-       member=Open
-       peer=(label=###SLOT_SECURITY_TAGS###),
+# This is needed when the app is already running and needs to be passed in
+# a URL to open. This is most often used with content-hub providers and
+# url-dispatcher, but is actually supported by Qt generally.
+dbus (receive)
+     bus=session
+     path=/###PLUG_DBUS_APPIDS###
+     interface=org.freedesktop.Application
+     member=Open
+     peer=(label=###SLOT_SECURITY_TAGS###),
 
-  # Unity launcher (e.g. app counter, progress, alert)
-  dbus (receive, send)
-       bus=session
-       path=/com/canonical/Unity/Launcher/###PLUG_DBUS_APPIDS###
-       peer=(name=com.canonical.Unity.Launcher,label=###SLOT_SECURITY_TAGS###),
+# Unity launcher (e.g. app counter, progress, alert)
+dbus (receive, send)
+     bus=session
+     path=/com/canonical/Unity/Launcher/###PLUG_DBUS_APPIDS###
+     peer=(name=com.canonical.Unity.Launcher,label=###SLOT_SECURITY_TAGS###),
 
-  # Content Hub (pasteboard, file transfers)
-  dbus (receive, send)
-       bus=session
-       interface=com.ubuntu.content.dbus.Service
-       path=/
-       peer=(name=com.ubuntu.content.dbus.Service,label=###SLOT_SECURITY_TAGS###),
+# Note: content-hub may become its own interface, but for now include it here
+# Pasteboard via Content Hub
+dbus (send)
+     bus=session
+     interface=com.ubuntu.content.dbus.Service
+     path=/
+     member={CreatePaste,GetAllPasteIds,GetLatestPasteData,GetPasteData,GetPasteSource,PasteFormats,RequestPasteByAppId,SelectPasteForAppId,SelectPasteForAppIdCancelled}
+     peer=(name=com.ubuntu.content.dbus.Service,label=###SLOT_SECURITY_TAGS###),
+dbus (receive)
+     bus=session
+     interface=com.ubuntu.content.dbus.Service
+     path=/
+     member={PasteboardChanged,PasteFormatsChanged,PasteSelected,PasteSelectionCancelled}
+     peer=(name=com.ubuntu.content.dbus.Service,label=###SLOT_SECURITY_TAGS###),
 
-  # Lttng tracing is very noisy and should not be allowed by confined apps.
-  # Can safely deny. LP: #1260491
-  deny /{,var/}{dev,run}/shm/lttng-ust-* r,
+# Lttng tracing is very noisy and should not be allowed by confined apps.
+# Can safely deny. LP: #1260491
+deny /{dev,run,var/run}/shm/lttng-ust-* r,
 `)
 
 var unity8ConnectedPlugSecComp = []byte(`
+recv
 recvfrom
 recvmsg
+send
 sendmsg
 sendto
 shutdown
@@ -112,21 +115,9 @@ func (iface *Unity8Interface) String() string {
 	return iface.Name()
 }
 
-func (iface *Unity8Interface) dbusAppId(app *snap.AppInfo) []byte {
-	var retval bytes.Buffer
-
-	appidbits := []string{app.Snap.Name(), app.Name, app.Snap.Revision.String()}
-	appid := strings.Join(appidbits, "_")
-
-	for _, value := range appid {
-		if unicode.In(rune(value), unicode.Letter, unicode.Digit) {
-			retval.WriteRune(rune(value))
-		} else {
-			retval.WriteString(fmt.Sprintf("_%2.2x", value))
-		}
-	}
-
-	return retval.Bytes()
+func (iface *Unity8Interface) dbusAppId(app *snap.AppInfo) string {
+	pieces := []string{app.Snap.Name(), app.Name, app.Snap.Revision.String()}
+	return dbus.SafePath(strings.Join(pieces, "_"))
 }
 
 func (iface *Unity8Interface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
@@ -144,7 +135,7 @@ func (iface *Unity8Interface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *
 		var appidsNew bytes.Buffer
 		var appidsList []string
 		for _, app := range plug.Apps {
-			appidsList = append(appidsList, string(iface.dbusAppId(app)))
+			appidsList = append(appidsList, iface.dbusAppId(app))
 		}
 		sort.Strings(appidsList) // makes tests reliable
 		if len(appidsList) == 1 {
