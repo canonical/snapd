0e8a0a8505011511f9abca14581ffbd59e40774e
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-02-20 11:39:56 +0100

    daemon: don't test the kernel version
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- daemon/api_test.go
+++ daemon/api_test.go
@@ -565,6 +565,9 @@ func (s *apiSuite) TestSysInfo(c *check.C) {
 	c.Assert(json.Unmarshal(rec.Body.Bytes(), &rsp), check.IsNil)
 	c.Check(rsp.Status, check.Equals, 200)
 	c.Check(rsp.Type, check.Equals, ResponseTypeSync)
+	// Ensure that we had a kernel-verrsion but don't check the actual value.
+	c.Check(rsp.Result.(map[string]interface{})["kerenl-version"], check.Not(check.Equals), "")
+	delete(rsp.Result.(map[string]interface{}), "kernel-version")
 	c.Check(rsp.Result, check.DeepEquals, expected)
 }
 
