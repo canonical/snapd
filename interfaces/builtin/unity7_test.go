99c4bb44e3b6dcf162bd9baae95cbb126c700f6f
Pawel Stolowski <stolowski@gmail.com>
2017-02-22 17:58:40 +0100

    Tests of the remaining interfaces using commonInterface and seccomp.

--- interfaces/builtin/unity7_test.go
+++ interfaces/builtin/unity7_test.go
@@ -24,7 +24,9 @@ import (
 
 	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/interfaces/seccomp"
 	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
 )
 
 type Unity7InterfaceSuite struct {
@@ -47,6 +49,12 @@ var _ = Suite(&Unity7InterfaceSuite{
 			Snap:      &snap.Info{SuggestedName: "other"},
 			Name:      "unity7",
 			Interface: "unity7",
+			Apps: map[string]*snap.AppInfo{
+				"app2": {
+					Snap: &snap.Info{
+						SuggestedName: "other",
+					},
+					Name: "app2"}},
 		},
 	},
 })
@@ -84,7 +92,9 @@ func (s *Unity7InterfaceSuite) TestUsedSecuritySystems(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(snippet, Not(IsNil))
 	// connected plugs have a non-nil security snippet for seccomp
-	snippet, err = s.iface.ConnectedPlugSnippet(s.plug, s.slot, interfaces.SecuritySecComp)
+	seccompSpec := &seccomp.Specification{}
+	err = seccompSpec.AddConnectedPlug(s.iface, s.plug, s.slot)
 	c.Assert(err, IsNil)
-	c.Assert(snippet, Not(IsNil))
+	c.Assert(len(seccompSpec.Snippets["snap.other.app2"]), Equals, 1)
+	c.Check(string(seccompSpec.Snippets["snap.other.app2"][0]), testutil.Contains, "shutdown\n")
 }
