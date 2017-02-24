853b63fcf2b93e4332f0816b1da522cbd76a8aa2
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-02-24 23:34:37 +0100

    cmd/libsnap: add function for validating snap names
    This patch adds a function that can be used to validate snap names. Due
    to the differences between go and libc regular expression engine the
    same expression cannot be used but care has been taken to make sure it
    has the same meaning.
    
    The only difference is that the syntax "(?:)" becomes just "()"
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/libsnap-confine-private/snap-test.c
+++ cmd/libsnap-confine-private/snap-test.c
@@ -61,7 +61,50 @@ static void test_verify_security_tag()
 	g_assert_false(verify_security_tag("snap.name.app.."));
 }
 
+static void test_sc_snap_name_validate()
+{
+	struct sc_error *err = NULL;
+
+	// Smoke test, a valid snap name
+	sc_snap_name_validate("hello-world", &err);
+	g_assert_null(err);
+
+	// Smoke test: invalid snap name (spaces are not allowed)
+	sc_snap_name_validate("hello world", &err);
+	g_assert_nonnull(err);
+	g_assert_true(sc_error_match
+		      (err, SC_SNAP_DOMAIN, SC_SNAP_INVALID_NAME));
+	g_assert_cmpstr(sc_error_msg(err), ==,
+			"invalid snap name \"hello world\"");
+	sc_error_free(err);
+
+	// Smoke test: empty name is not valid
+	sc_snap_name_validate("", &err);
+	g_assert_nonnull(err);
+	g_assert_true(sc_error_match
+		      (err, SC_SNAP_DOMAIN, SC_SNAP_INVALID_NAME));
+	g_assert_cmpstr(sc_error_msg(err), ==, "invalid snap name \"\"");
+	sc_error_free(err);
+}
+
+static void test_sc_snap_name_validate__respects_error_protocol()
+{
+	if (g_test_subprocess()) {
+		sc_snap_name_validate("hello world", NULL);
+		g_test_message("expected sc_snap_name_validate to return");
+		g_test_fail();
+		return;
+	}
+	g_test_trap_subprocess(NULL, 0, 0);
+	g_test_trap_assert_failed();
+	g_test_trap_assert_stderr("invalid snap name \"hello world\"\n");
+}
+
 static void __attribute__ ((constructor)) init()
 {
 	g_test_add_func("/snap/verify_security_tag", test_verify_security_tag);
+	g_test_add_func("/snap/sc_snap_name_validate",
+			test_sc_snap_name_validate);
+	g_test_add_func("/snap/sc_snap_name_validate/respects_error_protocol",
+			test_sc_snap_name_validate__respects_error_protocol);
 }
--- cmd/libsnap-confine-private/snap.c
+++ cmd/libsnap-confine-private/snap.c
@@ -17,11 +17,17 @@
 #include "config.h"
 #include "snap.h"
 
+#include <regex.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <regex.h>
+#include <string.h>
+#include <errno.h>
 
-#include "../libsnap-confine-private/utils.h"
+#include "utils.h"
+#include "string-utils.h"
+#include "cleanup-funcs.h"
+
+static regex_t sc_valid_snap_name_re;
 
 bool verify_security_tag(const char *security_tag)
 {
@@ -42,4 +48,48 @@ bool verify_security_tag(const char *security_tag)
 	regfree(&re);
 
 	return (status == 0);
+}
+
+void sc_snap_name_validate(const char *snap_name, struct sc_error **errorp)
+{
+	struct sc_error *err = NULL;
+
+	if (regexec(&sc_valid_snap_name_re, snap_name, 0, NULL, 0) != 0) {
+		char *quote_buf __attribute__ ((cleanup(sc_cleanup_string))) =
+		    NULL;
+		size_t quote_buf_size = strlen(snap_name) * 4 + 3;
+
+		quote_buf = calloc(1, quote_buf_size);
+		if (quote_buf == NULL) {
+			err =
+			    sc_error_init_from_errno(errno,
+						     "cannot allocate memory for quoted snap name");
+			goto out;
+		}
+
+		sc_string_quote(quote_buf, quote_buf_size, snap_name);
+		err =
+		    sc_error_init(SC_SNAP_DOMAIN, SC_SNAP_INVALID_NAME,
+				  "invalid snap name %s", quote_buf);
+	}
+
+ out:
+	sc_error_forward(errorp, err);
+}
+
+static void __attribute__ ((constructor)) init_snap()
+{
+	// NOTE: this regular expression should be kept in sync with what is in
+	// snap/validate.go, in the validSnapName variable.
+	const char *name_re = "^([a-z0-9]+-?)*[a-z](-?[a-z0-9])*$";
+
+	if (regcomp(&sc_valid_snap_name_re, name_re, REG_EXTENDED | REG_NOSUB)
+	    != 0) {
+		die("cannot compile regex %s", name_re);
+	}
+}
+
+static void __attribute__ ((destructor)) fini_snap()
+{
+	regfree(&sc_valid_snap_name_re);
 }
