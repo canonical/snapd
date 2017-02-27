5029abdb426ea5ad6ae84d8280ba2433df7f2236
Jamie Strandboge <jamie@ubuntu.com>
2017-02-27 21:10:05 +0000

    interfaces/udisks2: use 'const' instead of 'var' for security policy

--- interfaces/builtin/udisks2.go
+++ interfaces/builtin/udisks2.go
@@ -1,7 +1,7 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 
 /*
- * Copyright (C) 2016 Canonical Ltd
+ * Copyright (C) 2016-2017 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -89,7 +89,7 @@ umount /{,run/}media/**,
 /dev/mmcblk* r,
 `
 
-var udisks2ConnectedSlotAppArmor = []byte(`
+const udisks2ConnectedSlotAppArmor = `
 # Allow connected clients to interact with the service. This gives privileged
 # access to the system.
 
@@ -112,9 +112,9 @@ dbus (receive, send)
     path=/org/freedesktop/UDisks2/**
     interface=org.freedesktop.UDisks2.*
     peer=(label=###PLUG_SECURITY_TAGS###),
-`)
+`
 
-var udisks2ConnectedPlugAppArmor = []byte(`
+const udisks2ConnectedPlugAppArmor = `
 # Description: Allow using udisks service. This gives privileged access to the
 # service.
 
@@ -139,7 +139,7 @@ dbus (receive, send)
     path=/org/freedesktop/UDisks2/**
     interface=org.freedesktop.UDisks2.*
     peer=(label=###SLOT_SECURITY_TAGS###),
-`)
+`
 
 const udisks2PermanentSlotSecComp = `
 bind
@@ -341,7 +341,7 @@ func (iface *UDisks2Interface) ConnectedPlugSnippet(plug *interfaces.Plug, slot 
 	case interfaces.SecurityAppArmor:
 		old := []byte("###SLOT_SECURITY_TAGS###")
 		new := slotAppLabelExpr(slot)
-		snippet := bytes.Replace(udisks2ConnectedPlugAppArmor, old, new, -1)
+		snippet := bytes.Replace([]byte(udisks2ConnectedPlugAppArmor), old, new, -1)
 		return snippet, nil
 	case interfaces.SecurityDBus:
 		return []byte(udisks2ConnectedPlugDBus), nil
@@ -368,7 +368,7 @@ func (iface *UDisks2Interface) ConnectedSlotSnippet(plug *interfaces.Plug, slot 
 	case interfaces.SecurityAppArmor:
 		old := []byte("###PLUG_SECURITY_TAGS###")
 		new := plugAppLabelExpr(plug)
-		snippet := bytes.Replace(udisks2ConnectedSlotAppArmor, old, new, -1)
+		snippet := bytes.Replace([]byte(udisks2ConnectedSlotAppArmor), old, new, -1)
 		return snippet, nil
 	}
 	return nil, nil
