4101104d1cd36805ab4af6c98dd05aed828d0266
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-02 09:25:40 +0100

    cmd/snap-update-ns: use loop instead of goto
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-update-ns/mount-entry-change.c
+++ cmd/snap-update-ns/mount-entry-change.c
@@ -38,48 +38,53 @@ sc_compute_required_mount_changes(struct sc_mount_entry * *desiredp,
 	}
 	d = *desiredp;
 	c = *currentp;
- again:
-	if (c == NULL && d == NULL) {
-		// Both profiles exhausted. There is nothing to do left.
-		change->action = SC_ACTION_NONE;
-		change->entry = NULL;
-		*currentp = NULL;
-		*desiredp = NULL;
-	} else if (c == NULL && d != NULL) {
-		// Current profile exhausted but desired profile is not.
-		// Generate a MOUNT action based on desired profile and advance it.
-		change->action = SC_ACTION_MOUNT;
-		change->entry = d;
-		*currentp = NULL;
-		*desiredp = d->next;
-	} else if (c != NULL && d == NULL) {
-		// Current profile is not exhausted but the desired profile is.
-		// Generate an UNMOUNT action based on the current profile and advance it.
-		change->action = SC_ACTION_UNMOUNT;
-		change->entry = c;
-		*currentp = c->next;
-		*desiredp = NULL;
-	} else if (c != NULL && d != NULL) {
-		// Both profiles have entries to consider.
-		if (sc_compare_mount_entry(c, d) == 0) {
-			// Identical entries are just skipped and the algorithm continues.
-			c = c->next;
-			d = d->next;
-			goto again;
-		} else {
-			// Non-identail entries mean that we need to unmount the current
-			// entry and mount the desired entry.
-			//
-			// Let's process all the unmounts first. This way we can "clear the
-			// stage" (so to speak). Either the tip of the current profile and
-			// tip of the desired profile become identical (we're in sync) or
-			// we're eventually going to exhaust the current profile and the
-			// code above will start to process items in the desired profile
-			// (which will cause all the mount calls to happen).
+	bool again;
+	do {
+		again = false;
+		if (c == NULL && d == NULL) {
+			// Both profiles exhausted. There is nothing to do left.
+			change->action = SC_ACTION_NONE;
+			change->entry = NULL;
+			*currentp = NULL;
+			*desiredp = NULL;
+		} else if (c == NULL && d != NULL) {
+			// Current profile exhausted but desired profile is not.
+			// Generate a MOUNT action based on desired profile and advance it.
+			change->action = SC_ACTION_MOUNT;
+			change->entry = d;
+			*currentp = NULL;
+			*desiredp = d->next;
+		} else if (c != NULL && d == NULL) {
+			// Current profile is not exhausted but the desired profile is.
+			// Generate an UNMOUNT action based on the current profile and advance it.
 			change->action = SC_ACTION_UNMOUNT;
 			change->entry = c;
 			*currentp = c->next;
-			*desiredp = d;
+			*desiredp = NULL;
+		} else if (c != NULL && d != NULL) {
+			// Both profiles have entries to consider.
+			if (sc_compare_mount_entry(c, d) == 0) {
+				// Identical entries are just skipped and the algorithm continues.
+				c = c->next;
+				d = d->next;
+				// Do another pass over the algorithm.
+				again = true;
+				continue;
+			} else {
+				// Non-identail entries mean that we need to unmount the current
+				// entry and mount the desired entry.
+				//
+				// Let's process all the unmounts first. This way we can "clear the
+				// stage" (so to speak). Either the tip of the current profile and
+				// tip of the desired profile become identical (we're in sync) or
+				// we're eventually going to exhaust the current profile and the
+				// code above will start to process items in the desired profile
+				// (which will cause all the mount calls to happen).
+				change->action = SC_ACTION_UNMOUNT;
+				change->entry = c;
+				*currentp = c->next;
+				*desiredp = d;
+			}
 		}
-	}
+	} while (again);
 }
