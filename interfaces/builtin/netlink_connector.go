99892905969895aded1a240ebb994ef35ae152d9
Jamie Strandboge <jamie@ubuntu.com>
2017-03-02 21:19:14 +0000

    add netlink-connector interface

---
+++ interfaces/builtin/netlink_connector.go
@@ -0,0 +1,40 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+import (
+	"github.com/snapcore/snapd/interfaces"
+)
+
+const netlinkConnectorConnectedPlugSecComp = `
+# Description: Can use netlink to communicate with kernel connector
+# https://github.com/torvalds/linux/blob/master/Documentation/connector/connector.txt
+bind
+socket AF_NETLINK - NETLINK_CONNECTOR
+`
+
+// NewNetlinkConnectorInterface returns a new "netlink-connector" interface.
+func NewNetlinkConnectorInterface() interfaces.Interface {
+	return &commonInterface{
+		name:                 "netlink-connector",
+		connectedPlugSecComp: netlinkConnectorConnectedPlugSecComp,
+		reservedForOS:        true,
+	}
+}
