8d4f851f0926e7fdcbf12b59238af955837a11af
Samuele Pedroni <pedronis@lucediurna.net>
2017-03-07 20:34:04 +0100

    more -l

--- tests/lib/reset.sh
+++ tests/lib/reset.sh
@@ -25,7 +25,7 @@ reset_classic() {
 
     if [ "$1" = "--reuse-core" ]; then
         $(cd / && tar xzf $SPREAD_PATH/snapd-state.tar.gz)
-        mounts="$(systemctl list-unit-files | grep '^snap[-.].*\.mount' | cut -f1 -d ' ')"
+        mounts="$(systemctl list-unit-files -l | grep '^snap[-.].*\.mount' | cut -f1 -d ' ')"
         services="$(systemctl list-unit-files | grep '^snap[-.].*\.service' | cut -f1 -d ' ')"
         systemctl daemon-reload # Workaround for http://paste.ubuntu.com/17735820/
         for unit in $mounts $services; do
