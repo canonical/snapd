b092d430ff52baa34050b9160a126a432c2b8063
Michael Vogt <mvo@ubuntu.com>
2017-03-08 10:49:27 +0100

    Use "systemctl list-unit-files --full" everywhere
    The default of list-unit-files is to shorten long unit file names
    to something like "prefix...suffix". As we are using this to
    find the actual file names this is undesirable and we need to
    always use --full.

--- tests/lib/reset.sh
+++ tests/lib/reset.sh
@@ -26,7 +26,7 @@ reset_classic() {
     if [ "$1" = "--reuse-core" ]; then
         $(cd / && tar xzf $SPREAD_PATH/snapd-state.tar.gz)
         mounts="$(systemctl list-unit-files --full | grep '^snap[-.].*\.mount' | cut -f1 -d ' ')"
-        services="$(systemctl list-unit-files | grep '^snap[-.].*\.service' | cut -f1 -d ' ')"
+        services="$(systemctl list-unit-files --full | grep '^snap[-.].*\.service' | cut -f1 -d ' ')"
         systemctl daemon-reload # Workaround for http://paste.ubuntu.com/17735820/
         for unit in $mounts $services; do
             systemctl start $unit
