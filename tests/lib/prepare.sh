bded104079c1248480bdb5eb5b0fd33e69dcfa81
Michael Vogt <mvo@ubuntu.com>
2017-03-15 09:01:12 +0100

    ensure snap-confine-suid-trampoline is installed in the core snap

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -29,6 +29,7 @@ update_core_snap_for_classic_reexec() {
     # also inject new version of snap-confine and snap-scard-ns
     cp /usr/lib/snapd/snap-discard-ns squashfs-root/usr/lib/snapd/
     cp /usr/lib/snapd/snap-confine squashfs-root/usr/lib/snapd/
+    cp /usr/lib/snapd/snap-confine-suid-trampoline squashfs-root/usr/lib/snapd/
     # also add snap/snapd because we re-exec by default and want to test
     # this version
     cp /usr/lib/snapd/snapd squashfs-root/usr/lib/snapd/
