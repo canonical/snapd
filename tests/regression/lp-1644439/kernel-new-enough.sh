e3680fed5478a03b4f196cb07f16c41e49b8eaaf
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-17 17:00:41 +0100

    tests: fix / simplify kernel check (thanks chipaca)
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/regression/lp-1644439/kernel-new-enough.sh
+++ tests/regression/lp-1644439/kernel-new-enough.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 # Check if the kernel is at least 4.4.0-67
-if [ "$(uname -r | perl -ne 'print $1 if /^([0-9]+)\./')" -lt 4 ]; then
-	exit 1
-fi
-if [ "$(uname -r | perl -ne 'print $1 if /^[0-9]+\.([0-9]+)\./')" -lt 4 ]; then
-	exit 1
-fi
-if [ "$(uname -r | perl -ne 'print $1 if /^[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)/')" -lt 67 ]; then
+if ! uname -r | perl -ne '/^(\d+)\.(\d+)\.(\d+)-(\d+)/ or exit 1; exit 1 if $1<4; exit 1 if $2<4; exit 1 if $3==0 && $4<67'; then
 	exit 1
 fi
