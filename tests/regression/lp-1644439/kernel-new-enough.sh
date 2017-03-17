546870a9cf16eb52ff8c499a7d081f98b7092711
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-17 21:15:19 +0100

    tests: merge two files
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/regression/lp-1644439/kernel-new-enough.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Check if the kernel is at least 4.4.0-67
-if ! uname -r | perl -ne '/^(\d+)\.(\d+)\.(\d+)-(\d+)/ or exit 1; exit 1 if $1<4; exit 1 if $2<4; exit 1 if $3==0 && $4<67'; then
-	exit 1
-fi
