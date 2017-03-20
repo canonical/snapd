5ad76b44b37135740d34aecb4610eef37d4e4c6b
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-20 10:29:27 +0100

    interfaces/mount: use longer variable names
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/mount/change.go
+++ interfaces/mount/change.go
@@ -46,33 +46,33 @@ type Change struct {
 // lists are processed and a "diff" of mount changes is produced. The mount
 // changes, when applied in order, transform the current profile into the
 // desired profile.
-func NeededChanges(current, desired []Entry) []Change {
+func NeededChanges(currentProfile, desiredProfile []Entry) []Change {
 	var changes []Change
 	// Copy both as we will want to mutate them.
-	c := make([]Entry, len(current))
-	copy(c, current)
-	d := make([]Entry, len(desired))
-	copy(d, desired)
+	current := make([]Entry, len(currentProfile))
+	copy(current, currentProfile)
+	desired := make([]Entry, len(desiredProfile))
+	copy(desired, desiredProfile)
 
 	// Clean the directory part of both profiles. This is done so that we can
 	// easily test if a given directory is a subdirectory with
 	// strings.HasPrefix coupled with an extra slash character.
-	for i := range c {
-		c[i].Dir = path.Clean(c[i].Dir)
+	for i := range current {
+		current[i].Dir = path.Clean(current[i].Dir)
 	}
-	for i := range d {
-		d[i].Dir = path.Clean(d[i].Dir)
+	for i := range desired {
+		desired[i].Dir = path.Clean(desired[i].Dir)
 	}
 
 	// Sort both lists by directory name.
-	sort.Sort(byDir(c))
-	sort.Sort(byDir(d))
+	sort.Sort(byDir(current))
+	sort.Sort(byDir(desired))
 
 	// Construct a desired directory map.
 	// Maps from a directory to a pointer to an Entry from the desired list.
 	dm := make(map[string]*Entry)
-	for i := range d {
-		dm[d[i].Dir] = &d[i]
+	for i := range desired {
+		dm[desired[i].Dir] = &desired[i]
 	}
 
 	// Reuse map, indexed by Entry.Dir.
@@ -86,12 +86,12 @@ func NeededChanges(current, desired []Entry) []Change {
 	//
 	// Don't reuse any children if their parent changes.
 	var skipPrefix string
-	for i := range c {
-		dir := c[i].Dir
+	for i := range current {
+		dir := current[i].Dir
 		if skipPrefix != "" && strings.HasPrefix(dir, skipPrefix) && dir[len(skipPrefix)] == '/' {
 			continue
 		}
-		if entry, ok := dm[dir]; ok && EqualEntries(&c[i], entry) {
+		if entry, ok := dm[dir]; ok && EqualEntries(&current[i], entry) {
 			reuse[dir] = true
 			continue
 		}
@@ -101,16 +101,16 @@ func NeededChanges(current, desired []Entry) []Change {
 	// Unmount all the current entries (unless flagged for reuse).
 	// Because c is sorted by directory name we can iterate in reverse
 	// to ensure we unmount children before we try to unmount parents.
-	for i := len(c) - 1; i >= 0; i-- {
-		if !reuse[c[i].Dir] {
-			changes = append(changes, Change{Action: Unmount, Entry: c[i]})
+	for i := len(current) - 1; i >= 0; i-- {
+		if !reuse[current[i].Dir] {
+			changes = append(changes, Change{Action: Unmount, Entry: current[i]})
 		}
 	}
 
 	// Mount all the desired entries (unless flagged for reuse).
-	for i := range d {
-		if !reuse[d[i].Dir] {
-			changes = append(changes, Change{Action: Mount, Entry: d[i]})
+	for i := range desired {
+		if !reuse[desired[i].Dir] {
+			changes = append(changes, Change{Action: Mount, Entry: desired[i]})
 		}
 	}
 
