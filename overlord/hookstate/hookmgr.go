23e58eb8c311dc6c982da3cffea28e3957dee473
Michael Vogt <mvo@ubuntu.com>
2017-03-23 14:08:31 +0100

    include hook error output

--- overlord/hookstate/hookmgr.go
+++ overlord/hookstate/hookmgr.go
@@ -218,18 +218,19 @@ func (m *HookManager) doRunHook(task *state.Task, tomb *tomb.Tomb) error {
 
 	if hookExists {
 		output, err := runHook(context, tomb)
-		if hooksup.IgnoreFail && err != nil {
-			task.State().Lock()
-			task.Logf("ignoring failure in hook %q: %v", hooksup.Hook, err)
-			task.State().Unlock()
-		} else if err != nil {
+		if err != nil {
 			err = osutil.OutputErr(output, err)
+			if hooksup.IgnoreFail {
+				task.State().Lock()
+				task.Logf("ignoring failure in hook %q: %v", hooksup.Hook, err)
+				task.State().Unlock()
+			} else {
+				if handlerErr := context.Handler().Error(err); handlerErr != nil {
+					return handlerErr
+				}
 
-			if handlerErr := context.Handler().Error(err); handlerErr != nil {
-				return handlerErr
+				return fmt.Errorf("run hook %q: %v", hooksup.Hook, err)
 			}
-
-			return fmt.Errorf("run hook %q: %v", hooksup.Hook, err)
 		}
 	}
 
