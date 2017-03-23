80137bd72ebf7d40bffe319b983cc39172fcf3b2
Samuele Pedroni <pedronis@lucediurna.net>
2017-03-23 16:41:43 +0100

    introduce in all managers packages a *state.go file with the main public apis for manager related state querying, manipulation and creating tasks/task sets

---
+++ overlord/assertstate/assertstate.go
@@ -0,0 +1,359 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016-2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package assertstate
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/snapcore/snapd/asserts"
+	"github.com/snapcore/snapd/asserts/snapasserts"
+	"github.com/snapcore/snapd/overlord/snapstate"
+	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/release"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/store"
+)
+
+// Add the given assertion to the system assertion database.
+func Add(s *state.State, a asserts.Assertion) error {
+	// TODO: deal together with asserts itself with (cascading) side effects of possible assertion updates
+	return cachedDB(s).Add(a)
+}
+
+// Batch allows to accumulate a set of assertions possibly out of prerequisite order and then add them in one go to the system assertion database.
+type Batch struct {
+	bs   asserts.Backstore
+	refs []*asserts.Ref
+}
+
+// NewBatch creates a new Batch to accumulate assertions to add in one go to the system assertion database.
+func NewBatch() *Batch {
+	return &Batch{
+		bs:   asserts.NewMemoryBackstore(),
+		refs: nil,
+	}
+}
+
+// Add one assertion to the batch.
+func (b *Batch) Add(a asserts.Assertion) error {
+	if !a.SupportedFormat() {
+		return &asserts.UnsupportedFormatError{Ref: a.Ref(), Format: a.Format()}
+	}
+	if err := b.bs.Put(a.Type(), a); err != nil {
+		if revErr, ok := err.(*asserts.RevisionError); ok {
+			if revErr.Current >= a.Revision() {
+				// we already got something more recent
+				return nil
+			}
+		}
+		return err
+	}
+	b.refs = append(b.refs, a.Ref())
+	return nil
+}
+
+// AddStream adds a stream of assertions to the batch.
+// Returns references to to the assertions effectively added.
+func (b *Batch) AddStream(r io.Reader) ([]*asserts.Ref, error) {
+	start := len(b.refs)
+	dec := asserts.NewDecoder(r)
+	for {
+		a, err := dec.Decode()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		if err := b.Add(a); err != nil {
+			return nil, err
+		}
+	}
+	added := b.refs[start:]
+	if len(added) == 0 {
+		return nil, nil
+	}
+	refs := make([]*asserts.Ref, len(added))
+	copy(refs, added)
+	return refs, nil
+}
+
+// Commit adds the batch of assertions to the system assertion database.
+func (b *Batch) Commit(st *state.State) error {
+	db := cachedDB(st)
+	retrieve := func(ref *asserts.Ref) (asserts.Assertion, error) {
+		a, err := b.bs.Get(ref.Type, ref.PrimaryKey, ref.Type.MaxSupportedFormat())
+		if err == asserts.ErrNotFound {
+			// fallback to pre-existing assertions
+			a, err = ref.Resolve(db.Find)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("cannot find %s: %s", ref, err)
+		}
+		return a, nil
+	}
+
+	// linearize using fetcher
+	f := newFetcher(st, retrieve)
+	for _, ref := range b.refs {
+		if err := f.Fetch(ref); err != nil {
+			return err
+		}
+	}
+
+	// TODO: trigger w. caller a global sanity check if something is revoked
+	// (but try to save as much possible still),
+	// or err is a check error
+	return f.commit()
+}
+
+// RefreshSnapDeclarations refetches all the current snap declarations and their prerequisites.
+func RefreshSnapDeclarations(s *state.State, userID int) error {
+	snapStates, err := snapstate.All(s)
+	if err != nil {
+		return nil
+	}
+	fetching := func(f asserts.Fetcher) error {
+		for _, snapst := range snapStates {
+			info, err := snapst.CurrentInfo()
+			if err != nil {
+				return err
+			}
+			if info.SnapID == "" {
+				continue
+			}
+			if err := snapasserts.FetchSnapDeclaration(f, info.SnapID); err != nil {
+				return fmt.Errorf("cannot refresh snap-declaration for %q: %v", info.Name(), err)
+			}
+		}
+		return nil
+	}
+	return doFetch(s, userID, fetching)
+}
+
+type refreshControlError struct {
+	errs []error
+}
+
+func (e *refreshControlError) Error() string {
+	if len(e.errs) == 1 {
+		return e.errs[0].Error()
+	}
+	l := []string{""}
+	for _, e := range e.errs {
+		l = append(l, e.Error())
+	}
+	return fmt.Sprintf("refresh control errors:%s", strings.Join(l, "\n - "))
+}
+
+// ValidateRefreshes validates the refresh candidate revisions represented by the snapInfos, looking for the needed refresh control validation assertions, it returns a validated subset in validated and a summary error if not all candidates validated.
+func ValidateRefreshes(s *state.State, snapInfos []*snap.Info, userID int) (validated []*snap.Info, err error) {
+	// maps gated snap-ids to gating snap-ids
+	controlled := make(map[string][]string)
+	// maps gating snap-ids to their snap names
+	gatingNames := make(map[string]string)
+
+	db := DB(s)
+	snapStates, err := snapstate.All(s)
+	if err != nil {
+		return nil, err
+	}
+	for snapName, snapst := range snapStates {
+		info, err := snapst.CurrentInfo()
+		if err != nil {
+			return nil, err
+		}
+		if info.SnapID == "" {
+			continue
+		}
+		gatingID := info.SnapID
+		a, err := db.Find(asserts.SnapDeclarationType, map[string]string{
+			"series":  release.Series,
+			"snap-id": gatingID,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("internal error: cannot find snap declaration for installed snap %q (id %q): err", snapName, gatingID)
+		}
+		decl := a.(*asserts.SnapDeclaration)
+		control := decl.RefreshControl()
+		if len(control) == 0 {
+			continue
+		}
+		gatingNames[gatingID] = decl.SnapName()
+		for _, gatedID := range control {
+			controlled[gatedID] = append(controlled[gatedID], gatingID)
+		}
+	}
+
+	var errs []error
+	for _, candInfo := range snapInfos {
+		gatedID := candInfo.SnapID
+		gating := controlled[gatedID]
+		if len(gating) == 0 { // easy case, no refresh control
+			validated = append(validated, candInfo)
+			continue
+		}
+
+		var validationRefs []*asserts.Ref
+
+		fetching := func(f asserts.Fetcher) error {
+			for _, gatingID := range gating {
+				valref := &asserts.Ref{
+					Type:       asserts.ValidationType,
+					PrimaryKey: []string{release.Series, gatingID, gatedID, candInfo.Revision.String()},
+				}
+				err := f.Fetch(valref)
+				if notFound, ok := err.(*store.AssertionNotFoundError); ok && notFound.Ref.Type == asserts.ValidationType {
+					return fmt.Errorf("no validation by %q", gatingNames[gatingID])
+				}
+				if err != nil {
+					return fmt.Errorf("cannot find validation by %q: %v", gatingNames[gatingID], err)
+				}
+				validationRefs = append(validationRefs, valref)
+			}
+			return nil
+		}
+		err := doFetch(s, userID, fetching)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("cannot refresh %q to revision %s: %v", candInfo.Name(), candInfo.Revision, err))
+			continue
+		}
+
+		var revoked *asserts.Validation
+		for _, valref := range validationRefs {
+			a, err := valref.Resolve(db.Find)
+			if err != nil {
+				return nil, fmt.Errorf("internal error: cannot find just fetched %v: %v", valref, err)
+			}
+			if val := a.(*asserts.Validation); val.Revoked() {
+				revoked = val
+				break
+			}
+		}
+		if revoked != nil {
+			errs = append(errs, fmt.Errorf("cannot refresh %q to revision %s: validation by %q (id %q) revoked", candInfo.Name(), candInfo.Revision, gatingNames[revoked.SnapID()], revoked.SnapID()))
+			continue
+		}
+
+		validated = append(validated, candInfo)
+	}
+
+	if errs != nil {
+		return validated, &refreshControlError{errs}
+	}
+
+	return validated, nil
+}
+
+func init() {
+	// hook validation of refreshes into snapstate logic
+	snapstate.ValidateRefreshes = ValidateRefreshes
+	// hook auto refresh of assertions into snapstate
+	snapstate.AutoRefreshAssertions = AutoRefreshAssertions
+}
+
+// BaseDeclaration returns the base-declaration assertion with policies governing all snaps.
+func BaseDeclaration(s *state.State) (*asserts.BaseDeclaration, error) {
+	// TODO: switch keeping this in the DB and have it revisioned/updated
+	// via the store
+	baseDecl := asserts.BuiltinBaseDeclaration()
+	if baseDecl == nil {
+		return nil, asserts.ErrNotFound
+	}
+	return baseDecl, nil
+}
+
+// SnapDeclaration returns the snap-declaration for the given snap-id if it is present in the system assertion database.
+func SnapDeclaration(s *state.State, snapID string) (*asserts.SnapDeclaration, error) {
+	db := DB(s)
+	a, err := db.Find(asserts.SnapDeclarationType, map[string]string{
+		"series":  release.Series,
+		"snap-id": snapID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return a.(*asserts.SnapDeclaration), nil
+}
+
+// Publisher returns the account assertion for publisher of the given snap-id if it is present in the system assertion database.
+func Publisher(s *state.State, snapID string) (*asserts.Account, error) {
+	db := DB(s)
+	a, err := db.Find(asserts.SnapDeclarationType, map[string]string{
+		"series":  release.Series,
+		"snap-id": snapID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	snapDecl := a.(*asserts.SnapDeclaration)
+	a, err = db.Find(asserts.AccountType, map[string]string{
+		"account-id": snapDecl.PublisherID(),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("internal error: cannot find account assertion for the publisher of snap %q: %v", snapDecl.SnapName(), err)
+	}
+	return a.(*asserts.Account), nil
+}
+
+// AutoAliases returns the explicit automatic aliases alias=>app mapping for the given installed snap.
+func AutoAliases(s *state.State, info *snap.Info) (map[string]string, error) {
+	if info.SnapID == "" {
+		// without declaration
+		return nil, nil
+	}
+	decl, err := SnapDeclaration(s, info.SnapID)
+	if err != nil {
+		return nil, fmt.Errorf("internal error: cannot find snap-declaration for installed snap %q: %v", info.Name(), err)
+	}
+	explicitAliases := decl.Aliases()
+	if len(explicitAliases) != 0 {
+		return explicitAliases, nil
+	}
+	// XXX: old header fallback, just to keep edge working while we fix the
+	// store, to remove before next release!
+	oldAutoAliases := decl.AutoAliases()
+	if len(oldAutoAliases) == 0 {
+		return nil, nil
+	}
+	res := make(map[string]string, len(oldAutoAliases))
+	for _, alias := range oldAutoAliases {
+		app := info.Aliases[alias]
+		if app == nil {
+			// not a known alias anymore or yet, skip
+			continue
+
+		}
+		res[alias] = app.Name
+	}
+	return res, nil
+}
+
+func init() {
+	// hook retrieving auto-aliases into snapstate logic
+	snapstate.AutoAliases = AutoAliases
+}
+
+// AutoRefreshAssertions tries to refresh all assertions
+func AutoRefreshAssertions(s *state.State, userID int) error {
+	return RefreshSnapDeclarations(s, userID)
+}
---
+++ overlord/ifacestate/ifacestate.go
@@ -0,0 +1,201 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016-2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package ifacestate
+
+import (
+	"fmt"
+
+	"github.com/snapcore/snapd/i18n/dumb"
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/overlord/hookstate"
+	"github.com/snapcore/snapd/overlord/snapstate"
+	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/snap"
+)
+
+// Connect returns a set of tasks for connecting an interface.
+//
+func Connect(st *state.State, plugSnap, plugName, slotSnap, slotName string) (*state.TaskSet, error) {
+	if err := snapstate.CheckChangeConflict(st, plugSnap, nil); err != nil {
+		return nil, err
+	}
+	if err := snapstate.CheckChangeConflict(st, slotSnap, nil); err != nil {
+		return nil, err
+	}
+
+	// TODO: Store the intent-to-connect in the state so that we automatically
+	// try to reconnect on reboot (reconnection can fail or can connect with
+	// different parameters so we cannot store the actual connection details).
+
+	// Create a series of tasks:
+	//  - prepare-plug-<plug> hook
+	//  - prepare-slot-<slot> hook
+	//  - connect task
+	//  - connect-slot-<slot> hook
+	//  - connect-plug-<plug> hook
+	// The tasks run in sequence (are serialized by WaitFor).
+	// The prepare- hooks collect attributes via snapctl set.
+	// 'snapctl set' can only modify own attributes (plug's attributes in the *-plug-* hook and
+	// slot's attributes in the *-slot-* hook).
+	// 'snapctl get' can read both slot's and plug's attributes.
+	summary := fmt.Sprintf(i18n.G("Connect %s:%s to %s:%s"),
+		plugSnap, plugName, slotSnap, slotName)
+	connectInterface := st.NewTask("connect", summary)
+
+	initialContext := make(map[string]interface{})
+	initialContext["attrs-task"] = connectInterface.ID()
+
+	plugHookSetup := &hookstate.HookSetup{
+		Snap:     plugSnap,
+		Hook:     "prepare-plug-" + plugName,
+		Optional: true,
+	}
+
+	summary = fmt.Sprintf(i18n.G("Run hook %s of snap %q"), plugHookSetup.Hook, plugHookSetup.Snap)
+	preparePlugConnection := hookstate.HookTask(st, summary, plugHookSetup, initialContext)
+
+	slotHookSetup := &hookstate.HookSetup{
+		Snap:     slotSnap,
+		Hook:     "prepare-slot-" + slotName,
+		Optional: true,
+	}
+
+	summary = fmt.Sprintf(i18n.G("Run hook %s of snap %q"), slotHookSetup.Hook, slotHookSetup.Snap)
+	prepareSlotConnection := hookstate.HookTask(st, summary, slotHookSetup, initialContext)
+	prepareSlotConnection.WaitFor(preparePlugConnection)
+
+	connectInterface.Set("slot", interfaces.SlotRef{Snap: slotSnap, Name: slotName})
+	connectInterface.Set("plug", interfaces.PlugRef{Snap: plugSnap, Name: plugName})
+	if err := setInitialConnectAttributes(connectInterface, plugSnap, plugName, slotSnap, slotName); err != nil {
+		return nil, err
+	}
+	connectInterface.WaitFor(prepareSlotConnection)
+
+	connectSlotHookSetup := &hookstate.HookSetup{
+		Snap:     slotSnap,
+		Hook:     "connect-slot-" + slotName,
+		Optional: true,
+	}
+
+	summary = fmt.Sprintf(i18n.G("Run hook %s of snap %q"), connectSlotHookSetup.Hook, connectSlotHookSetup.Snap)
+	connectSlotConnection := hookstate.HookTask(st, summary, connectSlotHookSetup, initialContext)
+	connectSlotConnection.WaitFor(connectInterface)
+
+	connectPlugHookSetup := &hookstate.HookSetup{
+		Snap:     plugSnap,
+		Hook:     "connect-plug-" + plugName,
+		Optional: true,
+	}
+
+	summary = fmt.Sprintf(i18n.G("Run hook %s of snap %q"), connectPlugHookSetup.Hook, connectPlugHookSetup.Snap)
+	connectPlugConnection := hookstate.HookTask(st, summary, connectPlugHookSetup, initialContext)
+	connectPlugConnection.WaitFor(connectSlotConnection)
+
+	return state.NewTaskSet(preparePlugConnection, prepareSlotConnection, connectInterface, connectSlotConnection, connectPlugConnection), nil
+}
+
+func setInitialConnectAttributes(ts *state.Task, plugSnap string, plugName string, slotSnap string, slotName string) error {
+	// Set initial interface attributes for the plug and slot snaps in connect task.
+	var snapst snapstate.SnapState
+	var err error
+
+	st := ts.State()
+	if err = snapstate.Get(st, plugSnap, &snapst); err != nil {
+		return err
+	}
+	snapInfo, err := snapst.CurrentInfo()
+	if err != nil {
+		return err
+	}
+	if plug, ok := snapInfo.Plugs[plugName]; ok {
+		ts.Set("plug-attrs", plug.Attrs)
+	} else {
+		return fmt.Errorf("snap %q has no plug named %q", plugSnap, plugName)
+	}
+
+	if err = snapstate.Get(st, slotSnap, &snapst); err != nil {
+		return err
+	}
+	snapInfo, err = snapst.CurrentInfo()
+	if err != nil {
+		return err
+	}
+	snap.AddImplicitSlots(snapInfo)
+	if slot, ok := snapInfo.Slots[slotName]; ok {
+		ts.Set("slot-attrs", slot.Attrs)
+	} else {
+		return fmt.Errorf("snap %q has no slot named %q", slotSnap, slotName)
+	}
+
+	return nil
+}
+
+// Disconnect returns a set of tasks for  disconnecting an interface.
+func Disconnect(st *state.State, plugSnap, plugName, slotSnap, slotName string) (*state.TaskSet, error) {
+	if err := snapstate.CheckChangeConflict(st, plugSnap, nil); err != nil {
+		return nil, err
+	}
+	if err := snapstate.CheckChangeConflict(st, slotSnap, nil); err != nil {
+		return nil, err
+	}
+
+	summary := fmt.Sprintf(i18n.G("Disconnect %s:%s from %s:%s"),
+		plugSnap, plugName, slotSnap, slotName)
+	task := st.NewTask("disconnect", summary)
+	task.Set("slot", interfaces.SlotRef{Snap: slotSnap, Name: slotName})
+	task.Set("plug", interfaces.PlugRef{Snap: plugSnap, Name: plugName})
+	return state.NewTaskSet(task), nil
+}
+
+// CheckInterfaces checks whether plugs and slots of snap are allowed for installation.
+func CheckInterfaces(st *state.State, snapInfo *snap.Info) error {
+	// XXX: AddImplicitSlots is really a brittle interface
+	snap.AddImplicitSlots(snapInfo)
+
+	if snapInfo.SnapID == "" {
+		// no SnapID means --dangerous was given, so skip interface checks
+		return nil
+	}
+
+	baseDecl, err := assertstate.BaseDeclaration(st)
+	if err != nil {
+		return fmt.Errorf("internal error: cannot find base declaration: %v", err)
+	}
+
+	snapDecl, err := assertstate.SnapDeclaration(st, snapInfo.SnapID)
+	if err != nil {
+		return fmt.Errorf("cannot find snap declaration for %q: %v", snapInfo.Name(), err)
+	}
+
+	ic := policy.InstallCandidate{
+		Snap:            snapInfo,
+		SnapDeclaration: snapDecl,
+		BaseDeclaration: baseDecl,
+	}
+
+	return ic.Check()
+}
+
+func init() {
+	// hook interface checks into snapstate installation logic
+	snapstate.AddCheckSnapCallback(func(st *state.State, snapInfo, _ *snap.Info, _ snapstate.Flags) error {
+		return CheckInterfaces(st, snapInfo)
+	})
+}
