1b17b630eafbb9d7b7f4b361c5ac9c63c6d5ad3b
Michael Vogt <mvo@ubuntu.com>
2017-03-23 12:42:40 +0100

    add spread test for the ignore-configure-hook feature

--- tests/lib/snaps.sh
+++ tests/lib/snaps.sh
@@ -14,3 +14,15 @@ install_local() {
 install_local_devmode() {
     install_local "$1" --devmode
 }
+
+mksnap() {
+    dir="$1"
+    snap="$2"
+    
+    if [[ "$SPREAD_SYSTEM" == ubuntu-14.04-* ]]; then
+        # trusty does not support  -Xcompression-level 1
+        mksquashfs "$dir" "$snap" -comp gzip
+    else
+        mksquashfs "$dir" "$snap" -comp gzip -Xcompression-level 1
+    fi
+}
