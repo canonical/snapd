a1d6d743df5b8eb5d0e12d47b270ac63b8df02e3
Zygmunt Krynicki <me@zygoon.pl>
2017-03-24 20:58:43 +0100

    osutil: add BootID (#3079)
    This patch adds a small function for getting the current boot identifier.
    The kernel offers those from a well-known file in sysfs

---
+++ osutil/bootid_test.go
@@ -0,0 +1,36 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package osutil_test
+
+import (
+	"github.com/snapcore/snapd/osutil"
+
+	. "gopkg.in/check.v1"
+)
+
+type bootIdSuite struct{}
+
+var _ = Suite(&bootIdSuite{})
+
+func (s *bootIdSuite) TestSmoke(c *C) {
+	id, err := osutil.BootID()
+	c.Assert(err, IsNil)
+	c.Assert(id, HasLen, 36)
+}
