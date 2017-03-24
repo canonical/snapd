ce4e6617917e1085eca3049666595bc414166ea4
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-24 21:08:15 +0100

    interfaces/builtin: gofmt -s
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/builtin/gpio_test.go
+++ interfaces/builtin/gpio_test.go
@@ -137,7 +137,7 @@ func (s *GpioInterfaceSuite) TestSystemdConnectedSlot(c *C) {
 	err := spec.AddConnectedSlot(s.iface, s.gadgetPlug, s.gadgetGpioSlot)
 	c.Assert(err, IsNil)
 	c.Assert(spec.Services(), DeepEquals, map[string]*systemd.Service{
-		"snap.my-device.interface.gpio-100.service": &systemd.Service{
+		"snap.my-device.interface.gpio-100.service": {
 			Type:            "oneshot",
 			RemainAfterExit: true,
 			ExecStart:       `/bin/sh -c 'test -e /sys/class/gpio/gpio100 || echo 100 > /sys/class/gpio/export'`,
