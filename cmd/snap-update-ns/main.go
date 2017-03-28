c8e5ca34c3d018ddc8e1bf3adeeea977d4620180
Zygmunt Krynicki <me@zygoon.pl>
2017-03-28 16:41:10 +0200

    cmd: discard the C implementation of snap-update-ns (#3093)
    As the amount of logic increases the cost of writing snap-update-ns in C
    was just mounting so this patch is an experiment to write it all in Go
    instead.
    
    If successful it can also be used as a test to implement some of the
    snap-confine's shared parts and later on snap-confine itself.

---
+++ cmd/snap-update-ns/main.go
@@ -0,0 +1,54 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	Positionals struct {
+		SnapName string `positional-arg-name:"SNAP_NAME" required:"yes"`
+	} `positional-args:"true"`
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("cannot update snap namespace: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func parseArgs(args []string) error {
+	parser := flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash|flags.PassAfterNonOption)
+	_, err := parser.ParseArgs(args)
+	return err
+}
+
+func run() error {
+	if err := parseArgs(os.Args[1:]); err != nil {
+		return err
+	}
+
+	return fmt.Errorf("not implemented")
+}
