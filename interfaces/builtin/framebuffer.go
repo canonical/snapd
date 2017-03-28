32360cc10860978886e1b8be00805573414adae5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-03-28 11:36:29 +0200

    interfaces: capitalize Udev... as UDev...
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/builtin/framebuffer.go
+++ interfaces/builtin/framebuffer.go
@@ -76,7 +76,7 @@ func (iface *FramebufferInterface) AppArmorConnectedPlug(spec *apparmor.Specific
 	return nil
 }
 
-func (iface *FramebufferInterface) UdevConnectedPlug(spec *udev.Specification, plug *interfaces.Plug, slot *interfaces.Slot) error {
+func (iface *FramebufferInterface) UDevConnectedPlug(spec *udev.Specification, plug *interfaces.Plug, slot *interfaces.Slot) error {
 	const udevRule = `KERNEL=="fb[0-9]*", TAG+="%s"`
 	for appName := range plug.Apps {
 		tag := udevSnapSecurityName(plug.Snap.Name(), appName)
