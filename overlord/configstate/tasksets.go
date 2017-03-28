03bb81d4d6a13c0d5147b58d7af90cd759d5624f
Michael Vogt <michael.vogt@gmail.com>
2017-03-28 08:48:17 +0200

    many: ignore configure hook failures on core refresh
    Ignore configure hook failures on core refresh and add spread test for the ignore-configure-hook feature.

--- overlord/configstate/tasksets.go
+++ overlord/configstate/tasksets.go
@@ -35,11 +35,12 @@ func init() {
 }
 
 // Configure returns a taskset to apply the given configuration patch.
-func Configure(s *state.State, snapName string, patch map[string]interface{}) *state.TaskSet {
+func Configure(s *state.State, snapName string, patch map[string]interface{}, flags int) *state.TaskSet {
 	hooksup := &hookstate.HookSetup{
-		Snap:     snapName,
-		Hook:     "configure",
-		Optional: len(patch) == 0,
+		Snap:       snapName,
+		Hook:       "configure",
+		Optional:   len(patch) == 0,
+		IgnoreFail: flags&snapstate.IgnoreHookFailure != 0,
 	}
 	var contextData map[string]interface{}
 	if len(patch) > 0 {
