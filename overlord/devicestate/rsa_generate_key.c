423dc542835d5a9933d1db64281ed23885a6b192
Thomas Voß <thomas.voss.bochum@gmail.com>
2017-04-07 22:52:08 +0200

    Generate/encode/decode key in memory

--- overlord/devicestate/rsa_generate_key.c
+++ overlord/devicestate/rsa_generate_key.c
@@ -17,15 +17,17 @@
 
 #include "rsa_generate_key.h"
 
+#include <openssl/bio.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
 #include <openssl/rsa.h>
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-RSAKeyGenerationResult rsa_generate_key(uint64_t bits, char *private_key_file, char *public_key_file) {
-  RSAKeyGenerationResult result = RSA_KEY_GENERATION_SUCCESS;
+SnapdRSAKeyGenerationResult snapd_rsa_generate_key(uint64_t bits, SnapdRSAKeyGenerationBuffer *private_key) {
+    SnapdRSAKeyGenerationResult result = SNAPD_RSA_KEY_GENERATION_SUCCESS;
 
     BIGNUM *bne = NULL;
     RSA *rsa = NULL;
@@ -36,63 +38,67 @@ RSAKeyGenerationResult rsa_generate_key(uint64_t bits, char *private_key_file, c
     unsigned char *empty_passphrase = NULL;
     const size_t empty_passphrase_len = 0;
 
+    private_key->memory = NULL;
+    private_key->size = 0;
+
     if (RAND_status() != 1) {
-        result = RSA_KEY_GENERATION_SEED_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_SEED_FAILURE;
         goto free_all;
     }
 
     bne = BN_new();
     if (bne == NULL) {
-        result = RSA_KEY_GENERATION_ALLOCATION_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_ALLOCATION_FAILURE;
         goto free_all;
     }
 
     if (BN_set_word(bne, RSA_F4) != 1) {
-        result = RSA_KEY_GENERATION_ALLOCATION_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_ALLOCATION_FAILURE;
         goto free_all;
     }
 
     rsa = RSA_new();
     if (rsa == NULL) {
-        result = RSA_KEY_GENERATION_ALLOCATION_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_ALLOCATION_FAILURE;
         goto free_all;
     }
 
     if (RSA_generate_key_ex(rsa, bits, bne, NULL) != 1) {
-          result = RSA_KEY_GENERATION_KEY_GENERATION_FAILURE;
-          goto free_all;
-    }
-
-    bp_public = BIO_new_file(public_key_file, "w+");
-    if (bp_public == NULL) {
-        result = RSA_KEY_GENERATION_ALLOCATION_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_KEY_GENERATION_FAILURE;
         goto free_all;
     }
 
-    if (PEM_write_bio_RSAPublicKey(bp_public, rsa) != 1)  {
-        result = RSA_KEY_GENERATION_IO_FAILURE;
-        goto free_all;
-    }
-
-    bp_private = BIO_new_file(private_key_file, "w+");
+    bp_private = BIO_new(BIO_s_mem());
     if (bp_private == NULL) {
-        result = RSA_KEY_GENERATION_ALLOCATION_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_ALLOCATION_FAILURE;
         goto free_all;
     }
+
+    BIO_set_mem_buf(bp_private, BUF_MEM_new(), BIO_CLOSE);
+    BIO_clear_flags(bp_private, BIO_FLAGS_MEM_RDONLY);
 
     if (PEM_write_bio_RSAPrivateKey(bp_private, rsa, empty_cipher, empty_passphrase, empty_passphrase_len, NULL, NULL) != 1) {
-        result = RSA_KEY_GENERATION_IO_FAILURE;
+        result = SNAPD_RSA_KEY_GENERATION_IO_FAILURE;
         goto free_all;
     }
 
 free_all:
-    BIO_free_all(bp_public);
-    BIO_free_all(bp_private);
     RSA_free(rsa);
     BN_free(bne);
 
-    free(private_key_file);
-    free(public_key_file);
+    if (result == SNAPD_RSA_KEY_GENERATION_SUCCESS) {
+        // We are taking a copy here as we need to pass out ownership of the memory buffer.
+        // The underlying memory available from buf_mem might have been allocated by SSL-specific
+        // allocation functions and just free'ing it might not be correct.
+        BUF_MEM *buf_mem = NULL;
+        BIO_get_mem_ptr(bp_private, &buf_mem);
+        private_key->memory = malloc(buf_mem->length);
+        private_key->size = buf_mem->length;
+
+        memcpy(private_key->memory, buf_mem->data, buf_mem->length);
+    }
+
+    BIO_free_all(bp_private);
 
     return result;
 }
