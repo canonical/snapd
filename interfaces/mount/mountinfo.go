4e4d0749aa474506ea6b971fa732b5c92fc639af
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-04-12 23:52:19 +0200

    interfaces/mount: return pointer from ParseInfoEntry
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/mount/mountinfo.go
+++ interfaces/mount/mountinfo.go
@@ -44,7 +44,7 @@ type InfoEntry struct {
 }
 
 // ParseInfoEntry parses a single line of /proc/$PID/mountinfo file.
-func ParseInfoEntry(s string) (InfoEntry, error) {
+func ParseInfoEntry(s string) (*InfoEntry, error) {
 	var e InfoEntry
 	var err error
 	fields := strings.Fields(s)
@@ -53,30 +53,30 @@ func ParseInfoEntry(s string) (InfoEntry, error) {
 	// 36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue
 	// (1)(2)(3)   (4)   (5)      (6)      (7)   (8) (9)   (10)         (11)
 	if len(fields) < 10 {
-		return e, fmt.Errorf("incorrect number of fields, expected at least 10 but found %d", len(fields))
+		return nil, fmt.Errorf("incorrect number of fields, expected at least 10 but found %d", len(fields))
 	}
 	// Parse MountID (decimal number).
 	e.MountID, err = strconv.Atoi(fields[0])
 	if err != nil {
-		return e, fmt.Errorf("cannot parse mount ID: %q", fields[0])
+		return nil, fmt.Errorf("cannot parse mount ID: %q", fields[0])
 	}
 	// Parse ParentID (decimal number).
 	e.ParentID, err = strconv.Atoi(fields[1])
 	if err != nil {
-		return e, fmt.Errorf("cannot parse parent mount ID: %q", fields[1])
+		return nil, fmt.Errorf("cannot parse parent mount ID: %q", fields[1])
 	}
 	// Parses DevMajor:DevMinor pair (decimal numbers separated by colon).
 	subFields := strings.FieldsFunc(fields[2], func(r rune) bool { return r == ':' })
 	if len(subFields) != 2 {
-		return e, fmt.Errorf("cannot parse device major:minor number pair: %q", fields[2])
+		return nil, fmt.Errorf("cannot parse device major:minor number pair: %q", fields[2])
 	}
 	e.DevMajor, err = strconv.Atoi(subFields[0])
 	if err != nil {
-		return e, fmt.Errorf("cannot parse device major number: %q", subFields[0])
+		return nil, fmt.Errorf("cannot parse device major number: %q", subFields[0])
 	}
 	e.DevMinor, err = strconv.Atoi(subFields[1])
 	if err != nil {
-		return e, fmt.Errorf("cannot parse device minor number: %q", subFields[1])
+		return nil, fmt.Errorf("cannot parse device minor number: %q", subFields[1])
 	}
 	// NOTE: All string fields use the same escape/unescape logic as fstab files.
 	// Parse Root, MountDir and MountOptions fields.
@@ -90,16 +90,16 @@ func ParseInfoEntry(s string) (InfoEntry, error) {
 	for i = 6; i < len(fields) && fields[i] != "-"; i++ {
 	}
 	if i == len(fields) {
-		return e, fmt.Errorf("list of optional fields is not terminated properly")
+		return nil, fmt.Errorf("list of optional fields is not terminated properly")
 	}
 	e.OptionalFlds = strings.Join(fields[6:i], " ")
 	// Parse the last three fixed fields.
 	tailFields := fields[i+1:]
 	if len(tailFields) != 3 {
-		return e, fmt.Errorf("incorrect number of tail fields, expected 3 but found %d", len(tailFields))
+		return nil, fmt.Errorf("incorrect number of tail fields, expected 3 but found %d", len(tailFields))
 	}
 	e.FsType = unescape(tailFields[0])
 	e.MountSource = unescape(tailFields[1])
 	e.SuperOptions = unescape(tailFields[2])
-	return e, nil
+	return &e, nil
 }
