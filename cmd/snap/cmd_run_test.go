623cc519f6c557f8c0c5bcf5b1332eb92d8930cb
Simon Fels <simon.fels@canonical.com>
2017-04-20 14:59:29 +0200

    cmd/snap: correct syntax error

--- cmd/snap/cmd_run_test.go
+++ cmd/snap/cmd_run_test.go
@@ -510,7 +510,7 @@ func (s *SnapSuite) TestSnapRunXauthorityMigration(c *check.C) {
 	defer mockSnapConfine()()
 
 	// Ensure XDG_RUNTIME_DIR exists for the user we're testing with
-	os.MkdirAll(filepath.Join(dirs.XdgRuntimeDirBase, "1000")
+	os.MkdirAll(filepath.Join(dirs.XdgRuntimeDirBase, "1000"), 0700)
 
 	si := snaptest.MockSnap(c, string(mockYaml), string(mockContents), &snap.SideInfo{
 		Revision: snap.R("x2"),
