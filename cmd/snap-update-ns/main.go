63b4182b74814657769fccc2a61f95bef31f3198
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-04-24 12:55:21 +0200

    cmd/snap-update-ns: add actual implementation
    This patch adds a non-dummy implementation of snap-update-ns. There are
    still two pieces missing. The function that determines if a mount change
    is needed is dummy and always returns true. The mount changes are not
    really performed yet as the Perform function is just a stub. The stubs
    will be addressed with separate PRs.
    
    All that the tool now does is to print, to stdout, what should be done
    instead of actually doing it. Stderr is a bit more noisy, but
    essentially explains the same thing with more detail.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-update-ns/main.go
+++ cmd/snap-update-ns/main.go
@@ -25,6 +25,9 @@ import (
 
 	"github.com/jessevdk/go-flags"
 
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/interfaces/mount"
+	"github.com/snapcore/snapd/logger"
 	"github.com/snapcore/snapd/snap"
 )
 
@@ -53,12 +56,99 @@ func run() error {
 	if err := parseArgs(os.Args[1:]); err != nil {
 		return err
 	}
+
 	// There is some C code that runs before main() is started.
 	// That code always runs and sets an error condition if it fails.
 	// Here we just check for the error.
 	if err := BootstrapError(); err != nil {
 		return err
 	}
-	// TODO: implement this
-	return fmt.Errorf("not implemented")
+	snapName := opts.Positionals.SnapName
+
+	// Lock the mount namespace so that any concurrently attempted invocations
+	// of snap-confine are synchronized and will see consistent state.
+	lock, err := mount.OpenLock(snapName)
+	if err != nil {
+		return fmt.Errorf("cannot open mount namespace lock file: %s", err)
+	}
+	if err := lock.Lock(); err != nil {
+		return fmt.Errorf("cannot lock mount namespace: %s", err)
+	}
+	defer lock.Close()
+
+	// Read the desired and current mount profiles. Note that missing files
+	// count as empty profiles so that we can gracefully handle a mount
+	// interface connection/disconnection.
+	desiredProfilePath := fmt.Sprintf("%s/snap.%s.fstab", dirs.SnapMountPolicyDir, snapName)
+	desired, err := mount.LoadProfile(desiredProfilePath)
+	if err != nil {
+		return fmt.Errorf("cannot load desired mount profile: %s", err)
+	}
+	fmt.Fprintf(os.Stderr, "DESIRED:\n")
+	for _, entry := range desired.Entries {
+		fmt.Fprintf(os.Stderr, " - %s\n", entry)
+	}
+
+	currentProfilePath := fmt.Sprintf("%s/snap.%s.fstab", dirs.SnapRunNsDir, snapName)
+	current, err := mount.LoadProfile(currentProfilePath)
+	if err != nil {
+		return fmt.Errorf("cannot load current mount profile: %s", err)
+	}
+	fmt.Fprintf(os.Stderr, "CURRENT (before):\n")
+	for _, entry := range current.Entries {
+		fmt.Fprintf(os.Stderr, " - %s\n", entry)
+	}
+
+	// Compute the needed changes and perform each change if needed, collecting
+	// those that we managed to perform or that were performed already.
+	changesNeeded := mount.NeededChanges(current, desired)
+	fmt.Fprintf(os.Stderr, "CHANGES NEEDED:\n")
+	for _, change := range changesNeeded {
+		fmt.Fprintf(os.Stderr, " - %s\n", change)
+	}
+	var changesMade []mount.Change
+	for _, change := range changesNeeded {
+		if change.Action == mount.Keep {
+			changesMade = append(changesMade, change)
+			continue
+		}
+		// Read mount info each time as our operations may have unexpected
+		// consequences and we want to know the real state of what is mounted
+		// at each iteration.
+		mounted, err := mount.LoadMountInfo(mount.ProcSelfMountInfo)
+		if err != nil {
+			return fmt.Errorf("cannot read mount-info table: %s", err)
+		}
+		if !change.Needed(mounted) {
+			changesMade = append(changesMade, change)
+			continue
+		}
+		fmt.Printf("%s\n", change)
+		if err := change.Perform(); err != nil {
+			logger.Noticef("cannot perform mount change %s: %s", change, err)
+			continue
+		}
+		changesMade = append(changesMade, change)
+	}
+	fmt.Fprintf(os.Stderr, "CHANGES MADE:\n")
+	for _, change := range changesMade {
+		fmt.Fprintf(os.Stderr, " - %s\n", change)
+	}
+
+	// Compute the new current profile so that it contains only changes that were made
+	// and save it back for next runs.
+	current = &mount.Profile{}
+	for _, change := range changesMade {
+		if change.Action == mount.Mount || change.Action == mount.Keep {
+			current.Entries = append(current.Entries, change.Entry)
+		}
+	}
+	fmt.Fprintf(os.Stderr, "CURRENT (after):\n")
+	for _, entry := range current.Entries {
+		fmt.Fprintf(os.Stderr, " - %s\n", entry)
+	}
+	if err := current.Save(currentProfilePath); err != nil {
+		return fmt.Errorf("cannot save current mount profile: %s", err)
+	}
+	return nil
 }
