bf5c249bbe2a19726832155ddc575e90136339e3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-05-02 17:48:34 +0200

    cmd/snap-confine: aggregate operations holding global lock
    This patch moves the code that ensures /snap is rshared next to smilar
    operation that initializes the bind mount at /run/snapd/ns. This allows
    us to hold avoid one lock/unlock pair. The code is also only running if
    non-classic-confinement snaps are in operation. Since classic
    confinement snaps don't use any namespaces they don't rely on the /snap
    directory being rshared.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/snap-confine.c
+++ cmd/snap-confine/snap-confine.c
@@ -113,12 +113,6 @@ int main(int argc, char **argv)
 #endif				// ifdef HAVE_SECCOMP
 
 	if (geteuid() == 0) {
-		// ensure that "/" or "/snap" is mounted with the
-		// "shared" option, see LP:#1668659
-		int global_lock_fd = sc_lock_global();
-		sc_ensure_shared_snap_mount();
-		sc_unlock_global(global_lock_fd);
-
 		if (classic_confinement) {
 			/* 'classic confinement' is designed to run without the sandbox
 			 * inside the shared namespace. Specifically:
@@ -146,12 +140,13 @@ int main(int argc, char **argv)
 			 */
 			sc_reassociate_with_pid1_mount_ns();
 
-			// Do global initialization:
+			// ensure that "/" or "/snap" is mounted with the
+			// "shared" option, see LP:#1668659
 			int global_lock_fd = sc_lock_global();
+			debug("ensuring that snap mount directory is shared");
+			sc_ensure_shared_snap_mount();
 			debug("unsharing snap namespace directory");
 			sc_initialize_ns_groups();
-			// TODO: implement this.
-			debug("share snap directory here...");
 			sc_unlock_global(global_lock_fd);
 
 			// Do per-snap initialization.
