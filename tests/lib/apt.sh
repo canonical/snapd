6231900c787ea1f731fef8471dbfa9ff792f71c6
Simon Fels <simon.fels@canonical.com>
2017-05-12 09:43:35 +0200

    tests/lib: allow SRU validation only on ubuntu type systems

--- tests/lib/apt.sh
+++ tests/lib/apt.sh
@@ -4,6 +4,10 @@
 
 install_build_snapd(){
     if [ "$SRU_VALIDATION" = "1" ]; then
+        if [[ "$SPREAD_SYSTEM" != ubuntu-* ]]; then
+            echo "ERROR: SRU validation is only possible for Ubuntu based systems"
+            exit 1
+        fi
         apt install -y snapd
         cp /etc/apt/sources.list sources.list.back
         echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -c -s)-proposed restricted main multiverse universe" | tee /etc/apt/sources.list -a
