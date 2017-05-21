d6c56c4b5832be51477c0d12823b5d598aaa735e
Simon Fels <simon.fels@canonical.com>
2017-05-21 17:09:27 +0200

    tests/lib: define different snap mount dir / libexec dir for Fedora

--- tests/lib/dirs.sh
+++ tests/lib/dirs.sh
@@ -3,3 +3,13 @@
 # Default applies for: Ubuntu, Debian
 SNAPMOUNTDIR=/snap
 LIBEXECDIR=/usr/lib
+
+# For all other systems we need to change some directory paths
+case "$SPREAD_SYSTEM" in
+    fedora-*)
+        SNAPMOUNTDIR=/var/lib/snapd/snap
+        LIBEXECDIR=/usr/libexec
+        ;;
+    *)
+        ;;
+esac
