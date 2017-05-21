a7411a44582130e35d6f6e5962c8e9bf52ab6335
Simon Fels <simon.fels@canonical.com>
2017-05-21 17:10:08 +0200

    tests/lib: define necessary build dependencies for Fedora

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -180,6 +180,9 @@ case "$SPREAD_SYSTEM" in
     debian-*|ubuntu-*)
         DISTRO_BUILD_DEPS="build-essential curl devscripts expect gdebi-core jq rng-tools git netcat-openbsd"
         ;;
+    fedora-*)
+        DISTRO_BUILD_DEPS="mock git expect curl golang rpm-build redhat-lsb-core"
+        ;;
     *)
         ;;
 esac
