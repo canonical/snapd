5d350b7a5d70886959a35211bbbf307ec71ec7e8
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-06-12 23:48:23 -0300

    Improve comment for feed entropy method

--- tests/lib/entropy.sh
+++ tests/lib/entropy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 feed_kernel_entropy_pool() {
-	# Regenerate entropy when the level is under the entropy used by ubuntu-core (700)
+	# Feed the kernel entropy pool when the level is under 700 (level used by ubuntu-core)
 	if [ $# -gt 0 ] && [ "$1" = "--force" ] || [ $(sysctl --values kernel.random.entropy_avail || echo 0) -gt 700 ]; then
 		echo "HRNGDEVICE=/dev/urandom" > /etc/default/rng-tools
 		/etc/init.d/rng-tools restart
