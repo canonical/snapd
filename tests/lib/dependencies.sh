15c770bd8e18710c810ef58352905a91ddb3eed7
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-06-19 15:01:25 -0300

    4 spaces per indent and making echo to share deps

--- tests/lib/dependencies.sh
+++ tests/lib/dependencies.sh
@@ -3,129 +3,119 @@
 # shellcheck source=tests/lib/pkgdb.sh
 . "$TESTSLIB/pkgdb.sh"
 
-export DEPENDENCY_PACKAGES=
-export DISTRO_BUILD_DEPS=
-
-add_pkgs(){
-  DEPENDENCY_PACKAGES="$DEPENDENCY_PACKAGES $@"
+pkg_apt_dependencies_generic(){
+    echo autoconf
+    echo automake
+    echo autotools-dev
+    echo build-essential
+    echo curl
+    echo devscripts
+    echo expect
+    echo gdebi-core
+    echo git
+    echo indent
+    echo jq
+    echo libapparmor-dev
+    echo libglib2.0-dev
+    echo libseccomp-dev
+    echo libudev-dev
+    echo netcat-openbsd
+    echo pkg-config
+    echo python3-docutils
+    echo rng-tools
+    echo udev
 }
 
-get_apt_dependencies_generic(){
-  add_pkgs autoconf
-  add_pkgs automake
-  add_pkgs autotools-dev
-  add_pkgs build-essential
-  add_pkgs indent
-  add_pkgs libapparmor-dev
-  add_pkgs libglib2.0-dev
-  add_pkgs libseccomp-dev
-  add_pkgs libudev-dev
-  add_pkgs pkg-config
-  add_pkgs python3-docutils
-  add_pkgs udev
+pkg_apt_dependencies_classic(){
+    echo dbus-x11
+    echo jq
+    echo man
+    echo python3-yaml
+    echo upower
+
+    case "$SPREAD_SYSTEM" in
+        ubuntu-14.04-*)
+            echo cups-pdf
+            echo linux-image-extra-$(uname -r)
+            echo pollinate
+            ;;
+        ubuntu-16.04-32)
+            echo linux-image-extra-$(uname -r)
+            echo pollinate
+            echo printer-driver-cups-pdf
+            ;;
+        ubuntu-16.04-64)
+            echo gccgo-6
+            echo kpartx
+            echo libvirt-bin
+            echo linux-image-extra-$(uname -r)
+            echo pollinate
+            echo printer-driver-cups-pdf
+            echo qemu
+            echo x11-utils
+            echo xvfb
+            ;;
+        ubuntu-*)
+            echo linux-image-extra-$(uname -r)
+            echo pollinate
+            echo printer-driver-cups-pdf
+            ;;
+        debian-*)
+            echo printer-driver-cups-pdf
+            ;;
+    esac 
 }
 
-get_apt_dependencies_classic(){
-  add_pkgs dbus-x11
-  add_pkgs jq
-  add_pkgs man
-  add_pkgs python3-yaml
-  add_pkgs rng-tools
-  add_pkgs upower
-
-  case "$SPREAD_SYSTEM" in
-      ubuntu-14.04-*)
-          add_pkgs cups-pdf
-          add_pkgs linux-image-extra-$(uname -r)
-          add_pkgs pollinate
-          ;;
-      ubuntu-16.04-32)
-          add_pkgs linux-image-extra-$(uname -r)
-          add_pkgs pollinate
-          add_pkgs printer-driver-cups-pdf
-          ;;
-      ubuntu-16.04-64)
-          add_pkgs gccgo-6
-          add_pkgs kpartx
-          add_pkgs libvirt-bin
-          add_pkgs linux-image-extra-$(uname -r)
-          add_pkgs pollinate
-          add_pkgs printer-driver-cups-pdf
-          add_pkgs qemu
-          add_pkgs x11-utils
-          add_pkgs xvfb
-          ;;
-      ubuntu-*)
-          add_pkgs linux-image-extra-$(uname -r)
-          add_pkgs pollinate
-          add_pkgs printer-driver-cups-pdf
-          ;;
-      debian-*)
-          add_pkgs printer-driver-cups-pdf
-          ;;
-  esac 
+pkg_apt_dependencies_core(){
+    echo linux-image-extra-$(uname -r)
+    echo pollinate
 }
 
-get_apt_dependencies_core(){
-  add_pkgs linux-image-extra-$(uname -r)
-  add_pkgs pollinate
+pkg_dependency_fedora(){
+    echo curl
+    echo expect
+    echo git
+    echo golang
+    echo mock
+    echo redhat-lsb-core
+    echo rpm-build
 }
 
-get_dependency_fedora_packages(){
-  echo "Fedora dependencies not ready yet"
+pkg_dependency_opensuse(){
+    echo curl
+    echo expect
+    echo git
+    echo golang-packaging
+    echo jq
+    echo lsb-release
+    echo netcat-openbsd
+    echo osc
+    echo rng-tools
 }
 
-get_dependency_opensuse_packages(){
-  echo "Opensuse dependencies not ready yet"
+pkg_dependencies(){
+    case "$SPREAD_SYSTEM" in
+        ubuntu-core-16-*)
+            pkg_apt_dependencies_generic
+            pkg_apt_dependencies_core
+            ;;
+        ubuntu-*|debian-*)
+            pkg_apt_dependencies_generic
+            pkg_apt_dependencies_classic
+            ;;
+        fedora-*)
+            pkg_dependency_fedora
+            ;;
+        opensuse-*)
+            pkg_dependency_opensuse
+            ;;
+        *)
+            ;;
+    esac  
 }
 
-get_test_dependencies(){
-  case "$SPREAD_SYSTEM" in
-      ubuntu-core-16-*)
-          get_apt_dependencies_generic
-          get_apt_dependencies_core
-          ;;
-      ubuntu-*|debian-*)
-          get_apt_dependencies_generic
-          get_apt_dependencies_classic
-          ;;
-      fedora-*)
-          get_dependency_fedora_packages
-          ;;
-      opensuse-*)
-          get_dependency_opensuse_packages
-          ;;
-      *)
-          ;;
-  esac  
-}
-
-get_build_dependencies(){
-  case "$SPREAD_SYSTEM" in
-    debian-*|ubuntu-*)
-        DISTRO_BUILD_DEPS="build-essential curl devscripts expect gdebi-core jq rng-tools git netcat-openbsd"
-        ;;
-    fedora-*)
-        DISTRO_BUILD_DEPS="mock git expect curl golang rpm-build redhat-lsb-core"
-        ;;
-    opensuse-*)
-        DISTRO_BUILD_DEPS="osc git expect curl golang-packaging lsb-release netcat-openbsd jq rng-tools"
-        ;;
-    *)
-        ;;
-  esac
-}
-
-install_build_dependencies(){
-  # Specify necessary packages which need to be installed on a
-  # system to provide a basic build environment for snapd.
-  get_build_dependencies
-  echo "Installing the following packages: $DISTRO_BUILD_DEPS"
-  distro_install_package $DISTRO_BUILD_DEPS
-}
-
-install_test_dependencies(){
-  get_test_dependencies
-  echo "Installing the following packages: $DEPENDENCY_PACKAGES"
-  distro_install_package $DEPENDENCY_PACKAGES
+install_dependencies(){
+    pcks=$(pkg_dependencies | tr "\n" " ")
+    echo "Installing the following packages: $DEPENDENCY_PACKAGES"
+    distro_install_package $pcks
 }
