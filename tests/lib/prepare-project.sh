b9c19c938575f1dca2a93c67cea974e124b6878f
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-06-21 16:49:53 -0300

    Removing unneeded ""

--- tests/lib/prepare-project.sh
+++ tests/lib/prepare-project.sh
@@ -207,7 +207,7 @@ if [[ "$SPREAD_SYSTEM" == ubuntu-14.04-* ]]; then
 fi
 
 distro_purge_package snapd || true
-distro_install_package "${DISTRO_BUILD_DEPS[@]}"
+distro_install_package ${DISTRO_BUILD_DEPS[@]}
 
 # We take a special case for Debian/Ubuntu where we install additional build deps
 # base on the packaging. In Fedora/Suse this is handled via mock/osc
