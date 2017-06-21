6c475868c03d3919b4aeea35d8ee534df61f4ba4
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-06-21 16:49:53 -0300

    Removing unneeded ""

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -154,13 +154,13 @@ distro_install_package() {
 
         case "$SPREAD_SYSTEM" in
             ubuntu-*|debian-*)
-                quiet apt-get install "$APT_FLAGS" -y "$package_name"
+                quiet apt-get install $APT_FLAGS -y "$package_name"
                 ;;
             fedora-*)
-                dnf -q -y install "$DNF_FLAGS" "$package_name"
+                dnf -q -y install $DNF_FLAGS "$package_name"
                 ;;
             opensuse-*)
-                zypper -q install -y "$ZYPPER_FLAGS" "$package_name"
+                zypper -q install -y $ZYPPER_FLAGS "$package_name"
                 ;;
             *)
                 echo "ERROR: Unsupported distribution $SPREAD_SYSTEM"
