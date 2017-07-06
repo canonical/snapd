9bc201bbfc7762f638842c3f6593f1eb88d05f17
Michael Vogt <mvo@ubuntu.com>
2017-07-06 14:04:01 +0200

    add spread tests for snapctl internal configure-core

--- corecfg/corecfg.go
+++ corecfg/corecfg.go
@@ -69,8 +69,9 @@ func Run() error {
 		if err != nil {
 			return err
 		}
-		if output != nil {
-			if err := switchDisableService(service, string(output)); err != nil {
+		value := strings.TrimSpace(string(output))
+		if value != "" {
+			if err := switchDisableService(service, value); err != nil {
 				return err
 			}
 		}
@@ -81,23 +82,24 @@ func Run() error {
 	if err != nil {
 		return err
 	}
-	if output != nil {
-		switchHandlePowerKey(string(output))
+	value := strings.TrimSpace(string(output))
+	if value != "" {
+		if err := switchHandlePowerKey(value); err != nil {
+			return err
+		}
 	}
 
-	piConfig := os.Getenv("TEST_UBOOT_CONFIG")
-	if piConfig == "" {
-		piConfig = "/boot/uboot/config.txt"
-	}
+	piConfig := "/boot/uboot/config.txt"
 	if osutil.FileExists(piConfig) {
 		// snapctl can actually give us the whole dict in
 		// JSON, in a single call; use that instead of this.
 		config := map[string]string{}
 		for key := range piConfigKeys {
-			value, err := snapctlGet(fmt.Sprintf("pi-config.%s", strings.Replace(key, "_", "-", -1)))
+			output, err := snapctlGet(fmt.Sprintf("pi-config.%s", strings.Replace(key, "_", "-", -1)))
 			if err != nil {
 				return err
 			}
+			value := strings.TrimSpace(string(output))
 			config[key] = string(value)
 		}
 		if err := updatePiConfig(piConfig, config); err != nil {
