9cb850622bc703a80e7a50fe77fbbaf0930d2190
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-07-12 12:11:15 -0300

    Fixing how fake resets are done

---
+++ tests/lib/files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wait_for_file() {
+	the_file="$1"
+	iters="$2"
+	sleep_time="$3"
+
+	for _ in $(seq "$iters"); do
+        if [ -f "$the_file" ]; then
+            return 0
+        fi
+        sleep "$sleep_time"
+    done
+    return 1
+}
