682f3e8e976e7fb09115375ae27c609dd47e2e37
Jamie Strandboge <jamie@ubuntu.com>
2017-07-14 19:35:47 +0000

    cmd/snap-confine: remove unused and now unneeded setup_user_xdg_runtime_dir()

--- cmd/snap-confine/user-support.c
+++ cmd/snap-confine/user-support.c
@@ -40,26 +40,3 @@ void setup_user_data()
 		die("cannot create user data directory: %s", user_data);
 	};
 }
-
-void setup_user_xdg_runtime_dir()
-{
-	const char *xdg_runtime_dir = getenv("XDG_RUNTIME_DIR");
-
-	if (xdg_runtime_dir == NULL)
-		return;
-	// Only support absolute paths.
-	if (xdg_runtime_dir[0] != '/') {
-		die("XDG_RUNTIME_DIR must be an absolute path");
-	}
-
-	errno = 0;
-	debug("creating user XDG_RUNTIME_DIR directory: %s", xdg_runtime_dir);
-	if (sc_nonfatal_mkpath(xdg_runtime_dir, 0755) < 0) {
-		die("cannot create user XDG_RUNTIME_DIR directory: %s",
-		    xdg_runtime_dir);
-	}
-	// if successfully created the directory (ie, not EEXIST), then chmod it.
-	if (errno == 0 && chmod(xdg_runtime_dir, 0700) != 0) {
-		die("cannot change permissions of user XDG_RUNTIME_DIR directory to 0700");
-	}
-}
