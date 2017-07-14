eaeaa809b7dba9a4e637719d953428ca154777a3
Jamie Strandboge <jamie@ubuntu.com>
2017-07-14 19:53:30 +0000

    add some comments to the apparmor policy

--- interfaces/builtin/wayland.go
+++ interfaces/builtin/wayland.go
@@ -31,7 +31,14 @@ const waylandBaseDeclarationSlots = `
 const waylandConnectedPlugAppArmor = `
 # Description: Can access compositors supporting the wayland protocol
 
+# Allow access to the wayland compsitor server socket
 owner /run/user/*/wayland-[0-9]* rw,
+
+# The shared per-client sockets are opened and immediately unlinked so while we
+# use a glob rule, in practice snaps won't be able to interfere with each
+# other's client sockets. However, the glob rule does mean that coordinating
+# snaps are able to share data via paths that match the glob. Upstream Wayland
+# are considering changing how this works, so for now allow it.
 # TODO: should xwayland be here?
 owner /run/user/*/{mesa,mutter,sdl,weston,xwayland}-shared-* rw,
 
