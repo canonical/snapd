e2b58e6043eef207c4be93f122cb0ac836d671c5
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-07-17 14:35:14 -0300

    Cosmetic changes based on review comments

--- tests/lib/files.sh
+++ tests/lib/files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wait_for_file() {
-	the_file="$1"
-	iters="$2"
-	sleep_time="$3"
+    the_file="$1"
+    iters="$2"
+    sleep_time="$3"
 
-	for _ in $(seq "$iters"); do
+    for _ in $(seq "$iters"); do
         if [ -f "$the_file" ]; then
             return 0
         fi
