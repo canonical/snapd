87f785c2d69b6b34eef4f5cc751163828ce3ffaa
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-07-18 19:01:09 -0300

    fix the setup for pinentry

--- tests/lib/mkpinentry.sh
+++ tests/lib/mkpinentry.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "setup fake gpg pinentry environment"
-mkdir -p ~/.snap/gnupg/
-echo pinentry-program "$TESTSLIB/pinentry-fake.sh" > ~/.snap/gnupg/gpg-agent.conf
-chmod -R go-rwx ~/.snap
+if [ gpg --version | MATCH "gpg (GnuPG) 1." ]; then
+    echo "fake gpg pinentry not used for gpg v1"
+else
+    echo "setup fake gpg pinentry environment for gpg v2 or higher"
+    mkdir -p ~/.gnupg
+    echo pinentry-program "$TESTSLIB/pinentry-fake.sh" > ~/.gnupg/gpg-agent.conf
+fi
