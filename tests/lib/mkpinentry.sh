a0e31d6c17bf240e8515c2323173784276c740d9
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2017-07-19 19:12:04 -0300

    fix compilation for seccomp and how pinentry is done for debian

--- tests/lib/mkpinentry.sh
+++ tests/lib/mkpinentry.sh
@@ -4,6 +4,15 @@ if [ gpg --version | MATCH "gpg (GnuPG) 1." ]; then
     echo "fake gpg pinentry not used for gpg v1"
 else
     echo "setup fake gpg pinentry environment for gpg v2 or higher"
-    mkdir -p ~/.gnupg
-    echo pinentry-program "$TESTSLIB/pinentry-fake.sh" > ~/.gnupg/gpg-agent.conf
+    case "$SPREAD_SYSTEM" in
+        opensuse-*)
+            mkdir -p ~/.gnupg
+            echo pinentry-program "$TESTSLIB/pinentry-fake.sh" > ~/.gnupg/gpg-agent.conf
+            ;;
+        *)
+            mkdir -p ~/.snap/gnupg/
+            echo pinentry-program "$TESTSLIB/pinentry-fake.sh" > ~/.snap/gnupg/gpg-agent.conf
+            chmod -R go-rwx ~/.snap
+            ;;
+    esac
 fi
