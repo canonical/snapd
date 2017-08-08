9985a19e8e49dcf17435f5b33f2538c8c29b1aba
Jamie Strandboge <jamie@ubuntu.com>
2017-08-08 21:35:41 +0000

    interfaces/builtin/wayland_test.go: update for modern testing techniques

--- interfaces/builtin/wayland_test.go
+++ interfaces/builtin/wayland_test.go
@@ -26,38 +26,34 @@ import (
 	"github.com/snapcore/snapd/interfaces/apparmor"
 	"github.com/snapcore/snapd/interfaces/builtin"
 	"github.com/snapcore/snapd/snap"
-	"github.com/snapcore/snapd/snap/snaptest"
 	"github.com/snapcore/snapd/testutil"
 )
 
 type WaylandInterfaceSuite struct {
-	iface interfaces.Interface
-	slot  *interfaces.Slot
-	plug  *interfaces.Plug
+	iface    interfaces.Interface
+	coreSlot *interfaces.Slot
+	plug     *interfaces.Plug
 }
-
-const waylandMockPlugSnapInfoYaml = `name: other
-version: 1.0
-apps:
- app2:
-  command: foo
-  plugs: [wayland]
-`
 
 var _ = Suite(&WaylandInterfaceSuite{
 	iface: builtin.MustInterface("wayland"),
 })
 
+const waylandConsumerYaml = `name: consumer
+apps:
+ app:
+  plugs: [wayland]
+`
+
+const waylandCoreYaml = `name: core
+type: os
+slots:
+  wayland:
+`
+
 func (s *WaylandInterfaceSuite) SetUpTest(c *C) {
-	s.slot = &interfaces.Slot{
-		SlotInfo: &snap.SlotInfo{
-			Snap:      &snap.Info{SuggestedName: "core", Type: snap.TypeOS},
-			Name:      "wayland",
-			Interface: "wayland",
-		},
-	}
-	plugSnap := snaptest.MockInfo(c, waylandMockPlugSnapInfoYaml, nil)
-	s.plug = &interfaces.Plug{PlugInfo: plugSnap.Plugs["wayland"]}
+	s.plug = MockPlug(c, waylandConsumerYaml, nil, "wayland")
+	s.coreSlot = MockSlot(c, waylandCoreYaml, nil, "wayland")
 }
 
 func (s *WaylandInterfaceSuite) TestName(c *C) {
@@ -65,7 +61,8 @@ func (s *WaylandInterfaceSuite) TestName(c *C) {
 }
 
 func (s *WaylandInterfaceSuite) TestSanitizeSlot(c *C) {
-	c.Assert(s.slot.Sanitize(s.iface), IsNil)
+	c.Assert(s.coreSlot.Sanitize(s.iface), IsNil)
+	// wayland slot currently only used with core
 	slot := &interfaces.Slot{SlotInfo: &snap.SlotInfo{
 		Snap:      &snap.Info{SuggestedName: "some-snap"},
 		Name:      "wayland",
@@ -79,13 +76,25 @@ func (s *WaylandInterfaceSuite) TestSanitizePlug(c *C) {
 	c.Assert(s.plug.Sanitize(s.iface), IsNil)
 }
 
-func (s *WaylandInterfaceSuite) TestUsedSecuritySystems(c *C) {
-	// connected plugs have a non-nil security snippet for apparmor
-	apparmorSpec := &apparmor.Specification{}
-	err := apparmorSpec.AddConnectedPlug(s.iface, s.plug, nil, s.slot, nil)
-	c.Assert(err, IsNil)
-	c.Assert(apparmorSpec.SecurityTags(), DeepEquals, []string{"snap.other.app2"})
-	c.Assert(apparmorSpec.SnippetForTag("snap.other.app2"), testutil.Contains, `owner /run/user/*/wayland-[0-9]* rw,`)
+func (s *WaylandInterfaceSuite) TestAppArmorSpec(c *C) {
+	// connected plug to core slot
+	spec := &apparmor.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, nil, s.coreSlot, nil), IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, "owner /run/user/*/wayland-[0-9]* rw,")
+
+	// connected plug to core slot
+	spec = &apparmor.Specification{}
+	c.Assert(spec.AddConnectedSlot(s.iface, s.plug, nil, s.coreSlot, nil), IsNil)
+	c.Assert(spec.SecurityTags(), HasLen, 0)
+}
+
+func (s *WaylandInterfaceSuite) TestStaticInfo(c *C) {
+	si := interfaces.StaticInfoOf(s.iface)
+	c.Assert(si.ImplicitOnCore, Equals, false)
+	c.Assert(si.ImplicitOnClassic, Equals, true)
+	c.Assert(si.Summary, Equals, `allows access to compositors supporting wayland protocol`)
+	c.Assert(si.BaseDeclarationSlots, testutil.Contains, "wayland")
 }
 
 func (s *WaylandInterfaceSuite) TestInterfaces(c *C) {
