cfe2b2d8f35017e15f1dc0c95894db049e65acfd
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-08-16 09:06:50 +0200

    io: rename shouldReallySync to snapdUnsafeIO
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- osutil/io.go
+++ osutil/io.go
@@ -92,7 +92,7 @@ func AtomicWriteFileChown(filename string, data []byte, perm os.FileMode, flags 
 		return errors.New("internal error: AtomicWriteFileChown needs none or both of uid and gid set")
 	}
 
-	if shouldReallySync {
+	if !snapdUnsafeIO {
 		if err := fd.Sync(); err != nil {
 			return err
 		}
@@ -102,7 +102,7 @@ func AtomicWriteFileChown(filename string, data []byte, perm os.FileMode, flags 
 		return err
 	}
 
-	if shouldReallySync {
+	if !snapdUnsafeIO {
 		return dir.Sync()
 	}
 	return nil
@@ -111,10 +111,4 @@ func AtomicWriteFileChown(filename string, data []byte, perm os.FileMode, flags 
 // Allow disabling sync for testing. This brings massive improvements on
 // certain filesystems (like btrfs) and very much noticeable improvements in
 // all unit tests in genreal.
-var shouldReallySync bool = true
-
-func init() {
-	if len(os.Args) > 0 && strings.HasSuffix(os.Args[0], ".test") && GetenvBool("SNAPD_UNSAFE_IO") {
-		shouldReallySync = false
-	}
-}
+var snapdUnsafeIO bool = len(os.Args) > 0 && strings.HasSuffix(os.Args[0], ".test") && GetenvBool("SNAPD_UNSAFE_IO")
