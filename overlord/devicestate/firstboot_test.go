e43b1f74cc28a05fbfa6f872cd990bf63789ad40
Michael Vogt <mvo@ubuntu.com>
2017-08-21 07:53:24 +0200

    fix TestPopulateFromSeedMissingBootloader hanging

--- overlord/devicestate/firstboot_test.go
+++ overlord/devicestate/firstboot_test.go
@@ -363,11 +363,6 @@ snaps:
 	chg1 := st.NewChange("become-operational", "init device")
 	chg1.SetStatus(state.DoingStatus)
 
-	st.Unlock()
-	s.overlord.Settle()
-	st.Lock()
-	// unlocked by defer
-
 	return chg
 }
 
@@ -381,6 +376,8 @@ func (s *FirstBootTestSuite) TestPopulateFromSeedHappy(c *C) {
 	})
 
 	chg := s.makeBecomeOpertionalChange(c)
+	s.overlord.Settle()
+
 	st := s.overlord.State()
 	st.Lock()
 	defer st.Unlock()
@@ -445,9 +442,18 @@ func (s *FirstBootTestSuite) TestPopulateFromSeedHappy(c *C) {
 func (s *FirstBootTestSuite) TestPopulateFromSeedMissingBootloader(c *C) {
 	chg := s.makeBecomeOpertionalChange(c)
 	st := s.overlord.State()
+
+	// run the change, we cannot use s.overlord.Settle() here as each
+	// "DeviceManager.Ensure()" will call ensureSeedYaml which will
+	// create a new "seed" change so things never settle down
+	s.overlord.Loop()
+	defer s.overlord.Stop()
+	for !chg.IsReady() {
+		// nothing
+	}
+
 	st.Lock()
 	defer st.Unlock()
-
 	c.Assert(chg.Err(), ErrorMatches, `(?s).* cannot determine bootloader.*`)
 }
 
