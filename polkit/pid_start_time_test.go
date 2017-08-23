f3b96230865e7b6682f6137171f96b96c1cd51c8
James Henstridge <james@jamesh.id.au>
2017-08-23 16:10:58 +0800

    polkit: clean ups based on mvo's review.

--- polkit/pid_start_time_test.go
+++ polkit/pid_start_time_test.go
@@ -21,7 +21,9 @@
 package polkit
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"syscall"
 	"testing"
 
@@ -55,4 +57,15 @@ func (s *polkitSuite) TestGetStartTimeBadPid(c *check.C) {
 	startTime, err := getStartTimeForPid(uint32(pid))
 	c.Assert(err, check.ErrorMatches, "open .*: no such file or directory")
 	c.Check(startTime, check.Equals, uint64(0))
+}
+
+func (s *polkitSuite) TestProcStatParsing(c *check.C) {
+	filename := filepath.Join(c.MkDir(), "stat")
+	contents := []byte("18433 (cat) R 9732 18433 9732 34818 18433 4194304 96 0 1 0 0 0 0 0 20 0 1 0 123104764 7602176 182 18446744073709551615 94902526107648 94902526138492 140734457666896 0 0 0 0 0 0 0 0 0 17 5 0 0 0 0 0 94902528236168 94902528237760 94902542680064 140734457672267 140734457672287 140734457672287 140734457675759 0")
+	err := ioutil.WriteFile(filename, contents, 0644)
+	c.Assert(err, check.IsNil)
+
+	startTime, err := getStartTimeForProcStatFile(filename)
+	c.Assert(err, check.IsNil)
+	c.Check(startTime, check.Equals, uint64(123104764))
 }
