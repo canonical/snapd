268326694974b5749910879c2b17df47f0acd3c3
Zygmunt Krynicki <me@zygoon.pl>
2017-08-25 09:55:59 +0200

    interfaces/backends: enable apparmor backend on parital feature set
    AppArmor may be fully or only partially supported on a given system. In
    the past when partially supported apparmor was detected then the backend
    would not be loaded, leading to very confusing and error-prone behavior
    for people switching between mainline (unpatched) and ubuntu (patched)
    kernels. Because the backend was not loaded then now-stale profiles were
    *not* updated or removed and things behaved oddly, where incorrect
    profiles were actually loaded into the kernel (apparmor tries to degrade
    gracefully but the profiles were not permissive enough).
    
    With this patch the apparmor backend is loaded as long as the kernel
    module is loaded and enabled. This will be coupled with a patch to the
    backend, which will generate more permissive profiles for
    anything-less-than-full feature set. This patch will allow snapd to
    enable apparmor on Debian, openSUSE as well as Ubuntu running pre-4.15
    mainline kernels.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- interfaces/backends/backends.go
+++ interfaces/backends/backends.go
@@ -1,7 +1,7 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 
 /*
- * Copyright (C) 2016 Canonical Ltd
+ * Copyright (C) 2016-2017 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -20,6 +20,9 @@
 package backends
 
 import (
+	"fmt"
+
+	aa "github.com/snapcore/snapd/apparmor"
 	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/interfaces/apparmor"
 	"github.com/snapcore/snapd/interfaces/dbus"
@@ -28,21 +31,41 @@ import (
 	"github.com/snapcore/snapd/interfaces/seccomp"
 	"github.com/snapcore/snapd/interfaces/systemd"
 	"github.com/snapcore/snapd/interfaces/udev"
-	"github.com/snapcore/snapd/release"
 )
 
-// append when a new security backend is added
-var All = []interfaces.SecurityBackend{
-	&systemd.Backend{},
-	&seccomp.Backend{},
-	&dbus.Backend{},
-	&udev.Backend{},
-	&mount.Backend{},
-	&kmod.Backend{},
-}
+var All []interfaces.SecurityBackend = backends()
 
-func init() {
-	if !release.ReleaseInfo.ForceDevMode() {
-		All = append(All, &apparmor.Backend{})
+func backends() []interfaces.SecurityBackend {
+	all := []interfaces.SecurityBackend{
+		&systemd.Backend{},
+		&seccomp.Backend{},
+		&dbus.Backend{},
+		&udev.Backend{},
+		&mount.Backend{},
+		&kmod.Backend{},
 	}
+	featureLevel, summary := aa.ProbeKernel().Evaluate()
+
+	// This should be logger.Noticef but due to ordering of initialization
+	// calls, the logger is not ready at this point yet and the message goes
+	// nowehre. Per advice from other snapd developers, we just print it
+	// directly.
+	//
+	// TODO: on this should become a user-visible message via the user-warning
+	// framework, so that users are aware that we have non-strict confinement.
+	if featureLevel != aa.Full {
+		fmt.Printf("WARNING: %s\n", summary)
+	}
+
+	// Enable apparmor backend if there is any level of apparmor support,
+	// including partial feature set. This will allow snap-confine to always
+	// link to apparmor and check if it is enabled on boot, knowing that there
+	// is always *some* profile to apply to each snap process.
+	//
+	// When some features are missing the backend will generate more permissive
+	// profiles that keep applications operational, in forced-devmode.
+	if featureLevel != aa.None {
+		all = append(all, &apparmor.Backend{})
+	}
+	return all
 }
