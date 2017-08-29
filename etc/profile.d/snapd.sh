e2680fae496f89c5ed6188896fbc069c7ee2e076
John R. Lenton <jlenton@gmail.com>
2017-08-29 13:46:19 +0100

    dropping etc entirely (should now be unneeded)

--- etc/profile.d/snapd.sh
+++
@@ -1,6 +0,0 @@
-# set XDG_DATA_DIRS for snapd provisioned desktop files.
-if [ "${XDG_DATA_DIRS#*snapd}" = "${XDG_DATA_DIRS}" ]; then
-    XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}:/var/lib/snapd/desktop"
-fi
-
-export XDG_DATA_DIRS
