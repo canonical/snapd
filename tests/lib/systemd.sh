cbd8599ee8e49088287f44a0ae75f92dfc16a3a5
Michael Vogt <mvo@ubuntu.com>
2017-08-30 14:54:45 +0200

    make wait_for_service() less chatty and error if the service did not start after 5min

--- tests/lib/systemd.sh
+++ tests/lib/systemd.sh
@@ -22,7 +22,17 @@ systemd_stop_and_destroy_unit() {
 wait_for_service() {
     local service_name="$1"
     local state="${2:-active}"
-    while ! systemctl show -p ActiveState "$service_name" | grep -q "ActiveState=$state"; do
-        systemctl status "$service_name" || true; sleep 1;
+    for i in $(seq 300); do
+        if systemctl show -p ActiveState "$service_name" | grep -q "ActiveState=$state"; then
+            return
+        fi
+        # show debug output every 1min
+        if [ $(( i % 60 )) = 0 ]; then
+            systemctl status "$service_name" || true;
+        fi
+        sleep 1;
     done
+
+    echo "service $service_name did not start"
+    exit 1
 }
