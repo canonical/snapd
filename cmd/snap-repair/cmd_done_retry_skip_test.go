05fd934127b55fb1840a5a67856698356deb0ccd
Michael Vogt <mvo@ubuntu.com>
2017-09-15 16:33:04 +0200

    snap-repair: implement `snap-repair {done,skip,retry}`
    The snap-repairs will use `snap-repair {done,skip,retry}` to
    signal the status to the repair runner. The command in turn will
    write the right status to the SNAP_REPAIR_STATUS_FD but that is
    an implementation detail that should not leak into the repair
    scripts itself.

---
+++ cmd/snap-repair/cmd_done_retry_skip_test.go
@@ -0,0 +1,73 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main_test
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	. "gopkg.in/check.v1"
+
+	repair "github.com/snapcore/snapd/cmd/snap-repair"
+)
+
+func (r *repairSuite) TestStatusNoStatusFdEnv(c *C) {
+	for _, s := range []string{"done", "skip", "retry"} {
+		err := repair.ParseArgs([]string{s})
+		c.Check(err, ErrorMatches, "cannot find SNAP_REPAIR_STATUS_FD environment")
+	}
+}
+
+func (r *repairSuite) TestStatusBadStatusFD(c *C) {
+	for _, s := range []string{"done", "skip", "retry"} {
+		os.Setenv("SNAP_REPAIR_STATUS_FD", "123456789")
+		defer os.Unsetenv("SNAP_REPAIR_STATUS_FD")
+
+		err := repair.ParseArgs([]string{s})
+		c.Check(err, ErrorMatches, `write <snap-repair-status-fd>: bad file descriptor`)
+	}
+}
+
+func (r *repairSuite) TestStatusUnparsableStatusFD(c *C) {
+	for _, s := range []string{"done", "skip", "retry"} {
+		os.Setenv("SNAP_REPAIR_STATUS_FD", "xxx")
+		defer os.Unsetenv("SNAP_REPAIR_STATUS_FD")
+
+		err := repair.ParseArgs([]string{s})
+		c.Check(err, ErrorMatches, `cannot parse SNAP_REPAIR_STATUS_FD environment: strconv.ParseInt: parsing "xxx": invalid syntax`)
+	}
+}
+
+func (r *repairSuite) TestStatusHappy(c *C) {
+	for _, s := range []string{"done", "skip", "retry"} {
+		rp, wp, err := os.Pipe()
+		c.Assert(err, IsNil)
+		os.Setenv("SNAP_REPAIR_STATUS_FD", strconv.Itoa(int(wp.Fd())))
+		defer os.Unsetenv("SNAP_REPAIR_STATUS_FD")
+
+		err = repair.ParseArgs([]string{s})
+		c.Check(err, IsNil)
+
+		status, err := ioutil.ReadAll(rp)
+		c.Assert(err, IsNil)
+		c.Check(string(status), Equals, s+"\n")
+	}
+}
