82f286224de8a24c4b2052c32417cc7300fc339a
Jamie Strandboge <jamie@ubuntu.com>
2017-09-19 17:52:11 +0000

    fix one more whitespace syntax check

--- cmd/snap-confine/udev-support.c
+++ cmd/snap-confine/udev-support.c
@@ -235,7 +235,6 @@ void setup_devices_cgroup(const char *security_tag, struct snappy_udev *udev_s)
 	if (stat(nvpath, &sbuf) == 0) {
 		_run_snappy_app_dev_add_majmin(udev_s, nvpath, 247, 0);
 	}
-
 	// add the assigned devices
 	while (udev_s->assigned != NULL) {
 		const char *path = udev_list_entry_get_name(udev_s->assigned);
