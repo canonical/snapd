f0c37b69988ba40e3e708c7d9fa3b746506c5c0c
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-09-19 15:00:07 +0200

    cmd/snap-update-ns: add tests for find_1st_string
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-update-ns/bootstrap.h
+++ cmd/snap-update-ns/bootstrap.h
@@ -31,6 +31,7 @@ void process_arguments(const char* cmdline, const char** snap_name_out, bool* sh
 ssize_t read_cmdline(char* buf, size_t buf_size);
 const char* find_argv0(const char* buf);
 const char* find_snap_name(const char* buf);
+const char* find_1st_option(const char* buf);
 int validate_snap_name(const char* snap_name);
 
 #endif
