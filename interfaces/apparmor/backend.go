4da42a4241ac3eac17077fd29e17aeb9a00f5585
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-09-22 14:02:27 +0200

    interfaces/apparmor: treat nfs and nfs4 the same way
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/apparmor/backend.go
+++ interfaces/apparmor/backend.go
@@ -122,7 +122,7 @@ func anythingUsesNfs() (bool, error) {
 		return false, err
 	}
 	for _, entry := range entries {
-		if entry.FsType == "nfs4" {
+		if entry.FsType == "nfs4" || entry.FsType == "nfs" {
 			return true, nil
 		}
 	}
