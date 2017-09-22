c555dea3b26f5db0d05d7adc73d413c0f2e64ea5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-09-22 16:10:05 +0200

    cmd,dirs: treat "liri" the same way as "arch"
    Liri is a derivative of Arch and should use the same support code.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- dirs/dirs.go
+++ dirs/dirs.go
@@ -157,7 +157,7 @@ func SetRootDir(rootdir string) {
 	GlobalRootDir = rootdir
 
 	switch release.ReleaseInfo.ID {
-	case "fedora", "centos", "rhel", "arch", "manjaro":
+	case "fedora", "centos", "rhel", "arch", "manjaro", "lirios":
 		SnapMountDir = filepath.Join(rootdir, "/var/lib/snapd/snap")
 	default:
 		SnapMountDir = filepath.Join(rootdir, defaultSnapMountDir)
