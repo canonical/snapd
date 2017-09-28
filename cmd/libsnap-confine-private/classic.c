7489df5d523b0a2fada12b964f7d4fa00d22f798
Michael Vogt <mvo@ubuntu.com>
2017-09-28 14:08:30 -0400

    address review feedback

--- cmd/libsnap-confine-private/classic.c
+++ cmd/libsnap-confine-private/classic.c
@@ -10,18 +10,16 @@ char *os_release = "/etc/os-release";
 
 bool is_running_on_classic_distribution()
 {
-	char buf[255];
-	int is_core = false;
-
 	FILE *f SC_CLEANUP(sc_cleanup_file) = fopen(os_release, "r");
 	if (f == NULL) {
-		return !is_core;
+		return true;
 	}
+
+	char buf[255];
 	while (fgets(buf, sizeof buf, f) != NULL) {
 		if (strcmp(buf, "ID=ubuntu-core\n") == 0) {
-			is_core = true;
-			break;
+			return false;
 		}
 	}
-	return !is_core;
+	return true;
 }
