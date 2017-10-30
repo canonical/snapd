5524e61e25934a7d00756fee8a60cd6c0844bd33
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-10-30 13:52:37 +0100

    interfaces/builtin: correct comment
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/builtin/content_test.go
+++ interfaces/builtin/content_test.go
@@ -427,7 +427,7 @@ slots:
 	apparmorSpec := &apparmor.Specification{}
 	c.Assert(apparmorSpec.AddConnectedPlug(s.iface, connectedPlug, connectedSlot), IsNil)
 
-	// Create the mount and apparmor specifications.
+	// Analyze the mount specification.
 	expectedMnt := []mount.Entry{{
 		Name:    "/var/snap/producer/common/read-common",
 		Dir:     "/var/snap/consumer/common/import/read-common",
