6cfd0d7e1bdab0bfca957f581690a9ea4d5d31eb
Mickaël FRANC <mickael.franc@gmail.com>
2017-11-02 22:45:37 +0100

    udev: prepare implementation of info mode

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ import (
 )
 
 var (
-	filePath *string
+	filePath      *string
+	monitor, info *bool
 )
 
 func init() {
 	filePath = flag.String("file", "", "Optionnal input file path with matcher-rules (default: no matcher)")
+	monitor = flag.Bool("monitor", false, "Enable monitor mode")
+	info = flag.Bool("info", false, "Enable monitor mode")
 }
 
 func main() {
@@ -31,34 +34,50 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	conn := new(netlink.UEventConn)
-	if err = conn.Connect(); err != nil {
-		log.Fatalln("Unable to connect to Netlink Kobject UEvent socket")
+	if monitor == nil && info == nil {
+		log.Fatalln("You should use only one mode:", os.Args[0], "-monitor|-info")
 	}
-	defer conn.Close()
 
-	queue := make(chan netlink.UEvent)
-	quit := conn.Monitor(queue, matcher)
+	if (monitor != nil && *monitor) && (info != nil && *info) {
+		log.Fatalln("Unable to enable both mode : monitor & info")
+	}
 
-	// Signal handler to quit properly monitor mode
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		<-signals
-		log.Println("Exiting monitor mode...")
-		quit <- true
-		os.Exit(0)
-	}()
-
-	// Handling message from queue
-	for {
-		select {
-		case uevent := <-queue:
-			log.Printf("Handle %s\n", pretty.Sprint(uevent))
+	if *monitor {
+		log.Println("Monitoring UEvent kernel message to user-space...")
+		conn := new(netlink.UEventConn)
+		if err = conn.Connect(); err != nil {
+			log.Fatalln("Unable to connect to Netlink Kobject UEvent socket")
 		}
+		defer conn.Close()
+
+		queue := make(chan netlink.UEvent)
+		quit := conn.Monitor(queue, matcher)
+
+		// Signal handler to quit properly monitor mode
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		go func() {
+			<-signals
+			log.Println("Exiting monitor mode...")
+			quit <- true
+			os.Exit(0)
+		}()
+
+		// Handling message from queue
+		for {
+			select {
+			case uevent := <-queue:
+				log.Printf("Handle %s\n", pretty.Sprint(uevent))
+			}
+		}
+	}
+
+	if *info {
+
 	}
 }
 
+// getOptionnalMatcher Parse and load config file which contains rules for matching
 func getOptionnalMatcher() (matcher netlink.Matcher, err error) {
 	if filePath == nil || *filePath == "" {
 		return nil, nil
