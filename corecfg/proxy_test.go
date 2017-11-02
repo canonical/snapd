cdd999f0f09806085ae0b9249e7c85122f26413f
Samuele Pedroni <pedronis@lucediurna.net>
2017-11-02 14:35:38 +0100

    support setting proxy.store if there's a matching store assertion

--- corecfg/proxy_test.go
+++ corecfg/proxy_test.go
@@ -24,11 +24,15 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 
 	. "gopkg.in/check.v1"
 
+	"github.com/snapcore/snapd/asserts"
+	"github.com/snapcore/snapd/asserts/assertstest"
 	"github.com/snapcore/snapd/corecfg"
 	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/overlord/assertstate"
 	"github.com/snapcore/snapd/release"
 )
 
@@ -36,15 +40,35 @@ type proxySuite struct {
 	coreCfgSuite
 
 	mockEtcEnvironment string
+
+	storeSigning *assertstest.StoreStack
 }
 
 var _ = Suite(&proxySuite{})
 
 func (s *proxySuite) SetUpTest(c *C) {
+	s.coreCfgSuite.SetUpTest(c)
+
 	dirs.SetRootDir(c.MkDir())
 	err := os.MkdirAll(filepath.Join(dirs.GlobalRootDir, "/etc/"), 0755)
 	c.Assert(err, IsNil)
 	s.mockEtcEnvironment = filepath.Join(dirs.GlobalRootDir, "/etc/environment")
+
+	s.storeSigning = assertstest.NewStoreStack("canonical", nil)
+
+	db, err := asserts.OpenDatabase(&asserts.DatabaseConfig{
+		Backstore:       asserts.NewMemoryBackstore(),
+		Trusted:         s.storeSigning.Trusted,
+		OtherPredefined: s.storeSigning.Generic,
+	})
+	c.Assert(err, IsNil)
+
+	s.state.Lock()
+	assertstate.ReplaceDB(s.state, db)
+	s.state.Unlock()
+
+	err = db.Add(s.storeSigning.StoreAccountKey(""))
+	c.Assert(err, IsNil)
 }
 
 func (s *proxySuite) TearDownTest(c *C) {
@@ -75,4 +99,38 @@ PATH="/usr/bin"
 PATH="/usr/bin"
 %[1]s_proxy=%[1]s://example.com`, proto))
 	}
+}
+
+func (s *proxySuite) TestConfigureProxyStore(c *C) {
+	// no assertion
+	conf := &mockConf{
+		state: s.state,
+		conf: map[string]interface{}{
+			"proxy.store": "foo",
+		},
+	}
+
+	err := corecfg.Run(conf)
+	c.Check(err, ErrorMatches, `cannot set proxy.store to "foo" without a matching store assertion`)
+
+	operatorAcct := assertstest.NewAccount(s.storeSigning, "foo-operator", nil, "")
+	s.state.Lock()
+	err = assertstate.Add(s.state, operatorAcct)
+	s.state.Unlock()
+	c.Assert(err, IsNil)
+
+	// have a store assertion.
+	stoAs, err := s.storeSigning.Sign(asserts.StoreType, map[string]interface{}{
+		"store":       "foo",
+		"operator-id": operatorAcct.AccountID(),
+		"timestamp":   time.Now().Format(time.RFC3339),
+	}, nil, "")
+	c.Assert(err, IsNil)
+	s.state.Lock()
+	err = assertstate.Add(s.state, stoAs)
+	s.state.Unlock()
+	c.Assert(err, IsNil)
+
+	err = corecfg.Run(conf)
+	c.Check(err, IsNil)
 }
