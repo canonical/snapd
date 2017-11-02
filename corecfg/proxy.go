8ad0b4ac4a0238fba5047083265a171e5d67aaba
Samuele Pedroni <pedronis@lucediurna.net>
2017-11-02 14:35:38 +0100

    support setting proxy.store if there's a matching store assertion

--- corecfg/proxy.go
+++ corecfg/proxy.go
@@ -20,12 +20,15 @@
 package corecfg
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/snapcore/snapd/asserts"
 	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/overlord/assertstate"
 )
 
 var proxyConfigKeys = map[string]bool{
@@ -67,6 +70,28 @@ func handleProxyConfiguration(tr Conf) error {
 	}
 	if err := updateEtcEnvironmentConfig(etcEnvironment(), config); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func handleProxyStore(tr Conf) error {
+	proxyStore, err := coreCfg(tr, "proxy.store")
+	if err != nil {
+		return err
+	}
+
+	if proxyStore != "" {
+		st := tr.State()
+		st.Lock()
+		defer st.Unlock()
+		_, err := assertstate.Store(st, proxyStore)
+		if asserts.IsNotFound(err) {
+			return fmt.Errorf("cannot set proxy.store to %q without a matching store assertion", proxyStore)
+		}
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
