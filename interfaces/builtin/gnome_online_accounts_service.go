4c6d95e4218f5d3f2f8906708d6db63f81032a16
James Henstridge <james@jamesh.id.au>
2017-11-02 22:26:36 +0800

    interfaces: add an interface for gnome-online-accounts D-Bus service

---
+++ interfaces/builtin/gnome_online_accounts_service.go
@@ -0,0 +1,77 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const gnomeOnlineAccountsServiceSummary = `allows operating as the GNOME Online Accounts service`
+
+const gnomeOnlineAccountsServiceBaseDeclarationSlots = `
+  gnome-online-accounts-service:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-connection: true
+`
+
+const gnomeOnlineAccountsServiceConnectedPlugAppArmor = `
+# Description: Allow access to GNOME Online Accounts service
+
+#include <abstractions/dbus-session-strict>
+
+# Allow use of ObjectManager APIs, used to enumerate accounts
+dbus (receive, send)
+    bus=session
+    interface=org.freedesktop.DBus.ObjectManager
+    path=/org/gnome/OnlineAccounts
+    peer=(label=unconfined),
+
+# Allow access to properties on accounts
+dbus (receive, send)
+    bus=session
+    interface=org.freedesktop.DBus.Properties
+    path=/org/gnome/OnlineAccounts{,/**}
+    peer=(label=unconfined),
+
+# Allow access to OnlineAccounts methods
+dbus (receive, send)
+    bus=session
+    interface=org.gnome.OnlineAccounts.*
+    path=/org/gnome/OnlineAccounts{,/**}
+    peer=(label=unconfined),
+
+# Allow clients to introspect the service
+dbus (send)
+    bus=session
+    interface=org.freedesktop.DBus.Introspectable
+    path=/com/ubuntu/OnlineAccounts{,/**}
+    member=Introspect
+    peer=(label=unconfined),
+`
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "gnome-online-accounts-service",
+		summary:               gnomeOnlineAccountsServiceSummary,
+		implicitOnCore:        false,
+		implicitOnClassic:     true,
+		reservedForOS:         true,
+		baseDeclarationSlots:  gnomeOnlineAccountsServiceBaseDeclarationSlots,
+		connectedPlugAppArmor: gnomeOnlineAccountsServiceConnectedPlugAppArmor,
+	})
+}
