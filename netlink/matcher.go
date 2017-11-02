5a2eb4c869f40a0250350dd492414df5dcd86f3a
Mickaël FRANC <mickael.franc@gmail.com>
2017-11-02 22:14:55 +0100

    matcher: enrich interface

--- netlink/matcher.go
+++ netlink/matcher.go
@@ -6,6 +6,8 @@ import (
 
 type Matcher interface {
 	Evaluate(e UEvent) bool
+	EvaluateAction(a KObjAction) bool
+	EvaluateEnv(e map[string]string) bool
 	Compile() error
 }
 
@@ -17,7 +19,6 @@ type RuleDefinition struct {
 
 // Evaluate return true if all condition match uevent and envs in rule exists in uevent
 func (r RuleDefinition) Evaluate(e UEvent) bool {
-
 	// Compile if needed
 	if r.rule == nil {
 		if err := r.Compile(); err != nil {
@@ -25,29 +26,32 @@ func (r RuleDefinition) Evaluate(e UEvent) bool {
 		}
 	}
 
-	// Evaluate uevent with rule
-	matchAction := (r.rule.Action == nil)
-	if !matchAction {
-		matchAction = r.rule.Action.MatchString(e.Action.String())
-	}
+	return r.EvaluateAction(e.Action) && r.EvaluateEnv(e.Env)
+}
 
-	foundEnv := (len(r.rule.Env) == 0)
-	for envName, reg := range r.rule.Env {
-		foundEnv = false
-		for k, v := range e.Env {
-			if k == envName {
-				foundEnv = true
-				if !reg.MatchString(v) {
-					return false
-				}
-			}
-		}
-		if !foundEnv {
+// EvaluateAction return true if the action match
+func (r RuleDefinition) EvaluateAction(a KObjAction) (match bool) {
+	// Compile if needed
+	if r.rule == nil {
+		if err := r.Compile(); err != nil {
 			return false
 		}
 	}
+	if match = (r.rule.Action == nil); !match {
+		match = r.rule.Action.MatchString(a.String())
+	}
+	return
+}
 
-	return matchAction && foundEnv
+// EvaluateEnv return true if all env match and exists
+func (r RuleDefinition) EvaluateEnv(e map[string]string) bool {
+	// Compile if needed
+	if r.rule == nil {
+		if err := r.Compile(); err != nil {
+			return false
+		}
+	}
+	return r.rule.Env.Evaluate(e)
 }
 
 // Compile prepare rule definition to be able to Evaluate() an UEvent
@@ -77,7 +81,30 @@ func (r *RuleDefinition) Compile() error {
 // rule is the compiled version of the RuleDefinition
 type rule struct {
 	Action *regexp.Regexp
-	Env    map[string]*regexp.Regexp
+	Env    Env
+}
+
+type Env map[string]*regexp.Regexp
+
+func (e Env) Evaluate(env map[string]string) bool {
+	foundEnv := (len(e) == 0)
+	for envName, reg := range e {
+		foundEnv = false
+		for k, v := range env {
+			if k == envName {
+				foundEnv = true
+				if !reg.MatchString(v) {
+					return false
+				}
+			}
+		}
+		if !foundEnv {
+			return false
+		}
+	}
+
+	return foundEnv
+
 }
 
 // RuleDefinitions is like chained rule with OR operator
@@ -101,6 +128,26 @@ func (rs *RuleDefinitions) Compile() error {
 func (rs RuleDefinitions) Evaluate(e UEvent) bool {
 	for _, r := range rs.Rules {
 		if r.Evaluate(e) {
+			return true
+		}
+	}
+	return false
+}
+
+// EvaluateAction return true if the action match
+func (rs RuleDefinitions) EvaluateAction(a KObjAction) (match bool) {
+	for _, r := range rs.Rules {
+		if r.EvaluateAction(a) {
+			return true
+		}
+	}
+	return false
+}
+
+// EvaluateEnv return true if almost one env match all regexp
+func (rs RuleDefinitions) EvaluateEnv(e map[string]string) bool {
+	for _, r := range rs.Rules {
+		if r.EvaluateEnv(e) {
 			return true
 		}
 	}
