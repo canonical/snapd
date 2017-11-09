72d28fd3685dc9dedcbaf831182fe16090404b35
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2017-11-09 08:54:26 +0100

    osutil/user: import non-cgo implementation of Go stdlib os/user
    Import non-cgo bits of Go's os/user package. Implement missing integration
    calls.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

---
+++ osutil/user/listgroups.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package user
+
+import (
+	"fmt"
+)
+
+func listGroups(u *User) ([]string, error) {
+	return nil, fmt.Errorf("user: list groups for %s: not implemented", u.Username)
+}
