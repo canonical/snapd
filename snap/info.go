2e412186b7d65e1b07acd872f0d780bbf502f54d
Pawel Stolowski <stolowski@gmail.com>
2017-11-14 12:23:00 +0100

    Added hooks to slots.

--- snap/info.go
+++ snap/info.go
@@ -420,7 +420,9 @@ func (slot *SlotInfo) SecurityTags() []string {
 	for _, app := range slot.Apps {
 		tags = append(tags, app.SecurityTag())
 	}
-	// NOTE: hooks cannot have slots
+	for _, hook := range slot.Hooks {
+		tags = append(tags, hook.SecurityTag())
+	}
 	sort.Strings(tags)
 	return tags
 }
@@ -439,6 +441,7 @@ type SlotInfo struct {
 	Attrs     map[string]interface{}
 	Label     string
 	Apps      map[string]*AppInfo
+	Hooks     map[string]*HookInfo
 }
 
 // AppInfo provides information about a app.
@@ -481,6 +484,7 @@ type HookInfo struct {
 
 	Name  string
 	Plugs map[string]*PlugInfo
+	Slots map[string]*SlotInfo
 }
 
 // SecurityTag returns application-specific security tag.
