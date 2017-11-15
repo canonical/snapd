554c8a97ba8022995a7e53263c8b40e347994d32
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-11-16 00:19:43 +0100

    interfaces,tests: skip unknown plug/slot interfaces
    This patch changes the Repository.AddSnap method to skip unknown
    interfaces when adding constituent plugs and slots. This regression
    happened because the repository no longer validates the snap, assuming
    the loader did. The loader did notice but did not remove the offending
    interfaces.
    
    As it is unclear what should happen, this patch implements a minimal
    solution to prevent snapd from crashing on nil interface.
    
    Fixes: https://bugs.launchpad.net/snappy/+bug/1732555
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/repo.go
+++ interfaces/repo.go
@@ -822,6 +822,9 @@ func (r *Repository) AddSnap(snapInfo *snap.Info) error {
 	}
 
 	for plugName, plugInfo := range snapInfo.Plugs {
+		if _, ok := r.ifaces[plugInfo.Interface]; !ok {
+			continue
+		}
 		if r.plugs[snapName] == nil {
 			r.plugs[snapName] = make(map[string]*snap.PlugInfo)
 		}
@@ -829,6 +832,9 @@ func (r *Repository) AddSnap(snapInfo *snap.Info) error {
 	}
 
 	for slotName, slotInfo := range snapInfo.Slots {
+		if _, ok := r.ifaces[slotInfo.Interface]; !ok {
+			continue
+		}
 		if r.slots[snapName] == nil {
 			r.slots[snapName] = make(map[string]*snap.SlotInfo)
 		}
