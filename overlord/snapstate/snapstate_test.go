df435b9ccaa1162158b8489141b9b9247f6881ed
Pawel Stolowski <stolowski@gmail.com>
2017-11-15 10:06:40 +0100

    Test lanes in InstallMany unit test.

--- overlord/snapstate/snapstate_test.go
+++ overlord/snapstate/snapstate_test.go
@@ -6321,8 +6321,12 @@ func (s *snapmgrTestSuite) TestInstallMany(c *C) {
 	c.Assert(tts, HasLen, 2)
 	c.Check(installed, DeepEquals, []string{"one", "two"})
 
-	for _, ts := range tts {
+	for i, ts := range tts {
 		verifyInstallTasks(c, 0, 0, ts, s.state)
+		// check that tasksets are in separate lanes
+		for _, t := range ts.Tasks() {
+			c.Assert(t.Lanes(), DeepEquals, []int{i + 1})
+		}
 	}
 }
 
