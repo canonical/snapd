92491e072dd95592595346173f07c8c2f8fca2dc
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-11-22 12:16:32 +0100

    tests: document search for broken udev rules
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -45,6 +45,10 @@ prepare_project_each() {
 }
 
 restore_project_each() {
+    # Udev rules are notoriously hard to write and seemingly correct but subtly
+    # wrong rules can pass review. Whenever that happens udev logs an error
+    # message. As a last resort from lack of a better mechanism we can try to
+    # pick up such errors.
     if grep "invalid .*snap.*.rules" /var/log/syslog; then
         echo "Invalid udev file detected, test most likely broke it"
         exit 1
