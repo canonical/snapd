f43560727c9f7096db5ed838ef534a2fa43926d4
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2017-11-23 14:34:15 +0100

    tests/lib: fix shellcheck errors
    Fix the following warnings:
    
    In ./tests/lib/prepare.sh line 198:
                cd $TESTSLIB/cache/
                   ^-- SC2086: Double quote to prevent globbing and word splitting.
    
    In ./tests/lib/store.sh line 45:
        snap ack $p
                 ^-- SC2086: Double quote to prevent globbing and word splitting.
    
    In ./tests/lib/store.sh line 47:
        snap ack $p
                 ^-- SC2086: Double quote to prevent globbing and word splitting.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -195,7 +195,7 @@ EOF
         # used for resuming downloads.
         (
             set -x
-            cd $TESTSLIB/cache/
+            cd "$TESTSLIB/cache/"
             # Download each of the snaps we want to pre-cache. Note that `snap download`
             # a quick no-op if the file is complete.
             for snap_name in ${PRE_CACHE_SNAPS:-}; do
