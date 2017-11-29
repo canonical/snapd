2ae5b7f2b054f0feb9c7e8c3aea27a52ac690d4f
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-11-29 12:16:25 -0300

    Changes in python code for snap and descriptions of the test

--- tests/lib/snaps/test-snapd-network-status-provider/provider.py
+++ tests/lib/snaps/test-snapd-network-status-provider/provider.py
@@ -6,23 +6,26 @@ import dbus.service
 
 from dbus.mainloop.glib import DBusGMainLoop
 
+INTERFACE = 'com.ubuntu.connectivity1.NetworkingStatus'
+PATH = '/com/ubuntu/connectivity1/NetworkingStatus'
+
 DBusGMainLoop(set_as_default=True)
+
 
 class DBusProvider(dbus.service.Object):
     def __init__(self):
         bus = dbus.SystemBus()
-        bus_name = dbus.service.BusName("com.ubuntu.connectivity1.NetworkingStatus", bus=bus)
-        dbus.service.Object.__init__(self, bus_name, "/com/ubuntu/connectivity1/NetworkingStatus")
+        bus_name = dbus.service.BusName(INTERFACE, bus=bus)
+        dbus.service.Object.__init__(self, bus_name, PATH)
 
-    @dbus.service.method(dbus_interface="com.ubuntu.connectivity1.NetworkingStatus",
-                         out_signature="s")
+    @dbus.service.method(dbus_interface=INTERFACE, out_signature="s")
     def GetVersion(self):
         return "my-ap-version"
 
-    @dbus.service.method(dbus_interface="com.ubuntu.connectivity1.NetworkingStatus",
-                         out_signature="s")
+    @dbus.service.method(dbus_interface=INTERFACE, out_signature="s")
     def GetState(self):
         return "my-ap-state"
+
 
 if __name__ == "__main__":
     DBusProvider()
