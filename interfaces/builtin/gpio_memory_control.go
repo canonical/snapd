1d9711207e6800f1bcd29f145b37aba15609975f
Kyle Fazzari <kyrofa@ubuntu.com>
2017-11-29 11:32:18 -0800

    interfaces: add gpio-memory-control interface
    Raspberry Pi systems expose GPIO-only memory at /dev/gpiomem, which
    provides much faster memory-mapped access than sysfs while being more
    constrained than /dev/mem.
    
    Support write-access to /dev/gpiomem with a new interface named
    gpio-memory-control.
    
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

---
+++ interfaces/builtin/gpio_memory_control.go
@@ -0,0 +1,51 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const gpioMemoryControlSummary = `allows write access to all gpio memory`
+
+const gpioMemoryControlBaseDeclarationSlots = `
+  gpio-memory-control:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const gpioMemoryControlConnectedPlugAppArmor = `
+# Description: Allow writing to /dev/gpiomem on kernels that provide it.
+#
+/dev/gpiomem rw,
+`
+
+var gpioMemoryControlConnectedPlugUDev = []string{`KERNEL=="gpiomem"`}
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "gpio-memory-control",
+		summary:               gpioMemoryControlSummary,
+		implicitOnCore:        true,
+		implicitOnClassic:     true,
+		baseDeclarationSlots:  gpioMemoryControlBaseDeclarationSlots,
+		connectedPlugAppArmor: gpioMemoryControlConnectedPlugAppArmor,
+		connectedPlugUDev:     gpioMemoryControlConnectedPlugUDev,
+		reservedForOS:         true,
+	})
+}
