46e8cb5d567df1ff2e88dc9b6b679921ae7e471e
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2017-11-29 17:07:22 -0300

    Rename snap dir name

--- tests/lib/snaps/test-snapd-location-control-provider/provider.py
+++ tests/lib/snaps/test-snapd-location-control-provider/provider.py
@@ -6,22 +6,23 @@ import dbus.service
 
 from dbus.mainloop.glib import DBusGMainLoop
 
+INTERFACE = 'com.ubuntu.location.Service'
+PATH = '/com/ubuntu/location/Service'
+
 DBusGMainLoop(set_as_default=True)
 
 
 class DBusProvider(dbus.service.Object):
     def __init__(self):
         bus = dbus.SystemBus()
-        bus_name = dbus.service.BusName("com.ubuntu.location.Service", bus=bus)
-        dbus.service.Object.__init__(self, bus_name, "/com/ubuntu/location/Service")
+        bus_name = dbus.service.BusName(INTERFACE, bus=bus)
+        dbus.service.Object.__init__(self, bus_name, PATH)
 
-    @dbus.service.method(dbus_interface="com.ubuntu.location.Service",
-                         out_signature="s")
+    @dbus.service.method(dbus_interface=INTERFACE, out_signature="s")
     def Get(self):
         return "location-get"
 
-    @dbus.service.method(dbus_interface="com.ubuntu.location.Service",
-                         out_signature="s")
+    @dbus.service.method(dbus_interface=INTERFACE, out_signature="s")
     def Set(self):
         return "location-set"
 
