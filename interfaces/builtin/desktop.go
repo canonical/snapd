96952886730d53f072b28dbe5da2a3bb8d5e54fa
Jamie Strandboge <jamie@ubuntu.com>
2017-12-05 17:26:18 +0000

    interfaces/{desktop,unity7}: allow receiving ActionInvoked notification signal
    References:
    https://forum.snapcraft.io/t/call-for-testing-mailspring/2849/26

--- interfaces/builtin/desktop.go
+++ interfaces/builtin/desktop.go
@@ -97,7 +97,7 @@ dbus (receive)
     bus=session
     path=/org/freedesktop/Notifications
     interface=org.freedesktop.Notifications
-    member=NotificationClosed
+    member={ActionInvoked,NotificationClosed}
     peer=(label=unconfined),
 
 # Allow requesting interest in receiving media key events. This tells Gnome
