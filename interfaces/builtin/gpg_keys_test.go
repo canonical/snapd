9c17c9d85daa4dea4a6f87a5aa6b3eda26e11901
Jamie Strandboge <jamie@ubuntu.com>
2017-12-05 23:08:35 +0000

    update tests for modern practices

--- interfaces/builtin/gpg_keys_test.go
+++ interfaces/builtin/gpg_keys_test.go
@@ -26,37 +26,36 @@ import (
 	"github.com/snapcore/snapd/interfaces/apparmor"
 	"github.com/snapcore/snapd/interfaces/builtin"
 	"github.com/snapcore/snapd/snap"
-	"github.com/snapcore/snapd/snap/snaptest"
 	"github.com/snapcore/snapd/testutil"
 )
 
 type GpgKeysInterfaceSuite struct {
-	iface interfaces.Interface
-	slot  *interfaces.Slot
-	plug  *interfaces.Plug
+	iface    interfaces.Interface
+	slotInfo *snap.SlotInfo
+	slot     *interfaces.ConnectedSlot
+	plugInfo *snap.PlugInfo
+	plug     *interfaces.ConnectedPlug
 }
 
 var _ = Suite(&GpgKeysInterfaceSuite{
 	iface: builtin.MustInterface("gpg-keys"),
 })
 
-func (s *GpgKeysInterfaceSuite) SetUpTest(c *C) {
-	const mockPlugSnapInfoYaml = `name: other
-version: 1.0
+const gpgKeysConsumerYaml = `name: consumer
 apps:
  app:
-  command: foo
-  plugs: [gpg-keys]
+   plugs: [gpg-keys]
+   `
+
+const gpgKeysCoreYaml = `name: core
+type: os
+slots:
+  gpg-keys:
 `
-	s.slot = &interfaces.Slot{
-		SlotInfo: &snap.SlotInfo{
-			Snap:      &snap.Info{SuggestedName: "core", Type: snap.TypeOS},
-			Name:      "gpg-keys",
-			Interface: "gpg-keys",
-		},
-	}
-	snapInfo := snaptest.MockInfo(c, mockPlugSnapInfoYaml, nil)
-	s.plug = &interfaces.Plug{PlugInfo: snapInfo.Plugs["gpg-keys"]}
+
+func (s *GpgKeysInterfaceSuite) SetUpTest(c *C) {
+	s.plug, s.plugInfo = MockConnectedPlug(c, gpgKeysConsumerYaml, nil, "gpg-keys")
+	s.slot, s.slotInfo = MockConnectedSlot(c, gpgKeysCoreYaml, nil, "gpg-keys")
 }
 
 func (s *GpgKeysInterfaceSuite) TestName(c *C) {
@@ -64,26 +63,38 @@ func (s *GpgKeysInterfaceSuite) TestName(c *C) {
 }
 
 func (s *GpgKeysInterfaceSuite) TestSanitizeSlot(c *C) {
-	c.Assert(s.slot.Sanitize(s.iface), IsNil)
-	slot := &interfaces.Slot{SlotInfo: &snap.SlotInfo{
+	c.Assert(interfaces.SanitizeSlot(s.iface, s.slotInfo), IsNil)
+	slotInfo := &snap.SlotInfo{
 		Snap:      &snap.Info{SuggestedName: "some-snap"},
 		Name:      "gpg-keys",
 		Interface: "gpg-keys",
-	}}
-	c.Assert(slot.Sanitize(s.iface), ErrorMatches, "gpg-keys slots are reserved for the core snap")
+	}
+	c.Assert(interfaces.SanitizeSlot(s.iface, slotInfo), ErrorMatches,
+		"gpg-keys slots are reserved for the core snap")
 }
 
 func (s *GpgKeysInterfaceSuite) TestSanitizePlug(c *C) {
-	c.Assert(s.plug.Sanitize(s.iface), IsNil)
+	c.Assert(interfaces.SanitizePlug(s.iface, s.plugInfo), IsNil)
 }
 
-func (s *GpgKeysInterfaceSuite) TestUsedSecuritySystems(c *C) {
-	// connected plugs have a non-nil security snippet for apparmor
-	apparmorSpec := &apparmor.Specification{}
-	err := apparmorSpec.AddConnectedPlug(s.iface, s.plug, nil, s.slot, nil)
-	c.Assert(err, IsNil)
-	c.Assert(apparmorSpec.SecurityTags(), DeepEquals, []string{"snap.other.app"})
-	c.Assert(apparmorSpec.SnippetForTag("snap.other.app"), testutil.Contains, "owner @{HOME}/.gnupg/{,**} r,")
+func (s *GpgKeysInterfaceSuite) TestAppArmorSpec(c *C) {
+	spec := &apparmor.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, `owner @{HOME}/.gnupg/{,**} r,`)
+}
+
+func (s *GpgKeysInterfaceSuite) TestStaticInfo(c *C) {
+	si := interfaces.StaticInfoOf(s.iface)
+	c.Assert(si.ImplicitOnCore, Equals, true)
+	c.Assert(si.ImplicitOnClassic, Equals, true)
+	c.Assert(si.Summary, Equals, `allows reading gpg user configuration and keys`)
+	c.Assert(si.BaseDeclarationSlots, testutil.Contains, "gpg-keys")
+}
+
+func (s *GpgKeysInterfaceSuite) TestAutoConnect(c *C) {
+	// FIXME: fix AutoConnect
+	c.Assert(s.iface.AutoConnect(&interfaces.Plug{PlugInfo: s.plugInfo}, &interfaces.Slot{SlotInfo: s.slotInfo}), Equals, true)
 }
 
 func (s *GpgKeysInterfaceSuite) TestInterfaces(c *C) {
