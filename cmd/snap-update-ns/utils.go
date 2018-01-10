98b7605001d2b4aa005536cd7a1edb5da55c2917
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-01-10 11:47:08 +0100

    cmd/snap-update-ns: refactor and improve Change.Perform to handle EROFS
    This patch makes the Change.Perform function correctly handle a number
    of elements, collectively being able to pierce writable holes in read
    only filesystems. This builds upon the prior work on planning and
    construction of a writable mimic that uses tmpfs and a bind mount/symlink
    farm to reconstruct the content of the original directory.
    
    The details are interesting: existing directories and files are
    bind mounted onto empty directories / files specifically created for
    this purpose. For simplicity files are annotated with "x-snapd.kind=file"
    mount option. Directories remain the default for backwards compatibility
    and thus don't use a new mount option. Symlinks are different as they
    need to be directly re-created as actual symlinks (symlinks do not
    participate mounts directly).
    
    As a part of the refactor the ensureMountPoint function was discarded
    and inlined into changePerform. This was necessary as there are slightly
    different responsibilities necessary for each of the cases that are
    also compounded by the need to retry after constructing writable space.
    
    This patch is augmented with a separate large refactor of unit tests,
    for readability and clarity but to ensure that this is not breaking
    anything I will propose it separately, showing that existing unit tests
    don't break.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-update-ns/utils.go
+++ cmd/snap-update-ns/utils.go
@@ -458,26 +458,14 @@ func execWritableMimic(plan []*Change) ([]*Change, error) {
 	return undoChanges, nil
 }
 
-func ensureMountPoint(path string, mode os.FileMode, uid sys.UserID, gid sys.GroupID) error {
-	// If the mount point is not present then create a directory in its
-	// place.  This is very naive, doesn't handle read-only file systems
-	// but it is a good starting point for people working with things like
-	// $SNAP_DATA/subdirectory.
-	//
-	// We use lstat to ensure that we don't follow the symlink in case one
-	// was set up by the snap. Note that at the time this is run, all the
-	// snap's processes are frozen.
-	fi, err := osLstat(path)
-	switch {
-	case err != nil && os.IsNotExist(err):
-		return secureMkdirAll(path, mode, uid, gid)
-	case err != nil:
-		return fmt.Errorf("cannot inspect %q: %v", path, err)
-	case err == nil:
-		// Ensure that mount point is a directory.
-		if !fi.IsDir() {
-			return fmt.Errorf("cannot use %q for mounting, not a directory", path)
-		}
+func createWritableMimic(dir string) ([]*Change, error) {
+	plan, err := planWritableMimic(dir)
+	if err != nil {
+		return nil, err
 	}
-	return nil
+	changes, err := execWritableMimic(plan)
+	if err != nil {
+		return nil, err
+	}
+	return changes, nil
 }
