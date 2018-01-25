1061f7dc0828de6a60cce06718b9eb7144a7e407
Michael Vogt <mvo@ubuntu.com>
2018-01-25 22:08:43 +0100

    address review feedback

--- cmd/snap/cmd_run.go
+++ cmd/snap/cmd_run.go
@@ -482,8 +482,10 @@ func runCmdUnderStrace(origCmd, env []string) error {
 	if err != nil {
 		return err
 	}
-	filterDone := make(chan int)
+	filterDone := make(chan bool, 1)
 	go func() {
+		defer func() { filterDone <- true }()
+
 		r := bufio.NewReader(stderr)
 
 		// the first thing from strace if things work is
@@ -528,7 +530,6 @@ func runCmdUnderStrace(origCmd, env []string) error {
 			}
 		}
 		io.Copy(Stderr, r)
-		filterDone <- 1
 	}()
 	if err := gcmd.Start(); err != nil {
 		return err
