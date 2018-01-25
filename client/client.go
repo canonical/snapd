8a85e8036998055fd6f3be8a55f8d98a906d94e0
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-01-25 12:22:15 +0100

    client: reshuffle Timer and Schedule fields in RefreshInfo
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- client/client.go
+++ client/client.go
@@ -397,14 +397,14 @@ type OSRelease struct {
 
 // RefreshInfo contains information about refreshes.
 type RefreshInfo struct {
+	// Timer contains the refresh.timer setting. The field is omitted when
+	// the new setting is not used.
+	Timer string `json:"timer,omitempty"`
 	// Schedule contains the legacy refresh.schedule setting. The field is
 	// omitted when the legacy setting is not used.
 	Schedule string `json:"schedule,omitempty"`
 	Last     string `json:"last,omitempty"`
 	Next     string `json:"next,omitempty"`
-	// Timer contains the refresh.timer setting. The field is omitted when
-	// the new setting is not used.
-	Timer string `json:"timer,omitempty"`
 }
 
 // SysInfo holds system information
