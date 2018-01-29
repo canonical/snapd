bd774c33f4719d67ef6a33393cc4e465c4f5daa5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-01-29 22:49:05 +0100

    cmd/snap-update-ns: restore old behavior
    This will make the release manager happy as we have less unknowns to
    deal with. We can polish this by removing that fragment post release.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/mount-support.c
+++ cmd/snap-confine/mount-support.c
@@ -709,11 +709,12 @@ void sc_ensure_shared_snap_mount(void)
 	    && !is_mounted_with_shared_option(SNAP_MOUNT_DIR)) {
 		// TODO: We could be more aggressive and refuse to function but since
 		// we have no data on actual environments that happen to limp along in
-		// this configuration let's start with printing a simple warning. This
-		// should be removed once we have a measurement and feedback mechanism
-		// that lets us decide based on measurable data.
-		fprintf(stderr,
-			"the directory %s is not mounted with shared option\n",
-			SNAP_MOUNT_DIR);
+		// this configuration let's not do that yet.  This code should be
+		// removed once we have a measurement and feedback mechanism that lets
+		// us decide based on measurable data.
+		sc_do_mount(SNAP_MOUNT_DIR, SNAP_MOUNT_DIR, "none",
+			    MS_BIND | MS_REC, 0);
+		sc_do_mount("none", SNAP_MOUNT_DIR, NULL, MS_SHARED | MS_REC,
+			    NULL);
 	}
 }
