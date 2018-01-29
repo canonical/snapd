25882d1e5a4c620302c6c64ce69a241066ed6523
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-01-29 12:37:21 +0100

    many: fix removal of snaps inside LXD
    On LXD the root filesystem is not mounted with sharing of mount events
    so we don't get propagation of mount and unmount events, as snaps get
    added and removed, into persistent mount namespaces.
    
    To work around this ship a snap.mount systemd unit that creates a
    bind-mount at /snap and makes it shared. The unit is only active inside
    LXD which is a high-priority target.
    
    We may want to expand on this to other containers supported in the same
    way but this will be in another patch. One of the existing tests that
    checked for the sharing is now removed as it artificially broke what we
    were doing, instead a new test, explicitly using LXD is added, that
    doesn't do any magic manipulation and should be representative of
    real-world use.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/mount-support.c
+++ cmd/snap-confine/mount-support.c
@@ -707,9 +707,7 @@ void sc_ensure_shared_snap_mount(void)
 {
 	if (!is_mounted_with_shared_option("/")
 	    && !is_mounted_with_shared_option(SNAP_MOUNT_DIR)) {
-		sc_do_mount(SNAP_MOUNT_DIR, SNAP_MOUNT_DIR, "none",
-			    MS_BIND | MS_REC, 0);
-		sc_do_mount("none", SNAP_MOUNT_DIR, NULL, MS_SHARED | MS_REC,
-			    NULL);
+		die("cannot proceed, %s is not mounted with shared option",
+		    SNAP_MOUNT_DIR);
 	}
 }
