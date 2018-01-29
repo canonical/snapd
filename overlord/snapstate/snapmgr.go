c1fd51180356815c1339592378d4d01040e46223
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-01-29 10:21:52 +0100

    overlord/snapstate: fix typo in the comments
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/snapstate/snapmgr.go
+++ overlord/snapstate/snapmgr.go
@@ -366,7 +366,7 @@ func (m *SnapManager) LastRefresh() (time.Time, error) {
 	return m.autoRefresh.LastRefresh()
 }
 
-// RefreshSchedule returns the current refresh schedule as a string suitable to
+// RefreshSchedule returns the current refresh schedule as a string suitable for
 // display to a user and a flag indicating whether the schedule is a legacy one.
 // The caller should be holding the state lock.
 func (m *SnapManager) RefreshSchedule() (string, bool, error) {
