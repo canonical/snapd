7038375a52ed45a6ff974ee400d5640cc29e2437
Michael Vogt <mvo@ubuntu.com>
2018-01-30 15:35:52 +0100

    address review feedback from Jamie (thanks)

--- interfaces/system_key_test.go
+++ interfaces/system_key_test.go
@@ -20,17 +20,55 @@
 package interfaces_test
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	. "gopkg.in/check.v1"
 
+	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/osutil"
 )
 
-type systemKeySuite struct{}
+type systemKeySuite struct {
+	tmp              string
+	apparmorFeatures string
+	buildID          string
+}
 
 var _ = Suite(&systemKeySuite{})
 
-func (ts *systemKeySuite) TestInterfaceDigest(c *C) {
+func (s *systemKeySuite) SetUpTest(c *C) {
+	s.tmp = c.MkDir()
+	dirs.SetRootDir(s.tmp)
+	s.apparmorFeatures = filepath.Join(s.tmp, "/sys/kernel/security/apparmor/features")
+
+	id, err := osutil.MyBuildID()
+	c.Assert(err, IsNil)
+	s.buildID = id
+}
+
+func (s *systemKeySuite) TearDownTest(c *C) {
+	dirs.SetRootDir("/")
+}
+
+func (s *systemKeySuite) TestInterfaceSystemKeyEmpty(c *C) {
 	systemKey := interfaces.SystemKey()
-	c.Check(systemKey, Matches, "(?sm)^build-id: (unknown|[a-z0-9]+)$")
-	c.Check(systemKey, Matches, "(?sm).*apparmor-features:")
+	c.Check(systemKey, Equals, fmt.Sprintf(`build-id: %s
+apparmor-features: []
+`, s.buildID))
+}
+
+func (s *systemKeySuite) TestInterfaceSystemKey(c *C) {
+	err := os.MkdirAll(s.apparmorFeatures, 0755)
+	c.Assert(err, IsNil)
+	err = os.MkdirAll(filepath.Join(s.apparmorFeatures, "policy"), 0755)
+	c.Assert(err, IsNil)
+
+	systemKey := interfaces.SystemKey()
+	c.Check(systemKey, Equals, fmt.Sprintf(`build-id: %s
+apparmor-features:
+- policy
+`, s.buildID))
 }
