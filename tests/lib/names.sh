d4b42f5e6d1a9b11a9e33b3162830694a0b2a058
Paul Larson <paul.larson@canonical.com>
2018-01-30 13:47:16 -0600

    Only match kernel/gadget snap type at end of line in snap list

--- tests/lib/names.sh
+++ tests/lib/names.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gadget_name=$(snap list | grep gadget | awk '{ print $1 }')
-kernel_name=$(snap list | grep kernel | awk '{ print $1 }')
+gadget_name=$(snap list | grep 'gadget$' | awk '{ print $1 }')
+kernel_name=$(snap list | grep 'kernel$' | awk '{ print $1 }')
