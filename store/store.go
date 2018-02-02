5298d136b5d0435c016a0c419d52300254302870
Samuele Pedroni <pedronis@lucediurna.net>
2018-02-02 15:07:12 +0100

    send Snap-CDN header with none or with cloud instance placement info as needed

--- store/store.go
+++ store/store.go
@@ -899,7 +899,36 @@ func (s *Store) newRequest(reqOptions *requestOptions, user *auth.UserState) (*h
 	req.Header.Set("X-Ubuntu-Series", s.series)
 	req.Header.Set("X-Ubuntu-Classic", strconv.FormatBool(release.OnClassic))
 	req.Header.Set("X-Ubuntu-Wire-Protocol", UbuntuCoreWireProtocol)
+	// still send this for now
 	req.Header.Set("X-Ubuntu-No-CDN", strconv.FormatBool(s.noCDN))
+	if s.noCDN {
+		req.Header.Set("Snap-CDN", "none")
+	} else {
+		if s.authContext != nil {
+			// set Snap-CDN from cloud instance information
+			// if available
+			// TODO: do we want a more complex retry strategy
+			// where we first to send this header and if the
+			// operation fails that way to even get the connection
+			// then we retry without sending this?
+			cloudInfo, err := s.authContext.CloudInfo()
+			if err != nil {
+				return nil, err
+			}
+
+			if cloudInfo != nil {
+				cdnParams := []string{fmt.Sprintf("cloud-name=%q", cloudInfo.Name)}
+				if cloudInfo.Region != "" {
+					cdnParams = append(cdnParams, fmt.Sprintf("region=%q", cloudInfo.Region))
+				}
+				if cloudInfo.AvailabilityZone != "" {
+					cdnParams = append(cdnParams, fmt.Sprintf("availability-zone=%q", cloudInfo.AvailabilityZone))
+				}
+
+				req.Header.Set("Snap-CDN", strings.Join(cdnParams, " "))
+			}
+		}
+	}
 
 	if reqOptions.ContentType != "" {
 		req.Header.Set("Content-Type", reqOptions.ContentType)
