723b8a5aeef064e3be96f3b13f1930f3f5da305a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-02-08 19:27:06 +0100

    many: rename InfoEntry to MountInfoEntry
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- osutil/mountinfo_test.go
+++ osutil/mountinfo_test.go
@@ -34,8 +34,8 @@ type mountinfoSuite struct{}
 var _ = Suite(&mountinfoSuite{})
 
 // Check that parsing the example from kernel documentation works correctly.
-func (s *mountinfoSuite) TestParseInfoEntry1(c *C) {
-	entry, err := osutil.ParseInfoEntry(
+func (s *mountinfoSuite) TestParseMountInfoEntry1(c *C) {
+	entry, err := osutil.ParseMountInfoEntry(
 		"36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue")
 	c.Assert(err, IsNil)
 	c.Assert(entry.MountID, Equals, 36)
@@ -52,23 +52,23 @@ func (s *mountinfoSuite) TestParseInfoEntry1(c *C) {
 }
 
 // Check that various combinations of optional fields are parsed correctly.
-func (s *mountinfoSuite) TestParseInfoEntry2(c *C) {
+func (s *mountinfoSuite) TestParseMountInfoEntry2(c *C) {
 	// No optional fields.
-	entry, err := osutil.ParseInfoEntry(
+	entry, err := osutil.ParseMountInfoEntry(
 		"36 35 98:0 /mnt1 /mnt2 rw,noatime - ext3 /dev/root rw,errors=continue")
 	c.Assert(err, IsNil)
 	c.Assert(entry.MountOptions, DeepEquals, map[string]string{"rw": "", "noatime": ""})
 	c.Assert(entry.OptionalFields, HasLen, 0)
 	c.Assert(entry.FsType, Equals, "ext3")
 	// One optional field.
-	entry, err = osutil.ParseInfoEntry(
+	entry, err = osutil.ParseMountInfoEntry(
 		"36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue")
 	c.Assert(err, IsNil)
 	c.Assert(entry.MountOptions, DeepEquals, map[string]string{"rw": "", "noatime": ""})
 	c.Assert(entry.OptionalFields, DeepEquals, []string{"master:1"})
 	c.Assert(entry.FsType, Equals, "ext3")
 	// Two optional fields.
-	entry, err = osutil.ParseInfoEntry(
+	entry, err = osutil.ParseMountInfoEntry(
 		"36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 slave:2 - ext3 /dev/root rw,errors=continue")
 	c.Assert(err, IsNil)
 	c.Assert(entry.MountOptions, DeepEquals, map[string]string{"rw": "", "noatime": ""})
@@ -77,8 +77,8 @@ func (s *mountinfoSuite) TestParseInfoEntry2(c *C) {
 }
 
 // Check that white-space is unescaped correctly.
-func (s *mountinfoSuite) TestParseInfoEntry3(c *C) {
-	entry, err := osutil.ParseInfoEntry(
+func (s *mountinfoSuite) TestParseMountInfoEntry3(c *C) {
+	entry, err := osutil.ParseMountInfoEntry(
 		`36 35 98:0 /mnt\0401 /mnt\0402 rw\040,noatime mas\040ter:1 - ext\0403 /dev/ro\040ot rw\040,errors=continue`)
 	c.Assert(err, IsNil)
 	c.Assert(entry.MountID, Equals, 36)
@@ -96,29 +96,29 @@ func (s *mountinfoSuite) TestParseInfoEntry3(c *C) {
 }
 
 // Check that various malformed entries are detected.
-func (s *mountinfoSuite) TestParseInfoEntry4(c *C) {
+func (s *mountinfoSuite) TestParseMountInfoEntry4(c *C) {
 	var err error
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, "incorrect number of tail fields, expected 3 but found 4")
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root")
 	c.Assert(err, ErrorMatches, "incorrect number of tail fields, expected 3 but found 2")
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3")
 	c.Assert(err, ErrorMatches, "incorrect number of fields, expected at least 10 but found 9")
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 -")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 -")
 	c.Assert(err, ErrorMatches, "incorrect number of fields, expected at least 10 but found 8")
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1")
 	c.Assert(err, ErrorMatches, "incorrect number of fields, expected at least 10 but found 7")
-	_, err = osutil.ParseInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 garbage1 garbage2 garbage3")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 garbage1 garbage2 garbage3")
 	c.Assert(err, ErrorMatches, "list of optional fields is not terminated properly")
-	_, err = osutil.ParseInfoEntry("foo 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("foo 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, `cannot parse mount ID: "foo"`)
-	_, err = osutil.ParseInfoEntry("36 bar 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("36 bar 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, `cannot parse parent mount ID: "bar"`)
-	_, err = osutil.ParseInfoEntry("36 35 froz:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("36 35 froz:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, `cannot parse device major number: "froz"`)
-	_, err = osutil.ParseInfoEntry("36 35 98:bot /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("36 35 98:bot /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, `cannot parse device minor number: "bot"`)
-	_, err = osutil.ParseInfoEntry("36 35 corrupt /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
+	_, err = osutil.ParseMountInfoEntry("36 35 corrupt /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue foo")
 	c.Assert(err, ErrorMatches, `cannot parse device major:minor number pair: "corrupt"`)
 }
 
