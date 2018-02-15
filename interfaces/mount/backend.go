32c58af4dcc6b50a869b9610171a7ac32a1d14c4
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-02-15 10:55:00 +0100

    interfaces/mount: de-duplicate code to a helper
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/mount/backend.go
+++ interfaces/mount/backend.go
@@ -90,38 +90,30 @@ func (b *Backend) Remove(snapName string) error {
 	return nil
 }
 
+// addMountProfile adds a mount profile with the given name, based on the given entries.
+//
+// If there are no entries no profile is generated.
+func addMountProfile(content map[string]*osutil.FileState, fname string, entries []osutil.MountEntry) {
+	if len(entries) == 0 {
+		return
+	}
+	var buffer bytes.Buffer
+	for _, entry := range entries {
+		fmt.Fprintf(&buffer, "%s\n", entry)
+	}
+	content[fname] = &osutil.FileState{Content: buffer.Bytes(), Mode: 0644}
+}
+
 // deriveContent computes .fstab tables based on requests made to the specification.
 func deriveContent(spec *Specification, snapInfo *snap.Info) map[string]*osutil.FileState {
 	content := make(map[string]*osutil.FileState, 2)
-	mountEntries := spec.MountEntries()
-	userMountEntries := spec.UserMountEntries()
 	snapName := snapInfo.Name()
-
-	if len(mountEntries) != 0 {
-		// Compute the contents of the fstab file. It should
-		// contain all the mount rules collected by the
-		// backend controller.
-		var buffer bytes.Buffer
-		for _, entry := range mountEntries {
-			fmt.Fprintf(&buffer, "%s\n", entry)
-		}
-		fstate := &osutil.FileState{Content: buffer.Bytes(), Mode: 0644}
-		// Add the per-snap fstab file. This file is read by
-		// snap-update-ns in the global pass.
-		content[fmt.Sprintf("snap.%s.fstab", snapName)] = fstate
-	}
-
-	if len(userMountEntries) != 0 {
-		// Compute the contents of the user-fstab file.
-		var buffer bytes.Buffer
-		for _, entry := range userMountEntries {
-			fmt.Fprintf(&buffer, "%s\n", entry)
-		}
-		fstate := &osutil.FileState{Content: buffer.Bytes(), Mode: 0644}
-		// Add the per-snap user-fstab file. This file will be
-		// read by snap-update-ns in the per-user pass.
-		content[fmt.Sprintf("snap.%s.user-fstab", snapName)] = fstate
-	}
+	// Add the per-snap fstab file.
+	// This file is read by snap-update-ns in the global pass.
+	addMountProfile(content, fmt.Sprintf("snap.%s.fstab", snapName), spec.MountEntries())
+	// Add the per-snap user-fstab file.
+	// This file will be read by snap-update-ns in the per-user pass.
+	addMountProfile(content, fmt.Sprintf("snap.%s.user-fstab", snapName), spec.UserMountEntries())
 	return content
 }
 
