ee035c914bae4c449218e629ad4d9b4b33cd1eb1
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-02-16 12:36:21 +0100

    cmd/snap-confine: make sc_open_snap_update_ns public
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/mount-support.h
+++ cmd/snap-confine/mount-support.h
@@ -19,6 +19,15 @@
 #define SNAP_MOUNT_SUPPORT_H
 
 /**
+ * Return a file descriptor referencing the snap-update-ns utility
+ *
+ * By calling this prior to changing the mount namespace, it is
+ * possible to execute the utility even if a different version is now
+ * mounted at the expected location.
+ **/
+int sc_open_snap_update_ns(void);
+
+/**
  * Assuming a new mountspace, populate it accordingly.
  *
  * This function performs many internal tasks:
