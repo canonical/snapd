eed330c1c89447d3973c97ed354b67d3871fd4d7
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-02-20 10:29:04 +0100

    overlord/snapstate: use spread in the default refresh schedule
    Enable spreading in the default refresh schedule so that refresh requests are
    not hitting the store at the same (or close) time.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/snapstate/snapstate_test.go
+++ overlord/snapstate/snapstate_test.go
@@ -5471,7 +5471,7 @@ func (s *snapmgrTestSuite) TestEnsureRefreshRefusesLegacyWeekdaySchedules(c *C) 
 	c.Check(logbuf.String(), testutil.Contains, `cannot use refresh.schedule configuration: cannot parse "mon@12:00": not a valid time`)
 	schedule, legacy, err := s.snapmgr.RefreshSchedule()
 	c.Assert(err, IsNil)
-	c.Check(schedule, Equals, "00:00-24:00/4")
+	c.Check(schedule, Equals, "00:00~24:00/4")
 	c.Check(legacy, Equals, false)
 
 	tr = config.NewTransaction(s.state)
@@ -5551,7 +5551,7 @@ func (s *snapmgrTestSuite) TestEnsureRefreshFallbackToDefaultOnError(c *C) {
 	// cannot be parsed
 	schedule, legacy, err := s.snapmgr.RefreshSchedule()
 	c.Assert(err, IsNil)
-	c.Check(schedule, Equals, "00:00-24:00/4")
+	c.Check(schedule, Equals, "00:00~24:00/4")
 	c.Check(legacy, Equals, false)
 
 	tr = config.NewTransaction(s.state)
@@ -5582,7 +5582,7 @@ func (s *snapmgrTestSuite) TestEnsureRefreshFallbackOnEmptyToDefaultSchedule(c *
 	// refresh.schedule was set
 	schedule, legacy, err := s.snapmgr.RefreshSchedule()
 	c.Assert(err, IsNil)
-	c.Check(schedule, Equals, "00:00-24:00/4")
+	c.Check(schedule, Equals, "00:00~24:00/4")
 	c.Check(legacy, Equals, false)
 
 	tr = config.NewTransaction(s.state)
