c26ae5419325906fda21a240d306f5b57699881a
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-02-27 13:10:43 +0100

    cmd/snap: simple autostart for `snap userd`
    Add autostart mode to `snap userd` helper, that can be invoked by running `snap
    userd --autostart`. In this mode `userd` will attempt to autostart user
    applications, by listing the desktop files in ~/snap/*/current/.config/autostart
    across all snaps.
    
    We use a simple heuristic, the autostart application desktop file must be named
    the same as the snap app declared in snap.yaml. The application is started using
    the command from a desktop file that was autogenerated when the snap was
    installed.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/snap/cmd_userd.go
+++ cmd/snap/cmd_userd.go
@@ -20,19 +20,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"os/signal"
+	"os/user"
+	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/jessevdk/go-flags"
 
+	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/i18n"
+	"github.com/snapcore/snapd/logger"
+	"github.com/snapcore/snapd/snap"
 	"github.com/snapcore/snapd/userd"
 )
 
 type cmdUserd struct {
 	userd userd.Userd
+
+	Autostart bool `long:"autostart"`
 }
 
 var shortUserdHelp = i18n.G("Start the userd service")
@@ -44,16 +54,19 @@ func init() {
 		longUserdHelp,
 		func() flags.Commander {
 			return &cmdUserd{}
-		},
-		nil,
-		[]argDesc{},
-	)
+		}, map[string]string{
+			"autostart": i18n.G("Autostart user applications"),
+		}, nil)
 	cmd.hidden = true
 }
 
 func (x *cmdUserd) Execute(args []string) error {
 	if len(args) > 0 {
 		return ErrExtraArgs
+	}
+
+	if x.Autostart {
+		return x.runAutostart()
 	}
 
 	if err := x.userd.Init(); err != nil {
@@ -71,4 +84,84 @@ func (x *cmdUserd) Execute(args []string) error {
 	}
 
 	return x.userd.Stop()
+}
+
+var (
+	replacedDesktopKeys = []string{"%f", "%F", "%u", "%U", "%d", "%D",
+		"%n", "%N", "%i", "%c", "%k", "%v", "%m"}
+)
+
+func findExec(desktopFilePath string) (string, error) {
+	f, err := os.Open(desktopFilePath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.HasPrefix(line, "Exec=") {
+			continue
+		}
+
+		execCmd := strings.Split(line, "Exec=")[1]
+		for _, key := range replacedDesktopKeys {
+			execCmd = strings.Replace(execCmd, key, "", -1)
+		}
+		return execCmd, nil
+	}
+	return "", nil
+}
+
+func (x *cmdUserd) runAutostart() error {
+	logger.Debugf("autostart")
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	usrSnapDir := filepath.Join(usr.HomeDir, "snap")
+
+	glob := filepath.Join(usrSnapDir, "*/current/.config/autostart/*.desktop")
+	matches, err := filepath.Glob(glob)
+	if err != nil {
+		return err
+	}
+
+	for _, desktopFile := range matches {
+		logger.Debugf("autostart desktop file %v", desktopFile)
+		// /home/foo/snap/some-snap/current/.config/autostart/some-app.desktop ->
+		//    some-snap/current/.config/autostart/some-app.desktop
+		noPrefix := strings.TrimPrefix(desktopFile, usrSnapDir+"/")
+		// some-snap/current/.config/autostart/some-app.desktop -> some-snap
+		snapName := noPrefix[0:strings.IndexByte(noPrefix, '/')]
+		logger.Debugf("snap name: %v", snapName)
+		info, err := getSnapInfo(snapName, snap.R(0))
+		if err != nil {
+			logger.Noticef("failed to obtain snap information for snap %q: %v", snapName, err)
+			continue
+		}
+		// use the sanitized desktop file
+		snapAppDesktop := filepath.Join(dirs.SnapDesktopFilesDir,
+			fmt.Sprintf("%s_%s", info.Name(), filepath.Base(desktopFile)))
+		command, err := findExec(snapAppDesktop)
+		if err != nil {
+			logger.Noticef("failed to locate app command for %v: %v", filepath.Base(desktopFile), err)
+			continue
+		}
+		if command == "" {
+			logger.Noticef("Exec= line not found in desktop file %v", snapAppDesktop)
+			continue
+		}
+		logger.Debugf("exec line: %v", command)
+
+		// TODO: shlex
+		split := strings.Split(command, " ")
+		cmd := exec.Command(split[0], split[1:]...)
+		if err := cmd.Start(); err != nil {
+			logger.Noticef("failed to start %q: %v", command, err)
+		}
+	}
+	return nil
 }
