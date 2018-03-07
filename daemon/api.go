54180c10ae2b30de9e75cfac086404b44d86011d
Pawel Stolowski <stolowski@gmail.com>
2018-03-07 16:39:16 +0100

    Don't create an empty Change with "Hold" state if there are no connections found when using the abbreviated form of 'snap disconnect'.

--- daemon/api.go
+++ daemon/api.go
@@ -1846,7 +1846,7 @@ func changeInterfaces(c *Command, r *http.Request, user *auth.UserState) Respons
 		repo := c.d.overlord.InterfaceManager().Repository()
 		summary = fmt.Sprintf("Disconnect %s:%s from %s:%s", a.Plugs[0].Snap, a.Plugs[0].Name, a.Slots[0].Snap, a.Slots[0].Name)
 		conns, err = repo.ResolveDisconnect(a.Plugs[0].Snap, a.Plugs[0].Name, a.Slots[0].Snap, a.Slots[0].Name)
-		if err == nil {
+		if err == nil && len(conns) > 0 {
 			for _, connRef := range conns {
 				var ts *state.TaskSet
 				ts, err = ifacestate.Disconnect(st, connRef.PlugRef.Snap, connRef.PlugRef.Name, connRef.SlotRef.Snap, connRef.SlotRef.Name)
@@ -1864,6 +1864,11 @@ func changeInterfaces(c *Command, r *http.Request, user *auth.UserState) Respons
 	}
 
 	change := newChange(st, a.Action+"-snap", summary, tasksets, affected)
+	// Special case for disconnect: if no connections exit, we don't want to error out and just report Done.
+	// Note that for every other case (invalid snap/plug/slot names ResolveDisconnect will report a clear error that we report above)
+	if len(tasksets) == 0 {
+		change.SetStatus(state.DoneStatus)
+	}
 
 	st.EnsureBefore(0)
 
