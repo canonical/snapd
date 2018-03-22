4587e818262408ff47da17fc62455ee85ca885ba
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-03-22 09:33:13 +0100

    tests: move creation of test:test user to prepare-restore.d
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -51,40 +51,6 @@ do_phase() {
 }
 
 
-create_test_user(){
-   if ! id test >& /dev/null; then
-        quiet groupadd --gid 12345 test
-        case "$SPREAD_SYSTEM" in
-            ubuntu-*)
-                # manually setting the UID and GID to 12345 because we need to
-                # know the numbers match for when we set up the user inside
-                # the all-snap, which has its own user & group database.
-                # Nothing special about 12345 beyond it being high enough it's
-                # unlikely to ever clash with anything, and easy to remember.
-                quiet adduser --uid 12345 --gid 12345 --disabled-password --gecos '' test
-                ;;
-            debian-*|fedora-*|opensuse-*)
-                quiet useradd -m --uid 12345 --gid 12345 test
-                ;;
-            *)
-                echo "ERROR: system $SPREAD_SYSTEM not yet supported!"
-                exit 1
-        esac
-    fi
-
-    owner=$( stat -c "%U:%G" /home/test )
-    if [ "$owner" != "test:test" ]; then
-        echo "expected /home/test to be test:test but it's $owner"
-        exit 1
-    fi
-    unset owner
-
-    echo 'test ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
-    chown test.test -R "$SPREAD_PATH"
-    chown test.test "$SPREAD_PATH/../"
-}
-
 build_deb(){
     # Use fake version to ensure we are always bigger than anything else
     dch --newversion "1337.$(dpkg-parsechangelog --show-field Version)" "testing build"
@@ -206,8 +172,6 @@ prepare_project() {
         chown test.test -R "$PROJECT_PATH"
         exit 0
     fi
-
-    create_test_user
 
     distro_update_package_db
 
