076d850f00e429ef57f6b0f79f41c86944118b22
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-03-22 12:04:12 +0100

    tests: move prepare-restore tarball magic to prepare-restore.d
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -80,26 +80,10 @@ prepare_project_each() {
 
 prepare_suite() {
     do_phase prepare_suite
-
-    # shellcheck source=tests/lib/prepare.sh
-    . "$TESTSLIB"/prepare.sh
-    if [[ "$SPREAD_SYSTEM" == ubuntu-core-16-* ]]; then
-        prepare_all_snap
-    else
-        prepare_classic
-    fi
 }
 
 prepare_suite_each() {
     do_phase prepare_suite_each
-
-    # shellcheck source=tests/lib/reset.sh
-    "$TESTSLIB"/reset.sh --reuse-core
-    # shellcheck source=tests/lib/prepare.sh
-    . "$TESTSLIB"/prepare.sh
-    if [[ "$SPREAD_SYSTEM" != ubuntu-core-16-* ]]; then
-        prepare_each_classic
-    fi
 }
 
 restore_suite_each() {
@@ -116,18 +100,6 @@ restore_project_each() {
 
 restore_project() {
     do_phase restore_project
-
-    # We use a trick to accelerate prepare/restore code in certain suites. That
-    # code uses a tarball to store the vanilla state. Here we just remove this
-    # tarball.
-    rm -f "$SPREAD_PATH/snapd-state.tar.gz"
-
-    # Remove all of the code we pushed and any build results. This removes
-    # stale files and we cannot do incremental builds anyway so there's little
-    # point in keeping them.
-    if [ -n "$GOPATH" ]; then
-        rm -rf "${GOPATH%%:*}"
-    fi
 }
 
 case "$1" in
