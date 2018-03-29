6b0ea9b9ee6ba444a5c8a0492cbf6c4047635dda
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-03-29 10:47:30 +0200

    cmd/snap-confine: make /run/media an alias of /media
    This patch makes it so that inside the execution environment, the /media
    directory always corresponds to the correct directory on the host
    (either /media or /run/media).
    
    This makes the execution environment more portable across systems, for
    snap developers the programmatic difference between /media and
    /run/media no longer exists. Snap applications can always rely on
    /media.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/mount-support.c
+++ cmd/snap-confine/mount-support.c
@@ -199,6 +199,9 @@ static void sc_setup_mount_profiles(struct sc_apparmor *apparmor,
 struct sc_mount {
 	const char *path;
 	bool is_bidirectional;
+	// Alias defines the rbind mount "alias" of path. It exists mainly for
+	// /media <=> /run/media equivalence.
+	const char *alias;
 };
 
 struct sc_mount_config {
@@ -302,6 +305,17 @@ static void sc_bootstrap_mount_namespace(const struct sc_mount_config *config)
 			// Mount events will only propagate inwards to the namespace. This
 			// way the running application cannot alter any global state apart
 			// from that of its own snap.
+			sc_do_mount("none", dst, NULL, MS_REC | MS_SLAVE, NULL);
+		}
+		if (mnt->alias == NULL) {
+			continue;
+		}
+		// An alias of mnt->path is provided at another location.
+		// It should behave exactly the same as the original.
+		sc_must_snprintf(dst, sizeof dst, "%s/%s", scratch_dir,
+				 mnt->alias);
+		sc_do_mount(mnt->path, dst, NULL, MS_REC | MS_BIND, NULL);
+		if (!mnt->is_bidirectional) {
 			sc_do_mount("none", dst, NULL, MS_REC | MS_SLAVE, NULL);
 		}
 	}
@@ -603,7 +617,7 @@ void sc_populate_mount_ns(struct sc_apparmor *apparmor, int snap_update_ns_fd,
 			{"/usr/src"},	// FIXME: move to SecurityMounts in system-trace interface
 			{"/var/log"},	// FIXME: move to SecurityMounts in log-observe interface
 #ifdef MERGED_USR
-			{"/run/media", true},	// access to the users removable devices
+			{"/run/media", true, "/media"},	// access to the users removable devices
 #else
 			{"/media", true},	// access to the users removable devices
 #endif				// MERGED_USR
