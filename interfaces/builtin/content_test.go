db9feb353b7fb4dbb512c68740f4abc87b39b416
Gerry Boland <gerboland@users.noreply.github.com>
2018-03-31 10:28:36 +0100

    interfaces/content: add rule so slot can access writable files at plug's mountpoint (#4932)
    * Content-interface: add AppArmor rule so slot can access shared directory using plug's namespace
    * Add test
    * Improve comment

--- interfaces/builtin/content_test.go
+++ interfaces/builtin/content_test.go
@@ -780,3 +780,40 @@ slots:
 `
 	c.Assert(apparmorSpec.SnippetForTag("snap.consumer.app"), Equals, expected)
 }
+
+// Check that slot can access shared directory in plug's namespace
+func (s *ContentSuite) TestSlotCanAccessConnectedPlugSharedDirectory(c *C) {
+	const consumerYaml = `name: consumer
+version: 0
+plugs:
+ content:
+  target: $SNAP_COMMON/import
+`
+	consumerInfo := snaptest.MockInfo(c, consumerYaml, &snap.SideInfo{Revision: snap.R(7)})
+	plug := interfaces.NewConnectedPlug(consumerInfo.Plugs["content"], nil)
+	const producerYaml = `name: producer
+version: 0
+slots:
+ content:
+  write:
+   - $SNAP_COMMON/export
+apps:
+  app:
+    command: bar
+`
+	producerInfo := snaptest.MockInfo(c, producerYaml, &snap.SideInfo{Revision: snap.R(5)})
+	slot := interfaces.NewConnectedSlot(producerInfo.Slots["content"], nil)
+
+	apparmorSpec := &apparmor.Specification{}
+	err := apparmorSpec.AddConnectedSlot(s.iface, plug, slot)
+	c.Assert(err, IsNil)
+	c.Assert(apparmorSpec.SecurityTags(), DeepEquals, []string{"snap.producer.app"})
+	expected := `
+# When the content interface is writable, allow this slot
+# implementation to access the slot's exported files at the plugging
+# snap's mountpoint to accommodate software where the plugging app
+# tells the slotting app about files to share.
+/var/snap/consumer/common/import/** mrwklix,
+`
+	c.Assert(apparmorSpec.SnippetForTag("snap.producer.app"), Equals, expected)
+}
