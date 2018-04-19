24f34d658992247aa725a417b6477d07ad0eaad8
James Henstridge <james@jamesh.id.au>
2018-04-19 16:36:35 +0800

    tests/main/user-mounts: move check-user-mount.sh script to separate file

---
+++ tests/main/user-mounts/check-user-mount.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+# Add some content to the runtime dir
+rm -rf $XDG_RUNTIME_DIR/snap.test-snapd-sh
+mkdir $XDG_RUNTIME_DIR/snap.test-snapd-sh
+mkdir $XDG_RUNTIME_DIR/snap.test-snapd-sh/source
+mkdir $XDG_RUNTIME_DIR/snap.test-snapd-sh/target
+touch $XDG_RUNTIME_DIR/snap.test-snapd-sh/source/in-source
+touch $XDG_RUNTIME_DIR/snap.test-snapd-sh/target/in-target
+
+# Check target directory from sandbox
+test-snapd-sh -c 'ls $XDG_RUNTIME_DIR/target'
