3671e5a03d90d1e465d5991e6f58c581486457a7
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-04-27 15:50:52 -0300

    Save and restore the snapd state avoiding copy snap files
    The change is just applied for the all_snaps version and it copy the old
    state but the snap files are copied just in case they have changed,
    otherwise the snap files are preserved in the dirs snaps and seed/snaps
    
    This change is done because in average the 60% of the time it is
    required to reset the snapd state is is used to copy the snap files from
    the original state to /var/lib/snapd/{snaps,seed/snaps}.

--- tests/lib/state.sh
+++ tests/lib/state.sh
@@ -17,8 +17,11 @@ get_boot_path() {
 
 save_all_snap_state() {
     get_boot_path
+
     mkdir -p "$SNAPD_STATE_PATH"
-    cp -rf /var/lib/snapd "$SNAPD_STATE_PATH/snapdlib"
+
+    # Copy the state preserving the timestamps
+    cp -rfp /var/lib/snapd "$SNAPD_STATE_PATH/snapdlib"
     cp -rf "$BOOT" "$SNAPD_STATE_PATH/boot"
     cp -f /etc/systemd/system/snap-*core*.mount "$SNAPD_STATE_PATH"
 }
@@ -27,8 +30,75 @@ restore_all_snap_state() {
     # we need to ensure that we also restore the boot environment
     # fully for tests that break it
     get_boot_path
-    rm -rf /var/lib/snapd/*
-    cp -rf "$SNAPD_STATE_PATH"/snapdlib/* /var/lib/snapd
+
+    mkdir -p /var/lib/snapd/snaps
+    mkdir -p /var/lib/snapd/seed /var/lib/snapd/seed/snaps
+
+    # Clean all the state but the snaps and seed dirs
+    find /var/lib/snapd/* -maxdepth 0 ! \( -name 'snaps' -o -name 'seed' \) -exec rm -rf {} +
+    clean_snaps_dir_state
+    clean_seed_dir_state
+
+    # Copy the whole state but the snaps and seed dirs
+    find "$SNAPD_STATE_PATH"/snapdlib/* -maxdepth 0 ! \( -name 'snaps' -o -name 'seed' \) -exec cp -rf {} /var/lib/snapd \;
+    sync_snaps_dir_state
+    sync_seed_dir_state
+
+    # Restore boot and mount points
     cp -rf "$SNAPD_STATE_PATH"/boot/* "$BOOT"
     cp -f "$SNAPD_STATE_PATH"/snap-*core*.mount  /etc/systemd/system
+}
+
+clean_snaps_dir_state() {
+    find /var/lib/snapd/snaps/* -maxdepth 0 ! -name '*.snap' -exec rm -rf {} +
+}
+
+clean_seed_dir_state() {
+    find /var/lib/snapd/seed/* -maxdepth 0 ! -name 'snaps' -exec rm -rf {} +
+    find /var/lib/snapd/seed/snaps/* -maxdepth 0 ! -name '*.snap' -exec rm -rf {} +
+}
+
+sync_snaps_dir_state() {
+    find "$SNAPD_STATE_PATH"/snapdlib/snaps/* -maxdepth 0 ! -name '*.snap' -exec cp -rf {} /var/lib/snapd/snaps \;
+    sync_snaps "$SNAPD_STATE_PATH"/snapdlib/snaps /var/lib/snapd/snaps
+}
+
+sync_seed_dir_state() {
+    find "$SNAPD_STATE_PATH"/snapdlib/seed/* -maxdepth 0 ! -name 'snaps' -exec cp -rf {} /var/lib/snapd/seed \;
+    find "$SNAPD_STATE_PATH"/snapdlib/seed/snaps/* -maxdepth 0 ! -name '*.snap' -exec cp -rf {} /var/lib/snapd/seed/snaps \;
+    sync_snaps "$SNAPD_STATE_PATH"/snapdlib/seed/snaps /var/lib/snapd/seed/snaps
+}
+
+sync_snaps() {
+    SOURCE=$1
+    TARGET=$2
+
+    if ! [ -d "$SOURCE" ]; then
+        echo "Source directory does not exist $SOURCE"
+        exit 1
+    elif ! [ -d "$TARGET" ]; then
+        mkdir -p "$TARGET"
+    fi
+
+    # Remove new snaps and changed ones
+    for f in "$TARGET"/*.snap; do
+        fname="$(basename $f)"
+        if ! [ -e "$TARGET/$fname" ]; then
+            break
+        elif ! [ -e "$SOURCE/$fname" ]; then
+            rm "$TARGET/$fname"
+        elif [ "$TARGET/$fname" -nt "$SOURCE/$fname" ]; then
+            cp -f "$SOURCE/$fname" "$TARGET/$fname"
+        fi
+    done
+
+    # Add deleted snaps
+    for f in "$SOURCE"/*.snap; do
+        fname="$(basename $f)"
+        if ! [ -e "$SOURCE/$fname" ]; then
+            break
+        elif ! [ -e "$TARGET/$fname" ]; then
+            cp -f "$SOURCE/$fname" "$TARGET/$fname"
+        fi
+    done
 }
