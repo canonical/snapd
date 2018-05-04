c47f248e32b88801c56d875b1ae2bfb3385e426c
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-05-04 17:04:07 -0300

    Moving boot method to boot.sh
    Also changing the method to echo the path instead of using a BOOT
    variable.

--- tests/lib/state.sh
+++ tests/lib/state.sh
@@ -1,34 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 SNAPD_STATE_PATH="$SPREAD_PATH/snapd-state"
 
-get_boot_path() {
-    BOOT=""
-    if ls /boot/uboot/*; then
-        BOOT="/boot/uboot/"
-    elif ls /boot/grub/*; then
-        BOOT="/boot/grub/"
-    else
-        echo "Cannot determine bootdir in /boot:"
-        ls /boot
-        exit 1
-    fi
-}
+# shellcheck source=tests/lib/boot.sh
+. "$TESTSLIB/boot.sh"
 
 save_all_snap_state() {
-    get_boot_path
+    local boot_path="$(get_boot_path)"
     mkdir -p "$SNAPD_STATE_PATH"
     cp -rf /var/lib/snapd "$SNAPD_STATE_PATH/snapdlib"
-    cp -rf "$BOOT" "$SNAPD_STATE_PATH/boot"
+    cp -rf "$boot_path" "$SNAPD_STATE_PATH/boot"
     cp -f /etc/systemd/system/snap-*core*.mount "$SNAPD_STATE_PATH"
 }
 
 restore_all_snap_state() {
     # we need to ensure that we also restore the boot environment
     # fully for tests that break it
-    get_boot_path
+    local boot_path="$(get_boot_path)"
     rm -rf /var/lib/snapd/*
     cp -rf "$SNAPD_STATE_PATH"/snapdlib/* /var/lib/snapd
-    cp -rf "$SNAPD_STATE_PATH"/boot/* "$BOOT"
+    cp -rf "$SNAPD_STATE_PATH"/boot/* "$boot_path"
     cp -f "$SNAPD_STATE_PATH"/snap-*core*.mount  /etc/systemd/system
 }
