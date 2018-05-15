289ab44302238dfe4194e5ef12148c9f3b6c5fd3
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-05-15 12:47:35 -0300

    Unify how cursor is detected

--- tests/lib/journalctl.sh
+++ tests/lib/journalctl.sh
@@ -3,14 +3,7 @@
 JOURNALCTL_CURSOR_FILE="${SPREAD_PATH}"/journalctl_cursor
 
 get_last_journalctl_cursor(){
-    # It is not being used journalctl json output because jq is not installed on
-    # core systems
-    if [ -z "$(which jq)" ]; then
-        cursor=$(journalctl --output=export -n1 | grep -e '__CURSOR=')
-        echo ${cursor#__CURSOR=}
-    else
-        journalctl --output=json -n1 | jq -r '.__CURSOR'
-    fi
+    journalctl --output=export -n1 | grep --binary-files=text -o '__CURSOR=.*' | sed -e 's/^__CURSOR=//'
 }
 
 start_new_journalctl_log(){
@@ -25,10 +18,11 @@ start_new_journalctl_log(){
 
 get_journalctl_log(){
     cursor=$(cat "$JOURNALCTL_CURSOR_FILE")
-    journalctl "$@" --cursor "$cursor"
+    get_journalctl_log_from_cursor "$cursor" "$@"
 }
 
 get_journalctl_log_from_cursor(){
     cursor=$1
+    shift
     journalctl "$@" --cursor "$cursor"
 }
