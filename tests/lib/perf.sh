37e9fe8d57ace8433ce694f53d70592a5588fe82
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-05-18 09:51:34 -0300

    Code refactor and simplified

--- tests/lib/perf.sh
+++ tests/lib/perf.sh
@@ -2,20 +2,32 @@
 
 set +x
 
-execution_time_ms(){
-    command="$1"
+measure_exec_time() {
+    command=$1
+    test=$2
+    scenario=$3
+
+    et=$(execution_time_ms "$command")
+    echo "$SPREAD_SYSTEM,$test,$scenario,$et,milliseconds" >> "$PERF_RESULTS_FILE"
+}
+
+execution_time_ms() {
+    command=$1
     ts=$(date +%s%N)
-    ( $command ) > /dev/null 2>&1
+    ( "$command" ) > /dev/null 2>&1
     et=$((($(date +%s%N) - $ts)/1000000))
-    echo $et
+    echo "$et"
 }
 
-create_results_file(){
-    file="$1"
-    echo "#####START-PERF#####" > $file
+create_results_file() {
+    echo "#####START-PERF#####" > "$PERF_RESULTS_FILE"
 }
 
-finish_results_file(){
-    file="$1"
-    echo "#####END-PERF#####" >> $file
+finish_results_file() {
+    echo "#####END-PERF#####" >> "$PERF_RESULTS_FILE"
+    cat "$PERF_RESULTS_FILE"
+}
+
+clean_results_file() {
+    rm -f "$PERF_RESULTS_FILE"
 }
