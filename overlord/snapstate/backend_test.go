b142e6772e1982f45cd80a6fcc59c33709cdad68
Michael Vogt <mvo@ubuntu.com>
2018-06-07 08:49:18 +0200

    snapstate: do not generate "configure" tasks for bases

--- overlord/snapstate/backend_test.go
+++ overlord/snapstate/backend_test.go
@@ -162,8 +162,11 @@ func (f *fakeStore) snapInfo(spec store.SnapSpec, user *auth.UserState) (*snap.I
 	confinement := snap.StrictConfinement
 
 	typ := snap.TypeApp
-	if spec.Name == "core" || spec.Name == "some-core" {
+	switch spec.Name {
+	case "core", "some-core":
 		typ = snap.TypeOS
+	case "some-base":
+		typ = snap.TypeBase
 	}
 
 	if spec.Name == "snap-unknown" {
