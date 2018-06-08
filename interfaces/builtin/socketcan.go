698d6c37d661e4839151a43fab4c7a8418d43b56
Jonathan Cave <jonathan.cave@canonical.com>
2018-06-08 15:28:46 +0100

    interfaces/socketcan: add non-control interface

---
+++ interfaces/builtin/socketcan.go
@@ -0,0 +1,53 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const socketCanSummary = `allows use of SocketCAN network interfaces`
+
+const socketCanBaseDeclarationSlots = `
+  socketcan:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const socketCanConnectedPlugAppArmor = `
+# Description: Can access SocketCAN networks
+network can,
+`
+
+const socketCanConnectedPlugSecComp = `
+# Description: Can access SocketCAN networks.
+bind
+`
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "socketcan",
+		summary:               socketCanSummary,
+		implicitOnCore:        true,
+		implicitOnClassic:     true,
+		baseDeclarationSlots:  socketCanBaseDeclarationSlots,
+		connectedPlugAppArmor: socketCanConnectedPlugAppArmor,
+		connectedPlugSecComp:  socketCanConnectedPlugSecComp,
+		reservedForOS:         true,
+	})
+}
---
+++ interfaces/builtin/socketcan_test.go
@@ -0,0 +1,110 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/interfaces/seccomp"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type SocketCanInterfaceSuite struct {
+	iface    interfaces.Interface
+	slotInfo *snap.SlotInfo
+	slot     *interfaces.ConnectedSlot
+	plugInfo *snap.PlugInfo
+	plug     *interfaces.ConnectedPlug
+}
+
+var _ = Suite(&SocketCanInterfaceSuite{
+	iface: builtin.MustInterface("socketcan"),
+})
+
+const socketCanConsumerYaml = `name: consumer
+version: 0
+apps:
+ app:
+  plugs: [socketcan]
+`
+
+const socketCanCoreYaml = `name: core
+version: 0
+type: os
+slots:
+  socketcan:
+`
+
+func (s *SocketCanInterfaceSuite) SetUpTest(c *C) {
+	s.plug, s.plugInfo = MockConnectedPlug(c, socketCanConsumerYaml, nil, "socketcan")
+	s.slot, s.slotInfo = MockConnectedSlot(c, socketCanCoreYaml, nil, "socketcan")
+}
+
+func (s *SocketCanInterfaceSuite) TestName(c *C) {
+	c.Assert(s.iface.Name(), Equals, "socketcan")
+}
+
+func (s *SocketCanInterfaceSuite) TestSanitizeSlot(c *C) {
+	c.Assert(interfaces.BeforePrepareSlot(s.iface, s.slotInfo), IsNil)
+	slot := &snap.SlotInfo{
+		Snap:      &snap.Info{SuggestedName: "some-snap"},
+		Name:      "socketcan",
+		Interface: "socketcan",
+	}
+	c.Assert(interfaces.BeforePrepareSlot(s.iface, slot), ErrorMatches,
+		"socketcan slots are reserved for the core snap")
+}
+
+func (s *SocketCanInterfaceSuite) TestSanitizePlug(c *C) {
+	c.Assert(interfaces.BeforePreparePlug(s.iface, s.plugInfo), IsNil)
+}
+
+func (s *SocketCanInterfaceSuite) TestAppArmorSpec(c *C) {
+	spec := &apparmor.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, "network can,\n")
+}
+
+func (s *SocketCanInterfaceSuite) TestSecCompSpec(c *C) {
+	spec := &seccomp.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, "bind\n")
+}
+
+func (s *SocketCanInterfaceSuite) TestStaticInfo(c *C) {
+	si := interfaces.StaticInfoOf(s.iface)
+	c.Assert(si.ImplicitOnCore, Equals, true)
+	c.Assert(si.ImplicitOnClassic, Equals, true)
+	c.Assert(si.Summary, Equals, `allows use of SocketCAN network interfaces`)
+	c.Assert(si.BaseDeclarationSlots, testutil.Contains, "socketcan")
+}
+
+func (s *SocketCanInterfaceSuite) TestAutoConnect(c *C) {
+	c.Assert(s.iface.AutoConnect(s.plugInfo, s.slotInfo), Equals, true)
+}
+func (s *SocketCanInterfaceSuite) TestInterfaces(c *C) {
+	c.Check(builtin.Interfaces(), testutil.DeepContains, s.iface)
+}
