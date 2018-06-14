01a603ee95a884096b9b4217d794c64a354817bd
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-06-14 13:09:18 +0200

    cmd/libsnap-confine-private: tweak comments and variable naming
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/libsnap-confine-private/snap.c
+++ cmd/libsnap-confine-private/snap.c
@@ -169,31 +169,32 @@ void sc_snap_name_validate(const char *snap_name, struct sc_error **errorp)
 	sc_error_forward(errorp, err);
 }
 
-void sc_snap_drop_instance_key(const char *name, char *snap_name,
+void sc_snap_drop_instance_key(const char *instance_name, char *snap_name,
 			       size_t snap_name_size)
 {
-	sc_snap_split_instance_name(name, snap_name, snap_name_size, NULL, 0);
+	sc_snap_split_instance_name(instance_name, snap_name, snap_name_size,
+				    NULL, 0);
 }
 
-void sc_snap_split_instance_name(const char *name, char *snap_name,
+void sc_snap_split_instance_name(const char *instance_name, char *snap_name,
 				 size_t snap_name_size, char *instance_key,
 				 size_t instance_key_size)
 {
-	if (name == NULL) {
-		die("internal error: cannot split snap name when snap name is unset");
+	if (instance_name == NULL) {
+		die("internal error: cannot split instance name when it is unset");
 	}
 	if (snap_name == NULL && instance_key == NULL) {
 		die("internal error: cannot split instance name when both snap name and instance key are unset");
 	}
 
-	const char *pos = strchr(name, '_');
+	const char *pos = strchr(instance_name, '_');
 	const char *instance_key_start = "";
 	size_t snap_name_len = 0;
 	size_t instance_key_len = 0;
 	if (pos == NULL) {
-		snap_name_len = strlen(name);
+		snap_name_len = strlen(instance_name);
 	} else {
-		snap_name_len = pos - name;
+		snap_name_len = pos - instance_name;
 		instance_key_start = pos + 1;
 		instance_key_len = strlen(instance_key_start);
 	}
@@ -203,7 +204,7 @@ void sc_snap_split_instance_name(const char *name, char *snap_name,
 			die("snap name buffer too small");
 		}
 
-		memcpy(snap_name, name, snap_name_len);
+		memcpy(snap_name, instance_name, snap_name_len);
 		snap_name[snap_name_len] = '\0';
 	}
 
