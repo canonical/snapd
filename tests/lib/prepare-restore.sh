d86c35d925634c0177eda2ad79f83621b7fa7c92
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-06-18 13:03:40 -0300

    Creating the runtime state dir to store runtime test information

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -411,7 +411,9 @@ prepare_suite_each() {
     if is_classic_system; then
         prepare_each_classic
     fi
-    echo -n "$SPREAD_JOB " >> "$SPREAD_PATH/tests/runs"
+    #shellcheck source=tests/lib/state.sh
+    . "$TESTSLIB"/state.sh
+    echo -n "$SPREAD_JOB " >> "$RUNTIME_STATE_PATH/runs"
 }
 
 restore_suite_each() {
