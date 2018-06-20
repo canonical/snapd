3d212993a9ab799d5a61877f5e5a826f1b8e2b76
Michael Vogt <mvo@ubuntu.com>
2018-06-20 17:55:34 +0200

    tests,interfaces: support ss instead of netstat (and use in tests)
    This adds "ss" to network-control and also adds a new helper:
    wait_listen_port into network.sh that is used in tests instead
    of duplicating the use of netstat/ss all over the place.

--- tests/lib/network.sh
+++ tests/lib/network.sh
@@ -3,3 +3,16 @@
 get_default_iface(){
     ip route get 8.8.8.8 | awk '{ print $5; exit }'
 }
+
+wait_listen_port(){
+    PORT="$1"
+
+    for _ in $(seq 120); do
+        if ss -lnt | grep -Pq "LISTEN.*?:$PORT +.*?\n*"; then
+            break
+        fi
+        sleep 0.5
+    done
+
+    ss -lnt | grep -Pq "LISTEN.*?:$PORT +.*?\n*"
+}
