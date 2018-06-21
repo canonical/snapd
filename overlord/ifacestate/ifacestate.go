6e53884e9e37fec6a51473095fbca57590fef007
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-06-21 01:55:41 -0700

    overlord/ifacestate: implicit slots on "snapd", if present
    This patch changes the logic that adds implicit slots to the core-like
    snaps. The logic is now aware of the system state. If the state contains
    "snapd" snap then "core" and "ubuntu-core" will no longer gain implicit
    interfaces, instead those will go to the "snapd" snap. This ensures that
    on any system with at least one snap only one snap will contain implicit
    interfaces and that "snapd" will be preferred over "core" or
    "ubuntu-core".
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- overlord/ifacestate/ifacestate.go
+++ overlord/ifacestate/ifacestate.go
@@ -255,7 +255,7 @@ func initialConnectAttributes(st *state.State, plugSnap string, plugName string,
 		return nil, nil, err
 	}
 
-	addImplicitSlots(snapInfo)
+	addImplicitSlots(st, snapInfo)
 	slot, ok := snapInfo.Slots[slotName]
 	if !ok {
 		return nil, nil, fmt.Errorf("snap %q has no slot named %q", slotSnap, slotName)
@@ -284,7 +284,7 @@ func Disconnect(st *state.State, plugSnap, plugName, slotSnap, slotName string) 
 // CheckInterfaces checks whether plugs and slots of snap are allowed for installation.
 func CheckInterfaces(st *state.State, snapInfo *snap.Info) error {
 	// XXX: addImplicitSlots is really a brittle interface
-	addImplicitSlots(snapInfo)
+	addImplicitSlots(st, snapInfo)
 
 	if snapInfo.SnapID == "" {
 		// no SnapID means --dangerous was given, so skip interface checks
