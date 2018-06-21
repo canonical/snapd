85885076b767e0b365a8cb493b54e8e81808221d
Michael Vogt <mvo@ubuntu.com>
2018-06-21 11:47:19 +0200

    address review feedback

--- tests/lib/names.sh
+++ tests/lib/names.sh
@@ -4,7 +4,7 @@
 gadget_name=$(snap list | grep 'gadget$' | awk '{ print $1 }')
 kernel_name=$(snap list | grep 'kernel$' | awk '{ print $1 }')
 
-core_name="$(snap known model | grep base | cut -f2 -d: | tr -d '[:space:]' || true)"
+core_name="$(snap known model | awk '/^base: / { print $2 }' || true)"
 if [ -z "$core_name" ]; then
     core_name="core"
 fi
