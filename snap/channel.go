d24ae7a389ddbb718121e83b31dbd6122f437011
Samuele Pedroni <pedronis@lucediurna.net>
2018-06-22 17:21:37 +0200

    introduce snap.StoreChannel to represent a fully a store channel, with its helpers

---
+++ snap/channel.go
@@ -0,0 +1,154 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snap
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/snapcore/snapd/arch"
+	"github.com/snapcore/snapd/strutil"
+)
+
+var channelRisks = []string{"stable", "candidate", "beta", "edge"}
+
+// StoreChannel identifies and describes completely a store channel.
+type StoreChannel struct {
+	Architecture string `json:"architecture"`
+	Name         string `json:"name"`
+	Track        string `json:"track"`
+	Risk         string `json:"risk"`
+	Branch       string `json:"branch,omitempty"`
+}
+
+// ParseStoreChannel parses a string representing a store channel and includes the given architecture, if architecture is "" the system architecture is included.
+func ParseStoreChannel(s string, architecture string) (StoreChannel, error) {
+	if s == "" {
+		return StoreChannel{}, fmt.Errorf("channel name cannot be empty")
+	}
+	p := strings.Split(s, "/")
+	var risk, track, branch string
+	switch len(p) {
+	default:
+		return StoreChannel{}, fmt.Errorf("channel name has too many components: %s", s)
+	case 3:
+		track, risk, branch = p[0], p[1], p[2]
+	case 2:
+		if strutil.ListContains(channelRisks, p[0]) {
+			risk, branch = p[0], p[1]
+		} else {
+			track, risk = p[0], p[1]
+		}
+	case 1:
+		if strutil.ListContains(channelRisks, p[0]) {
+			risk = p[0]
+		} else {
+			track = p[0]
+			risk = "stable"
+		}
+	}
+
+	if !strutil.ListContains(channelRisks, risk) {
+		return StoreChannel{}, fmt.Errorf("invalid risk in channel name: %s", s)
+	}
+
+	if architecture == "" {
+		architecture = arch.UbuntuArchitecture()
+	}
+
+	return StoreChannel{
+		Architecture: architecture,
+		Track:        track,
+		Risk:         risk,
+		Branch:       branch,
+	}.Clean(), nil
+}
+
+// Clean returns a StoreChannel with a normalized track and name.
+func (c StoreChannel) Clean() StoreChannel {
+	track := c.Track
+
+	if track == "latest" {
+		track = ""
+	}
+
+	// normalized name
+	name := c.Risk
+	if track != "" {
+		name = track + "/" + name
+	}
+	if c.Branch != "" {
+		name = name + "/" + c.Branch
+	}
+
+	return StoreChannel{
+		Architecture: c.Architecture,
+		Name:         name,
+		Track:        track,
+		Risk:         c.Risk,
+		Branch:       c.Branch,
+	}
+}
+
+func (c *StoreChannel) String() string {
+	return c.Name
+}
+
+// Full returns the full name of the channel, inclusive the default track "latest".
+func (c *StoreChannel) Full() string {
+	if c.Track == "" {
+		return "latest/" + c.Name
+	}
+	return c.String()
+}
+
+func riskLevel(risk string) int {
+	for i, r := range channelRisks {
+		if r == risk {
+			return i
+		}
+	}
+	return -1
+}
+
+// Match returns a representation of which fields among architecture,track,risk match between c and c1 store channels, risk is matched taking channel inheritance into account and considering c requested. Results can be:
+//  "architecture:track:risk"
+//  "architecture:track"
+//  "architecture:risk"
+//  "track:risk"
+//  "architecture"
+//  "track"
+//  "risk"
+//  ""
+func (c *StoreChannel) Match(c1 *StoreChannel) string {
+	matching := []string{}
+	if c.Architecture == c1.Architecture {
+		matching = append(matching, "architecture")
+	}
+	if c.Track == c1.Track {
+		matching = append(matching, "track")
+	}
+	requestedRiskLevel := riskLevel(c.Risk)
+	rl1 := riskLevel(c1.Risk)
+	if requestedRiskLevel >= rl1 {
+		matching = append(matching, "risk")
+	}
+	return strings.Join(matching, ":")
+}
---
+++ snap/channel_test.go
@@ -0,0 +1,209 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snap_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/arch"
+	"github.com/snapcore/snapd/snap"
+)
+
+type storeChannelSuite struct{}
+
+var _ = Suite(&storeChannelSuite{})
+
+func (s storeChannelSuite) TestParseStoreChannel(c *C) {
+	ch, err := snap.ParseStoreChannel("stable", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "stable",
+		Track:        "",
+		Risk:         "stable",
+		Branch:       "",
+	})
+
+	ch, err = snap.ParseStoreChannel("latest/stable", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "stable",
+		Track:        "",
+		Risk:         "stable",
+		Branch:       "",
+	})
+
+	ch, err = snap.ParseStoreChannel("1.0/edge", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "1.0/edge",
+		Track:        "1.0",
+		Risk:         "edge",
+		Branch:       "",
+	})
+
+	ch, err = snap.ParseStoreChannel("1.0", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "1.0/stable",
+		Track:        "1.0",
+		Risk:         "stable",
+		Branch:       "",
+	})
+
+	ch, err = snap.ParseStoreChannel("1.0/beta/foo", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "1.0/beta/foo",
+		Track:        "1.0",
+		Risk:         "beta",
+		Branch:       "foo",
+	})
+
+	ch, err = snap.ParseStoreChannel("candidate/foo", "")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: arch.UbuntuArchitecture(),
+		Name:         "candidate/foo",
+		Track:        "",
+		Risk:         "candidate",
+		Branch:       "foo",
+	})
+
+	ch, err = snap.ParseStoreChannel("candidate/foo", "other-arch")
+	c.Assert(err, IsNil)
+	c.Check(ch, DeepEquals, snap.StoreChannel{
+		Architecture: "other-arch",
+		Name:         "candidate/foo",
+		Track:        "",
+		Risk:         "candidate",
+		Branch:       "foo",
+	})
+}
+
+func (s storeChannelSuite) TestClean(c *C) {
+	ch := snap.StoreChannel{
+		Architecture: "arm64",
+		Track:        "latest",
+		Name:         "latest/stable",
+		Risk:         "stable",
+	}
+
+	cleanedCh := ch.Clean()
+	c.Check(cleanedCh, Not(DeepEquals), c)
+	c.Check(cleanedCh, DeepEquals, snap.StoreChannel{
+		Architecture: "arm64",
+		Track:        "",
+		Name:         "stable",
+		Risk:         "stable",
+	})
+}
+
+func (s storeChannelSuite) TestParseStoreChannelErrors(c *C) {
+	_, err := snap.ParseStoreChannel("", "")
+	c.Check(err, ErrorMatches, "channel name cannot be empty")
+
+	_, err = snap.ParseStoreChannel("1.0////", "")
+	c.Check(err, ErrorMatches, "channel name has too many components: 1.0////")
+
+	_, err = snap.ParseStoreChannel("1.0/cand", "invalid risk in channel name: 1.0/cand")
+	c.Check(err, ErrorMatches, "invalid risk in channel name: 1.0/cand")
+}
+
+func (s *storeChannelSuite) TestString(c *C) {
+	tests := []struct {
+		channel string
+		str     string
+	}{
+		{"stable", "stable"},
+		{"latest/stable", "stable"},
+		{"1.0/edge", "1.0/edge"},
+		{"1.0/beta/foo", "1.0/beta/foo"},
+		{"candidate/foo", "candidate/foo"},
+	}
+
+	for _, t := range tests {
+		ch, err := snap.ParseStoreChannel(t.channel, "")
+		c.Assert(err, IsNil)
+
+		c.Check(ch.String(), Equals, t.str)
+	}
+}
+
+func (s *storeChannelSuite) TestFull(c *C) {
+	tests := []struct {
+		channel string
+		str     string
+	}{
+		{"stable", "latest/stable"},
+		{"latest/stable", "latest/stable"},
+		{"1.0/edge", "1.0/edge"},
+		{"1.0/beta/foo", "1.0/beta/foo"},
+		{"candidate/foo", "latest/candidate/foo"},
+	}
+
+	for _, t := range tests {
+		ch, err := snap.ParseStoreChannel(t.channel, "")
+		c.Assert(err, IsNil)
+
+		c.Check(ch.Full(), Equals, t.str)
+	}
+}
+
+func (s *storeChannelSuite) TestMatch(c *C) {
+	tests := []struct {
+		req      string
+		c1       string
+		sameArch bool
+		res      string
+	}{
+		{"stable", "stable", true, "architecture:track:risk"},
+		{"stable", "beta", true, "architecture:track"},
+		{"beta", "stable", true, "architecture:track:risk"},
+		{"stable", "edge", false, "track"},
+		{"edge", "stable", false, "track:risk"},
+		{"1.0/stable", "1.0/edge", true, "architecture:track"},
+		{"1.0/edge", "stable", true, "architecture:risk"},
+		{"1.0/edge", "stable", false, "risk"},
+		{"1.0/stable", "stable", false, "risk"},
+		{"1.0/stable", "beta", false, ""},
+		{"1.0/stable", "2.0/beta", false, ""},
+		{"2.0/stable", "2.0/beta", false, "track"},
+		{"1.0/stable", "2.0/beta", true, "architecture"},
+	}
+
+	for _, t := range tests {
+		reqArch := "amd64"
+		c1Arch := "amd64"
+		if !t.sameArch {
+			c1Arch = "arm64"
+		}
+		req, err := snap.ParseStoreChannel(t.req, reqArch)
+		c.Assert(err, IsNil)
+		c1, err := snap.ParseStoreChannel(t.c1, c1Arch)
+		c.Assert(err, IsNil)
+
+		c.Check(req.Match(&c1), Equals, t.res)
+	}
+}
