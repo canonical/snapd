af9a5eb47e6474e77c30184e732040910d84bd0c
Pawel Stolowski <stolowski@gmail.com>
2018-06-25 14:44:21 +0200

    Store deviceKey in SlotInfo.

--- snap/info.go
+++ snap/info.go
@@ -574,13 +574,13 @@ func (slot *SlotInfo) String() string {
 type SlotInfo struct {
 	Snap *Info
 
-	Name      string
-	Interface string
-	Attrs     map[string]interface{}
-	Label     string
-	Apps      map[string]*AppInfo
-	Hooks     map[string]*HookInfo
-	Hotplug   bool
+	Name             string
+	Interface        string
+	Attrs            map[string]interface{}
+	Label            string
+	Apps             map[string]*AppInfo
+	Hooks            map[string]*HookInfo
+	HotplugDeviceKey string
 }
 
 // SocketInfo provides information on application sockets.
