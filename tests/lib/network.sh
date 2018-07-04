3a1eb8c6f66673b1ca4f9a028be52ee9bee5e44b
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-07-04 11:33:13 +0200

    tests/lib/network: add helper for creating network services
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/lib/network.sh
+++ tests/lib/network.sh
@@ -18,3 +18,17 @@ wait_listen_port(){
     # exit code if the port is not available.
     ss -lnt | grep -Pq "LISTEN.*?:$PORT +.*?\\n*"
 }
+
+make_network_service() {
+    SERVICE_NAME="$1"
+    SERVICE_FILE="$2"
+    PORT="$3"
+
+    #shellcheck source=tests/lib/systemd.sh
+    . "$TESTLIB"/systemd.sh
+
+    printf '#!/bin/sh -e\nwhile true; do printf '\''HTTP/1.1 200 OK\\n\\nok\\n'\'' |  nc -l -p %s -w 1; done' "$PORT" > "$SERVICE_FILE"
+    chmod a+x "$SERVICE_FILE"
+    systemd_create_and_start_unit "$SERVICE_NAME" "$(readlink -f "$SERVICE_FILE")"
+    wait_listen_port "$PORT"
+}
