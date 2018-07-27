682bfff05dee1042a2eff664ddcf14bd5e123978
Michael Vogt <mvo@ubuntu.com>
2018-07-27 16:41:15 +0200

    asserts: test for "kernel: kernel" and "kernel: kernel=track"

--- asserts/device_asserts_test.go
+++ asserts/device_asserts_test.go
@@ -59,7 +59,7 @@ const (
 		"architecture: amd64\n" +
 		"gadget: brand-gadget\n" +
 		"base: core18\n" +
-		"kernel: baz-linux=18\n" +
+		"kernel: baz-linux\n" +
 		"store: brand-store\n" +
 		sysUserAuths +
 		reqSnaps +
@@ -102,7 +102,7 @@ func (mods *modelSuite) TestDecodeOK(c *C) {
 	c.Check(model.Architecture(), Equals, "amd64")
 	c.Check(model.Gadget(), Equals, "brand-gadget")
 	c.Check(model.Kernel(), Equals, "baz-linux")
-	c.Check(model.KernelTrack(), Equals, "18")
+	c.Check(model.KernelTrack(), Equals, "")
 	c.Check(model.Base(), Equals, "core18")
 	c.Check(model.Store(), Equals, "brand-store")
 	c.Check(model.RequiredSnaps(), DeepEquals, []string{"foo", "bar"})
@@ -181,6 +181,16 @@ func (mods *modelSuite) TestDecodeSystemUserAuthorityIsOptional(c *C) {
 	c.Check(model.SystemUserAuthority(), DeepEquals, []string{"foo", "bar"})
 }
 
+func (mods *modelSuite) TestDecodeKernelTrack(c *C) {
+	withTimestamp := strings.Replace(modelExample, "TSLINE", mods.tsLine, 1)
+	encoded := strings.Replace(withTimestamp, "kernel: baz-linux\n", "kernel: baz-linux=18\n", 1)
+	a, err := asserts.Decode([]byte(encoded))
+	c.Assert(err, IsNil)
+	model := a.(*asserts.Model)
+	c.Check(model.Kernel(), Equals, "baz-linux")
+	c.Check(model.KernelTrack(), Equals, "18")
+}
+
 const (
 	modelErrPrefix = "assertion model: "
 )
@@ -204,12 +214,12 @@ func (mods *modelSuite) TestDecodeInvalid(c *C) {
 		{"architecture: amd64\n", "architecture: \n", `"architecture" header should not be empty`},
 		{"gadget: brand-gadget\n", "", `"gadget" header is mandatory`},
 		{"gadget: brand-gadget\n", "gadget: \n", `"gadget" header should not be empty`},
-		{"kernel: baz-linux=18\n", "", `"kernel" header is mandatory`},
-		{"kernel: baz-linux=18\n", "kernel: \n", `"kernel" header should not be empty`},
-		{"kernel: baz-linux=18\n", "kernel: baz-linux=x/x/x\n", `"kernel" channel selector must be a track name only`},
-		{"kernel: baz-linux=18\n", "kernel: baz-linux=stable\n", `"kernel" channel selector must be a track name`},
-		{"kernel: baz-linux=18\n", "kernel: baz-linux=18/beta\n", `"kernel" channel selector must be a track name only`},
-		{"kernel: baz-linux=18\n", "kernel:\n  - xyz \n", `"kernel" header must be a string`},
+		{"kernel: baz-linux\n", "", `"kernel" header is mandatory`},
+		{"kernel: baz-linux\n", "kernel: \n", `"kernel" header should not be empty`},
+		{"kernel: baz-linux\n", "kernel: baz-linux=x/x/x\n", `"kernel" channel selector must be a track name only`},
+		{"kernel: baz-linux\n", "kernel: baz-linux=stable\n", `"kernel" channel selector must be a track name`},
+		{"kernel: baz-linux\n", "kernel: baz-linux=18/beta\n", `"kernel" channel selector must be a track name only`},
+		{"kernel: baz-linux\n", "kernel:\n  - xyz \n", `"kernel" header must be a string`},
 		{"store: brand-store\n", "store:\n  - xyz\n", `"store" header must be a string`},
 		{mods.tsLine, "", `"timestamp" header is mandatory`},
 		{mods.tsLine, "timestamp: \n", `"timestamp" header should not be empty`},
