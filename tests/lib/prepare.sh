c9e03392db3b33f943e3c0ecf8c5cfba98e7b2bb
Michael Vogt <mvo@ubuntu.com>
2018-07-31 09:49:35 +0200

    use MATCH in snapd.seeded.service check

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -590,7 +590,7 @@ prepare_ubuntu_core() {
     done
 
     echo "Ensure snapd.seeded.service has the right state"
-    systemctl status snapd.seeded.service | grep "Active: active"
+    systemctl status snapd.seeded.service | MATCH "Active: active"
     
     echo "Ensure fundamental snaps are still present"
     # shellcheck source=tests/lib/names.sh
