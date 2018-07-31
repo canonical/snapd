1c690e328ecdf9828db552cec699190e595a9412
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-07-31 17:07:54 +0200

    cmd/snap-update-ns: fix indentation
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/snap-update-ns/bootstrap.c
+++ cmd/snap-update-ns/bootstrap.c
@@ -249,42 +249,42 @@ int validate_snap_name(const char* snap_name)
 
 static int instance_key_validate(const char *instance_key)
 {
-	// NOTE: see snap.ValidateInstanceName for reference of a valid instance key
-	// format
+    // NOTE: see snap.ValidateInstanceName for reference of a valid instance key
+    // format
 
-	// Ensure that name is not NULL
-	if (instance_key == NULL) {
-		bootstrap_msg = "instance key cannot be NULL";
-		return -1;
-	}
+    // Ensure that name is not NULL
+    if (instance_key == NULL) {
+        bootstrap_msg = "instance key cannot be NULL";
+        return -1;
+    }
 
-	const char *p = instance_key;
-	int n = 0;
-	while (*p != '\0') {
-		int m = 0;
-		m = skip_lowercase_letters(&p);
-		if (m > 0) {
-			n += m;
-			continue;
-		}
-		m = skip_digits(&p);
-		if (m > 0) {
-			n += m;
-			continue;
-		}
-		bootstrap_msg = "instance key must use lower case letters or digits";
-		return -1;
-	}
+    const char *p = instance_key;
+    int n = 0;
+    while (*p != '\0') {
+        int m = 0;
+        m = skip_lowercase_letters(&p);
+        if (m > 0) {
+            n += m;
+            continue;
+        }
+        m = skip_digits(&p);
+        if (m > 0) {
+            n += m;
+            continue;
+        }
+        bootstrap_msg = "instance key must use lower case letters or digits";
+        return -1;
+    }
 
-	if (n == 0) {
-		bootstrap_msg = "instance key must contain at least one letter or digit";
-		return -1;
-	} else if (n > 10) {
-		bootstrap_msg = "instance key must be shorter than 10 characters";
-		return -1;
-	}
+    if (n == 0) {
+        bootstrap_msg = "instance key must contain at least one letter or digit";
+        return -1;
+    } else if (n > 10) {
+        bootstrap_msg = "instance key must be shorter than 10 characters";
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 // validate_instance_name performs full validation of the given snap instance name.
@@ -298,39 +298,37 @@ int validate_instance_name(const char* instance_name)
         return -1;
     }
 
-	const char *instance_sep = strchr(instance_name, '_');
+    const char *instance_sep = strchr(instance_name, '_');
 
-	if (instance_sep != NULL) {
-		size_t snap_name_len = instance_sep - instance_name;
-		const char *instance_key = instance_sep + 1;
+    if (instance_sep != NULL) {
+        size_t snap_name_len = instance_sep - instance_name;
+        const char *instance_key = instance_sep + 1;
 
-		if (snap_name_len == 0) {
-			bootstrap_msg = "snap name must contain at least one letter";
-			return -1;
-		}
+        if (snap_name_len == 0) {
+            bootstrap_msg = "snap name must contain at least one letter";
+            return -1;
+        }
 
-		if (instance_key_validate(instance_key) != 0) {
-			return -1;
-		}
+        if (instance_key_validate(instance_key) != 0) {
+            return -1;
+        }
 
-		if (snap_name_len > 40) {
-			bootstrap_msg = "snap name must be shorter than 40 characters";
-			return -1;
-		}
+        if (snap_name_len > 40) {
+            bootstrap_msg = "snap name must be shorter than 40 characters";
+            return -1;
+        }
 
-		char snap_name[41];
-		memcpy(snap_name, instance_name, snap_name_len);
-		snap_name[snap_name_len] = '\0';
+        char snap_name[41];
+        memcpy(snap_name, instance_name, snap_name_len);
+        snap_name[snap_name_len] = '\0';
 
-		return validate_snap_name(snap_name);
-	} else {
-		// just snap name
-		return validate_snap_name(instance_name);
-	}
-
-	return -1;
+        return validate_snap_name(snap_name);
+    } else {
+        // just snap name
+        return validate_snap_name(instance_name);
+    }
+    return -1;
 }
-
 
 // process_arguments parses given a command line
 // argc and argv are defined as for the main() function
