012602066b70593dc37b735f300454218f33ff13
Michael Vogt <mvo@ubuntu.com>
2018-08-01 16:45:02 +0200

    many: add snapd.failure unit to deal with snapd failures

---
+++ cmd/snap-failure/cmd_run.go
@@ -0,0 +1,129 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/osutil"
+)
+
+func init() {
+	const (
+		short = "Run failure handling"
+		long  = ""
+	)
+
+	if _, err := parser.AddCommand("run", short, long, &cmdRun{}); err != nil {
+		panic(err)
+	}
+
+}
+
+// We do not import anything from snapd here for safety reasons so make a
+// copy of the relevant struct data we care about.
+type sideInfo struct {
+	Revision string `json:"revision"`
+}
+
+type snapSeq struct {
+	Current  string      `json:"current"`
+	Sequence []*sideInfo `json:"sequence"`
+}
+
+type cmdRun struct{}
+
+var errNoSnapd = errors.New("no snapd sequence file found")
+
+func prevRevision(snapName string) (string, error) {
+	seqFile := filepath.Join(dirs.SnapSeqDir, snapName+".json")
+	content, err := ioutil.ReadFile(seqFile)
+	if os.IsNotExist(err) {
+		return "", errNoSnapd
+	}
+	if err != nil {
+		return "", err
+	}
+
+	var seq snapSeq
+	if err := json.Unmarshal(content, &seq); err != nil {
+		return "", err
+	}
+
+	var prev string
+	for i, si := range seq.Sequence {
+		if seq.Current == si.Revision {
+			if i == 0 {
+				return "", fmt.Errorf("no revision to go back to")
+			}
+			prev = seq.Sequence[i-1].Revision
+			break
+		}
+	}
+	if prev == "" {
+		return "", fmt.Errorf("internal error: current not found in sequence: %v %v", seq.Current, seq.Sequence)
+	}
+
+	return prev, nil
+}
+
+// FIXME: also do error reporting via errtracker
+func (c *cmdRun) Execute(args []string) error {
+	// find previous snapd
+	prevRev, err := prevRevision("snapd")
+	if err != nil {
+		if err == errNoSnapd {
+			return nil
+		}
+		return err
+	}
+	// stop the socket unit so that we can start snapd on its own
+	output, err := exec.Command("systemctl", "stop", "snapd.socket").CombinedOutput()
+	if err != nil {
+		return osutil.OutputErr(output, err)
+	}
+
+	// start previous snapd
+	snapdPath := filepath.Join(dirs.SnapMountDir, "snapd", prevRev, "/usr/lib/snapd/snapd")
+	cmd := exec.Command(snapdPath)
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "SNAPD_REVERT_TO_REV="+prevRev)
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		return osutil.OutputErr(output, err)
+	}
+
+	// at this point snapd is back but the unit is in failed state
+	// TODO: remove once https://github.com/snapcore/snapd/pull/5557
+	//       is merged
+	output, err = exec.Command("systemctl", "restart", "snapd.service").CombinedOutput()
+	if err != nil {
+		return osutil.OutputErr(output, err)
+	}
+
+	return nil
+}
