6404e73d056c58717a46e77d9a8691cefffbd70e
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-08-09 13:49:18 +0200

    overlord/snapstate: always clean SnapState when doing Get()
    SnapState is (un-)marshalled from/to JSON. Some fields some with omitempty tag,
    meaning those will not be included in the output when marshalling and the will
    not be cleared in the destination structure when unmarshalling.
    
    Sometimes in the code we reuse snapst for subsequent snapstate.Get() calls.
    Should the destination struct not be cleared between the calls, we may end up
    with data from the previous calls inside.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/snapstate/snapstate.go
+++ overlord/snapstate/snapstate.go
@@ -1761,6 +1761,11 @@ func CurrentInfo(st *state.State, name string) (*snap.Info, error) {
 
 // Get retrieves the SnapState of the given snap.
 func Get(st *state.State, name string, snapst *SnapState) error {
+	if snapst == nil {
+		return fmt.Errorf("internal error: snapst is nil")
+	}
+	*snapst = SnapState{}
+
 	var snaps map[string]*json.RawMessage
 	err := st.Get("snaps", &snaps)
 	if err != nil {
