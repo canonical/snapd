4a81b968210bf9d228a2aeb0292a6ac2d85dfc7c
Jamie Strandboge <jamie@ubuntu.com>
2018-08-13 19:35:46 +0000

    interfaces: allow base32/base64 in default template

--- interfaces/apparmor/template.go
+++ interfaces/apparmor/template.go
@@ -106,6 +106,8 @@ var defaultTemplate = `
   # Common utilities for shell scripts
   /{,usr/}bin/arch ixr,
   /{,usr/}bin/{,g,m}awk ixr,
+  /{,usr/}bin/base32 ixr,
+  /{,usr/}bin/base64 ixr,
   /{,usr/}bin/basename ixr,
   /{,usr/}bin/bunzip2 ixr,
   /{,usr/}bin/bzcat ixr,
