9eb3091e0c08d2b5f95de79590b5bb7af0255c67
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-08-22 13:29:13 +0200

    interfaces: rewrite adb-support interface
    This patch makes several changes to the adb-support interface. The
    permanent slot snippet now grants udev permissions that change the mode
    of all the interesting devices to 0666. The connected plug grants udev
    and apparmor snippets for tagging the very same devices and for
    accessing the relevant parts of /dev and /run/udev/data.
    
    With those changes the slot can be provided by interested applications
    directly, without involving the core. This is reflected in the implicit
    flags and in the expected policy.
    
    Lastly, since the only phone I could test this against was a BQ Ubuntu
    Aquaris E4.5 I added the vendor ID to the list of handled devices.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- interfaces/policy/basedeclaration_test.go
+++ interfaces/policy/basedeclaration_test.go
@@ -522,6 +522,7 @@ var (
 
 	slotInstallation = map[string][]string{
 		// other
+		"adb-support":             {"app"},
 		"autopilot-introspection": {"core"},
 		"avahi-control":           {"app", "core"},
 		"avahi-observe":           {"app", "core"},
