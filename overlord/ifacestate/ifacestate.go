6d94e1cb5f9915fe635f2b527a7dcf41c7173072
Pawel Stolowski <stolowski@gmail.com>
2018-08-22 10:02:41 +0200

    Improved autoconnect retry tests to reduce execution time.

--- overlord/ifacestate/ifacestate.go
+++ overlord/ifacestate/ifacestate.go
@@ -389,3 +389,9 @@ func delayedCrossMgrInit() {
 		snapstate.AddAffectedSnapsByKind("disconnect", connectDisconnectAffectedSnaps)
 	})
 }
+
+func MockConnectRetryTimeout(d time.Duration) (restore func()) {
+	old := connectRetryTimeout
+	connectRetryTimeout = d
+	return func() { connectRetryTimeout = old }
+}
