17dfdfc35225655a2fdf5e39016cba782859fe17
Michael Vogt <mvo@ubuntu.com>
2018-09-03 14:55:17 +0200

    release: 2.35.1

--- interfaces/mount/backend.go
+++ interfaces/mount/backend.go
@@ -61,7 +61,7 @@ func (b *Backend) Setup(snapInfo *snap.Info, confinement interfaces.ConfinementO
 	if err != nil {
 		return fmt.Errorf("cannot obtain mount security snippets for snap %q: %s", snapName, err)
 	}
-	spec.(*Specification).AddSnapLayout(snapInfo)
+	spec.(*Specification).AddLayout(snapInfo)
 	content := deriveContent(spec.(*Specification), snapInfo)
 	// synchronize the content with the filesystem
 	glob := fmt.Sprintf("snap.%s.*fstab", snapName)
