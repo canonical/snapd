7affeaf0a09a9f987cd242d580bcac2a467f96fe
Zygmunt Krynicki <me@zygoon.pl>
2018-09-04 12:25:46 +0200

    cmd/snap-update-ns: allow writes to /tmp, /var/snap and /snap/$SNAP_NAME
    We are almost ready to enable the restricted write mode but for that
    we still need to white-list a few places that we are reasonably expected
    to write as a part of normal operation. Those include the parent directory
    of $SNAP_DATA and $SNAP_COMMON (/var/snap) as well as the parent directory
    of $SNAP (/snap/$SNAP_NAME, since we are always on the inside of the mount
    namespace).
    
    The white-list allows us to say that we expect to make changes there
    even if those would hit a filesystem that is not isolated inside the
    per-snap mount namespace.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/snap-update-ns/main.go
+++ cmd/snap-update-ns/main.go
@@ -128,9 +128,28 @@ func applyFstab(snapName string, fromSnapConfine bool) error {
 		thawSnapProcesses(opts.Positionals.SnapName)
 	}()
 
-	// TODO: configure the secure helper and inform it about directories that
-	// can be created without trespassing.
+	// Allow creating directories related to this snap name.
+	//
+	// Note that we allow /var/snap instead of /var/snap/$SNAP_NAME because
+	// content interface connections can readily create missing mount points on
+	// both sides of the interface connection.
+	//
+	// We scope /snap/$SNAP_NAME because only one side of the connection can be
+	// created, as snaps are read-only, the mimic construction will kick-in and
+	// create the missing directory but this directory is only visible from the
+	// snap that we are operating on (either plug or slot side, the point is,
+	// the mount point is not universally visible).
+	//
+	// /snap/$SNAP_NAME needs to be there as the code that creates such mount
+	// points must traverse writable host filesystem that contains /snap/*/ and
+	// normally such access is off-limits. This approach allows /snap/foo
+	// without allowing /snap/bin, for example.
+
+	// TODO: Handle /home/*/snap/* when we do per-user mount namespaces and
+	// allow defining layout items that refer to SNAP_USER_DATA and
+	// SNAP_USER_COMMON.
 	sec := &Secure{}
+	sec.AddUnrestrictedPrefixes("/tmp", "/var/snap", "/snap/"+snapName)
 	return computeAndSaveChanges(snapName, sec)
 }
 
