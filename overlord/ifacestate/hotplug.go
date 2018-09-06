5c67880c6f23225b8385fdbe36759f97ecd4fc5c
Pawel Stolowski <stolowski@gmail.com>
2018-09-06 10:32:57 +0200

    Helpers for generating slot names for hotplug.

--- overlord/ifacestate/hotplug.go
+++ overlord/ifacestate/hotplug.go
@@ -20,6 +20,10 @@
 package ifacestate
 
 import (
+	"fmt"
+	"strconv"
+	"unicode"
+
 	"github.com/snapcore/snapd/interfaces/hotplug"
 )
 
@@ -29,4 +33,116 @@ func (m *InterfaceManager) HotplugDeviceAdded(devinfo *hotplug.HotplugDeviceInfo
 
 // HotplugDeviceRemoved gets called when a device is removed from the system.
 func (m *InterfaceManager) HotplugDeviceRemoved(devinfo *hotplug.HotplugDeviceInfo) {
+}
+
+// ensureUniqueName modifies proposedName so that it's unique according to isUnique predicate.
+// Uniqueness is achieved by appending a numeric suffix, or increasing existing suffix.
+func ensureUniqueName(proposedName string, isUnique func(string) bool) string {
+	// if the name is unique right away, do nothing
+	if isUnique(proposedName) {
+		return proposedName
+	}
+
+	// extract number suffix if present
+	pname := []rune(proposedName)
+	end := len(pname) - 1
+	suffixIndex := end
+	for suffixIndex >= 0 && unicode.IsDigit(pname[suffixIndex]) {
+		suffixIndex--
+	}
+
+	var suffixNumValue uint64
+	// if numeric suffix hasn't been not found, append "-" before the number
+	if suffixIndex == end {
+		pname = append(pname, '-')
+		suffixIndex++
+	} else {
+		var err error
+		suffixNumValue, err = strconv.ParseUint(string(pname[suffixIndex+1:]), 10, 32)
+		if err != nil {
+			suffixIndex = end
+		}
+	}
+
+	// increase suffix value until we have a unique name
+	for {
+		suffixNumValue++
+		proposedName = fmt.Sprintf("%s%d", string(pname[0:suffixIndex+1]), suffixNumValue)
+		if isUnique(proposedName) {
+			return proposedName
+		}
+	}
+}
+
+const maxLen = 20
+
+// cleanupSlotName sanitizes proposedName to make it a valid slot name that
+// passes validation rules implemented by ValidateSlotName (see snap/validate.go):
+// - only lowercase letter, digits and dashes are allowed
+// - must start with a letter
+// - no double dashes, cannot end with a dash.
+// In addition names are truncated not to exceed maxLen characters.
+func cleanupSlotName(proposedName string) string {
+	var out []rune
+	var charCount int
+	// the dash flag is used to prevent consecutive dashes, and the dash in the front
+	dash := true
+Loop:
+	for _, c := range proposedName {
+		switch {
+		case c == '-' && !dash:
+			dash = true
+			out = append(out, '-')
+		case unicode.IsLetter(c):
+			out = append(out, unicode.ToLower(c))
+			dash = false
+			charCount++
+			if charCount >= maxLen {
+				break Loop
+			}
+		case unicode.IsDigit(c) && charCount > 0:
+			out = append(out, c)
+			dash = false
+			charCount++
+			if charCount >= maxLen {
+				break Loop
+			}
+		default:
+			// any other character is ignored
+		}
+	}
+	// make sure the name doesn't end with a dash
+	if len(out) > 0 && out[len(out)-1] == '-' {
+		out = out[0 : len(out)-1]
+	}
+	return string(out)
+}
+
+var nameAttrs = []string{"NAME", "ID_MODEL_FROM_DATABASE", "ID_MODEL"}
+
+// suggestedSlotName returns the shortest name derived from attributes defined by nameAttrs, or
+// the fallbackName if there is no known attribute to derive name from. The name created from
+// attributes is cleaned up by cleanupSlotName function.
+// The fallbackName is typically the name of the interface.
+func suggestedSlotName(devinfo *hotplug.HotplugDeviceInfo, fallbackName string) string {
+	var candidates []string
+	for _, attr := range nameAttrs {
+		val, ok := devinfo.Attribute(attr)
+		if ok {
+			name := cleanupSlotName(val)
+			if name != "" {
+				candidates = append(candidates, name)
+			}
+		}
+	}
+	if len(candidates) == 0 {
+		return fallbackName
+	}
+	shortestName := candidates[0]
+	for _, cand := range candidates[1:] {
+		if len(cand) < len(shortestName) {
+			shortestName = cand
+		}
+	}
+	return shortestName
 }
---
+++ overlord/ifacestate/hotplug_test.go
@@ -0,0 +1,129 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package ifacestate_test
+
+import (
+	"github.com/snapcore/snapd/interfaces/hotplug"
+	"github.com/snapcore/snapd/overlord/ifacestate"
+
+	. "gopkg.in/check.v1"
+)
+
+type hotplugSuite struct{}
+
+var _ = Suite(&hotplugSuite{})
+
+func (s *hotplugSuite) TestEnsureUniqueName(c *C) {
+	fakeRepositoryLookup := func(n string) bool {
+		reserved := map[string]bool{
+			"slot1":    true,
+			"slot":     true,
+			"slot1234": true,
+			"slot-1":   true,
+			"slot-2":   true,
+			"slot3-5":  true,
+			"slot3-6":  true,
+			"11":       true,
+			"12foo":    true,
+		}
+		return !reserved[n]
+	}
+
+	names := []struct{ proposedName, resultingName string }{
+		{"foo", "foo"},
+		{"slot1", "slot2"},
+		{"slot1234", "slot1235"},
+		{"slot-1", "slot-3"},
+		{"slot3-5", "slot3-7"},
+		{"slot3-1", "slot3-1"},
+		{"11", "12"},
+		{"12foo", "12foo-1"},
+	}
+
+	for _, name := range names {
+		c.Assert(ifacestate.EnsureUniqueName(name.proposedName, fakeRepositoryLookup), Equals, name.resultingName)
+	}
+}
+
+func (s *hotplugSuite) TestCleanupSlotName(c *C) {
+	names := []struct{ proposedName, resultingName string }{
+		{"", ""},
+		{"-", ""},
+		{"slot1", "slot1"},
+		{"-slot1", "slot1"},
+		{"a--slot-1", "a-slot-1"},
+		{"Integrated_Webcam_HD", "integratedwebcamhd"},
+		{"Xeon E3-1200 v5/E3-1500 v5/6th Gen Core Processor Host Bridge/DRAM Registers", "xeone3-1200v5e3-1500v5"},
+	}
+	for _, name := range names {
+		c.Assert(ifacestate.CleanupSlotName(name.proposedName), Equals, name.resultingName)
+	}
+}
+
+func (s *hotplugSuite) TestSuggestedSlotName(c *C) {
+
+	events := []struct {
+		eventData map[string]string
+		outName   string
+	}{{
+		map[string]string{
+			"DEVPATH":                "a/path",
+			"ACTION":                 "add",
+			"SUBSYSTEM":              "foo",
+			"NAME":                   "Name",
+			"ID_MODEL":               "Longer Name",
+			"ID_MODEL_FROM_DATABASE": "Longest Name",
+		},
+		"name",
+	}, {
+		map[string]string{
+			"DEVPATH":                "a/path",
+			"ACTION":                 "add",
+			"SUBSYSTEM":              "foo",
+			"ID_MODEL":               "Longer Name",
+			"ID_MODEL_FROM_DATABASE": "Longest Name",
+		},
+		"longername",
+	}, {
+		map[string]string{
+			"DEVPATH":                "a/path",
+			"ACTION":                 "add",
+			"SUBSYSTEM":              "foo",
+			"ID_MODEL_FROM_DATABASE": "Longest Name",
+		},
+		"longestname",
+	}, {
+		map[string]string{
+			"DEVPATH":   "a/path",
+			"ACTION":    "add",
+			"SUBSYSTEM": "foo",
+		},
+		"fallbackname",
+	},
+	}
+
+	for _, data := range events {
+		di, err := hotplug.NewHotplugDeviceInfo(data.eventData)
+		c.Assert(err, IsNil)
+
+		slotName := ifacestate.SuggestedSlotName(di, "fallbackname")
+		c.Assert(slotName, Equals, data.outName)
+	}
+}
