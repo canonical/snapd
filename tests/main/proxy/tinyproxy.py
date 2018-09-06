d54647b6ebac8f4bbcd3a4aab9ed3ab9a0964431
Michael Vogt <mvo@ubuntu.com>
2018-09-06 10:28:54 +0200

    fix typo, modernize python

--- tests/main/proxy/tinyproxy.py
+++ tests/main/proxy/tinyproxy.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 
-__doc__ = """Tiny HTTP Proxy.
-
-This module implements GET, HEAD, POST, PUT and DELETE methods
-on BaseHTTPServer, and behaves as an HTTP proxy.  The CONNECT
-method is also implemented experimentally, but has not been
-tested yet.
-
-Any help will be greatly appreciated.		SUZUKI Hisao
-"""
-
-__version__ = "0.2.1"
+# Tiny HTTP Proxy. Based on the work of SUZUKI Hisao.
+#
+# Ported to py3 and modified to remove the bits we don't need
+# and modernized.
 
 import http.server
 import select
@@ -19,20 +12,18 @@ import socketserver
 import sys
 import urllib.parse
 
-def flush():
-    sys.stdout.flush()
-    sys.stderr.flush()
 
 class ProxyHandler (http.server.BaseHTTPRequestHandler):
-    __base = http.server.BaseHTTPRequestHandler
-    __base_handle = __base.handle
+    server_version = "testsproxy/1.0"
 
-    server_version = "TinyHTTPProxy/" + __version__
-    rbufsize = 0                        # self.rfile Be unbuffered
+    def log_request(self, m=""):
+        super().log_request(m)
+        sys.stdout.flush()
+        sys.stderr.flush()
 
     def handle(self):
         (ip, port) =  self.client_address
-        self.__base_handle()
+        super().handle()
 
     def _connect_to(self, netloc, soc):
         i = netloc.find(':')
@@ -40,23 +31,25 @@ class ProxyHandler (http.server.BaseHTTPRequestHandler):
             host_port = netloc[:i], int(netloc[i+1:])
         else:
             host_port = netloc, 80
-        try: soc.connect(host_port)
+        try:
+            soc.connect(host_port)
         except socket.error as arg:
-            try: msg = arg[1]
-            except: msg = arg
+            try:
+                msg = arg[1]
+            except:
+                msg = arg
             self.send_error(404, msg)
-            return 0
-        return 1
+            return False
+        return True
 
     def do_CONNECT(self):
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             if self._connect_to(self.path, soc):
                 self.log_request(200)
-                flush()
                 s = self.protocol_version + " 200 Connection established\r\n"
                 self.wfile.write(s.encode())
-                s = "Proxy-agent: %s\r\n" % self.version_string()
+                s = "Proxy-agent: {}\r\n".format(self.version_string())
                 self.wfile.write(s.encode())
                 self.wfile.write("\r\n".encode())
                 self._read_write(soc, 300)
@@ -68,21 +61,24 @@ class ProxyHandler (http.server.BaseHTTPRequestHandler):
         (scm, netloc, path, params, query, fragment) = urllib.parse.urlparse(
             self.path, 'http')
         if scm != 'http' or fragment or not netloc:
-            self.send_error(400, "bad url %s" % self.path)
+            s = "bad url {}".format(self.path)
+            self.send_error(400, s.encode())
             return
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             if self._connect_to(netloc, soc):
                 self.log_request()
-                soc.send("%s %s %s\r\n" % (
+                s = "{} {} {}\r\n".format(
                     self.command,
                     urllib.parse.urlunparse(('', '', path, params, query, '')),
-                    self.request_version))
+                    self.request_version)
+                soc.send(s.encode())
                 self.headers['Connection'] = 'close'
                 del self.headers['Proxy-Connection']
-                for key_val in list(self.headers.items()):
-                    soc.send("%s: %s\r\n" % key_val)
-                soc.send("\r\n")
+                for key, val in self.headers.items():
+                    s = "{}: {}\r\n".format(key, val)
+                    soc.send(s.encode())
+                soc.send("\r\n".encode())
                 self._read_write(soc)
         finally:
             soc.close()
@@ -92,10 +88,11 @@ class ProxyHandler (http.server.BaseHTTPRequestHandler):
         iw = [self.connection, soc]
         ow = []
         count = 0
-        while 1:
+        while True:
             count += 1
             (ins, _, exs) = select.select(iw, ow, iw, 3)
-            if exs: break
+            if exs:
+                break
             if ins:
                 for i in ins:
                     if i is soc:
@@ -106,7 +103,8 @@ class ProxyHandler (http.server.BaseHTTPRequestHandler):
                     if data:
                         out.send(data)
                         count = 0
-            if count == max_idling: break
+            if count == max_idling:
+                break
 
     do_HEAD = do_GET
     do_POST = do_GET
@@ -118,6 +116,6 @@ class ThreadingHTTPServer (socketserver.ThreadingMixIn,
     pass
 
 if __name__ == '__main__':
-    from sys import argv
-    print("starting tinyproxy")
-    http.server.test(ProxyHandler, ThreadingHTTPServer, port=3128)
+    port=3128
+    print("starting tinyproxy on port {}".format(port))
+    http.server.test(ProxyHandler, ThreadingHTTPServer, port=port)
