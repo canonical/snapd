52f812388da94fd7eaad65a0ded70d298cf16837
James Henstridge <james@jamesh.id.au>
2018-09-07 17:31:17 +0800

    cmd/snap: use the snap-store snap instead of gnome-software

--- cmd/snap/cmd_handle_link.go
+++ cmd/snap/cmd_handle_link.go
@@ -43,15 +43,15 @@ type cmdHandleLink struct {
 func init() {
 	addCommand("handle-link",
 		i18n.G("Handle a snap:// URI"),
-		i18n.G("The handle-link command installs the gnome-software snap and then invokes it."),
+		i18n.G("The handle-link command installs the snap-store snap and then invokes it."),
 		func() flags.Commander {
 			return &cmdHandleLink{}
 		}, nil, nil)
 }
 
-func (x *cmdHandleLink) ensureGnomeSoftwareInstalled(cli *client.Client) error {
-	// If the gnome-software snap is installed, our work is done
-	if _, _, err := cli.Snap("gnome-software"); err == nil {
+func (x *cmdHandleLink) ensureSnapStoreInstalled(cli *client.Client) error {
+	// If the snap-store snap is installed, our work is done
+	if _, _, err := cli.Snap("snap-store"); err == nil {
 		return nil
 	}
 
@@ -60,21 +60,21 @@ func (x *cmdHandleLink) ensureGnomeSoftwareInstalled(cli *client.Client) error {
 		return err
 	}
 	answeredYes := dialog.YesNo(
-		i18n.G("Install snap-aware GNOME Software snap?"),
-		i18n.G("GNOME Software is required to open snaps from a web browser."),
+		i18n.G("Install snap-aware Snap Store snap?"),
+		i18n.G("The Snap Store is required to open snaps from a web browser."),
 		&ui.DialogOptions{
 			Timeout: 5 * time.Minute,
 			Footer:  i18n.G("This dialog will close automatically after 5 minutes of inactivity."),
 		})
 	if !answeredYes {
-		return fmt.Errorf(i18n.G("GNOME Software required"))
+		return fmt.Errorf(i18n.G("Snap Store required"))
 	}
 
 	opts := client.SnapOptions{
-		Channel: "edge", // FIXME: remove this when gnome-software published to stable
+		Channel: "edge", // FIXME: remove this when snap-store published to stable
 		Classic: true,
 	}
-	changeID, err := cli.Install("gnome-software", &opts)
+	changeID, err := cli.Install("snap-store", &opts)
 	if err != nil {
 		return err
 	}
@@ -88,10 +88,10 @@ func (x *cmdHandleLink) ensureGnomeSoftwareInstalled(cli *client.Client) error {
 func (x *cmdHandleLink) Execute([]string) error {
 	cli := Client()
 
-	if err := x.ensureGnomeSoftwareInstalled(cli); err != nil {
+	if err := x.ensureSnapStoreInstalled(cli); err != nil {
 		return err
 	}
 
-	argv := []string{"snap", "run", "gnome-software", x.Positional.Uri}
+	argv := []string{"snap", "run", "snap-store", x.Positional.Uri}
 	return syscall.Exec("/proc/self/exe", argv, os.Environ())
 }
