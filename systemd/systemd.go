bcd8253516fa30defdd86427691b0dd6f1027218
Ian Johnson <ian.johnson@canonical.com>
2018-09-11 11:18:07 -0500

    systemd/systemd.go: add IsEnabled method for systemd
    It checks if a service is enabled or not. Note that if a service is disabled, `systemd is-enabled <name>` outputs "disabled\n" and exits with exit code 1, so we need to do a bit of extra checking.

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -117,6 +117,7 @@ type Systemd interface {
 	Kill(service, signal, who string) error
 	Restart(service string, timeout time.Duration) error
 	Status(services ...string) ([]*ServiceStatus, error)
+	IsEnabled(service string) (bool, error)
 	LogReader(services []string, n int, follow bool) (io.ReadCloser, error)
 	WriteMountUnitFile(name, revision, what, where, fstype string) (string, error)
 	Mask(service string) error
@@ -286,6 +287,22 @@ func (s *systemd) Status(serviceNames ...string) ([]*ServiceStatus, error) {
 	}
 
 	return sts, nil
+}
+
+// IsEnabled checkes whether the given service is enabled
+func (s *systemd) IsEnabled(serviceName string) (bool, error) {
+	_, err := systemctlCmd("--root", s.rootDir, "is-enabled", serviceName)
+	if err == nil {
+		return true, nil
+	}
+	// note that "systemctl is-enabled <name>" returns exit code 1 for disabled services
+	// as well as outputting "disabled\n", so we need to cast the error, and
+	// also trim the whitespace from the output to check the command output
+	sysdErr, ok := err.(*Error)
+	if ok && sysdErr.exitCode == 1 && strings.TrimSpace(string(sysdErr.msg)) == "disabled" {
+		return false, nil
+	}
+	return false, err
 }
 
 // Stop the given service, and wait until it has stopped.
