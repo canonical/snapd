eff3d4c810324431bdf287e9e53144f6b79f0a96
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-09-11 20:49:07 -0300

    Models signed and extra-snaps-assertions test fixed

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -4,12 +4,12 @@
 . "$TESTSLIB"/systemd.sh
 
 wait_for_ssh(){
-    retry=300
+    retry=150
     while ! execute_remote true; do
         retry=$(( retry - 1 ))
         if [ $retry -le 0 ]; then
             echo "Timed out waiting for ssh. Aborting!"
-            exit 1
+            return 1
         fi
         sleep 1
     done
@@ -51,11 +51,16 @@ create_nested_core_vm(){
     mv ubuntu-core.img /tmp/work-dir
 
     create_assertions_disk
-
-    systemd_create_and_start_unit nested-vm "${QEMU} -m 1024 -nographic -net nic,model=virtio -net user,hostfwd=tcp::8022-:22 -drive file=/tmp/work-dir/ubuntu-core.img,if=virtio,cache=none,format=raw -drive file=${PWD}/assertions.disk,if=virtio,cache=none,format=raw"
-
+    start_nested_vm
     wait_for_ssh
     prepare_ssh
+}
+
+start_nested_vm(){
+    systemd_create_and_start_unit nested-vm "${QEMU} -m 2048 -nographic -net nic,model=virtio -net user,hostfwd=tcp::8022-:22 -drive file=/tmp/work-dir/ubuntu-core.img,if=virtio,cache=none,format=raw -drive file=${PWD}/assertions.disk,if=virtio,cache=none,format=raw"
+    if ! wait_for_ssh; then
+        systemctl restart nested-vm
+    fi
 }
 
 destroy_nested_core_vm(){
@@ -64,5 +69,5 @@ destroy_nested_core_vm(){
 }
 
 execute_remote(){
-    sshpass -p ubuntu ssh -p 8022 -q -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user1@localhost "$*"
-}
+    sshpass -p ubuntu ssh -p 8022 -o ConnectTimeout=10 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no user1@localhost "$*"
+}   
