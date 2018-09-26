114ae04a0cca2b628a6cd46149fb44ab2d338342
Pawel Stolowski <stolowski@gmail.com>
2018-09-26 11:23:15 +0200

    Review feedback.

--- snap/info.go
+++ snap/info.go
@@ -648,12 +648,15 @@ func (slot *SlotInfo) String() string {
 type SlotInfo struct {
 	Snap *Info
 
-	Name             string
-	Interface        string
-	Attrs            map[string]interface{}
-	Label            string
-	Apps             map[string]*AppInfo
-	Hooks            map[string]*HookInfo
+	Name      string
+	Interface string
+	Attrs     map[string]interface{}
+	Label     string
+	Apps      map[string]*AppInfo
+	Hooks     map[string]*HookInfo
+
+	// HotplugDeviceKey is the key of the associated device; it's empty for
+	// regular slots.
 	HotplugDeviceKey string
 }
 
