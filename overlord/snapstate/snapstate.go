79f9599d04b3fb9c22ec1f459825fb0cff35c6d6
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-09-27 09:18:38 +0200

    overlord: allow experimental flags to have a default value
    This patch extends the logic for handling experimental flags to allow
    them to have a default value, such as true, while still having a way
    to turn the feature off entirely.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- overlord/snapstate/snapstate.go
+++ overlord/snapstate/snapstate.go
@@ -68,7 +68,7 @@ func doInstall(st *state.State, snapst *SnapState, snapsup *SnapSetup, flags int
 		}
 		if model == nil || model.Base() == "" {
 			tr := config.NewTransaction(st)
-			experimentalAllowSnapd, err := getFeatureFlagBool(tr, "experimental.snapd-snap")
+			experimentalAllowSnapd, err := getFeatureFlagBool(tr, "experimental.snapd-snap", false)
 			if err != nil && !config.IsNoOption(err) {
 				return nil, err
 			}
@@ -459,20 +459,20 @@ func defaultContentPlugProviders(st *state.State, info *snap.Info) []string {
 	return out
 }
 
-func getFeatureFlagBool(tr *config.Transaction, flag string) (bool, error) {
-	var v interface{} = false
+func getFeatureFlagBool(tr *config.Transaction, flag string, unset bool) (bool, error) {
+	var v interface{} = unset
 	if err := tr.GetMaybe("core", flag, &v); err != nil {
-		return false, err
+		return unset, err
 	}
 	switch value := v.(type) {
 	case string:
 		if value == "" {
-			return false, nil
+			return unset, nil
 		}
 	case bool:
 		return value, nil
 	}
-	return false, fmt.Errorf("internal error: feature flag %v has unexpected value %#v (%T)", flag, v, v)
+	return unset, fmt.Errorf("internal error: feature flag %v has unexpected value %#v (%T)", flag, v, v)
 }
 
 // validateFeatureFlags validates the given snap only uses experimental
@@ -481,7 +481,7 @@ func validateFeatureFlags(st *state.State, info *snap.Info) error {
 	tr := config.NewTransaction(st)
 
 	if len(info.Layout) > 0 {
-		flag, err := getFeatureFlagBool(tr, "experimental.layouts")
+		flag, err := getFeatureFlagBool(tr, "experimental.layouts", false)
 		if err != nil {
 			return err
 		}
@@ -491,7 +491,7 @@ func validateFeatureFlags(st *state.State, info *snap.Info) error {
 	}
 
 	if info.InstanceKey != "" {
-		flag, err := getFeatureFlagBool(tr, "experimental.parallel-instances")
+		flag, err := getFeatureFlagBool(tr, "experimental.parallel-instances", false)
 		if err != nil {
 			return err
 		}
