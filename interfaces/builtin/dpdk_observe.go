1bd64e9391e721b21d0fe8350748dc7c21e1c81f
Lucas Williams <wililupy@comcast.net>
2018-09-28 11:41:41 -0700

    dpdk interface files

---
+++ interfaces/builtin/dpdk_observe.go
@@ -0,0 +1,49 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2017 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const dpdkObserveSummary = `allows using dpdk`
+
+const dpdkObserveBaseDeclarationSlots = `
+  dpdk-observe:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const dpdkObserveConnectedPlugAppArmor = `
+# Description: Allow access to dpdk.
+/sys/kernel/mm/hugepages/{,**} r,
+/dev/hugepages/{,**} r,
+/run/dpdk/{,**} r,
+/proc/{,**} r,
+`
+func init() {
+	registerIface(&commonInterface{
+		name:                     "dpdk-observe",
+		summary:                  dpdkObserveSummary,
+		implicitOnCore:           true,
+		implicitOnClassic:        true,
+		reservedForOS:            true,
+		baseDeclarationSlots:     dpdkObserveBaseDeclarationSlots,
+		connectedPlugAppArmor:    dpdkObserveConnectedPlugAppArmor,
+	})
+}
