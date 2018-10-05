c709a1193a656056929fa705d98640ea74f4b4d6
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-10-05 10:46:11 +0200

    wrappers: do not depend on network.taget in socket units, tweak generated units
    Depending on network.target (which is currently used as PrerequisiteTarget) may
    lead to dependncy cycles, see https://forum.snapcraft.io/t/7666 for details.
    Socket units should depend on mount unit only.
    
    While at it, tweak the generated unit files to not leave empty lines and extend
    tests to verify contents of generated socket unit files.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- wrappers/services.go
+++ wrappers/services.go
@@ -492,18 +492,19 @@ WantedBy={{.ServicesTarget}}
 
 func genServiceSocketFile(appInfo *snap.AppInfo, socketName string) []byte {
 	socketTemplate := `[Unit]
-# Auto-generated, DO NO EDIT
+# Auto-generated, DO NOT EDIT
 Description=Socket {{.SocketName}} for snap application {{.App.Snap.InstanceName}}.{{.App.Name}}
 Requires={{.MountUnit}}
-Wants={{.PrerequisiteTarget}}
-After={{.MountUnit}} {{.PrerequisiteTarget}}
+After={{.MountUnit}}
 X-Snappy=yes
 
 [Socket]
 Service={{.ServiceFileName}}
 FileDescriptorName={{.SocketInfo.Name}}
 ListenStream={{.ListenStream}}
-{{if .SocketInfo.SocketMode}}SocketMode={{.SocketInfo.SocketMode | printf "%04o"}}{{end}}
+{{- if .SocketInfo.SocketMode}}
+SocketMode={{.SocketInfo.SocketMode | printf "%04o"}}
+{{- end}}
 
 [Install]
 WantedBy={{.SocketsTarget}}
