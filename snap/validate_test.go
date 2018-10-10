e75acbc14f5a1657d77e769ca8c32bae26e68aa3
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-10-10 12:26:47 +0200

    snap: use snap/name for names validation
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- snap/validate_test.go
+++ snap/validate_test.go
@@ -68,92 +68,6 @@ func (s *ValidateSuite) TearDownTest(c *C) {
 	s.BaseTest.TearDownTest(c)
 }
 
-func (s *ValidateSuite) TestValidateName(c *C) {
-	validNames := []string{
-		"a", "aa", "aaa", "aaaa",
-		"a-a", "aa-a", "a-aa", "a-b-c",
-		"a0", "a-0", "a-0a",
-		"01game", "1-or-2",
-		// a regexp stresser
-		"u-94903713687486543234157734673284536758",
-	}
-	for _, name := range validNames {
-		err := ValidateName(name)
-		c.Assert(err, IsNil)
-	}
-	invalidNames := []string{
-		// name cannot be empty
-		"",
-		// names cannot be too long
-		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-		"xxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxx",
-		"1111111111111111111111111111111111111111x",
-		"x1111111111111111111111111111111111111111",
-		"x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x",
-		// a regexp stresser
-		"u-9490371368748654323415773467328453675-",
-		// dashes alone are not a name
-		"-", "--",
-		// double dashes in a name are not allowed
-		"a--a",
-		// name should not end with a dash
-		"a-",
-		// name cannot have any spaces in it
-		"a ", " a", "a a",
-		// a number alone is not a name
-		"0", "123",
-		// identifier must be plain ASCII
-		"日本語", "한글", "ру́сский язы́к",
-	}
-	for _, name := range invalidNames {
-		err := ValidateName(name)
-		c.Assert(err, ErrorMatches, `invalid snap name: ".*"`)
-	}
-}
-
-func (s *ValidateSuite) TestValidateInstanceName(c *C) {
-	validNames := []string{
-		// plain names are also valid instance names
-		"a", "aa", "aaa", "aaaa",
-		"a-a", "aa-a", "a-aa", "a-b-c",
-		// snap instance
-		"foo_bar",
-		"foo_0123456789",
-		"01game_0123456789",
-		"foo_1", "foo_1234abcd",
-	}
-	for _, name := range validNames {
-		err := ValidateInstanceName(name)
-		c.Assert(err, IsNil)
-	}
-	invalidNames := []string{
-		// invalid names are also invalid instance names, just a few
-		// samples
-		"",
-		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-		"xxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxx",
-		"a--a",
-		"a-",
-		"a ", " a", "a a",
-		"_",
-		"ру́сский_язы́к",
-	}
-	for _, name := range invalidNames {
-		err := ValidateInstanceName(name)
-		c.Assert(err, ErrorMatches, `invalid snap name: ".*"`)
-	}
-	invalidInstanceKeys := []string{
-		// the snap names are valid, but instance keys are not
-		"foo_", "foo_1-23", "foo_01234567890", "foo_123_456",
-		"foo__bar",
-	}
-	for _, name := range invalidInstanceKeys {
-		err := ValidateInstanceName(name)
-		c.Assert(err, ErrorMatches, `invalid instance key: ".*"`)
-	}
-
-}
-
 func (s *ValidateSuite) TestValidateVersion(c *C) {
 	validVersions := []string{
 		"0", "v1.0", "0.12+16.04.20160126-0ubuntu1",
@@ -255,24 +169,6 @@ func (s *ValidateSuite) TestValidateHook(c *C) {
 }
 
 // ValidateApp
-
-func (s *ValidateSuite) TestValidateAppName(c *C) {
-	validAppNames := []string{
-		"1", "a", "aa", "aaa", "aaaa", "Aa", "aA", "1a", "a1", "1-a", "a-1",
-		"a-a", "aa-a", "a-aa", "a-b-c", "0a-a", "a-0a",
-	}
-	for _, name := range validAppNames {
-		c.Check(ValidateApp(&AppInfo{Name: name}), IsNil)
-	}
-	invalidAppNames := []string{
-		"", "-", "--", "a--a", "a-", "a ", " a", "a a", "日本語", "한글",
-		"ру́сский язы́к", "ໄຂ່​ອີ​ສ​ເຕີ້", ":a", "a:", "a:a", "_a", "a_", "a_a",
-	}
-	for _, name := range invalidAppNames {
-		err := ValidateApp(&AppInfo{Name: name})
-		c.Assert(err, ErrorMatches, `cannot have ".*" as app name.*`)
-	}
-}
 
 func (s *ValidateSuite) TestValidateAppSockets(c *C) {
 	app := createSampleApp()
@@ -668,33 +564,6 @@ apps:
 	c.Check(err, ErrorMatches, `cannot have "foo\$" as alias name for app "foo" - use only letters, digits, dash, underscore and dot characters`)
 }
 
-func (s *ValidateSuite) TestValidateAlias(c *C) {
-	validAliases := []string{
-		"a", "aa", "aaa", "aaaa",
-		"a-a", "aa-a", "a-aa", "a-b-c",
-		"a0", "a-0", "a-0a",
-		"a_a", "aa_a", "a_aa", "a_b_c",
-		"a0", "a_0", "a_0a",
-		"01game", "1-or-2",
-		"0.1game", "1_or_2",
-	}
-	for _, alias := range validAliases {
-		err := ValidateAlias(alias)
-		c.Assert(err, IsNil)
-	}
-	invalidAliases := []string{
-		"",
-		"_foo",
-		"-foo",
-		".foo",
-		"foo$",
-	}
-	for _, alias := range invalidAliases {
-		err := ValidateAlias(alias)
-		c.Assert(err, ErrorMatches, `invalid alias name: ".*"`)
-	}
-}
-
 func (s *ValidateSuite) TestValidatePlugSlotName(c *C) {
 	const yaml1 = `
 name: invalid-plugs
@@ -1049,41 +918,6 @@ layout:
 	c.Assert(info.Layout, HasLen, 2)
 	err = ValidateLayoutAll(info)
 	c.Assert(err, IsNil)
-}
-
-func (s *ValidateSuite) TestValidateSocketName(c *C) {
-	validNames := []string{
-		"a", "aa", "aaa", "aaaa",
-		"a-a", "aa-a", "a-aa", "a-b-c",
-		"a0", "a-0", "a-0a",
-		"01game", "1-or-2",
-	}
-	for _, name := range validNames {
-		err := ValidateSocketName(name)
-		c.Assert(err, IsNil)
-	}
-	invalidNames := []string{
-		// name cannot be empty
-		"",
-		// dashes alone are not a name
-		"-", "--",
-		// double dashes in a name are not allowed
-		"a--a",
-		// name should not end with a dash
-		"a-",
-		// name cannot have any spaces in it
-		"a ", " a", "a a",
-		// a number alone is not a name
-		"0", "123",
-		// identifier must be plain ASCII
-		"日本語", "한글", "ру́сский язы́к",
-		// no null chars in the string are allowed
-		"aa-a\000-b",
-	}
-	for _, name := range invalidNames {
-		err := ValidateSocketName(name)
-		c.Assert(err, ErrorMatches, `invalid socket name: ".*"`)
-	}
 }
 
 func (s *YamlSuite) TestValidateAppStartupOrder(c *C) {
