b803e1c62350e76e7f62fd937ea443191cc496f7
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-10-18 19:43:13 +0200

    tests: create dummy per-user mount namespace using stdlib, not libmount
    This patch fixes an issue that only manifests itself on Ubuntu 14.04
    where /etc/mtab is a real file and not a symbolic link to /proc/mtab.
    On such system mount(1) which uses libmount internally, keeps track
    of all the operations by editing the mtab file. Sadly, our hackery inside
    snap-confine and snap-discard-ns is totally not editing mtab. This causes
    confusion when umount(1) is asked to detach a mount namespace file.
    
    The problem is that at that time the file is already unmounted, thanks
    to updated snap-discard-ns. Because the test had atificially created the
    .mnt file with touch and mount(1) there is now a stale mount entry in mtab.
    In the cleanup section of each test (actually the prepare section but
    conceptually it is the cleanup after previous test) we, via snapd.postrm
    or snap-mgmt.sh unmount and remove all the preserved mount namespaces.
    When umount(1) is given a file that is not a mount point it goes to
    search mtab, treating it as a mount _source_ (not target) and proceeds
    to umount the target of whatever is found.
    
    To avoid this issue, since we are not using libmount in snap-discard-ns
    or snap-confine, simply avoid using libmount driven mount(1) in the test
    as well.
    
    This patch implements a thin python wrapper around mount and uses it
    for the construction of the fake per-user mount namespace.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ tests/main/snap-discard-ns/mount.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Pure-python limited-use wrapper around the mount library function.
+"""
+from argparse import ArgumentParser
+from ctypes import CDLL, c_char_p, c_long, get_errno
+from ctypes.util import find_library
+from enum import IntEnum
+from os import strerror
+from sys import stderr
+
+__all__ = ('mount', )
+
+
+class MountOpts(IntEnum):
+    """MountOpts contain various flags for the mount system call."""
+
+    Bind = 4096
+
+
+def mount(source: str, target: str, fstype: str, flags: int = 0,
+          data: str = "") -> None:
+    """mount is a thin wrapper around the mount library function."""
+    libc_name = find_library("c")
+    if libc_name is None:
+        raise Exception("cannot find the C library")
+    libc = CDLL(libc_name, use_errno=True)
+    retval = libc.mount(
+            c_char_p(source.encode("UTF-8")),
+            c_char_p(target.encode("UTF-8")),
+            c_char_p(fstype.encode("UTF-8")),
+            c_long(flags),
+            None if data == "" else c_char_p(data.encode("UTF-8")))
+    if retval < 0:
+        errno = get_errno()
+        raise OSError(errno, strerror(errno))
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("source", help="path of the device or bind source")
+    parser.add_argument("target", help="path of the new mount point")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--bind", action="store_const", const="bind",
+                       help="create a new mount point of existing path")
+    group.add_argument("-t", "--type", help="filesystem type to mount")
+    opts = parser.parse_args()
+    if opts.bind is not None:
+        mount(opts.source, opts.target, "", MountOpts.Bind)
+    else:
+        mount(opts.source, opts.target, opts.fstype)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        print(err, file=stderr)
