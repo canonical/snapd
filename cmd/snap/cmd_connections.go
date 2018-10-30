31bdc93b879b9d65c7c4584598b7d6864ddc0f5c
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2018-10-30 13:55:18 +0100

    cmd/snap: add `snap connections` command
    Add new snap command - `snap connections` that shows current interface
    connections for all or just the selected snaps.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

---
+++ cmd/snap/cmd_connections.go
@@ -0,0 +1,132 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/snapcore/snapd/i18n"
+
+	"github.com/jessevdk/go-flags"
+)
+
+type cmdConnections struct {
+	clientMixin
+	All         bool `short:"a"`
+	Positionals struct {
+		Query interfacesSlotOrPlugSpec `skip-help:"true"`
+	} `positional-args:"true"`
+}
+
+var shortConnectionsHelp = i18n.G("List interface connections")
+var longConnectionsHelp = i18n.G(`
+List connections between slots and plugs. When passed an optional
+snap name, lists connection only for that snap.
+
+Pass -a to list slots/plugs that are not connected to anything.
+`)
+
+func init() {
+	addCommand("connections", shortConnectionsHelp, longConnectionsHelp, func() flags.Commander {
+		return &cmdConnections{}
+	}, map[string]string{
+		"a": i18n.G("Show all connections"),
+	}, []argDesc{{
+		// TRANSLATORS: This needs to be wrapped in <>s.
+		name: i18n.G("<snap>"),
+		// TRANSLATORS: This should not start with a lowercase letter.
+		desc: i18n.G("Constrain listing to a specific snap"),
+	}})
+}
+
+func endpoint(snap, name string) string {
+	if snap == "core" || snap == "snapd" || snap == "system" {
+		return ":" + name
+	}
+	return snap + ":" + name
+}
+
+func wantedSnapMatches(name, wanted string) bool {
+	if wanted == "system" {
+		switch name {
+		case "core", "snapd", "system":
+			return true
+		default:
+			return false
+		}
+	}
+	return wanted == name
+}
+
+func (x *cmdConnections) Execute(args []string) error {
+	if len(args) > 0 {
+		return ErrExtraArgs
+	}
+
+	ifaces, err := x.client.Connections()
+	if err != nil {
+		return err
+	}
+	if len(ifaces.Plugs) == 0 && len(ifaces.Slots) == 0 {
+		return fmt.Errorf(i18n.G("no interfaces found"))
+	}
+	if x.Positionals.Query.Name != "" {
+		return errors.New("filtering by slot/plug name is not supported")
+	}
+	wanted := x.Positionals.Query.Snap
+
+	matched := false
+	w := tabWriter()
+	fmt.Fprintln(w, i18n.G("Plug\tSlot\tInterface\tNotes"))
+
+	for _, plug := range ifaces.Plugs {
+		for _, slot := range plug.Connections {
+			if wanted != "" && !wantedSnapMatches(plug.Snap, wanted) && !wantedSnapMatches(slot.Snap, wanted) {
+				continue
+			}
+			matched = true
+			fmt.Fprintf(w, "%s\t%s\t%s\t-\n", endpoint(plug.Snap, plug.Name), endpoint(slot.Snap, slot.Name), plug.Interface)
+		}
+		if len(plug.Connections) == 0 && x.All {
+			if wanted != "" && !wantedSnapMatches(plug.Snap, wanted) {
+				continue
+			}
+			matched = true
+			fmt.Fprintf(w, "%s\t%s\t%s\t-\n", endpoint(plug.Snap, plug.Name), "-", plug.Interface)
+		}
+	}
+	for _, slot := range ifaces.Slots {
+		if wanted != "" && !wantedSnapMatches(slot.Snap, wanted) {
+			continue
+		}
+		if len(slot.Connections) == 0 && x.All {
+			matched = true
+			fmt.Fprintf(w, "%s\t%s\t%s\t-\n", "-", endpoint(slot.Snap, slot.Name), slot.Interface)
+		}
+	}
+
+	if matched {
+		w.Flush()
+	} else {
+		fmt.Fprintln(Stdout, "no interface connections found")
+	}
+	return nil
+}
---
+++ cmd/snap/cmd_connections_test.go
@@ -0,0 +1,309 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main_test
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/client"
+	. "github.com/snapcore/snapd/cmd/snap"
+)
+
+func (s *SnapSuite) TestConnectionsNoneConnectedPlugs(c *C) {
+	s.RedirectClientToTestServer(func(w http.ResponseWriter, r *http.Request) {
+		c.Check(r.Method, Equals, "GET")
+		c.Check(r.URL.Path, Equals, "/v2/interfaces")
+		body, err := ioutil.ReadAll(r.Body)
+		c.Check(err, IsNil)
+		c.Check(body, DeepEquals, []byte{})
+		EncodeResponseBody(c, w, map[string]interface{}{
+			"type": "sync",
+			"result": client.Connections{
+				Plugs: []client.Plug{
+					{
+						Snap:      "keyboard-lights",
+						Name:      "capslock-led",
+						Interface: "leds",
+					},
+				},
+			},
+		})
+	})
+	rest, err := Parser(Client()).ParseArgs([]string{"connections"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout := "" +
+		"no interface connections found\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// with -a shows unconnected plugs
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "-a"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                          Slot  Interface  Notes\n" +
+		"keyboard-lights:capslock-led  -     leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+}
+
+func (s *SnapSuite) TestConnectionsNoneConnectedSlots(c *C) {
+	s.RedirectClientToTestServer(func(w http.ResponseWriter, r *http.Request) {
+		c.Check(r.Method, Equals, "GET")
+		c.Check(r.URL.Path, Equals, "/v2/interfaces")
+		body, err := ioutil.ReadAll(r.Body)
+		c.Check(err, IsNil)
+		c.Check(body, DeepEquals, []byte{})
+		EncodeResponseBody(c, w, map[string]interface{}{
+			"type": "sync",
+			"result": client.Connections{
+				Slots: []client.Slot{
+					{
+						Snap:      "leds-provider",
+						Name:      "capslock-led",
+						Interface: "leds",
+					},
+				},
+			},
+		})
+	})
+	rest, err := Parser(Client()).ParseArgs([]string{"connections"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout := "" +
+		"no interface connections found\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// with -a shows unconnected plugs
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "-a"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug  Slot                        Interface  Notes\n" +
+		"-     leds-provider:capslock-led  leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+}
+
+func (s *SnapSuite) TestConnectionsSomeConnectedSystem(c *C) {
+	s.RedirectClientToTestServer(func(w http.ResponseWriter, r *http.Request) {
+		c.Check(r.Method, Equals, "GET")
+		c.Check(r.URL.Path, Equals, "/v2/interfaces")
+		body, err := ioutil.ReadAll(r.Body)
+		c.Check(err, IsNil)
+		c.Check(body, DeepEquals, []byte{})
+		EncodeResponseBody(c, w, map[string]interface{}{
+			"type": "sync",
+			"result": client.Connections{
+				Plugs: []client.Plug{
+					{
+						Snap:      "keyboard-lights",
+						Name:      "capslock",
+						Interface: "leds",
+						Connections: []client.SlotRef{{
+							Snap: "leds-provider",
+							Name: "capslock-led",
+						}},
+					}, {
+						Snap:      "keyboard-lights",
+						Name:      "numlock",
+						Interface: "leds",
+						Connections: []client.SlotRef{{
+							Snap: "system",
+							Name: "numlock-led",
+						}},
+					},
+				},
+				Slots: []client.Slot{
+					{
+						Snap:      "system",
+						Name:      "capslock-led",
+						Interface: "leds",
+					},
+				},
+			},
+		})
+	})
+	rest, err := Parser(Client()).ParseArgs([]string{"connections"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout := "" +
+		"Plug                      Slot                        Interface  Notes\n" +
+		"keyboard-lights:capslock  leds-provider:capslock-led  leds       -\n" +
+		"keyboard-lights:numlock   :numlock-led                leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// with -a shows unconnected plugs
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "-a"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                      Slot                        Interface  Notes\n" +
+		"keyboard-lights:capslock  leds-provider:capslock-led  leds       -\n" +
+		"keyboard-lights:numlock   :numlock-led                leds       -\n" +
+		"-                         :capslock-led               leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+}
+
+func (s *SnapSuite) TestConnectionsUnsupportedFiltering(c *C) {
+	_, err := Parser(Client()).ParseArgs([]string{"connections", "foo:bar"})
+	c.Assert(err, ErrorMatches, "filtering by slot/plug name is not supported")
+	c.Assert(s.Stdout(), Equals, "")
+	c.Assert(s.Stderr(), Equals, "")
+}
+
+func (s *SnapSuite) TestConnectionsFiltering(c *C) {
+	s.RedirectClientToTestServer(func(w http.ResponseWriter, r *http.Request) {
+		c.Check(r.Method, Equals, "GET")
+		c.Check(r.URL.Path, Equals, "/v2/interfaces")
+		body, err := ioutil.ReadAll(r.Body)
+		c.Check(err, IsNil)
+		c.Check(body, DeepEquals, []byte{})
+		EncodeResponseBody(c, w, map[string]interface{}{
+			"type": "sync",
+			"result": client.Connections{
+				Plugs: []client.Plug{
+					{
+						Snap:      "keyboard-lights",
+						Name:      "capslock",
+						Interface: "leds",
+						Connections: []client.SlotRef{{
+							Snap: "leds-provider",
+							Name: "capslock-led",
+						}},
+					}, {
+						Snap:      "keyboard-lights",
+						Name:      "numlock",
+						Interface: "leds",
+						Connections: []client.SlotRef{{
+							Snap: "core",
+							Name: "numlock-led",
+						}},
+					}, {
+						Snap:      "mouse-buttons",
+						Name:      "left-button",
+						Interface: "buttons",
+						Connections: []client.SlotRef{{
+							Snap: "core",
+							Name: "mouse-left-button",
+						}},
+					}, {
+						Snap:      "mouse-buttons",
+						Name:      "right-button",
+						Interface: "buttons",
+					},
+				},
+				Slots: []client.Slot{
+					{
+						Snap:      "core",
+						Name:      "capslock-led",
+						Interface: "leds",
+					},
+				},
+			},
+		})
+	})
+
+	// filter by snap name, show only connected interfaces
+	rest, err := Parser(Client()).ParseArgs([]string{"connections", "mouse-buttons"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout := "" +
+		"Plug                       Slot                Interface  Notes\n" +
+		"mouse-buttons:left-button  :mouse-left-button  buttons    -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// filter by snap name, show all interfaces
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "mouse-buttons", "-a"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                        Slot                Interface  Notes\n" +
+		"mouse-buttons:left-button   :mouse-left-button  buttons    -\n" +
+		"mouse-buttons:right-button  -                   buttons    -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// filter by snap name, snap appearing only on the slot side
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "leds-provider"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                      Slot                        Interface  Notes\n" +
+		"keyboard-lights:capslock  leds-provider:capslock-led  leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// filter by system snap alias
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "system"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                       Slot                Interface  Notes\n" +
+		"keyboard-lights:numlock    :numlock-led        leds       -\n" +
+		"mouse-buttons:left-button  :mouse-left-button  buttons    -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// filter by system snap unaliased name
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "core"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+
+	// filter by system snap alias, show all
+	rest, err = Parser(Client()).ParseArgs([]string{"connections", "system", "-a"})
+	c.Assert(err, IsNil)
+	c.Assert(rest, DeepEquals, []string{})
+	expectedStdout = "" +
+		"Plug                       Slot                Interface  Notes\n" +
+		"keyboard-lights:numlock    :numlock-led        leds       -\n" +
+		"mouse-buttons:left-button  :mouse-left-button  buttons    -\n" +
+		"-                          :capslock-led       leds       -\n"
+	c.Assert(s.Stdout(), Equals, expectedStdout)
+	c.Assert(s.Stderr(), Equals, "")
+
+	s.ResetStdStreams()
+}
