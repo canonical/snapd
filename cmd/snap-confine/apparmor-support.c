4b5d44aca67d51d7b73a6c189e50c7c78ef558f3
Jamie Strandboge <jamie@ubuntu.com>
2018-11-07 17:04:42 +0000

    snap-confine: write 'apparmor' to display_lsm when available
    LSM stacking allows systems with multiple configured LSMs. On these
    systems, all LSMs are consulted for access with one set as the 'primary
    LSM' that applications normally see (configurable via the kernel command
    line). Eg, a Fedora system with stacking patches in the kernel and
    AppArmor userspace available might have both SELinux and AppArmor
    enabled, but SELinux would be the primary LSM, whereas on Ubuntu with
    SELinux enabled, AppArmor would be the primary LSM. So that existing
    userspace applications do not have to change, all applications by
    default see the primary LSM, but applications can adjust the display LSM
    so that it and all child processes see an alternate LSM.
    
    Adjust snap-confine to unconditionally set the display LSM to 'apparmor'
    when AppArmor is in use so that snaps don't have to worry about when
    AppArmor isn't the primary LSM.
    
    Note, the LSM stacking patches are not yet upstream but are available in
    Ubuntu kernels as a technology preview since Ubuntu 17.10. As such, the
    behavior does not change non-Ubuntu systems or default Ubuntu systems
    (since the display LSM is already "apparmor"). This patch is meant to
    allow experimenting with the feature.

--- cmd/snap-confine/apparmor-support.c
+++ cmd/snap-confine/apparmor-support.c
@@ -23,6 +23,7 @@
 
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #ifdef HAVE_APPARMOR
 #include <sys/apparmor.h>
 #endif				// ifdef HAVE_APPARMOR
@@ -36,6 +37,7 @@
 #define SC_AA_COMPLAIN_STR "complain"
 #define SC_AA_MIXED_STR "mixed"
 #define SC_AA_UNCONFINED_STR "unconfined"
+#define SC_AA_DISPLAY_LSM_STR "/proc/self/attr/display_lsm"
 
 void sc_init_apparmor_support(struct sc_apparmor *apparmor)
 {
@@ -113,6 +115,11 @@ sc_maybe_aa_change_onexec(struct sc_apparmor *apparmor, const char *profile)
 	if (apparmor->mode == SC_AA_NOT_APPLICABLE) {
 		return;
 	}
+	if (access(SC_AA_DISPLAY_LSM_STR, F_OK) == 0) {
+		debug("setting %s to 'apparmor'", SC_AA_DISPLAY_LSM_STR);
+		write_string_to_file(SC_AA_DISPLAY_LSM_STR, "apparmor");
+	}
+
 	debug("requesting changing of apparmor profile on next exec to %s",
 	      profile);
 	if (aa_change_onexec(profile) < 0) {
