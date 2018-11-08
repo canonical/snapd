c63719f967dbc503755e9da2b9413934b71f8969
Pawel Stolowski <stolowski@gmail.com>
2018-11-08 13:34:15 +0100

    Handler for hotplug-disconnect task.

--- overlord/ifacestate/ifacemgr.go
+++ overlord/ifacestate/ifacemgr.go
@@ -81,6 +81,7 @@ func Manager(s *state.State, hookManager *hookstate.HookManager, runner *state.T
 	addHandler("auto-connect", m.doAutoConnect, m.undoAutoConnect)
 	addHandler("gadget-connect", m.doGadgetConnect, nil)
 	addHandler("auto-disconnect", m.doAutoDisconnect, nil)
+	addHandler("hotplug-disconnect", m.doHotplugDisconnect, nil)
 
 	// helper for ubuntu-core -> core
 	addHandler("transition-ubuntu-core", m.doTransitionUbuntuCore, m.undoTransitionUbuntuCore)
