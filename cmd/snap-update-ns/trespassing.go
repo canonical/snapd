c2576f0e54a0daf65ced50ffa6979aaa5ef0ec47
Zygmunt Krynicki <me@zygoon.pl>
2018-11-09 12:43:30 +0100

    cmd/snap-confine,snap-update-ns: discard quirks
    Recent discussion about LXD has resurrected the topic of quirks. Quirks
    were created in response to a bug where a snap in devmode being unable
    to interact with with LXD running as a classic package. At the time it
    was a big deal so we implemented the quirk and everything was fine.
    
    Time passed and LXD moved away from storing the socket in that place. It
    also moved away from shipping as a classic package as well. Since we
    never allowed access to /var/lib/lxd via any interfaces and it is
    unlikely anyone still relies on this quirk I think it should be dropped
    now.
    
    Removing should speed up execution of all snaps by a small amount, since
    we don't need to construct a writable mimic in /var/lib anymore.  We
    also shrink snap-confine by removing a good chunk of C code.
    
    https://bugs.launchpad.net/snap-confine/+bug/1613845
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/snap-update-ns/trespassing.go
+++ cmd/snap-update-ns/trespassing.go
@@ -244,10 +244,5 @@ func isPrivateTmpfsCreatedBySnapd(dirName string, fsData *syscall.Statfs_t, file
 			return change.Action == Mount
 		}
 	}
-	// TODO: As a special exception, assume that a tmpfs over /var/lib is
-	// trusted. This tmpfs is created by snap-confine as a "quirk" to support
-	// a particular behavior of LXD.  Once the quirk is migrated to a mount
-	// profile (or removed entirely if no longer necessary) the following code
-	// fragment can go away.
-	return dirName == "/var/lib"
+	return false
 }
