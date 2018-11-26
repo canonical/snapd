3ca4274b4ead406bcb39f42600bc6c2e4e48919e
Michael Vogt <mvo@ubuntu.com>
2018-11-26 09:22:50 +0100

    snapstate: only run fontconfig update on classic

--- overlord/snapstate/backend/link.go
+++ overlord/snapstate/backend/link.go
@@ -68,8 +68,11 @@ func (b Backend) LinkSnap(info *snap.Info, model *asserts.Model) error {
 		return err
 	}
 
-	if err := updateFontconfigCaches(); err != nil {
-		logger.Noticef("cannot update fontconfig cache: %v", err)
+	// fontconfig is only relevant on classic
+	if release.OnClassic {
+		if err := updateFontconfigCaches(); err != nil {
+			logger.Noticef("cannot update fontconfig cache: %v", err)
+		}
 	}
 
 	// XXX/TODO: this needs to be a task with proper undo and tests!
--- overlord/snapstate/backend/link_test.go
+++ overlord/snapstate/backend/link_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/osutil"
 	"github.com/snapcore/snapd/progress"
+	"github.com/snapcore/snapd/release"
 	"github.com/snapcore/snapd/snap"
 	"github.com/snapcore/snapd/snap/snaptest"
 	"github.com/snapcore/snapd/systemd"
@@ -313,15 +314,25 @@ func (s *linkCleanupSuite) TestLinkCleanupOnSystemctlFail(c *C) {
 	}
 }
 
-func (s *linkCleanupSuite) TestLinkRunsUpdateFontconfigCaches(c *C) {
-	var updateFontconfigCaches int
-	restore := backend.MockUpdateFontconfigCaches(func() error {
-		updateFontconfigCaches += 1
-		return nil
-	})
-	defer restore()
+func (s *linkCleanupSuite) TestLinkRunsUpdateFontconfigCachesClassic(c *C) {
+	for _, onClassic := range []bool{false, true} {
 
-	err := s.be.LinkSnap(s.info, nil)
-	c.Assert(err, IsNil)
-	c.Assert(updateFontconfigCaches, Equals, 1)
+		restore := release.MockOnClassic(onClassic)
+		defer restore()
+
+		var updateFontconfigCaches int
+		restore = backend.MockUpdateFontconfigCaches(func() error {
+			updateFontconfigCaches += 1
+			return nil
+		})
+		defer restore()
+
+		err := s.be.LinkSnap(s.info, nil)
+		c.Assert(err, IsNil)
+		if onClassic {
+			c.Assert(updateFontconfigCaches, Equals, 1)
+		} else {
+			c.Assert(updateFontconfigCaches, Equals, 0)
+		}
+	}
 }
