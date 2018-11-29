05e74b99e6ec1f2e48386e025d93a2805a83ffbe
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2018-11-29 13:43:30 +0100

    overlord/ifacestate: allow mocking profilesNeedRegeneration
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- overlord/ifacestate/helpers.go
+++ overlord/ifacestate/helpers.go
@@ -139,7 +139,7 @@ func (m *InterfaceManager) addSnaps(snaps []*snap.Info) error {
 	return nil
 }
 
-func profilesNeedRegeneration() bool {
+func profilesNeedRegenerationImpl() bool {
 	mismatch, err := interfaces.SystemKeyMismatch()
 	if err != nil {
 		logger.Noticef("error trying to compare the snap system key: %v", err)
@@ -147,6 +147,8 @@ func profilesNeedRegeneration() bool {
 	}
 	return mismatch
 }
+
+var profilesNeedRegeneration = profilesNeedRegenerationImpl
 
 // regenerateAllSecurityProfiles will regenerate all security profiles.
 func (m *InterfaceManager) regenerateAllSecurityProfiles() error {
