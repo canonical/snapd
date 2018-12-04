447c0f4ff89535138a457228922830679996b682
James Henstridge <james@jamesh.id.au>
2018-12-04 19:16:24 +0800

    tests/lib/snaps: update test snaps for attribute name change

---
+++ tests/lib/snaps/test-snapd-dbus-service/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="test-snapd-dbus-serice",
+    version="1.0",
+    scripts=[
+        "bin/test-snapd-dbus-service"
+    ],
+)
