aea157ce6c88535a648cc26fa4f731cbc8cc5022
Pawel Stolowski <stolowski@gmail.com>
2018-12-17 16:38:21 +0100

    Include interface name in the hotplug-disconnect task summary.

--- overlord/ifacestate/hotplug.go
+++ overlord/ifacestate/hotplug.go
@@ -180,7 +180,7 @@ func suggestedSlotName(devinfo *hotplug.HotplugDeviceInfo, fallbackName string) 
 
 // updateDevice creates tasks to disconnect slots of given device, update the slot in the repository, then connect it back.
 func updateDevice(st *state.State, ifaceName, hotplugKey string, newAttrs map[string]interface{}) *state.TaskSet {
-	hotplugDisconnect := st.NewTask("hotplug-disconnect", fmt.Sprintf("Disable connections of device %q", hotplugKey))
+	hotplugDisconnect := st.NewTask("hotplug-disconnect", fmt.Sprintf("Disable connections for interface %s, hotplug key %q", ifaceName, hotplugKey))
 	setHotplugAttrs(hotplugDisconnect, ifaceName, hotplugKey)
 
 	updateSlot := st.NewTask("hotplug-update-slot", fmt.Sprintf("Update slot of interface %s, hotplug key %q", ifaceName, hotplugKey))
@@ -188,7 +188,7 @@ func updateDevice(st *state.State, ifaceName, hotplugKey string, newAttrs map[st
 	updateSlot.Set("slot-attrs", newAttrs)
 	updateSlot.WaitFor(hotplugDisconnect)
 
-	hotplugConnect := st.NewTask("hotplug-connect", fmt.Sprintf("Recreate connections of interface %s hotplug key %s", ifaceName, hotplugKey))
+	hotplugConnect := st.NewTask("hotplug-connect", fmt.Sprintf("Recreate connections of interface %s hotplug key %q", ifaceName, hotplugKey))
 	setHotplugAttrs(hotplugConnect, ifaceName, hotplugKey)
 	hotplugConnect.WaitFor(updateSlot)
 
