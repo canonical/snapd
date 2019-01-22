1052b81130456ad109ea7af12b214c12e5b7a475
Michael Vogt <mvo@ubuntu.com>
2019-01-22 11:29:39 +0100

    tests: add in-snap consumer for test-snapd-dbus-provider

---
+++ tests/lib/snaps/test-snapd-dbus-provider/consumer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import dbus
+import sys
+
+def run(bus):
+    obj = bus.get_object("com.dbustest.HelloWorld", "/com/dbustest/HelloWorld")
+    print(obj.SayHello(dbus_interface="com.dbustest.HelloWorld"))
+
+if __name__ == "__main__":
+    if sys.argv[1] == "system":
+        bus = dbus.SystemBus()
+    else:
+        bus = dbus.SessionBus()
+    run(bus)
