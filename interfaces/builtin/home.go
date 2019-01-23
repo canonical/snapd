59ae9be760b3de9e998758beae2bb6072073dbe8
Jamie Strandboge <jamie@ubuntu.com>
2019-01-23 22:05:40 +0000

    interfaces/home: allow dac_override with 'read: all'
    Reference:
    https://forum.snapcraft.io/t/plex-not-deleting-files/8010/17

--- interfaces/builtin/home.go
+++ interfaces/builtin/home.go
@@ -82,6 +82,7 @@ audit deny @{HOME}/bin/{,**} wl,
 
 const homeConnectedPlugAppArmorWithAllRead = `
 # Allow non-owner read to non-hidden and non-snap files and directories
+capability dac_override,
 @{HOME}/               r,
 @{HOME}/[^s.]**        r,
 @{HOME}/s[^n]**        r,
