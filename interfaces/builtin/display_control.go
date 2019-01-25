257080b350c669fc1276baf612bc3c1ebe7871d4
Jamie Strandboge <jamie@ubuntu.com>
2019-01-25 16:03:12 +0000

    add DBus methods for controlling brightness and dynamically detect backlights
    The kernel supports many different backlights with differing names in
    /sys/devices, so listing them in static rules is cumbersome and there is
    a maintenance burden of keeping in sync with new devices. The kernel
    will put a symlink under /sys/class/backlight for each backlight, so we
    can dereference the symlink for the /sys/devices paths. This works well
    for permanently attached devices like laptop screens or other primary
    screens since they aren't expected to change (even if they did on
    reboot, snapd would notice this and adjust the policy).
    
    snapd will not notice a new hotplugged monitor (but would if it was
    plugged in on boot). A future update could utilize snapd's hotplug
    support.

--- interfaces/builtin/display_control.go
+++ interfaces/builtin/display_control.go
@@ -19,6 +19,16 @@
 
 package builtin
 
+import (
+	"bytes"
+	"fmt"
+	"path"
+	"path/filepath"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+)
+
 const displayControlSummary = `allows configuring display parameters`
 
 const displayControlBaseDeclarationSlots = `
@@ -29,18 +39,93 @@ const displayControlBaseDeclarationSlots = `
     deny-auto-connection: true
 `
 
+// gnome-settings-daemon also provides an API via setting the Brightness
+// property via a Set() method on the org.gnome.SettingsDaemon.Power.Screen
+// interface, but we can't mediate member data. This could instead be supported
+// via userd...
 const displayControlConnectedPlugAppArmor = `
 # Description: This interface allows getting information about a connected
 # display and setting parameters like backlight brightness.
 
+# keyboard backlight key
+/sys/class/leds/ r,
+/sys/devices/**/leds/**kbd_backlight/{,**} r,
+/sys/devices/**/leds/**kbd_backlight/brightness w,
+
+# upower
+#include <abstractions/dbus-strict>
+dbus (send)
+    bus=system
+    path=/org/freedesktop/UPower/KbdBacklight
+    interface=org.freedesktop.DBus.Introspectable
+    member=Introspect
+    peer=(label=unconfined),
+dbus (send)
+    bus=system
+    path=/org/freedesktop/UPower/KbdBacklight
+    interface=org.freedesktop.UPower.KbdBacklight
+    member={GetBrightness,GetMaxBrightness,SetBrightness}
+    peer=(label=unconfined),
+
+# gnome-settings-daemon
+#include <abstractions/dbus-session-strict>
+dbus (send)
+    bus=session
+    path=/org/gnome/SettingsDaemon/Power
+    interface=org.freedesktop.DBus.Introspectable
+    member=Introspect
+    peer=(label=unconfined),
+dbus (send)
+    bus=session
+    path=/org/gnome/SettingsDaemon/Power
+    interface=org.gnome.SettingsDaemon.Power.Screen
+    member=Step{Down,Up}
+    peer=(label=unconfined),
+
 /sys/class/backlight/ r,
-/sys/devices/pci**/{backlight/acpi_video[0-9]*,intel_backlight}/{,**} r,
-/sys/devices/pci**/{backlight/acpi_video[0-9]*,intel_backlight}/bl_power w,
-/sys/devices/pci**/{backlight/acpi_video[0-9]*,intel_backlight}/brightness w,
 `
 
+type displayControlInterface struct {
+	commonInterface
+}
+
+func (iface *displayControlInterface) dereferencedBacklightPaths() []string {
+	var paths []string
+	sysClass := "/sys/class/backlight"
+	dirs, err := readDir(sysClass)
+	if err != nil {
+		return paths
+	}
+
+	for _, s := range dirs {
+		p, err := evalSymlinks(filepath.Join(sysClass, s.Name()))
+		if err != nil {
+			continue
+		}
+		paths = append(paths, filepath.Clean(p))
+	}
+	return paths
+}
+
+func (iface *displayControlInterface) AppArmorConnectedPlug(spec *apparmor.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
+	// add the static rules
+	spec.AddSnippet(displayControlConnectedPlugAppArmor)
+
+	// add the detected rules
+	for _, p := range iface.dereferencedBacklightPaths() {
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, "# autodetected backlight: %s\n", path.Base(p))
+		fmt.Fprintf(&buf, "%s/{,**} r,\n", p)
+		fmt.Fprintf(&buf, "%s/bl_power w,\n", p)
+		fmt.Fprintf(&buf, "%s/brightness w,\n", p)
+		spec.AddSnippet(buf.String())
+	}
+
+	return nil
+}
+
 func init() {
-	registerIface(&commonInterface{
+	registerIface(&displayControlInterface{commonInterface{
 		name:                  "display-control",
 		summary:               displayControlSummary,
 		implicitOnCore:        true,
@@ -48,5 +133,5 @@ func init() {
 		baseDeclarationSlots:  displayControlBaseDeclarationSlots,
 		connectedPlugAppArmor: displayControlConnectedPlugAppArmor,
 		reservedForOS:         true,
-	})
+	}})
 }
