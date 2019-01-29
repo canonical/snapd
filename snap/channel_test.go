fdf9ed240b9a5955c9ca19aec3a50a6b29bfdf49
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-01-29 11:05:23 +0100

    snap: tweak channel parser tests
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- snap/channel_test.go
+++ snap/channel_test.go
@@ -164,29 +164,24 @@ func (s storeChannelSuite) TestClean(c *C) {
 }
 
 func (s storeChannelSuite) TestParseChannelErrors(c *C) {
-	_, err := snap.ParseChannel("", "")
-	c.Check(err, ErrorMatches, "channel name cannot be empty")
-
-	_, err = snap.ParseChannel("1.0////", "")
-	c.Check(err, ErrorMatches, "channel name has too many components: 1.0////")
-
-	_, err = snap.ParseChannel("1.0/cand", "invalid risk in channel name: 1.0/cand")
-	c.Check(err, ErrorMatches, "invalid risk in channel name: 1.0/cand")
-
-	_, err = snap.ParseChannel("fix//hotfix", "")
-	c.Check(err, ErrorMatches, "invalid risk in channel name: fix//hotfix")
-
-	_, err = snap.ParseChannel("/stable/", "")
-	c.Check(err, ErrorMatches, "invalid track in channel name: /stable/")
-
-	_, err = snap.ParseChannel("//stable", "")
-	c.Check(err, ErrorMatches, "invalid risk in channel name: //stable")
-
-	_, err = snap.ParseChannel("stable/", "")
-	c.Check(err, ErrorMatches, "invalid branch in channel name: stable/")
-
-	_, err = snap.ParseChannel("/stable", "")
-	c.Check(err, ErrorMatches, "invalid track in channel name: /stable")
+	for _, tc := range []struct {
+		channel string
+		err     string
+	}{
+		{"", "channel name cannot be empty"},
+		{"1.0////", "channel name has too many components: 1.0////"},
+		{"1.0/cand", "invalid risk in channel name: 1.0/cand"},
+		{"fix//hotfix", "invalid risk in channel name: fix//hotfix"},
+		{"/stable/", "invalid track in channel name: /stable/"},
+		{"//stable", "invalid risk in channel name: //stable"},
+		{"stable/", "invalid branch in channel name: stable/"},
+		{"/stable", "invalid track in channel name: /stable"},
+	} {
+		_, err := snap.ParseChannel(tc.channel, "")
+		c.Check(err, ErrorMatches, tc.err)
+		_, err = snap.ParseChannelVerbatim(tc.channel, "")
+		c.Check(err, ErrorMatches, tc.err)
+	}
 }
 
 func (s *storeChannelSuite) TestString(c *C) {
