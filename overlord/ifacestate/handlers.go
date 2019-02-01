caf2a059a4803821c19fb5fc415e61f978de74af
Pawel Stolowski <stolowski@gmail.com>
2019-02-01 10:56:13 +0100

    Extra tests.

--- overlord/ifacestate/handlers.go
+++ overlord/ifacestate/handlers.go
@@ -1550,12 +1550,16 @@ func (m *InterfaceManager) doHotplugAddSlot(task *state.Task, _ *tomb.Tomb) erro
 	}
 
 	var slotSpec hotplug.RequestedSlotSpec
-	var devinfo hotplug.HotplugDeviceInfo
 	if err := task.Get("slot-spec", &slotSpec); err != nil {
 		return fmt.Errorf("internal error: cannot get hotplug slot specification from task attributes: %s", err)
 	}
-	if err := task.Get("device-info", &devinfo); err != nil {
+	var devinfoData map[string]string
+	if err := task.Get("device-info", &devinfoData); err != nil {
 		return fmt.Errorf("internal error: cannot get hotplug device info from task attributes: %s", err)
+	}
+	devinfo, err := hotplug.NewHotplugDeviceInfo(devinfoData)
+	if err != nil {
+		return fmt.Errorf("internal error: cannot create hotplug device info: %v", err.Error())
 	}
 
 	stateSlots, err := getHotplugSlots(st)
@@ -1593,6 +1597,7 @@ func (m *InterfaceManager) doHotplugAddSlot(task *state.Task, _ *tomb.Tomb) erro
 				return fmt.Errorf("cannot restore hotplug slot %q for interface %s: %s", slot.Name, slot.Interface, err)
 			}
 			slot.HotplugGone = false
+			slot.StaticAttrs = slotSpec.Attrs
 			stateSlots[slot.Name] = slot
 			setHotplugSlots(st, stateSlots)
 
@@ -1611,7 +1616,7 @@ func (m *InterfaceManager) doHotplugAddSlot(task *state.Task, _ *tomb.Tomb) erro
 	// New slot. Determine the name: use name provided by slot spec if set, otherwise use auto-generated name
 	proposedName := slotSpec.Name
 	if proposedName == "" {
-		proposedName = suggestedSlotName(&devinfo, ifaceName)
+		proposedName = suggestedSlotName(devinfo, ifaceName)
 	}
 	proposedName = ensureUniqueName(proposedName, func(name string) bool {
 		if slot, ok := stateSlots[name]; ok {
