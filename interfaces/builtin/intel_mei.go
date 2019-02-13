0c15a327c97fa45e7986e61c9c6ab7d930854d9d
Michael Vogt <mvo@ubuntu.com>
2019-02-13 14:55:55 +0100

    interfaces: add url of kernel intel MEI documentation to intel-mei interface

--- interfaces/builtin/intel_mei.go
+++ interfaces/builtin/intel_mei.go
@@ -19,6 +19,9 @@
 
 package builtin
 
+// Details about the Intel MEI can be found here:
+// https://www.kernel.org/doc/Documentation/misc-devices/mei/mei.txt
+
 const intelMEISummary = `allows access to the intel MEI management interface`
 
 const intelMEIBaseDeclarationSlots = `
