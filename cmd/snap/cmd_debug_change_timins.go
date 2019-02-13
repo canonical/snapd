1734736cad2e1af39e92ccd0f05cd0215c06ce11
Michael Vogt <mvo@ubuntu.com>
2019-02-13 13:09:02 +0100

    snap: add new `snap debug chagne-timings` command
    This adds a new debug endpoint that gives details about the timings
    of a given change. It looks like:
    ```
    $ snap  debug change-timings 567
    Status  Doing  Undoing  Summary
    Done    6ms    -        Stop snap "test-snapd-tools" (6) services
    Done    25ms   -        Remove aliases for snap "test-snapd-tools"
    Done    16ms   -        Make snap "test-snapd-tools" (6) unavailable to the system
    Done    17ms   -        Remove security profiles of snap "test-snapd-tools"
    ```

---
+++ cmd/snap/cmd_debug_change_timins.go
@@ -0,0 +1,87 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2019 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jessevdk/go-flags"
+)
+
+type cmdChangeTimings struct {
+	changeIDMixin
+}
+
+func init() {
+	addDebugCommand("change-timings",
+		"Get the timings of the tasks of a change",
+		"Details about tte time each task runs.",
+		func() flags.Commander {
+			return &cmdChangeTimings{}
+		}, changeIDMixinOptDesc, changeIDMixinArgDesc)
+}
+
+func (x *cmdChangeTimings) Execute(args []string) error {
+	if len(args) > 0 {
+		return ErrExtraArgs
+	}
+	chgid, err := x.GetChangeID()
+	if err != nil {
+		return err
+	}
+
+	// gather debug timings first
+	var timings map[string]struct {
+		DoingTime, UndoingTime time.Duration
+	}
+	params := struct {
+		ChgID string `json:"chg-id"`
+	}{
+		ChgID: chgid,
+	}
+	if err := x.client.Debug("change-timings", params, &timings); err != nil {
+		return err
+	}
+
+	// now combine with the other data about the change
+	chg, err := x.client.Change(chgid)
+	if err != nil {
+		return err
+	}
+	w := tabWriter()
+	fmt.Fprintf(w, "Status\tDoing\tUndoing\tSummary\n")
+	for _, t := range chg.Tasks {
+		doingTime := timings[t.ID].DoingTime.Round(time.Millisecond).String()
+		if timings[t.ID].DoingTime == 0 {
+			doingTime = "-"
+		}
+		undoingTime := timings[t.ID].UndoingTime.Round(time.Millisecond).String()
+		if timings[t.ID].UndoingTime == 0 {
+			undoingTime = "-"
+		}
+		summary := t.Summary
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", t.Status, doingTime, undoingTime, summary)
+	}
+	w.Flush()
+	fmt.Fprintln(Stdout)
+
+	return nil
+}
