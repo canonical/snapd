54cd520209ad8137766e2ffc87d2aa52e61dc6f3
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-03-06 12:11:00 +0100

    interfaces: populate system-key with seccomp compiler version
    Include seccomp profile compiler version in system key. This should allow us to
    trigger profile regeneration whenever the compiler has changed, or
    libseccomp (which can be linked dynamically) was updated.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- interfaces/system_key.go
+++ interfaces/system_key.go
@@ -32,6 +32,7 @@ import (
 	"github.com/snapcore/snapd/logger"
 	"github.com/snapcore/snapd/osutil"
 	"github.com/snapcore/snapd/release"
+	seccomp_compiler "github.com/snapcore/snapd/sandbox/seccomp"
 )
 
 // ErrSystemKeyIncomparableVersions indicates that the system-key
@@ -70,12 +71,14 @@ type systemKey struct {
 	NFSHome                bool     `json:"nfs-home"`
 	OverlayRoot            string   `json:"overlay-root"`
 	SecCompActions         []string `json:"seccomp-features"`
+	SeccompCompilerVersion string   `json:"seccomp-compiler-version"`
 }
 
 var (
-	isHomeUsingNFS  = osutil.IsHomeUsingNFS
-	mockedSystemKey *systemKey
-	osReadlink      = os.Readlink
+	isHomeUsingNFS             = osutil.IsHomeUsingNFS
+	mockedSystemKey            *systemKey
+	osReadlink                 = os.Readlink
+	seccompCompilerVersionInfo = seccompCompilerVersionInfoImpl
 )
 
 func findSnapdPath() (string, error) {
@@ -96,6 +99,11 @@ func findSnapdPath() (string, error) {
 		return filepath.Join(prefix, "/usr/lib/snapd/snapd"), nil
 	}
 	return snapdPath, nil
+}
+
+func seccompCompilerVersionInfoImpl(path string) (string, error) {
+	compiler := seccomp_compiler.NewAtPath(path)
+	return compiler.VersionInfo()
 }
 
 func generateSystemKey() (*systemKey, error) {
@@ -144,6 +152,13 @@ func generateSystemKey() (*systemKey, error) {
 
 	// Add seccomp-features
 	sk.SecCompActions = release.SecCompActions()
+
+	versionInfo, err := seccompCompilerVersionInfo(filepath.Join(filepath.Dir(snapdPath), "snap-seccomp"))
+	if err != nil {
+		logger.Noticef("cannot determine seccomp compiler version in generateSystemKey: %v", err)
+		return nil, err
+	}
+	sk.SeccompCompilerVersion = versionInfo
 
 	return sk, nil
 }
@@ -254,4 +269,12 @@ func MockSystemKey(s string) func() {
 	}
 	mockedSystemKey = &sk
 	return func() { mockedSystemKey = nil }
+}
+
+func MockSeccompCompilerVersionInfo(s func(p string) (string, error)) (restore func()) {
+	old := seccompCompilerVersionInfo
+	seccompCompilerVersionInfo = s
+	return func() {
+		seccompCompilerVersionInfo = old
+	}
 }
