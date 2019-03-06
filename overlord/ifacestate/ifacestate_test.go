a0b6e6ff41e935d67b66412c9658043161d3f8a4
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-03-06 16:48:44 +0100

    overlord/ifacestate: add mocking of snap-seccomp version info
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/ifacestate/ifacestate_test.go
+++ overlord/ifacestate/ifacestate_test.go
@@ -220,6 +220,10 @@ func (s *interfaceManagerSuite) SetUpTest(c *C) {
 	s.log = buf
 
 	s.BaseTest.AddCleanup(ifacestate.MockConnectRetryTimeout(0))
+	restore = interfaces.MockSeccompCompilerVersionInfo(func(_ string) (string, error) {
+		return "abcdef 1.2.3 1234abcd", nil
+	})
+	s.BaseTest.AddCleanup(restore)
 }
 
 func (s *interfaceManagerSuite) TearDownTest(c *C) {
