0c6dfc0d04ea93bcff240d3c43e405a5e21d0206
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2019-03-12 13:37:43 +0100

    cmd/snap-update-ns: add MountProfileUpdate interface
    The interface aims to abstract the essential algorithm of performing
    mount namespace updates from the differences between system-wide and
    per-user mount namespaces. It also simplifies testing by decoupling
    complex components from each other.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ cmd/snap-update-ns/update.go
@@ -0,0 +1,44 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2019 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"github.com/snapcore/snapd/osutil"
+)
+
+// MountProfileUpdate abstracts the operations around mount namespace updates.
+type MountProfileUpdate interface {
+	// Lock obtains locks appropriate for the update.
+	Lock() (unlock func(), err error)
+	// Assumptions computes filesystem assumptions under which the update shall operate.
+	Assumptions() *Assumptions
+	// LoadDesiredProfile loads the mount profile that should be constructed.
+	LoadDesiredProfile() (*osutil.MountProfile, error)
+	// LoadCurrentProfile loads the mount profile that is currently applied.
+	LoadCurrentProfile() (*osutil.MountProfile, error)
+	// SaveCurrentProfile saves the mount profile that is currently applied.
+	SaveCurrentProfile(*osutil.MountProfile) error
+	// NeededChanges computes the set of changes between a pair of profiles.
+	NeededChanges(old, new *osutil.MountProfile) []*Change
+	// PerformChange performs a single change.
+	// The returned changes represent any additional synthesized changes that
+	// were made as a prerequisite to complete the desired operation.
+	PerformChange(*Change, *Assumptions) ([]*Change, error)
+}
