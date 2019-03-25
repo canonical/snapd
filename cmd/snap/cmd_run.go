55ef144e6309a441ac2299f9b6139103727b95ba
Zygmunt Krynicki <me@zygoon.pl>
2019-03-25 19:19:09 +0100

    cmd/snap: document adjusting umask from go
    We discussed if controlling umask needs to be done with LockOSThread but
    ended up deciding that it doesn't. This patch adds the rationale as a
    comment.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/snap/cmd_run.go
+++ cmd/snap/cmd_run.go
@@ -313,6 +313,11 @@ func createOrUpdateUserDataSymlink(info *snap.Info, usr *user.User) error {
 }
 
 func createUserDataDirs(info *snap.Info) error {
+	// Adjust umask so that the created directories have the permissions we
+	// expect and are unaffected by the initial umask. While go runtime creates
+	// threads at will behind the scenes, the setting of umask applies to the
+	// entire process so it doesn't need any special handling to lock the
+	// executing goroutine to a single thread.
 	oldUmask := syscall.Umask(0)
 	defer syscall.Umask(oldUmask)
 
