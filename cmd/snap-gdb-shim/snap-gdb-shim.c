fcf09995bb7bf868b07fa01aef73417de3893dea
Michael Vogt <mvo@ubuntu.com>
2019-03-26 10:31:57 +0100

    snap-gdb-shim: also call setgid() and explain why setgroups() is not used

--- cmd/snap-gdb-shim/snap-gdb-shim.c
+++ cmd/snap-gdb-shim/snap-gdb-shim.c
@@ -15,12 +15,25 @@
  *
  */
 
+#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "../libsnap-confine-private/utils.h"
+
+int must_parse_int(const char *s)
+{
+	char *endptr = NULL;
+
+	errno = 0;
+	long i = strtol(s, &endptr, 10);
+	if (errno != 0 || s == endptr || *endptr != '\0' || i <= 0) {
+		die("cannot parse number in '%s'", s);
+	}
+	return i;
+}
 
 int main(int argc, char **argv)
 {
@@ -29,13 +42,37 @@ int main(int argc, char **argv)
 			printf("-%s-\n", argv[i]);
 		}
 	}
-        // check if we run as SUDO and if so switch to a normal user
-        const char *sudo_uid_env = getenv("SUDO_UID");
-        if (sudo_uid_env != NULL) {
-           int sudo_uid = atoi(sudo_uid_env);
-           if (sudo_uid != 0)
-              setuid(sudo_uid);
-        }
+
+	if (getuid() == 0) {
+		// check if we run as SUDO and if so switch to a normal user
+		const char *sudo_uid_env = getenv("SUDO_UID");
+		if (sudo_uid_env != NULL) {
+			int sudo_uid = must_parse_int(sudo_uid_env);
+			if (sudo_uid != 0) {
+				if (setuid(sudo_uid) != 0) {
+					die("cannot switch to uid %d",
+					    sudo_uid);
+				}
+			}
+		}
+	}
+	if (getgid() == 0) {
+		// check if we run as SUDO and if so switch to a normal user
+		const char *sudo_gid_env = getenv("SUDO_GID");
+		if (sudo_gid_env != NULL) {
+			int sudo_gid = must_parse_int(sudo_gid_env);
+			if (sudo_gid != 0) {
+				if (setgid(sudo_gid) != 0) {
+					die("cannot switch to gid %d",
+					    sudo_gid);
+				}
+			}
+		}
+	}
+	// Ideally we would also call setgroups() now but seccomp will
+	// prevent this. At this point we are inside the confinement
+	// of the snap already.
+
 	// signal gdb to stop here
 	printf("\n\n");
 	printf("Welcome to `snap run --gdb`.\n");
