5cba9654094948fe6c1faf0ffa73934367470162
Zygmunt Krynicki <me@zygoon.pl>
2019-03-28 13:26:47 +0100

    cmd/libsnap: inline sc_should_use_normal_mode
    The helper function is trivial and now, most importantly has only one
    user. As such it can be inlined without any loss of abstraction.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/libsnap-confine-private/classic.h
+++ cmd/libsnap-confine-private/classic.h
@@ -30,30 +30,4 @@ typedef enum sc_distro {
 
 sc_distro sc_classify_distro(void);
 
-/**
- * sc_should_use_normal_mode returns true if we should pivot into the base snap.
- *
- * There are two modes of execution for snaps that are not using classic
- * confinement: normal and legacy. The normal mode is where snap-confine sets
- * up a rootfs and then pivots into it using pivot_root(2). The legacy mode is
- * when snap-confine just unshares the initial mount namespace, makes some
- * extra changes but largely runs with what was presented to it initially.
- *
- * Historically the ubuntu-core distribution used the now-legacy mode. This
- * was sensible then since snaps already (kind of) have the right root file-system
- * and just need some privacy and isolation features applied. With the introduction
- * of snaps to classic distributions as well as the introduction of bases, where
- * each snap can use a different root filesystem, this lost sensibility and thus
- * became legacy.
- *
- * For compatibility with current installations of ubuntu-core distributions
- * the legacy mode is used when: the distribution is SC_DISTRO_CORE16 or when
- * the base snap name is not "core" or "ubuntu-core".
- *
- * The SC_DISTRO_CORE16 is applied to systems that boot with the "core",
- * "ubuntu-core" or "core16" snap. Systems using the "core18" base snap do not
- * qualify for that classification.
- **/
-bool sc_should_use_normal_mode(sc_distro distro, const char *base_snap_name);
-
 #endif
