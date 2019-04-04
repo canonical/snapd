07cd4fa0911006f2bbcc6c6bebf8f1470e093765
Zygmunt Krynicki <me@zygoon.pl>
2019-04-04 11:06:43 +0200

    tests: system-data/root should be 0755
    The test setup was incorrectly creating an open-access /root directory
    that is not representative of a real core or classic system. This patch
    fixes that.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -480,6 +480,9 @@ EOF
     # - make sure the group matches
     # - bind mount /root/test-etc/* to /etc/* via custom systemd job
     # We also create /var/lib/extrausers/* and append ubuntu,test there
+    ! test -e /mnt/system-data/root
+    mkdir -m 700 -p /mnt/system-data/root
+    test -d /mnt/system-data/root
     mkdir -p /mnt/system-data/root/test-etc
     mkdir -p /mnt/system-data/var/lib/extrausers/
     touch /mnt/system-data/var/lib/extrausers/sub{uid,gid}
