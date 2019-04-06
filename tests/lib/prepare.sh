78d1a32b60561688f50ea8f10775a9e841c62f9d
Sergio Cazzolato <sergiocazzolato@gmail.com>
2019-04-06 14:04:10 -0300

    Refresh the core in case it has been already cached

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -269,11 +269,14 @@ prepare_classic() {
         # shellcheck disable=SC2086
         cache_snaps ${PRE_CACHE_SNAPS}
 
-        ! snap list | grep core || exit 1
         # use parameterized core channel (defaults to edge) instead
         # of a fixed one and close to stable in order to detect defects
         # earlier
-        snap install --"$CORE_CHANNEL" core
+        if snap list | grep core; then
+            snap refresh --"$CORE_CHANNEL" core
+        else
+            snap install --"$CORE_CHANNEL" core
+        fi
         snap list | grep core
 
         systemctl stop snapd.{service,socket}
