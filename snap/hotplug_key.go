4e2233d76d91750cd0b44d7af955eeffc0977732
Pawel Stolowski <stolowski@gmail.com>
2019-04-08 12:30:55 +0200

    Introduce HotplugKey as a distinct type and pass it everywhere in hotplug code instead of plain strings. Provide ShortString method to use in task/change summaries.

---
+++ snap/hotplug_key.go
@@ -0,0 +1,33 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C)2019 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snap
+
+// HotplugKey is a string key of a hotplugged device
+type HotplugKey string
+
+// ShortString returns a truncated string representation of the hotplug key
+func (h HotplugKey) ShortString() string {
+	str := string(h)
+	// this is normally never the case, but makes testing easier
+	if len(str) < 12 {
+		return str
+	}
+	return str[0:12] + "..."
+}
