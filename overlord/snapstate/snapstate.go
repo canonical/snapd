18eedccbca2a31ac93f19975485409515db388fe
Michael Vogt <mvo@ubuntu.com>
2019-05-02 17:13:03 +0200

    snapstate: auto-install snapd when needed
    * snapstate: auto-install snapd when needed
    
    When installing a snap that does not require "core" on a fresh
    system, currently we do nothing special. This means however that
    on such systems there is no "core" or "snapd" snap installed.
    
    This means that no system interfaces are available. This PR
    fixes this by auto-installing the snapd snap if its missing.
    
    As a side effect, "snap install snapd" on classic is no longer
    blocked (it still is on core).
    
    * snapstate: add more tests for snapd auto-install with core18/other-bases
    
    * snapstate: rework the check if the snapd snap can be installed on a Core system
    
    * snapstate: use isInstalled instead of {snapd,core}SnapInstalled
    
    * snapstate: update doInstall now that the "snapd" install check moved to checkInstallPreconditions

--- overlord/snapstate/snapstate.go
+++ overlord/snapstate/snapstate.go
@@ -528,6 +528,23 @@ func validateFeatureFlags(st *state.State, info *snap.Info) error {
 }
 
 func checkInstallPreconditions(st *state.State, info *snap.Info, flags Flags, snapst *SnapState) error {
+	// Check if the snapd can be installed on Ubuntu Core systems, it is
+	// always ok to install on classic
+	if info.InstanceName() == "snapd" && !release.OnClassic {
+		tr := config.NewTransaction(st)
+		experimentalAllowSnapd, err := config.GetFeatureFlag(tr, features.SnapdSnap)
+		if err != nil && !config.IsNoOption(err) {
+			return err
+		}
+
+		model := Model()
+		if model != nil && model.Base() == "" {
+			if !experimentalAllowSnapd {
+				return fmt.Errorf("cannot install snapd snap on a model without a base snap yet")
+			}
+		}
+	}
+
 	if err := validateInfoAndFlags(info, snapst, flags); err != nil {
 		return err
 	}
