634a17c85718f15babe9fbe3cd85a36225bcfdd3
Michael Vogt <mvo@ubuntu.com>
2019-05-06 08:47:43 +0200

    snap-confine: when detecting non-home dirs check for /home/ and add test

--- cmd/snap-confine/user-support.c
+++ cmd/snap-confine/user-support.c
@@ -38,7 +38,7 @@ void setup_user_data(void)
 
 	debug("creating user data directory: %s", user_data);
 	if (sc_nonfatal_mkpath(user_data, 0755) < 0) {
-		if (errno == EROFS && !sc_startswith(user_data, "/home")) {
+		if (errno == EROFS && !sc_startswith(user_data, "/home/")) {
 				// clear errno or it will be displayed in die()
 				errno = 0;
 				die("Sorry, cannot use home directories outside /home.\nSee https://forum.snapcraft.io/t/11209 for details.");
