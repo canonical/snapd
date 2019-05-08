c91e34a251868c47146eba42c2026455488e1019
Michael Vogt <mvo@ubuntu.com>
2019-05-08 09:03:01 +0200

    snap-confine: tweak non-home error message (thanks to Graham)

--- cmd/snap-confine/user-support.c
+++ cmd/snap-confine/user-support.c
@@ -41,7 +41,7 @@ void setup_user_data(void)
 		if (errno == EROFS && !sc_startswith(user_data, "/home/")) {
 				// clear errno or it will be displayed in die()
 				errno = 0;
-				die("Sorry, cannot use home directories outside /home.\nSee https://forum.snapcraft.io/t/11209 for details.");
+				die("Sorry, home directories outside of /home are not currently supported. \nSee https://forum.snapcraft.io/t/11209 for details.");
 		}
 		die("cannot create user data directory: %s", user_data);
 	};
