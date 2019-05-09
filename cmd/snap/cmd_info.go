60120395dbe990a8a47f4254dd6baa5b94817a24
John R. Lenton <jlenton@gmail.com>
2019-05-09 15:36:04 +0100

    cmd/snap: mangle descriptions that have indent > terminal width
    Without this, a snap's description could have lines that were indented
    wider than the terminal, which would cause a panic in snap.
    
    As there is no good way of dealing with these, we simply replace any
    indent bigger than half the terminal width with a fixed two-space
    indent.
    
    This fixes https://bugs.launchpad.net/snapd/+bug/1828425

--- cmd/snap/cmd_info.go
+++ cmd/snap/cmd_info.go
@@ -209,6 +209,10 @@ func wrapLine(out io.Writer, text []rune, pad string, termWidth int) error {
 	}
 	indent := pad + string(text[:idx])
 	text = text[idx:]
+	if len(indent) > termWidth/2 {
+		// give up
+		indent = pad + "  "
+	}
 	return wrapGeneric(out, text, indent, indent, termWidth)
 }
 
