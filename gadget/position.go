fdde5fac1bf3756f0500bcf9174325c826887ca0
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-06-04 15:01:47 +0200

    gadget: tweak positioned content display
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- gadget/position.go
+++ gadget/position.go
@@ -94,7 +94,7 @@ type PositionedContent struct {
 
 func (p PositionedContent) String() string {
 	if p.Image != "" {
-		return fmtIndexAndName(p.Index, fmt.Sprintf("%v@%#x:%v", p.Image, p.StartOffset, p.Size))
+		return fmtIndexAndName(p.Index, fmt.Sprintf("%v@%#x{%v}", p.Image, p.StartOffset, p.Size))
 	}
 	return fmtIndexAndName(p.Index, fmt.Sprintf("source:%s", p.Source))
 }
