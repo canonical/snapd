d8feee77788c2d582378495c7193a0f14d7feeeb
John R. Lenton <jlenton@gmail.com>
2019-06-10 16:23:11 +0100

    daemon, overlord/snapstate: support leave-cohort
    With this change, you can remove (unstick?) a snap from a cohort using
    the `leave-cohort` option.
    
    An interesting thing about leave-cohort is that it's not a thing once
    the request has been made: the snap setup just needs to record that it
    needs to move to an empty cohort. It's a flag in daemon's
    snapInstruction (and snapstate.RevisionOptions) just to not have to
    fiddle with empty-string-means-no-change vs
    empty-string-means-change-to-nothing.
    
    As such, the PR might seem surprisingly simple.

--- daemon/api_test.go
+++ daemon/api_test.go
@@ -2446,6 +2446,50 @@ func (s *apiSuite) TestPostSnapCohortRandoAction(c *check.C) {
 	}
 }
 
+func (s *apiSuite) TestPostSnapLeaveCohortRandoAction(c *check.C) {
+	s.daemonWithOverlordMock(c)
+	s.vars = map[string]string{"name": "some-snap"}
+	const expectedErr = "leave-cohort can only be specified for refresh or switch"
+
+	for _, action := range []string{"install", "remove", "revert", "enable", "disable", "xyzzy"} {
+		buf := strings.NewReader(fmt.Sprintf(`{"action": "%s", "leave-cohort": true}`, action))
+		req, err := http.NewRequest("POST", "/v2/snaps/some-snap", buf)
+		c.Assert(err, check.IsNil)
+
+		rsp := postSnap(snapCmd, req, nil).(*resp)
+
+		c.Check(rsp.Type, check.Equals, ResponseTypeError)
+		c.Check(rsp.Status, check.Equals, 400, check.Commentf("%q", action))
+		c.Check(rsp.Result.(*errorResult).Message, check.Equals, expectedErr, check.Commentf("%q", action))
+	}
+}
+
+func (s *apiSuite) TestPostSnapCohortIncompat(c *check.C) {
+	s.daemonWithOverlordMock(c)
+	s.vars = map[string]string{"name": "some-snap"}
+
+	type T struct {
+		opts   string
+		errmsg string
+	}
+
+	for i, t := range []T{
+		// TODO: more?
+		{`"cohort-key": "what", "revision": "42"`, `cannot specify both cohort-key and revision`},
+		{`"cohort-key": "what", "leave-cohort": true`, `cannot specify both cohort-key and leave-cohort`},
+	} {
+		buf := strings.NewReader(fmt.Sprintf(`{"action": "refresh", %s}`, t.opts))
+		req, err := http.NewRequest("POST", "/v2/snaps/some-snap", buf)
+		c.Assert(err, check.IsNil, check.Commentf("%d (%s)", i, t.opts))
+
+		rsp := postSnap(snapCmd, req, nil).(*resp)
+
+		c.Check(rsp.Type, check.Equals, ResponseTypeError, check.Commentf("%d (%s)", i, t.opts))
+		c.Check(rsp.Status, check.Equals, 400, check.Commentf("%d (%s)", i, t.opts))
+		c.Check(rsp.Result.(*errorResult).Message, check.Equals, t.errmsg, check.Commentf("%d (%s)", i, t.opts))
+	}
+}
+
 func (s *apiSuite) TestPostSnapVerifyMultiSnapInstruction(c *check.C) {
 	s.daemonWithOverlordMock(c)
 
@@ -2467,11 +2511,12 @@ func (s *apiSuite) TestPostSnapsNoWeirdses(c *check.C) {
 	// one could add more actions here ... 🤷
 	for _, action := range []string{"install", "refresh", "remove"} {
 		for weird, v := range map[string]string{
-			"channel":    `"beta"`,
-			"revision":   `"1"`,
-			"devmode":    "true",
-			"jailmode":   "true",
-			"cohort-key": `"what"`,
+			"channel":      `"beta"`,
+			"revision":     `"1"`,
+			"devmode":      "true",
+			"jailmode":     "true",
+			"cohort-key":   `"what"`,
+			"leave-cohort": "true",
 		} {
 			buf := strings.NewReader(fmt.Sprintf(`{"action": "%s","snaps":["foo","bar"], "%s": %s}`, action, weird, v))
 			req, err := http.NewRequest("POST", "/v2/snaps", buf)
@@ -3754,10 +3799,42 @@ func (s *apiSuite) TestRefreshCohort(c *check.C) {
 	c.Check(summary, check.Equals, `Refresh "some-snap" snap`)
 }
 
+func (s *apiSuite) TestRefreshLeaveCohort(c *check.C) {
+	var leave *bool
+
+	snapstateUpdate = func(s *state.State, name string, opts *snapstate.RevisionOptions, userID int, flags snapstate.Flags) (*state.TaskSet, error) {
+		leave = &opts.LeaveCohort
+
+		t := s.NewTask("fake-refresh-snap", "Doing a fake install")
+		return state.NewTaskSet(t), nil
+	}
+	assertstateRefreshSnapDeclarations = func(s *state.State, userID int) error {
+		return nil
+	}
+
+	d := s.daemon(c)
+	inst := &snapInstruction{
+		Action:      "refresh",
+		LeaveCohort: true,
+		Snaps:       []string{"some-snap"},
+	}
+
+	st := d.overlord.State()
+	st.Lock()
+	defer st.Unlock()
+	summary, _, err := inst.dispatch()(inst, st)
+	c.Check(err, check.IsNil)
+
+	c.Check(*leave, check.Equals, true)
+	c.Check(summary, check.Equals, `Refresh "some-snap" snap`)
+}
+
 func (s *apiSuite) TestSwitchInstruction(c *check.C) {
 	var cohort, channel string
+	var leave *bool
 	snapstateSwitch = func(s *state.State, name string, opts *snapstate.RevisionOptions) (*state.TaskSet, error) {
 		cohort = opts.CohortKey
+		leave = &opts.LeaveCohort
 		channel = opts.Channel
 
 		t := s.NewTask("fake-switch", "Doing a fake switch")
@@ -3768,21 +3845,28 @@ func (s *apiSuite) TestSwitchInstruction(c *check.C) {
 	st := d.overlord.State()
 
 	type T struct {
-		channel, cohort, summary string
+		channel string
+		cohort  string
+		leave   bool
+		summary string
 	}
 	table := []T{
-		{"", "some-cohort", `Switch "some-snap" snap to cohort "some-coho…"`},
-		{"some-channel", "", `Switch "some-snap" snap to channel "some-channel"`},
-		{"some-channel", "some-cohort", `Switch "some-snap" snap to channel "some-channel" and cohort "some-coho…"`},
+		{"", "some-cohort", false, `Switch "some-snap" snap to cohort "some-coho…"`},
+		{"some-channel", "", false, `Switch "some-snap" snap to channel "some-channel"`},
+		{"some-channel", "some-cohort", false, `Switch "some-snap" snap to channel "some-channel" and cohort "some-coho…"`},
+		{"", "", true, `Switch "some-snap" snap away from cohort`},
+		{"some-channel", "", true, `Switch "some-snap" snap to channel "some-channel" and away from cohort`},
 	}
 
 	for _, t := range table {
 		cohort, channel = "", ""
+		leave = nil
 		inst := &snapInstruction{
-			Action:    "switch",
-			CohortKey: t.cohort,
-			Channel:   t.channel,
-			Snaps:     []string{"some-snap"},
+			Action:      "switch",
+			CohortKey:   t.cohort,
+			Channel:     t.channel,
+			Snaps:       []string{"some-snap"},
+			LeaveCohort: t.leave,
 		}
 
 		st.Lock()
@@ -3793,6 +3877,7 @@ func (s *apiSuite) TestSwitchInstruction(c *check.C) {
 		c.Check(cohort, check.Equals, t.cohort)
 		c.Check(channel, check.Equals, t.channel)
 		c.Check(summary, check.Equals, t.summary)
+		c.Check(*leave, check.Equals, t.leave)
 	}
 }
 
