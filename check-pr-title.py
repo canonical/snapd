3029d2829d75b51819abc17742286a40db9ef923
Michael Vogt <mvo@ubuntu.com>
2019-06-12 11:42:43 +0200

    travis.yml: also provide GITHUB_API_USER via env

--- check-pr-title.py
+++ check-pr-title.py
@@ -16,9 +16,10 @@ class InvalidPRTitle(Exception):
 def check_pr_title(pr_number: int):
     req = urllib.request.Request('https://api.github.com/repos/snapcore/snapd/pulls/{}'.format(pr_number))
     api_key=os.environ.get("GITHUB_API_KEY")
+    api_user=os.environ.get("GITHUB_API_USER")
     if api_key:
         # TODO: replace with a snapcore RO api key?
-        credentials = ('%s:%s' % ("mvo5", api_key))
+        credentials = ('%s:%s' % (api_user, api_key))
         encoded_credentials = base64.b64encode(credentials.encode('ascii'))
         req.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
     with urllib.request.urlopen(req) as f:
