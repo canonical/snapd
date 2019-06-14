f631967ca55ac3ea982bcea724b22b15ecaa5d45
Pawel Stolowski <stolowski@gmail.com>
2019-06-14 10:27:50 +0200

    Add showTasks and TODOs.

--- cmd/snapd-state-inspect/main.go
+++ cmd/snapd-state-inspect/main.go
@@ -24,6 +24,7 @@ import (
 	"io"
 	"os"
 	"sort"
+	"strings"
 	"text/tabwriter"
 	"time"
 
@@ -34,8 +35,10 @@ import (
 
 // commandline args
 var opts struct {
-	//Changes bool `long:"changes" description:"Show changes"`
-	ByReadyTime bool `long:"by-ready" description:"Sort by Ready time"`
+	DotOutput bool `long:"dot" description:"Dot (graphviz) output"` // XXX: mildly useful (too crowded in many cases), but let's have it just in case
+
+	// When inspecting errors/undone tasks, those in Hold state are usually irrelevant, make it possible to ignore them
+	NoHoldState bool `long:"no-hold" description:"Omit tasks in 'Hold' state in the output"`
 }
 
 func loadState(path string) (*state.State, error) {
@@ -56,9 +59,9 @@ func loadState(path string) (*state.State, error) {
 
 type byChangeID []*state.Change
 
-func (a byChangeID) Len() int           { return len(a) }
-func (a byChangeID) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byChangeID) Less(i, j int) bool { return a[i].ID() < a[j].ID() }
+func (c byChangeID) Len() int           { return len(c) }
+func (c byChangeID) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c byChangeID) Less(i, j int) bool { return c[i].ID() < c[j].ID() }
 
 func formatTime(t time.Time) string {
 	return t.Format(time.RFC3339)
@@ -78,7 +81,54 @@ func showChanges(w io.Writer, st *state.State) error {
 	return nil
 }
 
-func showTasks(w *tabwriter.Writer, st *state.State, changeID string) error {
+func waitChainSearch(startT, searchT *state.Task) bool {
+	for _, cand := range startT.HaltTasks() {
+		if cand == searchT {
+			return true
+		}
+		if waitChainSearch(cand, searchT) {
+			return true
+		}
+	}
+
+	return false
+}
+
+type byLaneAndWaitTaskChain []*state.Task
+
+func (t byLaneAndWaitTaskChain) Len() int      { return len(t) }
+func (t byLaneAndWaitTaskChain) Swap(i, j int) { t[i], t[j] = t[j], t[i] }
+func (t byLaneAndWaitTaskChain) Less(i, j int) bool {
+	// cover the typical case (just one lane), and order by first lane
+	if t[i].Lanes()[0] == t[j].Lanes()[0] {
+		return waitChainSearch(t[i], t[j])
+	}
+	return t[i].Lanes()[0] < t[j].Lanes()[0]
+}
+
+func writeDotOutput(st *state.State, changeID string) error {
+	st.Lock()
+	defer st.Unlock()
+
+	chg := st.Change(changeID)
+	if chg == nil {
+		return fmt.Errorf("no such change: %s", changeID)
+	}
+
+	fmt.Fprintf(os.Stdout, "digraph D{\n")
+	tasks := chg.Tasks()
+	for _, t := range tasks {
+		fmt.Fprintf(os.Stdout, "  %s [label=%q];\n", t.ID(), t.Kind())
+		for _, wt := range t.WaitTasks() {
+			fmt.Fprintf(os.Stdout, "  %s -> %s;\n", wt.ID(), t.ID())
+		}
+	}
+	fmt.Fprintf(os.Stdout, "}\n")
+
+	return nil
+}
+
+func showTasks(w *tabwriter.Writer, st *state.State, changeID string, noHoldState bool) error {
 	st.Lock()
 	defer st.Unlock()
 
@@ -88,10 +138,18 @@ func showTasks(w *tabwriter.Writer, st *state.State, changeID string) error {
 	}
 
 	tasks := chg.Tasks()
+	sort.Sort(byLaneAndWaitTaskChain(tasks))
 
-	fmt.Fprintf(w, "ID\tStatus\tSpawn\tReady\tLabel\tSummary\n")
+	fmt.Fprintf(w, "Lanes\tID\tStatus\tSpawn\tReady\tLabel\tSummary\n")
 	for _, t := range tasks {
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n", t.ID(), t.Status().String(), formatTime(t.SpawnTime()), formatTime(t.ReadyTime()), t.Kind(), t.Summary())
+		if noHoldState && t.Status() == state.HoldStatus {
+			continue
+		}
+		var lanes []string
+		for _, lane := range t.Lanes() {
+			lanes = append(lanes, fmt.Sprintf("%d", lane))
+		}
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n", strings.Join(lanes, ","), t.ID(), t.Status().String(), formatTime(t.SpawnTime()), formatTime(t.ReadyTime()), t.Kind(), t.Summary())
 	}
 
 	w.Flush()
@@ -138,7 +196,17 @@ func run() error {
 			return fmt.Errorf("expected single change ID")
 		}
 		changeID := rest[1]
-		err = showTasks(w, st, changeID)
+		if opts.DotOutput {
+			err = writeDotOutput(st, changeID)
+		} else {
+			err = showTasks(w, st, changeID, opts.NoHoldState)
+		}
+	case what == "conns":
+		// TODO: inspect connections
+		return fmt.Errorf("conns not implemented")
+	case what == "consistency":
+		// TODO: consistency check (e.g. connections vs snaps in the state etc.)
+		return fmt.Errorf("consistency not implemented")
 	default:
 		return fmt.Errorf("unknown command: %s", what)
 	}
