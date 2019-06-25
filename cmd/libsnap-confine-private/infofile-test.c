b05743b90454c94f59a915faf77f259706223f46
Zygmunt Krynicki <me@zygoon.pl>
2019-06-25 15:39:05 +0200

    cmd/libsnap: improve error checking in infofile parser
    The parser now checks for embedded NULs as well as for lines that don't
    contain a key=value line.
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/libsnap-confine-private/infofile-test.c
+++ cmd/libsnap-confine-private/infofile-test.c
@@ -21,29 +21,38 @@
 #include <glib.h>
 
 static void test_infofile_query(void) {
+    int rc;
+    sc_error *err;
+
     char text[] =
         "key=value\n"
         "other-key=other-value\n"
         "dup-key=value-one\n"
         "dup-key=value-two\n";
-    FILE *stream = fmemopen(text, strlen(text), "r");
+    FILE *stream = fmemopen(text, sizeof text - 1, "r");
     g_assert_nonnull(stream);
 
     /* Keys that are not found get NULL values. */
     char *value = (void *)0xfefefefe;
-    sc_infofile_query(stream, NULL, "missing-key", &value, NULL);
+    rc = sc_infofile_query(stream, &err, "missing-key", &value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
     g_assert_null(value);
 
     /* Keys that are found get strdup-duplicated values. */
     value = NULL;
-    sc_infofile_query(stream, NULL, "key", &value, NULL);
+    rc = sc_infofile_query(stream, &err, "key", &value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
     g_assert_nonnull(value);
     g_assert_cmpstr(value, ==, "value");
     free(value);
 
     /* Multiple keys can be extracted on one go. */
     char *other_value;
-    sc_infofile_query(stream, NULL, "key", &value, "other-key", &other_value, NULL);
+    rc = sc_infofile_query(stream, &err, "key", &value, "other-key", &other_value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
     g_assert_nonnull(value);
     g_assert_nonnull(other_value);
     g_assert_cmpstr(value, ==, "value");
@@ -52,7 +61,9 @@ static void test_infofile_query(void) {
     free(other_value);
 
     /* Order in which keys are extracted does not matter. */
-    sc_infofile_query(stream, NULL, "other-key", &other_value, "key", &value, NULL);
+    rc = sc_infofile_query(stream, &err, "other-key", &other_value, "key", &value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
     g_assert_nonnull(value);
     g_assert_nonnull(other_value);
     g_assert_cmpstr(value, ==, "value");
@@ -62,12 +73,47 @@ static void test_infofile_query(void) {
 
     /* When duplicate keys are present the first value is extracted. */
     char *dup_value;
-    sc_infofile_query(stream, NULL, "dup-key", &dup_value, NULL);
+    rc = sc_infofile_query(stream, &err, "dup-key", &dup_value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
     g_assert_nonnull(dup_value);
     g_assert_cmpstr(dup_value, ==, "value-one");
     free(dup_value);
 
     fclose(stream);
+
+    char *malformed_value;
+
+    char malformed1[] = "key\n";
+    stream = fmemopen(malformed1, sizeof malformed1 - 1, "r");
+    g_assert_nonnull(stream);
+    rc = sc_infofile_query(stream, &err, "key", &malformed_value, NULL);
+    g_assert_cmpint(rc, ==, -1);
+    g_assert_nonnull(err);
+    g_assert_null(malformed_value);
+    sc_error_free(err);
+    fclose(stream);
+
+    char malformed2[] = "key=value\0garbage\n";
+    stream = fmemopen(malformed2, sizeof malformed2 - 1, "r");
+    g_assert_nonnull(stream);
+    rc = sc_infofile_query(stream, &err, "key", &malformed_value, NULL);
+    g_assert_cmpint(rc, ==, -1);
+    g_assert_nonnull(err);
+    g_assert_null(malformed_value);
+    sc_error_free(err);
+    fclose(stream);
+
+    char malformed3[] = "key=";
+    stream = fmemopen(malformed3, sizeof malformed3 - 1, "r");
+    g_assert_nonnull(stream);
+    rc = sc_infofile_query(stream, &err, "key", &malformed_value, NULL);
+    g_assert_cmpint(rc, ==, 0);
+    g_assert_null(err);
+    g_assert_cmpstr(malformed_value, ==, "");
+    sc_error_free(err);
+    fclose(stream);
+    free(malformed_value);
 }
 
 static void __attribute__((constructor)) init(void) { g_test_add_func("/infofile/query", test_infofile_query); }
