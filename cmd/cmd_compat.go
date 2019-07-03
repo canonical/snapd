c6934ce60a3ec6177035e4a8614c71da9cf69a80
John R. Lenton <jlenton@gmail.com>
2019-07-03 12:52:36 +0100

    cmd: try to trick the compiler to building with cgo on 1.9

---
+++ cmd/cmd_compat.go
@@ -0,0 +1,26 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+// +build !cgo,!go1.10
+
+/*
+ * Copyright (C) 2019 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package cmd
+
+// this should only get built when building on go 1.9, where some
+// weird build id stuff needs to happen
+
+import "C"
