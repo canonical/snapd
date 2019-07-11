ff20e8d7e0056015cf37c695a0535b2c4458f86c
Pawel Stolowski <stolowski@gmail.com>
2019-07-11 11:06:03 +0200

    Added a comment.

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -321,6 +321,7 @@ repack_snapd_snap_with_deb_content() {
     dpkg-deb -x "$SPREAD_PATH"/../snapd_*.deb "$UNPACK_DIR"
     cp /usr/lib/snapd/info "$UNPACK_DIR"/usr/lib/
     # workaround for missing "type: snapd", until a new version is available in the store
+    # XXX: this should be removed once our snapcraft.yaml has "type: snapd"
     if ! grep -q "type:" "$UNPACK_DIR/meta/snap.yaml"; then
         echo "type: snapd" >> "$UNPACK_DIR/meta/snap.yaml"
     fi
