1cf30862978129b5390dfe65934bab9f88c30a94
Sergio Cazzolato <sergiocazzolato@gmail.com>
2019-07-24 18:19:57 -0300

    Add functions to make an abstraction for the snaps
    This is the first part of the change to avoid deciding which snap to use
    based on the core system which is being used.

--- tests/lib/systems.sh
+++ tests/lib/systems.sh
@@ -14,6 +14,13 @@ is_core18_system(){
     return 1
 }
 
+is_core20_system(){
+    if [[ "$SPREAD_SYSTEM" == ubuntu-core-20-* ]]; then
+        return 0
+    fi
+    return 1
+}
+
 is_classic_system(){
     if [[ "$SPREAD_SYSTEM" != ubuntu-core-* ]]; then
         return 0
@@ -26,4 +33,29 @@ is_ubuntu_14_system(){
         return 0
     fi
     return 1
+}
+
+get_snap_for_system(){
+    snap=$1
+    system=${2:-$SPREAD_SYSTEM}
+
+    if [[ "$system" == ubuntu-core-18-* ]]; then
+        echo "${snap}-core18"
+    elif [[ "$system" == ubuntu-core-20-* ]]; then
+        echo "${snap}-core20"
+    else
+        echo "$snap"
+    fi
+}
+
+get_core_for_system(){
+    system=${1:-$SPREAD_SYSTEM}
+
+    if [[ "$system" == ubuntu-core-18-* ]]; then
+        echo "core18"
+    elif [[ "$system" == ubuntu-core-20-* ]]; then
+        echo "core20"
+    else
+        echo "core"
+    fi
 }
