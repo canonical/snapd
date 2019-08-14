e0eef778d6d2950c5e05c4614292bfc00ed7f2e7
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-08-14 12:16:12 +0200

    interfaces/network-manager: allow using org.freedesktop.DBus.ObjectManager
    The org.freedesktop.DBus.ObjectManager is implemented in newer versions
    of NetworkManager and allows having a full view of current NM objects
    with just one DBus call. It is used by nmcli.

--- interfaces/builtin/network_manager.go
+++ interfaces/builtin/network_manager.go
@@ -257,6 +257,13 @@ dbus (receive, send)
     path=/org/freedesktop/NetworkManager{,/**}
     peer=(label=###PLUG_SECURITY_TAGS###),
 
+# NM implements org.freedesktop.DBus.ObjectManager too
+dbus (receive, send)
+    bus=system
+    path=/org/freedesktop
+    interface=org.freedesktop.DBus.ObjectManager
+    peer=(label=###PLUG_SECURITY_TAGS###),
+
 # Explicitly deny ptrace to silence noisy denials. These denials happen when NM
 # tries to access /proc/<peer_pid>/stat.  What apparmor prevents is showing
 # internal process addresses that live in that file, but that has no adverse
@@ -275,6 +282,13 @@ const networkManagerConnectedPlugAppArmor = `
 dbus (receive, send)
     bus=system
     path=/org/freedesktop/NetworkManager{,/**}
+    peer=(label=###SLOT_SECURITY_TAGS###),
+
+# NM implements org.freedesktop.DBus.ObjectManager too
+dbus (receive, send)
+    bus=system
+    path=/org/freedesktop
+    interface=org.freedesktop.DBus.ObjectManager
     peer=(label=###SLOT_SECURITY_TAGS###),
 `
 
