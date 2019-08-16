cb2c846bb4b02f727f18095aeffd93d655b53dfb
Michael Vogt <mvo@ubuntu.com>
2019-08-16 16:11:04 +0200

    osutil: improve comments around osutil.Find{Uid,Gid} (thanks to jdstrand)

--- osutil/group_no_cgo.go
+++ osutil/group_no_cgo.go
@@ -6,10 +6,14 @@ package osutil
 // The builtin os/user functions only look at /etc/passwd and
 // /etc/group when building without cgo.
 //
-// So nothing configured via nsswitch.conf, like extrausers. findUid()
-// and findGid() is searched. To fix this behavior we use
-// find{Uid,Gid}WithGetentFallback() that perform a 'getent
-// <database> <name>' automatically if no local user is found.
+// So if something extra is configured via nsswitch.conf, like
+// extrausers those are not searched with the standard user.Lookup()
+// which is used in find{Uid,Gid}NoGetenvFallback.
+//
+// To fix this behavior we use find{Uid,Gid}WithGetentFallback() that
+// perform a 'getent <database> <name>' automatically if no local user
+// is found and getent will use the libc nss functions to search all
+// configured data sources.
 
 var (
 	findUid = findUidWithGetentFallback
