9ab4c52c8adb8ac68a31090ada2fe64b41c9defa
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-08-21 07:29:25 +0200

    tests/lib/users: cleanup shellcheck warnings
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/lib/users.sh
+++ tests/lib/users.sh
@@ -8,19 +8,19 @@ remove_user_with_group() {
     REMOVE_USER="$1"
 
     if [ -e /var/lib/extrausers/passwd ]; then
-        userdel --extrausers --force ${REMOVE_USER} || true
+        userdel --extrausers --force "${REMOVE_USER}" || true
     else
-        userdel --force ${REMOVE_USER} || true
+        userdel --force "${REMOVE_USER}" || true
         # some systems do not set "USERGROUPS_ENAB yes" so we need to cleanup
         # the group manually. Use "-f" (force) when available, older versions
         # do not have it.
         if groupdel -h | grep force; then
-            groupdel -f ${REMOVE_USER} || true
+            groupdel -f "${REMOVE_USER}" || true
         else
-            groupdel ${REMOVE_USER} || true
+            groupdel "${REMOVE_USER}" || true
         fi
     fi
     # ensure the ${REMOVE_USER} user really got deleted    
-    not getent passwd ${REMOVE_USER}
-    not getent group ${REMOVE_USER}
+    not getent passwd "${REMOVE_USER}"
+    not getent group "${REMOVE_USER}"
 }
