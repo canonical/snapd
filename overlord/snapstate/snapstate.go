9ddc8aae796567f780a3e59a6bbe5af75d7f3e8b
Samuele Pedroni <pedronis@lucediurna.net>
2019-08-28 09:37:59 +0200

    move and enforce resolveChannel before installInfo
    move spread tests to install-errors where they belong

--- overlord/snapstate/snapstate.go
+++ overlord/snapstate/snapstate.go
@@ -702,6 +702,12 @@ func InstallWithDeviceContext(ctx context.Context, st *state.State, name string,
 		return nil, err
 	}
 
+	// check the channel specification, enforce pinned tracks
+	opts.Channel, err = resolveChannel(st, name, opts.Channel, deviceCtx)
+	if err != nil {
+		return nil, err
+	}
+
 	if err := snap.ValidateInstanceName(name); err != nil {
 		return nil, fmt.Errorf("invalid instance name: %v", err)
 	}
@@ -721,12 +727,6 @@ func InstallWithDeviceContext(ctx context.Context, st *state.State, name string,
 	}
 	// TODO: integrate classic override with the helper
 	if err := checkInstallPreconditions(st, info, flags, &snapst, deviceCtx); err != nil {
-		return nil, err
-	}
-
-	// check the channel specification
-	_, err = resolveChannel(st, name, opts.Channel, deviceCtx)
-	if err != nil {
 		return nil, err
 	}
 
@@ -1195,6 +1195,12 @@ func resolveChannel(st *state.State, snapName, newChannel string, deviceCtx Devi
 		return "", nil
 	}
 
+	// always check channel
+	nch, err := channel.ParseVerbatim(newChannel, "")
+	if err != nil {
+		return "", err
+	}
+
 	// ensure we do not switch away from the kernel-track in the model
 	model := deviceCtx.Model()
 
@@ -1220,18 +1226,9 @@ func resolveChannel(st *state.State, snapName, newChannel string, deviceCtx Devi
 	}
 
 	if pinnedTrack == "" {
-		// no pinned track, but still check for valid channel spec
-		_, err = channel.ParseVerbatim(newChannel, "")
-		if err != nil {
-			return "", err
-		}
-		// if the spec was valid, then just return the original spec
+		// no pinned track, then just return the original
+		// validated spec
 		return newChannel, nil
-	}
-
-	nch, err := snap.ParseChannelVerbatim(newChannel, "")
-	if err != nil {
-		return "", err
 	}
 
 	if nch.Track == "" {
