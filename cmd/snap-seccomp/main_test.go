41e95a07c1ba1825d9cab7abcceb53910017ea95
Arda Güçlü <ardaguclu@gmail.com>
2019-09-07 21:52:59 +0300

    tests: Remove import alias

--- cmd/snap-seccomp/main_test.go
+++ cmd/snap-seccomp/main_test.go
@@ -32,7 +32,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
-	seccomp "github.com/mvo5/libseccomp-golang"
+	"github.com/mvo5/libseccomp-golang"
 
 	"github.com/snapcore/snapd/arch"
 	main "github.com/snapcore/snapd/cmd/snap-seccomp"
