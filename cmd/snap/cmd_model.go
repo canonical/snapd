f7de1bb6586fbb6104587ac94fd714cebad8e584
Ian Johnson <ian.johnson@canonical.com>
2019-09-12 12:18:15 -0500

    cmd/model: adjust error message for no serial assertion
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- cmd/snap/cmd_model.go
+++ cmd/snap/cmd_model.go
@@ -48,7 +48,7 @@ model assertion.
 
 	invalidTypeMessage    = i18n.G("invalid type for %q header")
 	errNoMainAssertion    = errors.New(i18n.G("device not ready yet (no assertions found)"))
-	errNoSerial        = errors.New(i18n.G("device not ready yet (no serial assertion found)"))
+	errNoSerial           = errors.New(i18n.G("device not registered yet (no serial assertion found)"))
 	errNoVerboseAssertion = errors.New(i18n.G("cannot use --verbose with --assertion"))
 
 	// this list is a "nice" "human" "readable" "ordering" of headers to print
