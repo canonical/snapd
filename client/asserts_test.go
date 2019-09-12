608ccf09250206c085555021c97f67a4274d5abf
Ian Johnson <ian.johnson@canonical.com>
2019-09-12 12:06:10 -0500

    client/asserts.go: move to client/store.go
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- client/asserts_test.go
+++ client/asserts_test.go
@@ -25,9 +25,11 @@ import (
 	"net/http"
 	"net/url"
 
+	"gopkg.in/check.v1"
 	. "gopkg.in/check.v1"
 
 	"github.com/snapcore/snapd/asserts"
+	"github.com/snapcore/snapd/snap"
 )
 
 func (cs *clientSuite) TestClientAssert(c *C) {
@@ -156,4 +158,59 @@ func (cs *clientSuite) TestClientAssertsMissingAssertions(c *C) {
 	cs.status = 200
 	_, err := cs.cli.Known("snap-build", nil)
 	c.Assert(err, ErrorMatches, "response did not have the expected number of assertions")
+}
+
+func (cs *clientSuite) TestStoreAccount(c *check.C) {
+	cs.header = http.Header{}
+	cs.header.Add("X-Ubuntu-Assertions-Count", "1")
+	cs.rsp = `type: account
+authority-id: canonical
+account-id: canonicalID
+display-name: canonicalDisplay
+timestamp: 2016-04-01T00:00:00.0Z
+username: canonicalUser
+validation: certified
+sign-key-sha3-384: -CvQKAwRQ5h3Ffn10FILJoEZUXOv6km9FwA80-Rcj-f-6jadQ89VRswHNiEB9Lxk
+
+AcLDXAQAAQoABgUCV7UYzwAKCRDUpVvql9g3IK7uH/4udqNOurx5WYVknzXdwekp0ovHCQJ0iBPw
+TSFxEVr9faZSzb7eqJ1WicHsShf97PYS3ClRYAiluFsjRA8Y03kkSVJHjC+sIwGFubsnkmgflt6D
+WEmYIl0UBmeaEDS8uY4Xvp9NsLTzNEj2kvzy/52gKaTc1ZSl5RDL9ppMav+0V9iBYpiDPBWH2rJ+
+aDSD8Rkyygm0UscfAKyDKH4lrvZ0WkYyi1YVNPrjQ/AtBySh6Q4iJ3LifzKa9woIyAuJET/4/FPY
+oirqHAfuvNod36yNQIyNqEc20AvTvZNH0PSsg4rq3DLjIPzv5KbJO9lhsasNJK1OdL6x8Yqrdsbk
+ldZp4qkzfjV7VOMQKaadfcZPRaVVeJWOBnBiaukzkhoNlQi1sdCdkBB/AJHZF8QXw6c7vPDcfnCV
+1lW7ddQ2p8IsJbT6LzpJu3GW/P4xhNgCjtCJ1AJm9a9RqLwQYgdLZwwDa9iCRtqTbRXBlfy3apps
+1VjbQ3h5iCd0hNfwDBnGVm1rhLKHCD1DUdNE43oN2ZlE7XGyh0HFV6vKlpqoW3eoXCIxWu+HBY96
++LSl/jQgCkb0nxYyzEYK4Reb31D0mYw1Nji5W+MIF5E09+DYZoOT0UvR05YMwMEOeSdI/hLWg/5P
+k+GDK+/KopMmpd4D1+jjtF7ZvqDpmAV98jJGB2F88RyVb4gcjmFFyTi4Kv6vzz/oLpbm0qrizC0W
+HLGDN/ymGA5sHzEgEx7U540vz/q9VX60FKqL2YZr/DcyY9GKX5kCG4sNqIIHbcJneZ4frM99oVDu
+7Jv+DIx/Di6D1ULXol2XjxbbJLKHFtHksR97ceaFvcZwTogC61IYUBJCvvMoqdXAWMhEXCr0QfQ5
+Xbi31XW2d4/lF/zWlAkRnGTzufIXFni7+nEuOK0SQEzO3/WaRedK1SGOOtTDjB8/3OJeW96AUYK5
+oTIynkYkEyHWMNCXALg+WQW6L4/YO7aUjZ97zOWIugd7Xy63aT3r/EHafqaY2nacOhLfkeKZ830b
+o/ezjoZQAxbh6ce7JnXRgE9ELxjdAhBTpGjmmmN2sYrJ7zP9bOgly0BnEPXGSQfFA+NNNw1FADx1
+MUY8q9DBjmVtgqY+1KGTV5X8KvQCBMODZIf/XJPHdCRAHxMd8COypcwgL2vDIIXpOFbi1J/B0GF+
+eklxk9wzBA8AecBMCwCzIRHDNpD1oa2we38bVFrOug6e/VId1k1jYFJjiLyLCDmV8IMYwEllHSXp
+LQAdm3xZ7t4WnxYC8YSCk9mXf3CZg59SpmnV5Q5Z6A5Pl7Nc3sj7hcsMBZEsOMPzNC9dPsBnZvjs
+WpPUffJzEdhHBFhvYMuD4Vqj6ejUv9l3oTrjQWVC
+`
+
+	account, err := cs.cli.StoreAccount("canonicalID")
+	c.Assert(err, check.IsNil)
+	c.Check(cs.req.Method, check.Equals, "GET")
+	c.Check(cs.req.URL.Query(), check.HasLen, 1)
+	c.Check(cs.req.URL.Query().Get("account-id"), check.Equals, "canonicalID")
+	c.Assert(account, check.DeepEquals, &snap.StoreAccount{
+		ID:          "canonicalID",
+		Username:    "canonicalUser",
+		DisplayName: "canonicalDisplay",
+		Validation:  "verified",
+	})
+}
+
+func (cs *clientSuite) TestStoreAccountNoAssertionFound(c *check.C) {
+	cs.header = http.Header{}
+	cs.header.Add("X-Ubuntu-Assertions-Count", "0")
+	cs.rsp = ""
+
+	_, err := cs.cli.StoreAccount("canonicalID")
+	c.Assert(err, check.ErrorMatches, "no assertion found for account-id canonicalID")
 }
