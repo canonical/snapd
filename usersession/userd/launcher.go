00cc0f87d56c430f428e075f1c75da4cf5b854d7
Alan Griffiths <alan@octopull.co.uk>
2019-09-23 09:53:17 +0100

    We don't need github.com/google/shlex, we have github.com/snapcore/snapd/strutil/shlex

--- usersession/userd/launcher.go
+++ usersession/userd/launcher.go
@@ -32,12 +32,11 @@ import (
 
 	"github.com/godbus/dbus"
 
-	"github.com/google/shlex"
-
 	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/i18n"
 	"github.com/snapcore/snapd/osutil/sys"
 	"github.com/snapcore/snapd/strutil"
+	"github.com/snapcore/snapd/strutil/shlex"
 	"github.com/snapcore/snapd/usersession/userd/ui"
 )
 
