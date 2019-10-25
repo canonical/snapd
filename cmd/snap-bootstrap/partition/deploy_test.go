a3b7d238c000b3574ab6b7e110ab75377d61a644
Michael Vogt <mvo@ubuntu.com>
2019-10-25 11:36:09 +0200

    snap-bootstrap: update rename

--- cmd/snap-bootstrap/partition/deploy_test.go
+++ cmd/snap-bootstrap/partition/deploy_test.go
@@ -25,7 +25,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
-	"github.com/snapcore/snapd/cmd/snap-recovery/partition"
+	"github.com/snapcore/snapd/cmd/snap-bootstrap/partition"
 	"github.com/snapcore/snapd/gadget"
 	"github.com/snapcore/snapd/testutil"
 )
