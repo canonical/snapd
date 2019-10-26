mvo5/snappy

snap, cmd/snap: support (but warn) using deprecated multi-slash channel
Before, when `--channel=<foo>` was just passed on to the store, we
inherited a bit of undefined behaviour where
`--channel=//foo/////bar//baz/` was interpreted the same as
`--channel=foo/bar/baz`. In particular, somebody doing
`--channel=$FOO/candidate` in a script could rely on it working even
if `$FOO` was `""`.

The change to support `--channel=<risk>` meaning `--channel=<current
track>/<risk>` means that we now parse `--channel`, and lost this bit
of behaviour.

Now, we don't _want_ this behaviour: it presents a problem for the
introduction of default tracks (coming RSN!). But we dont' want to
break our users. So...

This change makes it so that the old spuriously-slashed channels are
accepted, and warned about. The warning will go away in a couple of
releases when we start enforcing clean channel names.

This change does _not_ address having spuriously-slashed channels in
state (which currently leaves you in a very strange situation). That
will come in a separate PR.

snap/channel_test.go
@@ -167,20 +167,26 @@ func (s storeChannelSuite) TestParseChannelErrors(c *C) {
 	for _, tc := range []struct {
 		channel string
 		err     string
+		full    string
 	}{
-		{"", "channel name cannot be empty"},
-		{"1.0////", "channel name has too many components: 1.0////"},
-		{"1.0/cand", "invalid risk in channel name: 1.0/cand"},
-		{"fix//hotfix", "invalid risk in channel name: fix//hotfix"},
-		{"/stable/", "invalid track in channel name: /stable/"},
-		{"//stable", "invalid risk in channel name: //stable"},
-		{"stable/", "invalid branch in channel name: stable/"},
-		{"/stable", "invalid track in channel name: /stable"},
+		{"", "channel name cannot be empty", ""},
+		{"1.0////", "channel name has too many components: 1.0////", "1.0/stable"},
+		{"1.0/cand", "invalid risk in channel name: 1.0/cand", ""},
+		{"fix//hotfix", "invalid risk in channel name: fix//hotfix", ""},
+		{"/stable/", "invalid track in channel name: /stable/", "latest/stable"},
+		{"//stable", "invalid risk in channel name: //stable", "latest/stable"},
+		{"stable/", "invalid branch in channel name: stable/", "latest/stable"},
+		{"/stable", "invalid track in channel name: /stable", "latest/stable"},
 	} {
 		_, err := snap.ParseChannel(tc.channel, "")
 		c.Check(err, ErrorMatches, tc.err)
-		_, err = snap.ParseChannelVerbatim(tc.channel, "")
+		ch, err := snap.ParseChannelVerbatim(tc.channel, "")
 		c.Check(err, ErrorMatches, tc.err)
+		if tc.full != "" {
+			c.Check(err, FitsTypeOf, snap.DeprecatedChannelStringError{})
+			ch = ch.Clean()
+			c.Check(ch.Full(), Equals, tc.full)
+		}
 	}
 }
 

canonical/snapd

snap/channel/channel_test.go
@@ -189,20 +189,26 @@ func (s storeChannelSuite) TestParseErrors(c *C) {
 	for _, tc := range []struct {
 		channel string
 		err     string
+		full    string
 	}{
-		{"", "channel name cannot be empty"},
-		{"1.0////", "channel name has too many components: 1.0////"},
-		{"1.0/cand", "invalid risk in channel name: 1.0/cand"},
-		{"fix//hotfix", "invalid risk in channel name: fix//hotfix"},
-		{"/stable/", "invalid track in channel name: /stable/"},
-		{"//stable", "invalid risk in channel name: //stable"},
-		{"stable/", "invalid branch in channel name: stable/"},
-		{"/stable", "invalid track in channel name: /stable"},
+		{"", "channel name cannot be empty", ""},
+		{"1.0////", "channel name has too many components: 1.0////", "1.0/stable"},
+		{"1.0/cand", "invalid risk in channel name: 1.0/cand", ""},
+		{"fix//hotfix", "invalid risk in channel name: fix//hotfix", ""},
+		{"/stable/", "invalid track in channel name: /stable/", "latest/stable"},
+		{"//stable", "invalid risk in channel name: //stable", "latest/stable"},
+		{"stable/", "invalid branch in channel name: stable/", "latest/stable"},
+		{"/stable", "invalid track in channel name: /stable", "latest/stable"},
 	} {
 		_, err := channel.Parse(tc.channel, "")
 		c.Check(err, ErrorMatches, tc.err)
-		_, err = channel.ParseVerbatim(tc.channel, "")
+		ch, err := channel.ParseVerbatim(tc.channel, "")
 		c.Check(err, ErrorMatches, tc.err)
+		if tc.full != "" {
+			c.Check(err, FitsTypeOf, channel.DeprecatedChannelStringError{})
+			ch = ch.Clean()
+			c.Check(ch.Full(), Equals, tc.full)
+		}
 	}
 }
 