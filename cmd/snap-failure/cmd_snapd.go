58aceb4e273f81c5022cb894d3caf271eac7f678
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2019-11-20 09:44:03 +0100

    cmd/snap-failure: fallback to snap from core, extend tests
    The snapd snap has no previous revision if this is the first time it gets
    installed. This can happen in a core to base remodel scenario, when the snap
    gets installed before all other snapd taking part in a remodel.
    
    Since there is no previous revision of snapd snap that we can call, fall back
    and try to run snapd from the core snap.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/snap-failure/cmd_snapd.go
+++ cmd/snap-failure/cmd_snapd.go
@@ -51,13 +51,14 @@ type sideInfo struct {
 }
 
 type snapSeq struct {
-	Current  string      `json:"current"`
-	Sequence []*sideInfo `json:"sequence"`
+	Current  string     `json:"current"`
+	Sequence []sideInfo `json:"sequence"`
 }
 
 type cmdSnapd struct{}
 
 var errNoSnapd = errors.New("no snapd sequence file found")
+var errNoPrevious = errors.New("no revision to go back to")
 
 func prevRevision(snapName string) (string, error) {
 	seqFile := filepath.Join(dirs.SnapSeqDir, snapName+".json")
@@ -71,21 +72,21 @@ func prevRevision(snapName string) (string, error) {
 
 	var seq snapSeq
 	if err := json.Unmarshal(content, &seq); err != nil {
-		return "", err
+		return "", fmt.Errorf("cannot parse %q sequence file: %v", filepath.Base(seqFile), err)
 	}
 
 	var prev string
 	for i, si := range seq.Sequence {
 		if seq.Current == si.Revision {
 			if i == 0 {
-				return "", fmt.Errorf("no revision to go back to")
+				return "", errNoPrevious
 			}
 			prev = seq.Sequence[i-1].Revision
 			break
 		}
 	}
 	if prev == "" {
-		return "", fmt.Errorf("internal error: current not found in sequence: %v %v", seq.Current, seq.Sequence)
+		return "", fmt.Errorf("internal error: current %v not found in sequence: %+v", seq.Current, seq.Sequence)
 	}
 
 	return prev, nil
@@ -93,12 +94,23 @@ func prevRevision(snapName string) (string, error) {
 
 // FIXME: also do error reporting via errtracker
 func (c *cmdSnapd) Execute(args []string) error {
-	// find previous snapd
+	var snapdPath string
+	// find previous the snapd snap
 	prevRev, err := prevRevision("snapd")
-	if err != nil {
-		if err == errNoSnapd {
-			return nil
-		}
+	switch err {
+	case errNoSnapd:
+		// the snapd snap is not installed
+		return nil
+	case errNoPrevious:
+		// this is the first revision of snapd to be installed on the
+		// system, either a remodel or a plain snapd installation, call
+		// the snapd from the core snap
+		snapdPath = filepath.Join(dirs.SnapMountDir, "core", "current", "/usr/lib/snapd/snapd")
+		prevRev = "0"
+	case nil:
+		// the snapd snap was installed before, use the previous revision
+		snapdPath = filepath.Join(dirs.SnapMountDir, "snapd", prevRev, "/usr/lib/snapd/snapd")
+	default:
 		return err
 	}
 	// stop the socket unit so that we can start snapd on its own
@@ -108,13 +120,12 @@ func (c *cmdSnapd) Execute(args []string) error {
 	}
 
 	// start previous snapd
-	snapdPath := filepath.Join(dirs.SnapMountDir, "snapd", prevRev, "/usr/lib/snapd/snapd")
 	cmd := exec.Command(snapdPath)
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "SNAPD_REVERT_TO_REV="+prevRev)
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		return osutil.OutputErr(output, err)
+		return fmt.Errorf("snapd failed: %v", osutil.OutputErr(output, err))
 	}
 
 	// at this point our manually started snapd stopped and
