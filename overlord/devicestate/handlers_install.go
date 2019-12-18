cd4286d268c9b73742c89cdd6a0c520e9fdecf43
Michael Vogt <mvo@ubuntu.com>
2019-12-18 17:08:03 +0100

    devicestate: fix typo in doSetupRunSystem

--- overlord/devicestate/handlers_install.go
+++ overlord/devicestate/handlers_install.go
@@ -87,7 +87,7 @@ func (m *DeviceManager) doSetupRunSystem(t *state.Task, _ *tomb.Tomb) error {
 		return fmt.Errorf("cannot make run system bootable: %v", err)
 	}
 
-	// request a restart as the last action after a successful installc
+	// request a restart as the last action after a successful install
 	st.RequestRestart(state.RestartSystem)
 
 	return nil
