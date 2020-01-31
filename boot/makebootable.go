c3b933187c96d4f729895b2625f0f1e1f9d68a98
Ian Johnson <ian.johnson@canonical.com>
2020-01-31 14:44:17 -0600

    many: don't use kernel from the modeenv anymore
    This is in preparation for the modeenv to merely track what list of kernels are
    valid to have booted from. For now, we shouldn't be using the Kernel setting in
    the modeenv at all.
    
    Note that this is because `snap-bootstrap initramfs-mounts` currently determines
    what kernel snap to use from the symlinks on the bootloader rather than from the
    modeenv - the modeenv will ultimately just be a verification that the kernel 
    from the bootloader is trusted.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- boot/makebootable.go
+++ boot/makebootable.go
@@ -211,7 +211,6 @@ func makeBootable20RunMode(model *asserts.Model, rootdir string, bootWith *Boota
 		Mode:           "run",
 		RecoverySystem: filepath.Base(bootWith.RecoverySystemDir),
 		Base:           filepath.Base(bootWith.BasePath),
-		Kernel:         filepath.Base(bootWith.KernelPath),
 	}
 	if err := modeenv.Write(filepath.Join(runMnt, "ubuntu-data", "system-data")); err != nil {
 		return fmt.Errorf("cannot write modeenv: %v", err)
