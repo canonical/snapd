6518d52b79c2a3f56ef10c68a680f760622a53e3
Michael Vogt <mvo@ubuntu.com>
2020-02-03 14:54:26 +0100

    netutil: add default gateway monitor
    This PR adds support for detecting adding/removing a default gateway.
    The use-case is that we can retry registration and refreshes when a
    new default gateway gets added. The assumption is that when a default
    gateway gets added that this most likely means that someone configured
    the device with new network configuration. This is most useful in a
    first-setup situation when the device connects to e.g. WiFi but needs
    to be configured first. As soon as the device is configured we should
    try to get a serial in this scenario.

---
+++ netutil/netlink.go
@@ -0,0 +1,122 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package netutil
+
+import (
+	"net"
+	"syscall"
+)
+
+const (
+	// see /usr/include/linux/rtnetlink.h
+	RTMGRP_IPV4_ROUTE = 0x40
+	RTMGRP_IPV6_ROUTE = 0x400
+
+	RTM_NEWROUTE = 24
+	RTM_DELROUTE = 25
+)
+
+type RoutesMonitor struct {
+	netlinkFd int
+
+	defaultGwAdded   func(string)
+	defaultGwRemoved func(string)
+}
+
+func NewRoutesMonitor(defaultGwAdded, defaultGwRemoved func(string)) *RoutesMonitor {
+	m := &RoutesMonitor{
+		defaultGwAdded:   defaultGwAdded,
+		defaultGwRemoved: defaultGwRemoved,
+	}
+	return m
+}
+
+func (m *RoutesMonitor) Connect() error {
+	fd, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_RAW, syscall.NETLINK_ROUTE)
+	if err != nil {
+		return err
+	}
+	addr := &syscall.SockaddrNetlink{
+		Family: syscall.AF_NETLINK,
+		Groups: RTMGRP_IPV4_ROUTE | RTMGRP_IPV6_ROUTE,
+	}
+	if err := syscall.Bind(fd, addr); err != nil {
+		return err
+	}
+	m.netlinkFd = fd
+	go m.run()
+	return nil
+}
+
+func (m *RoutesMonitor) Disconnect() {
+	syscall.Close(m.netlinkFd)
+	m.netlinkFd = 0
+}
+
+func isDefaultGw(mm *syscall.NetlinkMessage) (bool, net.IP) {
+	nras, err := syscall.ParseNetlinkRouteAttr(mm)
+	if err != nil {
+		// XXX: log error?
+		return false, nil
+	}
+	for _, nra := range nras {
+		switch nra.Attr.Type {
+		case syscall.RTA_GATEWAY:
+			return true, net.IP(nra.Value)
+		}
+	}
+	return false, nil
+}
+
+func (m *RoutesMonitor) run() {
+	buf := make([]byte, syscall.Getpagesize())
+	for {
+		n, _, err := syscall.Recvfrom(m.netlinkFd, buf, 0)
+		if err != nil {
+			// XXX: log error?
+			continue
+		}
+		if n < syscall.NLMSG_HDRLEN {
+			// XXX: log error?
+			continue
+		}
+		rawMsg := buf[:n]
+		msgs, err := syscall.ParseNetlinkMessage(rawMsg)
+		if err != nil {
+			// XXX: log error?
+			continue
+		}
+		for _, mm := range msgs {
+			switch mm.Header.Type {
+			case RTM_NEWROUTE:
+				isDefaultGw, gw := isDefaultGw(&mm)
+				if isDefaultGw && m.defaultGwAdded != nil {
+					m.defaultGwAdded(gw.String())
+				}
+			case RTM_DELROUTE:
+				isDefaultGw, gw := isDefaultGw(&mm)
+				if isDefaultGw && m.defaultGwRemoved != nil {
+					m.defaultGwRemoved(gw.String())
+				}
+
+			}
+		}
+	}
+}
