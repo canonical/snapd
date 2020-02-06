8d6a0bc5f3030755216c8dd4e9c2c35791f0ea88
Pawel Stolowski <stolowski@gmail.com>
2020-02-06 09:27:44 +0100

    Updated for the rename of StopSnapd -> StopDaemon.

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -496,7 +496,7 @@ func (d *Daemon) HandleRestart(t state.RestartType) {
 		d.mu.Lock()
 		defer d.mu.Unlock()
 		d.restartSocket = true
-	case state.StopSnapd:
+	case state.StopDaemon:
 		logger.Noticef("stopping snapd as requested")
 	default:
 		logger.Noticef("internal error: restart handler called with unknown restart type: %v", t)
