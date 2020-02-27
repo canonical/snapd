298ffae997c068e177ff92ac2cb290c4c73a95b3
Michael Vogt <michael.vogt@gmail.com>
2020-02-27 21:15:20 +0100

    netlink: tweak comment in rawsockstop_other.go
    Co-Authored-By: Ian Johnson <person.uwsome@gmail.com>

--- osutil/udev/netlink/rawsockstop_other.go
+++ osutil/udev/netlink/rawsockstop_other.go
@@ -1,5 +1,6 @@
 // +build !arm64
-// don't remove the whitespace below!
+// don't remove the newline between the above statement and the package statement
+// or else the build constraint will be ignored and assumed to be part of the package comment!
 
 package netlink
 
