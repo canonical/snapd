4c1de9317dd2750332af17d38b702d3e794b3ea4
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-03-23 13:06:40 +0100

    interfaces/network-manager: allow full access to /run/NetworkManager
    There were some limitations previously in the access to
    /run/NetworkManager/, to "protect" configuration of connections that
    is written by netplan. But, that had a couple of issues:
    
    1. It is perfectly legit for NM to change that connections
    configuration. There can be reasons for that, and in any case NM can
    disable those connections if desired. Also, NM has access to
    /etc/netplan/, so the snap could do the change indirectly: apparmor
    here was limiting how the interaction between applications happens,
    not protecting anything really.
    
    2. NM writes more files in /run/NetworkManager/ than just dhcp leases,
    for instance /run/NetworkManager/devices/, and newer versions fail if
    they cannot write them. To create the folder we need write permissions
    in /run/NetworkManager/.

--- interfaces/builtin/network_manager.go
+++ interfaces/builtin/network_manager.go
@@ -92,12 +92,9 @@ network packet,
 /etc/netplan/{,**} rw,
 
 # Allow access to configuration files generated on the fly
-# from netplan and let NetworkManager store its DHCP leases
-# in the dhcp subdirectory so that console-conf can access
-# it.
-/run/NetworkManager/ w,
-/run/NetworkManager/{,**} r,
-/run/NetworkManager/dhcp/{,**} w,
+# from netplan and let NetworkManager store its configuration
+# in the same place.
+/run/NetworkManager/{,**} rw,
 
 # Needed by the ifupdown plugin to check which interfaces can
 # be managed an which not.
