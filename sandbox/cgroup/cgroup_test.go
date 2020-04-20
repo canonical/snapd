6c7b16aca471bf7706c6294ef7cf78e9064a16cd
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2020-04-20 12:29:46 +0200

    sandbox/cgroup: test variant with two users
    The test now models two users, in two separate sessions, running the
    same application twice, each.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- sandbox/cgroup/cgroup_test.go
+++ sandbox/cgroup/cgroup_test.go
@@ -366,13 +366,15 @@ func (s *cgroupSuite) TestPidsOfInstances(c *C) {
 func (s *cgroupSuite) TestPidsOfAggregation(c *C) {
 	// A single snap may be invoked by multiple users in different sessions.
 	// All of their PIDs are collected though.
-	s.writePids(c, "user.slice/user-1000.slice/user@1000.service/gnome-shell-wayland.service/snap.$RANDOM1.pkg.app.scope", []int{1})
-	s.writePids(c, "user.slice/user-1001.slice/user@1001.service/gnome-shell-wayland.service/snap.$RANDOM2.pkg.app.scope", []int{2})
+	s.writePids(c, "user.slice/user-1000.slice/user@1000.service/gnome-shell-wayland.service/snap.$RANDOM1.pkg.app.scope", []int{1})  // mock 1st invocation
+	s.writePids(c, "user.slice/user-1000.slice/user@1000.service/gnome-shell-wayland.service/snap.$RANDOM2.pkg.app.scope", []int{2})  // mock fork() by pid 1
+	s.writePids(c, "user.slice/user-1001.slice/user@1001.service/gnome-shell-wayland.service/snap.$RANDOM3.pkg.app.scope", []int{3})  // mock 2nd invocation
+	s.writePids(c, "user.slice/user-1001.slice/user@1001.service/gnome-shell-wayland.service/snap.$RANDOM4.pkg.app.scope", []int{4})  // mock fork() by pid 3
 
 	pids, err := cgroup.PidsOfSnap("pkg")
 	c.Assert(err, IsNil)
 	c.Check(pids, DeepEquals, map[string][]int{
-		"snap.pkg.app": {1, 2},
+		"snap.pkg.app": {1, 2, 3, 4},
 	})
 }
 
