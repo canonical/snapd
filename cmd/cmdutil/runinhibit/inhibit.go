e128731fe6023ab4f2b17a24fc0f2f7235c2971e
Zygmunt Krynicki <me@zygoon.pl>
2020-04-27 11:22:41 +0200

    cmd/cmdutil: add run inhibition operations
    This patch adds three new functions to the new cmd/cmdutil/runinhibit
    package. Together they implement the run inhibition primitives.
    
    Forum: https://forum.snapcraft.io/t/wip-refresh-app-awareness/10736/33
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

---
+++ cmd/cmdutil/runinhibit/inhibit.go
@@ -0,0 +1,137 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package runinhibit contains operations for establishing, removing and
+// querying snap run inhibition lock.
+package runinhibit
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/osutil"
+)
+
+// defaultInhibitDir is the directory where inhibition files are stored.
+const defaultInhibitDir = "/var/lib/snapd/inhibit"
+
+// InhibitDir is the directory where inhibition files are stored.
+// This value can be changed by calling dirs.SetRootDir.
+var InhibitDir = defaultInhibitDir
+
+func init() {
+	dirs.AddRootDirCallback(func(root string) {
+		InhibitDir = filepath.Join(root, defaultInhibitDir)
+	})
+}
+
+// Hint is a string representing reason for the inhibition of "snap run".
+type Hint string
+
+const (
+	// HintNotInhibited is used when "snap run" is not inhibited.
+	HintNotInhibited Hint = ""
+	// HintInhibitedForRefresh represents inhibition of a "snap run" while a refresh change is being performed.
+	HintInhibitedForRefresh Hint = "refresh"
+)
+
+func openHintFile(snapName string) (*osutil.FileLock, error) {
+	fname := filepath.Join(dirs.GlobalRootDir, "var/lib/snapd/inhibit", snapName+".lock")
+	return osutil.NewFileModeLock(fname, 0644)
+}
+
+// LockAndSetHint grabs a persistent "snap run" inhibition lock with a given hint.
+//
+// The hint cannot be empty. It should be one of the Hint constants defined in
+// this package. While the hint in place "snap run" will not allow the snap to
+// start and will block, presenting a user interface if possible.
+func LockAndSetHint(snapName string, hint Hint) error {
+	if len(hint) == 0 {
+		return fmt.Errorf("hint cannot be empty")
+	}
+	if err := os.MkdirAll(InhibitDir, 0755); err != nil {
+		return err
+	}
+	flock, err := openHintFile(snapName)
+	if err != nil {
+		return err
+	}
+	defer flock.Close()
+
+	if err := flock.Lock(); err != nil {
+		return err
+	}
+	f := flock.File()
+	_, err = f.WriteString(string(hint))
+	return err
+}
+
+// Unlock truncates the run inhibition lock.
+//
+// Truncated inhibition lock is equivalent to uninhibited "snap run".
+func Unlock(snapName string) error {
+	flock, err := openHintFile(snapName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	defer flock.Close()
+
+	if err := flock.Lock(); err != nil {
+		return err
+	}
+	f := flock.File()
+	return f.Truncate(0)
+}
+
+// IsLocked returns information about the run inhibition hint, if any.
+func IsLocked(snapName string) (Hint, error) {
+	flock, err := openHintFile(snapName)
+	if os.IsNotExist(err) {
+		return "", nil
+	}
+	if err != nil {
+		return "", err
+	}
+	defer flock.Close()
+
+	if err := flock.ReadLock(); err != nil {
+		return "", err
+	}
+
+	f := flock.File()
+	fi, err := f.Stat()
+	if err != nil {
+		return "", err
+	}
+	if fi.Size() == 0 {
+		return "", nil
+	}
+
+	buf := make([]byte, fi.Size())
+	n, err := f.Read(buf)
+	if n == len(buf) {
+		return Hint(string(buf)), nil
+	}
+	return "", err
+}
