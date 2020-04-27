c236bb295ef417d50cfdae0cd311e830b380721a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2020-04-27 15:42:45 +0200

    cmd/snap-confine: migrate ~/snap <=> ~/.snapdata
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-confine/user-support.c
+++ cmd/snap-confine/user-support.c
@@ -21,25 +21,106 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+#include "../libsnap-confine-private/cleanup-funcs.h"
+#include "../libsnap-confine-private/feature.h"
 #include "../libsnap-confine-private/string-utils.h"
 #include "../libsnap-confine-private/utils.h"
-#include "../libsnap-confine-private/feature.h"
+
+static void sc_migrate_to_dot_snapdata(const char *real_home)
+{
+	struct stat fi;
+	int hfd SC_CLEANUP(sc_cleanup_close) = -1;
+	hfd = open(real_home, O_PATH | O_DIRECTORY | O_NOFOLLOW);
+	if (hfd < 0) {
+		die("cannot open path %s", real_home);
+	}
+	/* If ~/.snapdata exists then there is nothing to do. */
+	if (fstatat(hfd, ".snapdata", &fi, AT_SYMLINK_NOFOLLOW) < 0) {
+		if (errno != ENOENT) {
+			die("cannot stat ~/.snapdata");
+		}
+	} else {
+		return;
+	}
+
+	/* If ~/snap doesn't exist then there is nothing to do. */
+	if (fstatat(hfd, "snap", &fi, AT_SYMLINK_NOFOLLOW) < 0) {
+		if (errno == ENOENT) {
+			return;
+		}
+		die("cannot stat ~/snap");
+	}
+	/* If ~/snap is a symlink then don't perform the rename! */
+	if ((fi.st_mode & S_IFMT) == S_IFLNK) {
+		return;
+	}
+
+	/* Rename ~/snap to ~/.snapdata making sure not to clobber ~/.snapdata. */
+	if (renameat2(hfd, "snap", hfd, ".snapdata", RENAME_NOREPLACE) < 0) {
+		if (errno == EEXIST) {
+			return;
+		}
+		die("cannot migrate ~/snap to ~/.snapdata");
+	}
+}
+
+static void sc_migrate_to_snap(const char *real_home)
+{
+	struct stat fi;
+	int hfd SC_CLEANUP(sc_cleanup_close) = -1;
+	hfd = open(real_home, O_PATH | O_DIRECTORY | O_NOFOLLOW);
+	if (hfd < 0) {
+		die("cannot open path %s", real_home);
+	}
+	/* If ~/snap exists then there is nothing to do. */
+	if (fstatat(hfd, "snap", &fi, AT_SYMLINK_NOFOLLOW) < 0) {
+		if (errno != ENOENT) {
+			die("cannot stat ~/snap");
+		}
+	} else {
+		return;
+	}
+
+	/* If ~/.snapdata doesn't exist then there is nothing to do. */
+	if (fstatat(hfd, ".snapdata", &fi, AT_SYMLINK_NOFOLLOW) < 0) {
+		if (errno == ENOENT) {
+			return;
+		}
+		die("cannot stat ~/.snapdata");
+	}
+	/* If ~/.snapdata is a symlink then don't perform the rename! */
+	if ((fi.st_mode & S_IFMT) == S_IFLNK) {
+		return;
+	}
+
+	/* Rename ~/.snapdata to ~/snap making sure not to clobber ~/snap. */
+	if (renameat2(hfd, ".snapdata", hfd, "snap", RENAME_NOREPLACE) < 0) {
+		if (errno == EEXIST) {
+			return;
+		}
+		die("cannot migrate ~/.snapdata to ~/snap");
+	}
+}
 
 void setup_user_data(const char *snap_instance)
 {
 	const char *user_data = getenv("SNAP_USER_DATA");
+	const char *real_home = getenv("SNAP_REAL_HOME");
 
-	if (user_data == NULL)
+	if (user_data == NULL) {
 		return;
+	}
+	if (real_home == NULL) {
+		die("cannot remap snap folder, SNAP_REAL_HOME is not set");
+	}
 
 	if (sc_feature_enabled(SC_FEATURE_HIDDEN_SNAP_FOLDER)) {
 		char buf[PATH_MAX + 1];
-		const char *real_home = getenv("SNAP_REAL_HOME");
 		const char *revision = getenv("SNAP_REVISION");
-		if (real_home == NULL) {
-			die("cannot remap snap folder, SNAP_REAL_HOME is not set");
-		}
 		if (revision == NULL) {
 			die("cannot remap snap folder, SNAP_REVISION is not set");
 		}
@@ -50,8 +131,11 @@ void setup_user_data(const char *snap_instance)
 		sc_must_snprintf(buf, sizeof buf, "%s/.snapdata/%s/common",
 				 real_home, snap_instance);
 		setenv("SNAP_USER_COMMON", buf, 1);
-
 		user_data = getenv("SNAP_USER_DATA");
+
+		sc_migrate_to_dot_snapdata(real_home);
+	} else {
+		sc_migrate_to_snap(real_home);
 	}
 	// Only support absolute paths.
 	if (user_data[0] != '/') {
