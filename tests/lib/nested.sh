88a61c31c1ba5ac1b19a8911d082ed7fa7d3d9fb
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2020-05-06 09:25:35 -0300

    tests: fix spread system selection to use secboot-enabled systems
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -68,16 +68,16 @@ get_qemu_for_nested_vm(){
 
 get_google_image_url_for_nested_vm(){
     case "$SPREAD_SYSTEM" in
-        ubuntu-16.04-64)
+        ubuntu-16.04-64*)
             echo "https://storage.googleapis.com/spread-snapd-tests/images/xenial-server-cloudimg-amd64-disk1.img"
             ;;
-        ubuntu-18.04-64)
+        ubuntu-18.04-64*)
             echo "https://storage.googleapis.com/spread-snapd-tests/images/bionic-server-cloudimg-amd64.img"
             ;;
-        ubuntu-19.10-64)
+        ubuntu-19.10-64*)
             echo "https://storage.googleapis.com/spread-snapd-tests/images/eoan-server-cloudimg-amd64.img"
             ;;
-        ubuntu-20.04-64)
+        ubuntu-20.04-64*)
             echo "https://storage.googleapis.com/spread-snapd-tests/images/focal-server-cloudimg-amd64.img"
             ;;
         *)
@@ -89,16 +89,16 @@ get_google_image_url_for_nested_vm(){
 
 get_ubuntu_image_url_for_nested_vm(){
     case "$SPREAD_SYSTEM" in
-        ubuntu-16.04-64)
+        ubuntu-16.04-64*)
             echo "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
             ;;
-        ubuntu-18.04-64)
+        ubuntu-18.04-64*)
             echo "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
             ;;
-        ubuntu-19.10-64)
+        ubuntu-19.10-64*)
             echo "https://cloud-images.ubuntu.com/eoan/current/eoan-server-cloudimg-amd64.img"
             ;;
-        ubuntu-20.04-64)
+        ubuntu-20.04-64*)
             echo "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
             ;;
         *)
