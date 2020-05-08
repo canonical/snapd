74e0a1eca450390733b466de78bc7226ada9fc9b
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2020-05-08 09:32:24 +0200

    cmd: move runinhibit to snaplock
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snaplock/runinhibit/inhibit_test.go
+++ cmd/snaplock/runinhibit/inhibit_test.go
@@ -26,7 +26,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
-	"github.com/snapcore/snapd/cmd/cmdutil/runinhibit"
+	"github.com/snapcore/snapd/cmd/snaplock/runinhibit"
 	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/testutil"
 )
