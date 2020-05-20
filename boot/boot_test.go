a904708df422f344705a36ca2b3074119d733044
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2020-05-20 12:37:55 +0200

    boot: require mode in modeenv
    Ensure that the `mode` field in modeenv must be set for both reading and
    writing.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- boot/boot_test.go
+++ boot/boot_test.go
@@ -130,6 +130,8 @@ func (s *baseBootenv20Suite) SetUpTest(c *C) {
 	// state for after trying a new kernel for robustness tests, etc.
 	s.normalTryingKernelState = &bootenv20Setup{
 		modeenv: &boot.Modeenv{
+			// operating mode is run
+			Mode: "run",
 			// base is base1
 			Base: s.base1.Filename(),
 			// no try base
@@ -197,7 +199,7 @@ func setupUC20Bootenv(c *C, bl bootloader.Bootloader, opts *bootenv20Setup) (res
 		// multiple modeenvs from a single test and just call the restore
 		// function in between the parts of the test that use different modeenvs
 		r := func() {
-			emptyModeenv := &boot.Modeenv{}
+			emptyModeenv := &boot.Modeenv{Mode: "run"}
 			c.Assert(emptyModeenv.WriteTo(""), IsNil)
 		}
 		cleanups = append(cleanups, r)
