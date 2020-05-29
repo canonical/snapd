16436ee8312611afc84eeb235d9f81e285da058f
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-05-29 12:55:55 +0200

    interfaces/dpdk-control: address review comments

--- interfaces/builtin/dpdk_control.go
+++ interfaces/builtin/dpdk_control.go
@@ -19,7 +19,8 @@
 
 package builtin
 
-const dpdkControlSummary = `allows using dpdk`
+// http://doc.dpdk.org/guides/linux_gsg/
+const dpdkControlSummary = `allows configuring dpdk drivers`
 
 const dpdkControlBaseDeclarationSlots = `
   dpdk-control:
@@ -30,26 +31,19 @@ const dpdkControlBaseDeclarationSlots = `
 `
 
 const dpdkControlConnectedPlugAppArmor = `
-# Description: Allow control to dpdk.
+# Description: allow configuring dpdk drivers
 /run/dpdk{,/**} rwk,
 owner @{PROC}/@{pid}/pagemap r,
-/sys/bus/pci/drivers/virtio-pci/unbind rw,
-/sys/bus/pci/drivers/e1000/unbind rw,
-/sys/bus/pci/drivers/igb_uio/unbind rw,
-/sys/bus/pci/drivers/rte_kni/unbind rw,
-/sys/bus/pci/drivers/uio_pci_generic/unbind rw,
-/sys/bus/pci/drivers/virtio-pci/bind rw,
-/sys/bus/pci/drivers/e1000/bind rw,
-/sys/bus/pci/drivers/igb_uio/bind rw,
-/sys/bus/pci/drivers/rte_kni/bind rw,
-/sys/bus/pci/drivers/uio_pci_generic/bind rw,
+/sys/bus/pci/drivers/virtio-pci/{,un}bind rw,
+/sys/bus/pci/drivers/e1000/{,un}bind rw,
+/sys/bus/pci/drivers/igb_uio/{,un}bind rw,
+/sys/bus/pci/drivers/rte_kni/{,un}bind rw,
+/sys/bus/pci/drivers/uio_pci_generic/{,un}bind rw,
 /sys/devices/pci*{,/**} rw,
-/proc/ioports r,
 /dev/uio[0-9]* rw,
 /dev/vfio/vfio rw,
 /dev/infiniband/rdma_cm rw,
 /dev/infiniband/uverbs[0-9]* rw,
-/sys/bus/pci/drivers{,/**} rw,
 `
 
 const dpdkControlConnectedPlugSecComp = `
@@ -59,11 +53,15 @@ move_pages
 
 // Network drivers with DPDK support used by different vendors.
 var dpdkConnectedPlugKmod = []string{
+	"igb_uio",
 	"mlx4_core",
 	"mlx5_core",
+	"uio_pci_generic",
+	"vfio-pci",
 }
 
-var dpdkControlConnectedPlugUDev = []string{`SUBSYSTEM=="uio"`}
+var dpdkControlConnectedPlugUDev = []string{`SUBSYSTEM=="uio"`,
+	`KERNEL=="vfio"`, `KERNEL=="rdma_cm"`, `SUBSYSTEM=="infiniband_verbs"`}
 
 func init() {
 	registerIface(&commonInterface{
