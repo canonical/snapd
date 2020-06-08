dfeddeaaa4b59f4f432e98cf363a74212282e58b
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2020-06-08 18:03:40 -0300

    gadget/install,vendor: use snapcore/secboot luks2 api
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- gadget/install/encrypt.go
+++ gadget/install/encrypt.go
@@ -23,12 +23,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"syscall"
 
-	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/gadget"
 	"github.com/snapcore/snapd/osutil"
 	"github.com/snapcore/snapd/secboot"
@@ -57,7 +53,7 @@ func newEncryptedDevice(part *gadget.OnDiskStructure, key secboot.EncryptionKey,
 		Node: fmt.Sprintf("/dev/mapper/%s", name),
 	}
 
-	if err := cryptsetupFormat(key, name+"-enc", part.Node); err != nil {
+	if err := secboot.FormatEncryptedDevice(key, name+"-enc", part.Node); err != nil {
 		return nil, fmt.Errorf("cannot format encrypted device: %v", err)
 	}
 
@@ -69,45 +65,11 @@ func newEncryptedDevice(part *gadget.OnDiskStructure, key secboot.EncryptionKey,
 }
 
 func (dev *encryptedDevice) AddRecoveryKey(key secboot.EncryptionKey, rkey secboot.RecoveryKey) error {
-	return cryptsetupAddKey(key, rkey, dev.parent.Node)
+	return secboot.AddRecoveryKey(key, rkey, dev.parent.Node)
 }
 
 func (dev *encryptedDevice) Close() error {
 	return cryptsetupClose(dev.name)
-}
-
-func cryptsetupFormat(key secboot.EncryptionKey, label, node string) error {
-	// We use a keyfile with the same entropy as the derived key so we can
-	// keep the KDF iteration count to a minimum. Longer processing will not
-	// increase security in this case.
-	args := []string{
-		// batch processing, no password verification
-		"-q",
-		// formatting a new device
-		"luksFormat",
-		// use LUKS2
-		"--type", "luks2",
-		// read key from stdin
-		"--key-file", "-",
-		// use AES-256 with XTS block cipher mode (XTS requires 2 keys)
-		"--cipher", "aes-xts-plain64", "--key-size", "512",
-		// use --iter-time 1 with the default KDF argon2i so
-		// to do virtually no derivation, here key is a random
-		// key with good entropy, not a passphrase, so
-		// spending time deriving from it is not necessary or
-		// makes sense
-		"--pbkdf", "argon2i", "--iter-time", "1",
-		// set LUKS2 label
-		"--label", label,
-		// device to format
-		node,
-	}
-	cmd := exec.Command("cryptsetup", args...)
-	cmd.Stdin = bytes.NewReader(key[:])
-	if output, err := cmd.CombinedOutput(); err != nil {
-		return osutil.OutputErr(output, err)
-	}
-	return nil
 }
 
 func cryptsetupOpen(key secboot.EncryptionKey, node, name string) error {
@@ -123,68 +85,5 @@ func cryptsetupClose(name string) error {
 	if output, err := exec.Command("cryptsetup", "close", name).CombinedOutput(); err != nil {
 		return osutil.OutputErr(output, err)
 	}
-	return nil
-}
-
-func cryptsetupAddKey(key secboot.EncryptionKey, rkey secboot.RecoveryKey, node string) error {
-	// create a named pipe to pass the recovery key
-	fpath := filepath.Join(dirs.SnapRunDir, "tmp-rkey")
-	if err := os.MkdirAll(dirs.SnapRunDir, 0755); err != nil {
-		return err
-	}
-	if err := syscall.Mkfifo(fpath, 0600); err != nil {
-		return fmt.Errorf("cannot create named pipe: %v", err)
-	}
-	defer os.RemoveAll(fpath)
-
-	// add a new key to slot 1 reading the passphrase from the named pipe
-	// (explicitly choose keyslot 1 to ensure we have a predictable slot
-	// number in case we decide to kill all other slots later)
-	args := []string{
-		// add a new key
-		"luksAddKey",
-		// the encrypted device
-		node,
-		// batch processing, no password verification
-		"-q",
-		// read existing key from stdin
-		"--key-file", "-",
-		// store it in keyslot 1
-		"--key-slot", "1",
-		// the named pipe
-		fpath,
-	}
-
-	cmd := exec.Command("cryptsetup", args...)
-	cmd.Stdin = bytes.NewReader(key[:])
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-
-	// open the named pipe and write the recovery key
-	file, err := os.OpenFile(fpath, os.O_WRONLY, 0600)
-	if err != nil {
-		return fmt.Errorf("cannot open recovery key pipe: %v", err)
-	}
-	n, err := file.Write(rkey[:])
-	if n != len(rkey) {
-		file.Close()
-		return fmt.Errorf("cannot write recovery key: short write (%d bytes written)", n)
-	}
-	if err != nil {
-		cmd.Process.Kill()
-		file.Close()
-		return fmt.Errorf("cannot write recovery key: %v", err)
-	}
-	if err := file.Close(); err != nil {
-		cmd.Process.Kill()
-		return fmt.Errorf("cannot close recovery key pipe: %v", err)
-	}
-	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("cannot add recovery key: %v", err)
-	}
-
 	return nil
 }
