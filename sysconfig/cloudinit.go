e3e3969103f8c91f3546ecf44e9945dbe880c1aa
Ian Johnson <ian.johnson@canonical.com>
2020-06-11 15:05:16 -0500

    sysconfig/cloudinit: add CloudInitStatus func + CloudInitState type
    These are used to identify what state cloud-init is in for future manipulations
    of cloud-init during the running system on Ubuntu Core.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- sysconfig/cloudinit.go
+++ sysconfig/cloudinit.go
@@ -23,8 +23,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 
+	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/osutil"
 )
 
@@ -77,4 +80,76 @@ func configureCloudInit(opts *Options) (err error) {
 		err = installCloudInitCfg(opts.CloudInitSrcDir, opts.TargetRootDir)
 	}
 	return err
+}
+
+// CloudInitState represents the various cloud-init states
+type CloudInitState int
+
+var (
+	// the (?m) is needed since cloud-init output will have newlines
+	cloudInitStatusRe = regexp.MustCompile(`(?m)^status: (.*)$`)
+
+	cloudInitSnapdRestrictFile = "/etc/cloud/cloud.cfg.d/zzzz_snapd.cfg"
+)
+
+const (
+	// CloudInitDisabledPermanently is when cloud-init is disabled as per the
+	// cloud-init.disabled file.
+	CloudInitDisabledPermanently CloudInitState = iota
+	// CloudInitRestrictedBySnapd is when cloud-init has been restricted by
+	// snapd with a specific config file.
+	CloudInitRestrictedBySnapd
+	// CloudInitUntriggered is when cloud-init is disabled because nothing has
+	// triggered it to run, but it could still be run.
+	CloudInitUntriggered
+	// CloudInitDone is when cloud-init has been run on this boot.
+	CloudInitDone
+	// CloudInitRunning is when cloud-init is still running/processing and has
+	// not finished yet.
+	CloudInitRunning
+	// CloudInitErrored is when cloud-init tried to run, but failed or had invalid
+	// configuration.
+	CloudInitErrored
+)
+
+func cloudInitDisabledByFile() bool {
+	disabledFile := filepath.Join(dirs.GlobalRootDir, "etc/cloud/cloud-init.disabled")
+	return osutil.FileExists(disabledFile)
+}
+
+// CloudInitStatus returns the current status of cloud-init.
+func CloudInitStatus() (CloudInitState, error) {
+	// if cloud-init has been restricted by snapd, check that first
+	snapdRestrictingFile := filepath.Join(dirs.GlobalRootDir, cloudInitSnapdRestrictFile)
+	if osutil.FileExists(snapdRestrictingFile) {
+		return CloudInitRestrictedBySnapd, nil
+	}
+
+	out, err := exec.Command("cloud-init", "status").CombinedOutput()
+	if err != nil {
+		return CloudInitErrored, osutil.OutputErr(out, err)
+	}
+	// output should just be "status: <state>"
+	match := cloudInitStatusRe.FindSubmatch(out)
+	if len(match) != 2 {
+		return CloudInitErrored, fmt.Errorf("invalid cloud-init output: %v", osutil.OutputErr(out, err))
+	}
+	switch string(match[1]) {
+	case "disabled":
+		// check if it was permanently disabled by the disabled file or if it
+		// just "hasn't run" in which case it is untriggered
+		if cloudInitDisabledByFile() {
+			return CloudInitDisabledPermanently, nil
+		}
+		return CloudInitUntriggered, nil
+	case "error":
+		return CloudInitErrored, nil
+	case "done":
+		return CloudInitDone, nil
+	case "running":
+		return CloudInitRunning, nil
+	default:
+		// unknown what other state cloud-init could be in ...
+		return CloudInitErrored, fmt.Errorf("internal error: unexpected cloud-init status %q", string(match[1]))
+	}
 }
