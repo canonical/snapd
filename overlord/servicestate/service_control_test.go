cf54068982aa6a7b87e1b4c1b32e6bec2184ec4f
Paweł Stołowski <stolowski@gmail.com>
2020-06-18 20:44:38 +0200

    Add forgotten tests.

--- overlord/servicestate/service_control_test.go
+++ overlord/servicestate/service_control_test.go
@@ -476,3 +476,71 @@ func (s *serviceControlSuite) TestConflict(c *C) {
 	_, err := snapstate.Remove(st, "test-snap", snap.Revision{}, nil)
 	c.Assert(err, ErrorMatches, `snap "test-snap" has "service-control" change in progress`)
 }
+
+func (s *serviceControlSuite) TestUpdateSnapstateServices(c *C) {
+	var tests = []struct {
+		enable                    []string
+		disable                   []string
+		expectedSnapstateEnabled  []string
+		expectedSnapstateDisabled []string
+		changed                   bool
+	}{
+		{
+			changed: false,
+		},
+		{
+			enable:                   []string{"a"},
+			expectedSnapstateEnabled: []string{"a"},
+			changed:                  true,
+		},
+		// enable again does nothing
+		{
+			enable:                   []string{"a"},
+			expectedSnapstateEnabled: []string{"a"},
+			changed:                  false,
+		},
+		{
+			disable:                   []string{"a"},
+			expectedSnapstateDisabled: []string{"a"},
+			changed:                   true,
+		},
+		{
+			enable:                   []string{"a", "c"},
+			expectedSnapstateEnabled: []string{"a", "c"},
+			changed:                  true,
+		},
+		{
+			disable:                   []string{"b"},
+			expectedSnapstateEnabled:  []string{"a", "c"},
+			expectedSnapstateDisabled: []string{"b"},
+			changed:                   true,
+		},
+		{
+			disable:                   []string{"b", "c"},
+			expectedSnapstateEnabled:  []string{"a"},
+			expectedSnapstateDisabled: []string{"b", "c"},
+			changed:                   true,
+		},
+	}
+
+	snapst := snapstate.SnapState{}
+
+	for _, tst := range tests {
+		var enable, disable []*snap.AppInfo
+		for _, srv := range tst.enable {
+			enable = append(enable, &snap.AppInfo{Name: srv})
+		}
+		for _, srv := range tst.disable {
+			disable = append(disable, &snap.AppInfo{Name: srv})
+		}
+		result, err := servicestate.UpdateSnapstateServices(&snapst, enable, disable)
+		c.Assert(err, IsNil)
+		c.Check(result, Equals, tst.changed)
+		c.Check(snapst.ServicesEnabledByHooks, DeepEquals, tst.expectedSnapstateEnabled)
+		c.Check(snapst.ServicesDisabledByHooks, DeepEquals, tst.expectedSnapstateDisabled)
+	}
+
+	services := []*snap.AppInfo{{Name: "foo"}}
+	_, err := servicestate.UpdateSnapstateServices(nil, services, services)
+	c.Assert(err, ErrorMatches, `internal error: cannot handle enabled and disabled services at the same time`)
+}
