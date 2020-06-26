d20c0bfb6d48ce1cd650229c8d8b718ae9fe8640
Zygmunt Krynicki <me@zygoon.pl>
2020-06-26 17:03:20 +0200

    sandbox/cgroup: reorder comment for readability
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- sandbox/cgroup/tracking.go
+++ sandbox/cgroup/tracking.go
@@ -185,13 +185,13 @@ var (
 // The unit name and the PID to attach are provided as well. The DBus method
 // call is performed outside confinement established by snap-confine.
 var doCreateTransientScope = func(conn *dbus.Conn, unitName string, pid int) error {
+	// Documentation of StartTransientUnit is available at
+	// https://www.freedesktop.org/wiki/Software/systemd/dbus/
+	//
 	// The property and auxUnit types are not well documented but can be traced
 	// from systemd source code. Systemd defines the signature of
 	// StartTransientUnit as "ssa(sv)a(sa(sv))". The signature can be
 	// decomposed as follows:
-	//
-	// Partial documentation, at the time of this writing, is available at
-	// https://www.freedesktop.org/wiki/Software/systemd/dbus/
 	//
 	// unitName string // name of the unit to start
 	// jobMode string  // corresponds to --job-mode= (see systemctl(1) manual page)
