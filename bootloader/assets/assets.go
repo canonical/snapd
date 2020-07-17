03beaccb44b01f66b3ed14b70fc06d79f0dce97e
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2020-07-17 12:04:20 +0200

    bootloader/assets: allow registering and querying per-edition
    Add helpers for registering per edition snippets under the same key.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- bootloader/assets/assets.go
+++ bootloader/assets/assets.go
@@ -23,14 +23,24 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 )
 
 var registeredAssets = map[string][]byte{}
 
+type forEditions struct {
+	// First edition this snippet is used in
+	FirstEdition uint
+	// Snippet data
+	Snippet []byte
+}
+
+var registeredEditionAssets = map[string][]forEditions{}
+
 // registerInternal registers an internal asset under the given name.
 func registerInternal(name string, data []byte) {
 	if _, ok := registeredAssets[name]; ok {
-		panic(fmt.Sprintf("asset %v is already registered", name))
+		panic(fmt.Sprintf("asset %q is already registered", name))
 	}
 	registeredAssets[name] = data
 }
@@ -39,6 +49,51 @@ func registerInternal(name string, data []byte) {
 // name, or nil when none was found.
 func Internal(name string) []byte {
 	return registeredAssets[name]
+}
+
+type byFirstEdition []forEditions
+
+func (b byFirstEdition) Len() int           { return len(b) }
+func (b byFirstEdition) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byFirstEdition) Less(i, j int) bool { return b[i].FirstEdition < b[j].FirstEdition }
+
+// registerSnippetForEditions register a set of snippets, each carrying the
+// first edition number it applies to, under a given key.
+func registerSnippetForEditions(name string, snippets []forEditions) {
+	if _, ok := registeredEditionAssets[name]; ok {
+		panic(fmt.Sprintf("edition asset %q is already registered", name))
+	}
+
+	sort.Sort(byFirstEdition(snippets))
+	for i := range snippets {
+		if i == 0 {
+			continue
+		}
+		if snippets[i-1].FirstEdition == snippets[i].FirstEdition {
+			panic(fmt.Sprintf(`first edition %v repeated in edition asset %q`,
+				snippets[i].FirstEdition, name))
+		}
+	}
+	registeredEditionAssets[name] = snippets
+}
+
+// SnippetForEdition returns a snippet registered under given name,
+// applicable for the provided edition number.
+func SnippetForEdition(name string, edition uint) []byte {
+	snippets := registeredEditionAssets[name]
+	if snippets == nil {
+		return nil
+	}
+	var current []byte
+	// snippets are sorted by ascending edition number when adding
+	for _, snip := range snippets {
+		if edition >= snip.FirstEdition {
+			current = snip.Snippet
+		} else {
+			break
+		}
+	}
+	return current
 }
 
 // MockInternal mocks the contents of an internal asset for use in testing.
