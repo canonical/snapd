5cd1b6fe1bf33476f01073b75c380f820a8bf244
Ian Johnson <ian.johnson@canonical.com>
2020-07-22 14:16:58 -0500

    daemon/api_debug_seeding.go: adjust names of fields as per code-review
    Also return internal error on key errors
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- daemon/api_debug_seeding.go
+++ daemon/api_debug_seeding.go
@@ -26,81 +26,85 @@ import (
 )
 
 type seedingInfo struct {
-	// true if the device is seeded (same as "seeded" in the state)
+	// Seeded is true when the device was seeded (same as "seeded" in the
+	// state).
 	Seeded bool `json:"seeded,omitempty"`
 
-	// true if snap-preseed was applied
+	// Preseeded is true if snap-preseed was applied.
 	Preseeded bool `json:"preseeded,omitempty"`
 
-	// when snap-preseed was started
+	// PreseedStartTime is when snap-preseed was started.
 	PreseedStartTime time.Time `json:"preseed-start-time,omitempty"`
 
-	// when snap-preseed work finished (i.e. before firstboot)
-	PreseededTime time.Time `json:"preseeded-time,omitempty"`
+	// PreseedTime is when snap-preseed work finished (i.e. before firstboot).
+	PreseedTime time.Time `json:"preseed-time,omitempty"`
 
-	// when traditional seeding started (when not preseeding)
+	// SeedStartTime is when traditional seeding started (when not preseeding).
 	SeedStartTime time.Time `json:"seed-start-time,omitempty"`
 
-	// when seeding was started after first boot of preseeded image
+	// SeedRestartTime is when seeding was started after first boot of preseeded
+	// image.
 	SeedRestartTime time.Time `json:"seed-restart-time,omitempty"`
 
-	// when seeding finished
+	// SeedTime is when seeding finished.
 	SeedTime time.Time `json:"seed-time,omitempty"`
 
-	// system-key created during preseeding (when snap-preseed was ran)
+	// PreseedSystemKey is the system-key that was created during preseeding
+	// (when snap-preseed was ran).
 	PreseedSystemKey interface{} `json:"preseed-system-key,omitempty"`
 
-	// system-key created on first boot of the preseeded image
-	RestartSystemKey interface{} `json:"restart-system-key,omitempty"`
+	// SeedRestartSystemKey is the system-key that was created on first boot of the
+	// preseeded image.
+	SeedRestartSystemKey interface{} `json:"seed-restart-system-key,omitempty"`
 }
 
 func getSeedingInfo(st *state.State) Response {
 	var seeded, preseeded bool
 	err := st.Get("seeded", &seeded)
 	if err != nil && err != state.ErrNoState {
-		return BadRequest(err.Error())
+		return InternalError(err.Error())
 	}
 	if err = st.Get("preseeded", &preseeded); err != nil && err != state.ErrNoState {
-		return BadRequest(err.Error())
+		return InternalError(err.Error())
 	}
 
-	var preseedStartTime, preseededTime, seedStartTime, seedRestartTime, seedTime time.Time
+	var preseedStartTime, preseedTime, seedStartTime, seedRestartTime, seedTime time.Time
 	for _, t := range []struct {
 		name string
 		tm   *time.Time
 	}{
 		{"preseed-start-time", &preseedStartTime},
-		{"preseeded-time", &preseededTime},
+		{"preseed-time", &preseedTime},
 		{"seed-start-time", &seedStartTime},
 		{"seed-restart-time", &seedRestartTime},
 		{"seed-time", &seedTime},
 	} {
 		if err := st.Get(t.name, t.tm); err != nil && err != state.ErrNoState {
-			return BadRequest(err.Error())
+			return InternalError(err.Error())
 		}
 	}
 
-	var preseedSysKey, restartSysKey interface{}
+	var preseedSysKey, seedRestartSysKey interface{}
 	if err := st.Get("preseed-system-key", &preseedSysKey); err != nil && err != state.ErrNoState {
-		return BadRequest(err.Error())
+		return InternalError(err.Error())
 	}
-	if err := st.Get("restart-system-key", &restartSysKey); err != nil && err != state.ErrNoState {
-		return BadRequest(err.Error())
+	if err := st.Get("seed-restart-system-key", &seedRestartSysKey); err != nil && err != state.ErrNoState {
+		return InternalError(err.Error())
 	}
 
 	// XXX: consistency & sanity checks, e.g. if preseeded, then need to have
 	// preseed-start-time, preseeded-time, preseed-system-key etc?
 
 	data := &seedingInfo{
-		Seeded:           seeded,
-		Preseeded:        preseeded,
-		PreseedSystemKey: preseedSysKey,
-		RestartSystemKey: restartSysKey,
-		PreseedStartTime: preseedStartTime,
-		PreseededTime:    preseededTime,
-		SeedStartTime:    seedStartTime,
-		SeedRestartTime:  seedRestartTime,
-		SeedTime:         seedTime,
+		Seeded:               seeded,
+		Preseeded:            preseeded,
+		PreseedSystemKey:     preseedSysKey,
+		SeedRestartSystemKey: seedRestartSysKey,
+		PreseedStartTime:     preseedStartTime,
+		PreseedTime:          preseedTime,
+		SeedStartTime:        seedStartTime,
+		SeedRestartTime:      seedRestartTime,
+		SeedTime:             seedTime,
 	}
 
 	return SyncResponse(data, nil)
