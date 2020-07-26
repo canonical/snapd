4dc537f22a7837a06c2f4a106d76a477a6ee30a9
Samuele Pedroni <pedronis@lucediurna.net>
2020-07-26 16:49:51 +0200

    client,docs: standardize error kind doc comments
    add what's missing and align forum and doc comments
    
    then have a tool to generate what is on the forum:
    
    $ cd docs
    $ go run error-kinds.go
    
    open question: when/if to have a dot at the end? always? only for
    multi-sentence comments?

--- client/errors.go
+++ client/errors.go
@@ -22,12 +22,19 @@ package client
 // XXX: sync with the error kinds in daemon and ensure we define them
 //      only in a single place
 
-// Keep in sync with: https://forum.snapcraft.io/t/17954
+// error kind const value doc comments here have a non-default,
+// specialized style (to help docs/error-kind.go):
+//
+// // ErrorKind...: DESCRIPTION [no final dot]
+//
+// `code-like` quoting should be used when meaningful.
+
+// Error kinds. Keep in sync with: https://forum.snapcraft.io/t/using-the-rest-api/18603#heading--errors
 const (
-	// ErrorKindTwoFactorRequired the client needs to retry the
-	// login command including an OTP
+	// ErrorKindTwoFactorRequired: the client needs to retry the
+	// `login` command including an OTP
 	ErrorKindTwoFactorRequired = "two-factor-required"
-	// ErrorKindTwoFactorFailed: the OTP provided wasn’t recognised
+	// ErrorKindTwoFactorFailed: the OTP provided wasn't recognised
 	ErrorKindTwoFactorFailed = "two-factor-failed"
 	// ErrorKindLoginRequired: the requested operation cannot be
 	// performed without an authenticated user. This is the kind
@@ -35,7 +42,7 @@ const (
 	ErrorKindLoginRequired = "login-required"
 	// ErrorKindInvalidAuthData: the authentication data provided
 	// failed to validate (e.g. a malformed email address). The
-	// value of the error is an object with a key per failed field
+	// `value` of the error is an object with a key per failed field
 	// and a list of the failures on each field.
 	ErrorKindInvalidAuthData = "invalid-auth-data"
 	// ErrorKindTermsNotAccepted: deprecated, do not document
@@ -44,87 +51,93 @@ const (
 	ErrorKindNoPaymentMethods = "no-payment-methods"
 	// ErrorKindPaymentDeclined: deprecated, do not document
 	ErrorKindPaymentDeclined = "payment-declined"
-	// ErrorKindPasswordPolicy: provided password doesn’t meet
-	// system policy.
+	// ErrorKindPasswordPolicy: provided password doesn't meet
+	// system policy
 	ErrorKindPasswordPolicy = "password-policy"
 
 	// ErrorKindSnapAlreadyInstalled: the requested snap is
-	// already installed.
+	// already installed
 	ErrorKindSnapAlreadyInstalled = "snap-already-installed"
-	// ErrorKindSnapNotInstalled:  the requested snap is not installed.
+	// ErrorKindSnapNotInstalled:  the requested snap is not installed
 	ErrorKindSnapNotInstalled = "snap-not-installed"
-	// ErrorKindSnapNotFound: the requested snap couldn’t be found.
+	// ErrorKindSnapNotFound: the requested snap couldn't be found
 	ErrorKindSnapNotFound = "snap-not-found"
-	// ErrorKindAppNotFound: the requested app couldn’t be found.
+	// ErrorKindAppNotFound: the requested app couldn't be found
 	ErrorKindAppNotFound = "app-not-found"
-	// ErrorKindSnapLocal: the requested snap couldn’t be found in
-	// the store.
+	// ErrorKindSnapLocal: the requested snap couldn't be found in
+	// the store
 	ErrorKindSnapLocal = "snap-local"
 	// ErrorKindSnapNeedsDevMode: the requested snap needs devmode
-	// to be installed.
+	// to be installed
 	ErrorKindSnapNeedsDevMode = "snap-needs-devmode"
 	// ErrorKindSnapNeedsClassic: the requested snap needs classic
-	// confinement to be installed.
+	// confinement to be installed
 	ErrorKindSnapNeedsClassic = "snap-needs-classic"
-	// ErrorKindSnapNeedsClassicSystem: the requested snap can’t
-	// be installed on the current system.
+	// ErrorKindSnapNeedsClassicSystem: the requested snap can't
+	// be installed on the current non-classic system
 	ErrorKindSnapNeedsClassicSystem = "snap-needs-classic-system"
-	// ErrorKindSnapNotClassic: snap not compatible with classic mode.
+	// ErrorKindSnapNotClassic: snap not compatible with classic mode
 	ErrorKindSnapNotClassic = "snap-not-classic"
 	// ErrorKindNoUpdateAvailable: the requested snap does not
-	// have an update available.
+	// have an update available
 	ErrorKindNoUpdateAvailable = "snap-no-update-available"
 
 	// ErrorKindRevisionNotAvailable: no snap revision available
-	// as specified.
+	// as specified
 	ErrorKindRevisionNotAvailable = "snap-revision-not-available"
 	// ErrorKindChannelNotAvailable: no snap revision on specified
-	// channel. The value of the error is a rich object with
-	// requested snap-name, action, channel, architecture, and
-	// actually available releases as list of
-	// {"architecture":... , "channel": ...} objects.
+	// channel. The `value` of the error is a rich object with
+	// requested `snap-name`, `action`, `channel`, `architecture`, and
+	// actually available `releases` as list of
+	// `{"architecture":... , "channel": ...}` objects.
 	ErrorKindChannelNotAvailable = "snap-channel-not-available"
 	// ErrorKindArchitectureNotAvailable: no snap revision on
 	// specified architecture. Value has the same format as for
-	// snap-channel-not-available:
+	// `snap-channel-not-available`.
 	ErrorKindArchitectureNotAvailable = "snap-architecture-not-available"
 
 	// ErrorKindChangeConflict: the requested operation would
 	// conflict with currently ongoing change. This is a temporary
-	// error. The error value is an object with optional fields
-	// snap-name, change-kind of the ongoing change.
+	// error. The error `value` is an object with optional fields
+	// `snap-name`, `change-kind` of the ongoing change.
 	ErrorKindChangeConflict = "snap-change-conflict"
 
-	// ErrorKindNotSnap : the given snap or directory does not
-	// look like a snap.
+	// ErrorKindNotSnap: the given snap or directory does not
+	// look like a snap
 	ErrorKindNotSnap = "snap-not-a-snap"
 
-	// ErrorKindNetworkTimeout: a timeout occurred during the request.
+	// ErrorKindNetworkTimeout: a timeout occurred during the request
 	ErrorKindNetworkTimeout = "network-timeout"
 
-	// ErrorKindDNSFailure:
+	// ErrorKindDNSFailure: DNS not responding
 	ErrorKindDNSFailure = "dns-failure"
 
-	// ErrorKindInterfacesUnchanged: the requested interfaces’
-	// operation would have no effect.
+	// ErrorKindInterfacesUnchanged: the requested interfaces'
+	// operation would have no effect
 	ErrorKindInterfacesUnchanged = "interfaces-unchanged"
 
-	// ErrorKindBadQuery: a bad query was provided.
+	// ErrorKindBadQuery: a bad query was provided
 	ErrorKindBadQuery = "bad-query"
 	// ErrorKindConfigNoSuchOption: the given configuration option
-	// does not exist.
+	// does not exist
 	ErrorKindConfigNoSuchOption = "option-not-found"
 
-	// ErrorKindSystemRestart:
-	ErrorKindSystemRestart = "system-restart"
-	// ErrorKindDaemonRestart:
-	ErrorKindDaemonRestart = "daemon-restart"
-
-	// ErrorKindAssertionNotFound:
+	// ErrorKindAssertionNotFound: assertion can not be found
 	ErrorKindAssertionNotFound = "assertion-not-found"
 
-	// ErrorKindUnsuccessful:
+	// ErrorKindUnsuccessful: snapctl command was unsuccessful
 	ErrorKindUnsuccessful = "unsuccessful"
 
-	// XXX: auth-cancelled needs error kind and documentation
+	// ErrorKindAuthCancelled: authentication was cancelled by the user
+	ErrorKindAuthCancelled = "auth-cancelled"
+)
+
+// Maintenance error kinds.
+// These are used only inside the maintenance field of responses.
+// Keep in sync with: https://forum.snapcraft.io/t/using-the-rest-api/18603#heading--maint-errors
+const (
+	// ErrorKindDaemonRestart: daemon is restarting
+	ErrorKindDaemonRestart = "daemon-restart"
+	// ErrorKindSystemRestart: system is restarting
+	ErrorKindSystemRestart = "system-restart"
 )
