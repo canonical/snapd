eb4ec122674355295911a0cd18027ecc34e06d0e
Paweł Stołowski <stolowski@gmail.com>
2020-08-13 09:12:20 +0200

    snapd-generator: Generate drop-in to use fuse.squashfuse/fuse.snapfuse when running in a container.

--- cmd/snapd-generator/main.c
+++ cmd/snapd-generator/main.c
@@ -18,6 +18,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
 
 #include "config.h"
 
@@ -38,18 +41,8 @@ static sc_mountinfo_entry *find_root_mountinfo(sc_mountinfo * mounts)
 	return root;
 }
 
-int main(int argc, char **argv)
+int ensure_root_fs_shared(const char *normal_dir)
 {
-	if (argc != 4) {
-		printf("usage: snapd-workaround-generator "
-		       "normal-dir early-dir late-dir\n");
-		return 1;
-	}
-	const char *normal_dir = argv[1];
-	// For reference, but we don't use those variables here.
-	// const char *early_dir = argv[2];
-	// const char *late_dir = argv[3];
-
 	// Load /proc/self/mountinfo so that we can inspect the root filesystem.
 	sc_mountinfo *mounts SC_CLEANUP(sc_cleanup_mountinfo) = NULL;
 	mounts = sc_parse_mountinfo(NULL);
@@ -96,4 +89,81 @@ int main(int argc, char **argv)
 	fprintf(f, "[Install]\n");
 	fprintf(f, "WantedBy=local-fs.target\n");
 	return 0;
+}
+
+int ensure_fusesquashfs_inside_container(const char *normal_dir)
+{
+	// check if we are running inside a container
+	if (system("systemd-detect-virt --container --quiet") != 0) {
+		return 0;
+	}
+
+	char *fstype = NULL;
+	if (system("which squashfuse > /dev/null") == 0) {
+		fstype = "fuse.squashfuse";
+	} else if (system("which snapfuse > /dev/null") == 0) {
+		fstype = "fuse.snapfuse";
+	} else {
+		fprintf(stderr,
+			"cannot find squashfuse or snapfuse executable\n");
+		return 2;
+	}
+
+	DIR *units_dir SC_CLEANUP(sc_cleanup_closedir) = NULL;
+    units_dir = opendir("/etc/systemd/system");
+    if (units_dir == NULL) {
+		fprintf(stderr,
+			"cannot open /etc/systemd/system directory: %m\n");
+        return 2;
+    }
+
+	char fname[PATH_MAX + 1] = { 0 };
+
+    struct dirent *ent;
+    while (ent = readdir(units_dir)) {
+		if (!(sc_startswith(ent->d_name, "snap-") && sc_endswith(ent->d_name, ".mount"))) {
+			continue;
+		}
+		sc_must_snprintf(fname, sizeof fname,
+			"%s/%s.d", normal_dir, ent->d_name);
+		if (mkdir(fname, 0755) != 0) {
+			// generators are run over clean directory, no need to handle EEXIST
+			fprintf(stderr,
+				"cannot create %s directory: %m\n", fname);
+			return 2;
+		}
+
+		sc_must_snprintf(fname, sizeof fname,
+			"%s/%s.d/container.conf", normal_dir, ent->d_name);
+
+		FILE *f SC_CLEANUP(sc_cleanup_file) = NULL;
+		f = fopen(fname, "w");
+		if (!f) {
+			fprintf(stderr, "cannot open %s: %m\n", fname);
+			return 1;
+		}
+		fprintf(f, "[Mount]\n");
+		fprintf(f, "Type=%s\n", fstype);
+    }
+	
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4) {
+		printf("usage: snapd-workaround-generator "
+		       "normal-dir early-dir late-dir\n");
+		return 1;
+	}
+	const char *normal_dir = argv[1];
+	// For reference, but we don't use those variables here.
+	// const char *early_dir = argv[2];
+	// const char *late_dir = argv[3];
+
+	int status = 0;
+	status = ensure_root_fs_shared(normal_dir);
+	status |= ensure_fusesquashfs_inside_container(normal_dir);
+
+	return status;
 }
