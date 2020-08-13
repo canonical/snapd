23715d465acafcebdc7b5b26415f215c6f7469b2
Michael Vogt <mvo@ubuntu.com>
2020-08-13 12:32:15 +0200

    gadget,kernel: add new "kernel" and "gadget/edition" packages
    This commit moves the kernel.yaml parsing into the kernel package
    and extracts the gadget:editionNumber" helper into it's own
    gadget/editon packages so that it can be shared between the
    kernel and the gadget packages. Because meta/kernel.yaml is
    optional it is not an error if it is missing.
    
    Extra tests are also added.

--- kernel/kernel_test.go
+++ kernel/kernel_test.go
@@ -17,21 +17,24 @@
  *
  */
 
-package gadget_test
+package kernel_test
 
 import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"testing"
 
 	. "gopkg.in/check.v1"
 
-	"github.com/snapcore/snapd/gadget"
+	"github.com/snapcore/snapd/kernel"
 )
 
 type kernelYamlTestSuite struct{}
 
 var _ = Suite(&kernelYamlTestSuite{})
+
+func TestCommand(t *testing.T) { TestingT(t) }
 
 var mockKernelYaml = []byte(`
 assets:
@@ -43,17 +46,17 @@ assets:
 `)
 
 func (s *kernelYamlTestSuite) TestInfoFromKernelYamlSad(c *C) {
-	ki, err := gadget.KernelInfoFromKernelYaml([]byte("foo"))
+	ki, err := kernel.InfoFromKernelYaml([]byte("foo"))
 	c.Check(err, ErrorMatches, "(?m)cannot parse kernel metadata: .*")
 	c.Check(ki, IsNil)
 }
 
 func (s *kernelYamlTestSuite) TestInfoFromKernelYamlHappy(c *C) {
-	ki, err := gadget.KernelInfoFromKernelYaml(mockKernelYaml)
+	ki, err := kernel.InfoFromKernelYaml(mockKernelYaml)
 	c.Check(err, IsNil)
-	c.Check(ki, DeepEquals, &gadget.KernelInfo{
-		Assets: map[string]*gadget.KernelAsset{
-			"dtbs": &gadget.KernelAsset{
+	c.Check(ki, DeepEquals, &kernel.Info{
+		Assets: map[string]*kernel.Asset{
+			"dtbs": &kernel.Asset{
 				Edition: 1,
 				Content: []string{
 					"dtbs/bcm2711-rpi-4-b.dtb",
@@ -64,9 +67,22 @@ func (s *kernelYamlTestSuite) TestInfoFromKernelYamlHappy(c *C) {
 	})
 }
 
+func (s *kernelYamlTestSuite) TestReadKernelYamlOptional(c *C) {
+	ki, err := kernel.ReadInfo("this-path-does-not-exist")
+	c.Check(err, IsNil)
+	c.Check(ki, DeepEquals, &kernel.Info{})
+}
+
 func (s *kernelYamlTestSuite) TestReadKernelYamlSad(c *C) {
-	ki, err := gadget.ReadKernelInfo("this-path-does-not-exist")
-	c.Check(err, ErrorMatches, `cannot read kernel info: open this-path-does-not-exist/meta/kernel.yaml: no such file or directory`)
+	mockKernelSnapRoot := c.MkDir()
+	kernelYamlPath := filepath.Join(mockKernelSnapRoot, "meta/kernel.yaml")
+	err := os.MkdirAll(filepath.Dir(kernelYamlPath), 0755)
+	c.Assert(err, IsNil)
+	err = ioutil.WriteFile(kernelYamlPath, []byte(`invalid-kernel-yaml`), 0644)
+	c.Assert(err, IsNil)
+
+	ki, err := kernel.ReadInfo(mockKernelSnapRoot)
+	c.Check(err, ErrorMatches, `(?m)cannot parse kernel metadata: yaml: unmarshal errors:.*`)
 	c.Check(ki, IsNil)
 }
 
@@ -78,11 +94,11 @@ func (s *kernelYamlTestSuite) TestReadKernelYamlHappy(c *C) {
 	err = ioutil.WriteFile(kernelYamlPath, mockKernelYaml, 0644)
 	c.Assert(err, IsNil)
 
-	ki, err := gadget.ReadKernelInfo(mockKernelSnapRoot)
+	ki, err := kernel.ReadInfo(mockKernelSnapRoot)
 	c.Assert(err, IsNil)
-	c.Check(ki, DeepEquals, &gadget.KernelInfo{
-		Assets: map[string]*gadget.KernelAsset{
-			"dtbs": &gadget.KernelAsset{
+	c.Check(ki, DeepEquals, &kernel.Info{
+		Assets: map[string]*kernel.Asset{
+			"dtbs": &kernel.Asset{
 				Edition: 1,
 				Content: []string{
 					"dtbs/bcm2711-rpi-4-b.dtb",
