af8a76a26bf90bf4020f56ce62ec1565659ba585
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-09-02 16:42:50 +0200

    interfaces/fpga: add fpga interface
    The fpga interface will allow programming fpgas for different
    vendors. It lets plug users access the standard fpga kernel sysfs
    interface [1-3] and also vendor specific interfaces.
    
    [1] https://github.com/torvalds/linux/blob/master/Documentation/ABI/testing/sysfs-class-fpga-manager
    [2] https://github.com/torvalds/linux/blob/master/Documentation/ABI/testing/sysfs-class-fpga-region
    [3] https://github.com/torvalds/linux/blob/master/Documentation/ABI/testing/sysfs-class-fpga-bridge

---
+++ interfaces/builtin/fpga.go
@@ -0,0 +1,65 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const fpgaSummary = `allows access to the FPGA subsystem`
+
+const fpgaBaseDeclarationSlots = `
+  fpga:
+    allow-installation:
+      slot-snap-type:
+        - core
+`
+
+const fpgaConnectedPlugAppArmor = `
+# Description: Can access fpga subsystem.
+
+# Devices
+/dev/fpga[0-9]* rw,
+
+# /sys/class/fpga_* specified by the kernel docs
+/sys/class/fpga_manager/fpga[0-9]*/{name,state,status} r,
+/sys/class/fpga_region/region[0-9]*/compat_id r,
+/sys/class/fpga_bridge/bridge[0-9]*/{name,state} r,
+
+# Xilinx zynqmp FPGA
+/sys/devices/platform/firmware:zynqmp-firmware/firmware:zynqmp-firmware:pcap/fpga_manager/fpga[0-9]*/{name,state,status} r,
+/sys/devices/platform/firmware:zynqmp-firmware/firmware:zynqmp-firmware:pcap/fpga_manager/fpga[0-9]*/firmware w,
+/sys/devices/platform/firmware:zynqmp-firmware/firmware:zynqmp-firmware:pcap/fpga_manager/fpga[0-9]*/{flags,key} rw,
+/sys/devices/platform/fpga-full/fpga_region/region[0-9]*/compat_id r,
+
+# Xilinx zynqmp module parameters
+/sys/module/zynqmp_fpga/parameters/readback_type rw,
+
+# configfs interface
+/sys/kernel/config/device-tree/overlays/full{,1}/ rw,
+/sys/kernel/config/device-tree/overlays/full{,1}/path rw,
+`
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "fpga",
+		summary:               fpgaSummary,
+		implicitOnCore:        true,
+		implicitOnClassic:     true,
+		baseDeclarationSlots:  fpgaBaseDeclarationSlots,
+		connectedPlugAppArmor: fpgaConnectedPlugAppArmor,
+	})
+}
