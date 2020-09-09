3b36a8ad94e9bbd96d73ff83f0f18466d345efee
Oliver Grawert <oliver.grawert@canonical.com>
2020-09-09 17:13:09 +0200

    interfaces/tee: add TEE/OPTEE interface

---
+++ interfaces/builtin/tee.go
@@ -0,0 +1,55 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const teeSummary = `allows access to Trusted Execution Environment devices`
+
+const teeBaseDeclarationSlots = `
+  tee:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const teeConnectedPlugAppArmor = `
+# Description: for those who need to talk to the TEE subsystem over
+# /dev/tee[0-9]* and/or /dev/teepriv[0-0]*
+
+/dev/tee[0-9]* rw,
+/dev/teepriv[0-9]* rw,
+`
+
+var teeConnectedPlugUDev = []string{
+	`KERNEL=="tee[0-9]*"`,
+	`KERNEL=="teepriv[0-9]*"`,
+}
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "tee",
+		summary:               teeSummary,
+		implicitOnCore:        true,
+		implicitOnClassic:     true,
+		baseDeclarationSlots:  teeBaseDeclarationSlots,
+		connectedPlugAppArmor: teeConnectedPlugAppArmor,
+		connectedPlugUDev:     teeConnectedPlugUDev,
+	})
+}
---
+++ interfaces/builtin/tee_test.go
@@ -0,0 +1,108 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/interfaces/udev"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type TeeInterfaceSuite struct {
+	iface    interfaces.Interface
+	slotInfo *snap.SlotInfo
+	slot     *interfaces.ConnectedSlot
+	plugInfo *snap.PlugInfo
+	plug     *interfaces.ConnectedPlug
+}
+
+var _ = Suite(&TeeInterfaceSuite{
+	iface: builtin.MustInterface("tee"),
+})
+
+const teeConsumerYaml = `name: consumer
+version: 0
+apps:
+ app:
+  plugs: [tee]
+`
+
+const teeCoreYaml = `name: core
+version: 0
+type: os
+slots:
+  tee:
+`
+
+func (s *TeeInterfaceSuite) SetUpTest(c *C) {
+	s.plug, s.plugInfo = MockConnectedPlug(c, teeConsumerYaml, nil, "tee")
+	s.slot, s.slotInfo = MockConnectedSlot(c, teeCoreYaml, nil, "tee")
+}
+
+func (s *TeeInterfaceSuite) TestName(c *C) {
+	c.Assert(s.iface.Name(), Equals, "tee")
+}
+
+func (s *TeeInterfaceSuite) TestSanitizeSlot(c *C) {
+	c.Assert(interfaces.BeforePrepareSlot(s.iface, s.slotInfo), IsNil)
+}
+
+func (s *TeeInterfaceSuite) TestSanitizePlug(c *C) {
+	c.Assert(interfaces.BeforePreparePlug(s.iface, s.plugInfo), IsNil)
+}
+
+func (s *TeeInterfaceSuite) TestAppArmorSpec(c *C) {
+	spec := &apparmor.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, "/dev/tee[0-9]*")
+}
+
+func (s *TeeInterfaceSuite) TestUDevSpec(c *C) {
+	spec := &udev.Specification{}
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.Snippets(), HasLen, 3)
+	c.Assert(spec.Snippets(), testutil.Contains, `# tee
+KERNEL=="tee[0-9]*", TAG+="snap_consumer_app"`)
+	c.Assert(spec.Snippets(), testutil.Contains, `# tee
+KERNEL=="teepriv[0-9]*", TAG+="snap_consumer_app"`)
+	c.Assert(spec.Snippets(), testutil.Contains, `TAG=="snap_consumer_app", RUN+="/usr/lib/snapd/snap-device-helper $env{ACTION} snap_consumer_app $devpath $major:$minor"`)
+}
+
+func (s *TeeInterfaceSuite) TestStaticInfo(c *C) {
+	si := interfaces.StaticInfoOf(s.iface)
+	c.Assert(si.ImplicitOnCore, Equals, true)
+	c.Assert(si.ImplicitOnClassic, Equals, true)
+	c.Assert(si.Summary, Equals, `allows access to the Trusted Execution Environment devices`)
+	c.Assert(si.BaseDeclarationSlots, testutil.Contains, "tee")
+}
+
+func (s *TeeInterfaceSuite) TestAutoConnect(c *C) {
+	c.Assert(s.iface.AutoConnect(s.plugInfo, s.slotInfo), Equals, true)
+}
+
+func (s *TeeInterfaceSuite) TestInterfaces(c *C) {
+	c.Check(builtin.Interfaces(), testutil.DeepContains, s.iface)
+}
