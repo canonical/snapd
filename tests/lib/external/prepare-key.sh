58d80a195511955bcb32ffde4f24c8bbd3e7ebee
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-09-12 11:55:55 -0300

    Adjust change to spread which now waits for rsa key file

--- tests/lib/external/prepare-key.sh
+++ tests/lib/external/prepare-key.sh
@@ -4,19 +4,19 @@ set -ex
 INSTANCE_IP="${1:-localhost}"
 INSTANCE_PORT="${2:-8022}"
 USER="${3:-$(whoami)}"
-CERT_NAME="${4:-spread_external}"
-PASSPHRASE="${5:-ubuntu}"
+KEY_NAME="${4:-spread_external}"
+PASSPHRASE="${5:-}"
 
 execute_remote() {
     # shellcheck disable=SC2029
     ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p "$INSTANCE_PORT" "$USER@$INSTANCE_IP" "$@"
 }
 
-# Create certificates in case those are not stored
-if [ ! -f ~/.ssh/"$CERT_NAME" ] || [ ! -f ~/.ssh/"$CERT_NAME".pub ]; then
-	ssh-keygen -t rsa -N "$PASSPHRASE" -f ~/.ssh/"$CERT_NAME"
+# Create the key in case it is not already created
+if [ ! -f "$KEY_NAME" ] || [ ! -f "$KEY_NAME".pub ]; then
+	ssh-keygen -t rsa -N "$PASSPHRASE" -f "$KEY_NAME"
 fi
 
 execute_remote "sudo mkdir -p /root/.ssh"
 execute_remote "sudo chmod 700 /root/.ssh"
-cat ~/.ssh/"$CERT_NAME".pub | execute_remote "sudo tee -a /root/.ssh/authorized_keys > /dev/null"
+cat "$KEY_NAME".pub | execute_remote "sudo tee -a /root/.ssh/authorized_keys > /dev/null"
