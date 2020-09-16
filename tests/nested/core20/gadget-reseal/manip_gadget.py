ba67cf4c9a356db8e2623b0f512fcd6d264819ca
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2020-09-16 11:36:28 +0200

    tests/nested/core20/gadget-reseal: fixes for the test
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/nested/core20/gadget-reseal/manip_gadget.py
+++ tests/nested/core20/gadget-reseal/manip_gadget.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
 
 import sys
+import argparse
+
 import yaml
 
-with open(sys.argv[1]) as f:
-    gadget_yaml = yaml.load(f)
 
-i = 0
-structure = gadget_yaml["volumes"]["pc"]["structure"]
-while i < len(structure):
-    entry = structure[i]
-    if entry["name"] == "ubuntu-seed":
-        edition = entry["update"]["edition"]
-        entry["update"]["edition"] = edition+1
-        break
-    i += 1
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="pc gadget yaml generator for test")
+    parser.add_argument(
+        "gadgetyaml", type=argparse.FileType("r"), help="path to gadget.yaml input file"
+    )
+    return parser.parse_args()
 
-with open(sys.argv[1], "w") as f:
-    yaml.dump(seed, stream=f, indent=2, default_flow_style=False)
 
+def bump_update_edition(update):
+    if update is None:
+        return {"edition": 1}
+    if "edition" not in update:
+        update["edition"] = 1
+    else:
+        update["edition"] += 1
+    return update
+
+
+def main(opts):
+    gadget_yaml = yaml.safe_load(opts.gadgetyaml)
+
+    structs = gadget_yaml["volumes"]["pc"]["structure"]
+    matched = [s for s in structs if s["name"] == "ubuntu-seed"]
+    if not matched:
+        raise RuntimeError("ubuntu-seed not found among: {}".format(structs))
+
+    ubuntu_seed = matched[0]
+    ubuntu_seed["update"] = bump_update_edition(ubuntu_seed.get("update"))
+
+    yaml.dump(gadget_yaml, stream=sys.stdout, indent=2, default_flow_style=False)
+
+
+if __name__ == "__main__":
+    opts = parse_arguments()
+    main(opts)
