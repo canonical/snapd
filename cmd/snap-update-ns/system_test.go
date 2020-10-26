70287a286d0d669653dd8d287c8e86a6a0a73120
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2020-10-26 09:16:09 +0100

    cmd/snap-update-ns: remove ICE hints
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/snap-update-ns/system_test.go
+++ cmd/snap-update-ns/system_test.go
@@ -60,7 +60,6 @@ func (s *systemSuite) TestAssumptions(c *C) {
 	c.Check(as.ModeForPath("/var/lib/snapd/hostfs/tmp/snap.x11-server/tmp"), Equals, os.FileMode(1777))
 	c.Check(as.ModeForPath("/var/lib/snapd/hostfs/tmp/snap.x11-server/foo"), Equals, os.FileMode(0755))
 	c.Check(as.ModeForPath("/var/lib/snapd/hostfs/tmp/snap.x11-server/tmp/.X11-unix"), Equals, os.FileMode(1777))
-	c.Check(as.ModeForPath("/var/lib/snapd/hostfs/tmp/snap.x11-server/tmp/.ICE-unix"), Equals, os.FileMode(1777))
 
 	// Instances can, in addition, access /snap/$SNAP_INSTANCE_NAME
 	upCtx = update.NewSystemProfileUpdateContext("foo_instance", false)
