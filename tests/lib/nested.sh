a3d38a8e1a00ba68ad8b8065387ce54c036567e1
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2020-10-26 18:32:52 +0100

    tests/lib/nested: add ubuntu-save when needed
    With current changes, we require ubuntu-save to be present. Make sure there is
    one.
    
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -477,6 +477,13 @@ nested_create_core_vm() {
                         snap download --basename=pc --channel="20/edge" pc
                         unsquashfs -d pc-gadget pc.snap
                         nested_secboot_sign_gadget pc-gadget "$SNAKEOIL_KEY" "$SNAKEOIL_CERT"
+                        # TODO:UC20: until https://github.com/snapcore/pc-amd64-gadget/pull/51/
+                        # lands there is no ubuntu-save in the gadget, make sure we have one
+                        "$TESTSLIB"/ensure_ubuntu_save.py pc-gadget/meta/gadget.yaml > /tmp/gadget-with-save.yaml
+                        if [ "$(cat /tmp/gadget-with-save.yaml)" != "" ]; then
+                            mv /tmp/gadget-with-save.yaml pc-gadget/meta/gadget.yaml
+                        fi
+
                         # also make logging persistent for easier debugging of 
                         # test failures, otherwise we have no way to see what 
                         # happened during a failed nested VM boot where we 
