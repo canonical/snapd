f43e479fd650b83bdc6ab8590ea810fa23e09019
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2020-10-30 11:23:14 +0100

    exportmanager: fix gofmt compat
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- overlord/exportstate/special.go
+++ overlord/exportstate/special.go
@@ -100,8 +100,10 @@ func manifestForCoreSnap(info *snap.Info) *Manifest {
 	return &Manifest{
 		SnapInstanceName:          info.InstanceName(),
 		SnapRevision:              info.Revision,
-		ExportedForSnapdAsVersion: exportedForSnapdAsVersionForCore(info), // Exception from the rule
-		Sets:                      map[string]ExportSet{tools.Name: tools},
+		ExportedForSnapdAsVersion: exportedForSnapdAsVersionForCore(info), // Exception from the rule]
+
+		// Separate to avoid gofmt annoyance across versions.
+		Sets: map[string]ExportSet{tools.Name: tools},
 	}
 }
 
