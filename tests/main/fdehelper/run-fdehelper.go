8f51553e2c5faf76d95df31d484c537ac177fdc1
Michael Vogt <mvo@ubuntu.com>
2020-11-03 10:30:14 +0100

    tests: udpate spread test to match new fdehelper design

--- tests/main/fdehelper/run-fdehelper.go
+++ tests/main/fdehelper/run-fdehelper.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/snapcore/snapd/boot/fdehook"
+	"github.com/snapcore/snapd/boot/fdehelper"
 )
 
 func main() {
@@ -13,16 +13,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	params := &fdehook.UnlockParams{
+	params := &fdehelper.RevealParams{
 		SealedKey:        stdin,
 		VolumeName:       "some-volume-name",
 		SourceDevicePath: "some-source-device-path",
-		LockKeysOnFinish: true,
 	}
-	unsealedKey, err := fdehook.Unlock(os.Args[1], params)
+	revealedKey, err := fdehelper.Reveal(os.Args[1], params)
 	if err != nil {
-		fmt.Printf("unlock error: %v\n", err)
+		fmt.Printf("reveal error: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("unsealed key: %q\n", unsealedKey)
+	fmt.Printf("revealed key: %q\n", revealedKey)
 }
