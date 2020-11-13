cac1a7fd82be9da33f1c51f02c2629a9336d51d6
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2020-11-13 13:06:41 +0100

    tests/lib/nested: fix unbound NESTED_ADD_UBUNTU_SAVE
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -512,7 +512,7 @@ nested_create_core_vm() {
                         snap download --basename=pc --channel="20/edge" pc
                         unsquashfs -d pc-gadget pc.snap
                         nested_secboot_sign_gadget pc-gadget "$SNAKEOIL_KEY" "$SNAKEOIL_CERT"
-                        if [ "$NESTED_ENABLE_TPM" = "true" ] || [ "$NESTED_ADD_UBUNTU_SAVE" = "true" ]; then
+                        if [ "$NESTED_ENABLE_TPM" = "true" ] || [ "${NESTED_ADD_UBUNTU_SAVE:-}" = "true" ]; then
                             # TODO:UC20: until https://github.com/snapcore/pc-amd64-gadget/pull/51/
                             # lands there is no ubuntu-save in the gadget, make sure we have one
                             nested_ensure_ubuntu_save pc-gadget --add
