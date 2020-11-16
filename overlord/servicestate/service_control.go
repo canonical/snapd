ee38217f5231ea277eae4f4ee950c4d3bcd2283a
Paweł Stołowski <stolowski@gmail.com>
2020-11-16 18:01:04 +0100

    Unlock state before calling wrappers in doServiceControl. Keeping the lock causes slowdown/timeout if started service calls snapd.

--- overlord/servicestate/service_control.go
+++ overlord/servicestate/service_control.go
@@ -87,15 +87,19 @@ func (m *ServiceManager) doServiceControl(t *state.Task, _ *tomb.Tomb) error {
 		}
 	}
 
-	meter := snapstate.NewTaskProgressAdapterLocked(t)
+	meter := snapstate.NewTaskProgressAdapterUnlocked(t)
 
+	// Note - state must be unlocked when calling wrappers below.
 	switch sc.Action {
 	case "stop":
 		disable := sc.ActionModifier == "disable"
 		flags := &wrappers.StopServicesFlags{
 			Disable: disable,
 		}
-		if err := wrappers.StopServices(services, flags, snap.StopReasonOther, meter, perfTimings); err != nil {
+		st.Unlock()
+		err := wrappers.StopServices(services, flags, snap.StopReasonOther, meter, perfTimings)
+		st.Lock()
+		if err != nil {
 			return err
 		}
 		if disable {
@@ -116,7 +120,10 @@ func (m *ServiceManager) doServiceControl(t *state.Task, _ *tomb.Tomb) error {
 		flags := &wrappers.StartServicesFlags{
 			Enable: enable,
 		}
-		if err := wrappers.StartServices(startupOrdered, nil, flags, meter, perfTimings); err != nil {
+		st.Unlock()
+		err = wrappers.StartServices(startupOrdered, nil, flags, meter, perfTimings)
+		st.Lock()
+		if err != nil {
 			return err
 		}
 		if enable {
@@ -129,10 +136,16 @@ func (m *ServiceManager) doServiceControl(t *state.Task, _ *tomb.Tomb) error {
 			}
 		}
 	case "restart":
-		return wrappers.RestartServices(services, nil, meter, perfTimings)
+		st.Unlock()
+		err := wrappers.RestartServices(services, nil, meter, perfTimings)
+		st.Lock()
+		return err
 	case "reload-or-restart":
 		flags := &wrappers.RestartServicesFlags{Reload: true}
-		return wrappers.RestartServices(services, flags, meter, perfTimings)
+		st.Unlock()
+		err := wrappers.RestartServices(services, flags, meter, perfTimings)
+		st.Lock()
+		return err
 	default:
 		return fmt.Errorf("unhandled service action: %q", sc.Action)
 	}
