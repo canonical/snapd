a6469f81bb99a4899679d0647fd130a0eb81f154
Samuele Pedroni <pedronis@lucediurna.net>
2020-12-04 12:10:38 +0100

    gadget: add a test about hybrid 18->20 ready setups

--- gadget/export_test.go
+++ gadget/export_test.go
@@ -36,6 +36,7 @@ var (
 	ValidateRole            = validateRole
 	ValidateVolume          = validateVolume
 
+	RuleValidateVolumes         = ruleValidateVolumes
 	RuleValidateVolumeStructure = ruleValidateVolumeStructure
 	EnsureVolumeRuleConsistency = ensureVolumeRuleConsistency
 
--- gadget/validate_test.go
+++ gadget/validate_test.go
@@ -157,6 +157,57 @@ func (s *validateGadgetTestSuite) TestEnsureVolumeRuleConsistency(c *C) {
 	c.Assert(err, ErrorMatches, "system-save requires system-seed and system-data structures")
 }
 
+func (s *validateGadgetTestSuite) TestRuleValidateHybridGadget(c *C) {
+	// this is the kind of volumes setup recommended to be
+	// prepared for a possible UC18 -> UC20 transition
+	hybridyGadgetYaml := []byte(`volumes:
+  hybrid:
+    bootloader: grub
+    structure:
+      - name: mbr
+        type: mbr
+        size: 440
+        content:
+          - image: pc-boot.img
+      - name: BIOS Boot
+        type: DA,21686148-6449-6E6F-744E-656564454649
+        size: 1M
+        offset: 1M
+        offset-write: mbr+92
+        content:
+          - image: pc-core.img
+      - name: EFI System
+        type: EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+        filesystem: vfat
+        filesystem-label: system-boot
+        size: 1200M
+        content:
+          - source: grubx64.efi
+            target: EFI/boot/grubx64.efi
+          - source: shim.efi.signed
+            target: EFI/boot/bootx64.efi
+          - source: mmx64.efi
+            target: EFI/boot/mmx64.efi
+          - source: grub.cfg
+            target: EFI/ubuntu/grub.cfg
+      - name: Ubuntu Boot
+        type: 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+        filesystem: ext4
+        filesystem-label: ubuntu-boot
+        size: 750M
+`)
+
+	constraints := &modelConstraints{
+		classic: false,
+	}
+	giMeta, err := gadget.InfoFromGadgetYaml(hybridyGadgetYaml, constraints)
+	c.Assert(err, IsNil)
+
+	// XXX this should become a (new) Validate test
+	err = gadget.RuleValidateVolumes(giMeta.Volumes, constraints)
+	c.Check(err, IsNil)
+}
+
 func (s *validateGadgetTestSuite) TestValidateMissingRawContent(c *C) {
 	var gadgetYamlContent = `
 volumes:
