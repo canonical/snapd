dc4a7dc3cea8d325f2407136498d58bdfdfe8626
Samuele Pedroni <pedronis@lucediurna.net>
2020-12-06 23:31:30 +0100

    daemon: start splitting snaps op tests out of api_test.go
    this splits /v2/snaps GET and POST tests out of api_test.go
    to api_snaps_test.go and api_sideload_n_try_test.go
    
    api.go itself isn't split yet similarly but to clean test writing
    two changes were made
    
    * introduction of snapInstruction.dispatchForMany used in postSnaps
      as well
    * changed trySnap signature
    
    some imports in api_snaps_test.go are commented out atm,
    they were copied from api_test.go and most of them will likely
    come back soon

--- daemon/export_test.go
+++ daemon/export_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/snapcore/snapd/overlord"
 	"github.com/snapcore/snapd/overlord/snapstate"
 	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/snap"
 )
 
 var MinLane = minLane
@@ -92,11 +93,67 @@ func MockShutdownTimeout(tm time.Duration) (restore func()) {
 	}
 }
 
+func MockUnsafeReadSnapInfo(mock func(string) (*snap.Info, error)) (restore func()) {
+	oldUnsafeReadSnapInfo := unsafeReadSnapInfo
+	unsafeReadSnapInfo = mock
+	return func() {
+		unsafeReadSnapInfo = oldUnsafeReadSnapInfo
+	}
+}
+
+func MockAssertstateRefreshSnapDeclarations(mock func(*state.State, int) error) (restore func()) {
+	oldAssertstateRefreshSnapDeclarations := assertstateRefreshSnapDeclarations
+	assertstateRefreshSnapDeclarations = mock
+	return func() {
+		assertstateRefreshSnapDeclarations = oldAssertstateRefreshSnapDeclarations
+	}
+}
+
 func MockSnapstateInstall(mock func(context.Context, *state.State, string, *snapstate.RevisionOptions, int, snapstate.Flags) (*state.TaskSet, error)) (restore func()) {
 	oldSnapstateInstall := snapstateInstall
 	snapstateInstall = mock
 	return func() {
 		snapstateInstall = oldSnapstateInstall
+	}
+}
+
+func MockSnapstateInstallPath(mock func(*state.State, *snap.SideInfo, string, string, string, snapstate.Flags) (*state.TaskSet, *snap.Info, error)) (restore func()) {
+	oldSnapstateInstallPath := snapstateInstallPath
+	snapstateInstallPath = mock
+	return func() {
+		snapstateInstallPath = oldSnapstateInstallPath
+	}
+}
+
+func MockSnapstateTryPath(mock func(*state.State, string, string, snapstate.Flags) (*state.TaskSet, error)) (restore func()) {
+	oldSnapstateTryPath := snapstateTryPath
+	snapstateTryPath = mock
+	return func() {
+		snapstateTryPath = oldSnapstateTryPath
+	}
+}
+
+func MockSnapstateInstallMany(mock func(*state.State, []string, int) ([]string, []*state.TaskSet, error)) (restore func()) {
+	oldSnapstateInstallMany := snapstateInstallMany
+	snapstateInstallMany = mock
+	return func() {
+		snapstateInstallMany = oldSnapstateInstallMany
+	}
+}
+
+func MockSnapstateUpdateMany(mock func(context.Context, *state.State, []string, int, *snapstate.Flags) ([]string, []*state.TaskSet, error)) (restore func()) {
+	oldSnapstateUpdateMany := snapstateUpdateMany
+	snapstateUpdateMany = mock
+	return func() {
+		snapstateUpdateMany = oldSnapstateUpdateMany
+	}
+}
+
+func MockSnapstateRemoveMany(mock func(*state.State, []string) ([]string, []*state.TaskSet, error)) (restore func()) {
+	oldSnapstateRemoveMany := snapstateRemoveMany
+	snapstateRemoveMany = mock
+	return func() {
+		snapstateRemoveMany = oldSnapstateRemoveMany
 	}
 }
 
@@ -108,4 +165,8 @@ type (
 
 func (inst *snapInstruction) Dispatch() snapActionFunc {
 	return inst.dispatch()
+}
+
+func (inst *snapInstruction) DispatchForMany() snapsActionFunc {
+	return inst.dispatchForMany()
 }
