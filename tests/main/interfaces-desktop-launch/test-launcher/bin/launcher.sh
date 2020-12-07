a64a652a9b16d8d2a3b51ecc6156e819e5b11a2b
Alan Griffiths <alan@octopull.co.uk>
2020-12-07 12:12:45 +0000

    Don't try to pass environment in interfaces-desktop-launch

--- tests/main/interfaces-desktop-launch/test-launcher/bin/launcher.sh
+++ tests/main/interfaces-desktop-launch/test-launcher/bin/launcher.sh
@@ -3,4 +3,4 @@
 exec dbus-send --session --print-reply \
     --dest=io.snapcraft.Launcher /io/snapcraft/Launcher \
     io.snapcraft.PrivilegedDesktopLauncher.OpenDesktopEntry \
-    string:"$1" array:string:"XDG_CURRENT_DESKTOP=spread-test"
+    string:"$1"
