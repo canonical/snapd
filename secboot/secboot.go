99ab3ba0f3b507ea3b3052a8089e6e665c3ff79e
Michael Vogt <mvo@ubuntu.com>
2021-01-07 20:27:32 +0100

    {,sec}boot: pass "key-name" to the FDE hooks
    This is a followup to PR#9741 - we got asked that the FDE hooks
    contains the key name. One of the existing hooks is using the
    order in which the hooks are run to infere this informaton.
    
    This commit adds the key-name to the key.

--- secboot/secboot.go
+++ secboot/secboot.go
@@ -56,7 +56,10 @@ func NewLoadChain(bf bootloader.BootFile, next ...*LoadChain) *LoadChain {
 type SealKeyRequest struct {
 	// The key to seal
 	Key EncryptionKey
-	// The path to store the sealed key file
+	// The key name, idential keys should have idential names
+	KeyName string
+	// The path to store the sealed key file. The same Key/KeyName
+	// can be stored under multiple KeyFile names for safety.
 	KeyFile string
 }
 
