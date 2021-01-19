9d55ee8fac8bb2a698fe4747e09742059ec97b8a
Ian Johnson <ian.johnson@canonical.com>
2021-01-19 14:38:33 -0600

    gadget: pass sector size in to mkfs family of functions, use to select block sz
    If we are creating partitions that are small such as ubuntu-save, then we have
    code to specify the block size, but on some devices where the sector size is
    not the default of 512 (and is instead 4K for example), we need to adjust the
    block size when creating the filesystem to something that is at least the same
    as the sector size. This is confirmed looking at the source of e2fsprogs at
    https://github.com/tytso/e2fsprogs/blob/master/misc/mke2fs.c#L2151-L2156.
    
    As such, pass the sector size along to where we call mkfs, and adjust the
    specified block size to be the same as the sector size if the sector size is
    larger than 1K when creating ext4 and vfat partitions.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- gadget/internal/mkfs.go
+++ gadget/internal/mkfs.go
@@ -30,7 +30,7 @@ import (
 	"github.com/snapcore/snapd/osutil"
 )
 
-type MkfsFunc func(imgFile, label, contentsRootDir string, deviceSize quantity.Size) error
+type MkfsFunc func(imgFile, label, contentsRootDir string, deviceSize, sectorSize quantity.Size) error
 
 var (
 	mkfsHandlers = map[string]MkfsFunc{
@@ -40,42 +40,52 @@ var (
 )
 
 // Mkfs creates a filesystem of given type and provided label in the device or
-// file. The device size provides hints for additional tuning of the created
-// filesystem.
-func Mkfs(typ, img, label string, deviceSize quantity.Size) error {
-	return MkfsWithContent(typ, img, label, "", deviceSize)
+// file. The device size and sector size provides hints for additional tuning of
+// the created filesystem.
+func Mkfs(typ, img, label string, deviceSize, sectorSize quantity.Size) error {
+	return MkfsWithContent(typ, img, label, "", deviceSize, sectorSize)
 }
 
-// Mkfs creates a filesystem of given type and provided label in the device or
-// file. The filesystem is populated with contents of contentRootDir. The device
-// size provides hints for additional tuning of the created filesystem.
-func MkfsWithContent(typ, img, label, contentRootDir string, deviceSize quantity.Size) error {
+// MkfsWithContent creates a filesystem of given type and provided label in the
+// device or file. The filesystem is populated with contents of contentRootDir.
+// The device size provides hints for additional tuning of the created
+// filesystem.
+func MkfsWithContent(typ, img, label, contentRootDir string, deviceSize, sectorSize quantity.Size) error {
 	h, ok := mkfsHandlers[typ]
 	if !ok {
 		return fmt.Errorf("cannot create unsupported filesystem %q", typ)
 	}
-	return h(img, label, contentRootDir, deviceSize)
+	return h(img, label, contentRootDir, deviceSize, sectorSize)
 }
 
 // mkfsExt4 creates an EXT4 filesystem in given image file, with an optional
 // filesystem label, and populates it with the contents of provided root
 // directory.
-func mkfsExt4(img, label, contentsRootDir string, deviceSize quantity.Size) error {
+func mkfsExt4(img, label, contentsRootDir string, deviceSize, sectorSize quantity.Size) error {
 	// Originally taken from ubuntu-image
 	// Switched to use mkfs defaults for https://bugs.launchpad.net/snappy/+bug/1878374
 	// For caveats/requirements in case we need support for older systems:
 	// https://github.com/snapcore/snapd/pull/6997#discussion_r293967140
 	mkfsArgs := []string{"mkfs.ext4"}
+
 	const size32MiB = 32 * quantity.SizeMiB
 	if deviceSize != 0 && deviceSize <= size32MiB {
-		// With the default of 4096 bytes, the minimal journal size is
-		// 4M, meaning we loose a lot of usable space. Try to follow the
+		// With the default block size of 4096 bytes, the minimal journal size
+		// is 4M, meaning we loose a lot of usable space. Try to follow the
 		// e2fsprogs upstream and use a 1k block size for smaller
 		// filesystems, note that this may cause issues like
 		// https://bugs.launchpad.net/ubuntu/+source/lvm2/+bug/1817097
 		// if one migrates the filesystem to a device with a different
 		// block size
-		mkfsArgs = append(mkfsArgs, "-b", "1024")
+
+		// though note if the sector size was specified (i.e. non-zero) and
+		// larger than 1K, then we need to use that, since you can't create
+		// a filesystem with a block-size smaller than the sector-size
+		defaultSectorSize := 1 * quantity.SizeKiB
+		if sectorSize > 1024 {
+			defaultSectorSize = sectorSize
+		}
+		mkfsArgs = append(mkfsArgs, "-b", defaultSectorSize.String())
 	}
 	if contentsRootDir != "" {
 		// mkfs.ext4 can populate the filesystem with contents of given
@@ -106,11 +116,19 @@ func mkfsExt4(img, label, contentsRootDir string, deviceSize quantity.Size) erro
 // mkfsVfat creates a VFAT filesystem in given image file, with an optional
 // filesystem label, and populates it with the contents of provided root
 // directory.
-func mkfsVfat(img, label, contentsRootDir string, deviceSize quantity.Size) error {
+func mkfsVfat(img, label, contentsRootDir string, deviceSize, sectorSize quantity.Size) error {
 	// taken from ubuntu-image
+	// TODO: handle sector sizes other than 512, can't create a block size that
+	// is less than the sector size
+
+	// 512B logical sector size by default, unless the specified sector size is
+	// larger than 512, in which case use the sector size
+	defaultSectorSize := quantity.Size(512)
+	if sectorSize > defaultSectorSize {
+		defaultSectorSize = sectorSize
+	}
 	mkfsArgs := []string{
-		// 512B logical sector size
-		"-S", "512",
+		"-S", defaultSectorSize.String(),
 		// 1 sector per cluster
 		"-s", "1",
 		// 32b FAT size
