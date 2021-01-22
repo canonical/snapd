4de79c87e065383aa5251177b821b9fb31dc6913
Michael Vogt <mvo@ubuntu.com>
2021-01-22 12:33:09 +0100

    gadget,overlord: pass kernelRoot to install.Run()
    This commit adds kernelRoot to install.Run() so that kernel
    references can be resolved during install. No further functionality
    is changed this is the first trivial split out of #9780 to make
    the review easier.

--- gadget/install/install_test.go
+++ gadget/install/install_test.go
@@ -55,7 +55,7 @@ func (s *installSuite) SetUpTest(c *C) {
 }
 
 func (s *installSuite) TestInstallRunError(c *C) {
-	sys, err := install.Run(nil, "", "", install.Options{}, nil)
+	sys, err := install.Run(nil, "", "", "", install.Options{}, nil)
 	c.Assert(err, ErrorMatches, "cannot use empty gadget root directory")
 	c.Check(sys, IsNil)
 }
