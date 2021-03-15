14e9dd2c350ff678710ad765c38cd308d044714c
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-03-15 08:39:40 -0300

    Restore the check for invariant as part of the prepare tests section
    New the invariante clean up and check is done as part of the prepare and
    restore of each test

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -613,6 +613,12 @@ prepare_suite_each() {
             ausearch -i -m AVC --checkpoint "$RUNTIME_STATE_PATH/audit-stamp" || true
             ;;
     esac
+
+    # Check for invariants late, in order to detect any bugs in the code above.
+    if [[ "$variant" = full ]]; then
+        "$TESTSTOOLS"/cleanup-state pre-invariant
+    fi
+    tests.invariant check
 }
 
 restore_suite_each() {
