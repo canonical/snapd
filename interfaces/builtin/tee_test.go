6cdf8c1c5c1e8477616fe3231d19b3e7bada0513
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2021-03-17 11:34:10 +0100

    interfaces/builtin: update tee unit tests to not hardcode libexec dir
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- interfaces/builtin/tee_test.go
+++ interfaces/builtin/tee_test.go
@@ -20,8 +20,11 @@
 package builtin_test
 
 import (
+	"fmt"
+
 	. "gopkg.in/check.v1"
 
+	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/interfaces/apparmor"
 	"github.com/snapcore/snapd/interfaces/builtin"
@@ -88,7 +91,8 @@ func (s *TeeInterfaceSuite) TestUDevSpec(c *C) {
 KERNEL=="tee[0-9]*", TAG+="snap_consumer_app"`)
 	c.Assert(spec.Snippets(), testutil.Contains, `# tee
 KERNEL=="teepriv[0-9]*", TAG+="snap_consumer_app"`)
-	c.Assert(spec.Snippets(), testutil.Contains, `TAG=="snap_consumer_app", RUN+="/usr/lib/snapd/snap-device-helper $env{ACTION} snap_consumer_app $devpath $major:$minor"`)
+	c.Assert(spec.Snippets(), testutil.Contains,
+		fmt.Sprintf(`TAG=="snap_consumer_app", RUN+="%v/snap-device-helper $env{ACTION} snap_consumer_app $devpath $major:$minor"`, dirs.DistroLibExecDir))
 }
 
 func (s *TeeInterfaceSuite) TestStaticInfo(c *C) {
