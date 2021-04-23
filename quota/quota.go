3f0b2221df30a6dad4584926be206fb5c97823f0
Ian Johnson <ian.johnson@canonical.com>
2021-04-23 16:45:35 -0500

    quota: add new quota package
    With some helpers to get related quotas from a single quota reference and to
    initialize the references from a deserialized set of quotas.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ quota/quota.go
@@ -0,0 +1,315 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2021 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package quota
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+
+	"github.com/snapcore/snapd/gadget/quantity"
+)
+
+// Group is a quota group of snaps, services or sub-groups that are all subject
+// to specific resource quotas. The only quota resource types currently
+// supported is memory, but this can be expanded in the future.
+type Group struct {
+	// Name is the name of the quota group. Certain names are reserved and have
+	// special meaning such as user.* or system.* or snapd.*, but are otherwise
+	// generically choosable by a user. This name will correspond to the name of
+	// the systemd slice underlying the quota group.
+	Name string `json:"name,omitempty"`
+
+	// SubGroups is the set of sub-groups that are subject to this quota.
+	// Sub-groups have their own limits, subject to the requirement that the
+	// highest quota for a sub-group is that of the parent group.
+	SubGroups []string `json:"sub-groups,omitempty"`
+
+	// subGroups is the set of actual sub-group objects, needed for tracking and
+	// calculations
+	subGroups []*Group
+
+	// MemoryLimit is the limit of memory available to the processes in the
+	// group where if the total used memory of all the processes exceeds the
+	// limit, oom-killer is invoked which will start killing processes. The
+	// specific behavior of which processes are killed is subject to the
+	// ExhaustionBehavior.
+	MemoryLimit quantity.Size `json:"memory-limit,omitempty"`
+
+	// MemoryExhaustionBehavior is the behavior that is implemented to decide
+	// what processes to kill in the quota group when the memory limits are
+	// exhausted.
+	MemoryExhaustionBehavior MemoryExhaustionBehavior `json:"memory-exhaustion-behavior,omitempty"`
+
+	// ParentGroup is the the parent group that this group is a child of. It is
+	// here mainly for coding convenience to easily construct the full chain
+	// back to a root group to generate the full slice unit name of the quota
+	// group. If it is nil, then this is a "root" quota group.
+	ParentGroup string `json:"parent-group,omitempty"`
+
+	// parentGroup is the actual parent group object, needed for tracking and
+	// calculations
+	parentGroup *Group
+
+	// Snaps is the set of snaps that is part of this quota group. If this is
+	// empty then the underlying slice may not exist on the system.
+	Snaps []string `json:"snaps,omitempty"`
+}
+
+// MemoryExhaustionBehavior is the behavior determining what the system should
+// do when memory available to a quota group is exhausted.
+type MemoryExhaustionBehavior int
+
+const (
+	// DefaultOOMKiller is the default behavior to invoke to choose which
+	// processes when the memory quota resource limit is reached, and it
+	// consists of letting the Linux kernel's oom-killer run and decide, which
+	// is available on all cgroups version systems, but is not deterministic as
+	// to which processes are killed.
+	DefaultOOMKiller MemoryExhaustionBehavior = iota
+)
+
+// SliceFileName returns the name of the slice file that should be used for this
+// quota group. This name will include all of the group's parents in the name.
+// For example, a group named "bar" that is a child of the "foo" group will have
+// a systemd slice name as "foo-bar.slice". Note that the slice name may differ
+// from the snapd friendly group name, mainly in the case that the group is a
+// sub group.
+func (grp *Group) SliceFileName() string {
+	if grp.ParentGroup == "" {
+		// root group name, then the slice unit is just "<name>.slice"
+		return fmt.Sprintf("%s.slice", grp.Name)
+	}
+
+	// otherwise we need to track back to get all of the parent elements
+	grpNames := []string{}
+	parentGrp := grp.parentGroup
+	for parentGrp != nil {
+		grpNames = append([]string{parentGrp.Name}, grpNames...)
+		parentGrp = parentGrp.parentGroup
+	}
+
+	buf := &bytes.Buffer{}
+	for _, parentGrpName := range grpNames {
+		fmt.Fprintf(buf, "%s-", parentGrpName)
+	}
+	fmt.Fprintf(buf, "%s.slice", grp.Name)
+	return buf.String()
+}
+
+// allow only alphanumerics at least one character long, and not starting with
+// a number
+var validGroupNameRegexp = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9]*$`)
+
+func (grp *Group) validate() error {
+	// check that the name is a simple alphanumeric name
+	if !validGroupNameRegexp.MatchString(grp.Name) {
+		return fmt.Errorf("group name %q contains invalid characters (valid names are alphanumeric starting with a letter)", grp.Name)
+	}
+
+	// check if the name is reserved for future usage
+	switch grp.Name {
+	case "root", "system", "snapd", "user":
+		return fmt.Errorf("group name %q reserved", grp.Name)
+	}
+
+	if grp.MemoryLimit == 0 {
+		return fmt.Errorf("group memory limit must be non-zero")
+	}
+
+	// TODO: probably there is a minimum amount of bytes here that is
+	// technically usable/enforcable, should we check that too?
+
+	// check that if this is a sub-group, then the parent group has enough space
+	// to accommodate this new group (we assume that other existing sub-groups
+	// in the parent group have already been validated)
+	if grp.parentGroup != nil {
+		totalAvailable := quantity.Size(0)
+		for _, child := range grp.parentGroup.subGroups {
+			totalAvailable += child.MemoryLimit
+		}
+		if totalAvailable+grp.MemoryLimit > grp.parentGroup.MemoryLimit {
+			remaining := grp.parentGroup.MemoryLimit - totalAvailable
+			return fmt.Errorf("sub-group memory limit of %s is too large to fit inside remaining quota space %s for parent group %s", grp.MemoryLimit.IECString(), remaining.IECString(), grp.parentGroup.Name)
+		}
+	}
+
+	return nil
+}
+
+// NewSubGroup creates a new sub group under the current group
+func (grp *Group) NewSubGroup(name string, memLimit quantity.Size) (*Group, error) {
+	subGrp := &Group{
+		Name:        name,
+		MemoryLimit: memLimit,
+		ParentGroup: grp.Name,
+		parentGroup: grp,
+	}
+
+	if err := subGrp.validate(); err != nil {
+		return nil, err
+	}
+
+	// also double check that the sub group name is not the same as that of the
+	// parent, this is fine in systemd world, but in snapd we want unique quota
+	// groups
+	if name == grp.Name {
+		return nil, fmt.Errorf("cannot use same name %q for sub group as parent group", name)
+	}
+
+	// save the details of this new sub-group in the parent group
+	grp.subGroups = append(grp.subGroups, subGrp)
+	grp.SubGroups = append(grp.SubGroups, name)
+
+	return subGrp, nil
+}
+
+func NewGroup(name string, memLimit quantity.Size) (*Group, error) {
+	grp := &Group{
+		Name:        name,
+		MemoryLimit: memLimit,
+	}
+
+	if err := grp.validate(); err != nil {
+		return nil, err
+	}
+
+	return grp, nil
+}
+
+// CompleteCrossReferences takes a set of deserialized groups and sets all
+// cross references amongst them using the unexported fields which are not
+// serialized.
+func CompleteCrossReferences(grps map[string]*Group) error {
+	// iterate over all groups, looking for sub-groups which need to be threaded
+	// together with their respective parent groups from the set
+	for _, grp := range grps {
+		// first thread the parent link
+		if grp.ParentGroup != "" {
+			parent, ok := grps[grp.ParentGroup]
+			if !ok {
+				return fmt.Errorf("internal error: missing group %q referenced as the parent of group %q", grp.ParentGroup, grp.Name)
+			}
+			grp.parentGroup = parent
+		}
+
+		// now thread any child links from this group to any children
+		if len(grp.SubGroups) != 0 {
+			grp.subGroups = make([]*Group, len(grp.SubGroups))
+			for i, subName := range grp.SubGroups {
+				sub, ok := grps[subName]
+				if !ok {
+					return fmt.Errorf("internal error: missing group %q referenced as the child of group %q", subName, grp.Name)
+				}
+				grp.subGroups[i] = sub
+			}
+		}
+	}
+
+	return nil
+}
+
+// subTree recursively returns all of the sub-groups of the group
+func (grp *Group) subTree() []*Group {
+	subTreeList := grp.subGroups
+	for _, sub := range grp.subGroups {
+		subTreeList = append(subTreeList, sub.subTree()...)
+	}
+
+	return subTreeList
+}
+
+func deleteGrpFromList(grps []*Group, grpToDelete *Group) []*Group {
+	grpCp := make([]*Group, 0, len(grps))
+	for _, grp := range grps {
+		if grp != grpToDelete {
+			grpCp = append(grpCp, grp)
+		}
+	}
+	return grpCp
+}
+
+type QuotaGroupSet struct {
+	grps map[*Group]bool
+}
+
+// AddAllNecessaryGroups adds all groups that are required for the specified
+// group to be effective to the set. This means all sub-groups of this group,
+// all parent groups of this group, and all sub-trees of any parent groups. This
+// set is the set of quota groups that must exist for this quota group to be
+// fully realized on a system, since all sub-branches of the full tree must
+// exist since this group may share some quota resources with the other
+// branches.
+func (s *QuotaGroupSet) AddAllNecessaryGroups(grp *Group) {
+	if s.grps == nil {
+		s.grps = make(map[*Group]bool)
+	}
+
+	// add this group first directly
+	s.grps[grp] = true
+
+	// first get all sub-trees of this group
+	for _, child := range grp.subGroups {
+		// add the direct sub-group
+		s.grps[child] = true
+		// add any indirect sub-sub-groups
+		for _, subchild := range child.subTree() {
+			s.grps[subchild] = true
+		}
+	}
+
+	// now get all parents, and for each parent get the other sub-tree that
+	// excludes this branch of the tree to not duplicate anything and not
+	// traverse the same branch again
+	// we need all the other branches in the tree in order to
+	// properly enforce limits since other branches will share total resources
+	// with this branch
+	parentGrp := grp.parentGroup
+	currentBranch := grp
+	for parentGrp != nil {
+		// don't include the current branch in the sub-tree of our current
+		// parent, since the parent will have a link to us here
+		siblings := deleteGrpFromList(parentGrp.subGroups, currentBranch)
+
+		// get all the sub-trees for the siblings
+		for _, sibling := range siblings {
+			// add the direct sibling
+			s.grps[sibling] = true
+
+			// add any children of sibling branches
+			for _, indirectSiblingChild := range sibling.subTree() {
+				s.grps[indirectSiblingChild] = true
+			}
+		}
+
+		// move up a level
+		currentBranch = grp
+		parentGrp = parentGrp.parentGroup
+	}
+}
+
+func (s *QuotaGroupSet) AllQuotaGroups() []*Group {
+	grps := make([]*Group, 0, len(s.grps))
+	for grp := range s.grps {
+		grps = append(grps, grp)
+	}
+
+	return grps
+}
---
+++ quota/quota_test.go
@@ -0,0 +1,215 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2021 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package quota_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/snapcore/snapd/gadget/quantity"
+	"github.com/snapcore/snapd/quota"
+	. "gopkg.in/check.v1"
+)
+
+// Hook up check.v1 into the "go test" runner
+func Test(t *testing.T) { TestingT(t) }
+
+type quotaTestSuite struct{}
+
+var _ = Suite(&quotaTestSuite{})
+
+func (ts *quotaTestSuite) TestNewGroup(c *C) {
+
+	tt := []struct {
+		name    string
+		limit   quantity.Size
+		err     string
+		comment string
+	}{
+		{
+			name:    "group1",
+			limit:   quantity.SizeMiB,
+			comment: "basic happy",
+		},
+		{
+			name:    "biglimit",
+			limit:   quantity.Size(math.MaxUint64),
+			comment: "huge limit happy",
+		},
+		{
+			name:    "zero",
+			limit:   0,
+			err:     `group memory limit must be non-zero`,
+			comment: "group with zero memory limit",
+		},
+		{
+			name:    "group1-unsupported chars",
+			limit:   quantity.SizeMiB,
+			err:     `group name "group1-unsupported chars" contains invalid characters.*`,
+			comment: "unsupported characters in group name",
+		},
+		{
+			name:    "1group",
+			limit:   quantity.SizeMiB,
+			err:     `group name "1group" contains invalid characters.*`,
+			comment: "size negative",
+		},
+		{
+			name:    "root",
+			limit:   quantity.SizeMiB,
+			err:     `group name "root" reserved`,
+			comment: "reserved root name",
+		},
+		{
+			name:    "snapd",
+			limit:   quantity.SizeMiB,
+			err:     `group name "snapd" reserved`,
+			comment: "reserved snapd name",
+		},
+		{
+			name:    "system",
+			limit:   quantity.SizeMiB,
+			err:     `group name "system" reserved`,
+			comment: "reserved system name",
+		},
+		{
+			name:    "user",
+			limit:   quantity.SizeMiB,
+			err:     `group name "user" reserved`,
+			comment: "reserved user name",
+		},
+	}
+
+	for _, t := range tt {
+		comment := Commentf(t.comment)
+		grp, err := quota.NewGroup(t.name, t.limit)
+		if t.err != "" {
+			c.Assert(err, ErrorMatches, t.err, comment)
+			continue
+		}
+		c.Assert(err, IsNil, comment)
+
+		c.Assert(grp.SliceFileName(), Equals, t.name+".slice", comment)
+	}
+}
+
+func (ts *quotaTestSuite) TestSimpleSubGroupVerification(c *C) {
+	tt := []struct {
+		rootlimit quantity.Size
+		subname   string
+		sublimit  quantity.Size
+		err       string
+		comment   string
+	}{
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "sub",
+			sublimit:  quantity.SizeMiB,
+			comment:   "basic sub group with same quota as parent happy",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "sub",
+			sublimit:  quantity.SizeMiB / 2,
+			comment:   "basic sub group with smaller quota than parent happy",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "sub",
+			sublimit:  quantity.SizeMiB * 2,
+			err:       "sub-group memory limit of 2 MiB is too large to fit inside remaining quota space 1 MiB for parent group myroot",
+			comment:   "sub group with larger quota than parent unhappy",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "sub-invalid-chars",
+			sublimit:  quantity.SizeMiB,
+			err:       `group name "sub-invalid-chars" contains invalid characters.*`,
+			comment:   "sub group with invalid name",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "myroot",
+			sublimit:  quantity.SizeMiB,
+			err:       `cannot use same name "myroot" for sub group as parent group`,
+			comment:   "sub group with same name as parent group",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "snapd",
+			sublimit:  quantity.SizeMiB,
+			err:       `group name "snapd" reserved`,
+			comment:   "sub group with reserved name",
+		},
+		{
+			rootlimit: quantity.SizeMiB,
+			subname:   "zero",
+			sublimit:  0,
+			err:       `group memory limit must be non-zero`,
+			comment:   "sub group with zero memory limit",
+		},
+	}
+
+	for _, t := range tt {
+		comment := Commentf(t.comment)
+		// make a root group
+		rootGrp, err := quota.NewGroup("myroot", t.rootlimit)
+		c.Assert(err, IsNil, comment)
+
+		// make a sub-group under the root group
+		subGrp, err := rootGrp.NewSubGroup(t.subname, t.sublimit)
+		if t.err != "" {
+			c.Assert(err, ErrorMatches, t.err, comment)
+			continue
+		}
+		c.Assert(err, IsNil, comment)
+
+		c.Assert(subGrp.SliceFileName(), Equals, "myroot-"+t.subname+".slice")
+	}
+}
+
+func (ts *quotaTestSuite) TestComplexSubGroups(c *C) {
+	rootGrp, err := quota.NewGroup("myroot", quantity.SizeMiB)
+	c.Assert(err, IsNil)
+
+	// try adding 2 sub-groups with total quota split exactly equally
+	sub1, err := rootGrp.NewSubGroup("sub1", quantity.SizeMiB/2)
+	c.Assert(err, IsNil)
+	c.Assert(sub1.SliceFileName(), Equals, "myroot-sub1.slice")
+
+	sub2, err := rootGrp.NewSubGroup("sub2", quantity.SizeMiB/2)
+	c.Assert(err, IsNil)
+	c.Assert(sub2.SliceFileName(), Equals, "myroot-sub2.slice")
+
+	// adding another sub-group to this group fails
+	_, err = rootGrp.NewSubGroup("sub3", 1)
+	c.Assert(err, ErrorMatches, "sub-group memory limit of 1 B is too large to fit inside remaining quota space 0 B for parent group myroot")
+
+	// we can however add a sub-group to one of the sub-groups with the exact
+	// size of the parent sub-group
+	subsub1, err := sub1.NewSubGroup("subsub1", quantity.SizeMiB/2)
+	c.Assert(err, IsNil)
+	c.Assert(subsub1.SliceFileName(), Equals, "myroot-sub1-subsub1.slice")
+
+	// and we can even add a smaller sub-sub-sub-group to the sub-group
+	subsubsub1, err := subsub1.NewSubGroup("subsubsub1", quantity.SizeMiB/4)
+	c.Assert(err, IsNil)
+	c.Assert(subsubsub1.SliceFileName(), Equals, "myroot-sub1-subsub1-subsubsub1.slice")
+}
