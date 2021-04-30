51b2f7250750113689cd360d8fd7f5f620b2ab1b
Ian Johnson <ian.johnson@canonical.com>
2021-04-29 21:00:15 -0500

    o/servicestate/servicemgr_test.go: add logging of all systemctl calls on fail
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- overlord/servicestate/servicemgr_test.go
+++ overlord/servicestate/servicemgr_test.go
@@ -224,7 +224,9 @@ var _ = Suite(&ensureSnapServiceSuite{})
 
 func (s *baseServiceMgrTestSuite) mockSystemctlCalls(c *C, expCalls []expectedSystemctl) (restore func()) {
 	systemctlCalls := 0
+	allSystemctlCalls := [][]string{}
 	r := systemd.MockSystemctl(func(args ...string) ([]byte, error) {
+		allSystemctlCalls = append(allSystemctlCalls, args)
 		if systemctlCalls < len(expCalls) {
 			res := expCalls[systemctlCalls]
 			c.Assert(args, DeepEquals, res.expArgs)
@@ -241,7 +243,7 @@ func (s *baseServiceMgrTestSuite) mockSystemctlCalls(c *C, expCalls []expectedSy
 		// double-check at the end of the test that we got as many systemctl calls
 		// as were mocked and that we didn't get less, then re-set it for the next
 		// test
-		c.Assert(systemctlCalls, Equals, len(expCalls))
+		c.Assert(systemctlCalls, Equals, len(expCalls), Commentf("got: %v", allSystemctlCalls))
 	}
 }
 
