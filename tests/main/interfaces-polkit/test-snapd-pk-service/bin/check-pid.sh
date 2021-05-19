da971c2a1af815ab73d7ac398251f223991c18d8
James Henstridge <james@jamesh.id.au>
2021-05-19 22:54:21 +0800

    tests: rewrite test script to use busctl

--- tests/main/interfaces-polkit/test-snapd-pk-service/bin/check-pid.sh
+++ tests/main/interfaces-polkit/test-snapd-pk-service/bin/check-pid.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/sh -e
 
 pid="$1"
 action_id="$2"
 
 uid="$(stat --format %u "/proc/$pid")"
-pid_start="$(sed 's/([^)]*)/x/' < "/proc/$pid/stat" | cut -d ' ' -f 22)"
+pid_start="$(sed 's/([^)]*)/program/' < "/proc/$pid/stat" | cut -d ' ' -f 22)"
 
-subject="('unix-process', {'pid': <uint32 ${pid}>, 'start-time': <uint64 ${pid_start}>, 'uid': <uint32 ${uid}>})"
-
-exec gdbus call --system \
-     --dest org.freedesktop.PolicyKit1 \
-     --object-path /org/freedesktop/PolicyKit1/Authority \
-     --method org.freedesktop.PolicyKit1.Authority.CheckAuthorization \
-     "$subject" "'$action_id'" "@a{ss} {}" 0 "''"
+exec busctl --system call \
+     org.freedesktop.PolicyKit1 /org/freedesktop/PolicyKit1/Authority \
+     org.freedesktop.PolicyKit1.Authority CheckAuthorization \
+     '(sa{sv})sa{ss}us' \
+     unix-process 3 pid u "$pid" start-time t "$pid_start" uid u "$uid" \
+     "$action_id" \
+     0 \
+     0 \
+     ""
