8859f706f854ebe027cc064a0ef65b3dc8683a52
Paweł Stołowski <stolowski@gmail.com>
2021-05-20 14:45:28 +0200

    Introduce minimalInstallInfo interface in snapstate.

--- overlord/snapstate/autorefresh.go
+++ overlord/snapstate/autorefresh.go
@@ -69,7 +69,7 @@ func (rc *refreshCandidate) Type() snap.Type {
 	return rc.SnapSetup.Type
 }
 
-func (rc *refreshCandidate) Base() string {
+func (rc *refreshCandidate) SnapBase() string {
 	return rc.SnapSetup.Base
 }
 
@@ -77,7 +77,7 @@ func (rc *refreshCandidate) MakeSnapSetup(*state.State, *SnapState) (*SnapSetup,
 	return &rc.SnapSetup, nil
 }
 
-func (rc *refreshCandidate) Size() int64 {
+func (rc *refreshCandidate) DownloadSize() int64 {
 	return rc.DownloadInfo.Size
 }
 
