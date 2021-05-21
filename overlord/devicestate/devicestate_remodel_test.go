07f9b239f7d85b88664b62c16d37d2f4e026b597
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2021-05-21 13:44:27 +0200

    overlord/devicestate: use exclusive change conflict helper for remodel changes
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/devicestate/devicestate_remodel_test.go
+++ overlord/devicestate/devicestate_remodel_test.go
@@ -919,11 +919,14 @@ func (s *deviceMgrRemodelSuite) TestReregRemodelClashAnyChange(c *C) {
 	}
 
 	// simulate any other change
-	s.state.NewChange("chg", "other change")
+	chg := s.state.NewChange("chg", "other change")
+	chg.SetStatus(state.DoingStatus)
 
 	_, err := devicestate.Remodel(s.state, new)
-	c.Check(err, DeepEquals, &snapstate.ChangeConflictError{
-		Message: "cannot start complete remodel, other changes are in progress",
+	c.Assert(err, NotNil)
+	c.Assert(err, DeepEquals, &snapstate.ChangeConflictError{
+		ChangeKind: "chg",
+		Message:    `other changes in progress, change "remodel" not allowed until they are done`,
 	})
 }
 
