c190fd2b05d1a152427816d2505de0662693780b
Samuele Pedroni <pedronis@lucediurna.net>
2021-06-10 13:48:22 +0200

    asserts: test how ExternalKeypairManager.loadAllKeys is implemented

--- asserts/extkeypairmgr_test.go
+++ asserts/extkeypairmgr_test.go
@@ -176,6 +176,12 @@ func (s *extKeypairMgrSuite) TestGet(c *C) {
 	c.Assert(err, IsNil)
 	c.Check(pk2.PublicKey().ID(), Equals, modelsID)
 
+	c.Check(s.pgm.Calls(), DeepEquals, [][]string{
+		{"keymgr", "key-names"},
+		{"keymgr", "get-public-key", "-f", "DER", "-k", "default"},
+		{"keymgr", "get-public-key", "-f", "DER", "-k", "models"},
+	})
+
 	_, err = kmgr.Get("unknown-id")
 	c.Check(err, ErrorMatches, `cannot find external key with id "unknown-id"`)
 }
