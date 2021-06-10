bdaa87780fbab7171fda9caa057a18f6f36266c4
Alberto Mardegan <alberto.mardegan@canonical.com>
2021-06-10 16:25:02 +0300

    osutil/user: mention the lack of --extrauser in the error

--- osutil/user_test.go
+++ osutil/user_test.go
@@ -565,7 +565,8 @@ func (s *ensureUserSuite) TestEnsureUserGroupFailedUseraddCoreNoExtra(c *check.C
 	defer mockGroupDel.Restore()
 
 	err := osutil.EnsureUserGroup("lakatos", 123456, true)
-	c.Assert(err, check.ErrorMatches, "useradd failed with: some error")
+	c.Assert(err, check.ErrorMatches, "groupdel does not support '--extrausers'"+
+		" option \\(after useradd failed with: some error\\)")
 
 	c.Check(mockGroupDel.Calls(), check.DeepEquals, [][]string{
 		{"groupdel", "--extrausers", "lakatos"},
