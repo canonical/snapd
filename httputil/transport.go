c8122fc15a096f53f4242d090b4a1aac33e2aa01
Pawel Stolowski <stolowski@gmail.com>
2021-07-13 11:21:01 +0200

    store: set ResponseHeaderTimeout on the default transport
    It appears that ResponseHeaderTimeout needs to be set explicitly, no headers response from the server doesn't seem to be covered by any other timeout.
    
    This should fix the download getting stuck issue.
    
    For more context, see the stacktrace obtained from a system where download got stuck: https://pastebin.canonical.com/p/nZkRMTBbv3/
    (the affected goroutine seems to be sitting inside for-select loop of /usr/lib/go-1.10/src/net/http/transport.go:2033)
    
    (to play with it comment out ResponseHeaderTimeout: ... from transport.go and modify test timeout from 5 * time.Second to a huge value and it should be stuck)
    
    * Set ResponseHeaderTimeout on the default transport.
    
    * Use 250ms for mocked ResponseHeaderTimeout.
    
    * Bump ResponseHeaderTimeout to 15s.
    
    * Bump test timeout to avoid potential issues with LP builds.

--- httputil/transport.go
+++ httputil/transport.go
@@ -25,9 +25,11 @@ import (
 )
 
 var origDefaultTransport *http.Transport = http.DefaultTransport.(*http.Transport)
+var responseHeaderTimeout = 15 * time.Second
 
 // newDefaultTransport makes a fresh modifiable instance of Transport
-// with the same parameters as http.DefaultTransport.
+// with the same parameters as http.DefaultTransport but we also set
+// ResponseHeaderTimeout which is not set by default.
 func newDefaultTransport() *http.Transport {
 	// based on https://github.com/golang/go/blob/release-branch.go1.7/src/net/http/transport.go#L38
 	return &http.Transport{
@@ -37,5 +39,6 @@ func newDefaultTransport() *http.Transport {
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
+		ResponseHeaderTimeout: responseHeaderTimeout,
 	}
 }
