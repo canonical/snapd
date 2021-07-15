f9b7882f3237432d54308b1b6832e18160a6a771
Michael Vogt <mvo@ubuntu.com>
2021-07-15 20:11:31 +0200

    snap: add new `snap whoami --refresh-keys` to update ssh keys
    This commit adds support for manually refresh the ssh keys of
    the current user from Ubuntu SSO.
    
    This is a first step to help with:
    https://bugs.launchpad.net/snapd/+bug/1646559
    so that at least after adding a key it's trivial to refresh it.
    Users could enable a systemd user time to do this periodically
    too.
    
    Doing the real automatic syncing requires a bit more thinking
    
    Doing the real automatic syncing requires a bit more thinking
    (it seems to me) because then we would need to think about how
    to detect if keys are managed by non-snapd and how to avoid
    sending too many SSO requests etc.

--- cmd/snap/cmd_whoami.go
+++ cmd/snap/cmd_whoami.go
@@ -21,10 +21,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/jessevdk/go-flags"
 
 	"github.com/snapcore/snapd/i18n"
+	"github.com/snapcore/snapd/osutil"
+	"github.com/snapcore/snapd/release"
+	"github.com/snapcore/snapd/store"
 )
 
 var shortWhoAmIHelp = i18n.G("Show the email the user is logged in with")
@@ -34,10 +40,49 @@ The whoami command shows the email the user is logged in with.
 
 type cmdWhoAmI struct {
 	clientMixin
+
+	// XXX: rename to --{resync,overwrite,rewrite} or similar?
+	RefreshKeys bool `long:"refresh-keys"`
 }
 
 func init() {
 	addCommand("whoami", shortWhoAmIHelp, longWhoAmIHelp, func() flags.Commander { return &cmdWhoAmI{} }, nil, nil)
+}
+
+func refreshKeys(email string) error {
+	if release.OnClassic {
+		return fmt.Errorf("cannot refresh keys: not supported on classic")
+	}
+	if email == "" {
+		return fmt.Errorf("cannot refresh keys: no email")
+	}
+
+	// FIXME: set auth context
+	var storeCtx store.DeviceAndAuthContext
+	sto := storeNew(nil, storeCtx)
+	userInfo, err := sto.UserInfo(email)
+	if err != nil {
+		return fmt.Errorf("cannot refresh keys for %q: %s", email, err)
+	}
+	if len(userInfo.SSHKeys) == 0 {
+		return fmt.Errorf("cannot reresh keys for %q: no ssh keys found", email)
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("cannot refresh keys for %q: %s", email, err)
+	}
+	sshDir := filepath.Join(homeDir, ".ssh")
+	if err := os.MkdirAll(sshDir, 0700); err != nil {
+		return fmt.Errorf("cannot refresh %s: %s", sshDir, err)
+	}
+	authKeys := filepath.Join(sshDir, "authorized_keys")
+	authKeysContent := strings.Join(userInfo.SSHKeys, "\n")
+	if err := osutil.AtomicWriteFile(authKeys, []byte(authKeysContent), 0600, 0); err != nil {
+		return err
+	}
+	fmt.Fprintf(Stdout, "Wrote %v keys for %s\n", len(userInfo.SSHKeys), email)
+	return nil
 }
 
 func (cmd cmdWhoAmI) Execute(args []string) error {
@@ -49,6 +94,10 @@ func (cmd cmdWhoAmI) Execute(args []string) error {
 	if err != nil {
 		return err
 	}
+	if cmd.RefreshKeys {
+		return refreshKeys(email)
+	}
+
 	if email == "" {
 		// just printing nothing looks weird (as if something had gone wrong)
 		email = "-"
