29986f81936a6e4ceaee0645ad273f6ce1b7fda3
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2021-07-15 10:28:34 +0200

    overlord/devicestate: drop unnecessary todo
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- overlord/devicestate/handlers.go
+++ overlord/devicestate/handlers.go
@@ -170,9 +170,7 @@ func (m *DeviceManager) doMarkSeeded(t *state.Task, _ *tomb.Tomb) error {
 	}
 	if whatSeeded != nil && deviceCtx.RunMode() {
 		// record what seeded in the state only when in run mode
-
 		whatSeeded.SeedTime = now
-		// TODO:UC20 what about remodels?
 		if err := m.recordSeededSystem(st, whatSeeded); err != nil {
 			return fmt.Errorf("cannot record the seeded system: %v", err)
 		}
