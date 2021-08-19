e331222b2f2b7802d5ca34d18333837b1cd4d544
Michael Vogt <mvo@ubuntu.com>
2021-08-19 22:22:10 +0200

    secboot: use half the mem for KDF in AddRecoveryKey
    * osutil: rework TotalSystemMemory to TotalUsableMemory
    
    This commit renames total TotalSystemMemory to TotalUsableMemory
    and also changes the code to take the CmaTotal into account. This
    is the memory reserved by the  "Contiguous Memory Allocator" and
    it is not usable for normal processes. This kind of memory is
    used e.g. by the framebuffer of the Raspberry Pi or by DSPs on
    certain boards.
    
    * secboot: use half the mem for KDF in AddRecoveryKey
    
    Instead of benchmarking the KDF parameters for the recovery key
    (which takes some time to run) we can also use defaults for the
    KDF parameters. The defaults suggested by Chris are "4 iterations"
    and half the usable memory. This commit implements the suggestions.
    
    * secboot: update KDF memory heuristic
    
    After discussing with Chris and Samuele we updated the KDF memory
    heuristic so that it takes more parameters in mind. It now
    considers the usable memory and substracts a hardcoded 384MB
    that is required to have a working system (a bit of a conservative
    estiamte) and then takes half of this for the KDF memory.
    
    * osutil,secboot: fix typos
    
    * secboot: add comment about minimum mem

--- secboot/encrypt_sb_test.go
+++ secboot/encrypt_sb_test.go
@@ -22,10 +22,13 @@ package secboot_test
 
 import (
 	"errors"
+	"io/ioutil"
+	"path/filepath"
 
 	sb "github.com/snapcore/secboot"
 	. "gopkg.in/check.v1"
 
+	"github.com/snapcore/snapd/osutil"
 	"github.com/snapcore/snapd/secboot"
 )
 
@@ -72,7 +75,17 @@ func (s *encryptSuite) TestFormatEncryptedDevice(c *C) {
 	}
 }
 
+const mockedMeminfo = `MemTotal:         929956 kB
+CmaTotal:         131072 kB
+`
+
 func (s *encryptSuite) TestAddRecoveryKey(c *C) {
+	mockedMeminfoFile := filepath.Join(c.MkDir(), "meminfo")
+	err := ioutil.WriteFile(mockedMeminfoFile, []byte(mockedMeminfo), 0644)
+	c.Assert(err, IsNil)
+	restore := osutil.MockProcMeminfo(mockedMeminfoFile)
+	defer restore()
+
 	for _, tc := range []struct {
 		addErr error
 		err    string
@@ -94,7 +107,11 @@ func (s *encryptSuite) TestAddRecoveryKey(c *C) {
 			c.Assert(devicePath, Equals, "/dev/node")
 			c.Assert(recoveryKey[:], DeepEquals, myRecoveryKey[:])
 			c.Assert(key, DeepEquals, []byte(myKey))
-			c.Assert(opts, IsNil)
+			c.Assert(opts, DeepEquals, &sb.KDFOptions{
+				// (TotalMem - CmaMem - 384MB hardcoded) / 2
+				MemoryKiB:       int((929956 - 131072 - 384*1024) / 2),
+				ForceIterations: 4,
+			})
 			return tc.addErr
 		})
 		defer restore()
