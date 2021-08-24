4da8228fba171bb43e7df10aaa7daa2da5689464
Michael Vogt <mvo@ubuntu.com>
2021-08-24 19:51:01 +0200

    get-deps.sh: remove ref to govendor

--- get-deps.sh
+++ get-deps.sh
@@ -9,12 +9,13 @@ go mod vendor
 echo Obtaining c-dependencies
 (cd c-vendor && ./vendor.sh)
 
-if [ "$1" != "--skip-unused-check" ]; then
-    unused="$(govendor list +unused)"
-    if [ "$unused" != "" ]; then
-        echo "Found unused ./vendor packages:"
-        echo "$unused"
-        echo "Please fix via 'govendor remove +unused'"
-        exit 1
-    fi
-fi
+# TODO: port to go mod
+# if [ "$1" != "--skip-unused-check" ]; then
+#     unused="$(govendor list +unused)"
+#     if [ "$unused" != "" ]; then
+#         echo "Found unused ./vendor packages:"
+#         echo "$unused"
+#         echo "Please fix via 'govendor remove +unused'"
+#         exit 1
+#     fi
+# fi
