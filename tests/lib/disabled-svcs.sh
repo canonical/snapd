97631b8d00908cafd8e3a0b843ef116cd35855ae
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-08-24 15:16:37 -0300

    Fix failing tests

--- tests/lib/disabled-svcs.sh
+++ tests/lib/disabled-svcs.sh
@@ -3,7 +3,7 @@
 check_state_json_no_disabled_svcs() {
     "$TESTSTOOLS"/snapd-state check-state \
         '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?' \
-        "!=" \
+        "=" \
         "null" \
         "state.json has invalid last-active-disabled-services in it:"
 }
@@ -11,7 +11,7 @@ check_state_json_no_disabled_svcs() {
 check_state_json_yes_disabled_svcs() {
     "$TESTSTOOLS"/snapd-state check-state \
         '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?' \
-        "=" \
+        "!=" \
         "null" \
         "state.json has invalid last-active-disabled-services in it:"
 }
@@ -22,7 +22,7 @@ check_state_json_specific_disabled_svc() {
     SVC=$1
     "$TESTSTOOLS"/snapd-state check-state \
         '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"? | .[]' \
-        "!=" \
+        "=" \
         "$SVC" \
         "$SVC_MISSING_ERR_MSG"
 }
