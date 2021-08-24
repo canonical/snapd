dec644f5d92a2459f80a930b0b5a98885070a2e0
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-08-24 09:34:09 -0300

    Update command and minor fixes on tests

--- tests/lib/disabled-svcs.sh
+++ tests/lib/disabled-svcs.sh
@@ -2,7 +2,7 @@
 
 check_state_json_no_disabled_svcs() {
     "$TESTSTOOLS"/snapd-state check-state \
-        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?'
+        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?' \
         "!=" \
         "null" \
         "state.json has invalid last-active-disabled-services in it:"
@@ -10,7 +10,7 @@ check_state_json_no_disabled_svcs() {
 
 check_state_json_yes_disabled_svcs() {
     "$TESTSTOOLS"/snapd-state check-state \
-        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?'
+        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"?' \
         "=" \
         "null" \
         "state.json has invalid last-active-disabled-services in it:"
@@ -21,7 +21,7 @@ SVC_MISSING_ERR_MSG="state.json is missing last-active-disabled-services in it:"
 check_state_json_specific_disabled_svc() {
     SVC=$1
     "$TESTSTOOLS"/snapd-state check-state \
-        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"? | .[]'
+        '.data.snaps."disabled-svcs-kept" | ."last-active-disabled-services"? | .[]' \
         "!=" \
         "$SVC" \
         "$SVC_MISSING_ERR_MSG"
