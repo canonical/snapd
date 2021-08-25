9cd8f232e251cdbaecb653d3690a322b4070c93e
Miguel Pires <miguel.pires@canonical.com>
2021-08-25 10:37:52 +0100

    cmd/snap: move Size next to Rev column

--- cmd/snap/cmd_snap_op_test.go
+++ cmd/snap/cmd_snap_op_test.go
@@ -1088,8 +1088,8 @@ func (s *SnapSuite) TestRefreshList(c *check.C) {
 	rest, err := snap.Parser(snap.Client()).ParseArgs([]string{"refresh", "--list"})
 	c.Assert(err, check.IsNil)
 	c.Assert(rest, check.DeepEquals, []string{})
-	c.Check(s.Stdout(), check.Matches, `Name +Version +Rev +Publisher +Size +Notes
-foo +4.2update1 +17 +bar +436MB +-.*
+	c.Check(s.Stdout(), check.Matches, `Name +Version +Rev +Size +Publisher +Notes
+foo +4.2update1 +17 +436MB +bar +-.*
 `)
 	c.Check(s.Stderr(), check.Equals, "")
 	// ensure that the fake server api was actually hit
