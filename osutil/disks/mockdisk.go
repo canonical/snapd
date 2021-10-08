a7c8c651b2074b998e4b1ca94f8dc78fbba77451
Ian Johnson <ian.johnson@canonical.com>
2021-10-08 12:20:00 -0500

    osutil/disks/mockdisk.go: add MockDevicePathToDiskMapping
    This is needed for setting up mock disks from other packages.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- osutil/disks/mockdisk.go
+++ osutil/disks/mockdisk.go
@@ -293,9 +293,9 @@ func MockDeviceNameToDiskMapping(mockedDisks map[string]*MockDiskMapping) (resto
 
 	checkMockDiskMappingsForDuplicates(mockedDisks)
 
-	// note that devices can have multiple "names" that are recognized by
+	// note that devices can have multiple names that are recognized by
 	// udev/kernel, so we don't do any validation of the mapping here like we do
-	// for MockMountPointDisksToPartitionMapping and MockDevicePathDisksToPartitionMapping
+	// for MockMountPointDisksToPartitionMapping
 
 	old := diskFromDeviceName
 	diskFromDeviceName = func(deviceName string) (Disk, error) {
@@ -308,6 +308,32 @@ func MockDeviceNameToDiskMapping(mockedDisks map[string]*MockDiskMapping) (resto
 
 	return func() {
 		diskFromDeviceName = old
+	}
+}
+
+// MockDevicePathToDiskMapping will mock DiskFromDevicePath such that the
+// provided map of device names to mock disks is used instead of the actual
+// implementation using udev.
+func MockDevicePathToDiskMapping(mockedDisks map[string]*MockDiskMapping) (restore func()) {
+	osutil.MustBeTestBinary("mock disks only to be used in tests")
+
+	checkMockDiskMappingsForDuplicates(mockedDisks)
+
+	// note that devices can have multiple paths that are recognized by
+	// udev/kernel, so we don't do any validation of the mapping here like we do
+	// for MockMountPointDisksToPartitionMapping
+
+	old := diskFromDevicePath
+	diskFromDevicePath = func(devicePath string) (Disk, error) {
+		disk, ok := mockedDisks[devicePath]
+		if !ok {
+			return nil, fmt.Errorf("device path %q not mocked", devicePath)
+		}
+		return disk, nil
+	}
+
+	return func() {
+		diskFromDevicePath = old
 	}
 }
 
