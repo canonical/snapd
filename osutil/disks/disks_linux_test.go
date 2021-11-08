22f6bb630cee144ac94757bdf22449b458b29eab
Ian Johnson <ian.johnson@canonical.com>
2021-11-08 16:17:02 -0600

    osutil/disks: introduce RegisterDeviceMapperBackResolver + use for crypt-luks2
    Add a new callback functionality for registering handlers which "un-map" a
    device mapper node back to the original source for purposes of creating and/or
    locating a disk device.
    
    The CRYPT-LUKS2 based implementation currently supported is now implemented as
    a handler this way, and this paves the way for additional handlers to be
    implemented as well, specifically in other packages so that the osutil/disks
    package can remain as generic as possible and easily imported from anywhere
    without import cycles.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- osutil/disks/disks_linux_test.go
+++ osutil/disks/disks_linux_test.go
@@ -552,7 +552,7 @@ fi
 	c.Assert(err, ErrorMatches, "cannot find disk from mountpoint source /dev/mapper/something of /run/mnt/point: incomplete udev output missing required property \"ID_PART_ENTRY_DISK\"")
 }
 
-func (s *diskSuite) TestDiskFromMountPointIsDecryptedDeviceVolumeHappy(c *C) {
+func (s *diskSuite) TestDiskFromMountPointIsDecryptedLUKSDeviceVolumeHappy(c *C) {
 	restore := osutil.MockMountInfo(`130 30 242:1 / /run/mnt/point rw,relatime shared:54 - ext4 /dev/mapper/something rw
 `)
 	defer restore()
@@ -599,6 +599,20 @@ func (s *diskSuite) TestDiskFromMountPointIsDecryptedDeviceVolumeHappy(c *C) {
 	c.Assert(err, IsNil)
 
 	opts := &disks.Options{IsDecryptedDevice: true}
+
+	// when the handler is not available, we can't handle the mapper
+	disks.UnregisterDeviceMapperBackResolver("crypt-luks2")
+	defer func() {
+		// re-register it at the end, since it's registered by default
+		disks.RegisterDeviceMapperBackResolver("crypt-luks2", disks.CryptLuks2DeviceMapperHandler)
+	}()
+
+	_, err = disks.DiskFromMountPoint("/run/mnt/point", opts)
+	c.Assert(err, ErrorMatches, `cannot find disk from mountpoint source /dev/mapper/something of /run/mnt/point: no handler supports decrypted device mapper with UUID "CRYPT-LUKS2-5a522809c87e4dfa81a88dc5667d1304-something" and name "something"`)
+
+	// but when it is available it works
+	disks.RegisterDeviceMapperBackResolver("crypt-luks2", disks.CryptLuks2DeviceMapperHandler)
+
 	d, err := disks.DiskFromMountPoint("/run/mnt/point", opts)
 	c.Assert(err, IsNil)
 	c.Assert(d.Dev(), Equals, "42:0")
