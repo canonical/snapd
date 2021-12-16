dd9197c0311842ec9776aa0e8acb48b136146dcf
Matt Schulte <mschulte@cloudflare.com>
2021-12-16 09:16:09 -0800

    usersession/userd: support Cloudflare WARP URL scheme
    Allow Cloudflare WARP URL scheme to be opened by xdg-open with in snap
    to support WARP's user authorization work flow.

--- usersession/userd/launcher.go
+++ usersession/userd/launcher.go
@@ -80,6 +80,10 @@ var (
 		//   - scheme: apt:<name of package>
 		//   - https://github.com/snapcore/snapd/pull/7731
 		"apt",
+		// com.cloudflare.warp: the scheme allows for specifiying a package for
+		//   xdg-open to pass to the Cloudflare Teams client so that users can
+		//   sign into their team
+		"com.cloudflare.warp",
 		// help: the scheme allows for specifying a help URL. This code ensures that
 		//   the url is parseable
 		//   - scheme: help://topic
--- usersession/userd/launcher_test.go
+++ usersession/userd/launcher_test.go
@@ -74,7 +74,7 @@ func (s *launcherSuite) TestOpenURLWithNotAllowedScheme(c *C) {
 }
 
 func (s *launcherSuite) TestOpenURLWithAllowedSchemeHappy(c *C) {
-	for _, schema := range []string{"http", "https", "mailto", "snap", "help", "apt", "zoommtg", "zoomus", "zoomphonecall", "slack", "msteams"} {
+	for _, schema := range []string{"com.cloudflare.warp", "http", "https", "mailto", "snap", "help", "apt", "zoommtg", "zoomus", "zoomphonecall", "slack", "msteams"} {
 		err := s.launcher.OpenURL(schema+"://snapcraft.io", ":some-dbus-sender")
 		c.Assert(err, IsNil)
 		c.Assert(s.mockXdgOpen.Calls(), DeepEquals, [][]string{
