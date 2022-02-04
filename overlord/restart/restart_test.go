14170787a2af4274518ae6696be471d0569a9d4d
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-02-04 16:39:38 +0100

    o/restart: add tests for RebootInfo

--- overlord/restart/restart_test.go
+++ overlord/restart/restart_test.go
@@ -25,6 +25,7 @@ import (
 	. "gopkg.in/check.v1"
 
 	"github.com/snapcore/snapd/boot"
+	"github.com/snapcore/snapd/bootloader/bootloadertest"
 	"github.com/snapcore/snapd/overlord/restart"
 	"github.com/snapcore/snapd/overlord/state"
 )
@@ -39,10 +40,12 @@ type testHandler struct {
 	restartRequested   bool
 	rebootAsExpected   bool
 	rebootDidNotHappen bool
+	rebootInfo         *boot.RebootInfo
 }
 
-func (h *testHandler) HandleRestart(t restart.RestartType, rbl *boot.RebootInfo) {
+func (h *testHandler) HandleRestart(t restart.RestartType, rbi *boot.RebootInfo) {
 	h.restartRequested = true
+	h.rebootInfo = rbi
 }
 
 func (h *testHandler) RebootAsExpected(*state.State) error {
@@ -118,6 +121,52 @@ func (s *restartSuite) TestRequestRestartSystemAndVerifyReboot(c *C) {
 	restart.Request(st, restart.RestartSystem, nil)
 
 	c.Check(h.restartRequested, Equals, true)
+
+	ok, t = restart.Pending(st)
+	c.Check(ok, Equals, true)
+	c.Check(t, Equals, restart.RestartSystem)
+
+	var fromBootID string
+	c.Check(st.Get("system-restart-from-boot-id", &fromBootID), IsNil)
+	c.Check(fromBootID, Equals, "boot-id-1")
+
+	h1 := &testHandler{}
+	err = restart.Init(st, "boot-id-1", h1)
+	c.Assert(err, IsNil)
+	c.Check(h1.rebootAsExpected, Equals, false)
+	c.Check(h1.rebootDidNotHappen, Equals, true)
+	fromBootID = ""
+	c.Check(st.Get("system-restart-from-boot-id", &fromBootID), IsNil)
+	c.Check(fromBootID, Equals, "boot-id-1")
+
+	h2 := &testHandler{}
+	err = restart.Init(st, "boot-id-2", h2)
+	c.Assert(err, IsNil)
+	c.Check(h2.rebootAsExpected, Equals, true)
+	c.Check(st.Get("system-restart-from-boot-id", &fromBootID), Equals, state.ErrNoState)
+}
+
+func (s *restartSuite) TestRequestRestartSystemWithRebootInfo(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	h := &testHandler{}
+	err := restart.Init(st, "boot-id-1", h)
+	c.Assert(err, IsNil)
+	c.Check(h.rebootAsExpected, Equals, true)
+
+	ok, t := restart.Pending(st)
+	c.Check(ok, Equals, false)
+	c.Check(t, Equals, restart.RestartUnset)
+
+	restart.Request(st, restart.RestartSystem, &boot.RebootInfo{
+		RebootRequired: true,
+		Rbl:            &bootloadertest.MockRebootBootloader{}})
+
+	c.Check(h.restartRequested, Equals, true)
+	c.Check(h.rebootInfo.RebootRequired, Equals, true)
+	c.Check(h.rebootInfo.Rbl, NotNil)
 
 	ok, t = restart.Pending(st)
 	c.Check(ok, Equals, true)
