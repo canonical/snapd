0211b549f9c0c4d4435dfe607aceb41b483cae6c
Ian Johnson <ian.johnson@canonical.com>
2022-02-20 07:22:49 -0600

    gadget: add searchForVolumeWithTraits + tests
    This is used during a multi-volume asset update routine where we have the 
    traits for a volume which is to be updated and the LaidOutVolume for it, and we
    want to find a real disks.Disk{} for it.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- gadget/export_test.go
+++ gadget/export_test.go
@@ -59,6 +59,8 @@ var (
 	GadgetVolumeConsumesOneKernelUpdateAsset = gadgetVolumeConsumesOneKernelUpdateAsset
 
 	OnDiskStructureIsLikelyImplicitSystemDataRole = onDiskStructureIsLikelyImplicitSystemDataRole
+
+	SearchForVolumeWithTraits = searchForVolumeWithTraits
 )
 
 func MockEvalSymlinks(mock func(path string) (string, error)) (restore func()) {
