54f1c1fcce151cd87b8fe09bd41420c1906c2b03
Michael Vogt <mvo@ubuntu.com>
2022-03-17 10:47:01 +0100

    snapstate: remove system fontconfig rebuild
    Back in 2018 we added running `fc-cache` on install of snaps. This
    helped with performance issues for popular snaps like `vscode` [1].
    
    However nowdays the desktop helpers from snapcraft are much more
    elaborate and the fontconfig generation from snapd is no longer
    needed (expect for corner cases) and it was always a bit strange
    that snapd itself did things with fontconfig. Plus there were
    various issues over time with subtle incompatibilities in fc-cache
    that caused problems for our users.
    
    Hence this commit remove the run of system-wide fontconfig cache
    re-generation.
    
    [1] https://github.com/snapcore/snapd/pull/6195

--- overlord/snapstate/backend/export_test.go
+++ overlord/snapstate/backend/export_test.go
@@ -21,7 +21,6 @@ package backend
 
 import (
 	"os"
-	"os/exec"
 
 	"github.com/snapcore/snapd/osutil/sys"
 	"github.com/snapcore/snapd/snap"
@@ -39,22 +38,6 @@ func MockWrappersAddSnapdSnapServices(f func(s *snap.Info, opts *wrappers.AddSna
 	wrappersAddSnapdSnapServices = f
 	return func() {
 		wrappersAddSnapdSnapServices = old
-	}
-}
-
-func MockUpdateFontconfigCaches(f func() error) (restore func()) {
-	oldUpdateFontconfigCaches := updateFontconfigCaches
-	updateFontconfigCaches = f
-	return func() {
-		updateFontconfigCaches = oldUpdateFontconfigCaches
-	}
-}
-
-func MockCommandFromSystemSnap(f func(string, ...string) (*exec.Cmd, error)) (restore func()) {
-	old := commandFromSystemSnap
-	commandFromSystemSnap = f
-	return func() {
-		commandFromSystemSnap = old
 	}
 }
 
