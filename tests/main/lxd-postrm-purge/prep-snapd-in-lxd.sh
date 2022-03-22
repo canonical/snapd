f03724184177888d583b216e3711d31fa4489798
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-03-21 22:04:07 -0300

    Wait until snap command is available
    This is to prevent error trying to refresh lxd snap

--- tests/main/lxd-postrm-purge/prep-snapd-in-lxd.sh
+++ tests/main/lxd-postrm-purge/prep-snapd-in-lxd.sh
@@ -4,6 +4,12 @@ set -ex
 
 # TODO: Remove the refresh once the issue https://github.com/lxc/lxd/issues/10079 is release to 4.0/candidate
 # Make sure the lxd snap is updated before removing it
+for i in $(seq 30); do
+    if command -v snap; then
+        break
+    fi
+    sleep 1
+done
 snap refresh lxd --channel=latest/stable
 snap remove lxd
 
