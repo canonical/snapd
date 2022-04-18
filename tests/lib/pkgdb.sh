bc47bde1d0e04291a87aac8d48d4afc021f736ab
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-04-18 17:28:16 -0300

    Fix impish as well

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -621,17 +621,7 @@ pkg_dependencies_ubuntu_classic(){
                 shellcheck
                 "
             ;;
-        ubuntu-21.10-64)
-            # bpftool is part of linux-tools package
-            echo "
-                dbus-user-session
-                fwupd
-                golang
-                linux-tools-virtual
-                qemu-utils
-                "
-            ;;
-        ubuntu-22.04-64)
+        ubuntu-21.10-64|ubuntu-22.04-64)
             # bpftool is part of linux-tools package
             echo "
                 dbus-user-session
