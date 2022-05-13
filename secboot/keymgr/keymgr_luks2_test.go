1f7e1633ff634a23ba8e27ed9d338d9769241c32
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2022-05-13 11:01:13 +0200

    secboot/keymgr: add recovery key fail test
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- secboot/keymgr/keymgr_luks2_test.go
+++ secboot/keymgr/keymgr_luks2_test.go
@@ -153,6 +153,33 @@ func (s *keymgrSuite) TestAddRecoveryKeyToDeviceNoUnlockKey(c *C) {
 	c.Assert(cmd.Calls(), HasLen, 0)
 }
 
+func (s *keymgrSuite) TestAddRecoveryKeyToDeviceCryptsetupFail(c *C) {
+	unlockKey := "1234abcd"
+	restore := keymgr.MockGetDiskUnlockKeyFromKernel(func(prefix, devicePath string, remove bool) (sb.DiskUnlockKey, error) {
+		return []byte(unlockKey), nil
+	})
+	defer restore()
+
+	cmd := testutil.MockCommand(c, "cryptsetup", `
+while [ "$#" -gt 1 ]; do
+  case "$1" in
+    --key-file)
+      cat "$2" > /dev/null
+      shift 2
+      ;;
+    *)
+      shift 1
+      ;;
+  esac
+done
+echo "Other error, cryptsetup boom"
+exit 1
+`)
+	defer cmd.Restore()
+	err := keymgr.AddRecoveryKeyToLUKSDevice(mockRecoveryKey, "/dev/foobar")
+	c.Assert(err, ErrorMatches, "cannot add key: cryptsetup failed with: Other error, cryptsetup boom")
+}
+
 func (s *keymgrSuite) TestAddRecoveryKeyToDeviceOccupiedSlot(c *C) {
 	unlockKey := "1234abcd"
 	getCalls := 0
