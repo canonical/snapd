4f1c267a665b5d5e520cfbcbaa772ee1542b0b25
Maciej Borzecki <maciej.zenon.borzecki@canonical.com>
2022-05-23 14:02:13 +0200

    cmd/snap-confine: tweak naming, wrap startup timestamps in debug check
    Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

--- cmd/snap-confine/snap-confine.c
+++ cmd/snap-confine/snap-confine.c
@@ -287,8 +287,11 @@ static void sc_restore_process_state(const sc_preserved_process_state *
 	debug("the process has been placed in the special void directory");
 }
 
-static void log_startup_phase(const char *stage)
+static void log_startup_stage(const char *stage)
 {
+	if (!sc_is_debug_enabled()) {
+		return;
+	}
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	debug("-- snap startup {\"stage\":\"%s\", \"time\":\"%lu.%06lu\"}",
@@ -315,7 +318,7 @@ static void enter_non_classic_execution_environment(sc_invocation * inv,
 
 int main(int argc, char **argv)
 {
-	log_startup_phase("snap-confine enter");
+	log_startup_stage("snap-confine enter");
 	// Use our super-defensive parser to figure out what we've been asked to do.
 	sc_error *err = NULL;
 	struct sc_args *args SC_CLEANUP(sc_cleanup_args) = NULL;
@@ -395,7 +398,7 @@ int main(int argc, char **argv)
 		    " permission escalation attacks");
 	}
 
-	log_startup_phase("snap-confine mount namespace start");
+	log_startup_stage("snap-confine mount namespace start");
 
 	/* perform global initialization of mount namespace support for non-classic
 	 * snaps or both classic and non-classic when parallel-instances feature is
@@ -440,7 +443,7 @@ int main(int argc, char **argv)
 							real_gid, saved_gid);
 	}
 
-	log_startup_phase("snap-confine mount namespace finish");
+	log_startup_stage("snap-confine mount namespace finish");
 
 	// Temporarily drop privileges back to the calling user until we can
 	// permanently drop (which we can't do just yet due to seccomp, see
@@ -554,7 +557,7 @@ int main(int argc, char **argv)
 	}
 	// Restore process state that was recorded earlier.
 	sc_restore_process_state(&proc_state);
-	log_startup_phase("snap-confine to snap-exec");
+	log_startup_stage("snap-confine to snap-exec");
 	execv(invocation.executable, (char *const *)&argv[0]);
 	perror("execv failed");
 	return 1;
