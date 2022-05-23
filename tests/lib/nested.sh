a69dca4ec196e131d9f3cfe02e6e01ce465c7d69
Pawel Stolowski <stolowski@gmail.com>
2022-05-23 15:25:17 +0200

    tests: core20 preseed/nested spread test
    * Nested test for core20 preseeding using ubuntu-image.
    
    * Disable some shellcheck errors.
    
    * Exclude the new test from shellcheck.
    
    * Fix section order.
    
    * Fix unbound variable error.
    
    * Remove snap/.gnupg in restore.
    
    * Restore connections-after-failed-refresh test.
    
    * Fix typo
    
    Co-authored-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    * Remove newline.
    
    * Point at task.yaml.
    
    * Fix shellcheck exclude.
    
    * Fix exclude.
    
    * Squashed 'tests/lib/external/snapd-testing-tools/' changes from 53ca1b3b70..3b56339b88
    
    3b56339b88 Merge pull request #22 from snapcore/tests-fix-spread-shellcheck
    488408d792 fix shellcheck test
    b9340d265c Fix spread-shellcheck and update the test with new scenario
    
    git-subtree-dir: tests/lib/external/snapd-testing-tools
    git-subtree-split: 3b56339b88040d0b5b7abaf7ca6dbf33936e3cec
    
    * Improve nested.sh (thanks mardy!).
    
    * Disable SC2191
    
    Co-authored-by: Maciej Borzecki <maciek.borzecki@gmail.com>

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -22,6 +22,7 @@ NESTED_FAKESTORE_BLOB_DIR="${NESTED_FAKESTORE_BLOB_DIR:-$NESTED_WORK_DIR/fakesto
 NESTED_SIGN_SNAPS_FAKESTORE="${NESTED_SIGN_SNAPS_FAKESTORE:-false}"
 NESTED_FAKESTORE_SNAP_DECL_PC_GADGET="${NESTED_FAKESTORE_SNAP_DECL_PC_GADGET:-}"
 NESTED_UBUNTU_IMAGE_SNAPPY_FORCE_SAS_URL="${NESTED_UBUNTU_IMAGE_SNAPPY_FORCE_SAS_URL:-}"
+NESTED_UBUNTU_IMAGE_PRESEED_KEY="${NESTED_UBUNTU_IMAGE_PRESEED_KEY:-}"
 
 nested_wait_for_ssh() {
     # TODO:UC20: the retry count should be lowered to something more reasonable.
@@ -797,13 +798,21 @@ EOF
             else 
                 UBUNTU_IMAGE_CHANNEL_ARG=""
             fi
+
+            declare -a UBUNTU_IMAGE_PRESEED_ARGS
+            if [ -n "$NESTED_UBUNTU_IMAGE_PRESEED_KEY" ]; then
+                # shellcheck disable=SC2191
+                UBUNTU_IMAGE_PRESEED_ARGS+=(--preseed  --preseed-sign-key=\""$NESTED_UBUNTU_IMAGE_PRESEED_KEY"\")
+            fi
             # ubuntu-image creates sparse image files
             # shellcheck disable=SC2086
             "$UBUNTU_IMAGE" snap --image-size 10G "$NESTED_MODEL" \
                 $UBUNTU_IMAGE_CHANNEL_ARG \
+                "${UBUNTU_IMAGE_PRESEED_ARGS[@]}" \
                 --output-dir "$NESTED_IMAGES_DIR" \
                 $EXTRA_FUNDAMENTAL \
                 $EXTRA_SNAPS
+
             # ubuntu-image dropped the --output parameter, so we have to rename
             # the image ourselves, the images are named after volumes listed in
             # gadget.yaml
