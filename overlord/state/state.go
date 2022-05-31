e3cb8517013e1b9a8b7a2d7de72335fa6baad1b3
Miguel Pires <miguel.pires@canonical.com>
2022-05-31 11:42:22 +0100

    o/state: simplify Is() condition
    Signed-off-by: Miguel Pires <miguel.pires@canonical.com>

--- overlord/state/state.go
+++ overlord/state/state.go
@@ -258,7 +258,7 @@ func (e *NoStateError) Error() string {
 
 func (e *NoStateError) Is(err error) bool {
 	_, ok := err.(*NoStateError)
-	return ok || err == ErrNoState // nocheck=
+	return ok || errors.Is(err, ErrNoState)
 }
 
 // Get unmarshals the stored value associated with the provided key
