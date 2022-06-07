e2fa16e55192d9e67f182a67d80fec5af2aeecfd
Philip Meulengracht <philip.meulengracht@canonical.com>
2022-06-07 12:18:17 +0200

    dirs: fix rebase

--- dirs/dirs.go
+++ dirs/dirs.go
@@ -99,7 +99,6 @@ var (
 	SnapRuntimeServicesDir string
 	SnapUserServicesDir    string
 	SnapSystemdConfDir     string
-	SnapSystemdDir         string
 	SnapDesktopFilesDir    string
 	SnapDesktopIconsDir    string
 	SnapPolkitPolicyDir    string
