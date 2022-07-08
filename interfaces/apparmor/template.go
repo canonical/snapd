990bed36a1eeb5a7bf49602a724cb87f4bdd88ff
Philip Meulengracht <philip.meulengracht@canonical.com>
2022-07-08 10:06:56 +0200

    interfaces/apparmor: only add ubuntu-save rule for core systems

--- interfaces/apparmor/template.go
+++ interfaces/apparmor/template.go
@@ -471,11 +471,6 @@ var templateCommon = `
   /run/lock/snap.@{SNAP_INSTANCE_NAME}/ rw,
   /run/lock/snap.@{SNAP_INSTANCE_NAME}/** mrwklix,
   
-  # Allow each snaps to access each their own folder on the
-  # ubuntu-save partition, with write permissions.
-  /var/lib/snapd/save/snap/@{SNAP_INSTANCE_NAME}/ rw,
-  /var/lib/snapd/save/snap/@{SNAP_INSTANCE_NAME}/** mrwklix,
-
   ###DEVMODE_SNAP_CONFINE###
 `
 
@@ -817,6 +812,16 @@ var privDropAndChownRules = `
   # processes that ultimately run as non-root will send signals to those
   # processes as the matching non-root user.
   #capability kill,
+`
+
+// coreSnippet contains apparmor rules specific only for
+// snaps on native core systems.
+//
+var coreSnippet = `
+# Allow each snaps to access each their own folder on the
+# ubuntu-save partition, with write permissions.
+/var/lib/snapd/save/snap/@{SNAP_INSTANCE_NAME}/ rw,
+/var/lib/snapd/save/snap/@{SNAP_INSTANCE_NAME}/** mrwklix,
 `
 
 // classicTemplate contains apparmor template used for snaps with classic
