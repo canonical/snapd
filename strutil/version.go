c850c912cc4141be6cec42046111705cb5a2d2dc
Michael Vogt <mvo@ubuntu.com>
2022-08-15 18:21:10 +0200

    strutil: fix VersionCompare() to allow multiple `-` in the version
    The current code in strutil.VersionCompare() is not fully
    compatible with what a debian version allows. Specifically
    the version number of `libgcc-s1` in 22.04 is valid
    (`12-20220319-1ubuntu1`) but it's rejected right now.
    
    The debian policy [1] says:
    ```
    The upstream_version may contain only alphanumerics[14] and the
    characters . + - : (full stop, plus, hyphen, colon) and should
    start with a digit. If there is no debian_revision then hyphens
    are not allowed; if there is no epoch then colons are not allowed.
    ```
    and python-apt/apt/dpkg are all happy with the version. Given that
    we generally follow the debian policy for this we should fix this.
    
    Thanks to Gustavo for pointing this out.
    
    [1] https://www.debian.org/doc/debian-policy/ch-controlfields.html#version

--- strutil/version.go
+++ strutil/version.go
@@ -98,26 +98,13 @@ func matchEpoch(a string) bool {
 	return i < len(a) && a[i] == ':'
 }
 
-func atMostOneDash(a string) bool {
-	seen := false
-	for i := 0; i < len(a); i++ {
-		if a[i] == '-' {
-			if seen {
-				return false
-			}
-			seen = true
-		}
-	}
-	return true
-}
-
 // VersionIsValid returns true if the given string is a valid
 // version number according to the debian policy
 func VersionIsValid(a string) bool {
 	if matchEpoch(a) {
 		return false
 	}
-	return atMostOneDash(a)
+	return true
 }
 
 func nextFrag(s string) (frag, rest string, numeric bool) {
@@ -174,12 +161,12 @@ func VersionCompare(va, vb string) (res int, err error) {
 	}
 
 	var sa, sb string
-	if ia := strings.IndexByte(va, '-'); ia < 0 {
+	if ia := strings.LastIndexByte(va, '-'); ia < 0 {
 		sa = "0"
 	} else {
 		va, sa = va[:ia], va[ia+1:]
 	}
-	if ib := strings.IndexByte(vb, '-'); ib < 0 {
+	if ib := strings.LastIndexByte(vb, '-'); ib < 0 {
 		sb = "0"
 	} else {
 		vb, sb = vb[:ib], vb[ib+1:]
