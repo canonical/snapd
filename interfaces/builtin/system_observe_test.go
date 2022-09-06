6d94a771ee8fc0f8a5f55949e91bd5bdcb4cb0bf
Alberto Mardegan <mardy@users.sourceforge.net>
2022-09-06 16:51:01 +0300

    i/b/system_observe_test.go
    If OpenFile fails, file will be nil and Close would panic. Thanks @MiguelPires
    
    Co-authored-by: Miguel Pires <miguelpires94@gmail.com>

--- interfaces/builtin/system_observe_test.go
+++ interfaces/builtin/system_observe_test.go
@@ -114,8 +114,8 @@ func (s *SystemObserveInterfaceSuite) TestMountPermanentPlug(c *C) {
 	fakeBootDir := filepath.Join(tmpdir, "/boot")
 	c.Assert(os.MkdirAll(fakeBootDir, 0777), IsNil)
 	file, err := os.OpenFile(filepath.Join(fakeBootDir, "config-5.10"), os.O_CREATE, 0644)
-	c.Assert(file.Close(), IsNil)
 	c.Assert(err, IsNil)
+	c.Assert(file.Close(), IsNil)
 
 	mountSpec := &mount.Specification{}
 	c.Assert(mountSpec.AddPermanentPlug(s.iface, s.plugInfo), IsNil)
