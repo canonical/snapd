28a9a5b736bb6c304ece47f4ebcad4723cac815b
Alberto Mardegan <alberto.mardegan@canonical.com>
2022-09-06 09:38:20 +0300

    sandbox/cgroup: update comment about stale hierarchy

--- sandbox/cgroup/cgroup.go
+++ sandbox/cgroup/cgroup.go
@@ -259,10 +259,12 @@ func ProcessPathInTrackingCgroup(pid int) (string, error) {
 	// 1:name=systemd:/user.slice/user-1000.slice/user@1000.service/tmux.slice/tmux@default.service
 	// 0::/user.slice/user-1000.slice/user@1000.service/tmux.slice/tmux@default.service
 
-	// It seems cgroupv2 can be "dangling" after being mounted and unmounted.
-	// It will forever stay present in the kernel but will not be present in
-	// the file-system. As such, allow v2 to register only if it is really
-	// mounted on the system.
+	// The cgroup hierarchy (both v1 and v2) can be "dangling" after being
+	// mounted and unmounted.
+	// It will stay present in the kernel (and therefore its paths may appear
+	// in the /proc/<pid>/cgroup file) as long as there are some processes in
+	// it, but it will not be present in the file-system. As such, allow v2 to
+	// register only if it is really mounted on the system.
 	var useV2 bool
 	if ver, err := Version(); err != nil {
 		return "", err
