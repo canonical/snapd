900d9694ca828e07fb53ba1d77ff39c962d3067f
Michael Vogt <mvo@ubuntu.com>
2022-09-07 12:03:53 +0200

    tests: give fakeinstaller more structure

--- tests/lib/fakeinstaller/main.go
+++ tests/lib/fakeinstaller/main.go
@@ -30,24 +30,9 @@ import (
 	"github.com/snapcore/snapd/logger"
 )
 
-func run() error {
-	if len(os.Args) != 3 {
-		return fmt.Errorf("need seed-label and target-device as argument\n")
-	}
-	os.Setenv("SNAPD_DEBUG", "1")
-	logger.SimpleSetup()
-
-	seedLabel := os.Args[1]
-	bootDevice := os.Args[2]
-	logger.Noticef("installing on %q", bootDevice)
-
-	cli := client.New(nil)
-	details, err := cli.SystemDetails(seedLabel)
-	if err != nil {
-		return err
-	}
-	if len(details.Volumes) != 1 {
-		return fmt.Errorf("got unexpected number of volumes %v", len(details.Volumes))
+func createPartitions(bootDevice string, volumes map[string]*gadget.Volume) error {
+	if len(volumes) != 1 {
+		return fmt.Errorf("got unexpected number of volumes %v", len(volumes))
 	}
 
 	diskLayout, err := gadget.OnDiskVolumeFromDevice(bootDevice)
@@ -72,7 +57,7 @@ func run() error {
 
 	// TODO: support multiple volumes, see gadget/install/install.go
 	var vol *gadget.Volume
-	for _, avol := range details.Volumes {
+	for _, avol := range volumes {
 		vol = avol
 		break
 	}
@@ -80,12 +65,84 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("cannot layout volume: %v", err)
 	}
+
 	iconst := &install.Constraints{AllPartitions: true}
 	created, err := install.CreateMissingPartitions(gadgetRoot, diskLayout, lvol, iconst)
 	if err != nil {
 		return fmt.Errorf("cannot create parititons: %v", err)
 	}
 	logger.Noticef("created %v partitions", created)
+
+	return nil
+}
+
+func postSystemsInstallSetupStorageEncryption(details *client.SystemDetails) error {
+	// TODO: check details.StorageEncryption and call POST
+	// /systems/<seed-label> with "action":"install" and
+	// "step":"setup-storage-encryption"
+	return nil
+}
+
+func postSystemsInstallFinish(details *client.SystemDetails) error {
+	// TODO: call POST /systems/<seed-label> with
+	// "action":"install" and "step":"finish"
+	return nil
+}
+
+func createClassicRootfsIfNeeded(bootDevice string, details *client.SystemDetails) error {
+	// TODO: check model and if classic create rootfs somehow
+	return nil
+}
+
+func createSeedOnTarget(bootDevice, seedLabel string) error {
+	// TODO: copy installer seed to target
+	return nil
+}
+
+// XXX: or will POST {"action":"install","step":"finalize"} do that?
+func writeModeenvOnTarget(seedLabel string) error {
+	// TODO: write modeenv
+	return nil
+}
+
+func run() error {
+	if len(os.Args) != 3 {
+		return fmt.Errorf("need seed-label and target-device as argument\n")
+	}
+	os.Setenv("SNAPD_DEBUG", "1")
+	logger.SimpleSetup()
+
+	seedLabel := os.Args[1]
+	bootDevice := os.Args[2]
+	logger.Noticef("installing on %q", bootDevice)
+
+	cli := client.New(nil)
+	details, err := cli.SystemDetails(seedLabel)
+	if err != nil {
+		return err
+	}
+	// TODO: grow the data-partition based on disk size
+	if err := createPartitions(bootDevice, details.Volumes); err != nil {
+		return fmt.Errorf("cannot setup partitions: %v", err)
+	}
+	if err := postSystemsInstallSetupStorageEncryption(details); err != nil {
+		return fmt.Errorf("cannot setup storage encryption: %v", err)
+	}
+	if err := createClassicRootfsIfNeeded(bootDevice, details); err != nil {
+		return fmt.Errorf("cannot create classic rootfs: %v", err)
+	}
+	if err := createSeedOnTarget(bootDevice, seedLabel); err != nil {
+		return fmt.Errorf("cannot create seed on target: %v", err)
+	}
+	// XXX: or will POST {"action":"install","step":"finalize"} do that?
+	if err := writeModeenvOnTarget(seedLabel); err != nil {
+		return fmt.Errorf("cannot write modeenv on target: %v", err)
+	}
+	// XXX: will the POST below trigger a reboot on the snapd side? if
+	//      not we need to reboot here
+	if err := postSystemsInstallFinish(details); err != nil {
+		return fmt.Errorf("cannot finalize install: %v", err)
+	}
 
 	return nil
 }
