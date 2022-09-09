canonical/snapd

commit 5abb3aab6b74db5a9e9920c537d673806603349a

interfaces/system-packages-doc: allow read-only access to /usr/share/gtk-doc
* interfaces/system-packages-doc: allow read-only access to /usr/share/gtk-doc (LP: #1960546)

* interfaces/builtin/system-packages-doc: apparmor rules for a single mimic

Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

* tests/main/interfaces-system-packages-doc: verify that gtk-doc is accessible

Signed-off-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

Co-authored-by: Maciej Borzecki <maciej.zenon.borzecki@canonical.com>

interfaces/builtin/system_packages_doc.go
@@ -40,6 +40,7 @@ const systemPackagesDocConnectedPlugAppArmor = `
 # Description: can access documentation of system packages.
 
 /usr/share/doc/{,**} r,
+/usr/share/gtk-doc/{,**} r,
 /usr/share/libreoffice/help/{,**} r,
 /usr/share/xubuntu-docs/{,**} r,
 `
@@ -55,17 +56,18 @@ func (iface *systemPackagesDocInterface) AppArmorConnectedPlug(spec *apparmor.Sp
 	emit("  mount options=(bind) /var/lib/snapd/hostfs/usr/share/doc/ -> /usr/share/doc/,\n")
 	emit("  remount options=(bind, ro) /usr/share/doc/,\n")
 	emit("  umount /usr/share/doc/,\n")
+	emit("  mount options=(bind) /var/lib/snapd/hostfs/usr/share/gtk-doc/ -> /usr/share/gtk-doc/,\n")
+	emit("  remount options=(bind, ro) /usr/share/gtk-doc/,\n")
+	emit("  umount /usr/share/gtk-doc/,\n")
 	emit("  mount options=(bind) /var/lib/snapd/hostfs/usr/share/libreoffice/help/ -> /usr/share/libreoffice/help/,\n")
 	emit("  remount options=(bind, ro) /usr/share/libreoffice/help/,\n")
 	emit("  umount /usr/share/libreoffice/help/,\n")
-	// /usr/share/libreoffice may not exist, set up apparmor profile for
-	// creating a mimic, assuming that at least /usr/share exists
-	apparmor.GenWritableProfile(emit, "/usr/share/libreoffice/help", 3)
 	emit("  mount options=(bind) /var/lib/snapd/hostfs/usr/share/xubuntu-docs/ -> /usr/share/xubuntu-docs/,\n")
 	emit("  remount options=(bind, ro) /usr/share/xubuntu-docs/,\n")
 	emit("  umount /usr/share/xubuntu-docs/,\n")
-	// and a writable mimic for /usr/share/xubuntu-docs
-	apparmor.GenWritableProfile(emit, "/usr/share/xubuntu-docs", 3)
+	// the mount targets under /usr/share/ do not necessarily exist in the
+	// base image, in which case, we need to create a writable mimic
+	apparmor.GenWritableProfile(emit, "/usr/share/", 3)
 	return nil
 }
 
@@ -73,6 +75,11 @@ func (iface *systemPackagesDocInterface) MountConnectedPlug(spec *mount.Specific
 	spec.AddMountEntry(osutil.MountEntry{
 		Name:    "/var/lib/snapd/hostfs/usr/share/doc",
 		Dir:     "/usr/share/doc",
+		Options: []string{"bind", "ro"},
+	})
+	spec.AddMountEntry(osutil.MountEntry{
+		Name:    "/var/lib/snapd/hostfs/usr/share/gtk-doc",
+		Dir:     "/usr/share/gtk-doc",
 		Options: []string{"bind", "ro"},
 	})
 	spec.AddMountEntry(osutil.MountEntry{

interfaces/builtin/system_packages_doc_test.go
@@ -94,23 +94,25 @@ func (s *systemPackagesDocSuite) TestAppArmorSpec(c *C) {
 	c.Check(updateNS, testutil.Contains, "  mount options=(bind) /var/lib/snapd/hostfs/usr/share/doc/ -> /usr/share/doc/,\n")
 	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/doc/,\n")
 	c.Check(updateNS, testutil.Contains, "  umount /usr/share/doc/,\n")
+
+	c.Check(updateNS, testutil.Contains, "  mount options=(bind) /var/lib/snapd/hostfs/usr/share/gtk-doc/ -> /usr/share/gtk-doc/,\n")
+	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/gtk-doc/,\n")
+	c.Check(updateNS, testutil.Contains, "  umount /usr/share/gtk-doc/,\n")
+
 	c.Check(updateNS, testutil.Contains, "  mount options=(bind) /var/lib/snapd/hostfs/usr/share/libreoffice/help/ -> /usr/share/libreoffice/help/,\n")
 	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/libreoffice/help/,\n")
 	c.Check(updateNS, testutil.Contains, "  umount /usr/share/libreoffice/help/,\n")
 
+	c.Check(updateNS, testutil.Contains, "  mount options=(bind) /var/lib/snapd/hostfs/usr/share/xubuntu-docs/ -> /usr/share/xubuntu-docs/,\n")
+	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/xubuntu-docs/,\n")
+	c.Check(updateNS, testutil.Contains, "  umount /usr/share/xubuntu-docs/,\n")
 	// check mimic bits
-	c.Check(updateNS, testutil.Contains, "  # Writable mimic /usr/share/libreoffice\n")
+	c.Check(updateNS, testutil.Contains, "  # Writable mimic /usr/share\n")
 	c.Check(updateNS, testutil.Contains, "  mount fstype=tmpfs options=(rw) tmpfs -> \"/usr/share/\",\n")
 	c.Check(updateNS, testutil.Contains, "  \"/usr/share/\" r,\n")
 	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/\" rw,\n")
 	c.Check(updateNS, testutil.Contains, "  mount options=(bind, rw) \"/tmp/.snap/usr/share/*\" -> \"/usr/share/*\",\n")
 
-	c.Check(updateNS, testutil.Contains, "  mount options=(bind) /var/lib/snapd/hostfs/usr/share/xubuntu-docs/ -> /usr/share/xubuntu-docs/,\n")
-	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/xubuntu-docs/,\n")
-	c.Check(updateNS, testutil.Contains, "  umount /usr/share/xubuntu-docs/,\n")
-	// there is xubuntu-docs specific rule as it's already covered by mimic
-	// on /usr/share, so only check that
-	c.Check(updateNS, testutil.Contains, "  # Writable mimic /usr/share\n")
 }
 
 func (s *systemPackagesDocSuite) TestMountSpec(c *C) {
@@ -121,16 +123,19 @@ func (s *systemPackagesDocSuite) TestMountSpec(c *C) {
 	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.coreSlot), IsNil)
 
 	entries := spec.MountEntries()
-	c.Assert(entries, HasLen, 3)
+	c.Assert(entries, HasLen, 4)
 	c.Check(entries[0].Name, Equals, "/var/lib/snapd/hostfs/usr/share/doc")
 	c.Check(entries[0].Dir, Equals, "/usr/share/doc")
 	c.Check(entries[0].Options, DeepEquals, []string{"bind", "ro"})
-	c.Check(entries[1].Name, Equals, "/var/lib/snapd/hostfs/usr/share/libreoffice/help")
-	c.Check(entries[1].Dir, Equals, "/usr/share/libreoffice/help")
+	c.Check(entries[1].Name, Equals, "/var/lib/snapd/hostfs/usr/share/gtk-doc")
+	c.Check(entries[1].Dir, Equals, "/usr/share/gtk-doc")
 	c.Check(entries[1].Options, DeepEquals, []string{"bind", "ro"})
-	c.Check(entries[2].Name, Equals, "/var/lib/snapd/hostfs/usr/share/xubuntu-docs")
-	c.Check(entries[2].Dir, Equals, "/usr/share/xubuntu-docs")
+	c.Check(entries[2].Name, Equals, "/var/lib/snapd/hostfs/usr/share/libreoffice/help")
+	c.Check(entries[2].Dir, Equals, "/usr/share/libreoffice/help")
 	c.Check(entries[2].Options, DeepEquals, []string{"bind", "ro"})
+	c.Check(entries[3].Name, Equals, "/var/lib/snapd/hostfs/usr/share/xubuntu-docs")
+	c.Check(entries[3].Dir, Equals, "/usr/share/xubuntu-docs")
+	c.Check(entries[3].Options, DeepEquals, []string{"bind", "ro"})
 
 	entries = spec.UserMountEntries()
 	c.Assert(entries, HasLen, 0)

tests/main/interfaces-system-packages-doc/task.yaml
@@ -11,6 +11,8 @@ prepare: |
     echo text >/usr/share/libreoffice/help/content
     mkdir -p /usr/share/xubuntu-docs
     echo text >/usr/share/xubuntu-docs/content
+    mkdir -p /usr/share/gtk-doc
+    echo text >/usr/share/gtk-doc/content
 
 restore: |
     snap remove --purge test-snapd-app
@@ -26,9 +28,11 @@ execute: |
     test-snapd-app.sh -c 'cat /usr/share/doc/system-packages-doc-iface/content' | MATCH text
     test-snapd-app.sh -c 'cat /usr/share/libreoffice/help/content' | MATCH text
     test-snapd-app.sh -c 'cat /usr/share/xubuntu-docs/content' | MATCH text
+    test-snapd-app.sh -c 'cat /usr/share/gtk-doc/content' | MATCH text
 
     # The interface can be disconnected
     snap disconnect test-snapd-app:system-packages-doc
     not test-snapd-app.sh -c 'test -e /usr/share/doc/system-packages-doc-iface/content'
     not test-snapd-app.sh -c 'test -e /usr/share/libreoffice/help/content'
     not test-snapd-app.sh -c 'test -e /usr/share/xubuntu-docs/content'
+    not test-snapd-app.sh -c 'test -e /usr/share/gtk-doc/content'

commit 7e29ac97944aff4934188e2c2e194eecf48925f1

i/b/system_packages_doc: restore access to LO, cups and gimp docs
Commit 5abb3aab6b74db5a9e9920c537d673806603349a broke the access to all
directories of the form /usr/share/*/* by removing the AppArmor rules
that would allow snap-update-ns to create their mimics. Specifically, it
affected:

    /usr/share/libreoffice/help
    /usr/share/cups/doc-root
    /usr/share/gimp/2.0/help

because while it allowed snap-update-ns to create the parent directory
(e.g., /usr/share/libreoffice/) it would not contain a rule which
permitted the creation of the leaf mimic.

Such a permission is only granted by explicitly passing the parent
directory to GenWritableProfile().

Fixes: LP: #1951210

interfaces/builtin/system_packages_doc.go
@@ -73,9 +73,15 @@ func (iface *systemPackagesDocInterface) AppArmorConnectedPlug(spec *apparmor.Sp
 	emit("  mount options=(bind) /var/lib/snapd/hostfs/usr/share/xubuntu-docs/ -> /usr/share/xubuntu-docs/,\n")
 	emit("  remount options=(bind, ro) /usr/share/xubuntu-docs/,\n")
 	emit("  umount /usr/share/xubuntu-docs/,\n")
-	// the mount targets under /usr/share/ do not necessarily exist in the
-	// base image, in which case, we need to create a writable mimic
-	apparmor.GenWritableProfile(emit, "/usr/share/", 3)
+	// The mount targets under /usr/share/ do not necessarily exist in the
+	// base image, in which case, we need to create a writable mimic.
+	// We are probably abusing GenWritableProfile() here by passing an AppArmor
+	// pattern as the path, but as long as the pattern does not contain a "/"
+	// (which would confuse Go's filepath routines) we should be fine. The next
+	// line with the gimp/2.0 path could not be merged into the same rule
+	// exactly because it contains a "/".
+	apparmor.GenWritableProfile(emit, "/usr/share/{libreoffice,cups}/", 3)
+	apparmor.GenWritableProfile(emit, "/usr/share/gimp/2.0/", 3)
 	return nil
 }
 

interfaces/builtin/system_packages_doc_test.go
@@ -117,10 +117,18 @@ func (s *systemPackagesDocSuite) TestAppArmorSpec(c *C) {
 	c.Check(updateNS, testutil.Contains, "  remount options=(bind, ro) /usr/share/xubuntu-docs/,\n")
 	c.Check(updateNS, testutil.Contains, "  umount /usr/share/xubuntu-docs/,\n")
 	// check mimic bits
-	c.Check(updateNS, testutil.Contains, "  # Writable mimic /usr/share\n")
+	c.Check(updateNS, testutil.Contains, "  # Writable mimic /usr/share/{libreoffice,cups}\n")
 	c.Check(updateNS, testutil.Contains, "  mount fstype=tmpfs options=(rw) tmpfs -> \"/usr/share/\",\n")
 	c.Check(updateNS, testutil.Contains, "  \"/usr/share/\" r,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/usr/share/*/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/usr/share/{libreoffice,cups}/*/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/usr/share/gimp/*/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/usr/share/gimp/2.0/*/\" rw,\n")
 	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/{libreoffice,cups}/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/{libreoffice,cups}/*/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/gimp/2.0/\" rw,\n")
+	c.Check(updateNS, testutil.Contains, "  \"/tmp/.snap/usr/share/gimp/2.0/*/\" rw,\n")
 	c.Check(updateNS, testutil.Contains, "  mount options=(bind, rw) \"/tmp/.snap/usr/share/*\" -> \"/usr/share/*\",\n")
 
 }