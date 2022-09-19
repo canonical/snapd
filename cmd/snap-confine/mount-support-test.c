a9812c467597159dd7fdbbb2da16b41c1a002962
Alex Murray <alex.murray@canonical.com>
2022-09-19 13:50:36 +0930

    many: Use /tmp/snap-private-tmp for per-snap private tmps
    To avoid unprivileged users being able to interfere with the creation of the
    private snap mount namespace, instead of creating this as /tmp/snap.$SNAP_NAME/
    we can now use the systemd-tmpfiles configuration to do this for us
    at boot with a known fixed name (/tmp/snap-private-tmp/) and then use that as
    the base dir for creating per-snap private tmp mount
    namespaces (eg. /tmp/snap-private-tmp/snap.$SNAP_INSTANCE/tmp) etc.
    
    Signed-off-by: Alex Murray <alex.murray@canonical.com>

--- cmd/snap-confine/mount-support-test.c
+++ cmd/snap-confine/mount-support-test.c
@@ -92,50 +92,10 @@ static void test_is_subdir(void)
 	g_assert_false(is_subdir("/", ""));
 }
 
-static void test_must_mkdir_and_open_with_perms(void)
-{
-	// make a directory with some contents and check we can
-	// must_mkdir_and_open_with_perms() to get control of it
-	GError *error = NULL;
-	GStatBuf st;
-	gchar *test_dir = g_dir_make_tmp("test-mkdir-XXXXXX", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(test_dir);
-	g_assert_cmpint(chmod(test_dir, 0755), ==, 0);
-	g_assert_true(g_file_test
-		      (test_dir, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
-	g_assert_cmpint(g_stat(test_dir, &st), ==, 0);
-	g_assert_true(st.st_uid == getuid());
-	g_assert_true(st.st_gid == getgid());
-	g_assert_true(st.st_mode == (S_IFDIR | 0755));
-
-	gchar *test_subdir = g_build_filename(test_dir, "foo", NULL);
-	g_assert_cmpint(g_mkdir_with_parents(test_dir, 0755), ==, 0);
-	g_file_test(test_subdir, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR);
-
-	// take over dir
-	int fd =
-	    must_mkdir_and_open_with_perms(test_dir, getuid(), getgid(), 0700);
-	// check can unlink dir itself with no contents successfully and it
-	// still exists
-	g_assert_cmpint(fd, >=, 0);
-	g_assert_false(g_file_test
-		       (test_subdir, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
-	g_assert_true(g_file_test
-		      (test_dir, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
-	g_assert_cmpint(g_stat(test_dir, &st), ==, 0);
-	g_assert_true(st.st_uid == getuid());
-	g_assert_true(st.st_gid == getgid());
-	g_assert_true(st.st_mode == (S_IFDIR | 0700));
-	close(fd);
-}
-
 static void __attribute__((constructor)) init(void)
 {
 	g_test_add_func("/mount/get_nextpath/typical",
 			test_get_nextpath__typical);
 	g_test_add_func("/mount/get_nextpath/weird", test_get_nextpath__weird);
 	g_test_add_func("/mount/is_subdir", test_is_subdir);
-	g_test_add_func("/mount/must_mkdir_and_open_with_perms",
-			test_must_mkdir_and_open_with_perms);
 }
