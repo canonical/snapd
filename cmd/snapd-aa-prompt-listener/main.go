ca9c14d669fa973c8a246b359e7880bcdb85e61d
Alberto Mardegan <alberto.mardegan@canonical.com>
2022-09-20 14:10:38 +0300

    many: add stub services for prompting
    The implementation of these services is not yet ready, but in order to
    be able to deliver it in a snap package, we need to have at least the
    service files installed on the system by the distribution package.
    
    These stubs just reexecute the binaries from the snap.

---
+++ cmd/snapd-aa-prompt-listener/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/snapcore/snapd/logger"
+	"github.com/snapcore/snapd/snapdtool"
+)
+
+func init() {
+	err := logger.SimpleSetup()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "WARNING: failed to activate logging: %v\n", err)
+	}
+}
+
+func main() {
+	snapdtool.ExecInSnapdOrCoreSnap()
+	// This point is only reached if reexec did not happen
+	fmt.Fprintln(os.Stderr, "Prompt listener not implemented")
+}
