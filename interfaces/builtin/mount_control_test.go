2eb3cb7f8765dcc10e24c6982db69cdff476e4e5
Alberto Mardegan <alberto.mardegan@canonical.com>
2022-09-27 18:44:09 +0300

    i/b/mount-control: add optional `/` to umount rules (#12168)
    * i/b/mount-control: add optional `/` to umount rules
    
    In commit a10ecaaaafc0aeaac59d563fc08cc6ba0ad75f1a we added an optional
    `/` at the end of the AppArmor mount rules, since AppArmor is rather
    strict on them.
    
    Indeed, the same applies to umount rules.
    
    * tests: improve mount-control spread tests
    
    Diversify the paths in the "where" attributes, otherwise a
    value like "$SNAP_COMMON/**" would hide potential errors in the "where"
    attributes of other mount entries -- as indeed has been the case until
    now, when we realized of the `/` missing at the end of the umount rules
    (see the previous commit).
    
    * tests: improve mount-control tests with review comments
    
    Add a "/" to a mount point to verify that it's irrelevant.

--- interfaces/builtin/mount_control_test.go
+++ interfaces/builtin/mount_control_test.go
@@ -295,12 +295,16 @@ func (s *MountControlInterfaceSuite) TestAppArmorSpec(c *C) {
 		`reiserfs,squashfs,tmpfs,ubifs,udf,ufs,vfat,zfs,xfs` +
 		`) options=(ro) "/dev/sda{0,1}" -> "/var/snap/consumer/common/**{,/}",`
 	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, expectedMountLine3)
+	expectedUmountLine3 := `umount "/var/snap/consumer/common/**{,/}",`
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, expectedUmountLine3)
 
 	expectedMountLine4 := `mount fstype=(` +
 		`aufs,autofs,btrfs,ext2,ext3,ext4,hfs,iso9660,jfs,msdos,ntfs,ramfs,` +
 		`reiserfs,squashfs,tmpfs,ubifs,udf,ufs,vfat,zfs,xfs` +
 		`) options=(sync) "/dev/sda[0-1]" -> "/var/snap/consumer/common/{foo,other,**}{,/}",`
 	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, expectedMountLine4)
+	expectedUmountLine4 := `umount "/var/snap/consumer/common/{foo,other,**}{,/}",`
+	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, expectedUmountLine4)
 }
 
 func (s *MountControlInterfaceSuite) TestStaticInfo(c *C) {
