91565094b69096cee20978803f73d86f8412f92b
Samuele Pedroni <pedronis@lucediurna.net>
2022-10-12 09:59:47 +0200

    o/ifacestate: pass a pointer to the whole ConnectCandidate for convenience
    thanks @mardy

--- overlord/ifacestate/helpers.go
+++ overlord/ifacestate/helpers.go
@@ -667,7 +667,7 @@ func addNewConnection(st *state.State, task *state.Task, newconns map[string]*in
 
 // DebugAutoConnectCheck is a hook that can be set to debug auto-connection
 // candidates as they are checked.
-var DebugAutoConnectCheck func(*interfaces.ConnectedPlug, *interfaces.ConnectedSlot, interfaces.SideArity, error)
+var DebugAutoConnectCheck func(*policy.ConnectCandidate, interfaces.SideArity, error)
 
 type autoConnectChecker struct {
 	st   *state.State
@@ -752,7 +752,7 @@ func (c *autoConnectChecker) check(plug *interfaces.ConnectedPlug, slot *interfa
 
 	arity, err := ic.CheckAutoConnect()
 	if DebugAutoConnectCheck != nil {
-		DebugAutoConnectCheck(plug, slot, arity, err)
+		DebugAutoConnectCheck(&ic, arity, err)
 	}
 	if err == nil {
 		return true, arity, nil
