f9f1c04c90571bf74907a7be822f02b99afa270e
Michael Vogt <mvo@ubuntu.com>
2022-10-19 15:32:01 +0200

    tests: imprve muinstaller to be more role/installer aware
    The mk-classic-rootfs.sh script is either used as part of an installer
    image which will have a "base.squashfs" or as part of a spread test.
    
    For the installer image very little additional setup is needed. For
    the spread test the system needs to be setup to be used from spread.
    
    So a new "ROLE" var will be set accordingly so that the function
    "prepare_classic_rootfs" from the script knows what to do.

--- tests/lib/fakeinstaller/mk-classic-rootfs.sh
+++ tests/lib/fakeinstaller/mk-classic-rootfs.sh
@@ -10,9 +10,10 @@ set -e
 
 # XXX: merge with the work from alfonso in
 # tests/nested/manual/fde-on-classic/mk-image.sh (PR:12102)
-create_classic_rootfs() {
+prepare_classic_rootfs() {
     set -x
     local DESTDIR="$1"
+    local ROLE="$2"
 
 
     # Create basic devices to be able to install packages
@@ -20,15 +21,18 @@ create_classic_rootfs() {
     [ -e "$DESTDIR"/dev/zero ] || sudo mknod -m 666 "$DESTDIR"/dev/zero c 1 5
     [ -e "$DESTDIR"/dev/random ] || sudo mknod -m 666 "$DESTDIR"/dev/random c 1 8
     [ -e "$DESTDIR"/dev/urandom ] || sudo mknod -m 666 "$DESTDIR"/dev/urandom c 1 9
-    # ensure resolving works inside the chroot
-    echo "nameserver 8.8.8.8" | sudo tee -a "$DESTDIR"/etc/resolv.conf
-    # install additional packages
-    sudo chroot "$DESTDIR" /usr/bin/sh -c "DEBIAN_FRONTEND=noninteractive apt update"
-    local pkgs="snapd ssh openssh-server sudo iproute2 iputils-ping isc-dhcp-client netplan.io vim-tiny kmod cloud-init"
-    sudo chroot "$DESTDIR" /usr/bin/sh -c \
-         "DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends -y $pkgs"
-    # netplan config
-    cat <<'EOF' | sudo tee "$DESTDIR"/etc/netplan/00-ethernet.yaml
+
+    if [ "$ROLE" = spread ]; then
+        # ensure resolving works inside the chroot
+        echo "nameserver 8.8.8.8" | sudo tee -a "$DESTDIR"/etc/resolv.conf
+
+        # install additional packages
+        sudo chroot "$DESTDIR" /usr/bin/sh -c "DEBIAN_FRONTEND=noninteractive apt update"
+        local pkgs="snapd ssh openssh-server sudo iproute2 iputils-ping isc-dhcp-client netplan.io vim-tiny kmod cloud-init"
+        sudo chroot "$DESTDIR" /usr/bin/sh -c \
+             "DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends -y $pkgs"
+        # netplan config
+        cat <<'EOF' | sudo tee "$DESTDIR"/etc/netplan/00-ethernet.yaml
 network:
   ethernets:
     any:
@@ -37,28 +41,26 @@ network:
       dhcp4: true
   version: 2
 EOF
-
-    # ensure we can login
-    sudo chroot "$DESTDIR" /usr/sbin/adduser --disabled-password --gecos "" user1
-    printf "ubuntu\nubuntu\n" | sudo chroot "$DESTDIR" /usr/bin/passwd user1
-    echo "user1 ALL=(ALL) NOPASSWD:ALL" | sudo tee -a "$DESTDIR"/etc/sudoers
-
-    # set password for root user
-    sudo chroot "$DESTDIR" /usr/bin/sh -c 'echo root:root | chpasswd'
-    sudo tee -a "$DESTDIR/etc/ssh/sshd_config" <<'EOF'
+        # set password for root user
+        sudo chroot "$DESTDIR" /usr/bin/sh -c 'echo root:root | chpasswd'
+        sudo mkdir -p "$DESTDIR/etc/ssh"
+        sudo tee -a "$DESTDIR/etc/ssh/sshd_config" <<'EOF'
 PermitRootLogin yes
 PasswordAuthentication yes
 EOF
 
-    # install the current in-development version of snapd when available,
-    # this will give us seeding support
-    GOPATH="${GOPATH:-./}"
-    package=$(find "$GOPATH" -maxdepth 1 -name "snapd_*.deb")
-    if [ -e "$package"  ]; then
+        # install the current in-development version of snapd
+        GOPATH="${GOPATH:-./}"
+        package=$(find "$GOPATH" -maxdepth 1 -name "snapd_*.deb")
         cp "$package" "$DESTDIR"/var/cache/apt/archives
         sudo chroot "$DESTDIR" /usr/bin/sh -c \
              "DEBIAN_FRONTEND=noninteractive apt install -y /var/cache/apt/archives/$(basename "$package")"
     fi
+        
+    # ensure we can login
+    sudo chroot "$DESTDIR" /usr/sbin/adduser --disabled-password --gecos "" user1
+    printf "ubuntu\nubuntu\n" | sudo chroot "$DESTDIR" /usr/bin/passwd user1
+    echo "user1 ALL=(ALL) NOPASSWD:ALL" | sudo tee -a "$DESTDIR"/etc/sudoers
 
     # ensure that we have a mount point for the bind mount below
     sudo mkdir -p "$DESTDIR"/boot/grub
@@ -77,16 +79,23 @@ if [ ! -d "$DST" ]; then
     exit 1
 fi
 
-# extract the base
+# This script is either used as part of an installer image which will have
+# a "base.squashfs". Here very little additional setup is needed or as part
+# of a spread test in which case the installer needs to prepare the system
+# to be used from spread. The "ROLE" var will be set accordingly so that
+# the "prepare_classic_rootfs" knows what to do.
+ROLE="unset"
 if [ -f /cdrom/casper/base.squashfs ]; then
     sudo unsquashfs -f -d "$DST" /cdrom/casper/base.squashfs
     # TODO: find out why the squashfs is preseeded
     /usr/lib/snapd/snap-preseed --reset "$DST"
+    ROLE=installer
 else
     BASETAR=ubuntu-base.tar.gz
     wget -c http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04.1-base-amd64.tar.gz -O "$BASETAR"
     sudo tar -C "$DST" -xf "$BASETAR"
+    ROLE=spread
 fi
 
 # create minimal rootfs
-create_classic_rootfs "$DST"
+prepare_classic_rootfs "$DST" "$ROLE"
