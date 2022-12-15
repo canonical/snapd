72157d60eb6c6d3a1bae153be975a73f167fcca5
Michael Vogt <mvo@ubuntu.com>
2022-12-15 15:37:58 +0100

    hookstate: add context from task to Errorf() (thanks to Samuele)

--- overlord/hookstate/context.go
+++ overlord/hookstate/context.go
@@ -302,19 +302,20 @@ func (c *Context) Logf(fmt string, args ...interface{}) {
 // ephemeral contexts or the task log.
 //
 // Context must be locked.
-func (c *Context) Errorf(fmt string, args ...interface{}) {
+func (c *Context) Errorf(fmts string, args ...interface{}) {
 	c.writing()
 	if c.IsEphemeral() {
 		// XXX: loger has no Errorf() :/
-		logger.Noticef(fmt, args...)
+		logger.Noticef(fmts, args...)
 	} else {
-		c.task.Errorf(fmt, args...)
+		c.task.Errorf(fmts, args...)
 		// If errors are ignored the task will not be in "Error"
 		// state so the error is hard to find. In this case also
 		// log errors to the journal to ensure that e.g. seeding
 		// configure errors are observable.
 		if c.setup != nil && c.setup.IgnoreError {
-			logger.Noticef(fmt, args...)
+			msg := fmt.Sprintf(fmts, args...)
+			logger.Noticef("ERROR task %v (%v): %v", c.task.ID(), c.task.Kind(), msg)
 		}
 	}
 }
--- overlord/hookstate/context_test.go
+++ overlord/hookstate/context_test.go
@@ -204,7 +204,7 @@ func (s *contextSuite) TestChangeErrorf(c *C) {
 		{nil, false, `.* context.go:.*: some error\n`, ``},
 		{nil, true, `.* context.go:.*: some error\n`, ``},
 		// ignore error hooks log errors to both logger and task
-		{task1, true, `.* context.go:.*: some error\n`, `.* ERROR some error`},
+		{task1, true, `.* context.go:.*: ERROR task ` + task1.ID() + ` \(foo1\): some error\n`, `.* ERROR some error`},
 		// normal hooks only log errors to the task
 		{task2, false, ``, `.* ERROR some error`},
 	} {
