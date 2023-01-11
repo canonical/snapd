8a8fabc9d1ffa2b6c0f26288921cdaf683024aa6
Alex Lewontin <alex.lewontin@canonical.com>
2023-01-11 11:11:33 -0500

    cmd/snap: introduce '--chain' flag for sign command
    This flag causes 'snap sign' to append the account-key and account
    assertions that correspond to the specified signing key to the signed
    output of the command. This is often critical for creating working
    system-user assertions, especially in air-gapped or offline situations.
    
    The '--chain' flag takes one of three optional arguments, 'remote',
    'direct', and 'local' which correspond to where it should look for the
    assertion. The default is 'remote'.
    
    Signed-off-by: Alex Lewontin <alex.lewontin@canonical.com>

--- cmd/snap/cmd_sign.go
+++ cmd/snap/cmd_sign.go
@@ -1,7 +1,7 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 
 /*
- * Copyright (C) 2014-2021 Canonical Ltd
+ * Copyright (C) 2014-2023 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -20,12 +20,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 
 	"github.com/jessevdk/go-flags"
 
+	"github.com/snapcore/snapd/asserts"
 	"github.com/snapcore/snapd/asserts/signtool"
+	"github.com/snapcore/snapd/client"
 	"github.com/snapcore/snapd/i18n"
 )
 
@@ -37,11 +40,13 @@ of the assertion can be specified through a "body" pseudo-header.
 `)
 
 type cmdSign struct {
+	clientMixin
 	Positional struct {
 		Filename flags.Filename
 	} `positional-args:"yes"`
 
 	KeyName keyName `short:"k" default:"default"`
+	Chain   string  `long:"chain" optional:"true" optional-value:"remote" choice:"remote" choice:"direct" choice:"local"`
 }
 
 func init() {
@@ -50,6 +55,8 @@ func init() {
 	}, map[string]string{
 		// TRANSLATORS: This should not start with a lowercase letter.
 		"k": i18n.G("Name of the key to use, otherwise use the default key"),
+		// TRANSLATORS: This should not start with a lowercase letter.
+		"chain": i18n.G("Append the account and account-key assertions necessary to allow any device to validate the signed assertion"),
 	}, []argDesc{{
 		// TRANSLATORS: This needs to begin with < and end with >
 		name: i18n.G("<filename>"),
@@ -100,9 +107,87 @@ func (x *cmdSign) Execute(args []string) error {
 		return err
 	}
 
-	_, err = Stdout.Write(encodedAssert)
+	outBuf := bytes.NewBuffer(nil)
+
+	_, err = outBuf.Write(encodedAssert)
+	if err != nil {
+		return err
+	}
+
+	if x.Chain != "" {
+		var known assertKnower
+
+		switch x.Chain {
+		case "remote":
+			known = knownRemoteWithFallback
+		case "direct":
+			known = func(_ *client.Client, assertTypeName string, headers map[string]string) ([]asserts.Assertion, error) {
+				return downloadAssertion(assertTypeName, headers)
+			}
+		case "local":
+			known = func(cl *client.Client, assertTypeName string, headers map[string]string) ([]asserts.Assertion, error) {
+				return cl.Known(assertTypeName, headers, nil)
+			}
+		default:
+			return fmt.Errorf("internal error: impossible value %q for --chain=", x.Chain)
+		}
+
+		// separate asserts with blank line
+		_, err := outBuf.Write([]byte("\n"))
+		if err != nil {
+			return err
+		}
+
+		accountKey, err := mustKnowOneAssert(known, x.client, "account-key", map[string]string{"public-key-sha3-384": privKey.PublicKey().ID()})
+		if err != nil {
+			return err
+		}
+		_, err = outBuf.Write(asserts.Encode(accountKey))
+		if err != nil {
+			return err
+		}
+
+		// separate asserts with blank line
+		_, err = outBuf.Write([]byte("\n"))
+		if err != nil {
+			return err
+		}
+
+		account, err := mustKnowOneAssert(known, x.client, "account", map[string]string{"account-id": accountKey.(*asserts.AccountKey).AccountID()})
+		if err != nil {
+			return err
+		}
+
+		_, err = outBuf.Write(asserts.Encode(account))
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = Stdout.Write(outBuf.Bytes())
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+type assertKnower func(cli *client.Client, assertTypeName string, headers map[string]string) ([]asserts.Assertion, error)
+
+// call this function in a way that is guaranteed to specify a unique assertion
+// (i.e. with a header specifying a value for the assertion's primary key)
+func mustKnowOneAssert(known assertKnower, cl *client.Client, assertType string, headers map[string]string) (asserts.Assertion, error) {
+	asserts, err := known(cl, assertType, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	switch len(asserts) {
+	case 0:
+		// TRANSLATORS: %s is the assertion-type
+		return nil, fmt.Errorf(i18n.G("cannot retrieve %s assertion"), assertType)
+	case 1:
+		return asserts[0], nil
+	default:
+		return nil, fmt.Errorf(i18n.G("internal error: cannot identify unique %s assertion for specified headers"), assertType)
+	}
 }
