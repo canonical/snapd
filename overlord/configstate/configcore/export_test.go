65bf582a8dec75db6a034077f80013739b852a94
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-01-20 14:38:11 +0000

    o/configcore: rename export file and fix comment

--- overlord/configstate/configcore/export_test.go
+++ overlord/configstate/configcore/export_test.go
@@ -39,7 +39,7 @@ type PlainCoreConfig = plainCoreConfig
 
 // MockFSOnlyRun is used for tests that run also when nomanagers flag is
 // set, that is, for config groups that do not need access to the
-// state.
+// state but only the filesystem.
 func MockFSOnlyRun(dev sysconfig.Device, cfg ConfGetter) error {
 	return filesystemOnlyRun(dev, cfg, nil)
 }
