7b8fd2fd8726e7adf749d7763246bdfd3d921c2f
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2023-05-16 20:21:49 +0100

    interfaces: Allow suppressing pycache deny rule
    Fully confined snaps, and docker containers, look very similar. As
    moveroot/pivotroot/chroot is changed, and apparmor confinement
    applied. Yet confined snaps constantly try to write to .pyc files of
    the base snap, whereas docker containers are trying to write .pyc
    files inside the child docker containers... and yet both are
    denied. Whilst former is undesired, the latter is very confusing.
    
    Attempt to address
    https://github.com/docker-snap/docker-snap/issues/81 without
    disrupting intent of LP: #1496895
    
    Introduce a new inline template replacement for pycache deny rules
    (such that all existing snippet tests remain intact), and make
    docker-support interface suppress pycache deny rules.

--- interfaces/apparmor/template.go
+++ interfaces/apparmor/template.go
@@ -113,13 +113,7 @@ var templateCommon = `
   #include <abstractions/python>
   /etc/python3.[0-9]*/**                                r,
 
-  # explicitly deny noisy denials to read-only filesystems (see LP: #1496895
-  # for details)
-  deny /usr/lib/python3*/{,**/}__pycache__/ w,
-  deny /usr/lib/python3*/{,**/}__pycache__/**.pyc.[0-9]* w,
-  # bind mount used here (see 'parallel installs', above)
-  deny @{INSTALL_DIR}/{@{SNAP_NAME},@{SNAP_INSTANCE_NAME}}/**/__pycache__/             w,
-  deny @{INSTALL_DIR}/{@{SNAP_NAME},@{SNAP_INSTANCE_NAME}}/**/__pycache__/*.pyc.[0-9]* w,
+  ###PYCACHEDENY###
 
   # for perl apps/services
   #include <abstractions/perl>
@@ -900,6 +894,16 @@ var ptraceTraceDenySnippet = `
 # dangerous access frivolously.
 deny ptrace (trace),
 deny capability sys_ptrace,
+`
+
+var pycacheDenySnippet = `
+# explicitly deny noisy denials to read-only filesystems (see LP: #1496895
+# for details)
+deny /usr/lib/python3*/{,**/}__pycache__/ w,
+deny /usr/lib/python3*/{,**/}__pycache__/**.pyc.[0-9]* w,
+# bind mount used here (see 'parallel installs', above)
+deny @{INSTALL_DIR}/{@{SNAP_NAME},@{SNAP_INSTANCE_NAME}}/**/__pycache__/             w,
+deny @{INSTALL_DIR}/{@{SNAP_NAME},@{SNAP_INSTANCE_NAME}}/**/__pycache__/*.pyc.[0-9]* w,
 `
 
 var sysModuleCapabilityDenySnippet = `
