canonical/x-go

randutil: make RandomKernelUUID() return an error
Make RandomKernelUUID() return an error instead of generating a panic. This
is better suited for this library function. The severity of the failure can
be determined by the application code.

randutil/crypto.go
@@ -52,10 +52,10 @@ func CryptoToken(nbytes int) (string, error) {
 // RandomKernelUUID will return a UUID from the kernel's procfs API at
 // /proc/sys/kernel/random/uuid. Only to be used in very specific uses, most
 // random code should use CryptoToken(Bytes) instead.
-func RandomKernelUUID() string {
+func RandomKernelUUID() (string, error) {
 	b, err := ioutil.ReadFile("/proc/sys/kernel/random/uuid")
 	if err != nil {
-		panic("cannot read kernel generated uuid")
+		return "", fmt.Errorf("cannot access kernel generated uuid: %w", err)
 	}
-	return strings.TrimSpace(string(b))
+	return strings.TrimSpace(string(b)), nil
 }

canonical/snapd

randutil/crypto.go
@@ -52,10 +52,10 @@ func CryptoToken(nbytes int) (string, error) {
 // RandomKernelUUID will return a UUID from the kernel's procfs API at
 // /proc/sys/kernel/random/uuid. Only to be used in very specific uses, most
 // random code should use CryptoToken(Bytes) instead.
-func RandomKernelUUID() string {
+func RandomKernelUUID() (string, error) {
 	b, err := ioutil.ReadFile("/proc/sys/kernel/random/uuid")
 	if err != nil {
-		panic("cannot read kernel generated uuid")
+		return "", fmt.Errorf("cannot read kernel generated uuid: %w", err)
 	}
-	return strings.TrimSpace(string(b))
+	return strings.TrimSpace(string(b)), nil
 }