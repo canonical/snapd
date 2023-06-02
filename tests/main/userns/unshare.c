ab56b9c9d9444cb683943c5eb0ccb9919c253d1c
Alex Murray <alex.murray@canonical.com>
2023-06-02 22:23:57 +0930

    tests/main/userns: support other platforms
    Some of the CLONE_NEWXXXX flags are quite new so only use these in the test
    unshare implementation if they are defined, plus adapt to arch's different
    naming of the nogroup group as nobody.
    
    Signed-off-by: Alex Murray <alex.murray@canonical.com>

--- tests/main/userns/unshare.c
+++ tests/main/userns/unshare.c
@@ -16,12 +16,16 @@ usage(char *pname)
 {
   fprintf(stderr, "Usage: %s [options] program [arg...]\n", pname);
   fprintf(stderr, "Options can be:\n");
+#ifdef CLONE_NEWCGROUP
   fprintf(stderr, "    -C   unshare cgroup namespace\n");
+#endif
   fprintf(stderr, "    -i   unshare IPC namespace\n");
   fprintf(stderr, "    -m   unshare mount namespace\n");
   fprintf(stderr, "    -n   unshare network namespace\n");
   fprintf(stderr, "    -p   unshare PID namespace\n");
+#ifdef CLONE_NEWTIME
   fprintf(stderr, "    -t   unshare time namespace\n");
+#endif
   fprintf(stderr, "    -u   unshare UTS namespace\n");
   fprintf(stderr, "    -U   unshare user namespace\n");
   exit(EXIT_FAILURE);
@@ -36,12 +40,16 @@ main(int argc, char *argv[])
 
   while ((opt = getopt(argc, argv, "CimnptuU")) != -1) {
     switch (opt) {
+#ifdef CLONE_NEWCGROUP
     case 'C': flags |= CLONE_NEWCGROUP;      break;
+#endif
     case 'i': flags |= CLONE_NEWIPC;        break;
     case 'm': flags |= CLONE_NEWNS;         break;
     case 'n': flags |= CLONE_NEWNET;        break;
     case 'p': flags |= CLONE_NEWPID;        break;
+#ifdef CLONE_NEWTIME
     case 't': flags |= CLONE_NEWTIME;        break;
+#endif
     case 'u': flags |= CLONE_NEWUTS;        break;
     case 'U': flags |= CLONE_NEWUSER;       break;
     default:  usage(argv[0]);
