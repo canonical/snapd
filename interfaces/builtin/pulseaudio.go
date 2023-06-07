a860e357c6e48e4a6037c38e6fff54d9b10749b5
Sergio Costas <sergio.costas@canonical.com>
2023-06-07 15:37:03 +0200

    interfaces/audio: remove write permissions in pulse folder (#12864)
    Currently, any application that connects to the audio-playback or
    the pulseaudio insterfaces, have write permissions to the pulse/
    folder. This means that a snapped malicious application would be
    able to delete the socket and put its own, being able to read the
    audio of any other snapped application.
    
    This patch removes those permissions.
    
    * Remove unneeded rules
    
    The "owner /run/pulse/native/ rwk" rule is already managed by a
    similar previous one. Also, there is no need to allow to link the
    pulse folder.
    
    Tested both in Firefox and Telegram, and everything still works
    fine.

--- interfaces/builtin/pulseaudio.go
+++ interfaces/builtin/pulseaudio.go
@@ -50,7 +50,7 @@ owner /{,var/}run/pulse/ r,
 owner /{,var/}run/pulse/native rwk,
 owner /{,var/}run/pulse/pid r,
 owner /{,var/}run/user/[0-9]*/ r,
-owner /{,var/}run/user/[0-9]*/pulse/ rw,
+owner /{,var/}run/user/[0-9]*/pulse/ r,
 
 /run/udev/data/c116:[0-9]* r,
 /run/udev/data/+sound:card[0-9]* r,
@@ -64,7 +64,7 @@ const pulseaudioConnectedPlugAppArmorDesktop = `
 /etc/pulse/** r,
 owner @{HOME}/.pulse-cookie rk,
 owner @{HOME}/.config/pulse/cookie rk,
-owner /{,var/}run/user/*/pulse/ rwk,
+owner /{,var/}run/user/*/pulse/ r,
 owner /{,var/}run/user/*/pulse/native rwk,
 owner /{,var/}run/user/*/pulse/pid r,
 `
@@ -111,7 +111,6 @@ owner /{,var/}run/pulse/** rwk,
 
 /usr/share/applications/ r,
 
-owner /run/pulse/native/ rwk,
 owner /run/user/[0-9]*/ r,
 owner /run/user/[0-9]*/pulse/ rw,
 `
