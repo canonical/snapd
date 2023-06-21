783e2be48288a9855c485a91d0d0dfcc34240889
Spyros Seimenis <spyros.seimenis@canonical.com>
2023-06-21 13:47:26 +0300

    snap/integrity: add functions for integrity data discovery and validation

--- snap/integrity/integrity.go
+++ snap/integrity/integrity.go
@@ -21,13 +21,19 @@ package integrity
 
 import (
 	"bytes"
+	"crypto"
+	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 
+	"github.com/snapcore/snapd/asserts"
 	"github.com/snapcore/snapd/logger"
+	"github.com/snapcore/snapd/osutil"
 	"github.com/snapcore/snapd/snap/integrity/dmverity"
+	"github.com/snapcore/snapd/snap/squashfs"
 )
 
 const (
@@ -36,6 +42,8 @@ const (
 	// the header size is always gonna be less than blockSize and will always get aligned
 	// to blockSize.
 	HeaderSize = 4096
+	// https://github.com/plougher/squashfs-tools/blob/master/squashfs-tools/squashfs_fs.h#L289
+	squashfsSuperblockBytesUsedOffset = 40
 )
 
 var (
@@ -46,6 +54,126 @@ var (
 // align aligns input `size` to closest `blockSize` value
 func align(size uint64) uint64 {
 	return (size + blockSize - 1) / blockSize * blockSize
+}
+
+// An IntegrityData structure represents a snap's integrity data and
+// contains information useful for locating it.
+type IntegrityData struct {
+	Header         *IntegrityDataHeader
+	Offset         uint64
+	SourceFilePath string
+}
+
+// FindIntegrityData returns integrity data information given a snap filename.
+// It currently supports only integrity data attached at the end of snap files.
+func FindIntegrityData(snapPath string) (*IntegrityData, error) {
+	integrityData := IntegrityData{}
+
+	snapFile, err := os.OpenFile(snapPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer snapFile.Close()
+
+	snapFileInfo, err := snapFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if !squashfs.FileHasSquashfsHeader(snapPath) {
+		return nil, errors.New("input file does not contain a SquashFS filesystem")
+	}
+
+	// Seek to bytes_used field of SquashFS superblock
+	_, err = snapFile.Seek(squashfsSuperblockBytesUsedOffset, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
+	var squashFSSize uint64
+	if err := binary.Read(snapFile, binary.LittleEndian, &squashFSSize); err != nil {
+		return nil, err
+	}
+
+	logger.Debugf("SquashFS bytes used: %d", squashFSSize)
+
+	// Align squashFSSize to blockSize
+	offset := align(squashFSSize)
+
+	if offset == uint64(snapFileInfo.Size()) {
+		return nil, fmt.Errorf("integrity data not found for snap %s", snapPath)
+	}
+
+	integrityData.SourceFilePath = snapPath
+	// TODO check for integrity data in separate file
+
+	_, err = snapFile.Seek(int64(offset), io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
+	integrityDataBytes := make([]byte, uint64(HeaderSize))
+	_, err = io.ReadFull(snapFile, integrityDataBytes)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read integrity data: %s", err)
+	}
+
+	integrityDataHeader, err := extractIntegrityDataHeader(integrityDataBytes)
+	if err != nil {
+		return nil, err
+	}
+	integrityData.Header = integrityDataHeader
+	integrityData.Offset = offset
+
+	return &integrityData, nil
+}
+
+// Validate checks integrity data against a snap-revision assertion.
+// The entire integrity data including the header gets hashed and compared to the hash
+// included in the integrity stanza in a snap-revision assertion.
+func (integrityData IntegrityData) Validate(snapRev asserts.SnapRevision) error {
+	integrityDataHash, err := integrityData.SHA3_384()
+	if err != nil {
+		return err
+	}
+
+	snapRevIntegrityData := snapRev.SnapIntegrity()
+	if snapRevIntegrityData == nil {
+		return fmt.Errorf("Snap revision assertion does not contain an integrity stanza")
+	}
+
+	if integrityDataHash != snapRevIntegrityData.SHA3_384 {
+		return fmt.Errorf("integrity data hash mismatch")
+	}
+	return nil
+}
+
+// extractIntegrityDataHeader parses a byte array containing integrity data header information
+// into an IntegrityDataHeader structure.
+func extractIntegrityDataHeader(integrityDataBytes []byte) (*IntegrityDataHeader, error) {
+	integrityDataHeader := IntegrityDataHeader{}
+
+	err := integrityDataHeader.Decode(integrityDataBytes[:HeaderSize])
+	if err != nil {
+		return nil, err
+	}
+
+	return &integrityDataHeader, nil
+}
+
+// SHA3_384 computes the SHA3-384 digest of the integrity data and encodes it in the hash format used
+// by snap assertions.
+func (integrityData IntegrityData) SHA3_384() (string, error) {
+	digest, _, err := osutil.PartialFileDigest(integrityData.SourceFilePath, crypto.SHA3_384, integrityData.Offset)
+	if err != nil {
+		return "", err
+	}
+
+	sha3_384, err := asserts.EncodeDigest(crypto.SHA3_384, digest)
+	if err != nil {
+		return "", fmt.Errorf("cannot encode snap's %q integrity data digest: %v", integrityData.SourceFilePath, err)
+	}
+	return sha3_384, nil
 }
 
 // IntegrityDataHeader gets appended first at the end of a squashfs packed snap
