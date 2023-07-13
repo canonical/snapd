275b07b4cc99e864ff1806f3631a1b835f8b33a2
Philip Meulengracht <the_meulengracht@hotmail.com>
2023-07-13 09:19:52 +0200

    osutil,tests: move runas code to own file
    Add additional testing to verify goroutines dont inherit from a locked thread

---
+++ tests/main/drop-privs/runas-2/runas-verify-thread-locked.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"syscall"
+
+	"github.com/snapcore/snapd/osutil/sys"
+)
+
+var wg sync.WaitGroup
+var mu sync.Mutex
+
+func check(tids []int, uids []sys.UserID, n int) {
+	// spin
+	for i := 0; i < 1<<30; i++ {
+	}
+
+	mu.Lock()
+	tids[n] = syscall.Gettid()
+	uids[n] = sys.Geteuid()
+	mu.Unlock()
+
+	wg.Done()
+}
+
+func main() {
+	N := 2 * runtime.NumCPU()
+	tids := make([]int, N)
+	uids := make([]sys.UserID, N)
+	err := sys.RunAsUidGid(12345, 12345, func() error {
+		// running in a locked os thread, get the ID
+		orig := syscall.Gettid()
+
+		// launch a lot of goroutines so we cover all threads with space to spare
+		for i := 0; i < N; i++ {
+			wg.Add(1)
+			go check(tids, uids, i)
+		}
+		wg.Wait()
+
+		// now verify all go-routines ran on a different thread as the one we are on
+		var badTids int
+		for _, tid := range tids {
+			if tid == orig {
+				badTids++
+			}
+		}
+
+		// verify that uid/gid was not inheritted
+		var badUids int
+		for _, uid := range uids {
+			if uid == 12345 {
+				badUids++
+			}
+		}
+
+		return fmt.Errorf("bad tids: %d, bad uids: %d", badTids, badUids)
+	})
+	fmt.Println(err)
+}
