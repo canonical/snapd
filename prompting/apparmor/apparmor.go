01a43a04d83f03ed7d567c474368acbf19decc3a
Oliver Calder <oliver.calder@canonical.com>
2023-07-25 23:05:31 -0500

    prompting/apparmor: added notifier path (from e69c5efe02f605338d2ce90e29d8847f27fc57f5)
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- prompting/apparmor/apparmor.go
+++ prompting/apparmor/apparmor.go
@@ -1,2 +1,16 @@
 // Package apparmor implements high-level interface to a subset of AppArmor features
 package apparmor
+
+import (
+	"github.com/snapcore/snapd/osutil"
+)
+
+const notifyPath = "/sys/kernel/security/apparmor/.notify"
+
+func PromptingAvailable() bool {
+	return osutil.FileExists(notifyPath)
+}
+
+func NotifyPath() string {
+	return notifyPath
+}
