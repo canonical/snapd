684637dbae707e17f5a689e592b97259c64d6eca
Oliver Calder <oliver.calder@canonical.com>
2023-07-31 11:18:29 -0500

    prompting/apparmor: improved `MockSyscall()` signature
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- prompting/apparmor/export_test.go
+++ prompting/apparmor/export_test.go
@@ -4,8 +4,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func MockSyscall(syscall func(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err unix.Errno)) func() {
+func MockSyscall(syscall func(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err unix.Errno)) (restore func()) {
 	old := doSyscall
 	doSyscall = syscall
-	return func() { doSyscall = old }
+	restore = func() { doSyscall = old }
+	return restore
 }
