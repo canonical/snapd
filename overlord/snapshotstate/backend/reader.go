5de2d83fa52937acd5761c278f9ff64b95f9b928
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-08-02 16:56:48 +0100

    WIP, we need to drop namespace

--- overlord/snapshotstate/backend/reader.go
+++ overlord/snapshotstate/backend/reader.go
@@ -35,6 +35,7 @@ import (
 
 	"github.com/snapcore/snapd/client"
 	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/interfaces/mount"
 	"github.com/snapcore/snapd/jsonutil"
 	"github.com/snapcore/snapd/logger"
 	"github.com/snapcore/snapd/osutil"
@@ -372,6 +373,12 @@ func (r *Reader) Restore(ctx context.Context, current snap.Revision, usernames [
 
 		sz.Reset()
 		hasher.Reset()
+	}
+
+	// We need to discard the consumer namespace!!
+	logger.Noticef("discarding namespace for %q", r.Snap)
+	if err := mount.DiscardSnapNamespace("test-cons"); err != nil {
+		return rs, fmt.Errorf("cannot discard namespace: %v", err)
 	}
 
 	return rs, nil
