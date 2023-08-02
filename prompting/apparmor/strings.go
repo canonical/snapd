5111b471a861345c9bbcf1be492d5bc3e649d1e1
Oliver Calder <oliver.calder@canonical.com>
2023-08-02 16:15:43 -0500

    prompting/apparmor: refactored stringPacker to improve ergonomics
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- prompting/apparmor/strings.go
+++ prompting/apparmor/strings.go
@@ -2,6 +2,7 @@ package apparmor
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -9,13 +10,21 @@ import (
 // variable length string elements. It implements the static offset
 // and ensures that non-empty strings are properly terminated.
 type stringPacker struct {
-	BaseOffset uint16
-	Buffer     bytes.Buffer
+	baseOffset uint16
+	buffer     bytes.Buffer
+}
+
+// newStringPacker returns a new string packer for the given struct.
+// The base offset is set to be the size of the given struct.
+func newStringPacker(rawStruct any) stringPacker {
+	return stringPacker{
+		baseOffset: uint16(binary.Size(rawStruct)),
+	}
 }
 
 // PackString computes the layout of a string encoded in an apparmor message.
 // The return value is the offset of the beginning of the string relative to
-// the start of the fixed portion of the structure, captured by BaseOffset.
+// the start of the fixed portion of the structure, captured by baseOffset.
 //
 // Empty strings use a special encoding that requires no space and used a fixed
 // dummy offset of zero. The actual string is always nil-terminated, for
@@ -24,16 +33,34 @@ func (sp *stringPacker) PackString(s string) uint32 {
 	if s == "" {
 		return 0
 	}
-	offset := uint32(sp.Buffer.Len())
-	sp.Buffer.WriteString(s)
-	sp.Buffer.WriteRune(0)
-	return offset + uint32(sp.BaseOffset)
+	offset := uint32(sp.buffer.Len())
+	sp.buffer.WriteString(s)
+	sp.buffer.WriteRune(0)
+	return offset + uint32(sp.baseOffset)
+}
+
+// TotalLen returns the total length of the data which is being packed,
+// equal to the base offset plus the length of the data buffer.
+func (sp *stringPacker) TotalLen() uint16 {
+	return sp.baseOffset + uint16(sp.buffer.Len())
+}
+
+// Bytes returns the underlying byte array into which data base been packed.
+func (sp *stringPacker) Bytes() []byte {
+	return sp.buffer.Bytes()
 }
 
 // stringUnpacker assists in unpacking apparmor data structures with
 // variable length string elements.
 type stringUnpacker struct {
 	Bytes []byte
+}
+
+// newStringUnpacker returns a new string unpacker for the given data.
+func newStringUnpacker(data []byte) stringUnpacker {
+	return stringUnpacker{
+		Bytes: data,
+	}
 }
 
 // UnpackString unpacks NUL-terminated string at a given offset into the buffer.
