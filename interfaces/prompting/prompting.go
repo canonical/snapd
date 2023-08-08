dbddf6bd3a669e72a22afa96c98362f76cbcc9f8
Oliver Calder <oliver.calder@canonical.com>
2023-08-07 20:49:51 -0500

    i/prompting: add package for prompting common types/functions
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: make `TimestampToTime()` return a local time, matching `time.Now()` and `time.Unix()`
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: make duration a string parsable by `time.ParseDuration()`
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: reorder functions
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: remove "app" from prompts and rules
    
    AppArmor permissions are granted by interfaces according to plugs/slots
    which may or may not differ between apps within a given snap. For the
    home interface, for example, there is never an instance where one app
    within a snap is granted a permission that another app in that same snap
    is not. Thus, it does not make sense for a rule to apply to only a
    single app within a snap.
    
    Instead, the interface field should optionally include the name of the
    plug or slot which granted the permissions, which should be received
    from the kernel using message tagging. The result should be of the form:
    
    `<interface name>[/<plug or slot>]`
    
    As such, rules should no longer be associated with a particular app, and
    in order to match, neither should request prompts. Adjustments are made
    to the prompting API to remove the "app" parameter when querying rules.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: add validation functions for expiration
    
    Adds expiration-related analogues for `ValidateLifespanParseDuration`
    and `ValidateConstraintsOutcomeLifespanDuration`.
    
    Additionally, improves error messages by including the contents of the
    invalid field in the message.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    i/prompting: move prompting common types/functions to interfaces/prompting
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

---
+++ interfaces/prompting/prompting.go
@@ -0,0 +1,189 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package prompting
+
+import (
+	"encoding/base32"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+var (
+	ErrInvalidSnapLabel = errors.New("the given label cannot be converted to snap")
+)
+
+type OutcomeType string
+
+const (
+	OutcomeUnset OutcomeType = ""
+	OutcomeAllow OutcomeType = "allow"
+	OutcomeDeny  OutcomeType = "deny"
+)
+
+// AsBool returns the outcome as a boolean, or an error if it cannot be parsed.
+func (outcome OutcomeType) AsBool() (bool, error) {
+	switch outcome {
+	case OutcomeAllow:
+		return true, nil
+	case OutcomeDeny:
+		return false, nil
+	default:
+		return false, fmt.Errorf(`invalid outcome: must be %q or %q: %q`, OutcomeAllow, OutcomeDeny, outcome)
+	}
+}
+
+type LifespanType string
+
+const (
+	LifespanUnset    LifespanType = ""
+	LifespanForever  LifespanType = "forever"
+	LifespanSession  LifespanType = "session"
+	LifespanSingle   LifespanType = "single"
+	LifespanTimespan LifespanType = "timespan"
+)
+
+// TimestampToTime converts the given timestamp string to a time.Time in Local
+// time. The timestamp string is expected to be of the format time.RFC3339Nano.
+// If it cannot be parsed as such, returns an error.
+func TimestampToTime(timestamp string) (time.Time, error) {
+	t, err := time.Parse(time.RFC3339Nano, timestamp)
+	if err != nil {
+		return t, err
+	}
+	return t.Local(), nil
+}
+
+// CurrentTimestamp returns the current time as a time.RFC3339Nano string.
+func CurrentTimestamp() string {
+	return time.Now().Format(time.RFC3339Nano)
+}
+
+// NewID returns a new unique ID.
+//
+// The ID is the current unix time in nanoseconds encoded as base32.
+func NewID() string {
+	nowUnix := uint64(time.Now().UnixNano())
+	nowBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(nowBytes, nowUnix)
+	id := base32.StdEncoding.EncodeToString(nowBytes)
+	return id
+}
+
+// NewIDAndTimestamp returns a new unique ID and corresponding timestamp.
+//
+// The ID is the current unix time in nanoseconds encoded as a string in base32.
+// The timestamp is the same time, encoded as a string in time.RFC3339Nano.
+func NewIDAndTimestamp() (id string, timestamp string) {
+	now := time.Now()
+	nowUnix := uint64(now.UnixNano())
+	nowBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(nowBytes, nowUnix)
+	id = base32.StdEncoding.EncodeToString(nowBytes)
+	timestamp = now.Format(time.RFC3339Nano)
+	return id, timestamp
+}
+
+// LabelToSnap extracts the snap name from the given AppArmor label.
+//
+// If the label is not of the form 'snap.<snap>.<app>', returns an error, and
+// returns the label as the snap.
+func LabelToSnap(label string) (string, error) {
+	components := strings.Split(label, ".")
+	if len(components) != 3 || components[0] != "snap" {
+		return label, ErrInvalidSnapLabel
+	}
+	snap := components[1]
+	return snap, nil
+}
+
+// ValidateOutcome returns nil if the given outcome is valid, otherwise an error.
+func ValidateOutcome(outcome OutcomeType) error {
+	switch outcome {
+	case OutcomeAllow, OutcomeDeny:
+		return nil
+	default:
+		return fmt.Errorf(`invalid outcome: must be %q or %q: %q`, OutcomeAllow, OutcomeDeny, outcome)
+	}
+}
+
+// ValidateLifespanExpiration checks that the given lifespan is valid and that
+// the given expiration is valid for that lifespan.
+//
+// If the lifespan is LifespanTimespan LifespanTimespan, then expiration must
+// be a string parsable as time.Duration with RFC3339 format. Otherwise, it must
+// be empty. Returns an error if any of the above are invalid.
+func ValidateLifespanExpiration(lifespan LifespanType, expiration string, currTime time.Time) error {
+	switch lifespan {
+	case LifespanForever, LifespanSession, LifespanSingle:
+		if expiration != "" {
+			return fmt.Errorf(`invalid expiration: expiration must be empty when lifespan is %q, but received non-empty expiration: %q`, lifespan, expiration)
+		}
+	case LifespanTimespan:
+		if expiration == "" {
+			return fmt.Errorf(`invalid expiration: expiration must be non-empty when lifespan is %q, but received empty expiration`, lifespan)
+		}
+		parsedTime, err := time.Parse(time.RFC3339, expiration)
+		if err != nil {
+			return fmt.Errorf("invalid expiration: expiration not parsable as a time in RFC3339 format: %q", expiration)
+		}
+		if currTime.After(parsedTime) {
+			return fmt.Errorf("invalid expiration: expiration time has already passed: %q", expiration)
+		}
+	default:
+		return fmt.Errorf(`invalid lifespan: %q`, lifespan)
+	}
+	return nil
+}
+
+// ValidateLifespanParseDuration checks that the given lifespan is valid and
+// that the given duration is valid for that lifespan.
+//
+// If the lifespan is LifespanTimespan, then duration must be a string parsable
+// by time.ParseDuration(), representing the duration of time for which the rule
+// should be valid. Otherwise, it must be empty. Returns an error if any of the
+// above are invalid, otherwise computes the expiration time of the rule based
+// on the current time and the given duration and returns it.
+func ValidateLifespanParseDuration(lifespan LifespanType, duration string) (string, error) {
+	expirationString := ""
+	switch lifespan {
+	case LifespanForever, LifespanSession, LifespanSingle:
+		if duration != "" {
+			return "", fmt.Errorf(`invalid duration: duration must be empty when lifespan is %q, but received non-empty duration: %q`, lifespan, duration)
+		}
+	case LifespanTimespan:
+		if duration == "" {
+			return "", fmt.Errorf(`invalid duration: duration must be non-empty when lifespan is %q, but received empty expiration`, lifespan)
+		}
+		parsedDuration, err := time.ParseDuration(duration)
+		if err != nil {
+			return "", fmt.Errorf(`invalid duration: error parsing duration string: %q`, duration)
+		}
+		if parsedDuration <= 0 {
+			return "", fmt.Errorf(`invalid duration: duration must be greater than zero: %q`, duration)
+		}
+		expirationString = time.Now().Add(parsedDuration).Format(time.RFC3339)
+	default:
+		return "", fmt.Errorf(`invalid lifespan: %q`, lifespan)
+	}
+	return expirationString, nil
+}
---
+++ interfaces/prompting/prompting_test.go
@@ -0,0 +1,213 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package prompting_test
+
+import (
+	"encoding/base32"
+	"encoding/binary"
+	"testing"
+	"time"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/interfaces/prompting"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type promptingSuite struct {
+	tmpdir string
+}
+
+var _ = Suite(&promptingSuite{})
+
+func (s *promptingSuite) SetUpTest(c *C) {
+	s.tmpdir = c.MkDir()
+	dirs.SetRootDir(s.tmpdir)
+}
+
+func (s *promptingSuite) TestOutcomeAsBool(c *C) {
+	result, err := prompting.OutcomeAllow.AsBool()
+	c.Check(err, IsNil)
+	c.Check(result, Equals, true)
+	result, err = prompting.OutcomeDeny.AsBool()
+	c.Check(err, IsNil)
+	c.Check(result, Equals, false)
+	_, err = prompting.OutcomeUnset.AsBool()
+	c.Check(err, ErrorMatches, `invalid outcome.*`)
+	_, err = prompting.OutcomeType("foo").AsBool()
+	c.Check(err, ErrorMatches, `invalid outcome.*`)
+}
+
+func (s *promptingSuite) TestTimestamps(c *C) {
+	before := time.Now()
+	ts := prompting.CurrentTimestamp()
+	after := time.Now()
+	parsedTime, err := prompting.TimestampToTime(ts)
+	c.Assert(err, IsNil)
+	c.Assert(parsedTime.After(before), Equals, true)
+	c.Assert(parsedTime.Before(after), Equals, true)
+}
+
+func (s *promptingSuite) TestNewIDAndTimestamp(c *C) {
+	before := time.Now()
+	id := prompting.NewID()
+	idPaired, timestampPaired := prompting.NewIDAndTimestamp()
+	after := time.Now()
+	data1, err := base32.StdEncoding.DecodeString(id)
+	c.Assert(err, IsNil)
+	data2, err := base32.StdEncoding.DecodeString(idPaired)
+	c.Assert(err, IsNil)
+	parsedNs := int64(binary.BigEndian.Uint64(data1))
+	parsedNsPaired := int64(binary.BigEndian.Uint64(data2))
+	parsedTime := time.Unix(parsedNs/1000000000, parsedNs%1000000000)
+	parsedTimePaired := time.Unix(parsedNsPaired/1000000000, parsedNsPaired%1000000000)
+	c.Assert(parsedTime.After(before), Equals, true)
+	c.Assert(parsedTime.Before(after), Equals, true)
+	c.Assert(parsedTimePaired.After(before), Equals, true)
+	c.Assert(parsedTimePaired.Before(after), Equals, true)
+	parsedTimestamp, err := prompting.TimestampToTime(timestampPaired)
+	c.Assert(err, IsNil)
+	c.Assert(parsedTimePaired, Equals, parsedTimestamp)
+}
+
+func (s *promptingSuite) TestLabelToSnapHappy(c *C) {
+	cases := []struct {
+		label string
+		snap  string
+	}{
+		{
+			label: "snap.nextcloud.occ",
+			snap:  "nextcloud",
+		},
+		{
+			label: "snap.lxd.lxc",
+			snap:  "lxd",
+		},
+		{
+			label: "snap.firefox.firefox",
+			snap:  "firefox",
+		},
+	}
+	for _, testCase := range cases {
+		snap, err := prompting.LabelToSnap(testCase.label)
+		c.Check(err, IsNil)
+		c.Check(snap, Equals, testCase.snap)
+	}
+}
+
+func (s *promptingSuite) TestLabelToSnapUnhappy(c *C) {
+	cases := []string{
+		"snap",
+		"snap.nextcloud",
+		"nextcloud.occ",
+		"snap.nextcloud.nextcloud.occ",
+		"SNAP.NEXTCLOUD.OCC",
+	}
+	for _, label := range cases {
+		snap, err := prompting.LabelToSnap(label)
+		c.Check(err, Equals, prompting.ErrInvalidSnapLabel)
+		c.Check(snap, Equals, label)
+	}
+}
+
+func (s *promptingSuite) TestValidateOutcome(c *C) {
+	c.Assert(prompting.ValidateOutcome(prompting.OutcomeAllow), Equals, nil)
+	c.Assert(prompting.ValidateOutcome(prompting.OutcomeDeny), Equals, nil)
+	c.Assert(prompting.ValidateOutcome(prompting.OutcomeUnset), ErrorMatches, `invalid outcome.*`)
+	c.Assert(prompting.ValidateOutcome(prompting.OutcomeType("foo")), ErrorMatches, `invalid outcome.*`)
+}
+
+func (s *promptingSuite) TestValidateLifespanExpiration(c *C) {
+	unsetExpiration := ""
+	invalidExpiration := "foo"
+	currTime := time.Now()
+	negativeExpiration := currTime.Add(-5 * time.Second).Format(time.RFC3339)
+	validExpiration := currTime.Add(10 * time.Minute).Format(time.RFC3339)
+
+	for _, lifespan := range []prompting.LifespanType{
+		prompting.LifespanForever,
+		prompting.LifespanSession,
+		prompting.LifespanSingle,
+	} {
+		err := prompting.ValidateLifespanExpiration(lifespan, unsetExpiration, currTime)
+		c.Check(err, IsNil)
+		for _, exp := range []string{invalidExpiration, negativeExpiration, validExpiration} {
+			err = prompting.ValidateLifespanExpiration(lifespan, exp, currTime)
+			c.Check(err, ErrorMatches, `invalid expiration: expiration must be empty.*`)
+		}
+	}
+
+	err := prompting.ValidateLifespanExpiration(prompting.LifespanTimespan, unsetExpiration, currTime)
+	c.Check(err, ErrorMatches, `invalid expiration: expiration must be non-empty.*`)
+
+	err = prompting.ValidateLifespanExpiration(prompting.LifespanTimespan, invalidExpiration, currTime)
+	c.Check(err, ErrorMatches, `invalid expiration: expiration not parsable.*`)
+
+	err = prompting.ValidateLifespanExpiration(prompting.LifespanTimespan, negativeExpiration, currTime)
+	c.Check(err, ErrorMatches, `invalid expiration: expiration time has already passed.*`)
+
+	err = prompting.ValidateLifespanExpiration(prompting.LifespanTimespan, validExpiration, currTime)
+	c.Check(err, IsNil)
+}
+
+func (s *promptingSuite) TestValidateLifespanParseDuration(c *C) {
+	unsetDuration := ""
+	invalidDuration := "foo"
+	negativeDuration := "-5s"
+	validDuration := "10m"
+	parsedValidDuration, err := time.ParseDuration(validDuration)
+	c.Assert(err, IsNil)
+
+	for _, lifespan := range []prompting.LifespanType{
+		prompting.LifespanForever,
+		prompting.LifespanSession,
+		prompting.LifespanSingle,
+	} {
+		expiration, err := prompting.ValidateLifespanParseDuration(lifespan, unsetDuration)
+		c.Check(expiration, Equals, "")
+		c.Check(err, IsNil)
+		for _, dur := range []string{invalidDuration, negativeDuration, validDuration} {
+			expiration, err = prompting.ValidateLifespanParseDuration(lifespan, dur)
+			c.Check(expiration, Equals, "")
+			c.Check(err, ErrorMatches, `invalid duration: duration must be empty.*`)
+		}
+	}
+
+	expiration, err := prompting.ValidateLifespanParseDuration(prompting.LifespanTimespan, unsetDuration)
+	c.Check(expiration, Equals, "")
+	c.Check(err, ErrorMatches, `invalid duration: duration must be non-empty.*`)
+
+	expiration, err = prompting.ValidateLifespanParseDuration(prompting.LifespanTimespan, invalidDuration)
+	c.Check(expiration, Equals, "")
+	c.Check(err, ErrorMatches, `invalid duration: error parsing duration string.*`)
+
+	expiration, err = prompting.ValidateLifespanParseDuration(prompting.LifespanTimespan, negativeDuration)
+	c.Check(expiration, Equals, "")
+	c.Check(err, ErrorMatches, `invalid duration: duration must be greater than zero.*`)
+
+	expiration, err = prompting.ValidateLifespanParseDuration(prompting.LifespanTimespan, validDuration)
+	c.Check(err, IsNil)
+	expirationTime, err := time.Parse(time.RFC3339, expiration)
+	c.Check(err, IsNil)
+	c.Check(expirationTime.After(time.Now()), Equals, true)
+	c.Check(expirationTime.Before(time.Now().Add(parsedValidDuration)), Equals, true)
+}
