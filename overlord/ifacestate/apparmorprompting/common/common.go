11d4c0a20b35ee55a681ea0ff81a3f737f07a598
Oliver Calder <oliver.calder@canonical.com>
2023-08-09 10:00:48 -0500

    o/ifacestate: abstracted path pattern matching to function in `common`
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- overlord/ifacestate/apparmorprompting/common/common.go
+++ overlord/ifacestate/apparmorprompting/common/common.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	doublestar "github.com/bmatcuk/doublestar/v4"
 	"github.com/snapcore/snapd/prompting/apparmor"
 )
 
@@ -68,7 +69,7 @@ const (
 // If the given permission is not found in the list, returns an error, along
 // with the original list.
 func RemovePermissionFromList(list []PermissionType, permission PermissionType) ([]PermissionType, error) {
-	newList := make([]PermissionType, 0, len(list)-1)
+	newList := make([]PermissionType, 0, len(list))
 	found := false
 	for _, perm := range list {
 		if perm == permission {
@@ -325,4 +326,8 @@ func GetHighestPrecedencePattern(patterns []string) (string, error) {
 		}
 	}
 	return shortestPattern, nil
+}
+
+func PathPatternMatches(pathPattern string, path string) (bool, error) {
+	return doublestar.Match(pathPattern, path)
 }
