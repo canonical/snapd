8fe3baa39f031b0ddfa8cd45365a6602e255cdae
Michael Vogt <mvo@ubuntu.com>
2023-08-14 20:27:49 +0200

    snap-confine: extract must_read_and_validate_header_from_file_dies_with() helper

--- cmd/snap-confine/seccomp-support-test.c
+++ cmd/snap-confine/seccomp-support-test.c
@@ -21,7 +21,8 @@
 #include <glib.h>
 #include <glib/gstdio.h>
 
-static void make_seccomp_profile(struct sc_seccomp_file_header *hdr, int *fd, char **fname)
+static void make_seccomp_profile(struct sc_seccomp_file_header *hdr, int *fd,
+				 char **fname)
 {
 	*fd = g_file_open_tmp(NULL, fname, NULL);
 	g_assert_true(*fd > 0);
@@ -36,148 +37,121 @@ static void test_must_read_and_validate_header_from_file__happy(void)
 		.header[1] = 'C',
 		.version = 1,
 	};
-	char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
+	char SC_CLEANUP(sc_cleanup_string) * profile = NULL;
 	int SC_CLEANUP(sc_cleanup_close) fd = 0;
 	make_seccomp_profile(&hdr, &fd, &profile);
 
-	FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
+	FILE *SC_CLEANUP(sc_cleanup_file) file =
+	    sc_must_read_and_validate_header_from_file(profile, &hdr);
 	g_assert_true(file != NULL);
+}
+
+static void must_read_and_validate_header_from_file_dies_with(struct sc_seccomp_file_header hdr, const char *err_msg)
+{
+	if (g_test_subprocess()) {
+		char SC_CLEANUP(sc_cleanup_string) * profile = NULL;
+		int SC_CLEANUP(sc_cleanup_close) fd = 0;
+		make_seccomp_profile(&hdr, &fd, &profile);
+
+		FILE *SC_CLEANUP(sc_cleanup_file) file =
+		    sc_must_read_and_validate_header_from_file(profile, &hdr);
+		// the function above is expected to call die()
+		g_assert_not_reached();
+		// reference "file" to keep the compiler from warning
+		// that "file" is unused
+		g_assert_null(file);
+	}
+
+	g_test_trap_subprocess(NULL, 0, 0);
+	g_test_trap_assert_failed();
+	g_test_trap_assert_stderr(err_msg);
 }
 
 static void test_must_read_and_validate_header_from_file__invalid_header(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {0};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("unexpected seccomp header: 00\n");
+	struct sc_seccomp_file_header hdr = { 0 };
+	const char *expected_err = "unexpected seccomp header: 00\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
 static void test_must_read_and_validate_header_from_file__invalid_version(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {
-			.header[0] = 'S',
-			.header[1] = 'C',
-			.version = 0,
-		};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("unexpected seccomp file version: 0\n");
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 0,
+	};
+	const char *expected_err = "unexpected seccomp file version: 0\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
-static void test_must_read_and_validate_header_from_file__len_allow_too_big(void)
+static void
+test_must_read_and_validate_header_from_file__len_allow_too_big(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {
-			.header[0] = 'S',
-			.header[1] = 'C',
-			.version = 1,
-			.len_allow_filter = MAX_BPF_SIZE + 1,
-		};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("allow filter size too big 32769\n");
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_allow_filter = MAX_BPF_SIZE + 1,
+	};
+	const char *expected_err = "allow filter size too big 32769\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
-static void test_must_read_and_validate_header_from_file__len_allow_no_multiplier(void)
+static void
+test_must_read_and_validate_header_from_file__len_allow_no_multiplier(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {
-			.header[0] = 'S',
-			.header[1] = 'C',
-			.version = 1,
-			.len_allow_filter = sizeof(struct sock_filter) + 1,
-		};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("allow filter size not multiple of sock_filter\n");
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_allow_filter = sizeof(struct sock_filter) + 1,
+	};
+	const char *expected_err =
+	    "allow filter size not multiple of sock_filter\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
 static void test_must_read_and_validate_header_from_file__len_deny_too_big(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {
-			.header[0] = 'S',
-			.header[1] = 'C',
-			.version = 1,
-			.len_deny_filter = MAX_BPF_SIZE + 1,
-		};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("deny filter size too big 32769\n");
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_deny_filter = MAX_BPF_SIZE + 1,
+	};
+	const char *expected_err = "deny filter size too big 32769\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
-static void test_must_read_and_validate_header_from_file__len_deny_no_multiplier(void)
+static void
+test_must_read_and_validate_header_from_file__len_deny_no_multiplier(void)
 {
-	if (g_test_subprocess()) {
-		struct sc_seccomp_file_header hdr = {
-			.header[0] = 'S',
-			.header[1] = 'C',
-			.version = 1,
-			.len_deny_filter = sizeof(struct sock_filter) + 1,
-		};
-		char SC_CLEANUP(sc_cleanup_string) *profile = NULL;
-		int SC_CLEANUP(sc_cleanup_close) fd = 0;
-		make_seccomp_profile(&hdr, &fd, &profile);
-		
-		FILE* SC_CLEANUP(sc_cleanup_file) file = sc_must_read_and_validate_header_from_file(profile, &hdr);
-	}
-
-	g_test_trap_subprocess(NULL, 0, 0);
-	g_test_trap_assert_failed();
-	g_test_trap_assert_stderr("deny filter size not multiple of sock_filter\n");
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_deny_filter = sizeof(struct sock_filter) + 1,
+	};
+	const char *expected_err =
+	    "deny filter size not multiple of sock_filter\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
 }
 
 static void __attribute__((constructor)) init(void)
 {
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/happy",
-			test_must_read_and_validate_header_from_file__happy);
+	     test_must_read_and_validate_header_from_file__happy);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/invalid_header",
-			test_must_read_and_validate_header_from_file__invalid_header);
+	     test_must_read_and_validate_header_from_file__invalid_header);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/invalid_version",
-			test_must_read_and_validate_header_from_file__invalid_version);
+	     test_must_read_and_validate_header_from_file__invalid_version);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/len_allow_too_big",
-			test_must_read_and_validate_header_from_file__len_allow_too_big);
+	     test_must_read_and_validate_header_from_file__len_allow_too_big);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/len_allow_no_multiplier",
-			test_must_read_and_validate_header_from_file__len_allow_no_multiplier);
+	     test_must_read_and_validate_header_from_file__len_allow_no_multiplier);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/len_deny_too_big",
-			test_must_read_and_validate_header_from_file__len_deny_too_big);
+	     test_must_read_and_validate_header_from_file__len_deny_too_big);
 	g_test_add_func("/seccomp/must_read_and_validate_header_from_file/len_deny_no_multiplier",
-			test_must_read_and_validate_header_from_file__len_deny_no_multiplier);
+	     test_must_read_and_validate_header_from_file__len_deny_no_multiplier);
 }
