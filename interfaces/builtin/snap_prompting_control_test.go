43b4c1eddfe50af048997939ed882e3738884968
Robert Ancell <robert.ancell@canonical.com>
2023-08-25 14:15:21 +1200

    interfaces: Add snap-prompting-control interface
    tests: add `snap-prompting-control` interface to list of tests
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    interfaces/policy: add `snap-prompting-control` to the list of interfaces which have both slots and plugs
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

---
+++ interfaces/builtin/snap_prompting_control_test.go
@@ -0,0 +1,106 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2023 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type SnapPromptingInterfaceSuite struct {
+	iface    interfaces.Interface
+	slotInfo *snap.SlotInfo
+	slot     *interfaces.ConnectedSlot
+	plugInfo *snap.PlugInfo
+	plug     *interfaces.ConnectedPlug
+}
+
+var _ = Suite(&SnapPromptingInterfaceSuite{
+	iface: builtin.MustInterface("snap-prompting-control"),
+})
+
+func (s *SnapPromptingInterfaceSuite) SetUpTest(c *C) {
+	const coreSlotYaml = `
+name: core
+type: os
+version: 1.0
+slots:
+  snap-prompting-control:
+`
+	s.slot, s.slotInfo = MockConnectedSlot(c, coreSlotYaml, nil, "snap-prompting-control")
+
+	const appPlugYaml = `
+name: other
+version: 0
+apps:
+ app:
+    command: foo
+    plugs: [snap-prompting-control]
+`
+	s.plug, s.plugInfo = MockConnectedPlug(c, appPlugYaml, nil, "snap-prompting-control")
+}
+
+func (s *SnapPromptingInterfaceSuite) TestName(c *C) {
+	c.Check(s.iface.Name(), Equals, "snap-prompting-control")
+}
+
+func (s *SnapPromptingInterfaceSuite) TestSanitizeSlot(c *C) {
+	c.Check(interfaces.BeforePrepareSlot(s.iface, s.slotInfo), IsNil)
+}
+
+func (s *SnapPromptingInterfaceSuite) TestSanitizePlug(c *C) {
+	c.Check(interfaces.BeforePreparePlug(s.iface, s.plugInfo), IsNil)
+}
+
+func (s *SnapPromptingInterfaceSuite) TestAppArmor(c *C) {
+	// The interface generates no AppArmor rules
+	appSet, err := interfaces.NewSnapAppSet(s.plug.Snap(), nil)
+	c.Assert(err, IsNil)
+	spec := apparmor.NewSpecification(appSet)
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Check(spec.SecurityTags(), HasLen, 0)
+
+	appSet, err = interfaces.NewSnapAppSet(s.plug.Snap(), nil)
+	c.Assert(err, IsNil)
+	spec = apparmor.NewSpecification(appSet)
+	c.Assert(spec.AddConnectedSlot(s.iface, s.plug, s.slot), IsNil)
+	c.Check(spec.SecurityTags(), HasLen, 0)
+
+	appSet, err = interfaces.NewSnapAppSet(s.plug.Snap(), nil)
+	c.Assert(err, IsNil)
+	spec = apparmor.NewSpecification(appSet)
+	c.Assert(spec.AddPermanentPlug(s.iface, s.plugInfo), IsNil)
+	c.Check(spec.SecurityTags(), HasLen, 0)
+
+	appSet, err = interfaces.NewSnapAppSet(s.plug.Snap(), nil)
+	c.Assert(err, IsNil)
+	spec = apparmor.NewSpecification(appSet)
+	c.Assert(spec.AddPermanentSlot(s.iface, s.slotInfo), IsNil)
+	c.Check(spec.SecurityTags(), HasLen, 0)
+}
+
+func (s *SnapPromptingInterfaceSuite) TestInterfaces(c *C) {
+	c.Check(builtin.Interfaces(), testutil.DeepContains, s.iface)
+}
