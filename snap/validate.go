0815f66dd9b04876161b2cfe54d10b8092514ea2
Ernest Lotter <ernest.lotter@canonical.com>
2023-09-06 11:47:56 +0200

    snap, snap/pack: add pack validation for default-configure hook (#13097)
    * snap/pack: enhance snap pack to validate all container info
    
    * snap: enhanced snap info validation to flag default-configure without configure hook
    
    * snap: fixed implicit hook test
    
    * snap, snap/pack: added unit tests
    
    * snap/pack: add additional unit tests
    
    * snap: revert back to not using ReadInfoFromSnapFile

--- snap/validate.go
+++ snap/validate.go
@@ -162,6 +162,22 @@ func ValidateLicense(license string) error {
 	return nil
 }
 
+func validateHooks(info *Info) error {
+	for _, hook := range info.Hooks {
+		if err := ValidateHook(hook); err != nil {
+			return err
+		}
+	}
+
+	hasDefaultConfigureHook := info.Hooks["default-configure"] != nil
+	hasConfigureHook := info.Hooks["configure"] != nil
+	if hasDefaultConfigureHook && !hasConfigureHook {
+		return fmt.Errorf(`cannot specify "default-configure" hook without "configure" hook`)
+	}
+
+	return nil
+}
+
 // ValidateHook validates the content of the given HookInfo
 func ValidateHook(hook *HookInfo) error {
 	if err := naming.ValidateHook(hook.Name); err != nil {
@@ -368,11 +384,9 @@ func Validate(info *Info) error {
 		}
 	}
 
-	// validate hook entries
-	for _, hook := range info.Hooks {
-		if err := ValidateHook(hook); err != nil {
-			return err
-		}
+	// Validate hook entries
+	if err := validateHooks(info); err != nil {
+		return err
 	}
 
 	// Ensure that plugs and slots have appropriate names and interface names.
