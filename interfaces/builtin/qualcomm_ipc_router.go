6cd2aa71d1381bf87b1e3816344bdb2b02fc5be2
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-09-13 16:48:55 +0100

    interfaces/qualcomm-ipc-router: split in slot and plug
    Split permissions between a slot and a plug that can both be provided
    by a snap, but still allow to connect to a system provided slot.

--- interfaces/builtin/qualcomm_ipc_router.go
+++ interfaces/builtin/qualcomm_ipc_router.go
@@ -1,7 +1,7 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 
 /*
- * Copyright (C) 2021 Canonical Ltd
+ * Copyright (C) 2021-2023 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -28,6 +28,7 @@ import (
 	"github.com/snapcore/snapd/interfaces/seccomp"
 	apparmor_sandbox "github.com/snapcore/snapd/sandbox/apparmor"
 	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/snap/naming"
 	"github.com/snapcore/snapd/strutil"
 )
 
@@ -46,7 +47,23 @@ const qipcrtrBaseDeclarationSlots = `
       slot-snap-type:
         - app
         - core
-    deny-auto-connection: true
+    allow-connection:
+      -
+        plug-attributes:
+          qcipc: $SLOT(qcipc)
+      -
+        plug-attributes:
+          qcipc: $MISSING
+        slot-snap-type:
+          - core
+    allow-auto-connection:
+      plug-publisher-id:
+        - $SLOT_PUBLISHER_ID
+      plug-attributes:
+        qcipc: $SLOT(qcipc)
+    deny-auto-connection:
+      plug-attributes:
+        qcipc: $MISSING
 `
 
 const qipcrtrConnectedPlugAppArmorCompat = `
@@ -63,7 +80,7 @@ const qipcrtrConnectedPlugAppArmor = `
 #              and limits to sock_dgram only
 network qipcrtr,
 
-unix (connect, send, receive) type=seqpacket addr="@**" peer=(label=###SLOT_SECURITY_TAGS###),
+unix (connect, send, receive) type=seqpacket addr="###SOCKET_ADDRESS###" peer=(label=###SLOT_SECURITY_TAGS###),
 `
 
 const qipcrtrPermanentSlotAppArmor = `
@@ -75,11 +92,11 @@ network qipcrtr,
 capability net_admin,
 capability net_bind_service,
 
-unix (bind, listen) type=seqpacket addr="@**",
+unix (bind, listen) type=seqpacket addr="###SOCKET_ADDRESS###",
 `
 
 const qipcrtrConnectedSlotAppArmor = `
-unix (accept, send, receive) type=seqpacket addr="@**" peer=(label=###PLUG_SECURITY_TAGS###),
+unix (accept, send, receive) type=seqpacket addr="###SOCKET_ADDRESS###" peer=(label=###PLUG_SECURITY_TAGS###),
 `
 
 const qipcrtrConnectedPlugSecCompCompat = `
@@ -101,6 +118,13 @@ listen
 #socket AF_QIPCRTR
 `
 
+const (
+	// qcipcAttrib is a tag that must match between plug and slot
+	qcipcAttrib = "qcipc"
+	// addressAttrib is the allowed socket address and is specified in the slot
+	addressAttrib = "address"
+)
+
 type qualcomIPCRouterInterface struct {
 	commonInterface
 }
@@ -120,39 +144,127 @@ func (iface *qualcomIPCRouterInterface) StaticInfo() interfaces.StaticInfo {
 	}
 }
 
+func fillSnippetSocketAddress(slot *interfaces.ConnectedSlot, snippet string) (string, error) {
+	var address string
+	if err := slot.Attr(addressAttrib, &address); err != nil {
+		return "", err
+	}
+
+	return strings.ReplaceAll(snippet, "###SOCKET_ADDRESS###", address), nil
+}
+
+// Note that we cannot use implicitSystemConnectedSlot as legacy slots can be
+// for UC too in this case.
+// TODO replace with implicitSystemConnectedSlot when
+// https://github.com/snapcore/snapd/pull/13194 lands
+func isConnectedSlotSystem(slot *interfaces.ConnectedSlot) bool {
+	if slot.Snap().Type() == snap.TypeOS || slot.Snap().Type() == snap.TypeSnapd {
+		return true
+	}
+	return false
+}
+
+func isSlotInfoSystem(slot *snap.SlotInfo) bool {
+	if slot.Snap.SnapType == snap.TypeOS || slot.Snap.SnapType == snap.TypeSnapd {
+		return true
+	}
+	return false
+}
+
+func validateTag(qcipc string) error {
+	if len(qcipc) == 0 {
+		return fmt.Errorf("qualcomm-ipc-router %s attribute cannot be empty", qcipcAttrib)
+	}
+	if err := naming.ValidateIfaceTag(qcipc); err != nil {
+		return fmt.Errorf("bad name for %s attribute: %v", qcipcAttrib, err)
+	}
+	return nil
+}
+
+func validateAddress(address string) error {
+	if len(address) == 0 {
+		return fmt.Errorf("qualcomm-ipc-router %s attribute cannot be empty", qcipcAttrib)
+	}
+	// do not allow apparmor RE in the address
+	if err := apparmor.ValidateNoAppArmorRegexp(address); err != nil {
+		return fmt.Errorf(`address is invalid: %v`, err)
+	}
+	return nil
+}
+
 func (iface *qualcomIPCRouterInterface) AppArmorConnectedPlug(spec *apparmor.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
-	if implicitSystemConnectedSlot(slot) {
+	// The qcipc attribute for the plug must be checked for here,
+	// as in BeforePreparePlug we do not know yet if we are
+	// connecting to a system or app provided slot.
+	var qcipc string
+	err := plug.Attr(qcipcAttrib, &qcipc)
+	if isConnectedSlotSystem(slot) {
+		if err == nil {
+			return fmt.Errorf("%q attribute not allowed if connecting to a system slot", qcipcAttrib)
+		}
 		// backward compatibility
 		spec.AddSnippet(qipcrtrConnectedPlugAppArmorCompat)
 	} else {
+		// Plug must have qcipc if connecting to app slot
+		if err != nil {
+			return err
+		}
+		if err := validateTag(qcipc); err != nil {
+			return err
+		}
+
 		old := "###SLOT_SECURITY_TAGS###"
-		new := slotAppLabelExpr(slot)
-		snippet := strings.Replace(qipcrtrConnectedPlugAppArmor, old, new, -1)
+		slotLabel := slotAppLabelExpr(slot)
+		snippet := strings.ReplaceAll(qipcrtrConnectedPlugAppArmor, old, slotLabel)
+		var err error
+		if snippet, err = fillSnippetSocketAddress(slot, snippet); err != nil {
+			return err
+		}
 		spec.AddSnippet(snippet)
 	}
 	return nil
 }
 
 func (iface *qualcomIPCRouterInterface) AppArmorConnectedSlot(spec *apparmor.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
+	if isConnectedSlotSystem(slot) {
+		return nil
+	}
+
 	old := "###PLUG_SECURITY_TAGS###"
 	new := plugAppLabelExpr(plug)
-	snippet := strings.Replace(qipcrtrConnectedSlotAppArmor, old, new, -1)
+	snippet := strings.ReplaceAll(qipcrtrConnectedSlotAppArmor, old, new)
+	var err error
+	if snippet, err = fillSnippetSocketAddress(slot, snippet); err != nil {
+		return err
+	}
 	spec.AddSnippet(snippet)
 	return nil
 }
 
 func (iface *qualcomIPCRouterInterface) AppArmorPermanentSlot(spec *apparmor.Specification, slot *snap.SlotInfo) error {
-	spec.AddSnippet(qipcrtrPermanentSlotAppArmor)
+	if isSlotInfoSystem(slot) {
+		return nil
+	}
+
+	var address string
+	if err := slot.Attr(addressAttrib, &address); err != nil {
+		return err
+	}
+	snippet := strings.ReplaceAll(qipcrtrPermanentSlotAppArmor, "###SOCKET_ADDRESS###", address)
+	spec.AddSnippet(snippet)
 	return nil
 }
 
 func (iface *qualcomIPCRouterInterface) SecCompPermanentSlot(spec *seccomp.Specification, slot *snap.SlotInfo) error {
+	if isSlotInfoSystem(slot) {
+		return nil
+	}
 	spec.AddSnippet(qipcrtrPermanentSlotSecComp)
 	return nil
 }
 
 func (iface *qualcomIPCRouterInterface) SecCompConnectedPlug(spec *seccomp.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
-	if implicitSystemConnectedSlot(slot) {
+	if isConnectedSlotSystem(slot) {
 		// backward compatibility
 		spec.AddSnippet(qipcrtrConnectedPlugSecCompCompat)
 	}
@@ -179,7 +291,30 @@ func (iface *qualcomIPCRouterInterface) verifySupport(what string) error {
 	return nil
 }
 
-func (iface *qualcomIPCRouterInterface) BeforePrepareSlot(plug *snap.SlotInfo) error {
+func (iface *qualcomIPCRouterInterface) BeforePrepareSlot(slot *snap.SlotInfo) error {
+	if slot.Attrs == nil {
+		slot.Attrs = make(map[string]interface{})
+	}
+	if isSlotInfoSystem(slot) {
+		return nil
+	}
+
+	qcipc, ok := slot.Attrs[qcipcAttrib].(string)
+	if !ok {
+		return fmt.Errorf("qualcomm-ipc-router slot must have a %s attribute", qcipcAttrib)
+	}
+	if err := validateTag(qcipc); err != nil {
+		return err
+	}
+
+	address, ok := slot.Attrs[addressAttrib].(string)
+	if !ok {
+		return fmt.Errorf("qualcomm-ipc-router slot must have an %s attribute", addressAttrib)
+	}
+	if err := validateAddress(address); err != nil {
+		return err
+	}
+
 	return iface.verifySupport("prepare slot")
 }
 
