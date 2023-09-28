64a74a270a2a513e8cf7d3e1cd411ea5f7e7ef49
Michael Vogt <mvo@ubuntu.com>
2023-09-28 12:55:33 +0200

    secboot: port the simple API changes

--- secboot/encrypt_sb.go
+++ secboot/encrypt_sb.go
@@ -39,7 +39,7 @@ import (
 
 var (
 	sbInitializeLUKS2Container       = sb.InitializeLUKS2Container
-	sbAddRecoveryKeyToLUKS2Container = sb.AddRecoveryKeyToLUKS2Container
+	sbAddRecoveryKeyToLUKS2Container = sb.AddLUKS2ContainerRecoveryKey
 )
 
 const keyslotsAreaKiBSize = 2560 // 2.5MB
@@ -71,7 +71,7 @@ func FormatEncryptedDevice(key keys.EncryptionKey, encType EncryptionType, label
 		},
 		InlineCryptoEngine: useICE,
 	}
-	return sbInitializeLUKS2Container(node, label, key[:], opts)
+	return sbInitializeLUKS2Container(node, label, sb.DiskUnlockKey(key), opts)
 }
 
 // AddRecoveryKey adds a fallback recovery key rkey to the existing encrypted
--- secboot/secboot_tpm.go
+++ secboot/secboot_tpm.go
@@ -282,7 +282,8 @@ func unlockEncryptedPartitionWithSealedKey(mapperName, sourceDevice, keyfile str
 	}
 	options := activateVolOpts(allowRecovery)
 	// ignoring model checker as it doesn't work with tpm "legacy" platform key data
-	_, err = sbActivateVolumeWithKeyData(mapperName, sourceDevice, keyData, options)
+	// TODO: provide AuthRequestor/KDF instead of nil
+	err = sbActivateVolumeWithKeyData(mapperName, sourceDevice, nil, nil, options, keyData)
 	if err == sb.ErrRecoveryKeyUsed {
 		logger.Noticef("successfully activated encrypted device %q using a fallback activation method", sourceDevice)
 		return UnlockedWithRecoveryKey, nil
@@ -397,7 +398,7 @@ func SealKeys(keys []SealKeyRequest, params *SealKeysParams) error {
 	sbKeys := make([]*sb_tpm2.SealKeyRequest, 0, len(keys))
 	for i := range keys {
 		sbKeys = append(sbKeys, &sb_tpm2.SealKeyRequest{
-			Key:  keys[i].Key,
+			Key:  sb.DiskUnlockKey(keys[i].Key),
 			Path: keys[i].KeyFile,
 		})
 	}
