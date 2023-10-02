6439d876559374276f6bf65bc5ef52657030d912
Dilyn Corner <dilyn.corner@canonical.com>
2023-10-02 17:17:44 -0400

    snapshotstate: fix defer unlocking for some tests
    Some tests will defer unlocking while also unlocking the state before
    running some function like Settle. If one of the Managers spawned by
    Settle fails, the test failure will indicate that an unlocked mutex
    failed to be unlocked instead of reporting the actual error emitted by
    the failing Manager.
    
    Signed-off-by: Dilyn Corner <dilyn.corner@canonical.com>

--- overlord/snapshotstate/snapshotstate_test.go
+++ overlord/snapshotstate/snapshotstate_test.go
@@ -635,7 +635,6 @@ func (snapshotSuite) TestSaveIntegration(c *check.C) {
 	o.AddManager(o.TaskRunner())
 
 	st.Lock()
-	defer st.Unlock()
 
 	snapshotOptions := map[string]*snap.SnapshotOptions{
 		"one-snap": {Exclude: []string{"$SNAP_COMMON/exclude-a-snap", "$SNAP_DATA/exclude-a-snap"}},
@@ -680,6 +679,7 @@ func (snapshotSuite) TestSaveIntegration(c *check.C) {
 	c.Assert(o.Settle(5*time.Second), check.IsNil)
 	st.Lock()
 	c.Check(change.Err(), check.IsNil)
+	defer st.Unlock()
 
 	tf := time.Now()
 	c.Assert(backend.Iter(context.TODO(), func(r *backend.Reader) error {
@@ -752,7 +752,6 @@ exec /bin/tar "$@"
 	o.AddManager(o.TaskRunner())
 
 	st.Lock()
-	defer st.Unlock()
 
 	for i, name := range []string{"one-snap", "too-snap", "tri-snap"} {
 		sideInfo := &snap.SideInfo{RealName: name, Revision: snap.R(i + 1)}
@@ -785,6 +784,7 @@ exec /bin/tar "$@"
 	c.Assert(o.Settle(5*time.Second), check.IsNil)
 	st.Lock()
 	c.Check(change.Err(), check.NotNil)
+	defer st.Unlock()
 	tasks := change.Tasks()
 	c.Assert(tasks, check.HasLen, 3)
 
@@ -858,7 +858,6 @@ exec /bin/tar "$@"
 	o.AddManager(o.TaskRunner())
 
 	st.Lock()
-	defer st.Unlock()
 
 	sideInfo := &snap.SideInfo{RealName: "tar-fail-snap", Revision: snap.R(1)}
 	snapstate.Set(st, "tar-fail-snap", &snapstate.SnapState{
@@ -885,6 +884,7 @@ exec /bin/tar "$@"
 	c.Assert(o.Settle(testutil.HostScaledTimeout(5*time.Second)), check.IsNil)
 	st.Lock()
 	c.Check(change.Err(), check.NotNil)
+	defer st.Unlock()
 	tasks := change.Tasks()
 	c.Assert(tasks, check.HasLen, 1)
 
