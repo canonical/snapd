152691c3c9c82ec19a0d9c6f08262d06e37cdb89
Philip Meulengracht <philip.meulengracht@canonical.com>
2023-10-10 19:51:31 +0200

    wrappers/internal: split unit tests

--- wrappers/internal/service_unit_gen_test.go
+++ wrappers/internal/service_unit_gen_test.go
@@ -21,8 +21,6 @@ package internal_test
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 	"strings"
 	"testing"
 	"time"
@@ -38,17 +36,16 @@ import (
 	"github.com/snapcore/snapd/snap/snaptest"
 	"github.com/snapcore/snapd/testutil"
 	"github.com/snapcore/snapd/timeout"
-	"github.com/snapcore/snapd/timeutil"
 	"github.com/snapcore/snapd/wrappers/internal"
 )
 
-type servicesWrapperGenSuite struct {
+type serviceUnitGenSuite struct {
 	testutil.BaseTest
 }
 
 func TestInternal(t *testing.T) { TestingT(t) }
 
-var _ = Suite(&servicesWrapperGenSuite{})
+var _ = Suite(&serviceUnitGenSuite{})
 
 const expectedServiceFmt = `[Unit]
 # Auto-generated, DO NOT EDIT
@@ -132,16 +129,16 @@ Type=%s
 	expectedTypeForkingWrapper = fmt.Sprintf(expectedServiceWrapperFmt, mountUnitPrefix, mountUnitPrefix, "forking", expectedInstallSection)
 )
 
-func (s *servicesWrapperGenSuite) SetUpTest(c *C) {
+func (s *serviceUnitGenSuite) SetUpTest(c *C) {
 	s.BaseTest.SetUpTest(c)
 	s.BaseTest.AddCleanup(snap.MockSanitizePlugsSlots(func(snapInfo *snap.Info) {}))
 }
 
-func (s *servicesWrapperGenSuite) TearDownTest(c *C) {
+func (s *serviceUnitGenSuite) TearDownTest(c *C) {
 	s.BaseTest.TearDownTest(c)
 }
 
-func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileOnClassic(c *C) {
+func (s *serviceUnitGenSuite) TestWriteSnapServiceUnitFileOnClassic(c *C) {
 	yamlText := `
 name: snap
 version: 1.0
@@ -164,7 +161,7 @@ apps:
 	c.Check(string(generatedWrapper), Equals, expectedAppService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenerateSnapServiceOnCore(c *C) {
+func (s *serviceUnitGenSuite) TestGenerateSnapServiceOnCore(c *C) {
 	defer func() { dirs.SetRootDir("/") }()
 
 	expectedAppServiceOnCore := `[Unit]
@@ -248,7 +245,7 @@ WantedBy=multi-user.target
 	c.Check(string(generatedWrapper), Equals, expectedAppServiceOnCoreWithSnapd)
 }
 
-func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileWithStartTimeout(c *C) {
+func (s *serviceUnitGenSuite) TestWriteSnapServiceUnitFileWithStartTimeout(c *C) {
 	yamlText := `
 name: snap
 version: 1.0
@@ -268,7 +265,7 @@ apps:
 	c.Check(string(generatedWrapper), testutil.Contains, "\nTimeoutStartSec=600\n")
 }
 
-func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileRestart(c *C) {
+func (s *serviceUnitGenSuite) TestWriteSnapServiceUnitFileRestart(c *C) {
 	yamlTextTemplate := `
 name: snap
 apps:
@@ -297,7 +294,7 @@ apps:
 	}
 }
 
-func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileTypeForking(c *C) {
+func (s *serviceUnitGenSuite) TestWriteSnapServiceUnitFileTypeForking(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "xkcd-webserver",
@@ -319,7 +316,7 @@ func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileTypeForking(c *C) 
 	c.Assert(string(generatedWrapper), Equals, expectedTypeForkingWrapper)
 }
 
-func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileIllegalChars(c *C) {
+func (s *serviceUnitGenSuite) TestWriteSnapServiceUnitFileIllegalChars(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "xkcd-webserver",
@@ -340,7 +337,7 @@ func (s *servicesWrapperGenSuite) TestWriteSnapServiceUnitFileIllegalChars(c *C)
 	c.Assert(err, NotNil)
 }
 
-func (s *servicesWrapperGenSuite) TestGenServiceFileWithBusName(c *C) {
+func (s *serviceUnitGenSuite) TestGenServiceFileWithBusName(c *C) {
 	yamlText := `
 name: snap
 version: 1.0
@@ -372,7 +369,7 @@ apps:
 	c.Assert(string(generatedWrapper), Equals, expectedDbusService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenServiceFileWithBusNameOnly(c *C) {
+func (s *serviceUnitGenSuite) TestGenServiceFileWithBusNameOnly(c *C) {
 
 	yamlText := `
 name: snap
@@ -400,7 +397,7 @@ apps:
 	c.Assert(string(generatedWrapper), Equals, expectedDbusService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenServiceFileWithBusNameFromSlot(c *C) {
+func (s *serviceUnitGenSuite) TestGenServiceFileWithBusNameFromSlot(c *C) {
 
 	yamlText := `
 name: snap
@@ -438,7 +435,7 @@ apps:
 	c.Assert(string(generatedWrapper), Equals, expectedDbusService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenOneshotServiceFile(c *C) {
+func (s *serviceUnitGenSuite) TestGenOneshotServiceFile(c *C) {
 
 	info := snaptest.MockInfo(c, `
 name: snap
@@ -461,7 +458,7 @@ apps:
 	c.Assert(string(generatedWrapper), Equals, expectedOneshotService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenerateSnapUserServiceFile(c *C) {
+func (s *serviceUnitGenSuite) TestGenerateSnapUserServiceFile(c *C) {
 	yamlText := `
 name: snap
 version: 1.0
@@ -485,84 +482,7 @@ apps:
 	c.Check(string(generatedWrapper), Equals, expectedUserAppService)
 }
 
-func (s *servicesWrapperGenSuite) TestGenerateSnapServiceWithSockets(c *C) {
-	const sock1ExpectedFmt = `[Unit]
-# Auto-generated, DO NOT EDIT
-Description=Socket sock1 for snap application some-snap.app
-Requires=%s-some\x2dsnap-44.mount
-After=%s-some\x2dsnap-44.mount
-X-Snappy=yes
-
-[Socket]
-Service=snap.some-snap.app.service
-FileDescriptorName=sock1
-ListenStream=%s/sock1.socket
-SocketMode=0666
-
-[Install]
-WantedBy=sockets.target
-`
-	const sock2ExpectedFmt = `[Unit]
-# Auto-generated, DO NOT EDIT
-Description=Socket sock2 for snap application some-snap.app
-Requires=%s-some\x2dsnap-44.mount
-After=%s-some\x2dsnap-44.mount
-X-Snappy=yes
-
-[Socket]
-Service=snap.some-snap.app.service
-FileDescriptorName=sock2
-ListenStream=%s/sock2.socket
-
-[Install]
-WantedBy=sockets.target
-`
-
-	si := &snap.Info{
-		SuggestedName: "some-snap",
-		Version:       "1.0",
-		SideInfo:      snap.SideInfo{Revision: snap.R(44)},
-	}
-	service := &snap.AppInfo{
-		Snap:        si,
-		Name:        "app",
-		Command:     "bin/foo start",
-		Daemon:      "simple",
-		DaemonScope: snap.SystemDaemon,
-		Plugs:       map[string]*snap.PlugInfo{"network-bind": {Interface: "network-bind"}},
-		Sockets: map[string]*snap.SocketInfo{
-			"sock1": {
-				Name:         "sock1",
-				ListenStream: "$SNAP_DATA/sock1.socket",
-				SocketMode:   0666,
-			},
-			"sock2": {
-				Name:         "sock2",
-				ListenStream: "$SNAP_DATA/sock2.socket",
-			},
-		},
-	}
-	service.Sockets["sock1"].App = service
-	service.Sockets["sock2"].App = service
-
-	sock1Expected := fmt.Sprintf(sock1ExpectedFmt, mountUnitPrefix, mountUnitPrefix, si.DataDir())
-	sock2Expected := fmt.Sprintf(sock2ExpectedFmt, mountUnitPrefix, mountUnitPrefix, si.DataDir())
-
-	generatedWrapper, err := internal.GenerateSnapServiceUnitFile(service, nil)
-	c.Assert(err, IsNil)
-	c.Assert(strings.Contains(string(generatedWrapper), "[Install]"), Equals, false)
-	c.Assert(strings.Contains(string(generatedWrapper), "WantedBy=multi-user.target"), Equals, false)
-
-	generatedSockets, err := internal.GenerateSnapSocketUnitFiles(service)
-	c.Assert(err, IsNil)
-	c.Assert(generatedSockets, HasLen, 2)
-	c.Assert(generatedSockets, DeepEquals, map[string][]byte{
-		"sock1": []byte(sock1Expected),
-		"sock2": []byte(sock2Expected),
-	})
-}
-
-func (s *servicesWrapperGenSuite) TestServiceAfterBefore(c *C) {
+func (s *serviceUnitGenSuite) TestServiceAfterBefore(c *C) {
 	const expectedServiceFmt = `[Unit]
 # Auto-generated, DO NOT EDIT
 Description=Service for snap application snap.app
@@ -653,255 +573,7 @@ WantedBy=multi-user.target
 	}
 }
 
-func (s *servicesWrapperGenSuite) TestServiceTimerUnit(c *C) {
-	const expectedServiceFmt = `[Unit]
-# Auto-generated, DO NOT EDIT
-Description=Timer app for snap application snap.app
-Requires=%s-snap-44.mount
-After=%s-snap-44.mount
-X-Snappy=yes
-
-[Timer]
-Unit=snap.snap.app.service
-OnCalendar=*-*-* 10:00
-OnCalendar=*-*-* 11:00
-
-[Install]
-WantedBy=timers.target
-`
-
-	expectedService := fmt.Sprintf(expectedServiceFmt, mountUnitPrefix, mountUnitPrefix)
-	service := &snap.AppInfo{
-		Snap: &snap.Info{
-			SuggestedName: "snap",
-			Version:       "0.3.4",
-			SideInfo:      snap.SideInfo{Revision: snap.R(44)},
-		},
-		Name:        "app",
-		Command:     "bin/foo start",
-		Daemon:      "simple",
-		DaemonScope: snap.SystemDaemon,
-		StopTimeout: timeout.DefaultTimeout,
-		Timer: &snap.TimerInfo{
-			Timer: "10:00-12:00/2",
-		},
-	}
-	service.Timer.App = service
-
-	generatedWrapper, err := internal.GenerateSnapServiceTimerUnitFile(service)
-	c.Assert(err, IsNil)
-
-	c.Logf("timer: \n%v\n", string(generatedWrapper))
-	c.Assert(string(generatedWrapper), Equals, expectedService)
-}
-
-func (s *servicesWrapperGenSuite) TestServiceTimerUnitBadTimer(c *C) {
-	service := &snap.AppInfo{
-		Snap: &snap.Info{
-			SuggestedName: "snap",
-			Version:       "0.3.4",
-			SideInfo:      snap.SideInfo{Revision: snap.R(44)},
-		},
-		Name:        "app",
-		Command:     "bin/foo start",
-		Daemon:      "simple",
-		DaemonScope: snap.SystemDaemon,
-		StopTimeout: timeout.DefaultTimeout,
-		Timer: &snap.TimerInfo{
-			Timer: "bad-timer",
-		},
-	}
-	service.Timer.App = service
-
-	generatedWrapper, err := internal.GenerateSnapServiceTimerUnitFile(service)
-	c.Assert(err, ErrorMatches, `cannot parse "bad-timer": "bad" is not a valid weekday`)
-	c.Assert(generatedWrapper, IsNil)
-}
-
-func (s *servicesWrapperGenSuite) TestServiceTimerServiceUnit(c *C) {
-	const expectedServiceFmt = `[Unit]
-# Auto-generated, DO NOT EDIT
-Description=Service for snap application snap.app
-Requires=%s-snap-44.mount
-Wants=network.target
-After=%s-snap-44.mount network.target snapd.apparmor.service
-X-Snappy=yes
-
-[Service]
-EnvironmentFile=-/etc/environment
-ExecStart=/usr/bin/snap run --timer="10:00-12:00,,mon,23:00~01:00/2" snap.app
-SyslogIdentifier=snap.app
-Restart=%s
-WorkingDirectory=/var/snap/snap/44
-TimeoutStopSec=30
-Type=%s
-`
-
-	expectedService := fmt.Sprintf(expectedServiceFmt, mountUnitPrefix, mountUnitPrefix, "on-failure", "simple")
-	service := &snap.AppInfo{
-		Snap: &snap.Info{
-			SuggestedName: "snap",
-			Version:       "0.3.4",
-			SideInfo:      snap.SideInfo{Revision: snap.R(44)},
-		},
-		Name:        "app",
-		Command:     "bin/foo start",
-		Daemon:      "simple",
-		DaemonScope: snap.SystemDaemon,
-		StopTimeout: timeout.DefaultTimeout,
-		Timer: &snap.TimerInfo{
-			Timer: "10:00-12:00,,mon,23:00~01:00/2",
-		},
-	}
-
-	generatedWrapper, err := internal.GenerateSnapServiceUnitFile(service, nil)
-	c.Assert(err, IsNil)
-
-	c.Logf("service: \n%v\n", string(generatedWrapper))
-	c.Assert(string(generatedWrapper), Equals, expectedService)
-}
-
-func (s *servicesWrapperGenSuite) TestTimerGenerateSchedules(c *C) {
-	systemdAnalyzePath, _ := exec.LookPath("systemd-analyze")
-	if systemdAnalyzePath != "" {
-		// systemd-analyze is in the path, but it will fail if the
-		// daemon is not running (as it happens in LP builds) and writes
-		// the following to stderr:
-		//   Failed to create bus connection: No such file or directory
-		cmd := exec.Command(systemdAnalyzePath, "calendar", "12:00")
-		err := cmd.Run()
-		if err != nil {
-			// turns out it's not usable, disable extra verification
-			fmt.Fprintln(os.Stderr, `WARNING: systemd-analyze not usable, cannot validate a known schedule "12:00"`)
-			systemdAnalyzePath = ""
-		}
-	}
-
-	if systemdAnalyzePath == "" {
-		fmt.Fprintln(os.Stderr, "WARNING: generated schedules will not be validated by systemd-analyze")
-	}
-
-	for _, t := range []struct {
-		in         string
-		expected   []string
-		randomized bool
-	}{{
-		in:       "9:00-11:00,,20:00-22:00",
-		expected: []string{"*-*-* 09:00", "*-*-* 20:00"},
-	}, {
-		in:       "9:00-11:00/2,,20:00",
-		expected: []string{"*-*-* 09:00", "*-*-* 10:00", "*-*-* 20:00"},
-	}, {
-		in:         "9:00~11:00/2,,20:00",
-		expected:   []string{`\*-\*-\* 09:[0-5][0-9]`, `\*-\*-\* 10:[0-5][0-9]`, `\*-\*-\* 20:00`},
-		randomized: true,
-	}, {
-		in:       "mon,10:00,,fri,15:00",
-		expected: []string{"Mon *-*-* 10:00", "Fri *-*-* 15:00"},
-	}, {
-		in:       "mon-fri,10:00-11:00",
-		expected: []string{"Mon,Tue,Wed,Thu,Fri *-*-* 10:00"},
-	}, {
-		in:       "fri-mon,10:00-11:00",
-		expected: []string{"Fri,Sat,Sun,Mon *-*-* 10:00"},
-	}, {
-		in:       "mon5,10:00",
-		expected: []string{"Mon *-*-22,23,24,25,26,27,28,29,30,31 10:00"},
-	}, {
-		in:       "mon2,10:00",
-		expected: []string{"Mon *-*-8,9,10,11,12,13,14 10:00"},
-	}, {
-		in:       "mon2,mon1,10:00",
-		expected: []string{"Mon *-*-8,9,10,11,12,13,14 10:00", "Mon *-*-1,2,3,4,5,6,7 10:00"},
-	}, {
-		// (deprecated syntax, reduced to mon1-mon)
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon1-mon3,10:00",
-		expected: []string{"*-*-8,9,10,11,12,13,14 10:00", "*-*-1,2,3,4,5,6,7 10:00"},
-	}, {
-		in:         "mon,10:00~12:00,,fri,15:00",
-		expected:   []string{`Mon \*-\*-\* 1[01]:[0-5][0-9]`, `Fri \*-\*-\* 15:00`},
-		randomized: true,
-	}, {
-		in:         "23:00~24:00/4",
-		expected:   []string{`\*-\*-\* 23:[01][0-9]`, `\*-\*-\* 23:[12][0-9]`, `\*-\*-\* 23:[34][0-9]`, `*-*-* 23:[45][0-9]`},
-		randomized: true,
-	}, {
-		in:         "23:00~01:00/4",
-		expected:   []string{`\*-\*-\* 23:[0-2][0-9]`, `\*-\*-\* 23:[3-5][0-9]`, `\*-\*-\* 00:[0-2][0-9]`, `\*-\*-\* 00:[3-5][0-9]`},
-		randomized: true,
-	}, {
-		in:       "23:00-01:00/4",
-		expected: []string{`*-*-* 23:00`, `*-*-* 23:30`, `*-*-* 00:00`, `*-*-* 00:30`},
-	}, {
-		in:       "24:00",
-		expected: []string{`*-*-* 00:00`},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "fri-mon1,10:00",
-		expected: []string{"*-*-22,23,24,25,26,27,28,29,30,31 10:00", "*-*-1,2,3,4,5,6,7 10:00"},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon5-fri,10:00",
-		expected: []string{"*-*-29,30,31 10:00", "*-*-1,2,3,4,5,6,7 10:00", "*-*-22,23,24,25,26,27,28 10:00"},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon4-fri,10:00",
-		expected: []string{"*-*-29,30,31 10:00", "*-*-1,2,3,4,5,6,7 10:00", "*-*-22,23,24,25,26,27,28 10:00"},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon-fri2,10:00",
-		expected: []string{"*-*-1,2,3,4,5,6,7 10:00", "*-*-8,9,10,11,12,13,14 10:00"},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon-fri5,10:00",
-		expected: []string{"*-*-29,30,31 10:00", "*-*-22,23,24,25,26,27,28 10:00"},
-	}, {
-		// NOTE: non-representable, assumes that service runner does the
-		// filtering of when to run the timer
-		in:       "mon1-mon,10:00",
-		expected: []string{"*-*-8,9,10,11,12,13,14 10:00", "*-*-1,2,3,4,5,6,7 10:00"},
-	}, {
-		in:       "mon",
-		expected: []string{"Mon *-*-*"},
-	}, {
-		in:       "mon,fri",
-		expected: []string{"Mon *-*-*", "Fri *-*-*"},
-	}, {
-		in:       "mon2,mon1",
-		expected: []string{"Mon *-*-8,9,10,11,12,13,14", "Mon *-*-1,2,3,4,5,6,7"},
-	}} {
-		c.Logf("trying %+v", t)
-
-		schedule, err := timeutil.ParseSchedule(t.in)
-		c.Check(err, IsNil)
-
-		timer := internal.GenerateOnCalendarSchedules(schedule)
-		c.Check(timer, Not(IsNil))
-		if !t.randomized {
-			c.Check(timer, DeepEquals, t.expected)
-		} else {
-			c.Assert(timer, HasLen, len(t.expected))
-			for i := range timer {
-				c.Check(timer[i], Matches, t.expected[i])
-			}
-		}
-
-		if systemdAnalyzePath != "" {
-			cmd := exec.Command(systemdAnalyzePath, append([]string{"calendar"}, timer...)...)
-			out, err := cmd.CombinedOutput()
-			c.Check(err, IsNil, Commentf("systemd-analyze failed with output:\n%s", string(out)))
-		}
-	}
-}
-
-func (s *servicesWrapperGenSuite) TestKillModeSig(c *C) {
+func (s *serviceUnitGenSuite) TestKillModeSig(c *C) {
 	for _, rm := range []string{"sigterm", "sighup", "sigusr1", "sigusr2", "sigint"} {
 		service := &snap.AppInfo{
 			Snap: &snap.Info{
@@ -944,7 +616,7 @@ WantedBy=multi-user.target
 	}
 }
 
-func (s *servicesWrapperGenSuite) TestRestartDelay(c *C) {
+func (s *serviceUnitGenSuite) TestRestartDelay(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "snap",
@@ -984,7 +656,7 @@ WantedBy=multi-user.target
 `, mountUnitPrefix, mountUnitPrefix))
 }
 
-func (s *servicesWrapperGenSuite) TestVitalityScore(c *C) {
+func (s *serviceUnitGenSuite) TestVitalityScore(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "snap",
@@ -1026,7 +698,7 @@ WantedBy=multi-user.target
 `, mountUnitPrefix, mountUnitPrefix))
 }
 
-func (s *servicesWrapperGenSuite) TestQuotaGroupSlice(c *C) {
+func (s *serviceUnitGenSuite) TestQuotaGroupSlice(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "snap",
@@ -1069,7 +741,7 @@ WantedBy=multi-user.target
 `, mountUnitPrefix, mountUnitPrefix))
 }
 
-func (s *servicesWrapperGenSuite) TestQuotaGroupLogNamespace(c *C) {
+func (s *serviceUnitGenSuite) TestQuotaGroupLogNamespace(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "snap",
@@ -1113,7 +785,7 @@ WantedBy=multi-user.target
 `, mountUnitPrefix, mountUnitPrefix))
 }
 
-func (s *servicesWrapperGenSuite) TestQuotaGroupLogNamespaceInheritParent(c *C) {
+func (s *serviceUnitGenSuite) TestQuotaGroupLogNamespaceInheritParent(c *C) {
 	service := &snap.AppInfo{
 		Snap: &snap.Info{
 			SuggestedName: "snap",
