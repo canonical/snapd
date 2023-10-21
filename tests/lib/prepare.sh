dbf09c9b948d386c60f865d25bb6dfd528f0ee25
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-10-21 17:27:15 +0100

    tests: increase image size for core tests
    It was too small for UC18 tests lately.

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1169,6 +1169,7 @@ EOF
     fi
     # shellcheck disable=SC2086
     "$UBUNTU_IMAGE" snap \
+                    --image-size 5G \
                     -w "$IMAGE_HOME" "$IMAGE_HOME/pc.model" \
                     --channel "$IMAGE_CHANNEL" \
                     $EXTRA_FUNDAMENTAL \
