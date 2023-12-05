7f42f397959adfa89d1fbbdf74c9c241cc5929a0
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2023-12-05 10:56:44 -0300

    tests: add support for amazon-linux-2023

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -725,14 +725,20 @@ pkg_dependencies_fedora(){
 }
 
 pkg_dependencies_amazon(){
+    if os.query is-amazon-linux 2; then
+        echo "
+            fish
+            fwupd
+            system-lsb-core
+            upower
+            "
+    fi
     echo "
         python3
         curl
         dbus-x11
         expect
-        fish
         fontconfig
-        fwupd
         git
         golang
         grub2-tools
@@ -744,12 +750,10 @@ pkg_dependencies_amazon(){
         net-tools
         nfs-utils
         PackageKit
-        system-lsb-core
         rpm-build
         xdg-user-dirs
         xdg-utils
         udisks2
-        upower
         zsh
         "
 }
