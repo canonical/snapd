840b59351a4f7f8d4405d9a91d17f2ff7a57ff9d
Valentin David <valentin.david@canonical.com>
2024-01-23 11:31:48 +0100

    secboot: remove dead code

--- secboot/encrypt_sb.go
+++ secboot/encrypt_sb.go
@@ -39,7 +39,6 @@ import (
 
 var (
 	sbInitializeLUKS2Container     = sb.InitializeLUKS2Container
-	sbAddLUKS2ContainerRecoveryKey = sb.AddLUKS2ContainerRecoveryKey
 )
 
 const keyslotsAreaKiBSize = 2560 // 2.5MB
