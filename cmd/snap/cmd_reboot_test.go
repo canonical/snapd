1919aec133ac3630e62ace7ba44571de1d30a7f2
Valentin David <valentin.david@canonical.com>
2024-01-31 16:42:36 +0100

    cmd/snap/cmd_reboot_test.go: add tests for --reseal flag

--- cmd/snap/cmd_reboot_test.go
+++ cmd/snap/cmd_reboot_test.go
@@ -66,41 +66,117 @@ rebooting. This is only available for the current system.
 
 func (s *SnapSuite) TestRebootHappy(c *C) {
 
-	for _, tc := range []struct {
-		cmdline          []string
+	type request struct {
+		method           string
 		expectedEndpoint string
 		expectedJSON     string
-		expectedMsg      string
+		status           int
+		response         string
+	}
+	for _, tc := range []struct {
+		cmdline     []string
+		expectedMsg string
+		expectedErr string
+		requests    []request
 	}{
 		{
-			cmdline:          []string{"reboot"},
-			expectedEndpoint: "/v2/systems",
-			expectedJSON:     `{"action":"reboot","mode":""}`,
-			expectedMsg:      `Reboot`,
+			cmdline:     []string{"reboot"},
+			expectedMsg: `Reboot` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/systems",
+				expectedJSON:     `{"action":"reboot","mode":""}` + "\n",
+				status:           200,
+				response:         `{"type": "sync", "result": {}}`,
+			}},
 		},
 		{
-			cmdline:          []string{"reboot", "--recover"},
-			expectedEndpoint: "/v2/systems",
-			expectedJSON:     `{"action":"reboot","mode":"recover"}`,
-			expectedMsg:      `Reboot into "recover" mode.`,
+			cmdline:     []string{"reboot", "--recover"},
+			expectedMsg: `Reboot into "recover" mode.` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/systems",
+				expectedJSON:     `{"action":"reboot","mode":"recover"}` + "\n",
+				status:           200,
+				response:         `{"type": "sync", "result": {}}`,
+			}},
 		},
 		{
-			cmdline:          []string{"reboot", "20200101"},
-			expectedEndpoint: "/v2/systems/20200101",
-			expectedJSON:     `{"action":"reboot","mode":""}`,
-			expectedMsg:      `Reboot into "20200101".`,
+			cmdline:     []string{"reboot", "20200101"},
+			expectedMsg: `Reboot into "20200101".` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/systems/20200101",
+				expectedJSON:     `{"action":"reboot","mode":""}` + "\n",
+				status:           200,
+				response:         `{"type": "sync", "result": {}}`,
+			}},
 		},
 		{
-			cmdline:          []string{"reboot", "--recover", "20200101"},
-			expectedEndpoint: "/v2/systems/20200101",
-			expectedJSON:     `{"action":"reboot","mode":"recover"}`,
-			expectedMsg:      `Reboot into "20200101" "recover" mode.`,
+			cmdline:     []string{"reboot", "--recover", "20200101"},
+			expectedMsg: `Reboot into "20200101" "recover" mode.` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/systems/20200101",
+				expectedJSON:     `{"action":"reboot","mode":"recover"}` + "\n",
+				status:           200,
+				response:         `{"type": "sync", "result": {}}`,
+			}},
 		},
 		{
-			cmdline:          []string{"reboot", "--factory-reset", "20200101"},
-			expectedEndpoint: "/v2/systems/20200101",
-			expectedJSON:     `{"action":"reboot","mode":"factory-reset"}`,
-			expectedMsg:      `Reboot into "20200101" "factory-reset" mode.`,
+			cmdline:     []string{"reboot", "--factory-reset", "20200101"},
+			expectedMsg: `Reboot into "20200101" "factory-reset" mode.` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/systems/20200101",
+				expectedJSON:     `{"action":"reboot","mode":"factory-reset"}` + "\n",
+				status:           200,
+				response:         `{"type": "sync", "result": {}}`,
+			}},
+		},
+		{
+			cmdline:     []string{"reboot", "--reseal", "--no-wait"},
+			expectedMsg: `42` + "\n",
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/system-reseal",
+				expectedJSON:     `{"reboot":true}`,
+				status:           202,
+				response:         `{"type": "async", "change": "42", "status-code": 202}`,
+			}},
+		},
+		{
+			cmdline:     []string{"reboot", "--reseal"},
+			expectedMsg: ``,
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/system-reseal",
+				expectedJSON:     `{"reboot":true}`,
+				status:           202,
+				response:         `{"type": "async", "change": "42", "status-code": 202}`,
+			}, {
+				method:           "GET",
+				expectedEndpoint: "/v2/changes/42",
+				status:           200,
+				response:         `{"type": "sync", "result": {"ready": true, "status": "Done"}}` + "\n",
+			}},
+		},
+		{
+			cmdline:     []string{"reboot", "--reseal"},
+			expectedMsg: ``,
+			expectedErr: `(?s).*some error.*`,
+			requests: []request{{
+				method:           "POST",
+				expectedEndpoint: "/v2/system-reseal",
+				expectedJSON:     `{"reboot":true}`,
+				status:           202,
+				response:         `{"type": "async", "change": "42", "status-code": 202}`,
+			}, {
+				method:           "GET",
+				expectedEndpoint: "/v2/changes/42",
+				status:           400,
+				response:         `{"type": "error", "result": {"message": "some error"}, "status-code": 400}`,
+			}},
 		},
 	} {
 
@@ -108,28 +184,32 @@ func (s *SnapSuite) TestRebootHappy(c *C) {
 		s.ResetStdStreams()
 
 		s.RedirectClientToTestServer(func(w http.ResponseWriter, r *http.Request) {
-			switch n {
-			case 0:
-				c.Check(r.Method, Equals, "POST")
-				c.Check(r.URL.Path, Equals, tc.expectedEndpoint, Commentf("%v", tc.cmdline))
-				c.Check(r.URL.RawQuery, Equals, "")
-				body, err := ioutil.ReadAll(r.Body)
-				c.Check(err, IsNil)
-				c.Check(string(body), Equals, tc.expectedJSON+"\n")
-				fmt.Fprintln(w, `{"type": "sync", "result": {}}`)
-			default:
-				c.Fatalf("expected to get 1 requests, now on %d", n+1)
+			if n > len(tc.requests) {
+				c.Fatalf("expected to get %d requests, now on %d", len(tc.requests), n+1)
 			}
-
+			e := tc.requests[n]
 			n++
+			c.Check(r.Method, Equals, e.method)
+			c.Check(r.URL.Path, Equals, e.expectedEndpoint, Commentf("%v", tc.cmdline))
+			c.Check(r.URL.RawQuery, Equals, "")
+			body, err := ioutil.ReadAll(r.Body)
+			c.Check(err, IsNil)
+			c.Check(string(body), Equals, e.expectedJSON)
+			w.WriteHeader(e.status)
+			fmt.Fprintln(w, e.response)
 		})
 
 		// The server side will work out if the request is valid
 		rest, err := snap.Parser(snap.Client()).ParseArgs(tc.cmdline)
-		c.Assert(err, IsNil)
-		c.Assert(rest, DeepEquals, []string{})
-		c.Check(s.Stdout(), Equals, tc.expectedMsg+"\n", Commentf("%v", tc.cmdline))
+		if tc.expectedErr != "" {
+			c.Assert(err, ErrorMatches, tc.expectedErr)
+		} else {
+			c.Assert(err, IsNil)
+			c.Assert(rest, DeepEquals, []string{})
+		}
+		c.Check(s.Stdout(), Equals, tc.expectedMsg, Commentf("%v", tc.cmdline))
 		c.Check(s.Stderr(), Equals, "")
+		c.Check(n, Equals, len(tc.requests))
 	}
 }
 
