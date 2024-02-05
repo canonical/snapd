0698fcf6936462b5b5f974438dac31888e46a52f
Valentin David <valentin.david@canonical.com>
2024-02-05 21:06:47 +0100

    fixup! many: use recovery key when forcefully reseal

--- overlord/devicestate/reseal_test.go
+++ overlord/devicestate/reseal_test.go
@@ -25,13 +25,18 @@ import (
 
 	. "gopkg.in/check.v1"
 
+	"github.com/snapcore/snapd/osutil/disks"
 	"github.com/snapcore/snapd/boot"
 	"github.com/snapcore/snapd/gadget"
 	"github.com/snapcore/snapd/overlord/devicestate"
+	"github.com/snapcore/snapd/overlord/snapstate"
 	"github.com/snapcore/snapd/overlord/restart"
 	"github.com/snapcore/snapd/overlord/state"
 	"github.com/snapcore/snapd/secboot"
 	"github.com/snapcore/snapd/secboot/keys"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/overlord/snapstate/snapstatetest"
+	"github.com/snapcore/snapd/snap/snaptest"
 )
 
 type deviceMgrResealSuite struct {
@@ -40,10 +45,127 @@ type deviceMgrResealSuite struct {
 
 var _ = Suite(&deviceMgrResealSuite{})
 
+var pcSnapYaml = `
+name: pc
+type: gadget
+`
+
+var pcGadgetYaml = `
+volumes:
+  pc:
+    bootloader: grub
+    structure:
+      - name: ubuntu-seed
+        role: system-seed
+        size: 100M
+        type: EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+      - name: ubuntu-boot
+        role: system-boot
+        size: 100M
+        type: EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+      - name: ubuntu-data
+        role: system-data
+        size: 100M
+        type: EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+      - name: ubuntu-save
+        role: system-save
+        size: 100M
+        type: EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+`
+
 func (s *deviceMgrResealSuite) SetUpTest(c *C) {
 	s.deviceMgrBaseSuite.setupBaseTest(c, false)
 	s.setUC20PCModelInState(c)
 	devicestate.SetSystemMode(s.mgr, "run")
+	gadgetSideInfo := &snap.SideInfo{
+		RealName: "pc",
+		Revision: snap.R(1),
+		SnapID:   "pc-id",
+	}
+	snaptest.MockSnapWithFiles(c, pcSnapYaml, &snap.SideInfo{Revision: snap.R(1)}, [][]string{
+		{"meta/gadget.yaml", pcGadgetYaml},
+	})
+
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	snapstate.Set(s.state, "pc", &snapstate.SnapState{
+		Active:   true,
+		Sequence: snapstatetest.NewSequenceFromSnapSideInfos([]*snap.SideInfo{gadgetSideInfo}),
+		Current:  snap.R(1),
+		SnapType: "gadget",
+	})
+
+	seedPart := disks.Partition{
+		FilesystemLabel:  "ubuntu-seed",
+		PartitionLabel:   "ubuntu-seed",
+		PartitionUUID:    "ubuntu-seed-partuuid",
+		KernelDeviceNode: "/dev/fakedevice0p1",
+		PartitionType:    "EF,C12A7328-F81F-11D2-BA4B-00A0C93EC93B",
+		DiskIndex:        1,
+		StartInBytes:     1024*1024,
+		SizeInBytes:      100*1024*1024,
+	}
+
+	bootPart := disks.Partition{
+		FilesystemLabel:  "ubuntu-boot",
+		PartitionLabel:   "ubuntu-boot",
+		PartitionUUID:    "ubuntu-boot-partuuid",
+		KernelDeviceNode: "/dev/fakedevice0p2",
+		DiskIndex:        2,
+		StartInBytes:     101*1024*1024,
+		SizeInBytes:      100*1024*1024,
+	}
+
+	dataPart := disks.Partition{
+		FilesystemLabel:  "ubuntu-data-enc",
+		FilesystemType:   "crypto_LUKS",
+		PartitionLabel:   "ubuntu-data",
+		PartitionUUID:    "ubuntu-data-partuuid",
+		KernelDeviceNode: "/dev/fakedevice0p4",
+		DiskIndex:        3,
+		StartInBytes:     201*1024*1024,
+		SizeInBytes:      100*1024*1024,
+	}
+
+	savePart := disks.Partition{
+		FilesystemLabel:  "ubuntu-save-enc",
+		FilesystemType:   "crypto_LUKS",
+		PartitionLabel:   "ubuntu-save",
+		PartitionUUID:    "ubuntu-save-partuuid",
+		KernelDeviceNode: "/dev/fakedevice0p3",
+		DiskIndex:        4,
+		StartInBytes:     301*1024*1024,
+		SizeInBytes:      100*1024*1024,
+	}
+
+	fakeDisk := &disks.MockDiskMapping{
+		DevNum:  "42:0",
+		DevNode: "/dev/fakedevice0",
+		DevPath: "/sys/block/fakedevice0",
+		Structure: []disks.Partition{
+			seedPart,
+			bootPart,
+			dataPart,
+			savePart,
+		},
+		DiskSchema: "gpt",
+		SectorSizeBytes: 512,
+		DiskUsableSectorEnd: 2*1024*1024-1,
+		DiskSizeInBytes: 1024*1024*1024,
+	}
+
+	s.AddCleanup(disks.MockDeviceNameToDiskMapping(
+		map[string]*disks.MockDiskMapping{
+			"/dev/fakedevice0": fakeDisk,
+		},
+	))
+
+	s.AddCleanup(disks.MockMountPointDisksToPartitionMapping(
+		map[disks.Mountpoint]*disks.MockDiskMapping{
+			disks.Mountpoint{Mountpoint: "/run/mnt/ubuntu-boot", IsDecryptedDevice: false}: fakeDisk,
+		},
+	))
 }
 
 func (s *deviceMgrResealSuite) testResealHappy(c *C, reboot bool) {
@@ -124,6 +246,15 @@ func (s *deviceMgrResealSuite) TestResealNoRebootHappy(c *C) {
 
 func (s *deviceMgrResealSuite) TestResealError(c *C) {
 	mockSystemRecoveryKeys(c, false)
+	mockSnapFDEFile(c, "marker", nil)
+
+	rkeystr, err := hex.DecodeString("e1f01302c5d43726a9b85b4a8d9c7f6e")
+	c.Assert(err, IsNil)
+	defer devicestate.MockSecbootEnsureRecoveryKey(func(keyFile string, rkeyDevs []secboot.RecoveryKeyDevice) (keys.RecoveryKey, error) {
+		var rkey keys.RecoveryKey
+		copy(rkey[:], []byte(rkeystr))
+		return rkey, nil
+	})()
 
 	finishReseal := make(chan struct{})
 	startedReseal := make(chan struct{})
