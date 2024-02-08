7891c8d2c7051fb3e3b2178580a2e78563998dde
Andrew Phelps <andrew.phelps@canonical.com>
2024-02-08 14:47:23 -0500

    interfaces: implement methods on SnapAppSet in terms of methods on snap.Info

--- interfaces/snap_app_set.go
+++ interfaces/snap_app_set.go
@@ -1,6 +1,10 @@
 package interfaces
 
 import (
+	"bytes"
+	"fmt"
+	"sort"
+
 	"github.com/snapcore/snapd/snap"
 )
 
@@ -13,25 +17,88 @@ func NewSnapAppSet(info *snap.Info) *SnapAppSet {
 }
 
 func (a *SnapAppSet) SecurityTagsForConnectedPlug(plug *ConnectedPlug) []string {
-	return nil
+	return a.SecurityTagsForPlug(plug.plugInfo)
 }
 
 func (a *SnapAppSet) SecurityTagsForPlug(plug *snap.PlugInfo) []string {
-	return nil
+	apps := a.info.AppsForPlug(plug)
+	hooks := a.info.HooksForPlug(plug)
+
+	tags := make([]string, 0, len(apps)+len(hooks))
+	for _, app := range apps {
+		tags = append(tags, app.SecurityTag())
+	}
+
+	for _, hook := range hooks {
+		tags = append(tags, hook.SecurityTag())
+	}
+
+	sort.Strings(tags)
+
+	return tags
 }
 
 func (a *SnapAppSet) SecurityTagsForConnectedSlot(slot *ConnectedSlot) []string {
-	return nil
+	return a.SecurityTagsForSlot(slot.slotInfo)
 }
 
 func (a *SnapAppSet) SecurityTagsForSlot(slot *snap.SlotInfo) []string {
-	return nil
+	apps := a.info.AppsForSlot(slot)
+	hooks := a.info.HooksForSlot(slot)
+
+	tags := make([]string, 0, len(apps)+len(hooks))
+	for _, app := range apps {
+		tags = append(tags, app.SecurityTag())
+	}
+
+	for _, hook := range hooks {
+		tags = append(tags, hook.SecurityTag())
+	}
+
+	sort.Strings(tags)
+
+	return tags
 }
 
 func (a *SnapAppSet) PlugLabelExpression(plug *ConnectedPlug) string {
-	return ""
+	apps := a.info.AppsForPlug(plug.plugInfo)
+	hooks := a.info.HooksForPlug(plug.plugInfo)
+	return labelExpr(apps, hooks, plug.Snap())
 }
 
 func (a *SnapAppSet) SlotLabelExpression(slot *ConnectedSlot) string {
-	return ""
+	apps := a.info.AppsForSlot(slot.slotInfo)
+	hooks := a.info.HooksForSlot(slot.slotInfo)
+	return labelExpr(apps, hooks, slot.Snap())
+}
+
+func labelExpr(apps []*snap.AppInfo, hooks []*snap.HookInfo, snap *snap.Info) string {
+	var buf bytes.Buffer
+
+	names := make([]string, 0, len(apps)+len(hooks))
+	for _, app := range apps {
+		names = append(names, app.Name)
+	}
+	for _, hook := range hooks {
+		names = append(names, fmt.Sprintf("hook.%s", hook.Name))
+	}
+	sort.Strings(names)
+
+	fmt.Fprintf(&buf, `"snap.%s.`, snap.InstanceName())
+	if len(names) == 1 {
+		buf.WriteString(names[0])
+	} else if len(apps) == len(snap.Apps) && len(hooks) == len(snap.Hooks) {
+		buf.WriteByte('*')
+	} else if len(names) > 0 {
+		buf.WriteByte('{')
+		for _, name := range names {
+			buf.WriteString(name)
+			buf.WriteByte(',')
+		}
+		// remove trailing comma
+		buf.Truncate(buf.Len() - 1)
+		buf.WriteByte('}')
+	} // else: len(names)==0, gives "snap.<name>." that doesn't match anything
+	buf.WriteByte('"')
+	return buf.String()
 }
