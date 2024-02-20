598a0873626010bd216e11638700a662952460d0
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2024-02-20 09:31:45 +0000

    o/snapstate/backend: add methods to set-up kernel-modules components

--- overlord/snapstate/backend_test.go
+++ overlord/snapstate/backend_test.go
@@ -85,6 +85,9 @@ type fakeOp struct {
 
 	dirOpts  *dirs.SnapDirOptions
 	undoInfo *backend.UndoInfo
+
+	compsToInstall, currentComps []*snap.ComponentSideInfo
+	compsToRemove, finalComps    []*snap.ComponentSideInfo
 }
 
 type fakeOps []fakeOp
@@ -934,6 +937,32 @@ func (f *fakeSnappyBackend) SetupComponent(compFilePath string, compPi snap.Cont
 		return nil, fmt.Errorf("cannot set-up component %q", compPi.ContainerName())
 	}
 	return &backend.InstallRecord{}, nil
+}
+
+func (f *fakeSnappyBackend) SetupKernelModulesComponents(compsToInstall, currentComps []*snap.ComponentSideInfo, ksnapName string, ksnapRev snap.Revision, meter progress.Meter) (err error) {
+	meter.Notify("setup-kernel-modules-components")
+	f.appendOp(&fakeOp{
+		op:             "setup-kernel-modules-components",
+		compsToInstall: compsToInstall,
+		currentComps:   currentComps,
+	})
+	if strings.HasSuffix(ksnapName, "+broken") {
+		return fmt.Errorf("cannot set-up kernel-modules for %s", ksnapName)
+	}
+	return nil
+}
+
+func (f *fakeSnappyBackend) RemoveKernelModulesComponentsSetup(compsToRemove, finalComps []*snap.ComponentSideInfo, ksnapName string, ksnapRev snap.Revision, meter progress.Meter) (err error) {
+	meter.Notify("remove-kernel-modules-components-setup")
+	f.appendOp(&fakeOp{
+		op:            "remove-kernel-modules-components-setup",
+		compsToRemove: compsToRemove,
+		finalComps:    finalComps,
+	})
+	if strings.HasSuffix(ksnapName, "+reverterr") {
+		return fmt.Errorf("cannot remove set-up of kernel-modules for %s", ksnapName)
+	}
+	return nil
 }
 
 func (f *fakeSnappyBackend) UndoSetupComponent(cpi snap.ContainerPlaceInfo, installRecord *backend.InstallRecord, dev snap.Device, meter progress.Meter) error {
