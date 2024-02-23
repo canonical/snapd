ef70dc5fb7560cc1ebec8bac9dbdaa2f2b7ca81b
Peter Sabaini <peter.sabaini@canonical.com>
2024-02-23 16:05:51 +0100

    Rebase and fixup unit test
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>

--- interfaces/builtin/microceph_support_test.go
+++ interfaces/builtin/microceph_support_test.go
@@ -73,7 +73,7 @@ func (s *MicrocephSupportInterfaceSuite) TestSanitizePlug(c *C) {
 }
 
 func (s *MicrocephSupportInterfaceSuite) TestAppArmorSpec(c *C) {
-	spec := &apparmor.Specification{}
+	spec := apparmor.NewSpecification(interfaces.NewSnapAppSet(s.plug.Snap()))
 	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
 	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
 	c.Assert(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, "/sys/bus/rbd/add_single_major rwk,                         # add single major dev\n")
