f43f9601c482b1a9635cbab79b16891bc1c1cd46
Valentin David <valentin.david@canonical.com>
2024-03-14 15:07:26 +0100

    boot: revert bb7d327a36d8fd923126052c32792e1293c1d243
    This reverts #13402.
    
    `tests/nested/manual/uc-update-assets-secure-add-sbat` shows some
    failures from time to time. It is possible in some context, the wrong
    hashes are pruned when resealing.

--- boot/bootchain.go
+++ boot/bootchain.go
@@ -257,13 +257,7 @@ func predictableBootChainsEqualForReseal(pb1, pb2 predictableBootChains) bootCha
 // bootAssetsToLoadChains generates a list of load chains covering given boot
 // assets sequence. At the end of each chain, adds an entry for the kernel boot
 // file.
-// We do not calculate some boot chains because they are impossible as
-// when we update assets we write first the binaries that are used
-// later, that is, if updating both shim and grub, the new grub is
-// copied first to the disk, so booting from the new shim to the old
-// grub is not possible. This is controlled by expectNew, that tells
-// us that the previous step in the chain is from a new asset.
-func bootAssetsToLoadChains(assets []bootAsset, kernelBootFile bootloader.BootFile, roleToBlName map[bootloader.Role]string, expectNew bool) ([]*secboot.LoadChain, error) {
+func bootAssetsToLoadChains(assets []bootAsset, kernelBootFile bootloader.BootFile, roleToBlName map[bootloader.Role]string) ([]*secboot.LoadChain, error) {
 	// kernel is added after all the assets
 	addKernelBootFile := len(assets) == 0
 	if addKernelBootFile {
@@ -276,24 +270,7 @@ func bootAssetsToLoadChains(assets []bootAsset, kernelBootFile bootloader.BootFi
 		return nil, fmt.Errorf("internal error: no bootloader name for boot asset role %q", thisAsset.Role)
 	}
 	var chains []*secboot.LoadChain
-
-	for i, hash := range thisAsset.Hashes {
-		// There should be 1 or 2 assets, and their position has a meaning.
-		// See TrustedAssetsUpdateObserver.observeUpdate
-		if i == 0 {
-			// i == 0 means currently installed asset.
-			// We do not expect this asset to be used as
-			// we have new assets earlier in the chain
-			if len(thisAsset.Hashes) == 2 && expectNew {
-				continue
-			}
-		} else if i == 1 {
-			// i == 1 means new asset
-		} else {
-			// If there is a second asset, it is the next asset to be installed
-			return nil, fmt.Errorf("internal error: did not expect more than 2 hashes for %s", thisAsset.Name)
-		}
-
+	for _, hash := range thisAsset.Hashes {
 		var bf bootloader.BootFile
 		var next []*secboot.LoadChain
 		var err error
@@ -309,7 +286,7 @@ func bootAssetsToLoadChains(assets []bootAsset, kernelBootFile bootloader.BootFi
 			p,
 			thisAsset.Role,
 		)
-		next, err = bootAssetsToLoadChains(assets[1:], kernelBootFile, roleToBlName, expectNew || i == 1)
+		next, err = bootAssetsToLoadChains(assets[1:], kernelBootFile, roleToBlName)
 		if err != nil {
 			return nil, err
 		}
