c54d3d174322272a91775b81748a2c15982976b3
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2024-03-21 16:36:52 +0000

    kernel: use normal mount points when building drivers tree

--- kernel/kernel_drivers_test.go
+++ kernel/kernel_drivers_test.go
@@ -158,7 +158,7 @@ func doDirChecks(c *C, dir string, expected []expectInode) {
 }
 
 func testBuildKernelDriversTree(c *C) {
-	mountDir := filepath.Join(dirs.RunDir, "mnt/pc-kernel")
+	mountDir := filepath.Join(dirs.SnapMountDir, "pc-kernel/1")
 	kversion := "5.15.0-78-generic"
 	createKernelSnapFiles(c, kversion, mountDir)
 
@@ -170,11 +170,11 @@ func testBuildKernelDriversTree(c *C) {
 	modsRoot := filepath.Join(dirs.SnapdStateDir(dirs.GlobalRootDir), "kernel", "pc-kernel", "1", "lib", "modules", kversion)
 	modsMntDir := filepath.Join(mountDir, "modules", kversion)
 	expected := []expectInode{
+		{"components", fs.ModeDir, ""},
 		{"kernel", fs.ModeSymlink, filepath.Join(modsMntDir, "kernel")},
 		{"modules.builtin.alias.bin", 0, ""},
 		{"modules.dep.bin", 0, ""},
 		{"modules.symbols", 0, ""},
-		{"updates", fs.ModeDir, ""},
 		{"vdso", fs.ModeSymlink, filepath.Join(modsMntDir, "vdso")},
 	}
 	doDirChecks(c, modsRoot, expected)
@@ -211,7 +211,7 @@ func (s *kernelDriversTestSuite) TestBuildKernelDriversNoModsOrFw(c *C) {
 	buf, restore := logger.MockLogger()
 	defer restore()
 
-	mountDir := filepath.Join(dirs.RunDir, "mnt/pc-kernel")
+	mountDir := filepath.Join(dirs.SnapMountDir, "pc-kernel/11")
 	c.Assert(os.MkdirAll(mountDir, 0755), IsNil)
 
 	// Build the tree should not fail
@@ -414,12 +414,12 @@ func testBuildKernelDriversTreeWithComps(c *C, opts *kernel.KernelDriversTreeOpt
 	mockCmd := testutil.MockCommand(c, "depmod", "")
 	defer mockCmd.Restore()
 
-	mountDir := filepath.Join(dirs.RunDir, "mnt/pc-kernel")
+	mountDir := filepath.Join(dirs.SnapMountDir, "pc-kernel/1")
 	kversion := "5.15.0-78-generic"
 	createKernelSnapFiles(c, kversion, mountDir)
 
-	compMntDir1 := filepath.Join(dirs.RunDir, "mnt/kernel-snaps/pc-kernel/components/1/comp1/11")
-	compMntDir2 := filepath.Join(dirs.RunDir, "mnt/kernel-snaps/pc-kernel/components/1/comp2/22")
+	compMntDir1 := filepath.Join(dirs.SnapMountDir, "pc-kernel/components/1/comp1")
+	compMntDir2 := filepath.Join(dirs.SnapMountDir, "pc-kernel/components/1/comp2")
 	createKernelModulesCompFiles(c, kversion, compMntDir1, "comp1")
 	createKernelModulesCompFiles(c, kversion, compMntDir2, "comp2")
 	kmods := []*snap.ComponentSideInfo{
@@ -443,17 +443,17 @@ func testBuildKernelDriversTreeWithComps(c *C, opts *kernel.KernelDriversTreeOpt
 	modsRoot := filepath.Join(dirs.SnapdStateDir(dirs.GlobalRootDir), "kernel", "pc-kernel", "1", "lib", "modules", kversion)
 	modsMntDir := filepath.Join(mountDir, "modules", kversion)
 	expected := []expectInode{
+		{"components", fs.ModeDir, ""},
 		{"kernel", fs.ModeSymlink, filepath.Join(modsMntDir, "kernel")},
 		{"modules.builtin.alias.bin", 0, ""},
 		{"modules.dep.bin", 0, ""},
 		{"modules.symbols", 0, ""},
-		{"updates", fs.ModeDir, ""},
 		{"vdso", fs.ModeSymlink, filepath.Join(modsMntDir, "vdso")},
 	}
 	doDirChecks(c, modsRoot, expected)
 
 	// Check links for modules shipped in components
-	updatesDir := filepath.Join(modsRoot, "updates")
+	updatesDir := filepath.Join(modsRoot, "components")
 	expected = []expectInode{
 		{"comp1", fs.ModeSymlink, filepath.Join(compMntDir1, "modules", kversion)},
 		{"comp2", fs.ModeSymlink, filepath.Join(compMntDir2, "modules", kversion)},
