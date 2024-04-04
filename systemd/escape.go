aacbe287eaf29f2f7348123aea540bd33ff01e80
Andrew Phelps <andrew.phelps@canonical.com>
2024-04-04 11:42:10 -0400

    Revert "systemd: add function that implements "systemd-escape" in addition to already existing "systemd-escape --path""
    This reverts commit 0521600ec8fa785b69d2b7a85fa8da9be4938a5a.

--- systemd/escape.go
+++ systemd/escape.go
@@ -28,38 +28,28 @@ import (
 
 const allowed = `:_.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`
 
-type escapeOptions struct {
-	path bool
-}
-
-func escape(in string, opts escapeOptions) string {
-	var buf bytes.Buffer
-
+// EscapeUnitNamePath works like systemd-escape --path
+// FIXME: we could use github.com/coreos/go-systemd/unit/escape.go and EscapePath
+// from it. But that's not in the archive and it won't work with go1.3
+func EscapeUnitNamePath(in string) string {
+	// "" is the same as "/" which is escaped to "-"
+	// the filepath.Clean will turn "" into "." and make this incorrect
 	if len(in) == 0 {
-		// "" is treated as "/" when treating the input as a path, which is escaped to "-".
-		// filepath.Clean would turn "" into "." and make this incorrect
-		if opts.path {
-			return "-"
-		}
-
-		// when not treating the input as a path, "" remains as an empty string.
-		return ""
+		return "-"
 	}
+	buf := bytes.NewBuffer(nil)
 
-	if opts.path {
-		// clean and trim leading/trailing "/"
-		in = filepath.Clean(in)
-		in = strings.Trim(in, "/")
+	// clean and trim leading/trailing "/"
+	in = filepath.Clean(in)
+	in = strings.Trim(in, "/")
 
-		// empty strings is "/"
-		if len(in) == 0 {
-			in = "/"
-		}
+	// empty strings is "/"
+	if len(in) == 0 {
+		in = "/"
 	}
-
 	// leading "." is special
 	if in[0] == '.' {
-		fmt.Fprintf(&buf, `\x%x`, in[0])
+		fmt.Fprintf(buf, `\x%x`, in[0])
 		in = in[1:]
 	}
 
@@ -71,21 +61,9 @@ func escape(in string, opts escapeOptions) string {
 		} else if strings.IndexByte(allowed, c) >= 0 {
 			buf.WriteByte(c)
 		} else {
-			fmt.Fprintf(&buf, `\x%x`, []byte{in[i]})
+			fmt.Fprintf(buf, `\x%x`, []byte{in[i]})
 		}
 	}
 
 	return buf.String()
-}
-
-// EscapeUnitNamePath works like systemd-escape --path
-// FIXME: we could use github.com/coreos/go-systemd/unit/escape.go and EscapePath
-// from it. But that's not in the archive and it won't work with go1.3
-func EscapeUnitNamePath(in string) string {
-	return escape(in, escapeOptions{path: true})
-}
-
-// EscapeUnitNamePath works like systemd-escape
-func EscapeUnitName(in string) string {
-	return escape(in, escapeOptions{})
 }
