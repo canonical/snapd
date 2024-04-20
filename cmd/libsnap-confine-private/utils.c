e63665aa2c9b585602259c47b76fca6825f6a6e3
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-04-20 09:30:14 +0200

    cmd/snap-confine: skip device cgroup setup when running inside a container (#13859)
    * cmd/libsnap-confine-private: helper for detecting if executing inside a container
    
    Add a helper which attempts to detect if the current process is executing inside
    a container environment. Specifically, look for /run/systemd/container and check
    whether it is non empty.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snap-confine: do not setup device cgroup if running inside a container
    
    Do not set up a device cgroup filter, if we're running inside the container. The
    rationale is that the container environment has already shut down device access
    sufficiently, and especially if running in unprivileged container, we may not be
    able to set it up correctly anyway.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snap-confine: allow reading of /run/systemd/container
    
    Allow snap-confine to read /run/system/container to implement container
    execution check.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snap-confine: use strnlen for sc_is_container
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
    
    ---------
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
    Co-authored-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/libsnap-confine-private/utils.c
+++ cmd/libsnap-confine-private/utils.c
@@ -261,3 +261,44 @@ bool sc_wait_for_file(const char *path, size_t timeout_sec)
 	}
 	return false;
 }
+
+const char *run_systemd_container = "/run/systemd/container";
+
+static bool _sc_is_in_container(const char *p)
+{
+	// see what systemd-detect-virt --container does in, see:
+	// https://github.com/systemd/systemd/blob/5dcd6b1d55a1cfe247621d70f0e25d020de6e0ed/src/basic/virt.c#L749-L755
+	// https://systemd.io/CONTAINER_INTERFACE/
+	FILE *in SC_CLEANUP(sc_cleanup_file) = fopen(p, "r");
+	if (in == NULL) {
+		return false;
+	}
+
+	char container[128] = { 0 };
+
+	if (fgets(container, sizeof(container), in) == NULL) {
+		/* nothing read or other error? */
+		return false;
+	}
+
+	size_t r = strnlen(container, sizeof container);
+	// TODO add sc_str_chomp()?
+	if (r > 0 && container[r - 1] == '\n') {
+		/* replace trailing newline */
+		container[r - 1] = 0;
+		r--;
+	}
+
+	if (r == 0) {
+		/* empty or just a newline */
+		return false;
+	}
+
+	debug("detected container environment: %s", container);
+	return true;
+}
+
+bool sc_is_in_container(void)
+{
+	return _sc_is_in_container(run_systemd_container);
+}
