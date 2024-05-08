0a3e5efdf40ff420cb90302de7e9d9cba0998ee6
Zeyad Gouda <zeyad.gouda@canonical.com>
2024-05-08 10:03:52 +0300

    daemon: attach affected-snaps data to tasks
    This allows API caller to associate each task with the
    relevant snaps which is useful in the case change progress
    tracking when a change is associated with multiple snaps.
    
    This exact use case is needed by snapd-desktop-integration.
    
    Signed-off-by: Zeyad Gouda <zeyad.gouda@canonical.com>

--- daemon/export_api_general_test.go
+++ daemon/export_api_general_test.go
@@ -56,3 +56,11 @@ func MockWarningsAccessors(okay func(*state.State, time.Time) int, all func(*sta
 type (
 	ChangeInfo = changeInfo
 )
+
+func MockSnapstateSnapsAffectedByTask(f func(t *state.Task) ([]string, error)) (restore func()) {
+	old := snapstateSnapsAffectedByTask
+	snapstateSnapsAffectedByTask = f
+	return func() {
+		snapstateSnapsAffectedByTask = old
+	}
+}
