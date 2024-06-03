a6c18a91e9de9a931d743df5462c5866e69dcd1a
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-06-03 20:55:32 -0300

    Squashed 'tests/lib/external/snapd-testing-tools/' changes from 1c8efb77e1..d3eed3faa5
    d3eed3faa5 fix codespell in CODE_OF_CONDUCT.md
    18bcca6b14 new log helper
    d60381fcd9 add run number to filtered filename
    5dde2d67b8 consider the tests execution in main
    6b9a3aabcc change filtered log name
    b2756aa579 default file is .filtered.log
    500b9dace4 Fix tests workflow
    45db26a3d2 fix shellcheck error in log-filter
    fe45c27b7d create a var to store filter params
    5a9b66d7dc filter spread results
    51f9b055af New tool used to filter the spread.log output
    b8d20c1d5b fix snaps.name test with correct siffix spelling
    f640ac72e3 Add missing test details
    f0754df304 Filter the error y debug output in log-parser
    fc10196efd Add suggestions to details
    94ac5ffe58 Add details on tests
    501578c719 add more checks in os.query to check is-core_xx
    e8929207ff fix os-query for ubuntu comparing with core
    226114641f os.query won't check SPREAD_SYSTEM anymore to compare core systems
    b89ec98b23 use local variables in os.query tool
    dacfd81de9 fix is_core functions
    1db5214d5f Improve the remote docs (#36)
    2e4a3153a2 1 more comment
    3a0fc57e1e add explanation about why we check for ( Do | Doing )
    4cf8e635bf fix os.query test after merge
    b89b4f8647 fix artifacts name
    d30cee6da0 Merge remote-tracking branch 'upstream/main'
    5ef5dcbe8f Tests use artifacts in spread tests (#51)
    555c43d2ab Support auto-refresh with Do instead of Doing
    96c2b0c19c remove tests support for ubuntu 23.04 (EoL)
    74082c0c34 Tests improve remote wait (#49)
    5121bfb659 remove support for opensuse leap 15.4 (#48)
    30df700d08 Add new systems support (#47)
    1f08938925 Support check amazon linux version (#46)
    43533bdd97 Change the exit value checking for test formats (#45)
    3c88244c04 Update check-test-format to support a dir and a list of files (#44)
    510d95f429 add extra check for error in auto-refresh detection function
    3289d4031b Try open the log with latin-1 encoding when utf-8 is not working
    9db785499f improved how the tools are waiting for system reboot
    2a5c4414a3 fix shellcheck errors
    5e7b63883d Fixes for osquery and tests pkgs (#43)
    4c9145e2ac support reboot waiting for auto-refresh
    45768f5188 show changes in unknown status after refresh
    8013c30c2a Remove support for ubuntu 22.10
    b32b80bf54 Fix remote.rait-for test in bionic
    5675c625e9 Enable fedora 38
    55f4471957 Support for new oss
    f2e88b357c New tool used to query spread json reports
    cacd35ede0 utils/spread-shellcheck: explain disabled warnings (#42)
    c82afb2dee Support --no-install-recommends parameter when installing dependencies with tests.pkgs
    b84eea92e2 spread-shellcheck: fix quotes in environment variables (#41)
    ab1e51c29f New comparison in os-query for core systems (#40)
    e5ae22a5d4 systemd units can be overwritten
    63540b845a Fix error messages in remote pull and push
    75e8a426a5 make sure the unit is removed in tests.systemd test
    9089ff5c02 Update tests to use the new tests.systemd stop-unit
    44ecd5e56a Move tests.systemd stop-units to stop-unit
    01a2a83b4b Update tests.systemd to have stop units as systemd.sh
    162e93bd35 update tests.systemd CLI options to be the same than retry command
    14aa43a405 new feature to re-run failed spread tests (#39)
    604cb782db Fix shellcheck in systemd tool
    bfc71082c8 Update the tests.systemd to allow parameters waiting for service status
    8a2d0a99df Adding quiet tool and removing set +-x from tests.pkgs
    d90935d2a4 A comment explaining about the default values for wait-for
    3232c5dba7 Add support for ubuntu 23.04
    a7164fba07 remove fedora 35 support, add fedora 37 support
    89b9eb5301 Update systems supported
    92bb6a0664 Include snap-sufix in the snaps.name tool
    
    git-subtree-dir: tests/lib/external/snapd-testing-tools
    git-subtree-split: d3eed3faa50fd6d666f35d016ad3571807a60dd3

---
+++ utils/log_helper.py
@@ -0,0 +1,165 @@
+"""
+This library provides a set of functions that can be used to
+process the spread output.
+"""
+
+import re
+
+# Tasks status
+PREPARING = 'Preparing'
+EXECUTING = 'Executing'
+RESTORING = 'Restoring'
+
+# Tasks info
+DEBUG = 'Debug'
+ERROR = 'Error'
+WARNING = 'WARNING:'
+
+# General actions
+REBOOTING = 'Rebooting'
+DISCARDING = 'Discarding'
+ALLOCATING = 'Allocating'
+WAITING = 'Waiting'
+CONNECTING = 'Connecting'
+SENDING = 'Sending'
+
+# Actions status
+ALLOCATED = 'Allocated'
+CONNECTED = 'Connected'
+
+# Results
+FAILED = 'Failed'
+SUCCESSFUL = 'Successful'
+ABORTED = 'Aborted'
+
+# Tasks levels
+TASK = 'task'
+SUITE = 'suite'
+PROJECT = 'project'
+
+# Start line
+START_LINE = '.*Project content is packed for delivery.*'
+
+OPERATIONS = [
+    PREPARING, EXECUTING, RESTORING,
+    REBOOTING , DISCARDING, ALLOCATING, WAITING,
+    ALLOCATED, CONNECTING , CONNECTED, SENDING,
+    ERROR, DEBUG, WARNING, FAILED, SUCCESSFUL, ABORTED
+    ]
+
+def _match_date(date):
+    return re.match(r'\d{4}-\d{2}-\d{2}', date)
+
+def _match_time(time):
+    return re.match(r'\d{2}:\d{2}:\d{2}', time)
+
+def is_start_line(line: str) -> bool: 
+    if not line:
+        return False
+
+    pattern = re.compile(START_LINE)
+    parts = line.strip().split(' ')
+    return len(parts) > 2 and \
+        _match_date(parts[0]) and \
+        _match_time(parts[1]) and \
+        re.match(pattern, line)
+
+# Debug line starts with the operation
+def is_line(line: str, operation: str) -> bool:
+    if not line:
+        return False
+
+    parts = line.strip().split(' ')
+    return len(parts) > 2 and \
+        _match_date(parts[0]) and \
+        _match_time(parts[1]) and \
+        parts[2] == operation
+
+# Debug line starts with the operation
+def is_operation(line: str) -> bool:
+    if not line:
+        return False
+
+    parts = line.strip().split(' ')
+    return len(parts) > 2 and \
+        _match_date(parts[0]) and \
+        _match_time(parts[1]) and \
+        parts[2] in OPERATIONS
+
+# Return the date in the line
+def get_date(line: str) -> str:
+    if not line:
+        raise RuntimeError("Empty line provided")
+
+    parts = line.strip().split(' ')
+    if len(parts) <= 2 or \
+        not _match_date(parts[0]):
+        raise ValueError("Incorrect format for line provided: {}".format(line))
+    
+    return parts[0]
+
+# Return the time in the line
+def get_time(line: str) -> str:
+    if not line:
+        raise RuntimeError("Empty line provided")
+
+    parts = line.strip().split(' ')
+    if len(parts) <= 2 or \
+        not _match_date(parts[0]) or \
+        not _match_time(parts[1]):
+        raise ValueError("Incorrect format for line provided: {}".format(line))
+    
+    return parts[1]
+
+# Return the operation in the line
+def get_operation(line: str) -> str:
+    if not line:
+        raise RuntimeError("Empty line provided")
+
+    parts = line.strip().split(' ')
+    if len(parts) <= 2 or \
+        not _match_date(parts[0]) or \
+        not _match_time(parts[1]):
+        raise ValueError("Incorrect format for line provided: {}".format(line))
+    
+    return parts[2]
+
+# Return the information that comes after the operation
+def get_operation_info(line: str) -> str:
+    if not line:
+        raise RuntimeError("Empty line provided")
+
+    parts = line.strip().split(' ')
+    if len(parts) <= 3 or \
+        not _match_date(parts[0]) or \
+        not _match_time(parts[1]):
+        raise ValueError("Incorrect format for line provided: {}".format(line))
+    
+    return ' '.join(parts[3:])
+
+# Details are displayed after Error/Debug/Failed/Warning operations
+def is_detail_start(line: str) -> bool:
+    return is_line(line, DEBUG) or \
+        is_line(line, ERROR) or \
+        is_line(line, WARNING) or \
+        is_line(line, FAILED)
+
+# Error/Debug/Failed output sometimes finish witr EOF error
+def is_detail_eof(line: str) -> bool:
+    parts = line.strip().split(' ')
+    return ( is_line(line, DEBUG) or \
+        is_line(line, ERROR) or \
+        is_line(line, WARNING) ) and \
+        parts[-1].strip() == 'EOF'
+
+# Error/Debug/Failed output finishes when a new other line starts
+def is_detail_finished(line: str) -> bool:
+    parts = line.strip().split(' ')
+    return len(parts) > 3 and \
+        _match_date(parts[0]) and \
+        _match_time(parts[1]) and \
+        parts[2] in OPERATIONS
+
+
+if __name__ == "__main__":
+    main()
