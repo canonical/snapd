cc11b65bdff3e7ab5705d6512ad2d6d12c04bb2e
Oliver Calder <oliver.calder@canonical.com>
2024-06-07 18:48:30 -0500

    i/prompting/patterns: add minimal tests for scan and render
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

---
+++ interfaces/prompting/patterns/render_test.go
@@ -0,0 +1,99 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*",
+ * Copyright (C) 2024 Canonical Ltd
+ *",
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *",
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *",
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, Text: see <http://www.gnu.org/licenses/>.
+ *",
+ */
+
+package patterns_test
+
+import (
+	"bytes"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces/prompting/patterns"
+)
+
+type renderSuite struct{}
+
+var _ = Suite(&renderSuite{})
+
+func (s *renderSuite) TestRender(c *C) {
+	pattern := "/{,usr/}lib{,32,64,x32}/{,@{multiarch}/{,atomics/}}ld{-*,64}.so*"
+	scanned := patterns.Scan(pattern)
+	parsed, err := patterns.Parse(scanned)
+	c.Assert(err, IsNil)
+
+	expectedExpansions := []string{
+		"/lib/ld-*.so*",
+		"/lib/ld64.so*",
+		"/lib/@multiarch/ld-*.so*",
+		"/lib/@multiarch/ld64.so*",
+		"/lib/@multiarch/atomics/ld-*.so*",
+		"/lib/@multiarch/atomics/ld64.so*",
+		"/lib32/ld-*.so*",
+		"/lib32/ld64.so*",
+		"/lib32/@multiarch/ld-*.so*",
+		"/lib32/@multiarch/ld64.so*",
+		"/lib32/@multiarch/atomics/ld-*.so*",
+		"/lib32/@multiarch/atomics/ld64.so*",
+		"/lib64/ld-*.so*",
+		"/lib64/ld64.so*",
+		"/lib64/@multiarch/ld-*.so*",
+		"/lib64/@multiarch/ld64.so*",
+		"/lib64/@multiarch/atomics/ld-*.so*",
+		"/lib64/@multiarch/atomics/ld64.so*",
+		"/libx32/ld-*.so*",
+		"/libx32/ld64.so*",
+		"/libx32/@multiarch/ld-*.so*",
+		"/libx32/@multiarch/ld64.so*",
+		"/libx32/@multiarch/atomics/ld-*.so*",
+		"/libx32/@multiarch/atomics/ld64.so*",
+		"/usr/lib/ld-*.so*",
+		"/usr/lib/ld64.so*",
+		"/usr/lib/@multiarch/ld-*.so*",
+		"/usr/lib/@multiarch/ld64.so*",
+		"/usr/lib/@multiarch/atomics/ld-*.so*",
+		"/usr/lib/@multiarch/atomics/ld64.so*",
+		"/usr/lib32/ld-*.so*",
+		"/usr/lib32/ld64.so*",
+		"/usr/lib32/@multiarch/ld-*.so*",
+		"/usr/lib32/@multiarch/ld64.so*",
+		"/usr/lib32/@multiarch/atomics/ld-*.so*",
+		"/usr/lib32/@multiarch/atomics/ld64.so*",
+		"/usr/lib64/ld-*.so*",
+		"/usr/lib64/ld64.so*",
+		"/usr/lib64/@multiarch/ld-*.so*",
+		"/usr/lib64/@multiarch/ld64.so*",
+		"/usr/lib64/@multiarch/atomics/ld-*.so*",
+		"/usr/lib64/@multiarch/atomics/ld64.so*",
+		"/usr/libx32/ld-*.so*",
+		"/usr/libx32/ld64.so*",
+		"/usr/libx32/@multiarch/ld-*.so*",
+		"/usr/libx32/@multiarch/ld64.so*",
+		"/usr/libx32/@multiarch/atomics/ld-*.so*",
+		"/usr/libx32/@multiarch/atomics/ld64.so*",
+	}
+
+	expansions := make([]string, 0, len(expectedExpansions))
+	patterns.RenderAllVariants(parsed, func(i int, buf *bytes.Buffer) {
+		expansions = append(expansions, buf.String())
+	})
+
+	c.Check(expansions, DeepEquals, expectedExpansions)
+
+	c.Check(expansions, HasLen, parsed.NumVariants())
+}
---
+++ interfaces/prompting/patterns/scan_test.go
@@ -0,0 +1,75 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, Text: see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package patterns_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces/prompting/patterns"
+)
+
+type scanSuite struct{}
+
+var _ = Suite(&scanSuite{})
+
+func (s *scanSuite) TestScan(c *C) {
+	pattern := "/{,usr/}lib{,32,64,x32}/{,@{multiarch}/{,atomics/}}ld{-*,64}.so*"
+
+	expectedTokens := []patterns.Token{
+		patterns.Token{Type: patterns.TokText, Text: "/"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "usr/"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokText, Text: "lib"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "32"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "64"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "x32"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokText, Text: "/"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "@"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokText, Text: "multiarch"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokText, Text: "/"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "atomics/"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokText, Text: "ld"},
+		patterns.Token{Type: patterns.TokBraceOpen, Text: "{"},
+		patterns.Token{Type: patterns.TokText, Text: "-*"},
+		patterns.Token{Type: patterns.TokComma, Text: ","},
+		patterns.Token{Type: patterns.TokText, Text: "64"},
+		patterns.Token{Type: patterns.TokBraceClose, Text: "}"},
+		patterns.Token{Type: patterns.TokText, Text: ".so*"},
+	}
+
+	tokens := patterns.Scan(pattern)
+
+	c.Check(tokens, DeepEquals, expectedTokens)
+}
