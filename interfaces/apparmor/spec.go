0d3ab0aefe7fed3aec5960abb3a76f51e1246885
Sergio Costas <sergio.costas@canonical.com>
2024-06-11 13:19:35 +0200

    Update interfaces/apparmor/spec.go
    Co-authored-by: Zygmunt Bazyli Krynicki <me@zygoon.pl>

--- interfaces/apparmor/spec.go
+++ interfaces/apparmor/spec.go
@@ -64,7 +64,7 @@ type Specification struct {
 
 	// prioritizedSnippets are just like snippets, but they have a priority value
 	// and a snippet key. An interface can add a snippet with a specific key and a priority.
-	// If there doesn't exist any snippet with that UID, the passed snippet will be
+	// If there doesn't exist any snippet with that key, the passed snippet will be
 	// added as-is. But if it does exist, the new snippet will replace the old one if
 	// the new priority is bigger than the old one; will be appended if the new
 	// priority is the same as the old one, and will be discarded if the new priority
