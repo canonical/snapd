canonical/snapd

commit 182b8ae699e39bd3707cec80777f58e5baed34c6

snap-seccomp, snap-confine, i/seccomp, tests: rework seccomp denylist (#13443)
* snap-{seccomp,confine}: rework seccomp denylist

When a denylist was introduced in PR#12849 we reached the limits
of the API of libseccomp and some things are now hard to reason
about [1]. This is mostly because what we try to do does not
match the libseccomp API very well and a slightly different
approach is probably more aligned with it's design (see also
this libseccomp issue [2] that is related to our issue).

So this commit changes the approach and instead of trying to use
a single filter the work is split into two filters:
1. explicit allow list
2. explicit deny list

and then load both filters into the kernel. The way the kernel
works is that it selects the most restrictive action.

So in the case of PR#12849:
```
~ioctl - TIOCSTI
~ioctl - TIOCLINUX
ioctl
```
For `ioctl(TIOCLINUX)` the first allow filter would pass `ioctl`
but the second deny filter would correctly deny the TIOCLINUX.

The file format of the `snap.snap.app.bin` changes to `.bin2`
and includes a proper header that would allow us to extend more
easily in the future.

The exiting tests for negative filtering got also updated so that
the deny/allow is tested via different errno numbers to ensure that
the expected filter denies the access.

The `snap-seccomp` spread test now also runs on all ubuntu releases.

This work will also allow us to remove the `global.bin` seccomp
filter in a followup PR.

[1] https://github.com/snapcore/snapd/pull/12849#discussion_r1206855700
[2] https://github.com/seccomp/libseccomp/issues/44

* snap-confine: tweak sc_seccomp_file_header struct (thanks Philip!)

* snap-confine: tweak struct init workaround in sc_apply_seccomp_profile_for_security_tag (thanks Philip)

* snap-seccomp: remove outdated comment about big endian

* snap-confine: rename sc_must_read_header_from_file->sc_must_read_and_validate_header_from_file

cmd/snap-seccomp/main.go
@@ -210,7 +210,9 @@ import "C"
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -582,11 +584,12 @@ var (
 	errnoOnImplicitDenial int16 = C.EPERM
 )
 
-func parseLine(line string, secFilter *seccomp.ScmpFilter) error {
+func parseLine(line string, secFilterAllow, secFilterDeny *seccomp.ScmpFilter) error {
 	// ignore comments and empty lines
 	if strings.HasPrefix(line, "#") || line == "" {
 		return nil
 	}
+	secFilter := secFilterAllow
 
 	// regular line
 	tokens := strings.Fields(line)
@@ -603,6 +606,7 @@ func parseLine(line string, secFilter *seccomp.ScmpFilter) error {
 	if strings.HasPrefix(syscallName, "~") {
 		action = seccomp.ActErrno.SetReturnCode(errnoOnExplicitDenial)
 		syscallName = syscallName[1:]
+		secFilter = secFilterDeny
 	}
 
 	secSyscall, err := seccomp.GetSyscallFromName(syscallName)
@@ -690,8 +694,11 @@ func parseLine(line string, secFilter *seccomp.ScmpFilter) error {
 	if err = secFilter.AddRuleConditionalExact(secSyscall, action, conds); err != nil {
 		err = secFilter.AddRuleConditional(secSyscall, action, conds)
 	}
+	if err != nil {
+		return fmt.Errorf("cannot add rule for line %q: %v", line, err)
+	}
 
-	return err
+	return nil
 }
 
 // used to mock in tests
@@ -788,18 +795,116 @@ func complainAction() seccomp.ScmpAction {
 	return seccomp.ActAllow
 }
 
+var osCreateTemp = os.CreateTemp
+
+func exportBPF(fout *os.File, filter *seccomp.ScmpFilter) (bpfLen int64, err error) {
+	// TODO: use a common way to handle prefixed errors across snapd
+	errPrefixFmt := "cannot export bpf filter: %w"
+
+	oldPos, err := fout.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, fmt.Errorf(errPrefixFmt, err)
+	}
+	if err := filter.ExportBPF(fout); err != nil {
+		return 0, fmt.Errorf(errPrefixFmt, err)
+	}
+	nowPos, err := fout.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, fmt.Errorf(errPrefixFmt, err)
+	}
+
+	return nowPos - oldPos, nil
+}
+
+// New .bin2 seccomp files are composed by the following header, and potentially one
+// allow filter and/or one deny filter (if lenAllowFilter and lenDenyFilter are greater
+// than 0 respectively). When more than one filter is loaded, the kernel applies
+// the most restrictive action, thus any explicit deny will take precedence.
+// This struct needs to be in sync with seccomp-support.c
+type scSeccompFileHeader struct {
+	header  [2]byte
+	version byte
+	// flags
+	unrestricted byte
+	// unused
+	padding [4]byte
+	// location of allow/deny, all offsets/len in bytes
+	lenAllowFilter uint32
+	lenDenyFilter  uint32
+	// reserved for future use
+	reserved2 [112]byte
+}
+
+func writeUnrestrictedFilter(outFile string) error {
+	hdr := scSeccompFileHeader{
+		header:  [2]byte{'S', 'C'},
+		version: 0x1,
+		// tell snap-confine
+		unrestricted: 0x1,
+	}
+	fout, err := osutil.NewAtomicFile(outFile, 0644, 0, osutil.NoChown, osutil.NoChown)
+	if err != nil {
+		return err
+	}
+	defer fout.Cancel()
+
+	if err := binary.Write(fout, arch.Endian(), hdr); err != nil {
+		return err
+	}
+	return fout.Commit()
+}
+
+func writeSeccompFilter(outFile string, filterAllow, filterDeny *seccomp.ScmpFilter) error {
+	fout, err := osutil.NewAtomicFile(outFile, 0644, 0, osutil.NoChown, osutil.NoChown)
+	if err != nil {
+		return err
+	}
+	defer fout.Cancel()
+
+	// Write preliminary header because we don't know the sizes of the
+	// seccomp filters yet and the only way to know is to export to
+	// a file (until seccomp_export_bpf_mem() becomes available)
+	hdr := scSeccompFileHeader{
+		header:  [2]byte{'S', 'C'},
+		version: 0x1,
+	}
+	if err := binary.Write(fout, arch.Endian(), hdr); err != nil {
+		return err
+	}
+	allowSize, err := exportBPF(fout.File, filterAllow)
+	if err != nil {
+		return err
+	}
+	denySize, err := exportBPF(fout.File, filterDeny)
+	if err != nil {
+		return err
+	}
+
+	// now write final header
+	hdr.lenAllowFilter = uint32(allowSize)
+	hdr.lenDenyFilter = uint32(denySize)
+	if _, err := fout.Seek(0, io.SeekStart); err != nil {
+		return err
+	}
+	if err := binary.Write(fout, arch.Endian(), hdr); err != nil {
+		return err
+	}
+
+	return fout.Commit()
+}
+
 func compile(content []byte, out string) error {
 	var err error
-	var secFilter *seccomp.ScmpFilter
+	var secFilterAllow, secFilterDeny *seccomp.ScmpFilter
 
 	unrestricted, complain := preprocess(content)
 	switch {
 	case unrestricted:
-		return osutil.AtomicWrite(out, bytes.NewBufferString("@unrestricted\n"), 0644, 0)
+		return writeUnrestrictedFilter(out)
 	case complain:
 		var complainAct seccomp.ScmpAction = complainAction()
 
-		secFilter, err = seccomp.NewFilter(complainAct)
+		secFilterAllow, err = seccomp.NewFilter(complainAct)
 		if err != nil {
 			if complainAct != seccomp.ActAllow {
 				// ActLog is only supported in newer versions
@@ -807,8 +912,15 @@ func compile(content []byte, out string) error {
 				// libseccomp-golang. Attempt to fall back to
 				// ActAllow before erroring out.
 				complainAct = seccomp.ActAllow
-				secFilter, err = seccomp.NewFilter(complainAct)
+				secFilterAllow, err = seccomp.NewFilter(complainAct)
 			}
+		}
+		if err != nil {
+			return fmt.Errorf("cannot create allow seccomp filter: %s", err)
+		}
+		secFilterDeny, err = seccomp.NewFilter(complainAct)
+		if err != nil {
+			return fmt.Errorf("cannot create deny seccomp filter: %s", err)
 		}
 
 		// Set unrestricted to 'true' to fallback to the pre-ActLog
@@ -818,19 +930,26 @@ func compile(content []byte, out string) error {
 			unrestricted = true
 		}
 	default:
-		secFilter, err = seccomp.NewFilter(seccomp.ActErrno.SetReturnCode(errnoOnImplicitDenial))
+		secFilterAllow, err = seccomp.NewFilter(seccomp.ActErrno.SetReturnCode(errnoOnImplicitDenial))
+		if err != nil {
+			return fmt.Errorf("cannot create seccomp filter: %s", err)
+		}
+		secFilterDeny, err = seccomp.NewFilter(seccomp.ActAllow)
+		if err != nil {
+			return fmt.Errorf("cannot create seccomp filter: %s", err)
+		}
 	}
-	if err != nil {
-		return fmt.Errorf("cannot create seccomp filter: %s", err)
+	if err := addSecondaryArches(secFilterAllow); err != nil {
+		return err
 	}
-	if err := addSecondaryArches(secFilter); err != nil {
+	if err := addSecondaryArches(secFilterDeny); err != nil {
 		return err
 	}
 
 	if !unrestricted {
 		scanner := bufio.NewScanner(bytes.NewBuffer(content))
 		for scanner.Scan() {
-			if err := parseLine(scanner.Text(), secFilter); err != nil {
+			if err := parseLine(scanner.Text(), secFilterAllow, secFilterDeny); err != nil {
 				return fmt.Errorf("cannot parse line: %s", err)
 			}
 		}
@@ -840,21 +959,14 @@ func compile(content []byte, out string) error {
 	}
 
 	if osutil.GetenvBool("SNAP_SECCOMP_DEBUG") {
-		secFilter.ExportPFC(os.Stdout)
+		secFilterAllow.ExportPFC(os.Stdout)
+		secFilterDeny.ExportPFC(os.Stdout)
 	}
 
-	// write atomically
-	fout, err := osutil.NewAtomicFile(out, 0644, 0, osutil.NoChown, osutil.NoChown)
-	if err != nil {
+	if err := writeSeccompFilter(out, secFilterAllow, secFilterDeny); err != nil {
 		return err
 	}
-	// Cancel once Committed is a NOP
-	defer fout.Cancel()
-
-	if err := secFilter.ExportBPF(fout.File); err != nil {
-		return err
-	}
-	return fout.Commit()
+	return nil
 }
 
 // caches for uid and gid lookups

cmd/Makefile.am
@@ -356,6 +356,7 @@ snap_confine_unit_tests_SOURCES = \
 	snap-confine/cookie-support-test.c \
 	snap-confine/mount-support-test.c \
 	snap-confine/ns-support-test.c \
+	snap-confine/seccomp-support-test.c \
 	snap-confine/snap-confine-args-test.c \
 	snap-confine/snap-confine-invocation-test.c
 snap_confine_unit_tests_CFLAGS = $(snap_confine_snap_confine_CFLAGS) $(GLIB_CFLAGS)

cmd/snap-confine/seccomp-support-test.c
@@ -1,0 +1,193 @@
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "seccomp-support-ext.c"
+#include "seccomp-support.c"
+
+#include <glib.h>
+#include <glib/gstdio.h>
+
+static void make_seccomp_profile(struct sc_seccomp_file_header *hdr, int *fd,
+				 char **fname)
+{
+	*fd = g_file_open_tmp(NULL, fname, NULL);
+	g_assert_true(*fd > 0);
+	int written = write(*fd, hdr, sizeof(struct sc_seccomp_file_header));
+	g_assert_true(written == sizeof(struct sc_seccomp_file_header));
+}
+
+static void test_must_read_and_validate_header_from_file__happy(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+	};
+	char SC_CLEANUP(sc_cleanup_string) * profile = NULL;
+	int SC_CLEANUP(sc_cleanup_close) fd = 0;
+	make_seccomp_profile(&hdr, &fd, &profile);
+
+	FILE *file SC_CLEANUP(sc_cleanup_file) = fopen(profile, "rb");
+	sc_must_read_and_validate_header_from_file(file, profile, &hdr);
+	g_assert_true(file != NULL);
+}
+
+static void test_must_read_and_validate_header_from_file__missing_file(void)
+{
+	struct sc_seccomp_file_header hdr;
+	const char *profile = "/path/to/missing/file";
+	const char *expected_err =
+	    "cannot open seccomp filter /path/to/missing/file: No such file or directory\n";
+
+	if (g_test_subprocess()) {
+		FILE *file SC_CLEANUP(sc_cleanup_file) = fopen(profile, "rb");
+		sc_must_read_and_validate_header_from_file(file, profile, &hdr);
+		// the function above is expected to call die()
+		g_assert_not_reached();
+		// reference "file" to keep the compiler from warning
+		// that "file" is unused
+		g_assert_null(file);
+	}
+	g_test_trap_subprocess(NULL, 0, 0);
+	g_test_trap_assert_failed();
+	g_test_trap_assert_stderr(expected_err);
+}
+
+static void must_read_and_validate_header_from_file_dies_with(struct
+							      sc_seccomp_file_header
+							      hdr, const char
+							      *err_msg)
+{
+	if (g_test_subprocess()) {
+		char SC_CLEANUP(sc_cleanup_string) * profile = NULL;
+		int SC_CLEANUP(sc_cleanup_close) fd = 0;
+		make_seccomp_profile(&hdr, &fd, &profile);
+
+		FILE *file SC_CLEANUP(sc_cleanup_file) = fopen(profile, "rb");
+		sc_must_read_and_validate_header_from_file(file, profile, &hdr);
+		// the function above is expected to call die()
+		g_assert_not_reached();
+		// reference "file" to keep the compiler from warning
+		// that "file" is unused
+		g_assert_null(file);
+	}
+
+	g_test_trap_subprocess(NULL, 0, 0);
+	g_test_trap_assert_failed();
+	g_test_trap_assert_stderr(err_msg);
+}
+
+static void test_must_read_and_validate_header_from_file__invalid_header(void)
+{
+	//  when we stop supporting 14.04 we could just use hdr = {0}
+	struct sc_seccomp_file_header hdr;
+	memset(&hdr, 0, sizeof hdr);
+	const char *expected_err = "unexpected seccomp header: 00\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void test_must_read_and_validate_header_from_file__invalid_version(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 0,
+	};
+	const char *expected_err = "unexpected seccomp file version: 0\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void
+test_must_read_and_validate_header_from_file__len_allow_too_big(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_allow_filter = MAX_BPF_SIZE + 1,
+	};
+	const char *expected_err = "allow filter size too big 32769\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void
+test_must_read_and_validate_header_from_file__len_allow_no_multiplier(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_allow_filter = sizeof(struct sock_filter) + 1,
+	};
+	const char *expected_err =
+	    "allow filter size not multiple of sock_filter\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void test_must_read_and_validate_header_from_file__len_deny_too_big(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_deny_filter = MAX_BPF_SIZE + 1,
+	};
+	const char *expected_err = "deny filter size too big 32769\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void
+test_must_read_and_validate_header_from_file__len_deny_no_multiplier(void)
+{
+	struct sc_seccomp_file_header hdr = {
+		.header[0] = 'S',
+		.header[1] = 'C',
+		.version = 1,
+		.len_deny_filter = sizeof(struct sock_filter) + 1,
+	};
+	const char *expected_err =
+	    "deny filter size not multiple of sock_filter\n";
+	must_read_and_validate_header_from_file_dies_with(hdr, expected_err);
+}
+
+static void __attribute__((constructor)) init(void)
+{
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/happy",
+	     test_must_read_and_validate_header_from_file__happy);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/missing_file",
+	     test_must_read_and_validate_header_from_file__missing_file);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/invalid_header",
+	     test_must_read_and_validate_header_from_file__invalid_header);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/invalid_version",
+	     test_must_read_and_validate_header_from_file__invalid_version);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/len_allow_too_big",
+	     test_must_read_and_validate_header_from_file__len_allow_too_big);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/len_allow_no_multiplier",
+	     test_must_read_and_validate_header_from_file__len_allow_no_multiplier);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/len_deny_too_big",
+	     test_must_read_and_validate_header_from_file__len_deny_too_big);
+	g_test_add_func
+	    ("/seccomp/must_read_and_validate_header_from_file/len_deny_no_multiplier",
+	     test_must_read_and_validate_header_from_file__len_deny_no_multiplier);
+}

cmd/snap-confine/seccomp-support.c
@@ -17,11 +17,13 @@
 #include "config.h"
 #include "seccomp-support.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 #include <sys/prctl.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
@@ -44,6 +46,30 @@ static const char *filter_profile_dir = "/var/lib/snapd/seccomp/bpf/";
 #define MAX_BPF_SIZE (32 * 1024)
 
 typedef struct sock_filter bpf_instr;
+
+// Keep in sync with snap-seccomp/main.go
+//
+// Header of a seccomp.bin2 filter file in native byte order.
+struct __attribute__((__packed__)) sc_seccomp_file_header {
+	// header: "SC"
+	char header[2];
+	// version: 0x1
+	uint8_t version;
+	// flags
+	uint8_t unrestricted;
+	// unused
+	uint8_t padding[4];
+
+	// size of allow filter in byte
+	uint32_t len_allow_filter;
+	// size of deny filter in byte
+	uint32_t len_deny_filter;
+	// reserved for future use
+	uint8_t reserved2[112];
+};
+
+static_assert(sizeof(struct sc_seccomp_file_header) == 128,
+	      "unexpected struct size");
 
 static void validate_path_has_strict_perms(const char *path)
 {
@@ -109,12 +135,94 @@ static void validate_bpfpath_is_safe(const char *path)
 	}
 }
 
+static void sc_cleanup_sock_fprog(struct sock_fprog *prog)
+{
+	free(prog->filter);
+	prog->filter = NULL;
+}
+
+static void sc_must_read_filter_from_file(FILE *file, uint32_t len_bytes,
+					  char *what, struct sock_fprog *prog)
+{
+	if (len_bytes == 0) {
+		die("%s filter may only be empty in unrestricted profiles",
+		    what);
+	}
+	prog->len = len_bytes / sizeof(struct sock_filter);
+	prog->filter = malloc(len_bytes);
+	if (prog->filter == NULL) {
+		die("cannot allocate %u bytes of memory for %s seccomp filter ",
+		    len_bytes, what);
+	}
+	size_t num_read =
+	    fread(prog->filter, 1, prog->len * sizeof(struct sock_filter),
+		  file);
+	if (ferror(file)) {
+		die("cannot read %s filter", what);
+	}
+	if (num_read != len_bytes) {
+		die("short read for filter %s %zu != %i", what, num_read,
+		    len_bytes);
+	}
+}
+
+static void sc_must_read_and_validate_header_from_file(FILE *file,
+						       const char *profile_path,
+						       struct
+						       sc_seccomp_file_header
+						       *hdr)
+{
+	if (file == NULL) {
+		die("cannot open seccomp filter %s", profile_path);
+	}
+	size_t num_read =
+	    fread(hdr, 1, sizeof(struct sc_seccomp_file_header), file);
+	if (ferror(file) != 0) {
+		die("cannot read seccomp profile %s", profile_path);
+	}
+	if (num_read < sizeof(struct sc_seccomp_file_header)) {
+		die("short read on seccomp header: %zu", num_read);
+	}
+	if (hdr->header[0] != 'S' || hdr->header[1] != 'C') {
+		die("unexpected seccomp header: %x%x", hdr->header[0],
+		    hdr->header[1]);
+	}
+	if (hdr->version != 1) {
+		die("unexpected seccomp file version: %x", hdr->version);
+	}
+	if (hdr->len_allow_filter > MAX_BPF_SIZE) {
+		die("allow filter size too big %u", hdr->len_allow_filter);
+	}
+	if (hdr->len_allow_filter % sizeof(struct sock_filter) != 0) {
+		die("allow filter size not multiple of sock_filter");
+	}
+	if (hdr->len_deny_filter > MAX_BPF_SIZE) {
+		die("deny filter size too big %u", hdr->len_deny_filter);
+	}
+	if (hdr->len_deny_filter % sizeof(struct sock_filter) != 0) {
+		die("deny filter size not multiple of sock_filter");
+	}
+	struct stat stat_buf;
+	if (fstat(fileno(file), &stat_buf) != 0) {
+		die("cannot fstat the seccomp file");
+	}
+	off_t expected_size =
+	    sizeof(struct sc_seccomp_file_header) + hdr->len_allow_filter +
+	    hdr->len_deny_filter;
+	if (stat_buf.st_size != expected_size) {
+		die("unexpected filesize %ju != %ju", stat_buf.st_size,
+		    expected_size);
+	}
+}
+
 bool sc_apply_seccomp_profile_for_security_tag(const char *security_tag)
 {
 	debug("loading bpf program for security tag %s", security_tag);
 
 	char profile_path[PATH_MAX] = { 0 };
-	sc_must_snprintf(profile_path, sizeof(profile_path), "%s/%s.bin",
+	struct sock_fprog SC_CLEANUP(sc_cleanup_sock_fprog) prog_allow = { 0 };
+	struct sock_fprog SC_CLEANUP(sc_cleanup_sock_fprog) prog_deny = { 0 };
+	sc_must_snprintf(profile_path, sizeof(profile_path), "%s/%s.bin2",
 			 filter_profile_dir, security_tag);
 
 	// Wait some time for the security profile to show up. When
@@ -151,24 +259,25 @@ bool sc_apply_seccomp_profile_for_security_tag(const char *security_tag)
 	// set on the system.
 	validate_bpfpath_is_safe(profile_path);
 
-	/* The extra space has dual purpose. First of all, it is required to detect
-	 * feof() while still being able to correctly read MAX_BPF_SIZE bytes of
-	 * seccomp profile.  In addition, because we treat the profile as a
-	 * quasi-string and use sc_streq(), to compare it. The extra space is used
-	 * as a way to ensure the result is a terminated string (though in practice
-	 * it can contain embedded NULs any earlier position). Note that
-	 * sc_read_seccomp_filter knows about the extra space and ensures that the
-	 * buffer is never empty. */
-	char bpf[MAX_BPF_SIZE + 1] = { 0 };
-	size_t num_read = sc_read_seccomp_filter(profile_path, bpf, sizeof bpf);
-	if (sc_streq(bpf, "@unrestricted\n")) {
+	//  when we stop supporting 14.04 we could just use hdr = {0}
+	struct sc_seccomp_file_header hdr;
+	memset(&hdr, 0, sizeof hdr);
+	FILE *file SC_CLEANUP(sc_cleanup_file) = fopen(profile_path, "rb");
+
+	sc_must_read_and_validate_header_from_file(file, profile_path, &hdr);
+	if (hdr.unrestricted & 0x1) {
 		return false;
 	}
-	struct sock_fprog prog = {
-		.len = num_read / sizeof(struct sock_filter),
-		.filter = (struct sock_filter *)bpf,
-	};
-	sc_apply_seccomp_filter(&prog);
+	// populate allow
+	sc_must_read_filter_from_file(file, hdr.len_allow_filter, "allow",
+				      &prog_allow);
+	sc_must_read_filter_from_file(file, hdr.len_deny_filter, "deny",
+				      &prog_deny);
+
+	// apply both filters
+	sc_apply_seccomp_filter(&prog_deny);
+	sc_apply_seccomp_filter(&prog_allow);
+
 	return true;
 }
 

cmd/snap-confine/seccomp-support.h
@@ -23,7 +23,7 @@
  * sc_apply_seccomp_profile_for_security_tag applies a seccomp profile to the
  * current process. The filter is loaded from a pre-compiled bpf bytecode
  * stored in "/var/lib/snap/seccomp/bpf" using the security tag and the
- * extension ".bin". All components along that path must be owned by root and
+ * extension ".bin2". All components along that path must be owned by root and
  * cannot be writable by UNIX _other_.
  *
  * The security tag is shared with other parts of snapd.

cmd/snap-confine/snap-confine.apparmor.in
@@ -148,8 +148,11 @@
     # deny change_profile unsafe /** -> {unconfined,/**},
     # change_profile unsafe /** -> **,
 
-    # reading seccomp filters
-    /{tmp/snap.rootfs_*/,}var/lib/snapd/seccomp/bpf/*.bin r,
+    # reading seccomp filters.
+    # Note 1: We still need to consider .bin extension because of global.bin file.
+    # Note 2: This rule is not needed because of rule '/var/lib/** rw', however we keep it because at
+    # some point we want to investigate if we can narrow the scope of the aforementioned rule.
+    /{tmp/snap.rootfs_*/,}var/lib/snapd/seccomp/bpf/*.bin{,2} r,
 
     # adding a missing bpf mount
     mount fstype=bpf options=(rw) bpf -> /sys/fs/bpf/,
@@ -288,7 +291,7 @@
     # pivot_root mediation in AppArmor is not complete. See LP: #1791711.
     # However, we can mediate the new_root and put_old to be what we expect,
     # and then deny directory creation within old_root to prevent trivial
-    # pivoting into a whitelisted path.
+    # pivoting into an allowlisted path.
     pivot_root oldroot=/tmp/snap.rootfs_*/var/lib/snapd/hostfs/ /tmp/snap.rootfs_*/,
     # Explicitly deny creating the old_root directory in case it is
     # inadvertently added somewhere else. While this doesn't resolve

cmd/snap-seccomp/export_test.go
@@ -19,11 +19,18 @@
 
 package main
 
+import (
+	"os"
+
+	"github.com/snapcore/snapd/testutil"
+)
+
 var (
 	Compile           = compile
 	SeccompResolver   = seccompResolver
 	VersionInfo       = versionInfo
 	GoSeccompFeatures = goSeccompFeatures
+	ExportBPF         = exportBPF
 )
 
 func MockArchDpkgArchitecture(f func() string) (restore func()) {
@@ -64,4 +71,10 @@ func MockSeccompSyscalls(syscalls []string) (resture func()) {
 	return func() {
 		seccompSyscalls = old
 	}
+}
+
+func MockOsCreateTemp(f func(dir, pattern string) (*os.File, error)) (restore func()) {
+	restore = testutil.Backup(&osCreateTemp)
+	osCreateTemp = f
+	return restore
 }

commit 23ad066af92612d616cc0f287526aabfa928caf2

cmd/snap-seccomp: fix default action on deny filter in devmode
Recent change to seccomp processing has introduced a regression
from commit 182b8ae699e39bd3707cec80777f58e5baed34c6
"snap-seccomp, snap-confine, i/seccomp, tests: rework seccomp denylist * snap-{seccomp,confine}: rework seccomp denylist"
where the deny processing of devmode snaps was incorrectly logging *all* system
calls (not denying, just logging). Restore the correct default action on the
deny filter to "allow", as the deny filter explicitly denies on matching system
calls.

Jira: SNAPDENG-22672
Thanks-To: Canonical Certification Team

Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

cmd/snap-seccomp/main.go
@@ -918,7 +918,13 @@ func compile(content []byte, out string) error {
 		if err != nil {
 			return fmt.Errorf("cannot create allow seccomp filter: %s", err)
 		}
-		secFilterDeny, err = seccomp.NewFilter(complainAct)
+
+		// Deny filter uses "act allow" as a default action, as it is only
+		// populated with deny rules. Any matching it does results in an
+		// explicit denial. When seccomp profiles are loaded the most
+		// restrictive action is used, so without any matching allow rules in
+		// the same filter, all system calls would be forever denied.
+		secFilterDeny, err = seccomp.NewFilter(seccomp.ActAllow)
 		if err != nil {
 			return fmt.Errorf("cannot create deny seccomp filter: %s", err)
 		}
@@ -934,6 +940,11 @@ func compile(content []byte, out string) error {
 		if err != nil {
 			return fmt.Errorf("cannot create seccomp filter: %s", err)
 		}
+		// Deny filter uses "act allow" as a default action, as it is only
+		// populated with deny rules. Any matching it does results in an
+		// explicit denial. When seccomp profiles are loaded the most
+		// restrictive action is used, so without any matching allow rules in
+		// the same filter, all system calls would be forever denied.
 		secFilterDeny, err = seccomp.NewFilter(seccomp.ActAllow)
 		if err != nil {
 			return fmt.Errorf("cannot create seccomp filter: %s", err)