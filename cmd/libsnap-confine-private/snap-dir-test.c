e899ee3dc8f39c4473f54ebafb3b256ecd338ec0
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2024-06-28 09:29:23 +0200

    cmd/libsnap-confine-private: recognize relative /snap symlink
    The symlink /snap -> /var/lib/snapd/snap may be alternatively encoded as /snap
    -> var/lib/snapd/snap in order to stay compatible with symlink packaging rules.
    
    Adjusts tests to handle both variatns and tweak naming to match the non-test code.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/libsnap-confine-private/snap-dir-test.c
+++ cmd/libsnap-confine-private/snap-dir-test.c
@@ -42,7 +42,7 @@ static void test_sc_probe_snap_mount_dir__absent(void) {
     g_assert_cmpstr(snap_mount_dir, ==, "/var/lib/snapd/snap");
 }
 
-static void test_sc_probe_snap_mount_dir__preferred(void) {
+static void test_sc_probe_snap_mount_dir__canonical(void) {
     char *root_dir = g_dir_make_tmp(NULL, NULL);
     g_assert_nonnull(root_dir);
     g_test_queue_free(root_dir);
@@ -63,7 +63,7 @@ static void test_sc_probe_snap_mount_dir__preferred(void) {
     g_assert_cmpstr(snap_mount_dir, ==, "/snap");
 }
 
-static void test_sc_probe_snap_mount_dir__fallback(void) {
+static void test_sc_probe_snap_mount_dir__alternate_absolute(void) {
     char *root_dir = g_dir_make_tmp(NULL, NULL);
     g_assert_nonnull(root_dir);
     g_test_queue_free(root_dir);
@@ -77,6 +77,32 @@ static void test_sc_probe_snap_mount_dir__fallback(void) {
     char *proc_snap_symlink = g_build_filename(proc_root_dir, "snap", NULL);
     g_test_queue_free(proc_snap_symlink);
     ret = symlink("/var/lib/snapd/snap", proc_snap_symlink);
+    g_assert_cmpint(ret, ==, 0);
+
+    int root_fd = open(root_dir, O_PATH | O_DIRECTORY | O_NOFOLLOW | O_CLOEXEC);
+    g_assert_cmpint(root_fd, !=, -1);
+    g_test_queue_destroy((GDestroyNotify)close_noerr, (void *)(uintptr_t)root_fd);
+
+    sc_probe_snap_mount_dir_from_pid_1_mount_ns(root_fd, NULL);
+    const char *snap_mount_dir = sc_snap_mount_dir(NULL);
+    g_assert_nonnull(snap_mount_dir);
+    g_assert_cmpstr(snap_mount_dir, ==, "/var/lib/snapd/snap");
+}
+
+static void test_sc_probe_snap_mount_dir__alternate_relative(void) {
+    char *root_dir = g_dir_make_tmp(NULL, NULL);
+    g_assert_nonnull(root_dir);
+    g_test_queue_free(root_dir);
+    g_test_queue_destroy((GDestroyNotify)rm_rf_tmp, root_dir);
+
+    char *proc_root_dir = g_build_filename(root_dir, "/proc/1/root/", NULL);
+    g_test_queue_free(proc_root_dir);
+    int ret = g_mkdir_with_parents(proc_root_dir, 0755);
+    g_assert_cmpint(ret, ==, 0);
+
+    char *proc_snap_symlink = g_build_filename(proc_root_dir, "snap", NULL);
+    g_test_queue_free(proc_snap_symlink);
+    ret = symlink("var/lib/snapd/snap", proc_snap_symlink);
     g_assert_cmpint(ret, ==, 0);
 
     int root_fd = open(root_dir, O_PATH | O_DIRECTORY | O_NOFOLLOW | O_CLOEXEC);
@@ -131,8 +157,9 @@ static void test_sc_snap_mount_dir__not_probed(void) {
 
 static void __attribute__((constructor)) init(void) {
     g_test_add_func("/snap-dir/probe-absent", test_sc_probe_snap_mount_dir__absent);
-    g_test_add_func("/snap-dir/probe-preferred", test_sc_probe_snap_mount_dir__preferred);
-    g_test_add_func("/snap-dir/probe-fallback", test_sc_probe_snap_mount_dir__fallback);
+    g_test_add_func("/snap-dir/probe-canonical", test_sc_probe_snap_mount_dir__canonical);
+    g_test_add_func("/snap-dir/probe-alternate-absolute", test_sc_probe_snap_mount_dir__alternate_absolute);
+    g_test_add_func("/snap-dir/probe-alternate-relative", test_sc_probe_snap_mount_dir__alternate_relative);
     g_test_add_func("/snap-dir/probe-bad-symlink-target", test_sc_probe_snap_mount_dir__bad_symlink_target);
     g_test_add_func("/snap-dir/dir-not-probed", test_sc_snap_mount_dir__not_probed);
 }
