4f540066ebcf0f40a838e5a9d0a7514ce4607d0b
Andrew Phelps <andrew.phelps@canonical.com>
2024-07-09 13:41:50 -0400

    o/snapstate: add TODO about future work for updatePlan methods

--- overlord/snapstate/target.go
+++ overlord/snapstate/target.go
@@ -763,6 +763,8 @@ func (p *updatePlan) refreshAll() bool {
 	return len(p.requested) == 0
 }
 
+// TODO: consider making functions that call this method into a method on
+// updatePlan
 func (p *updatePlan) targetInfos() []*snap.Info {
 	infos := make([]*snap.Info, 0, len(p.targets))
 	for _, t := range p.targets {
