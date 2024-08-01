b49b8249143e9101a5918efef2612c2ab3e092ce
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2024-08-01 14:39:06 +0200

    nvidia: add support module with known nvidia libraries and globs
    Test data contains a list of shared objects from numerous nvidia drivers,
    spanning Ubuntu 18.04 to 22.04, with data from driver 390, 470, 510, 515,
    525, 530, 535, 545 and 550.
    
    Go unit tests verify that each glob,  matches at least one library stored in
    test data, and that each library is matched by at least one glob.  The only
    exception is globs corresponding to libglvndr, which are not present in the
    driver libraries.
    
    Jira: https://warthogs.atlassian.net/browse/SNAPDENG-26663
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ nvidia/nvidia_test.go
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package nvidia_test
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/snapcore/snapd/nvidia"
+	"github.com/snapcore/snapd/strutil"
+)
+
+func TestAllLibrariesMatchSomePattern(t *testing.T) {
+	paths, err := loadPathsFromAllLibFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Globs do not contain the common prefix which is specific to distribution packaging structure.
+	prefix := strutil.CommonPrefix(paths)
+
+	for _, path := range paths {
+		shortPath := strings.TrimPrefix(path, prefix)
+		if !matchesAny(shortPath, nvidia.RegularGlobs) && !matchesAny(shortPath, nvidia.LibGlvndGlobs) && !matchesAny(shortPath, nvidia.ExceptionGlobs) {
+			t.Errorf("Path is not matched by any glob: %s (short path: %s)", path, shortPath)
+		}
+	}
+}
+
+func matchesAny(path string, globs []string) bool {
+	for _, glob := range globs {
+		ok, err := filepath.Match(glob, path)
+		if err != nil {
+			panic(err) // Glob has wrong syntax.
+		}
+		if ok {
+			return ok
+		}
+	}
+
+	return false
+}
+
+func TestAllPatternsMatchSomeLibraries(t *testing.T) {
+	paths, err := loadPathsFromAllLibFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Globs do not contain the common prefix which is specific to distribution packaging structure.
+	prefix := strutil.CommonPrefix(paths)
+
+	for _, globList := range [][]string{nvidia.RegularGlobs, nvidia.ExceptionGlobs} {
+		matched := false
+		for _, pattern := range globList {
+			for _, path := range paths {
+				ok, err := filepath.Match(pattern, strings.TrimPrefix(path, prefix))
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if ok {
+					matched = true
+					break
+				}
+			}
+
+			if !matched {
+				// We have exhausted all files but the pattern did not match.
+				t.Errorf("Pattern does not match any file: %s", pattern)
+			}
+		}
+	}
+}
+
+func loadPathsFromAllLibFiles() ([]string, error) {
+	libFiles, err := filepath.Glob("testdata/*.libs")
+	if err != nil {
+		return nil, err
+	}
+
+	// Get all the libraries from all the *.lib files.
+	return loadPathsFromFiles(libFiles)
+}
+
+func loadPathsFromFiles(fileNames []string) (paths []string, err error) {
+	for _, fileName := range fileNames {
+		err := func() error {
+			f, err := os.Open(fileName)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			sc := bufio.NewScanner(f)
+			for sc.Scan() {
+				path := string(bytes.TrimSpace(sc.Bytes()))
+				paths = append(paths, path)
+			}
+
+			return sc.Err()
+		}()
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return paths, nil
+}
