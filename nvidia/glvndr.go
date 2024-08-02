850c4c7b2978651fba4637daa90be6dc6e5c4401
Zygmunt Bazyli Krynicki <zygmunt.krynicki@canonical.com>
2024-08-02 14:20:21 +0200

    nvidia: add support module with known nvidia libraries and globs (#14293)
    * strutil: add CommonPrefix function
    
    The function finds and returns a prefix common to a slice of strings.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
    
    * nvidia: add support module with known nvidia libraries and globs
    
    Test data contains a list of shared objects from numerous nvidia drivers,
    spanning Ubuntu 18.04 to 22.04, with data from driver 390, 470, 510, 515,
    525, 530, 535, 545 and 550.
    
    Go unit tests verify that each glob,  matches at least one library stored in
    test data, and that each library is matched by at least one glob.  The only
    exception is globs corresponding to libglvndr, which are not present in the
    driver libraries.
    
    Jira: https://warthogs.atlassian.net/browse/SNAPDENG-26663
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
    
    * nvidia/testdata: add more library listings
    
    Add more library listings collected using tooling from
    https://github.com/bboozzoo/nvidia-drivers-collect-libs/
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * nvidia: filter out known packaging error data
    
    The nvidia-drivers-515 release on 18.04 contained a bug where a file was placed
    at `/NVIDIA-Linux/..` path. Filter it out.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    ---------
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    Co-authored-by: Maciej Borzecki <maciej.borzecki@canonical.com>

---
+++ nvidia/glvndr.go
@@ -0,0 +1,35 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package nvidia
+
+// LibGlvndGlobs are related to libglvndr library.
+//
+// The library is further described at https://github.com/NVIDIA/libglvnd.
+var LibGlvndGlobs = []string{
+	"libEGL.so*",          // OpenGL for EGL window system.
+	"libGLX_indirect.so*", // OpenGL for X11 window system, with indirect rendering.
+	"libGLX.so*",          // OpenGL for X11 window system, with direct rendering.
+	"libGL.so*",           // Wrapper around libGldispatch and libGLX.
+	"libOpenGL.so*",       // Wrapper for libGLdispatch exposing openGL.
+	"libGLESv1_CM.so*",    // Wrapper for libGLdispatch exposing openGL ES v1 (common profile).
+	"libGLESv2.so*",       // Wrapper for libGLdispatch exposing openGL ES v2.
+	"libGLdispatch.so*",   // Pass-through dispatching calls to vendor-specific library based on EGL or GLX rendering context.
+	"libGLU.so*",          // OpenGL utility library.
+}
