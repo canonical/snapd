c2c2f38d1d5fe284c0b5761ae2bbf00bfd1f7bed
Dilyn Corner <dilyn.corner@canonical.com>
2024-08-09 16:34:00 -0400

    many: remove unecessary commandline arguments
    Signed-off-by: Dilyn Corner <dilyn.corner@canonical.com>

--- bootloader/assets/grub.go
+++ bootloader/assets/grub.go
@@ -36,7 +36,7 @@ var cmdlineForArch = map[string][]ForEditions{
 		{FirstEdition: 3, Snippet: []byte("console=ttyS0,115200n8 console=tty1 panic=-1")},
 	},
 	"riscv64": {
-		{FirstEdition: 1, Snippet: []byte("console=ttyS0,115200n8 earlycon panic=-1")},
+		{FirstEdition: 1, Snippet: []byte("panic=-1")},
 	},
 }
 
