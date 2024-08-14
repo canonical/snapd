0a352722e52e14e561748e63b4e0fe102ce8b497
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-08-14 12:27:35 +0200

    o/configstate/configcore: enable and start prompting handlers (#14351)
    * tests/lib/snaps/test-snapd-prompt-handler: use disabled install mode
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * o/configstate/configcore: use testutil.Mock
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * overlord/configstate/configcore: start prompt handler services
    
    Start all the prompt handler services, such that the whole of request prompts
    may function and we do not end up with auto denial.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * tests/main/interfaces-requests-activates-handlers: verify handler service activation
    
    Add a spread test to verify prompt handler service activation when the apparmor
    prompting feature gets enabled.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    ---------
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/configstate/configcore/prompting_test.go
+++ overlord/configstate/configcore/prompting_test.go
@@ -20,18 +20,26 @@
 package configcore_test
 
 import (
+	"errors"
 	"fmt"
+	"os/user"
+	"time"
 
 	. "gopkg.in/check.v1"
+	"gopkg.in/tomb.v2"
 
 	"github.com/snapcore/snapd/boot"
+	"github.com/snapcore/snapd/client"
 	"github.com/snapcore/snapd/features"
 	"github.com/snapcore/snapd/interfaces"
 	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/overlord"
 	"github.com/snapcore/snapd/overlord/configstate/config"
 	"github.com/snapcore/snapd/overlord/configstate/configcore"
+	"github.com/snapcore/snapd/overlord/hookstate"
 	"github.com/snapcore/snapd/overlord/ifacestate/ifacerepo"
 	"github.com/snapcore/snapd/overlord/restart"
+	"github.com/snapcore/snapd/overlord/servicestate"
 	"github.com/snapcore/snapd/overlord/snapstate"
 	"github.com/snapcore/snapd/overlord/snapstate/snapstatetest"
 	"github.com/snapcore/snapd/overlord/state"
@@ -44,7 +52,8 @@ import (
 type promptingSuite struct {
 	configcoreSuite
 
-	repo *interfaces.Repository
+	repo     *interfaces.Repository
+	overlord *overlord.Overlord
 }
 
 var _ = Suite(&promptingSuite{})
@@ -56,6 +65,24 @@ func (s *promptingSuite) SetUpTest(c *C) {
 		[]string{"policy:permstable32:prompt"}, nil,
 		[]string{"prompt"}, nil,
 	))
+
+	s.overlord = overlord.MockWithState(nil)
+	// override state set up by configcoreSuite
+	s.state = s.overlord.State()
+	s.overlord.AddManager(s.overlord.TaskRunner())
+
+	// default handler for mock task
+	s.overlord.TaskRunner().AddHandler("service-command",
+		func(task *state.Task, tomb *tomb.Tomb) error { return nil },
+		nil)
+
+	// mock a reasonable handler so that tests not checking this specific
+	// bit can stay relatively simple
+	restore := configcore.MockServicestateControl(func(st *state.State, appInfos []*snap.AppInfo, inst *servicestate.Instruction, cu *user.User, flags *servicestate.Flags, context *hookstate.Context) ([]*state.TaskSet, error) {
+		tsk := st.NewTask("service-command", "mock task")
+		return []*state.TaskSet{state.NewTaskSet(tsk)}, nil
+	})
+	s.AddCleanup(restore)
 
 	s.repo = interfaces.NewRepository()
 	for _, iface := range builtin.Interfaces() {
@@ -79,6 +106,12 @@ func (s *promptingSuite) TestDoExperimentalApparmorPromptingDaemonRestartNoPrist
 
 	s.mockSnapd(c)
 	s.mockPromptingHandler(c, mockPromptingHandlerOpts{snapName: "test-snap", hasHandler: true})
+
+	c.Assert(s.overlord.StartUp(), IsNil)
+	s.overlord.Loop()
+	defer func() {
+		c.Assert(s.overlord.Stop(), IsNil)
+	}()
 
 	snap, confName := features.AppArmorPrompting.ConfigOption()
 
@@ -124,6 +157,12 @@ func (s *promptingSuite) TestDoExperimentalApparmorPromptingDaemonRestartWithPri
 	s.mockSnapd(c)
 	s.mockPromptingHandler(c, mockPromptingHandlerOpts{snapName: "test-snap", hasHandler: true})
 
+	c.Assert(s.overlord.StartUp(), IsNil)
+	s.overlord.Loop()
+	defer func() {
+		c.Assert(s.overlord.Stop(), IsNil)
+	}()
+
 	snap, confName := features.AppArmorPrompting.ConfigOption()
 
 	testCases := []struct {
@@ -147,6 +186,7 @@ func (s *promptingSuite) TestDoExperimentalApparmorPromptingDaemonRestartWithPri
 			true,
 		},
 	}
+
 	for _, testCase := range testCases {
 		s.state.Lock()
 		rt := configcore.NewRunTransaction(config.NewTransaction(s.state), nil)
@@ -296,6 +336,12 @@ func (s *promptingSuite) TestDoExperimentalApparmorPromptingOnCoreDesktop(c *C) 
 	})
 	defer restore()
 
+	c.Assert(s.overlord.StartUp(), IsNil)
+	s.overlord.Loop()
+	defer func() {
+		c.Assert(s.overlord.Stop(), IsNil)
+	}()
+
 	snap, confName := features.AppArmorPrompting.ConfigOption()
 
 	// one cannot enable prompting if it's not supported
@@ -369,6 +415,131 @@ func (s *promptingSuite) TestDoExperimentalApparmorPromptingChecksHandlersDiscon
 
 	s.testDoExperimentalApparmorPromptingUnsupported(c,
 		"cannot enable prompting feature no interfaces requests handler services are installed")
+}
+
+type promptEnableWithServicesTestCase struct {
+	serviceControlErr error
+	taskErr           error
+
+	expectedError string
+
+	timeout bool
+}
+
+func (s *promptingSuite) testDoExperimentalApparmorPromptingHandlerServices(c *C, tc promptEnableWithServicesTestCase) {
+	restore := release.MockOnClassic(true)
+	defer restore()
+
+	s.mockSnapd(c)
+	s.mockPromptingHandler(c, mockPromptingHandlerOpts{snapName: "test-snap1", hasHandler: true})
+	s.mockPromptingHandler(c, mockPromptingHandlerOpts{snapName: "test-snap2", hasHandler: true})
+
+	restartCalled := 0
+	restore = configcore.MockRestartRequest(func(st *state.State, t restart.RestartType, rebootInfo *boot.RebootInfo) {
+		restartCalled++
+	})
+	defer restore()
+
+	if tc.timeout {
+		restore = configcore.MockServicestateChangeTimeout(time.Microsecond)
+		defer restore()
+	}
+
+	s.overlord.TaskRunner().AddHandler("service-command",
+		func(task *state.Task, tomb *tomb.Tomb) error {
+			if tc.timeout {
+				time.Sleep(time.Second)
+			}
+			return tc.taskErr
+		},
+		nil)
+
+	servicestateControlCalls := 0
+	restore = configcore.MockServicestateControl(
+		func(
+			st *state.State, appInfos []*snap.AppInfo, inst *servicestate.Instruction,
+			cu *user.User, flags *servicestate.Flags, context *hookstate.Context,
+		) ([]*state.TaskSet, error) {
+			servicestateControlCalls++
+
+			c.Check(cu, IsNil)
+			c.Assert(inst, NotNil)
+			c.Check(inst.Action, Equals, "start")
+			c.Check(inst.Enable, Equals, true)
+			c.Check(inst.Scope, DeepEquals, client.ScopeSelector([]string{"user"}))
+
+			// 2 mock handlers
+			seen := map[string]bool{}
+			for _, ai := range appInfos {
+				seen[ai.String()] = true
+			}
+
+			c.Check(seen, DeepEquals, map[string]bool{
+				"test-snap1.prompts-handler": true,
+				"test-snap2.prompts-handler": true,
+			})
+
+			if tc.serviceControlErr != nil {
+				return nil, tc.serviceControlErr
+			}
+
+			tsk := st.NewTask("service-command", "mock task")
+			return []*state.TaskSet{state.NewTaskSet(tsk)}, nil
+		})
+	defer restore()
+
+	c.Assert(s.overlord.StartUp(), IsNil)
+	s.overlord.Loop()
+	defer func() {
+		c.Assert(s.overlord.Stop(), IsNil)
+	}()
+
+	snap, confName := features.AppArmorPrompting.ConfigOption()
+
+	// one cannot enable prompting if it's not supported
+	s.state.Lock()
+	rt := configcore.NewRunTransaction(config.NewTransaction(s.state), nil)
+	rt.Set(snap, confName, true)
+	s.state.Unlock()
+
+	err := configcore.DoExperimentalApparmorPromptingDaemonRestart(rt, nil)
+	if tc.expectedError != "" {
+		c.Check(err, ErrorMatches, tc.expectedError)
+		c.Check(restartCalled, Equals, 0)
+	} else {
+		c.Check(err, IsNil)
+		c.Check(restartCalled, Equals, 1)
+	}
+
+	c.Check(servicestateControlCalls, Equals, 1)
+}
+
+func (s *promptingSuite) TestDoExperimentalApparmorPromptingHandlerServicesHappy(c *C) {
+	s.testDoExperimentalApparmorPromptingHandlerServices(c, promptEnableWithServicesTestCase{})
+}
+
+func (s *promptingSuite) TestDoExperimentalApparmorPromptingHandlerServicesActivationFails(c *C) {
+	s.testDoExperimentalApparmorPromptingHandlerServices(c, promptEnableWithServicesTestCase{
+		taskErr: fmt.Errorf("mock start error"),
+
+		expectedError: `(?s)cannot enable prompting, unable to start prompting handlers: cannot perform the following tasks:.- mock task \(mock start error\)`,
+	})
+}
+
+func (s *promptingSuite) TestDoExperimentalApparmorPromptingHandlerServicesServiceStateFails(c *C) {
+	s.testDoExperimentalApparmorPromptingHandlerServices(c, promptEnableWithServicesTestCase{
+		serviceControlErr: fmt.Errorf("mock error"),
+
+		expectedError: "cannot enable prompting, unable to start prompting handlers: mock error",
+	})
+}
+
+func (s *promptingSuite) TestDoExperimentalApparmorPromptingHandlerServicesTimeout(c *C) {
+	s.testDoExperimentalApparmorPromptingHandlerServices(c, promptEnableWithServicesTestCase{
+		timeout: true,
+
+		expectedError: "cannot enable prompting, unable to start prompting handlers: timeout waiting for handler services start to complete",
+	})
 }
 
 func (s *promptingSuite) mockSnapd(c *C) {
@@ -450,10 +621,20 @@ plugs:
 		plugStatic["handler-service"] = "prompts-handler"
 	}
 
-	s.state.Set("conns", map[string]interface{}{
-		fmt.Sprintf("%s:snap-interfaces-requests-control core:snap-interfaces-requests-control", name): map[string]interface{}{
-			"interface":   "snap-interfaces-requests-control",
-			"plug-static": plugStatic,
-		},
-	})
+	var conns map[string]interface{}
+	err := s.state.Get("conns", &conns)
+	if err != nil {
+		if errors.Is(err, state.ErrNoState) {
+			conns = map[string]interface{}{}
+		} else {
+			c.Fatalf("unexpected error: %v", err)
+		}
+	}
+
+	conns[fmt.Sprintf("%s:snap-interfaces-requests-control core:snap-interfaces-requests-control", name)] = map[string]interface{}{
+		"interface":   "snap-interfaces-requests-control",
+		"plug-static": plugStatic,
+	}
+
+	s.state.Set("conns", conns)
 }
