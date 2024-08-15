88f3961ad8745e8382e48ea5587a3c2779f42020
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-08-15 14:57:59 -0300

    tests: remove support for ubuntu 23.10
    Ubuntu mantic is already EoL
    
    Ubuntu-no-lts will be in github workflow but no executions will be done.
    This is done to avoid removing it and force everyone to rebase master.

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -376,7 +376,7 @@ distro_install_build_snapd(){
         cp /etc/apt/sources.list sources.list.back
         echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -c -s)-proposed restricted main multiverse universe" | tee /etc/apt/sources.list -a
         apt update
-        if os.query is-ubuntu-ge 23.10; then
+        if os.query is-ubuntu-ge 24.04; then
             apt install -y --only-upgrade -t "$(lsb_release -c -s)-proposed" snapd
         else
             apt install -y --only-upgrade snapd
