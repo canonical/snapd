3a86b68fa9cd20a9964fd5ae5552eada574ffc27
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-08-26 12:51:11 +0200

    cmd/snap: use subcommand for respective execution aspect
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/snap/cmd_debug_execution.go
+++ cmd/snap/cmd_debug_execution.go
@@ -20,6 +20,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -28,73 +29,84 @@ import (
 	"github.com/snapcore/snapd/snapdtool"
 )
 
-type cmdDebugExec struct {
-	Tool        string `long:"tool"`
-	Positionals struct {
-		What string `required:"yes" positional-arg-name:"<what>"`
-	} `positional-args:"true"`
-}
+type cmdDebugExec struct{}
 
 func init() {
-	addDebugCommand("execution",
+	cmd := addDebugCommand("execution",
 		"Obtain information about execution aspects of snap toolchain commands",
 		`Display debugging information about aspects of snap toolchain execution, such
 as reexecution, tools location etc.`,
 		func() flags.Commander { return &cmdDebugExec{} },
-		map[string]string{
-			"tool": "Internal tool name",
-		}, []argDesc{{
-			name: "<what>",
-			desc: "What aspect, one of: snap, apparmor, internal-tool",
-		}})
+		nil,
+		nil,
+	)
+	cmd.extra = func(c *flags.Command) {
+		c.AddCommand("apparmor", "Show apparmor", "", &cmdDebugExecAppArmor{})
+		c.AddCommand("snap", "Show snap execution info", "", &cmdDebugExecSnap{})
+		c.AddCommand("internal-tool", "Show internal tool execution info", "", &cmdDebugExecInternalTool{})
+	}
 }
 
 func (x *cmdDebugExec) Execute(args []string) error {
-	if len(args) > 0 {
-		return ErrExtraArgs
+	return flag.ErrHelp
+}
+
+type cmdDebugExecAppArmor struct{}
+
+func (x *cmdDebugExecAppArmor) Execute(args []string) error {
+	cmd, internal, err := apparmor.AppArmorParser()
+	if err != nil {
+		fmt.Fprintf(Stdout, "apparmor-parser: error:%s\n", err.Error())
+		fmt.Fprint(Stdout, "internal: false\n")
+		return err
+	} else {
+		fmt.Fprintf(Stdout, "apparmor-parser: %s\n", cmd.Path)
+		fmt.Fprintf(Stdout, "internal: %v\n", internal)
 	}
-	switch x.Positionals.What {
-	case "snap":
-		fmt.Fprintf(Stdout, "distro-supports-reexec: %v\n", snapdtool.DistroSupportsReExec())
-		fmt.Fprintf(Stdout, "is-reexec-enabled: %v\n", snapdtool.IsReexecEnabled())
-		fmt.Fprintf(Stdout, "is-reexec-explicitly-enabled: %v\n", snapdtool.IsReexecExplicitlyEnabled())
+	return nil
+}
 
-		isReexecd, err := snapdtool.IsReexecd()
-		if err == nil {
-			fmt.Fprintf(Stdout, "is-reexecd: %v\n", isReexecd)
-		} else {
-			fmt.Fprintf(Stdout, "is-reexecd: error:%v\n", err)
-		}
+type cmdDebugExecSnap struct{}
 
-		exe, err := os.Readlink("/proc/self/exe")
-		if err != nil {
-			return err
-		}
+func (x *cmdDebugExecSnap) Execute(args []string) error {
+	fmt.Fprintf(Stdout, "distro-supports-reexec: %v\n", snapdtool.DistroSupportsReExec())
+	fmt.Fprintf(Stdout, "is-reexec-enabled: %v\n", snapdtool.IsReexecEnabled())
+	fmt.Fprintf(Stdout, "is-reexec-explicitly-enabled: %v\n", snapdtool.IsReexecExplicitlyEnabled())
 
-		fmt.Fprintf(Stdout, "self-exe: %v\n", exe)
-
-	case "apparmor":
-		cmd, internal, err := apparmor.AppArmorParser()
-		if err != nil {
-			fmt.Fprintf(Stdout, "apparmor-parser: error:%s\n", err.Error())
-			fmt.Fprint(Stdout, "internal: false\n")
-			return err
-		} else {
-			fmt.Fprintf(Stdout, "apparmor-parser: %s\n", cmd.Path)
-			fmt.Fprintf(Stdout, "internal: %v\n", internal)
-		}
-
-	case "internal-tool":
-		if x.Tool == "" {
-			return fmt.Errorf("tool name is missing")
-		}
-
-		tool, err := snapdtool.InternalToolPath(x.Tool)
-		if err != nil {
-			return err
-		}
-
-		fmt.Fprintf(Stdout, "%s: %s\n", x.Tool, tool)
+	isReexecd, err := snapdtool.IsReexecd()
+	if err == nil {
+		fmt.Fprintf(Stdout, "is-reexecd: %v\n", isReexecd)
+	} else {
+		fmt.Fprintf(Stdout, "is-reexecd: error:%v\n", err)
 	}
+
+	exe, err := os.Readlink("/proc/self/exe")
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(Stdout, "self-exe: %v\n", exe)
+
+	return nil
+}
+
+type cmdDebugExecInternalTool struct {
+	Positionals struct {
+		Tool string `required:"yes" positional-arg-name:"<tool>" description:"internal tool name"`
+	} `positional-args:"true"`
+}
+
+func (x *cmdDebugExecInternalTool) Execute(args []string) error {
+	if x.Positionals.Tool == "" {
+		return fmt.Errorf("tool name is missing")
+	}
+
+	tool, err := snapdtool.InternalToolPath(x.Positionals.Tool)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(Stdout, "%s: %s\n", x.Positionals.Tool, tool)
+
 	return nil
 }
