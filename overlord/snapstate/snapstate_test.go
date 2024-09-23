bc37fa60711107006204e0fc9d7f29977ad1c4bd
ernestl <ernest.lotter@canonical.com>
2024-09-23 22:37:38 +0200

    overlord/snapstate: remove snapd-snap experimental feature implementation

--- overlord/snapstate/snapstate_test.go
+++ overlord/snapstate/snapstate_test.go
@@ -403,7 +403,6 @@ func AddForeignTaskHandlers(runner *state.TaskRunner, tracker ForeignTaskTracker
 	runner.AddHandler("validate-snap", fakeHandler, nil)
 	runner.AddHandler("validate-component", fakeHandler, nil)
 	runner.AddHandler("transition-ubuntu-core", fakeHandler, nil)
-	runner.AddHandler("transition-to-snapd-snap", fakeHandler, nil)
 	runner.AddHandler("update-gadget-assets", fakeHandler, nil)
 	runner.AddHandler("update-managed-boot-config", fakeHandler, nil)
 
@@ -6061,236 +6060,6 @@ func (s *snapmgrTestSuite) TestTransitionCoreValidationSetsRevision(c *C) {
 	})
 }
 
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapDoesNotRunWithoutSnaps(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	// no snaps installed on this system (e.g. fresh classic)
-	snapstate.Set(s.state, "core", nil)
-
-	s.settle(c)
-
-	c.Check(s.state.Changes(), HasLen, 0)
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapDoesRunWithAnySnap(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	// some snap installed on this system but no core
-	snapstate.Set(s.state, "core", nil)
-	snapstate.Set(s.state, "foo", &snapstate.SnapState{
-		Active:   true,
-		Sequence: snapstatetest.NewSequenceFromSnapSideInfos([]*snap.SideInfo{{RealName: "foo", SnapID: "foo-id", Revision: snap.R(1), Channel: "beta"}}),
-		Current:  snap.R(1),
-	})
-
-	s.settle(c)
-
-	c.Check(s.state.Changes(), HasLen, 1)
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapDoesNotRunWhenNotEnabled(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	snapstate.Set(s.state, "core", &snapstate.SnapState{
-		Active:   true,
-		Sequence: snapstatetest.NewSequenceFromSnapSideInfos([]*snap.SideInfo{{RealName: "core", SnapID: "core-snap-id", Revision: snap.R(1), Channel: "beta"}}),
-		Current:  snap.R(1),
-		SnapType: "os",
-	})
-
-	s.settle(c)
-
-	c.Check(s.state.Changes(), HasLen, 0)
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapStartsAutomaticallyWhenEnabled(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	snapstate.Set(s.state, "core", &snapstate.SnapState{
-		Active:   true,
-		Sequence: snapstatetest.NewSequenceFromSnapSideInfos([]*snap.SideInfo{{RealName: "core", SnapID: "core-snap-id", Revision: snap.R(1), Channel: "beta"}}),
-		Current:  snap.R(1),
-		SnapType: "os",
-	})
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	s.settle(c)
-
-	c.Assert(s.state.Changes(), HasLen, 1)
-	chg := s.state.Changes()[0]
-	c.Check(chg.Kind(), Equals, "transition-to-snapd-snap")
-	c.Assert(chg.Err(), IsNil)
-	c.Assert(chg.IsReady(), Equals, true)
-
-	// snapd snap is installed from the default channel
-	var snapst snapstate.SnapState
-	snapstate.Get(s.state, "snapd", &snapst)
-	c.Assert(snapst.TrackingChannel, Equals, "latest/stable")
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapWithCoreRunthrough(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	// setup a classic model so the device context says we are on classic
-	defer snapstatetest.MockDeviceModel(ClassicModel())()
-
-	snapstate.Set(s.state, "core", &snapstate.SnapState{
-		Active:   true,
-		Sequence: snapstatetest.NewSequenceFromSnapSideInfos([]*snap.SideInfo{{RealName: "core", SnapID: "core-snap-id", Revision: snap.R(1), Channel: "edge"}}),
-		Current:  snap.R(1),
-		SnapType: "os",
-		// TrackingChannel
-		TrackingChannel: "latest/beta",
-	})
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	s.settle(c)
-
-	c.Assert(s.state.Changes(), HasLen, 1)
-	chg := s.state.Changes()[0]
-	c.Assert(chg.Kind(), Equals, "transition-to-snapd-snap")
-	c.Assert(chg.Err(), IsNil)
-	c.Assert(chg.IsReady(), Equals, true)
-	c.Check(s.fakeStore.downloads, HasLen, 1)
-	ts := state.NewTaskSet(chg.Tasks()...)
-	// task set was reconstituted from change tasks, so edges information is
-	// lost
-	verifyInstallTasks(c, snap.TypeSnapd, noConfigure|noLastBeforeModificationsEdge, 0, ts)
-
-	// ensure preferences from the core snap got transferred over
-	var snapst snapstate.SnapState
-	snapstate.Get(s.state, "snapd", &snapst)
-	c.Assert(snapst.TrackingChannel, Equals, "latest/beta")
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapTimeLimitWorks(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	// tried 3h ago, no retry
-	s.state.Set("snapd-transition-last-retry-time", time.Now().Add(-3*time.Hour))
-
-	s.settle(c)
-
-	c.Check(s.state.Changes(), HasLen, 0)
-
-	// tried 7h ago, retry
-	s.state.Set("snapd-transition-last-retry-time", time.Now().Add(-7*time.Hour))
-
-	s.settle(c)
-
-	c.Check(s.state.Changes(), HasLen, 1)
-
-	var t time.Time
-	s.state.Get("snapd-transition-last-retry-time", &t)
-	c.Assert(time.Since(t) < 2*time.Minute, Equals, true)
-}
-
-type unhappyStore struct {
-	*fakeStore
-}
-
-func (s unhappyStore) SnapAction(ctx context.Context, currentSnaps []*store.CurrentSnap, actions []*store.SnapAction, assertQuery store.AssertionQuery, user *auth.UserState, opts *store.RefreshOptions) ([]store.SnapActionResult, []store.AssertionResult, error) {
-	if assertQuery != nil {
-		panic("no assertion query support")
-	}
-
-	return nil, nil, fmt.Errorf("a grumpy store")
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapError(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	snapstate.ReplaceStore(s.state, unhappyStore{fakeStore: s.fakeStore})
-
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	s.state.Unlock()
-
-	err := s.o.Settle(5 * time.Second)
-	c.Assert(err, ErrorMatches, `state ensure errors: \[a grumpy store\]`)
-
-	s.state.Lock()
-	c.Check(s.state.Changes(), HasLen, 0)
-
-	// all the attempts were recorded
-	var t time.Time
-	s.state.Get("snapd-transition-last-retry-time", &t)
-	c.Assert(time.Since(t) < 2*time.Minute, Equals, true)
-
-	var cnt int
-	s.state.Get("snapd-transition-retry", &cnt)
-	c.Assert(cnt, Equals, 1)
-
-	// the transition is not tried again (because of retry time)
-	s.state.Unlock()
-	err = s.o.Settle(5 * time.Second)
-	c.Assert(err, IsNil)
-	s.state.Lock()
-
-	s.state.Get("snapd-transition-retry", &cnt)
-	c.Assert(cnt, Equals, 1)
-}
-
-func (s *snapmgrTestSuite) TestTransitionSnapdSnapBlocksOtherChanges(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// if we have a snapd transition
-	chg := s.state.NewChange("transition-to-snapd-snap", "...")
-	chg.SetStatus(state.DoStatus)
-
-	// other tasks block until the transition is done
-	_, err := snapstate.Install(context.Background(), s.state, "some-snap", &snapstate.RevisionOptions{Channel: "stable"}, s.user.ID, snapstate.Flags{})
-	c.Check(err, FitsTypeOf, &snapstate.ChangeConflictError{})
-	c.Check(err, ErrorMatches, "transition to snapd snap in progress, no other changes allowed until this is done")
-
-	// and when the transition is done, other tasks run
-	chg.SetStatus(state.DoneStatus)
-	ts, err := snapstate.Install(context.Background(), s.state, "some-snap", &snapstate.RevisionOptions{Channel: "stable"}, s.user.ID, snapstate.Flags{})
-	c.Check(err, IsNil)
-	c.Check(ts, NotNil)
-}
-
 func (s *snapmgrTestSuite) TestForceDevModeCleanupRunsForUbuntuCore(c *C) {
 	s.checkForceDevModeCleanupRuns(c, "ubuntu-core", true)
 }
@@ -7010,40 +6779,6 @@ func (s *snapmgrTestSuite) TestSnapdSnapOnCoreWithoutBase(c *C) {
 
 	// it is now possible to install snapd snap on a system with core
 	_, err := snapstate.Install(context.Background(), s.state, "snapd", &snapstate.RevisionOptions{Channel: "some-channel"}, s.user.ID, snapstate.Flags{})
-	c.Assert(err, IsNil)
-}
-
-func (s *snapmgrTestSuite) TestSnapdSnapOnSystemsWithoutBaseOnUbuntuCore(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-	r := release.MockOnClassic(false)
-	defer r()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	// it is not possible to opt-into the snapd snap on core yet
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	// it is now possible to install snapd snap on a system with core, experimental option has no effect
-	_, err := snapstate.Install(context.Background(), s.state, "snapd", nil, s.user.ID, snapstate.Flags{})
-	c.Assert(err, IsNil)
-}
-
-func (s *snapmgrTestSuite) TestNoSnapdSnapOnSystemsWithoutBaseButOption(c *C) {
-	s.state.Lock()
-	defer s.state.Unlock()
-
-	// remove snapd snap added for snapmgrBaseTest
-	snapstate.Set(s.state, "snapd", nil)
-
-	tr := config.NewTransaction(s.state)
-	tr.Set("core", "experimental.snapd-snap", true)
-	tr.Commit()
-
-	_, err := snapstate.Install(context.Background(), s.state, "snapd", nil, s.user.ID, snapstate.Flags{})
 	c.Assert(err, IsNil)
 }
 
@@ -8611,7 +8346,7 @@ func (s *snapmgrTestSuite) TestRemodelSwitchNewGadgetConflictExclusiveKind(c *C)
 	s.addSnapsForRemodel(c)
 
 	tugc := s.state.NewTask("some-random-task", "...")
-	chg := s.state.NewChange("transition-to-snapd-snap", "...")
+	chg := s.state.NewChange("transition-ubuntu-core", "...")
 	chg.AddTask(tugc)
 
 	si := &snap.SideInfo{RealName: "some-snap", Revision: snap.R(3)}
@@ -8623,7 +8358,7 @@ func (s *snapmgrTestSuite) TestRemodelSwitchNewGadgetConflictExclusiveKind(c *C)
 		SnapType: "app",
 	})
 	ts, err := snapstate.SwitchToNewGadget(s.state, "some-snap", "")
-	c.Assert(err, ErrorMatches, "transition to snapd snap in progress, no other changes allowed until this is done")
+	c.Assert(err, ErrorMatches, "ubuntu-core to core transition in progress, no other changes allowed until this is done")
 	c.Assert(ts, IsNil)
 }
 
