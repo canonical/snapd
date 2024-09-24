9603ca36c746bf034b79977a6d778f84ac579418
Oliver Calder <oliver.calder@canonical.com>
2024-09-24 16:13:48 -0500

    tests: add apparmor prompting rule timespan tests
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

---
+++ tests/main/apparmor-prompting-integration-tests/timespan_allow.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/sh
+
+# A test that replying with allow always allows multiple files which match the
+# path pattern to be created, but doesn't allow other file creation.
+
+TEST_DIR="$1"
+
+echo "Run the prompting client in scripted mode in the background"
+prompting-client.scripted \
+	--script="${TEST_DIR}/script.json" \
+	--grace-period=1 \
+	--var="BASE_PATH:${TEST_DIR}" | tee "${TEST_DIR}/result" &
+
+sleep 1 # give the client a chance to start listening
+
+for name in test1.txt test2.txt test3.txt ; do
+	echo "Attempt to write $name"
+	snap run --shell prompting-client.scripted -c "echo $name is written > ${TEST_DIR}/${name}"
+done
+
+sleep 5 # wait for the rule to time out
+
+echo "Attempt to write test4.txt (should fail)"
+snap run --shell prompting-client.scripted -c "echo test4.txt is written > ${TEST_DIR}/test4.txt"
+
+sleep 5 # give the client a chance to write its result and exit
+
+CLIENT_OUTPUT="$(cat "${TEST_DIR}/result")"
+
+if [ "$CLIENT_OUTPUT" != "success" ] ; then
+	echo "test failed"
+	echo "output='$CLIENT_OUTPUT'"
+	exit 1
+fi
+
+for name in test1.txt test2.txt test3.txt ; do
+	TEST_OUTPUT="$(cat "${TEST_DIR}/${name}")"
+	if [ "$TEST_OUTPUT" != "$name is written" ] ; then
+		echo "file creation failed"
+		exit 1
+	fi
+done
+
+if [ -f "${TEST_DIR}/test4.txt" ] ; then
+	echo "file creation unexpectedly succeeded"
+	exit 1
+fi
