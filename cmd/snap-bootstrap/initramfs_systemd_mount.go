de03bd8a33b8e2d246f2328f6af5c021be7e9c7d
Spyros Seimenis <spyros.seimenis@canonical.com>
2024-10-09 19:09:23 +0300

    Revert "c/snap-bootstrap: escape spaces in systemd mount options"
    This reverts commit a63b608e9fc818bbfa6ec2f0dce2bf16b51cfea7.

--- cmd/snap-bootstrap/initramfs_systemd_mount.go
+++ cmd/snap-bootstrap/initramfs_systemd_mount.go
@@ -167,9 +167,7 @@ func doSystemdMountImpl(what, where string, opts *systemdMountOptions) error {
 	}
 	if opts.VerityHashDevice != "" && opts.VerityRootHash != "" {
 		options = append(options, fmt.Sprintf("verity.roothash=%s", opts.VerityRootHash))
-
-		escaper := strings.NewReplacer(",", `\,`, " ", `\ `)
-		options = append(options, fmt.Sprintf("verity.hashdevice=%s", escaper.Replace(opts.VerityHashDevice)))
+		options = append(options, fmt.Sprintf("verity.hashdevice=%s", strings.ReplaceAll(opts.VerityHashDevice, ",", `\,`)))
 
 		if opts.VerityHashOffset != 0 {
 			options = append(options, fmt.Sprintf("verity.hashoffset=%d", opts.VerityHashOffset))
