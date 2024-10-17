7d4b1cebba5bfca316cc19059fb61c3839c58164
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-10-17 10:53:14 +0200

    overlord/fdestate: DBX update unit tests
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/fdestate/fdemgr_test.go
+++ overlord/fdestate/fdemgr_test.go
@@ -22,19 +22,25 @@ package fdestate_test
 import (
 	"crypto"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	. "gopkg.in/check.v1"
 
 	"github.com/snapcore/snapd/asserts"
 	"github.com/snapcore/snapd/boot"
+	"github.com/snapcore/snapd/boot/boottest"
 	"github.com/snapcore/snapd/dirs"
 	"github.com/snapcore/snapd/gadget/device"
 	"github.com/snapcore/snapd/overlord/fdestate"
 	"github.com/snapcore/snapd/overlord/fdestate/backend"
 	"github.com/snapcore/snapd/overlord/state"
 	"github.com/snapcore/snapd/secboot"
+	"github.com/snapcore/snapd/seed"
+	"github.com/snapcore/snapd/snap"
 	"github.com/snapcore/snapd/testutil"
+	"github.com/snapcore/snapd/timings"
 )
 
 func TestFDE(t *testing.T) { TestingT(t) }
@@ -62,6 +68,11 @@ func (s *fdeMgrSuite) SetUpTest(c *C) {
 	s.AddCleanup(fdestate.MockBackendResealKeyForBootChains(
 		func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, expectReseal bool) error {
 			panic("BackendResealKeyForBootChains not mocked")
+		}))
+
+	s.AddCleanup(fdestate.MockBackendResealKeysForSignaturesDBUpdate(
+		func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, update []byte) error {
+			panic("BackendResealKeysForSignaturesDBUpdate not mocked")
 		}))
 }
 
@@ -230,4 +241,198 @@ func (s *fdeMgrSuite) TestUpdateReseal(c *C) {
 	c.Check(containerRole.Models[0].Model(), Equals, "mock-model")
 	c.Check(containerRole.BootModes, DeepEquals, []string{"run"})
 	c.Check(containerRole.TPM2PCRProfile, DeepEquals, secboot.SerializedPCRProfile(`"serialized-profile"`))
+}
+
+func (s *fdeMgrSuite) TestEFIDBXNoSealedKeys(c *C) {
+	// no sealed keys in the system, all operations are NOP
+
+	st := s.st
+	s.startedManager(c)
+
+	defer fdestate.MockBackendResealKeysForSignaturesDBUpdate(func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, update []byte) error {
+		panic("unexpected call")
+	})()
+
+	err := fdestate.EFISecureBootDBManagerStartup(st)
+	c.Assert(err, IsNil)
+
+	err = fdestate.EFISecureBootDBUpdatePrepare(st, fdestate.EFISecurebootDBX, []byte("payload"))
+	c.Assert(err, IsNil)
+
+	func() {
+		st.Lock()
+		defer st.Unlock()
+		// make sure nothing was added to the state
+		var fdeSt fdestate.FdeState
+		err = st.Get("fde", &fdeSt)
+		c.Assert(err, IsNil)
+		c.Check(fdeSt.PendingExternalOperations, HasLen, 0)
+	}()
+
+	err = fdestate.EFISecureBootDBUpdateCleanup(st)
+	c.Assert(err, IsNil)
+}
+
+func (s *fdeMgrSuite) TestEFIDBXStartupClean(c *C) {
+	c.Assert(device.StampSealedKeys(dirs.GlobalRootDir, device.SealingMethodTPM), IsNil)
+
+	st := s.st
+	s.startedManager(c)
+
+	defer fdestate.MockBackendResealKeysForSignaturesDBUpdate(func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, update []byte) error {
+		panic("unexpected call")
+	})()
+
+	err := fdestate.EFISecureBootDBManagerStartup(st)
+	c.Assert(err, IsNil)
+
+	st.Lock()
+	defer st.Unlock()
+
+	var fdeSt fdestate.FdeState
+	err = st.Get("fde", &fdeSt)
+	c.Assert(err, IsNil)
+
+	c.Check(fdeSt.PendingExternalOperations, HasLen, 0)
+}
+
+func (s *fdeMgrSuite) TestEFIDBXPrepareHappy(c *C) {
+	c.Assert(device.StampSealedKeys(dirs.GlobalRootDir, device.SealingMethodTPM), IsNil)
+
+	st := s.st
+	s.startedManager(c)
+
+	s.mockBootAssetsStateForModeenv(c)
+
+	resealCalls := 0
+	defer fdestate.MockBackendResealKeysForSignaturesDBUpdate(func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, update []byte) error {
+		resealCalls++
+		c.Check(update, DeepEquals, []byte("payload"))
+		return nil
+	})()
+
+	err := fdestate.EFISecureBootDBUpdatePrepare(st, fdestate.EFISecurebootDBX, []byte("payload"))
+	c.Assert(err, IsNil)
+
+	st.Lock()
+	defer st.Unlock()
+
+	var fdeSt fdestate.FdeState
+	err = st.Get("fde", &fdeSt)
+	c.Assert(err, IsNil)
+
+	c.Check(resealCalls, Equals, 1)
+	c.Check(fdeSt.PendingExternalOperations, HasLen, 1)
+	c.Check(fdeSt.PendingExternalOperations[0], DeepEquals, fdestate.ExternalOperation{
+		Kind:     "fde-efi-secureboot-db-update",
+		ChangeID: "1",
+		Context:  []byte(`{"payload":"cGF5bG9hZA=="}`),
+	})
+}
+
+func (s *fdeMgrSuite) TestEFIDBXCleanupNoChange(c *C) {
+	c.Assert(device.StampSealedKeys(dirs.GlobalRootDir, device.SealingMethodTPM), IsNil)
+
+	st := s.st
+	s.startedManager(c)
+
+	defer fdestate.MockBackendResealKeysForSignaturesDBUpdate(func(updateState backend.StateUpdater, method device.SealingMethod, rootdir string, params *boot.ResealKeyForBootChainsParams, update []byte) error {
+		panic("unexpected call")
+	})()
+
+	err := fdestate.EFISecureBootDBUpdateCleanup(st)
+	c.Assert(err, IsNil)
+
+	st.Lock()
+	defer st.Unlock()
+
+	var fdeSt fdestate.FdeState
+	err = st.Get("fde", &fdeSt)
+	c.Assert(err, IsNil)
+
+	c.Check(fdeSt.PendingExternalOperations, HasLen, 0)
+}
+
+func createMockGrubCfg(baseDir string) error {
+	cfg := filepath.Join(baseDir, "EFI/ubuntu/grub.cfg")
+	if err := os.MkdirAll(filepath.Dir(cfg), 0755); err != nil {
+		return err
+	}
+	return os.WriteFile(cfg, []byte("# Snapd-Boot-Config-Edition: 1\n"), 0644)
+}
+
+func (s *fdeMgrSuite) mockBootAssetsStateForModeenv(c *C) {
+	model := boottest.MakeMockUC20Model()
+
+	rootdir := dirs.GlobalRootDir
+
+	modeenv := &boot.Modeenv{
+		Mode: "run",
+
+		// no recovery systems to keep things relatively short
+		//
+		CurrentTrustedRecoveryBootAssets: map[string][]string{
+			"grubx64.efi": {"grub-hash"},
+			"bootx64.efi": {"shim-hash"},
+		},
+
+		CurrentTrustedBootAssets: map[string][]string{
+			"grubx64.efi": {"run-grub-hash"},
+		},
+
+		CurrentKernels: []string{"pc-kernel_500.snap"},
+
+		CurrentKernelCommandLines: []string{
+			"snapd_recovery_mode=run console=ttyS0 console=tty1 panic=-1",
+		},
+		Model:          model.Model(),
+		BrandID:        model.BrandID(),
+		Grade:          string(model.Grade()),
+		ModelSignKeyID: model.SignKeyID(),
+	}
+
+	c.Assert(modeenv.WriteTo(rootdir), IsNil)
+
+	err := createMockGrubCfg(filepath.Join(rootdir, "run/mnt/ubuntu-seed"))
+	c.Assert(err, IsNil)
+
+	err = createMockGrubCfg(filepath.Join(rootdir, "run/mnt/ubuntu-boot"))
+	c.Assert(err, IsNil)
+
+	collectAssetHashes := func(bmaps ...map[string][]string) []string {
+		uniqAssetsHashes := map[string]bool{}
+
+		for _, bm := range bmaps {
+			for bl, hashes := range bm {
+				for _, h := range hashes {
+					uniqAssetsHashes[fmt.Sprintf("%s-%s", bl, h)] = true
+				}
+			}
+		}
+
+		l := make([]string, 0, len(uniqAssetsHashes))
+		for h := range uniqAssetsHashes {
+			l = append(l, h)
+		}
+
+		c.Logf("assets: %v", l)
+		return l
+	}
+
+	// mock asset cache
+	boottest.MockAssetsCache(c, rootdir, "grub",
+		collectAssetHashes(modeenv.CurrentTrustedBootAssets, modeenv.CurrentTrustedRecoveryBootAssets))
+
+	const gadgetSnapYaml = `name: gadget
+version: 1.0
+type: gadget
+`
+
+	restore := boot.MockSeedReadSystemEssential(func(seedDir, label string, essentialTypes []snap.Type, tm timings.Measurer) (*asserts.Model, []*seed.Snap, error) {
+		return model, []*seed.Snap{
+			boottest.MockNamedKernelSeedSnap(snap.R(1), "pc-kernel"),
+			boottest.MockGadgetSeedSnap(c, gadgetSnapYaml, nil),
+		}, nil
+	})
+	s.AddCleanup(restore)
 }
