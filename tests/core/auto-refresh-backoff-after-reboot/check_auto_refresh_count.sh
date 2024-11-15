a2640d0bc3bd204d4120c96966acdda4f09ad7f4
Maciej Borzecki <maciej.borzecki@canonical.com>
2024-11-15 18:48:52 +0100

    fixup! tests/core/auto-refresh-backoff-after-reboot: use gojq
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- tests/core/auto-refresh-backoff-after-reboot/check_auto_refresh_count.sh
+++ tests/core/auto-refresh-backoff-after-reboot/check_auto_refresh_count.sh
@@ -4,4 +4,4 @@ LAST_CHANGE_ID=$1
 CHANGES_COUNT=$2
 
 #shellcheck disable=SC2086,SC2046
-test $(snap debug api /v2/changes?select=ready | jq "[.result[] | select(.kind == \"auto-refresh\" and (.id|tonumber) > ($LAST_CHANGE_ID|tonumber))] | length") == $CHANGES_COUNT
+test $(snap debug api /v2/changes?select=ready | gojq "[.result[] | select(.kind == \"auto-refresh\" and (.id|tonumber) > ($LAST_CHANGE_ID|tonumber))] | length") == $CHANGES_COUNT
