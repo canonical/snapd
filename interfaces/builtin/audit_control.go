387fea0c7619575f120d96c7953b2ac4164bf371
Oliver Calder <oliver.calder@canonical.com>
2024-12-05 12:53:53 -0600

    i/builtin: add audit-control interface
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

---
+++ interfaces/builtin/audit_control.go
@@ -0,0 +1,57 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const auditControlSummary = `allows control over the kernel audit system`
+
+const auditControlBaseDeclarationSlots = `
+  audit-control:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const auditControlConnectedPlugAppArmor = `
+# CAP_AUDIT_CONTROL required to enable/disable kernel auditing, change auditing
+# filter rules, and retrieve auditing status and filtering rules.
+capability audit_control,
+
+# Allow reading /proc/self/{loginuid,sessionid}
+@{PROC}/@{pid}/{loginuid,sessionid} r,
+
+# Allow writing /run/auditd.pid so the auditd pid is known by other programs.
+/{,var/}run/auditd.pid rw,
+`
+
+type auditControlInterface struct {
+	commonInterface
+}
+
+func init() {
+	registerIface(&auditControlInterface{commonInterface{
+		name:                  "audit-control",
+		summary:               auditControlSummary,
+		implicitOnCore:        false, // XXX: is this correct?
+		implicitOnClassic:     false, // XXX: is this correct?
+		baseDeclarationSlots:  auditControlBaseDeclarationSlots,
+		connectedPlugAppArmor: auditControlConnectedPlugAppArmor,
+	}})
+}
---
+++ interfaces/builtin/audit_control_test.go
@@ -0,0 +1,89 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2024 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type AuditControlInterfaceSuite struct {
+	iface    interfaces.Interface
+	slotInfo *snap.SlotInfo
+	slot     *interfaces.ConnectedSlot
+	plugInfo *snap.PlugInfo
+	plug     *interfaces.ConnectedPlug
+}
+
+const auditControlMockPlugSnapInfoYaml = `name: other
+version: 1.0
+apps:
+ app2:
+  command: foo
+  plugs: [audit-control]
+`
+
+const auditControlMockSlotSnapInfoYaml = `name: core
+version: 1.0
+type: os
+slots:
+ audit-control:
+  interface: audit-control
+`
+
+var _ = Suite(&AuditControlInterfaceSuite{
+	iface: builtin.MustInterface("audit-control"),
+})
+
+func (s *AuditControlInterfaceSuite) SetUpTest(c *C) {
+	s.slot, s.slotInfo = MockConnectedSlot(c, auditControlMockSlotSnapInfoYaml, nil, "audit-control")
+	s.plug, s.plugInfo = MockConnectedPlug(c, auditControlMockPlugSnapInfoYaml, nil, "audit-control")
+}
+
+func (s *AuditControlInterfaceSuite) TestName(c *C) {
+	c.Assert(s.iface.Name(), Equals, "audit-control")
+}
+
+func (s *AuditControlInterfaceSuite) TestSanitizeSlot(c *C) {
+	c.Assert(interfaces.BeforePrepareSlot(s.iface, s.slotInfo), IsNil)
+}
+
+func (s *AuditControlInterfaceSuite) TestSanitizePlug(c *C) {
+	c.Assert(interfaces.BeforePreparePlug(s.iface, s.plugInfo), IsNil)
+}
+
+func (s *AuditControlInterfaceSuite) TestAppArmorSpec(c *C) {
+	spec := apparmor.NewSpecification(s.plug.AppSet())
+	err := spec.AddConnectedPlug(s.iface, s.plug, s.slot)
+	c.Assert(err, IsNil)
+	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.other.app2"})
+	c.Check(spec.SnippetForTag("snap.other.app2"), testutil.Contains, "capability audit_control,\n")
+	c.Check(spec.SnippetForTag("snap.other.app2"), testutil.Contains, "@{PROC}/@{pid}/{loginuid,sessionid} r,\n")
+	c.Check(spec.SnippetForTag("snap.other.app2"), testutil.Contains, "/{,var/}run/auditd.pid rw,\n")
+}
+
+func (s *AuditControlInterfaceSuite) TestInterfaces(c *C) {
+	c.Check(builtin.Interfaces(), testutil.DeepContains, s.iface)
+}
