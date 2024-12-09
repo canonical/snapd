49171ac9b95db014b42c0a59ff27d206affece96
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2024-12-09 16:52:31 -0500

    core-initrd: build snap-bootstrap and include in deb package

--- core-initrd/build-source-pkgs.sh
+++ core-initrd/build-source-pkgs.sh
@@ -2,15 +2,40 @@
 
 git clean -ffdx
 
+# The current commit must be in the repo to be able to get the dependencies
+# of snap-bootstrap.
+commit=$(git rev-parse HEAD)
+
 for dir in */debian; do
     rel=${dir%/debian}
 
     if [ "$rel" != latest ]; then
-        for f in latest/*; do
-            ln -s "$f" "$rel"/"${f#latest/}"
+        for p in latest/*; do
+            file=${p#latest/}
+            if [ "$file" = debian ] || [ "$file" = go.mod ] ||
+                   [ "$file" = go.sum ] || [ "$file" = cmd ]; then
+                continue
+            fi
+            cp -a "$p" "$rel/"
         done
     fi
+
     pushd "$rel"
+    mkdir cmd
+    # go commands do not follow symlinks, copy instead
+    cp -a ../../cmd/snap-bootstrap/ cmd/
+    cat << EOF > go.mod
+module github.com/snapcore/snap-bootstrap
+
+go 1.18
+
+require	github.com/snapcore/snapd $commit
+EOF
+    # solve dependencies
+    go mod tidy
+    # build vendor folder
+    go mod vendor
     dpkg-buildpackage -S -sa -d
     popd
+
 done
