6a1a4fb27fd7b20af85164ccd9655d8b67dc5e0b
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2024-12-09 18:04:45 -0500

    core-initrd: pulls snapd info and needed service files

--- core-initrd/build-source-pkgs.sh
+++ core-initrd/build-source-pkgs.sh
@@ -6,14 +6,26 @@ git clean -ffdx
 # of snap-bootstrap.
 commit=$(git rev-parse HEAD)
 
+# build info file
+pushd ..
+./mkversion.sh
+popd
+
+contains_element() {
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
+}
+
+no_link=(debian go.mod go.sum cmd snapd)
 for dir in */debian; do
     rel=${dir%/debian}
 
     if [ "$rel" != latest ]; then
         for p in latest/*; do
             file=${p#latest/}
-            if [ "$file" = debian ] || [ "$file" = go.mod ] ||
-                   [ "$file" = go.sum ] || [ "$file" = cmd ]; then
+            if contains_element "$file" "${no_link[@]}"; then
                 continue
             fi
             cp -a "$p" "$rel/"
@@ -21,6 +33,11 @@ for dir in */debian; do
     fi
 
     pushd "$rel"
+    mkdir snapd
+    cp ../../data/info snapd/
+    sed 's#@libexecdir@#/usr/lib#' ../../data/systemd/snapd.recovery-chooser-trigger.service.in > \
+        snapd/snapd.recovery-chooser-trigger.service
+    cp ../../data/info snapd/
     mkdir cmd
     # go commands do not follow symlinks, copy instead
     cp -a ../../cmd/snap-bootstrap/ cmd/
