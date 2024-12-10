44da226dc6d7793a23e7a8be66a83257c47ca3e9
Valentin David <valentin.david@canonical.com>
2024-12-10 11:37:28 +0100

    overlord/devicestate,secboot: fix factory reset on older versions
    Until now, we were just delete old keys on factory-reset instead of
    renaming if cryptsetup was too old. But this is a bit too dangerous.
    Instead we copy-delete them.
    
    Second, we need to convert old keyslots to have a name so we can
    remove them after.

--- overlord/devicestate/export_test.go
+++ overlord/devicestate/export_test.go
@@ -619,11 +619,11 @@ func MockSecbootAddBootstrapKeyOnExistingDisk(f func(node string, newKey keys.En
 	}
 }
 
-func MockSecbootRenameOrDeleteKeys(f func(node string, renames map[string]string) error) (restore func()) {
-	old := secbootRenameOrDeleteKeys
-	secbootRenameOrDeleteKeys = f
+func MockSecbootRenameKeys(f func(node string, renames map[string]string) error) (restore func()) {
+	old := secbootRenameKeys
+	secbootRenameKeys = f
 	return func() {
-		secbootRenameOrDeleteKeys = old
+		secbootRenameKeys = old
 	}
 }
 
@@ -640,6 +640,22 @@ func MockSecbootDeleteKeys(f func(node string, matches map[string]bool) error) (
 	secbootDeleteKeys = f
 	return func() {
 		secbootDeleteKeys = old
+	}
+}
+
+func MockSecbootDeleteOldKeys(f func(devicePath string) error) (restore func()) {
+	old := secbootDeleteOldKeys
+	secbootDeleteOldKeys = f
+	return func() {
+		secbootDeleteOldKeys = old
+	}
+}
+
+func MockSecbootTemporaryNameOldKeys(f func(devicePath string) error) (restore func()) {
+	old := secbootTemporaryNameOldKeys
+	secbootTemporaryNameOldKeys = f
+	return func() {
+		secbootTemporaryNameOldKeys = old
 	}
 }
 
