c395eb60a249ded8a6e259e804500c21510ed550
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-02-19 12:05:15 +0100

    cmd/libsnap-confine-private: mkdir(at) & chmod helpers
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/libsnap-confine-private/utils.h
+++ cmd/libsnap-confine-private/utils.h
@@ -126,4 +126,21 @@ __attribute__((warn_unused_result)) bool sc_is_expected_path(const char *path);
  */
 bool sc_wait_for_file(const char *path, size_t timeout_sec);
 
+/**
+ * Ensure a directory exists inside a given parent directory. Essentially a
+ * convenience wrapper around mkdirat() followed by fchownat(), if a new
+ * directory was created.
+ *
+ * Returns -1 in case of error.
+ */
+int sc_ensure_mkdirat(int fd, const char *name, mode_t mode, uid_t uid, uid_t gid);
+
+/**
+ * Ensure a directory exists. Essentially a convenience wrapper around mkdirat()
+ * followed by chown() if a new directory was created.
+ *
+ * Returns -1 in case of error.
+ */
+int sc_ensure_mkdir(const char *name, mode_t mode, uid_t uid, uid_t gid);
+
 #endif
