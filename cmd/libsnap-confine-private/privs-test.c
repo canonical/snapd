8f0fd46779e623c92b63a77b00eba2b8df9dbb89
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-02-25 16:24:19 +0100

    cmd/libsnap-confine-private/privs: simplify dropping privileges
    Use CAP_MODE_NOPRIV which is more strict than what was set earlier.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/libsnap-confine-private/privs-test.c
+++ cmd/libsnap-confine-private/privs-test.c
@@ -18,40 +18,49 @@
 #include "privs.h"
 #include "privs.c"
 
+#include <sys/capability.h>
+
 #include <glib.h>
 
 // Test that dropping permissions really works
 static void test_sc_privs_drop(void) {
-    if (geteuid() != 0 || getuid() == 0) {
-        g_test_skip("run this test after chown root:root; chmod u+s");
+    /* expecting a file capability */
+    cap_t start SC_CLEANUP(sc_cleanup_cap_t) = cap_get_proc();
+    g_assert_nonnull(start);
+
+    cap_t ref SC_CLEANUP(sc_cleanup_cap_t) = cap_init();
+    g_assert_nonnull(ref);
+    cap_value_t alarm = CAP_WAKE_ALARM;
+    g_assert_cmpint(cap_set_flag(ref, CAP_INHERITABLE, 1, &alarm, CAP_SET), ==, 0);
+    cap_value_t net_raw = CAP_NET_RAW;
+    g_assert_cmpint(cap_set_flag(ref, CAP_PERMITTED, 1, &net_raw, CAP_SET), ==, 0);
+
+    if (cap_compare(start, ref) != 0) {
+        g_test_skip("run this test after 'sudo setcap cap_net_raw=p <unit-test-file>'");
         return;
     }
-    if (getegid() != 0 || getgid() == 0) {
-        g_test_skip("run this test after chown root:root; chmod g+s");
-        return;
-    }
+
     if (g_test_subprocess()) {
-        // We start as a regular user with effective-root identity.
-        g_assert_cmpint(getuid(), !=, 0);
-        g_assert_cmpint(getgid(), !=, 0);
+        /* gtest reexecs itself, hence we retain CAP_NET_RAW even though it's
+         * not in inherited set */
+        g_assert_cmpint(cap_compare(start, ref), ==, 0);
 
-        g_assert_cmpint(geteuid(), ==, 0);
-        g_assert_cmpint(getegid(), ==, 0);
-
-        // We drop the privileges.
+        /* drop privileges */
         sc_privs_drop();
 
-        // The we are no longer root.
-        g_assert_cmpint(getuid(), !=, 0);
-        g_assert_cmpint(geteuid(), !=, 0);
-        g_assert_cmpint(getgid(), !=, 0);
-        g_assert_cmpint(getegid(), !=, 0);
+        cap_t working SC_CLEANUP(sc_cleanup_cap_t) = cap_get_proc();
+        g_assert_nonnull(working);
+
+        cap_t ref SC_CLEANUP(sc_cleanup_cap_t) = cap_init();
+        g_assert_cmpint(cap_compare(working, ref), ==, 0);
 
         // We don't have any supplementary groups.
         gid_t groups[2];
         int num_groups = getgroups(1, groups);
-        g_assert_cmpint(num_groups, ==, 1);
-        g_assert_cmpint(groups[0], ==, getgid());
+        g_assert_cmpint(num_groups, <=, 1);
+        if (num_groups == 1) {
+            g_assert_cmpint(groups[0], ==, getgid());
+        }
 
         // All done.
         return;
