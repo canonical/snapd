46448f75c5e9904cbfe75bd61416732a10b6e538
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-02-25 16:24:19 +0100

    cmd/libsnap-confine-private/privs: simplify dropping privileges
    Use CAP_MODE_NOPRIV which is more strict than what was set earlier.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/libsnap-confine-private/privs.c
+++ cmd/libsnap-confine-private/privs.c
@@ -46,49 +46,15 @@ static void sc_cleanup_cap_str(char **ptr) {
     }
 }
 
-static bool sc_has_capability(const char *cap_name) {
-    // Lookup capability with the given name.
-    cap_value_t cap;
-    if (cap_from_name(cap_name, &cap) < 0) {
-        die("cannot resolve capability name %s", cap_name);
-    }
-    // Get the capability state of the current process.
-    cap_t caps;
-    if ((caps = cap_get_proc()) == NULL) {
-        die("cannot obtain capability state (cap_get_proc)");
-    }
-    // Read the effective value of the flag we're dealing with
-    cap_flag_value_t cap_flags_value;
-    if (cap_get_flag(caps, cap, CAP_EFFECTIVE, &cap_flags_value) < 0) {
-        cap_free(caps);  // don't bother checking, we die anyway.
-        die("cannot obtain value of capability flag (cap_get_flag)");
-    }
-    // Free the representation of the capability state of the current process.
-    if (cap_free(caps) < 0) {
-        die("cannot free capability flag (cap_free)");
-    }
-    // Check if the effective bit of the capability is set.
-    return cap_flags_value == CAP_SET;
-}
-
 void sc_privs_drop(void) {
-    gid_t gid = getgid();
-    uid_t uid = getuid();
-
-    // Drop extra group membership if we can.
-    if (sc_has_capability("cap_setgid")) {
-        gid_t gid_list[1] = {gid};
-        if (setgroups(1, gid_list) < 0) {
-            die("cannot set supplementary group identifiers");
-        }
+    /* TODO: this should use cap_set_mode(CAP_MODE_NOPRIV) for better effect,
+     * but it's not supported by libcap 2.25 in 18.04 */
+    cap_t working SC_CLEANUP(sc_cleanup_cap_t) = cap_init();
+    if (working == NULL) {
+        die("cannot allocate working caps set");
     }
-    // Switch to real group ID
-    if (setgid(getgid()) < 0) {
-        die("cannot set group identifier to %d", gid);
-    }
-    // Switch to real user ID
-    if (setuid(getuid()) < 0) {
-        die("cannot set user identifier to %d", uid);
+    if (cap_set_proc(working) != 0) {
+        die("cannot drop capabilities");
     }
 }
 
