243d172905e565208d39595d0c4dbe0a21785c81
Spyros Seimenis <spyros.seimenis@canonical.com>
2025-03-15 00:42:38 +0200

    c/snap-bootstrap: fix some cross-distro unit tests
    hardcoding the data-blocks to 4 seem to fail in open suse as the test
    snaps that get generated are bigger. Calculate the data blocks value
    using the actual size of a test snap instead.

--- cmd/snap-bootstrap/cmd_initramfs_mounts_recover_test.go
+++ cmd/snap-bootstrap/cmd_initramfs_mounts_recover_test.go
@@ -3698,9 +3698,10 @@ func (s *initramfsMountsSuite) TestInitramfsMountsRecoverModeHappyWithIntegrityA
 		snaps: snaps,
 	})
 
-	// test snaps have a fixed size of 16384 so this is divided by the 4096 blockSize
-	// to get the --data-blocks argument to veritysetup
-	dataBlocks := 4
+	snapPath := filepath.Join(s.seedDir, "snaps", s.snapd.Filename())
+	fileInfo, err := os.Stat(snapPath)
+	c.Assert(err, IsNil)
+	dataBlocks := int(fileInfo.Size()) / 4096
 
 	var calls [][]string
 
@@ -3885,9 +3886,10 @@ func (s *initramfsMountsSuite) TestInitramfsMountsRecoverModeErrorWithIntegrityA
 		fmt.Errorf("internal error: unexpected root hash length"),
 	)
 
-	// test snaps have a fixed size of 16384 so this is divided by the 4096 blockSize
-	// to get the --data-blocks argument to veritysetup
-	dataBlocks := 4
+	snapPath := filepath.Join(s.seedDir, "snaps", s.snapd.Filename())
+	fileInfo, err := os.Stat(snapPath)
+	c.Assert(err, IsNil)
+	dataBlocks := int(fileInfo.Size()) / 4096
 
 	var calls [][]string
 
@@ -3953,9 +3955,10 @@ func (s *initramfsMountsSuite) TestInitramfsMountsRecoverModeErrorWithIntegrityA
 		fmt.Errorf("computed root hash doesn't match trusted root hash from assertion: %s != %s", assertedRootHash, computedRootHash),
 	)
 
-	// test snaps have a fixed size of 16384 so this is divided by the 4096 blockSize
-	// to get the --data-blocks argument to veritysetup
-	dataBlocks := 4
+	snapPath := filepath.Join(s.seedDir, "snaps", s.snapd.Filename())
+	fileInfo, err := os.Stat(snapPath)
+	c.Assert(err, IsNil)
+	dataBlocks := int(fileInfo.Size()) / 4096
 
 	var calls [][]string
 
