818529bd2c4f91f7fbad995991974479e5d02b25
Valentin David <valentin.david@canonical.com>
2025-03-27 09:46:06 +0100

    fixup! tests/lib/tools/build_kernel_with_comps.sh: do not fail if components exist

--- tests/lib/tools/build_kernel_with_comps.sh
+++ tests/lib/tools/build_kernel_with_comps.sh
@@ -51,7 +51,8 @@ EOF
     depmod -b kernel/ "$kern_ver"
     rm "${kernel_snap_file}"
     # append component meta-information
-    COMP_NAME="${comp_name}" yq '.components = {env(COMP_NAME):{"type":"kernel-modules"}}' -i kernel/meta/snap.yaml
+    gojq --arg COMP_NAME "${comp_name}" '.components = {env(COMP_NAME):{"type":"kernel-modules"}}' --yaml-input kernel/meta/snap.yaml --yaml-ouput >kernel/meta/snap.yaml.new
+    mv kernel/meta/snap.yaml.new kernel/meta/snap.yaml
     snap pack --filename="${kernel_snap_file}" kernel
 
     if [ "${use_provided_kernel}" = false ]; then
