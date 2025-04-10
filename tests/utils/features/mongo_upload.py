c96cca9708b3ed929ddff5aeadc053260bb1a9ef
katie <katie.may@canonical.com>
2025-04-10 10:04:21 +0200

    tests, github:  use env variables for mongo parameters; add verbose mode to print insert statement

--- tests/utils/features/mongo_upload.py
+++ tests/utils/features/mongo_upload.py
@@ -6,9 +6,30 @@ import json
 import os
 from pymongo import MongoClient, InsertOne
 
+HOST = 'MONGO_HOST'
+PORT = 'MONGO_PORT'
+USER = 'MONGO_USER'
+PASSWORD = 'MONGO_PASSWORD'
 
-def upload_documents(host, port, user, password, folder):
-    with MongoClient(host=host, port=port, username=user, password=password) as client:
+
+def upload_documents(folder, verbose):
+    if HOST not in os.environ.keys():
+        raise RuntimeError(
+            "the {} environment variable must be set and contain the host data".format(HOST))
+    if PORT not in os.environ.keys():
+        raise RuntimeError(
+            "the {} environment variable must be set and contain the port data".format(PORT))
+    if USER not in os.environ.keys():
+        raise RuntimeError(
+            "the {} environment variable must be set and contain the username".format(USER))
+    if PASSWORD not in os.environ.keys():
+        raise RuntimeError(
+            "the {} environment variable must be set and contain the password".format(PASSWORD))
+    if not os.environ[PORT].isdigit():
+        raise RuntimeError(
+            "the {} environment variable must contain a valid port number".format(PORT))
+
+    with MongoClient(host=os.environ[HOST], port=int(os.environ[PORT]), username=os.environ[USER], password=os.environ[PASSWORD]) as client:
         db = client.snapd
         collection = db.features
 
@@ -22,26 +43,21 @@ def upload_documents(host, port, user, password, folder):
                     requesting.append(InsertOne(j))
 
         result = collection.bulk_write(requesting)
-        print("inserted {} new documents".format(result.inserted_count))
+        if verbose:
+            print("inserted {} new documents".format(result.inserted_count))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="mongodb document uploader")
-    parser.add_argument('--host', help='Host for mongodb',
-                        required=True, type=str)
-    parser.add_argument('--port', help='Port for mongodb',
-                        required=True, type=int, choices=(range(1, 65535)))
-    parser.add_argument('--user', help='mongodb user', required=True, type=str)
-    parser.add_argument('--password', help='mongodb password',
-                        required=True, type=str)
+        description="mongodb document uploader. It assumes the following environment variables are set: {}, {}, {}, and {}".format(HOST, PORT, USER, PASSWORD))
     parser.add_argument(
-        '--folder', help='folder containing json files', required=True, type=str)
+        '--dir', help='directory containing json files', required=True, type=str)
+    parser.add_argument(
+        '--verbose', help='print upload statement', action='store_true')
     args = parser.parse_args()
 
-    if not os.path.isdir(args.folder):
+    if not os.path.isdir(args.dir):
         raise RuntimeError(
-            "the indicated folder {} does not exist.".format(args.folder))
+            "the indicated directory {} does not exist.".format(args.dir))
 
-    upload_documents(args.host, args.port, args.user,
-                     args.password, args.folder)
+    upload_documents(args.dir, args.verbose)
