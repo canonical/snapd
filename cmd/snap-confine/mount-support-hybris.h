94f12d2982df85641ee532da03af99f9407871d6
Alfred Neumayer <dev.beidl@gmail.com>
2025-04-17 15:12:15 +0200

    cmd/snap-confine: mount-support-hybris: Update Copyright year

--- cmd/snap-confine/mount-support-hybris.h
+++ cmd/snap-confine/mount-support-hybris.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Canonical Ltd
+ * Copyright (C) 2025 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
