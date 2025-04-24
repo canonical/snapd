39107d49e9f0b67f589bda7d8195c4a216492eff
Katie May <katie.may@canonical.com>
2025-04-24 15:29:26 +0200

    spread, tests: add environment variables to spread tests for feature tagging (#15336)
    * spread, tests: add environment variables to spread tests for feature tagging
    
    * packaging: add structuredlogging build tag when adding test keys to package
    
    * tests: correct artifact script invocation
    
    * github, spread: expose variables and set default as SPREAD_TAG_FEATURES

--- tests/lib/nested.sh
+++ tests/lib/nested.sh
@@ -1772,9 +1772,11 @@ nested_prepare_tools() {
     if [ -n "$TAG_FEATURES" ]; then
         # If feature tagging is enabled, then we need to enable debug logging
         remote.exec "sudo mkdir -p /etc/systemd/system/snapd.service.d"
-        remote.exec "printf '[Service]\nEnvironment=SNAPD_DEBUG_HTTP=7 SNAPD_DEBUG=1 SNAPPY_TESTING=1\n' | sudo tee /etc/systemd/system/snapd.service.d/99-feature-tags.conf"
+        remote.exec "printf '[Service]\nEnvironment=SNAPD_DEBUG=1 SNAPPY_TESTING=1 SNAPD_TRACE=1 SNAPD_JSON_LOGGING=1\n' | sudo tee /etc/systemd/system/snapd.service.d/99-feature-tags.conf"
         # Persist journal logs
         remote.exec "sudo snap set system journal.persistent=true"
+        # Add trace structured logging to journal for snap commands
+        remote.exec "printf 'SNAPD_DEBUG=1\nSNAPD_TRACE=1\nSNAPD_JSON_LOGGING=1\nSNAP_LOG_TO_JOURNAL=1\n' | sudo tee -a /etc/environment"
         # We changed the service configuration so we need to reload and restart
         # the units to get them applied
         remote.exec "sudo systemctl daemon-reload"
