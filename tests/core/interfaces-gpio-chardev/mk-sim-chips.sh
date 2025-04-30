cfa6d03204e9f9d6766baffe6bd3c3208757af35
Zeyad Gouda <zeyad.gouda@canonical.com>
2025-04-30 11:04:06 +0300

    tests: address review comments
    Signed-off-by: Zeyad Gouda <zeyad.gouda@canonical.com>

--- tests/core/interfaces-gpio-chardev/mk-sim-chips.sh
+++ tests/core/interfaces-gpio-chardev/mk-sim-chips.sh
@@ -3,16 +3,16 @@
 CONFIG_BASE=/sys/kernel/config/gpio-sim
 CHIP_NAME=snaptest
 
-/usr/sbin/modprobe gpio-sim || true
+/usr/sbin/modprobe gpio-sim
 
-mkdir -p "${CONFIG_BASE}/${CHIP_NAME}" || true
-mkdir -p "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0" || true
-echo "gpio-bank0" > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0/label" || true
-echo 8 > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0/num_lines" || true
+mkdir -p "${CONFIG_BASE}/${CHIP_NAME}"
+mkdir -p "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0"
+echo "gpio-bank0" > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0/label"
+echo 8 > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank0/num_lines"
 
-mkdir -p "${CONFIG_BASE}/${CHIP_NAME}" || true
-mkdir -p "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1" || true
-echo "gpio-bank1" > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1/label" || true
-echo 8 > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1/num_lines" || true
+mkdir -p "${CONFIG_BASE}/${CHIP_NAME}"
+mkdir -p "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1"
+echo "gpio-bank1" > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1/label"
+echo 8 > "${CONFIG_BASE}/${CHIP_NAME}/gpio-bank1/num_lines"
 
-echo 1 > /sys/kernel/config/gpio-sim/snaptest/live || true
+echo 1 > /sys/kernel/config/gpio-sim/snaptest/live
--- tests/core/interfaces-gpio-chardev/rm-sim-chips.sh
+++ tests/core/interfaces-gpio-chardev/rm-sim-chips.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo 0 > /sys/kernel/config/gpio-sim/snaptest/live || true
-rmdir /sys/kernel/config/gpio-sim/snaptest/gpio-bank1 || true
-rmdir /sys/kernel/config/gpio-sim/snaptest/gpio-bank0 || true
-rmdir /sys/kernel/config/gpio-sim/snaptest || true
-rmmod gpio-sim || true
+echo 0 > /sys/kernel/config/gpio-sim/snaptest/live
+rmdir /sys/kernel/config/gpio-sim/snaptest/gpio-bank1
+rmdir /sys/kernel/config/gpio-sim/snaptest/gpio-bank0
+rmdir /sys/kernel/config/gpio-sim/snaptest
+rmmod gpio-sim
