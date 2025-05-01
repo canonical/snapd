b358fc77dc70d04cce143e188b32e2a17d17b864
Andrew Phelps <andrew.phelps@canonical.com>
2025-05-01 12:22:24 -0400

    k/f/optee: remove usage of runtime.Pinner

--- kernel/fde/optee/optee_impl.go
+++ kernel/fde/optee/optee_impl.go
@@ -26,6 +26,7 @@ package optee
 #include <tee_client_api.h>
 #include <fde_key_handler_ta_type.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 TEEC_UUID fde_ta_uuid = FDE_KEY_HANDLER_UUID_ID;
 
@@ -39,8 +40,8 @@ import "C"
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
-	"runtime"
 	"unsafe"
 
 	"github.com/snapcore/snapd/logger"
@@ -49,12 +50,9 @@ import (
 type opteeClient struct{}
 
 func (c *opteeClient) Present() bool {
-	pinner := &runtime.Pinner{}
-	defer pinner.Unpin()
-
 	// the first time we invoke the PTA without sending a buffer to fill. the
 	// PTA handles this by just returning the size of the buffer it will need.
-	bufferSize, err := devicesBufferSize(pinner)
+	bufferSize, err := devicesBufferSize()
 	if err != nil {
 		return false
 	}
@@ -69,23 +67,18 @@ func (c *opteeClient) Present() bool {
 		started:    1,
 		paramTypes: params,
 	}
-	pinner.Pin(op)
 
-	output := make([]byte, bufferSize)
-	pinner.Pin(&output[0])
-
-	outputMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
-	outputMemRef.size = C.size_t(len(output))
-	outputMemRef.buffer = unsafe.Pointer(&output[0])
+	outputMemRef, free := addMemRefToOp(op, 0, make([]byte, bufferSize))
+	defer free()
 
 	// this PTA (psuedo trusted application) command returns a list of PTA and
 	// early TA UUIDs. we know that our TA will always be an early TA.
-	if err := invoke(pinner, C.pta_device_uuid, 0x0 /* PTA_CMD_GET_DEVICES */, op); err != nil {
+	if err := invoke(C.pta_device_uuid, 0x0 /* PTA_CMD_GET_DEVICES */, op); err != nil {
 		return false
 	}
 
 	// output here is a slice of bytes, each 16 byte segment is a UUID
-	output = output[:outputMemRef.size]
+	output := C.GoBytes(outputMemRef.buffer, C.int(outputMemRef.size))
 
 	// fdeUUID is the byte encoding of the FDE TA UUID (FDE_KEY_HANDLER_UUID_ID
 	// in fde_key_handler_ta_type.h). having this pre-calculated eliminates some
@@ -110,7 +103,7 @@ func (c *opteeClient) Present() bool {
 	return false
 }
 
-func devicesBufferSize(pinner *runtime.Pinner) (int, error) {
+func devicesBufferSize() (int, error) {
 	// parameters:
 	// - output parameter containing a slice of bytes, each 16 byte segment is a
 	//   UUID; in this case, we will send in an empty slice to just retrieve the
@@ -123,15 +116,16 @@ func devicesBufferSize(pinner *runtime.Pinner) (int, error) {
 		started:    1,
 		paramTypes: params,
 	}
-	pinner.Pin(op)
 
-	sizeMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
-	sizeMemRef.size = 0
-	sizeMemRef.buffer = nil
+	// we don't use addMemRefToOp here because we don't need to allocate
+	// anything
+	memRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
+	memRef.size = 0
+	memRef.buffer = nil
 
 	// the first time we invoke the PTA without sending a buffer to fill. the
 	// PTA handles this by just returning the size of the buffer it will need.
-	res, err := invokeUnchecked(pinner, C.pta_device_uuid, 0x0 /* PTA_CMD_GET_DEVICES */, op)
+	res, err := invokeUnchecked(C.pta_device_uuid, 0x0 /* PTA_CMD_GET_DEVICES */, op)
 	if err != nil {
 		return 0, err
 	}
@@ -140,12 +134,25 @@ func devicesBufferSize(pinner *runtime.Pinner) (int, error) {
 		return 0, fmt.Errorf("expected short buffer error from PTA: %v", res)
 	}
 
-	return int(sizeMemRef.size), nil
+	return int(memRef.size), nil
+}
+
+func addMemRefToOp(op *C.TEEC_Operation, index int, buffer []byte) (*C.TEEC_TempMemoryReference, func()) {
+	malloced := C.CBytes(buffer)
+
+	memRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[index])
+	memRef.size = C.size_t(len(buffer))
+	memRef.buffer = malloced
+
+	return memRef, func() {
+		C.free(malloced)
+	}
 }
 
 func (c *opteeClient) DecryptKey(input []byte, handle []byte) ([]byte, error) {
-	pinner := &runtime.Pinner{}
-	defer pinner.Unpin()
+	if len(input) == 0 || len(handle) == 0 {
+		return nil, errors.New("internal error: input and handle both must contain data")
+	}
 
 	// parameters:
 	// - input parameter containing the encrypted key
@@ -157,40 +164,30 @@ func (c *opteeClient) DecryptKey(input []byte, handle []byte) ([]byte, error) {
 		started:    1,
 		paramTypes: params,
 	}
-	pinner.Pin(op)
 
-	pinner.Pin(&input[0])
+	_, free := addMemRefToOp(op, 0, input)
+	defer free()
 
-	inputMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
-	inputMemRef.size = C.size_t(len(input))
-	inputMemRef.buffer = unsafe.Pointer(&input[0])
+	_, free = addMemRefToOp(op, 1, handle)
+	defer free()
 
-	pinner.Pin(&handle[0])
+	unsealedMemRef, free := addMemRefToOp(op, 2, make([]byte, C.MAX_BUF_SIZE))
+	defer free()
 
-	handleMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[1])
-	handleMemRef.size = C.size_t(len(handle))
-	handleMemRef.buffer = unsafe.Pointer(&handle[0])
-
-	unsealed := make([]byte, C.MAX_BUF_SIZE)
-	pinner.Pin(&unsealed[0])
-
-	unsealedMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[2])
-	unsealedMemRef.size = C.size_t(len(unsealed))
-	unsealedMemRef.buffer = unsafe.Pointer(&unsealed[0])
-
-	err := invoke(pinner, C.fde_ta_uuid, C.TA_CMD_KEY_DECRYPT, op)
+	err := invoke(C.fde_ta_uuid, C.TA_CMD_KEY_DECRYPT, op)
 	if err != nil {
 		return nil, err
 	}
 
-	unsealed = unsealed[:unsealedMemRef.size]
+	unsealed := C.GoBytes(unsealedMemRef.buffer, C.int(unsealedMemRef.size))
 
 	return unsealed, nil
 }
 
 func (c *opteeClient) EncryptKey(input []byte) (handle []byte, sealed []byte, err error) {
-	pinner := &runtime.Pinner{}
-	defer pinner.Unpin()
+	if len(input) == 0 {
+		return nil, nil, errors.New("internal error: input must contain data")
+	}
 
 	// parameters:
 	// - input parameter containing the key to encrypt
@@ -202,56 +199,37 @@ func (c *opteeClient) EncryptKey(input []byte) (handle []byte, sealed []byte, er
 		started:    1,
 		paramTypes: params,
 	}
-	pinner.Pin(op)
 
-	pinner.Pin(&input[0])
+	_, free := addMemRefToOp(op, 0, input)
+	defer free()
 
-	inputMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
-	inputMemRef.size = C.size_t(len(input))
-	inputMemRef.buffer = unsafe.Pointer(&input[0])
+	handleMemRef, free := addMemRefToOp(op, 1, make([]byte, C.HANDLE_SIZE))
+	defer free()
 
-	handle = make([]byte, C.HANDLE_SIZE)
-	pinner.Pin(&handle[0])
+	sealedMemRef, free := addMemRefToOp(op, 2, make([]byte, C.MAX_BUF_SIZE))
+	defer free()
 
-	handleMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[1])
-	handleMemRef.size = C.size_t(len(handle))
-	handleMemRef.buffer = unsafe.Pointer(&handle[0])
-
-	sealed = make([]byte, C.MAX_BUF_SIZE)
-	pinner.Pin(&sealed[0])
-
-	sealedMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[2])
-	sealedMemRef.size = C.size_t(len(sealed))
-	sealedMemRef.buffer = unsafe.Pointer(&sealed[0])
-
-	err = invoke(pinner, C.fde_ta_uuid, C.TA_CMD_KEY_ENCRYPT, op)
+	err = invoke(C.fde_ta_uuid, C.TA_CMD_KEY_ENCRYPT, op)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	handle = handle[:handleMemRef.size]
-	sealed = sealed[:sealedMemRef.size]
+	handle = C.GoBytes(handleMemRef.buffer, C.int(handleMemRef.size))
+	sealed = C.GoBytes(sealedMemRef.buffer, C.int(sealedMemRef.size))
 
 	return handle, sealed, nil
 }
 
 func (c *opteeClient) Lock() error {
-	pinner := &runtime.Pinner{}
-	defer pinner.Unpin()
-
 	op := &C.TEEC_Operation{
 		started:    1,
 		paramTypes: teecParamTypes(C.TEEC_NONE, C.TEEC_NONE, C.TEEC_NONE, C.TEEC_NONE),
 	}
-	pinner.Pin(op)
 
-	return invoke(pinner, C.fde_ta_uuid, C.TA_CMD_LOCK, op)
+	return invoke(C.fde_ta_uuid, C.TA_CMD_LOCK, op)
 }
 
 func (c *opteeClient) Version() (string, error) {
-	pinner := &runtime.Pinner{}
-	defer pinner.Unpin()
-
 	// parameters:
 	// - output parameter containing the version string
 	// - none
@@ -262,21 +240,16 @@ func (c *opteeClient) Version() (string, error) {
 		started:    1,
 		paramTypes: params,
 	}
-	pinner.Pin(op)
 
-	version := make([]byte, 256)
-	pinner.Pin(&version[0])
+	versionMemRef, free := addMemRefToOp(op, 0, make([]byte, 256))
+	defer free()
 
-	versionMemRef := unionAsType[C.TEEC_TempMemoryReference](&op.params[0])
-	versionMemRef.size = C.size_t(len(version))
-	versionMemRef.buffer = unsafe.Pointer(&version[0])
-
-	if err := invoke(pinner, C.fde_ta_uuid, C.TA_CMD_VERSION, op); err != nil {
+	if err := invoke(C.fde_ta_uuid, C.TA_CMD_VERSION, op); err != nil {
 		return "", err
 	}
 
 	// version is a string, but no null terminator is included in the buffer
-	version = version[:versionMemRef.size]
+	version := C.GoBytes(versionMemRef.buffer, C.int(versionMemRef.size))
 
 	return string(version), nil
 }
@@ -303,8 +276,8 @@ func teecParamTypes(p0, p1, p2, p3 C.uint32_t) C.uint32_t {
 	return p0 | (p1 << 4) | (p2 << 8) | (p3 << 12)
 }
 
-func invoke(pinner *runtime.Pinner, uuid C.TEEC_UUID, cmd uint32, op *C.TEEC_Operation) error {
-	res, err := invokeUnchecked(pinner, uuid, cmd, op)
+func invoke(uuid C.TEEC_UUID, cmd uint32, op *C.TEEC_Operation) error {
+	res, err := invokeUnchecked(uuid, cmd, op)
 	if err != nil {
 		return err
 	}
@@ -315,23 +288,27 @@ func invoke(pinner *runtime.Pinner, uuid C.TEEC_UUID, cmd uint32, op *C.TEEC_Ope
 	return nil
 }
 
-func invokeUnchecked(pinner *runtime.Pinner, uuid C.TEEC_UUID, cmd uint32, op *C.TEEC_Operation) (C.TEEC_Result, error) {
-	var ctx C.TEEC_Context
-	pinner.Pin(&ctx)
+func invokeUnchecked(uuid C.TEEC_UUID, cmd uint32, op *C.TEEC_Operation) (C.TEEC_Result, error) {
+	// the optee library stores a pointer to the context (ctx) when opening a
+	// session (sess) inside of the session struct. to avoid passing a go
+	// pointer containing another go pointer into C (which cgo forbids), we must
+	// allocate the context itself in C memory
+	ctx := (*C.TEEC_Context)(C.malloc(C.size_t(unsafe.Sizeof(C.TEEC_Context{}))))
+	if ctx == nil {
+		return 0, errors.New("cannot allocate op-tee context")
+	}
+	defer C.free(unsafe.Pointer(ctx))
 
-	res := C.TEEC_InitializeContext(nil, &ctx)
+	res := C.TEEC_InitializeContext(nil, ctx)
 	if res != 0 {
 		return 0, fmt.Errorf("cannot initalize op-tee context: 0x%x", uint32(res))
 	}
-	defer C.TEEC_FinalizeContext(&ctx)
+	defer C.TEEC_FinalizeContext(ctx)
 
 	var code C.uint32_t
 	var sess C.TEEC_Session
-	pinner.Pin(&code)
-	pinner.Pin(&sess)
-	pinner.Pin(&uuid)
 
-	res = C.TEEC_OpenSession(&ctx, &sess, &uuid, C.TEEC_LOGIN_PUBLIC, nil, nil, &code)
+	res = C.TEEC_OpenSession(ctx, &sess, &uuid, C.TEEC_LOGIN_PUBLIC, nil, nil, &code)
 	if res != 0 {
 		return 0, fmt.Errorf("cannot open op-tee session: 0x%x", uint32(res))
 	}
