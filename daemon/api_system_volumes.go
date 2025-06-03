f6696048149bd825b7dcca43b2b2ae5d3b9559a6
Zeyad Gouda <zeyad.gouda@canonical.com>
2025-06-03 13:07:53 +0300

    daemon: add /v2/system-volumes endpoint for generating recovery keys
    Signed-off-by: Zeyad Gouda <zeyad.gouda@canonical.com>

---
+++ daemon/api_system_volumes.go
@@ -0,0 +1,89 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2025 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package daemon
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/snapcore/snapd/overlord/auth"
+	"github.com/snapcore/snapd/overlord/fdestate"
+	"github.com/snapcore/snapd/secboot/keys"
+)
+
+var systemVolumesCmd = &Command{
+	Path:        "/v2/system-volumes",
+	POST:        postSystemVolumesAction,
+	WriteAccess: rootAccess{},
+}
+
+type systemVolumesActionRequest struct {
+	Action string `json:"action"`
+}
+
+func postSystemVolumesAction(c *Command, r *http.Request, user *auth.UserState) Response {
+	contentType := r.Header.Get("Content-Type")
+
+	switch contentType {
+	case "application/json":
+		return postSystemVolumesActionJSON(c, r)
+	default:
+		return BadRequest("unexpected content type: %q", contentType)
+	}
+}
+
+func postSystemVolumesActionJSON(c *Command, r *http.Request) Response {
+	var req systemVolumesActionRequest
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&req); err != nil {
+		return BadRequest("cannot decode request body: %v", err)
+	}
+
+	if decoder.More() {
+		return BadRequest("extra content found in request body")
+	}
+
+	switch req.Action {
+	case "generate-recovery-key":
+		return postSystemVolumesActionGenerateRecoveryKey(c)
+	default:
+		return BadRequest("unsupported system volumes action %q", req.Action)
+	}
+}
+
+var fdemgerGenerateRecoveryKey = func(fdemgr *fdestate.FDEManager) (rkey keys.RecoveryKey, keyID string, err error) {
+	return fdemgr.GenerateRecoveryKey()
+}
+
+func postSystemVolumesActionGenerateRecoveryKey(c *Command) Response {
+	fdemgr := c.d.overlord.FDEManager()
+
+	rkey, keyID, err := fdemgerGenerateRecoveryKey(fdemgr)
+	if err != nil {
+		return InternalError(err.Error())
+	}
+
+	return SyncResponse(map[string]string{
+		"recovery-key": rkey.String(),
+		"key-id":       keyID,
+	})
+}
