93b1cb00c32f2e69fd5491299a67e623259500fb
Oliver Calder <oliver.calder@canonical.com>
2025-06-24 17:43:34 -0500

    s/apparmor: check notify version support files directly
    When checking whether a particular notification protocol version is
    supported, do not look through the list of probed AppArmor kernel
    features. Instead check for the presence of notify version files
    directly from the version support checks, and use the
    `apparmor.MetadataTagsSupported()` function to check whether tagging is
    supported, rather than performing a manual check.
    
    This means that there will be no more reason to include the notify
    version feature files in the probed AppArmor kernel features, and
    furthermore, no reason to include any feature files at all, as no other
    ones were ever checked.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- sandbox/apparmor/apparmor.go
+++ sandbox/apparmor/apparmor.go
@@ -462,14 +462,14 @@ func PromptingSupportedByFeatures(apparmorFeatures *FeaturesSupported) (bool, st
 	// the presence of the notification socket will no longer be sufficient to
 	// indicate that the system supports prompting (assuming the kernel and
 	// parser features checks above pass). When this lands, an additional
-	// directory will be added at featuresSysPath/policy/notify. At this point,
-	// there will be a file at featuresSysPath/policy/notify/user which contains
+	// directory will be added at FeaturesSysDir()/policy/notify. At this point,
+	// there will be a file at FeaturesSysDir()/policy/notify/user which contains
 	// a list of mediation classes for which prompting is supported. Thus:
-	// - If featuresSysPath/policy/notify does not exist, and the above checks
+	// - If featuresSysDir()/policy/notify does not exist, and the above checks
 	//   pass, then prompting is supported.
-	// - If featuresSysPath/policy/notify exists, but does not contain a file
+	// - If featuresSysDir()/policy/notify exists, but does not contain a file
 	//   called user, then prompting is not supported.
-	// - If featuresSysPath/policy/notify exists and includes a file called
+	// - If featuresSysDir()/policy/notify exists and includes a file called
 	//   user, then prompting is supported.
 	// The user file should contain a list of mediation classes which are
 	// supported, and this should at the bare minimum include "file", so check
@@ -565,6 +565,10 @@ var (
 
 // Each apparmor feature is manifested as a directory entry.
 const featuresSysPath = "sys/kernel/security/apparmor/features"
+
+func FeaturesSysDir() string {
+	return filepath.Join(rootPath, featuresSysPath)
+}
 
 type appArmorProber interface {
 	KernelFeatures() ([]string, error)
@@ -688,17 +692,17 @@ func (aap *appArmorProbe) ParserFeatures() ([]string, error) {
 }
 
 func probeKernelFeatures() ([]string, error) {
-	features, err := probeKernelFeaturesInDirRecursively(filepath.Join(rootPath, featuresSysPath), "")
+	features, err := probeKernelFeaturesInDirRecursively(FeaturesSysDir(), "")
 	if err != nil {
 		return []string{}, err
 	}
-	if data, err := os.ReadFile(filepath.Join(rootPath, featuresSysPath, "policy", "permstable32")); err == nil {
+	if data, err := os.ReadFile(filepath.Join(FeaturesSysDir(), "policy", "permstable32")); err == nil {
 		permstableFeatures := strings.Fields(string(data))
 		for _, feat := range permstableFeatures {
 			features = append(features, "policy:permstable32:"+feat)
 		}
 	}
-	if data, err := os.ReadFile(filepath.Join(rootPath, featuresSysPath, "policy", "notify", "user")); err == nil {
+	if data, err := os.ReadFile(filepath.Join(FeaturesSysDir(), "policy", "notify", "user")); err == nil {
 		notifyUserFeatures := strings.Fields(string(data))
 		for _, feat := range notifyUserFeatures {
 			features = append(features, "policy:notify:user:"+feat)
@@ -733,7 +737,7 @@ func probeKernelFeaturesInDirRecursively(dir string, prefix string) ([]string, e
 }
 
 func probeKernelFeaturesPermstable32Version() (int64, error) {
-	data, err := os.ReadFile(filepath.Join(rootPath, featuresSysPath, "policy", "permstable32_version"))
+	data, err := os.ReadFile(filepath.Join(FeaturesSysDir(), "policy", "permstable32_version"))
 	if err != nil {
 		return 0, err
 	}
