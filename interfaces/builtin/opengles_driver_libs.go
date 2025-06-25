47a308cb02fe8d13f88625ce658521f99f746868
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2025-06-25 16:24:37 -0400

    interfaces/builtin: add opengles-driver-libs interface
    New interface to export OpenGLES libraries to the system.

---
+++ interfaces/builtin/opengles_driver_libs.go
@@ -0,0 +1,82 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+import (
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/ldconfig"
+	"github.com/snapcore/snapd/snap"
+)
+
+const openglesDriverLibsSummary = `allows exposing OpenGLES driver libraries to the system`
+
+// Plugs only supported for the system on classic for the moment (note this is
+// checked on "system" snap installation even though this is an implicit plug
+// in that case) - in the future we will allow snaps having this as plug and
+// this declaration will have to change.
+const openglesDriverLibsBaseDeclarationPlugs = `
+  opengles-driver-libs:
+    allow-installation:
+      plug-snap-type:
+        - core
+    allow-connection:
+      slots-per-plug: *
+    deny-auto-connection: true
+`
+
+// Installation only allowed if permitted by the snap declaration (for asserted snaps)
+const openglesDriverLibsBaseDeclarationSlots = `
+  opengles-driver-libs:
+    allow-installation: false
+    deny-auto-connection: true
+`
+
+// openglesDriverLibsInterface allows exposing OpenGLES driver libraries to the system or snaps.
+type openglesDriverLibsInterface struct {
+	commonInterface
+}
+
+func (iface *openglesDriverLibsInterface) BeforePrepareSlot(slot *snap.SlotInfo) error {
+	// Validate directories
+	return validateLdconfigLibDirs(slot)
+}
+
+func (iface *openglesDriverLibsInterface) LdconfigConnectedPlug(spec *ldconfig.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
+	// The plug can only be the system plug for the time being
+	return addLdconfigLibDirs(spec, slot)
+}
+
+func (iface *openglesDriverLibsInterface) AutoConnect(*snap.PlugInfo, *snap.SlotInfo) bool {
+	return true
+}
+
+func init() {
+	registerIface(&openglesDriverLibsInterface{
+		commonInterface: commonInterface{
+			name:                 "opengles-driver-libs",
+			summary:              openglesDriverLibsSummary,
+			baseDeclarationPlugs: openglesDriverLibsBaseDeclarationPlugs,
+			baseDeclarationSlots: openglesDriverLibsBaseDeclarationSlots,
+			// Not supported on core yet
+			implicitPlugOnCore:    false,
+			implicitPlugOnClassic: true,
+		},
+	})
+}
