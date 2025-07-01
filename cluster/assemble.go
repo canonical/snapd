afea36540b38fa13c3702857515d6ac86355b13b
Andrew Phelps <andrew.phelps@canonical.com>
2025-07-01 18:59:12 -0400

    cluster, c/assemblestate: rework things into a Run method; soon to drop top-level cluster.Assemble function

--- cluster/assemble.go
+++ cluster/assemble.go
@@ -1,50 +1,30 @@
 package cluster
 
 import (
-	"bytes"
 	"context"
 	"crypto/ed25519"
 	"crypto/rand"
-	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"encoding/json"
 	"encoding/pem"
 	"errors"
-	"fmt"
-	"io"
 	"log/slog"
 	"math/big"
 	"net"
-	"net/http"
-	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/snapcore/snapd/cluster/assemblestate"
 	"github.com/snapcore/snapd/overlord/state"
-	"github.com/snapcore/snapd/randutil"
 )
 
 // AssembleOpts carries all of the options the caller can provide to [Assemble].
 type AssembleOpts struct {
 	ListenIP    net.IP
 	ListenPort  int
-	Observer    Observer
 	Logger      *slog.Logger
 	Secret      string
 	RDTOverride string
 }
-
-// Observer lets a caller watch the assembly process. This will gain some
-// methods to handle more status updates so that we can report back to the
-// caller/user.
-type Observer interface {
-	Errors(error)
-}
-
-// Discoverer returns a set of addresses that should be considered for assembly.
-type Discoverer = func(context.Context) ([]string, error)
 
 // Assemble starts an assembly session. Without a known number of expected
 // devices assembly will run until the given [context.Context] is cancelled.
@@ -55,7 +35,9 @@ type Discoverer = func(context.Context) ([]string, error)
 //
 // Eventually, this function will use the accepted [state.State] to resume a
 // stopped assemble session.
-func Assemble(st *state.State, ctx context.Context, discover Discoverer, opts AssembleOpts) (assemblestate.Routes, error) {
+//
+// TODO: get rid of this entrypoint
+func Assemble(st *state.State, ctx context.Context, discover assemblestate.Discoverer, opts AssembleOpts) (assemblestate.Routes, error) {
 	// TODO: pick how we're going to generate RDTs
 	rdt := assemblestate.RDT(opts.RDTOverride)
 	if rdt == "" {
@@ -67,11 +49,6 @@ func Assemble(st *state.State, ctx context.Context, discover Discoverer, opts As
 		logger = slog.New(slog.DiscardHandler)
 	}
 	logger = logger.With("local-rdt", rdt)
-
-	observer := opts.Observer
-	if observer == nil {
-		observer = &loggingObserver{logger: logger}
-	}
 
 	cert, key, err := createCertAndKey(opts.ListenIP)
 	if err != nil {
@@ -93,498 +70,13 @@ func Assemble(st *state.State, ctx context.Context, discover Discoverer, opts As
 
 	as, err := assemblestate.NewAssembleState(st, func(self assemblestate.RDT) (assemblestate.RouteSelector, error) {
 		return assemblestate.NewPrioritySelector(self, nil), nil
-	})
+	}, logger)
 	if err != nil {
 		return assemblestate.Routes{}, err
 	}
 
-	return assemble(ctx, as, discover, logger, observer)
-}
-
-func assemble(
-	ctx context.Context,
-	as *assemblestate.AssembleState,
-	discover Discoverer,
-	logger *slog.Logger,
-	observer Observer,
-) (assemblestate.Routes, error) {
-	server, err := newAssembleServer(as, logger, observer)
-	if err != nil {
-		return assemblestate.Routes{}, err
-	}
-
-	messenger := HTTPMessenger{
-		logger:   logger,
-		cert:     as.Cert(),
-		observer: observer,
-	}
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		periodic(ctx, time.Second*5, time.Second*1, func(ctx context.Context) {
-			discoveries, err := discover(ctx)
-			if err != nil {
-				observer.Errors(err)
-				return
-			}
-
-			// filter out our address, maybe this should be done somewhere else?
-			addrs := make([]string, 0, len(discoveries))
-			for _, addr := range discoveries {
-				if addr == as.Address() {
-					continue
-				}
-
-				addrs = append(addrs, addr)
-			}
-
-			// if this returns an error, someone is doing something
-			// wrong/malicious
-			if err := as.PublishAuth(ctx, addrs, &messenger); err != nil {
-				observer.Errors(err)
-				return
-			}
-		})
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		// TODO: consider some sort of jitter here
-
-		routes := time.NewTicker(time.Second * 5)
-		defer routes.Stop()
-
-		devices := time.NewTicker(time.Second * 1)
-		defer devices.Stop()
-
-		queries := time.NewTicker(time.Second * 1)
-		defer queries.Stop()
-
-		for {
-			select {
-			case <-routes.C:
-				const (
-					peers  = 5
-					routes = 5000
-				)
-				as.PublishRoutes(ctx, &messenger, peers, routes)
-			case <-devices.C:
-				as.PublishDevices(ctx, &messenger)
-			case <-queries.C:
-				as.PublishDeviceQueries(ctx, &messenger)
-			case <-ctx.Done():
-			}
-
-			if ctx.Err() != nil {
-				return
-			}
-		}
-	}()
-
-	// cancelling the context will cause the goroutines to terminate. until
-	// then, we wait here.
-	wg.Wait()
-
-	server.stop()
-
-	logger.Info("assemble stopped",
-		"sent-bytes", messenger.sent.Load(),
-		"received-bytes", server.received.Load(),
-	)
-
-	return as.Routes(), nil
-}
-
-func periodic(
-	ctx context.Context,
-	interval time.Duration,
-	jitter time.Duration,
-	work func(ctx context.Context),
-) {
-	delay := func() time.Duration {
-		if jitter <= 0 {
-			return interval
-		}
-
-		// +- jitter from the given interval
-		j := time.Duration(randutil.Int63n(int64(jitter)*2)) - jitter
-		return interval + j
-	}
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(delay()):
-		}
-
-		// even if the timer won the select, we should still check if the
-		// context has been cancelled
-		if ctx.Err() != nil {
-			return
-		}
-
-		work(ctx)
-	}
-}
-
-type assembleServer struct {
-	as       *assemblestate.AssembleState
-	server   *http.Server
-	received atomic.Int64
-
-	logger   *slog.Logger
-	observer Observer
-
-	wg sync.WaitGroup
-}
-
-func newAssembleServer(a *assemblestate.AssembleState, logger *slog.Logger, observer Observer) (*assembleServer, error) {
-	svr := assembleServer{
-		as:       a,
-		logger:   logger,
-		observer: observer,
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/assemble/auth", http.HandlerFunc(svr.handleAuth))
-	mux.Handle("/assemble/routes", svr.trustedHandler(svr.handleRoutes))
-	mux.Handle("/assemble/unknown", svr.trustedHandler(svr.handleUnknown))
-	mux.Handle("/assemble/devices", svr.trustedHandler(svr.handleDevices))
-
-	svr.server = &http.Server{
-		Handler: mux,
-	}
-
-	// this will be closed by svr.stop
-	ln, err := net.Listen("tcp", a.Address())
-	if err != nil {
-		return nil, err
-	}
-
-	svr.wg.Add(1)
-	go func() {
-		defer svr.wg.Done()
-
-		listener := tls.NewListener(ln, &tls.Config{
-			Certificates: []tls.Certificate{a.Cert()},
-			ClientAuth:   tls.RequireAnyClientCert,
-		})
-		_ = svr.server.Serve(listener)
-	}()
-
-	return &svr, nil
-}
-
-func (svr *assembleServer) stop() {
-	_ = svr.server.Shutdown(context.Background())
-	svr.wg.Wait()
-}
-
-type countingReader struct {
-	r     io.Reader
-	count int64
-}
-
-func (c *countingReader) Read(p []byte) (int, error) {
-	n, err := c.r.Read(p)
-	c.count += int64(n)
-	return n, err
-}
-
-func (svr *assembleServer) trustedHandler(next func(http.ResponseWriter, *http.Request, *assemblestate.PeerHandle)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.TLS == nil {
-			w.WriteHeader(403)
-			return
-		}
-
-		if len(r.TLS.PeerCertificates) != 1 {
-			w.WriteHeader(403)
-			return
-		}
-
-		h, err := svr.as.VerifyPeer(r.TLS.PeerCertificates[0].Raw)
-		if err != nil {
-			svr.logger.Debug("dropping message from untrusted peer")
-			w.WriteHeader(403)
-			return
-		}
-
-		next(w, r, h)
-	}
-}
-
-func (a *assembleServer) handleAuth(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		return
-	}
-
-	if r.TLS == nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	if len(r.TLS.PeerCertificates) != 1 {
-		w.WriteHeader(400)
-		return
-	}
-
-	// set a max size so an untrusted peer can't send some massive JSON
-	const maxAuthSize = 1024 * 4
-	counter := countingReader{
-		r: http.MaxBytesReader(w, r.Body, maxAuthSize),
-	}
-
-	var auth assemblestate.Auth
-	if err := json.NewDecoder(&counter).Decode(&auth); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	cert := r.TLS.PeerCertificates[0].Raw
-	if err := a.as.Authenticate(auth, cert); err != nil {
-		w.WriteHeader(403)
-		return
-	}
-
-	a.logger.Debug("got valid auth message", "peer-rdt", auth.RDT)
-	a.received.Add(counter.count)
-}
-
-func (svr *assembleServer) handleRoutes(w http.ResponseWriter, r *http.Request, h *assemblestate.PeerHandle) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		return
-	}
-
-	counter := countingReader{r: r.Body}
-
-	var routes assemblestate.Routes
-	if err := json.NewDecoder(&counter).Decode(&routes); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	added, total, err := h.AddRoutes(routes)
-	if err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	received := len(routes.Routes) / 3
-	svr.logger.Debug("got routes update",
-		"peer-rdt", h.RDT(),
-		"added-routes", added,
-		"wasted-routes", received-added,
-		"total-routes", total,
-	)
-	svr.received.Add(counter.count)
-}
-
-func (svr *assembleServer) handleUnknown(w http.ResponseWriter, r *http.Request, h *assemblestate.PeerHandle) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		return
-	}
-
-	counter := countingReader{r: r.Body}
-
-	var unknown assemblestate.UnknownDevices
-	if err := json.NewDecoder(&counter).Decode(&unknown); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	if err := h.AddQueries(unknown); err != nil {
-		w.WriteHeader(400)
-		svr.logger.Error("cannot add queries for device info", "error", err.Error())
-		return
-	}
-
-	svr.logger.Debug("got device queries", "peer-rdt", h.RDT())
-	svr.received.Add(counter.count)
-}
-
-func (svr *assembleServer) handleDevices(w http.ResponseWriter, r *http.Request, h *assemblestate.PeerHandle) {
-	if r.Method != "POST" {
-		w.WriteHeader(405)
-		return
-	}
-
-	counter := countingReader{r: r.Body}
-
-	var devices assemblestate.Devices
-	if err := json.NewDecoder(&counter).Decode(&devices); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	if err := h.AddDevices(devices); err != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	svr.logger.Debug("got unknown device information", "peer-rdt", h.RDT(), "devices-count", len(devices.Devices))
-	svr.received.Add(counter.count)
-}
-
-type HTTPMessenger struct {
-	// cert is the TLS certificate that we should use when sending messages.
-	cert tls.Certificate
-	// sent keeps track of how many bytes we've sent
-	sent atomic.Int64
-
-	logger   *slog.Logger
-	observer Observer
-}
-
-func (m *HTTPMessenger) Trusted(ctx context.Context, rdt assemblestate.RDT, addr string, cert []byte, kind string, data any) error {
-	if err := m.trusted(ctx, cert, addr, kind, data, rdt); err != nil {
-		m.observer.Errors(err)
-		return err
-	}
-	return nil
-}
-
-func (m *HTTPMessenger) trusted(ctx context.Context, cert []byte, addr string, kind string, data any, rdt assemblestate.RDT) error {
-	verify := func(certs [][]byte, chains [][]*x509.Certificate) error {
-		if len(certs) != 1 {
-			return fmt.Errorf("exactly one peer certificate expected, got %d", len(certs))
-		}
-
-		if !bytes.Equal(certs[0], cert) {
-			return errors.New("refusing to communicate with unexpected peer certificate")
-		}
-
-		return nil
-	}
-
-	client := http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify:    true,
-				VerifyPeerCertificate: verify,
-				Certificates:          []tls.Certificate{m.cert},
-			},
-		},
-		Timeout: time.Minute,
-	}
-
-	sent, err := send(ctx, &client, addr, kind, data)
-	if err != nil {
-		m.observer.Errors(err)
-		return err
-	}
-
-	m.logger.Debug("sent message to trusted peer",
-		"peer-rdt", rdt,
-		"peer-address", addr,
-		"kind", kind,
-	)
-
-	m.sent.Add(sent)
-
-	return nil
-}
-
-func (m *HTTPMessenger) Untrusted(ctx context.Context, addr string, kind string, data any) ([]byte, error) {
-	cert, err := m.untrusted(ctx, addr, kind, data)
-	if err != nil {
-		m.observer.Errors(err)
-		return nil, err
-	}
-	return cert, nil
-}
-
-func (m *HTTPMessenger) untrusted(ctx context.Context, addr string, kind string, data any) ([]byte, error) {
-	client := http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-				Certificates:       []tls.Certificate{m.cert},
-			},
-		},
-		Timeout: time.Minute,
-	}
-
-	res, sent, err := sendWithResponse(ctx, &client, addr, kind, data)
-	if err != nil {
-		m.observer.Errors(err)
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	m.logger.Debug("sent message to untrusted peer",
-		"peer-address", addr,
-		"kind", kind,
-	)
-
-	m.sent.Add(sent)
-
-	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("got non-200 status code in response to auth message: %d", res.StatusCode)
-	}
-
-	// TODO: we should fail earlier on this case, somewhere in the HTTP client's
-	// hooks maybe
-	if res.TLS == nil {
-		return nil, errors.New("peer attempting to communicate over unencrypted connection")
-	}
-
-	if len(res.TLS.PeerCertificates) != 1 {
-		return nil, fmt.Errorf("exactly one peer certificate expected, got %d", len(res.TLS.PeerCertificates))
-	}
-
-	return bytes.Clone(res.TLS.PeerCertificates[0].Raw), nil
-}
-
-type loggingObserver struct {
-	logger *slog.Logger
-}
-
-func (lo *loggingObserver) Errors(err error) {
-	lo.logger.Error(err.Error())
-}
-
-func send(ctx context.Context, client *http.Client, addr string, kind string, data any) (int64, error) {
-	res, count, err := sendWithResponse(ctx, client, addr, kind, data)
-	if err != nil {
-		return 0, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		return 0, fmt.Errorf("response to '%s' message contains status code %d", kind, res.StatusCode)
-	}
-
-	return count, nil
-}
-
-func sendWithResponse(ctx context.Context, client *http.Client, addr string, kind string, data any) (*http.Response, int64, error) {
-	payload, err := json.Marshal(data)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	url := fmt.Sprintf("https://%s/assemble/%s", addr, kind)
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewReader(payload))
-	if err != nil {
-		return nil, 0, err
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	return res, int64(len(payload)), nil
+	transport := assemblestate.NewHTTPTransport(logger)
+	return as.Run(ctx, transport, discover)
 }
 
 func createCertAndKey(ip net.IP) (certPEM []byte, keyPEM []byte, err error) {
---
+++ cluster/assemblestate/transport.go
@@ -0,0 +1,371 @@
+package assemblestate
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"net/http"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+// HTTPTransport implements the Transport interface using HTTPS.
+type HTTPTransport struct {
+	received atomic.Int64
+	logger   *slog.Logger
+	client   *HTTPClient
+}
+
+func NewHTTPTransport(logger *slog.Logger) *HTTPTransport {
+	return &HTTPTransport{
+		logger: logger,
+	}
+}
+
+func (h *HTTPTransport) Serve(ctx context.Context, addr string, cert tls.Certificate, as *AssembleState) error {
+	mux := http.NewServeMux()
+	mux.Handle("/assemble/auth", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.handleAuth(w, r, as)
+	}))
+	mux.Handle("/assemble/routes", h.trustedHandler(h.handleRoutes, as))
+	mux.Handle("/assemble/unknown", h.trustedHandler(h.handleUnknown, as))
+	mux.Handle("/assemble/devices", h.trustedHandler(h.handleDevices, as))
+
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		listener := tls.NewListener(ln, &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			ClientAuth:   tls.RequireAnyClientCert,
+		})
+
+		_ = server.Serve(listener)
+	}()
+
+	// wait for context cancellation
+	<-ctx.Done()
+
+	// shutdown the server
+	_ = server.Shutdown(context.Background())
+	wg.Wait()
+
+	return nil
+}
+
+type countingReader struct {
+	r     io.Reader
+	count int64
+}
+
+func (c *countingReader) Read(p []byte) (int, error) {
+	n, err := c.r.Read(p)
+	c.count += int64(n)
+	return n, err
+}
+
+func (h *HTTPTransport) trustedHandler(next func(http.ResponseWriter, *http.Request, *PeerHandle), as *AssembleState) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.TLS == nil {
+			w.WriteHeader(403)
+			return
+		}
+
+		if len(r.TLS.PeerCertificates) != 1 {
+			w.WriteHeader(403)
+			return
+		}
+
+		cert := r.TLS.PeerCertificates[0].Raw
+		peer, err := as.VerifyPeer(cert)
+		if err != nil {
+			h.logger.Debug("dropping message from untrusted peer")
+			w.WriteHeader(403)
+			return
+		}
+
+		next(w, r, peer)
+	}
+}
+
+func (h *HTTPTransport) handleAuth(w http.ResponseWriter, r *http.Request, as *AssembleState) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	if r.TLS == nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	if len(r.TLS.PeerCertificates) != 1 {
+		w.WriteHeader(400)
+		return
+	}
+
+	// set a max size so an untrusted peer can't send some massive JSON
+	const maxAuthSize = 1024 * 4
+	counter := countingReader{
+		r: http.MaxBytesReader(w, r.Body, maxAuthSize),
+	}
+
+	var auth Auth
+	if err := json.NewDecoder(&counter).Decode(&auth); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	cert := r.TLS.PeerCertificates[0].Raw
+	if err := as.Authenticate(auth, cert); err != nil {
+		w.WriteHeader(403)
+		return
+	}
+
+	h.received.Add(counter.count)
+}
+
+func (h *HTTPTransport) handleRoutes(w http.ResponseWriter, r *http.Request, peer *PeerHandle) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	counter := countingReader{r: r.Body}
+
+	var routes Routes
+	if err := json.NewDecoder(&counter).Decode(&routes); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	err := peer.AddRoutes(routes)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	h.received.Add(counter.count)
+}
+
+func (h *HTTPTransport) handleUnknown(w http.ResponseWriter, r *http.Request, peer *PeerHandle) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	counter := countingReader{r: r.Body}
+
+	var unknown UnknownDevices
+	if err := json.NewDecoder(&counter).Decode(&unknown); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	if err := peer.AddQueries(unknown); err != nil {
+		w.WriteHeader(400)
+		h.logger.Error("cannot add queries for device info", "error", err.Error())
+		return
+	}
+
+	h.received.Add(counter.count)
+}
+
+func (h *HTTPTransport) handleDevices(w http.ResponseWriter, r *http.Request, peer *PeerHandle) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	counter := countingReader{r: r.Body}
+
+	var devices Devices
+	if err := json.NewDecoder(&counter).Decode(&devices); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	if err := peer.AddDevices(devices); err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	h.received.Add(counter.count)
+}
+
+// NewClient creates a [Client] for sending outbound messages using this
+// transport.
+func (h *HTTPTransport) NewClient(cert tls.Certificate) Client {
+	// TODO: this is hacky and bad, come back to this later
+	if h.client == nil {
+		h.client = NewHTTPClient(cert)
+	}
+	return h.client
+}
+
+func (h *HTTPTransport) Stats() (tx, rx int64) {
+	rx = h.received.Load()
+	if h.client != nil {
+		tx = h.client.sent.Load()
+	}
+	return tx, rx
+}
+
+type HTTPClient struct {
+	// cert is the TLS certificate that we should use when sending messages.
+	cert tls.Certificate
+	// sent keeps track of how many bytes we've sent.
+	sent atomic.Int64
+	// limiter helps us rate limit how many outbound messages we send per
+	// second.
+	limiter *rate.Limiter
+}
+
+func NewHTTPClient(cert tls.Certificate) *HTTPClient {
+	return &HTTPClient{
+		// TODO: this can and probably should be based on byte counts, not
+		// message counts
+		limiter: rate.NewLimiter(rate.Limit(20), 1),
+		cert:    cert,
+	}
+}
+
+func (m *HTTPClient) Trusted(ctx context.Context, addr string, cert []byte, kind string, data any) error {
+	return m.trusted(ctx, cert, addr, kind, data)
+}
+
+func (m *HTTPClient) trusted(ctx context.Context, cert []byte, addr string, kind string, data any) error {
+	verify := func(certs [][]byte, chains [][]*x509.Certificate) error {
+		if len(certs) != 1 {
+			return fmt.Errorf("exactly one peer certificate expected, got %d", len(certs))
+		}
+
+		if !bytes.Equal(certs[0], cert) {
+			return errors.New("refusing to communicate with unexpected peer certificate")
+		}
+
+		return nil
+	}
+
+	client := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify:    true,
+				VerifyPeerCertificate: verify,
+				Certificates:          []tls.Certificate{m.cert},
+			},
+		},
+		Timeout: time.Minute,
+	}
+
+	if err := m.limiter.Wait(ctx); err != nil {
+		return err
+	}
+
+	sent, err := send(ctx, &client, addr, kind, data)
+	if err != nil {
+		return err
+	}
+
+	m.sent.Add(sent)
+
+	return nil
+}
+
+func (m *HTTPClient) Untrusted(ctx context.Context, addr string, kind string, data any) ([]byte, error) {
+	return m.untrusted(ctx, addr, kind, data)
+}
+
+func (m *HTTPClient) untrusted(ctx context.Context, addr string, kind string, data any) ([]byte, error) {
+	client := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+				Certificates:       []tls.Certificate{m.cert},
+			},
+		},
+		Timeout: time.Minute,
+	}
+
+	if err := m.limiter.Wait(ctx); err != nil {
+		return nil, err
+	}
+
+	res, sent, err := sendWithResponse(ctx, &client, addr, kind, data)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	m.sent.Add(sent)
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("got non-200 status code in response to auth message: %d", res.StatusCode)
+	}
+
+	// TODO: we should fail earlier on this case, somewhere in the HTTP client's
+	// hooks maybe
+	if res.TLS == nil {
+		return nil, errors.New("peer attempting to communicate over unencrypted connection")
+	}
+
+	if len(res.TLS.PeerCertificates) != 1 {
+		return nil, fmt.Errorf("exactly one peer certificate expected, got %d", len(res.TLS.PeerCertificates))
+	}
+
+	return bytes.Clone(res.TLS.PeerCertificates[0].Raw), nil
+}
+
+func send(ctx context.Context, client *http.Client, addr string, kind string, data any) (int64, error) {
+	res, count, err := sendWithResponse(ctx, client, addr, kind, data)
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return 0, fmt.Errorf("response to '%s' message contains status code %d", kind, res.StatusCode)
+	}
+
+	return count, nil
+}
+
+func sendWithResponse(ctx context.Context, client *http.Client, addr string, kind string, data any) (*http.Response, int64, error) {
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	url := fmt.Sprintf("https://%s/assemble/%s", addr, kind)
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewReader(payload))
+	if err != nil {
+		return nil, 0, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return res, int64(len(payload)), nil
+}
