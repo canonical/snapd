79f545310fc63b6a046702ab75934064535f3a24
katie <katie.may@canonical.com>
2025-07-15 09:37:49 +0200

    tests/utils: minor corrections and formatting

--- tests/utils/features/feattranslator.py
+++ tests/utils/features/feattranslator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from features import Change, Cmd, Endpoint, Interface, Status, Task
 import argparse
 import json
 import os
@@ -7,50 +8,77 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from features import Change, Cmd, Endpoint, Interface, Status, Task
 
-
-def translate_all_features(all_features_json: str, output_json: str) -> dict:
-    all_features=None
-    with open(all_features_json, 'r', encoding='utf-8') as f:
+def translate_all_features(all_features_json: str, output_json: str):
+    with open(all_features_json, "r", encoding="utf-8") as f:
         all_features = json.load(f)
 
-    if any(key for key in ["endpoints", "tasks", "commands", "ensures", "interfaces", "changes"] if key not in all_features):
+    if any(
+        key not in all_features
+        for key in [
+            "endpoints",
+            "tasks",
+            "commands",
+            "ensures",
+            "interfaces",
+            "changes",
+        ]
+    ):
         raise RuntimeError(f"missing key in all features file {all_features_json}")
 
     all_endpoints = []
-    for endpoint in all_features['endpoints']:
-        if 'actions' in endpoint and endpoint['actions']:
-            for action in endpoint['actions']:
-                all_endpoints.append(Endpoint(method=endpoint['method'], path=endpoint['path'], action=action))
+    for endpoint in all_features["endpoints"]:
+        if "actions" in endpoint and endpoint["actions"]:
+            for action in endpoint["actions"]:
+                all_endpoints.append(
+                    Endpoint(
+                        method=endpoint["method"], path=endpoint["path"], action=action
+                    )
+                )
         else:
-            all_endpoints.append(Endpoint(method=endpoint['method'], path=endpoint['path']))
-    all_cmds = [Cmd(cmd=cmd) for cmd in all_features['commands']]
+            all_endpoints.append(
+                Endpoint(method=endpoint["method"], path=endpoint["path"])
+            )
+    all_cmds = [Cmd(cmd=cmd) for cmd in all_features["commands"]]
     all_tasks = []
-    for task in all_features['tasks']:
-        status_list = [Status.done, Status.undone, Status.error] if 'has-undo' in task and task['has-undo'] else [Status.done, Status.error]
+    for task in all_features["tasks"]:
+        status_list = (
+            [Status.done, Status.undone, Status.error]
+            if "has-undo" in task and task["has-undo"]
+            else [Status.done, Status.error]
+        )
         for status in status_list:
-            all_tasks.append(Task(kind=task['kind'], last_status=status))
-    all_changes = [Change(kind=change) for change in all_features['changes']]
-    all_interfaces = [Interface(name=iface) for iface in all_features['interfaces']]
+            all_tasks.append(Task(kind=task["kind"], last_status=status))
+    all_changes = [Change(kind=change) for change in all_features["changes"]]
+    all_interfaces = [Interface(name=iface) for iface in all_features["interfaces"]]
 
-    with open(output_json, 'w', encoding='utf-8') as f:
-        json.dump({
-            'cmds': all_cmds,
-            'ensures': all_features['ensures'],
-            'tasks': all_tasks,
-            'changes': all_changes,
-            'endpoints': all_endpoints,
-            'interfaces': all_interfaces,
-        }, f)
+    with open(output_json, "w", encoding="utf-8") as f:
+        json.dump(
+            {
+                "cmds": all_cmds,
+                "ensures": all_features["ensures"],
+                "tasks": all_tasks,
+                "changes": all_changes,
+                "endpoints": all_endpoints,
+                "interfaces": all_interfaces,
+            },
+            f,
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Translates feature tagging output from 'snap debug features' to the same format as feature tagging reports")
+        description="Translates feature tagging output from 'snap debug features' to the same format as feature tagging reports"
+    )
     parser.add_argument(
-        '-f', '--file', help='snap debug features output file', required=True, type=str)
+        "-f", "--file", help="snap debug features output file", required=True, type=str
+    )
     parser.add_argument(
-        '-o', '--output', help='where to write translated output', required=True, type=str)
+        "-o",
+        "--output",
+        help="where to write translated output",
+        required=True,
+        type=str,
+    )
     args = parser.parse_args()
     translate_all_features(args.file, args.output)
--- tests/utils/features/test_feattranslator.py
+++ tests/utils/features/test_feattranslator.py
@@ -11,40 +11,51 @@ import feattranslator
 from query_features import ALL_FEATURES_FILE
 from features import Cmd, Endpoint, Change, Task, Ensure, Interface, Status
 
+
 class TestMongoUpload:
 
     def test_all_features_translation(self):
         data = {
             "changes": ["pre-download"],
-            "commands": ["abort","debug execution apparmor"],
+            "commands": ["abort", "debug execution apparmor"],
             "endpoints": [
-                {"method": "GET","path": "/v2/system-info"},
+                {"method": "GET", "path": "/v2/system-info"},
                 {
                     "actions": ["advise-system-key-mismatch"],
                     "method": "POST",
-                    "path": "/v2/system-info"
+                    "path": "/v2/system-info",
                 },
                 {
-                    "actions": ["connect","disconnect"],
+                    "actions": ["connect", "disconnect"],
                     "method": "POST",
-                    "path": "/v2/interfaces"
+                    "path": "/v2/interfaces",
                 },
             ],
             "ensures": [
-                {"function": "ensureAliasesV2","manager": "SnapManager"},
-                {"function": "autoRefresh.ensureLastRefreshAnchor","manager": "SnapManager"},
+                {"function": "ensureAliasesV2", "manager": "SnapManager"},
+                {
+                    "function": "autoRefresh.ensureLastRefreshAnchor",
+                    "manager": "SnapManager",
+                },
             ],
             "interfaces": ["accel"],
             "tasks": [
                 {"kind": "hotplug-add-slot"},
-                {"has-undo": True,"kind": "migrate-snap-home"},
-            ]
+                {"has-undo": True, "kind": "migrate-snap-home"},
+            ],
         }
         with tempfile.TemporaryDirectory() as tmpdir:
-            with open(os.path.join(tmpdir, ALL_FEATURES_FILE), 'w', encoding='utf-8') as f:
+            with open(
+                os.path.join(tmpdir, ALL_FEATURES_FILE), "w", encoding="utf-8"
+            ) as f:
                 json.dump(data, f)
-            feattranslator.translate_all_features(os.path.join(tmpdir, ALL_FEATURES_FILE), os.path.join(tmpdir, "my-output.json"))
-            with open(os.path.join(tmpdir, "my-output.json"), 'r', encoding='utf-8') as f:
+            feattranslator.translate_all_features(
+                os.path.join(tmpdir, ALL_FEATURES_FILE),
+                os.path.join(tmpdir, "my-output.json"),
+            )
+            with open(
+                os.path.join(tmpdir, "my-output.json"), "r", encoding="utf-8"
+            ) as f:
                 rewritten = json.load(f)
                 assert "changes" in rewritten
                 assert len(rewritten["changes"]) == 1
@@ -58,22 +69,36 @@ class TestMongoUpload:
                 for ep in data["endpoints"]:
                     if "actions" in ep:
                         for act in ep["actions"]:
-                            assert Endpoint(method=ep["method"], path=ep["path"], action=act)
+                            assert (
+                                Endpoint(
+                                    method=ep["method"], path=ep["path"], action=act
+                                )
+                                in rewritten["endpoints"]
+                            )
                     else:
-                        assert Endpoint(method=ep["method"], path=ep["path"]) in rewritten["endpoints"]
+                        assert (
+                            Endpoint(method=ep["method"], path=ep["path"])
+                            in rewritten["endpoints"]
+                        )
                 assert "ensures" in rewritten
                 assert len(rewritten["ensures"]) == 2
                 for en in data["ensures"]:
-                    assert Ensure(manager=en["manager"], function=en["function"]) in rewritten["ensures"]
+                    assert (
+                        Ensure(manager=en["manager"], function=en["function"])
+                        in rewritten["ensures"]
+                    )
                 assert "interfaces" in rewritten
                 assert len(rewritten["interfaces"]) == 1
                 assert Interface(name=data["interfaces"][0]) in rewritten["interfaces"]
                 assert "tasks" in rewritten
                 assert len(rewritten["tasks"]) == 5
-                for status in [Status.done, Status.error, Status.undone]:
-                    assert Task(kind=data["tasks"][0]["kind"], last_status=status)
                 for status in [Status.done, Status.error]:
-                    assert Task(kind=data["tasks"][1]["kind"], last_status=status)
-
-
-
+                    assert (
+                        Task(kind=data["tasks"][0]["kind"], last_status=status)
+                        in rewritten["tasks"]
+                    )
+                for status in [Status.done, Status.error, Status.undone]:
+                    assert (
+                        Task(kind=data["tasks"][1]["kind"], last_status=status)
+                        in rewritten["tasks"]
+                    )
