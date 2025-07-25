18147de6b1eaf07183e0821e2d32f34659e3435e
Andrew Phelps <andrew.phelps@canonical.com>
2025-07-25 12:30:45 -0400

    many: introduce KeyProtectorFactory abstraction to prepare for optee changes

--- boot/export_test.go
+++ boot/export_test.go
@@ -184,11 +184,11 @@ func MockBootloaderFind(f func(rootdir string, opts *bootloader.Options) (bootlo
 	return r
 }
 
-func MockHasFDESetupHook(f func(*snap.Info) (bool, error)) (restore func()) {
-	oldHasFDESetupHook := HasFDESetupHook
-	HasFDESetupHook = f
+func MockFDEKeyProtectorFactory(f func(*snap.Info) (secboot.KeyProtectorFactory, error)) (restore func()) {
+	oldFDEKeyProtector := FDEKeyProtectorFactory
+	FDEKeyProtectorFactory = f
 	return func() {
-		HasFDESetupHook = oldHasFDESetupHook
+		FDEKeyProtectorFactory = oldFDEKeyProtector
 	}
 }
 
