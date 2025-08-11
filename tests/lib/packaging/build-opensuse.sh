929d2fe6700362bcfcc6e5cc1665f4569622a8d8
Katie May <katie.may@canonical.com>
2025-08-11 09:59:24 +0200

    github, tests: use rpmbuild for opensuse instead of osc

---
+++ tests/lib/packaging/build-opensuse.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+    echo "Usage: $0 --vendor-tar-dir <vendor_tar_dir> --build-dir <build_dir> --pkg-dir <pkg_dir>"
+    exit 1
+}
+
+vendor_tar_dir=""
+build_dir=""
+pkg_dir=""
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --vendor-tar-dir)
+            vendor_tar_dir="$2"
+            shift 2
+            ;;
+        --build-dir)
+            build_dir="$2"
+            shift 2
+            ;;
+        --pkg-dir)
+            pkg_dir="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Check required arguments
+if [ -z "$vendor_tar_dir" ] || [ -z "$build_dir" ] || [ -z "$pkg_dir" ]; then
+    usage
+fi
+
+rpm_dir=$(rpm --eval "%_topdir")
+
+mkdir -p "$rpm_dir/SOURCES"
+mv "packaging/$pkg_dir"/* "$rpm_dir/SOURCES/"
+mv "$vendor_tar_dir"/* "$rpm_dir/SOURCES/"
+
+# Build our source package
+unshare -n -- \
+        rpmbuild --with testkeys -bs "$rpm_dir/SOURCES/snapd.spec"
+
+# And now build our binary package
+unshare -n -- \
+        rpmbuild \
+        --with testkeys \
+        --nocheck \
+        -ba \
+        "$rpm_dir/SOURCES/snapd.spec"
+
+
+find "$rpm_dir"/RPMS -type f \
+        ! -name '*debug*' \
+        -name '*.rpm' \
+        -exec mv {} "$build_dir" \;
