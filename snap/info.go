e728ac8e2863b1e5467381d89cae06b71f5c6ccb
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-08-11 18:48:01 +0200

    snap/info: Cleanup fallback desktop file computation
    It was done twice, and it's not trivial logic, so better to use a
    separate function for it

--- snap/info.go
+++ snap/info.go
@@ -1478,8 +1478,7 @@ func (app *AppInfo) SecurityTag() string {
 func (app *AppInfo) DesktopFile() string {
 	desktopFileIDs, err := app.Snap.DesktopPlugFileIDs()
 	if err != nil || len(desktopFileIDs) == 0 {
-		// fallback to a simple heuristic "$PREFIX_$APP.desktop"
-		return filepath.Join(dirs.SnapDesktopFilesDir, fmt.Sprintf("%s_%s.desktop", app.Snap.DesktopPrefix(), app.Name))
+		return app.fallbackDesktopFile()
 	}
 	// Loop through desktop-file-ids desktop interface plug attribute in order to
 	// have deterministic output
@@ -1503,7 +1502,12 @@ func (app *AppInfo) DesktopFile() string {
 			return desktopFile
 		}
 	}
-	// fallback to a simple heuristic "$PREFIX_$APP.desktop"
+	return app.fallbackDesktopFile()
+}
+
+// fallbackDesktopFile returns the fallback desktop file using the simple
+// heuristic "$PREFIX_$APP.desktop".
+func (app *AppInfo) fallbackDesktopFile() string {
 	return filepath.Join(dirs.SnapDesktopFilesDir, fmt.Sprintf("%s_%s.desktop", app.Snap.DesktopPrefix(), app.Name))
 }
 
