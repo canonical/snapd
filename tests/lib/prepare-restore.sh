0501c900c5d86798427c16b49507df4146e4e7a7
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-09-03 10:36:16 +0200

    packaging, tests: bump deb Go FIPS to 1.23 (#15890)
    Bump the version of Go FIPS toolchain to 1.23.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -528,10 +528,10 @@ prepare_project() {
                     # we are limited by the FIPS variants of go toolchain
                     # available from the PPA, and we need to match the Go
                     # version expected by during FIPS build of the deb, which
-                    # currently expects 1.21, see:
+                    # currently expects 1.23, see:
                     # https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/golang-fips
-                    best_golang=golang-1.21
-                    quiet apt install -y golang-1.21
+                    best_golang=golang-1.23
+                    quiet apt install -y golang-1.23
                     ;;
             esac
             # in 16.04: "apt build-dep -y ./" would also work but not on 14.04
