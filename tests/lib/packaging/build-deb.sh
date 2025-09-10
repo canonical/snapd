1f98bed368f664b1b3e1170f6616a246811baf73
Katie May <katie.may@canonical.com>
2025-09-10 18:24:37 +0200

    tests, github, spread: add pre-built packages for spread CI workflow (#15740)
    * spread, tests: add the ability to use pre-built packages in spread
    
    * github, tests: add workflow for RPM builds
    
    * github, tests: add deb build workflow
    
    * github, tests: add arch builds
    
    * github: add packging use in CI spread workflow
    
    * for testing only - only run one spread test
    
    * github, tests: make tarballs in an external step
    
    * tests: fix shellcheck error
    
    * github, tests: add separate workflow for opensuse 15.6
    
    * github: remove dependency on packaging for spread jobs
    
    * github: remove source rpm packages from build artifacts
    
    * tests, github: use osc when building opensuse packages
    
    * github: use osc for building opensuse
    
    * github: fix cache usage for mock dependencies
    
    * tests: use version from version file in previous job
    
    * tests: use dependencies from PKGBUILD file on arch
    
    * github: remove tarballs from package uploads
    
    * github: add missing testkeys to build for openssue
    
    * github, tests: use mock to build SRPMs
    
    * github, tests: use vendor packages in deb builds
    
    * github, tests: set version outside of build script
    
    * github, tests: use rpmbuild for opensuse instead of osc
    
    * github: remove fakeroot dependency for centos
    
    * github, tests: add comment; rename workflow; remove srpm build
    
    * github: remove fakeroot manipulation
    
    * github: rename workflows to rpm-mock/rpm-rpmbuild
    
    * tests: add newline to dockerfile
    
    * github: remove single-test run in spread
    
    * github: use prebuilt-packages also for core
    
    * tests: add glibc-static to opensuse, fedora, and amazon and libcap-devel to fedora
    
    * github: update action version
    
    * github, tests: remove go version
    
    * tests: use fedora latest container for mock
    
    * github: use correct debian directory for build
    
    * github: add message when prebuilt packages are disabled
    
    * tests: use snapd deb in GOHOME instead of SPREAD_PATH
    
    * tests: set the snapd version for core
    
    * tests: correct snapd deb path
    
    * github: add read/write permission for everybody rather than 777
    
    * tests: use GOHOME instead of GOPATH when copying packages
    
    * tests: add comments

---
+++ tests/lib/packaging/build-deb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+
+usage() {
+    echo "Usage: $0 --user <user> --pkg-version <pkg_version>"
+    exit 1
+}
+
+user=""
+pkg_version=""
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --user)
+            user="$2"
+            shift 2
+            ;;
+        --pkg-version)
+            pkg_version="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Check required arguments
+if [ -z "$user" ] || [ -z "$pkg_version" ]; then
+    usage
+fi
+
+snapd_dir=$(pwd)
+
+dch --newversion "$pkg_version" "testing build"
+# Skip tests since the unit tests are run separately in dedicated jobs in the CI
+unshare -n -- \
+    su -l -c "cd $snapd_dir && DEB_BUILD_OPTIONS='nocheck testkeys' dpkg-buildpackage -tc -b -Zgzip -uc -us" "$user"
