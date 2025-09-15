36212d4a87b65e1f92fb6cddc0a9137b3925b9ea
alfonsosanchezbeato <alfonso.sanchez-beato@canonical.com>
2025-09-15 12:43:00 -0400

    interfaces: add interfaces to expose nvidia userspace (#15645)
    * interfaces/builtin: add helper to look for file in source dir
    
    Look for a concrete file in one of the folders specified in a "source:" list in
    an interface.
    
    * interfaces/backends: add symlinks backend
    
    * interfaces/builtin: add compatibility labels to {cuda,egl}-driver-libs
    
    * interfaces/builtin: add gbm-driver-libs interface
    
    * interfaces/builtin: add opengl{,es}-driver-libs interfaces
    
    * run-checks: ignote GPU interfaces for the moment
    
    * tests/interfaces-userspace-libs: add compatibility labels needed for
    
    {cuda,egl}-driver-libs.
    
    * tests: add nvidia-userspace-libs test to test GPU interfaces
    
    Including a test snap that can be used as reference on how to expose nvidia
    libraries from a snap.
    
    * interfaces/builtin: change "source" attributes to "library-source"
    
    in GPU related interfaces.
    
    * interfaces/builtin: add helper to check icd files from icd-source path
    
    * interfaces/egl-driver_libs: create symlinks to snap config files
    
    instead of creating ourselves the configuration file.
    
    * tests: adapt to changes in interfaces new using *-source

--- interfaces/builtin/opengles_driver_libs.go
+++ interfaces/builtin/opengles_driver_libs.go
@@ -28,14 +28,14 @@ import (
 	"github.com/snapcore/snapd/snap"
 )
 
-const cudaDriverLibsSummary = `allows exposing CUDA driver libraries to the system`
+const openglesDriverLibsSummary = `allows exposing OpenGLES driver libraries to the system`
 
 // Plugs only supported for the system on classic for the moment (note this is
 // checked on "system" snap installation even though this is an implicit plug
 // in that case) - in the future we will allow snaps having this as plug and
 // this declaration will have to change.
-const cudaDriverLibsBaseDeclarationPlugs = `
-  cuda-driver-libs:
+const openglesDriverLibsBaseDeclarationPlugs = `
+  opengles-driver-libs:
     allow-installation:
       plug-snap-type:
         - core
@@ -45,28 +45,30 @@ const cudaDriverLibsBaseDeclarationPlugs = `
 `
 
 // Installation only allowed if permitted by the snap declaration (for asserted snaps)
-const cudaDriverLibsBaseDeclarationSlots = `
-  cuda-driver-libs:
+const openglesDriverLibsBaseDeclarationSlots = `
+  opengles-driver-libs:
     allow-installation: false
     deny-auto-connection: true
 `
 
-// cudaDriverLibsInterface allows exposing CUDA driver libraries to the system or snaps.
-type cudaDriverLibsInterface struct {
+// openglesDriverLibsInterface allows exposing OpenGLES driver libraries to the system or snaps.
+type openglesDriverLibsInterface struct {
 	commonInterface
 }
 
-func (iface *cudaDriverLibsInterface) BeforePrepareSlot(slot *snap.SlotInfo) error {
+func (iface *openglesDriverLibsInterface) BeforePrepareSlot(slot *snap.SlotInfo) error {
 	var compatField string
 	if err := slot.Attr("compatibility", &compatField); err != nil {
 		return err
 	}
-	// Validate format of compatibility field - we don't actually need to do
-	// anything else with it until we start to support regular snaps.
+	// Validate format of compatibility field - we don't actually need to
+	// do anything else with it until we start to support regular snaps.
 	_, err := compatibility.DecodeCompatField(compatField,
 		&compatibility.CompatSpec{Dimensions: []compatibility.CompatDimension{
-			{Tag: "cuda", Values: []compatibility.CompatRange{{Min: 0, Max: math.MaxUint}}},
-			{Tag: "ubuntu", Values: []compatibility.CompatRange{{Min: 2404, Max: math.MaxUint}}},
+			{Tag: "opengles", Values: []compatibility.CompatRange{
+				{Min: 0, Max: math.MaxUint}, {Min: 0, Max: math.MaxUint}}},
+			{Tag: "ubuntu", Values: []compatibility.CompatRange{
+				{Min: 0, Max: math.MaxUint}}},
 		}})
 	if err != nil {
 		return err
@@ -75,24 +77,24 @@ func (iface *cudaDriverLibsInterface) BeforePrepareSlot(slot *snap.SlotInfo) err
 	return validateLdconfigLibDirs(slot)
 }
 
-func (iface *cudaDriverLibsInterface) LdconfigConnectedPlug(spec *ldconfig.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
+func (iface *openglesDriverLibsInterface) LdconfigConnectedPlug(spec *ldconfig.Specification, plug *interfaces.ConnectedPlug, slot *interfaces.ConnectedSlot) error {
 	// The plug can only be the system plug for the time being
 	return addLdconfigLibDirs(spec, slot)
 }
 
-func (iface *cudaDriverLibsInterface) AutoConnect(*snap.PlugInfo, *snap.SlotInfo) bool {
+func (iface *openglesDriverLibsInterface) AutoConnect(*snap.PlugInfo, *snap.SlotInfo) bool {
 	// TODO This might need changes when we support plugs in non-system
 	// snaps for this interface.
 	return true
 }
 
 func init() {
-	registerIface(&cudaDriverLibsInterface{
+	registerIface(&openglesDriverLibsInterface{
 		commonInterface: commonInterface{
-			name:                 "cuda-driver-libs",
-			summary:              cudaDriverLibsSummary,
-			baseDeclarationPlugs: cudaDriverLibsBaseDeclarationPlugs,
-			baseDeclarationSlots: cudaDriverLibsBaseDeclarationSlots,
+			name:                 "opengles-driver-libs",
+			summary:              openglesDriverLibsSummary,
+			baseDeclarationPlugs: openglesDriverLibsBaseDeclarationPlugs,
+			baseDeclarationSlots: openglesDriverLibsBaseDeclarationSlots,
 			// Not supported on core yet
 			implicitPlugOnCore:    false,
 			implicitPlugOnClassic: true,
