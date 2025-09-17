43cce7ed66185b1f5587e40c1839dcc02be349e7
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2025-09-17 15:50:05 +0200

    osutil/vfs/lists: depth-first-search tree iterator
    The function returns a seq.Iter-like iterator that allows performing
    depth-first search of a tree. Since mounts have complex relationships,
    the tree nature one is interested in is a type parameter that provides
    the list of children, the child node and the parent pointers.
    
    The function DepthFirstSearch is nearly 1-to-1 translation of the Linux
    kernel's next_mnt function, with type safety and iterator interfaces
    bolted on. The ability to look at a particular tree aspect of a given
    type T is provided by the TreeAspect type which provides functions that
    take *T as as argument.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- osutil/vfs/lists/export_test.go
+++ osutil/vfs/lists/export_test.go
@@ -19,6 +19,15 @@
 
 package lists
 
+func (n *Node[T]) Next() *Node[T] { return n.next }
+func (n *Node[T]) Prev() *Node[T] { return n.prev }
+func (n *Node[T]) Container() *T  { return n.container }
+func (n *Node[T]) LazyInit()      { n.lazyInit() }
+func (l *List[T]) Head() *Node[T] { return &l.head }
+func (l *List[T]) Next() *Node[T] { return l.head.next }
+func (l *List[T]) Prev() *Node[T] { return l.head.prev }
+func (l *List[T]) LazyInit()      { l.head.lazyInit() }
+
 // At returns the Nth element of the list.
 //
 // Positive indices iterate first-to-last, with 0 being the first visited element.
