67f0a65b257370f88b4cd56ba2263c242ac14597
Valentin David <valentin.david@canonical.com>
2025-09-24 16:49:41 +0200

    overlord: initialize fde state after device

--- overlord/overlord.go
+++ overlord/overlord.go
@@ -189,6 +189,7 @@ func New(restartHandler restart.Handler) (*Overlord, error) {
 	if err != nil {
 		return nil, err
 	}
+	deviceMgr.AddOnInit(fdeMgr)
 	o.addManager(deviceMgr)
 
 	o.addManager(cmdstate.Manager(s, o.runner))
