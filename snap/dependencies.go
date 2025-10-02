05951a7766b32d37feae95de740b79a0f967849c
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-10-02 17:27:28 +0200

    Add accessibility interface
    A new interface is required to be able to snap the Orca screen
    reader. This interface has to give full access to the accessible
    bus managed by the AT-SPI daemon, and also to the sockets inside
    the at-spi, at-spi2-XXXXX and speech-dispatcher folders located
    at /run/user/YYYYY. Finally, it has to allow any desktop binary
    to send messages to the accessibility bus no matter if the
    receiver (Orca reader) is unconfined or snapped.
    
    Also, this interface is made dependent on `desktop-legacy`, which
    is the one that includes the default accessibility permissions,
    so it neither can nor have to be manually defined when used as a
    plug, but it's automagically added and connected when the old
    `desktop-legacy` interface is used by a snap. In the future this
    can change, to make that dependent on the base Core snap, thus
    making it automatic only for Core24 or less.
    
    Since this interface gives full access to all the accessibility
    data, which includes the current contents of any window or
    terminal, it must be marked as SUPERPRIVILEGED.

---
+++ snap/dependencies.go
@@ -0,0 +1,103 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2025 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snap
+
+import (
+	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
+)
+
+type dependency struct {
+	Name        string
+	MinimumBase int // 0 means "no minimum base"
+	MaximumBase int // 0 means "no maximum base"
+}
+
+// This is the list of interfaces to add for each interface available
+var dependencies = map[string][]dependency{
+	"desktop-legacy": {{Name: "accessibility"}},
+}
+
+// This is the list of interfaces that can't be manually defined in the
+// snapcraft.yaml file.
+var forbidenInterfaces = []string{
+	"accessibility",
+}
+
+func GetDependenciesFor(plugs []string, slots []string, globalPlugs map[string]*PlugInfo, globalSlots map[string]*SlotInfo, base string) ([]string, error) {
+	globalPlugNames := []string{}
+	for plugName := range globalPlugs {
+		globalPlugNames = append(globalPlugNames, plugName)
+	}
+	baseVersion := 0
+	if match, _ := regexp.MatchString("[cC]ore[0-9]+", base); match {
+		baseVersion, _ = strconv.Atoi(base[4:])
+	}
+	dependeciesList := []string{}
+	for _, plug := range append(plugs, globalPlugNames...) {
+		// Check no forbiden dependency is in the app plugs list
+		if CheckInterfaceIsInvalid(plug) {
+			return nil, fmt.Errorf("the interface %q is internal and can't be manually defined in the snapcraft.yaml file", plug)
+		}
+
+		deps, ok := dependencies[plug]
+		if !ok {
+			continue
+		}
+		for _, dep := range deps {
+			if baseVersion != 0 {
+				if (dep.MinimumBase != 0) && (dep.MinimumBase > baseVersion) {
+					continue
+				}
+				if (dep.MaximumBase != 0) && (dep.MaximumBase < baseVersion) {
+					continue
+				}
+			}
+			if slices.Contains(dependeciesList, dep.Name) {
+				// Don't add duplicated dependencies
+				continue
+			}
+			if slices.Contains(plugs, dep.Name) {
+				// Don't add a dependency plug if it is already a app-specific plug
+				continue
+			}
+			if slices.Contains(slots, dep.Name) {
+				// Don't add a dependency plug if it is already an app-specific slot
+				continue
+			}
+			if _, ok := globalPlugs[dep.Name]; ok {
+				// Don't add a plug dependency that is already in the global plugs
+				continue
+			}
+			if _, ok := globalSlots[dep.Name]; ok {
+				// Don't add a plug dependency that is already in the global slots
+				continue
+			}
+			dependeciesList = append(dependeciesList, dep.Name)
+		}
+	}
+	return dependeciesList, nil
+}
+
+func CheckInterfaceIsInvalid(iface string) bool {
+	return slices.Contains(forbidenInterfaces, iface)
+}
