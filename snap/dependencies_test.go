ba6618cc700b0f5944ad62f62043b73de37c8d0e
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-10-02 17:27:28 +0200

    Add accessibility interface
    A new interface is required to be able to snap the Orca screen
    reader. This interface has to give full access to the accessible
    bus managed by the AT-SPI daemon, and also to the sockets inside
    the at-spi, at-spi2-XXXXX and speech-dispatcher folders located
    at /run/user/YYYYY. Finally, it has to allow any desktop binary
    to send messages to the accessibility bus no matter if the
    receiver (Orca reader) is unconfined or snapped.
    
    To maintain backwards compatibility, a dependencies system has
    been also added. This allows to make an interface dependent on
    another. In this case, this new accessibility interface is made
    dependent on desktop-legacy interface, which is the one that
    includes the default accessibility permissions. This makes that
    any snap that has a plug for desktop-legacy will automagically
    have a plug for accessibility interface too.
    
    All the AT-SPI rules have, thus, been fully moved from the
    `desktop-legacy` to the new `accessibility` interface. The
    `AppArmorPermanentPlug` method is used to configure the
    `unconfined` rules, thus unifying them in a single interface.
    
    Since this interface gives full access to all the accessibility
    data, which includes the current contents of any window or
    terminal, it must be marked as SUPERPRIVILEGED.

---
+++ snap/dependencies_test.go
@@ -0,0 +1,133 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snap_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type DependenciesSuite struct{}
+
+var _ = Suite(&DependenciesSuite{})
+
+type dependenciesSuite struct {
+	testutil.BaseTest
+}
+
+func (s *dependenciesSuite) TestBasicDependencies(c *C) {
+	plugs := []string{"desktop-legacy"}
+	slots := []string{}
+	deps, err := snap.GetDependenciesFor(plugs, slots, "core24")
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{"accessibility"})
+}
+
+func (s *dependenciesSuite) TestForbiddenDependencies(c *C) {
+	plugs := []string{"desktop-legacy", "accessibility"}
+	slots := []string{}
+	deps, err := snap.GetDependenciesFor(plugs, slots, "core24")
+	c.Assert(err, NotNil)
+	c.Assert(deps, IsNil)
+}
+
+func (s *dependenciesSuite) TestSlotDependencies(c *C) {
+	plugs := []string{"desktop-legacy"}
+	slots := []string{"accessibility"}
+	deps, err := snap.GetDependenciesFor(plugs, slots, "core24")
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{})
+}
+
+func (s *dependenciesSuite) TestNoDependencies(c *C) {
+	plugs := []string{"desktop"}
+	slots := []string{}
+	deps, err := snap.GetDependenciesFor(plugs, slots, "core24")
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{})
+}
+
+func (s *dependenciesSuite) TestMultiDependencies(c *C) {
+	plugs := []string{"dep1", "dep2"}
+	slots := []string{}
+
+	dependencyList := map[string][]snap.DependencyElement{
+		"dep1": {
+			{Name: "dep3"},
+			{Name: "dep4"},
+		},
+		"dep2": {
+			{Name: "dep4"},
+			{Name: "dep5"},
+		},
+	}
+
+	forbiddenList := []string{}
+
+	deps, err := snap.GetDependenciesForTest(plugs, slots, "core24", dependencyList, forbiddenList)
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{"dep3", "dep4", "dep5"})
+}
+
+func (s *dependenciesSuite) TestMultiDependenciesWithMinimumBase(c *C) {
+	plugs := []string{"dep1", "dep2"}
+	slots := []string{}
+
+	dependencyList := map[string][]snap.DependencyElement{
+		"dep1": {
+			{Name: "dep3", MinimumBase: 26},
+			{Name: "dep4"},
+		},
+		"dep2": {
+			{Name: "dep4"},
+			{Name: "dep5", MinimumBase: 22},
+		},
+	}
+
+	forbiddenList := []string{}
+
+	deps, err := snap.GetDependenciesForTest(plugs, slots, "core24", dependencyList, forbiddenList)
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{"dep4", "dep5"})
+}
+
+func (s *dependenciesSuite) TestMultiDependenciesWithMaximumBase(c *C) {
+	plugs := []string{"dep1", "dep2"}
+	slots := []string{}
+
+	dependencyList := map[string][]snap.DependencyElement{
+		"dep1": {
+			{Name: "dep3", MaximumBase: 26},
+			{Name: "dep4"},
+		},
+		"dep2": {
+			{Name: "dep4"},
+			{Name: "dep5", MaximumBase: 22},
+		},
+	}
+
+	forbiddenList := []string{}
+
+	deps, err := snap.GetDependenciesForTest(plugs, slots, "core24", dependencyList, forbiddenList)
+	c.Assert(err, IsNil)
+	c.Assert(deps, DeepEquals, []string{"dep3", "dep4"})
+}
