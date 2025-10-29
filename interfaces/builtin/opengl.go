a87682f26f02aea04718635c259afbdb3866d47e
Uhm-Why <dustin.hacker0@icloud.com>
2025-10-29 03:23:19 -0600

    Address review feedback

--- interfaces/builtin/opengl.go
+++ interfaces/builtin/opengl.go
@@ -80,25 +80,6 @@ const openglConnectedPlugAppArmor = `
 /var/lib/snapd/lib/vulkan/** r,
 /var/lib/snapd/hostfs/usr/share/vulkan/icd.d/*nvidia*.json r,
 
-# Support reading Vulkan configuration from /etc (checked first by loader)
-# Even if empty, Vulkan loader tries these paths and AppArmor denials cause failures
-/etc/vulkan/ r,
-/etc/vulkan/icd.d/ r,
-/etc/vulkan/icd.d/*.json r,
-/etc/vulkan/implicit_layer.d/ r,
-/etc/vulkan/implicit_layer.d/*.json r,
-/etc/vulkan/explicit_layer.d/ r,
-/etc/vulkan/explicit_layer.d/*.json r,
-
-# Comprehensive Vulkan ICD/layer support (includes Mesa/Asahi)
-/usr/share/vulkan/ r,
-/usr/share/vulkan/icd.d/ r,
-/usr/share/vulkan/icd.d/*.json r,
-/usr/share/vulkan/implicit_layer.d/ r,
-/usr/share/vulkan/implicit_layer.d/*.json r,
-/usr/share/vulkan/explicit_layer.d/ r,
-/usr/share/vulkan/explicit_layer.d/*.json r,
-
 # Support reading the GLVND EGL vendor files
 /var/lib/snapd/lib/glvnd/ r,
 /var/lib/snapd/lib/glvnd/** r,
