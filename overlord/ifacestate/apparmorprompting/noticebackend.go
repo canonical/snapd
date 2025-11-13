fe7eb1206ac5785fbf56678fb4b340afce23be36
Oliver Calder <oliver.calder@canonical.com>
2025-11-13 11:42:30 -0600

    many: re-enable prompting notice backend (#16194)
    * Reapply "many: use notice backend for prompting notice types (#15934)"
    
    This reverts commit de2a689e3075c340784ab6b95d22bad87a9c7429.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    * s/a/n/listener: add debug log when listener sends response back to the kernel
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    * i/prompting,o/i/apparmorprompting: add debug statements around prompting notices
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    * tests: rewrite apparmor-prompting-snapd-startup to add rules via API
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    * o/i/apparmorprompting: store rule notices in /var/lib/snapd
    
    Prompting notices are more tightly coupled to the prompts or rules with
    which they are affiliated -- namely, they expect that prompt/rule IDs
    are not reused between different users. This is always true in
    production, but tests sometimes erase rules on disk directly and then
    re-create rules with the same IDs but for different users, which causes
    the prompting notice backend to throw an error.
    
    Rather than storing rule notices in /run/snapd/interfaces-requests,
    store them in /var/lib/snapd/interfaces-requests. This means they will
    more easily stay in sync with the rules on disk when tests manually
    write or roll back the contents of /var/lib/snapd.
    
    However, continue to store prompt notices in
    /run/snapd/interfaces-requests, as that is where the prompt ID mappings
    live and, importantly, prompt IDs may be reused after reboot, as the
    kernel reloads and may reuse request IDs. We don't want any
    prompt-related state persisting after reboot, including prompt notices.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    * o/i/apparmorprompting: add comment explaining notice state storage
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>
    
    ---------
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- overlord/ifacestate/apparmorprompting/noticebackend.go
+++ overlord/ifacestate/apparmorprompting/noticebackend.go
@@ -60,13 +60,24 @@ func newNoticeBackends(noticeMgr *notices.NoticeManager) (*noticeBackends, error
 		return nil, fmt.Errorf("cannot create interfaces requests run directory: %w", err)
 	}
 
+	if err := os.MkdirAll(dirs.SnapInterfacesRequestsStateDir, 0o755); err != nil {
+		return nil, fmt.Errorf("cannot create interfaces requests state directory: %w", err)
+	}
+
+	// The prompting notice backend is more tightly coupled to the prompt/rule
+	// state, since they rely on the prompt/rule state providing unique IDs.
+	// Thus, store prompt/rule notice state in the same directory as the
+	// prompts/rules themselves.
+
+	// Store prompt notices alongside the prompt ID mapping, in the run dir.
 	path := filepath.Join(dirs.SnapInterfacesRequestsRunDir, "prompt-notices.json")
 	promptNoticeBackend, err := newNoticeTypeBackend(now, nextNoticeTimestamp, path, state.InterfacesRequestsPromptNotice, promptNoticeNamespace)
 	if err != nil {
 		return nil, err
 	}
 
-	path = filepath.Join(dirs.SnapInterfacesRequestsRunDir, "rule-notices.json")
+	// Store rule notices alongside the rule state, in the state dir.
+	path = filepath.Join(dirs.SnapInterfacesRequestsStateDir, "rule-notices.json")
 	ruleNoticeBackend, err := newNoticeTypeBackend(now, nextNoticeTimestamp, path, state.InterfacesRequestsRuleUpdateNotice, ruleNoticeNamespace)
 	if err != nil {
 		return nil, err
@@ -168,6 +179,7 @@ func newNoticeTypeBackend(now time.Time, nextNoticeTimestamp func() time.Time, p
 // key equal to the given prompt/rule ID, and the given data, with notice ID
 // and type derived from the receiver.
 func (ntb *noticeTypeBackend) addNotice(userID uint32, id prompting.IDType, data map[string]string) error {
+	logger.Debugf("called addNotice(%d, %s, %v)", userID, id, data)
 	ntb.rwmu.Lock()
 	defer ntb.rwmu.Unlock()
 	key := id.String()
@@ -175,6 +187,7 @@ func (ntb *noticeTypeBackend) addNotice(userID uint32, id prompting.IDType, data
 
 	userNotices, existingNotice, existingIndex, err := ntb.searchExistingNotices(userID, noticeID)
 	if err != nil {
+		logger.Debugf("error when searching for existing notice with userID %d, noticeID %s: %v", userID, noticeID, err)
 		return err
 	}
 
@@ -198,8 +211,10 @@ func (ntb *noticeTypeBackend) addNotice(userID uint32, id prompting.IDType, data
 	if existingNotice != nil && !existingNotice.Expired(timestamp) {
 		newNotice = existingNotice.DeepCopy()
 		newNotice.Reoccur(timestamp, data, 0)
+		logger.Debugf("calling newNotice.Reoccur() for notice with ID %s", noticeID)
 	} else {
 		newNotice = state.NewNotice(noticeID, &userID, ntb.noticeType, key, timestamp, data, 0, defaultExpireAfter)
+		logger.Debugf("calling NewNotice(%s, %d, %s, %s, ...)", noticeID, userID, ntb.noticeType, key)
 	}
 
 	newUserNotices := appendNotice(userNotices, newNotice, existingIndex, expiredCount)
@@ -214,6 +229,7 @@ func (ntb *noticeTypeBackend) addNotice(userID uint32, id prompting.IDType, data
 		} else {
 			delete(ntb.idToNotice, noticeID)
 		}
+		logger.Debugf("error when saving prompting %s notice backend: %v", ntb.noticeType, err)
 		return fmt.Errorf("cannot add notice to prompting %s backend: %w", ntb.noticeType, err)
 	}
 
@@ -224,10 +240,12 @@ func (ntb *noticeTypeBackend) addNotice(userID uint32, id prompting.IDType, data
 
 	ntb.cond.Broadcast()
 
+	logger.Debugf("successfully added notice with ID %s", noticeID)
+
 	return nil
 }
 
-// searchExistingNotice looks up the list of existing notices for the given
+// searchExistingNotices looks up the list of existing notices for the given
 // userID and checks whether a notice with the given noticeID already exists.
 //
 // Returns the slice of existing notices for the given userID. If the notice
