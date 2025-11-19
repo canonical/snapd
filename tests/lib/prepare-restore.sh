00bf0f1304e0b23e77a34192c7d5437b5eb8e2c1
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-11-19 14:27:44 +0100

    tests/lib/prepare-restore: set up debian directory when building for Ubuntu
    Related: SNAPDENG-35973
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -70,22 +70,14 @@ create_test_user(){
 }
 
 build_deb(){
-    # debian-sid packaging is special
-    if os.query is-debian sid; then
-        if [ ! -d packaging/debian-sid ]; then
-            echo "no packaging/debian-sid/ directory "
-            echo "broken test setup"
-            exit 1
-        fi
+    if [ ! -L debian ] ; then
+        echo "no debian directory or symlink, broken test setup"
+        exit 1
+    fi
 
+    if os.query is-debian sid; then
         # remove etckeeper
         apt purge -y etckeeper
-
-        # debian has its own packaging
-        rm -f debian
-        # the debian dir must be a real dir, a symlink will make
-        # dpkg-buildpackage choke later.
-        mv packaging/debian-sid debian
 
         apt build-dep -y ./
 
@@ -372,7 +364,22 @@ prepare_project() {
         fi
     fi
 
-    # so is ubuntu-14.04
+    # set up debian symlink as needed
+    if os.query is-trusty; then
+        # no packaging setup for 14.04, we're no longer building the packages in
+        # CI
+        :
+    elif os.query is-ubuntu; then
+        # TODO generate packaging appropriate for a given ubuntu release
+        ln -sf packaging/ubuntu-16.04 debian
+    elif os.query is-debian-sid ; then
+        # debian sid has special packaging
+        ln -sf packaging/debian-sid debian
+    elif os.query is-debian; then
+        # TODO debian reuses ubuntu 16.04 packaging
+        ln -sf packaging/ubuntu-16.04 debian
+    fi
+
     if os.query is-trusty; then
         quiet eatmydata apt-get install -y software-properties-common
 
