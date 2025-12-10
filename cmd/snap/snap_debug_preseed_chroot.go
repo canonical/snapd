4ee54e302ce0d12015132b486418a2574372f4c7
Maciej Borzecki <maciej.borzecki@canonical.com>
2025-12-10 10:51:02 +0100

    cmd/snap/snap_debug_preseed_chroot: add snap debug preseed-chroot WIP
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

---
+++ cmd/snap/snap_debug_preseed_chroot.go
@@ -0,0 +1,51 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/jessevdk/go-flags"
+
+	"github.com/snapcore/snapd/image/preseed"
+)
+
+type cmdPreseedChroot struct {
+	Positional struct {
+		Chroot      string `positional-arg-name:"<chroot>"`
+		SystemLabel string `positional-arg-name:"<system-label>"`
+	} `positional-args:"yes" required:"yes"`
+}
+
+func init() {
+	addDebugCommand("preseed-chroot",
+		"Preseed a chroot",
+		"Preseed a chroot",
+		func() flags.Commander {
+			return &cmdPreseedChroot{}
+		}, nil, nil)
+}
+
+func (x *cmdPreseedChroot) Execute(args []string) error {
+	if err := preseed.Hybrid(x.Positional.Chroot, x.Positional.SystemLabel); err != nil {
+		return fmt.Errorf("cannot preseed hybrid system: %w", err)
+	}
+	return nil
+}
