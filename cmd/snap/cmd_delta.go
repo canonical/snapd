89d092efd40bda4278259cac7bc32741de7adac8
Ondrej Kubik <ondrej.kubik@canonical.com>
2025-12-15 16:07:06 +0000

    cmd/snap: delta: add hdiffz support
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- cmd/snap/cmd_delta.go
+++ cmd/snap/cmd_delta.go
@@ -37,9 +37,14 @@ Operations:
   generate : generate delta between source and target
   apply    : apply delta on the source
 
+  delta tool, for generate operation one of the following tools has to be spefified
+	--hdiff:   use hdiffz(hpatchz) as delta tool to generate and apply delta on squashfs pseudo file definition
+	           As this delta tool does not support streaming, pseudo file definition is processed per file within the stream.
+	--xdelta3: use xdelta3 as delta tool to generate and apply delta on squashfs pseudo file definition
+
 Examples:
-  $ snap delta generate --source <source snap file>  --target <output file> --delta <delta file>
-  $ snap delta apply    --source <source snap file>  --target <output file> --delta <delta file>
+  $ snap delta generate --hdiffz --source <SNAP>  --target <SNAP> --delta <DELTA>
+  $ snap delta apply             --source <SNAP>  --target <SNAP> --delta <DELTA>
 `)
 
 type cmdDelta struct {
@@ -48,9 +53,11 @@ type cmdDelta struct {
 		Operation string `positional-args:"yes" choices:"apply|generate"`
 	} `positional-args:"yes" required:"yes"`
 
-	Source string `long:"source" short:"s" required:"yes"`
-	Target string `long:"target" short:"t" required:"yes"`
-	Delta  string `long:"delta" short:"d" required:"yes"`
+	Source      string `long:"source" short:"s" required:"yes"`
+	Target      string `long:"target" short:"t" required:"yes"`
+	Delta       string `long:"delta" short:"d" required:"yes"`
+	Xdelta3Tool bool   `long:"xdelta3" short:"x"`
+	HdiffzTool  bool   `long:"hdiffz" short:"h"`
 }
 
 func init() {
@@ -62,6 +69,10 @@ func init() {
 			"target": i18n.G("Target snap package"),
 			// TRANSLATORS: This should not start with a lowercase letter.
 			"delta": i18n.G("Delta between source and target snap package"),
+			// TRANSLATORS: This should not start with a lowercase letter.
+			"xdelta3": i18n.G("Use xdelta3 tool for delta"),
+			// TRANSLATORS: This should not start with a lowercase letter.
+			"hdiffz": i18n.G("Use hdiffz tool for delta generation"),
 		}, []argDesc{
 			{
 				// TRANSLATORS: This needs to begin with < and end with >
@@ -73,15 +84,26 @@ func init() {
 }
 
 func (x *cmdDelta) Execute(args []string) error {
-	algo, _, _, _, err := squashfs.CheckSupportedDetlaFormats(nil)
+	algo, _, _, _, _, _, err := squashfs.CheckSupportedDeltaFormats(nil)
 	if err != nil {
 		return fmt.Errorf(i18n.G("snap delta not supported: %v"), err)
 	}
 	fmt.Fprintf(Stdout, i18n.G("Using snap delta algorithm '%s'\n"), strings.Split(algo, ";")[0])
 	if "generate" == x.Positional.Operation {
+		var deltaTool uint16
+		if x.HdiffzTool {
+			deltaTool = squashfs.DeltaToolHdiffz
+		} else if x.Xdelta3Tool {
+			deltaTool = squashfs.DeltaToolXdelta3
+		} else {
+			return fmt.Errorf(i18n.G("missing delta tool setting for generate operation"))
+		}
 		fmt.Fprintf(Stdout, i18n.G("Generating delta...\n"))
-		return squashfs.GenerateSnapDelta(x.Source, x.Target, x.Delta)
+		return squashfs.GenerateSnapDelta(x.Source, x.Target, x.Delta, deltaTool)
 	} else if "apply" == x.Positional.Operation {
+		if x.Xdelta3Tool || x.HdiffzTool {
+			return fmt.Errorf(i18n.G("cannot define delta tool (xdelta3/hdiffz) for apply operation"))
+		}
 		fmt.Fprintf(Stdout, i18n.G("Applying delta...\n"))
 		return squashfs.ApplySnapDelta(x.Source, x.Delta, x.Target)
 	}
