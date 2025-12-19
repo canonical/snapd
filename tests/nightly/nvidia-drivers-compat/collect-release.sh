d3c5b4edda7975fa6f28c393e557277f55650850
Mohit Chachada <mohit.chachada@canonical.com>
2025-12-19 10:19:28 +0100

    tests/nightly/nvidia-drivers-compat: fix filtering of transitional packages by checking number of dependencies

--- tests/nightly/nvidia-drivers-compat/collect-release.sh
+++ tests/nightly/nvidia-drivers-compat/collect-release.sh
@@ -18,7 +18,19 @@ fi
 
 # shellcheck disable=SC2086
 "$TESTSTOOLS"/lxd-state launch --remote ubuntu --image "$release" --name "$cname" --params "--ephemeral -c limits.cpu=8 -c limits.memory=8GiB ${EXTRA_LXC_ARGS-}"
-driver_versions=$(lxc exec "$cname" -- sh -c "apt-cache search nvidia-driver | grep nvidia-driver | grep -v -i transition | cut -f1 -d' '")
+# Search for nvidia-drivers in apt-cache and remove the ones which have
+#   - "transition" in their first line of Description, or,
+#   - a single dependency to other packages which is typical for transitional packages
+# The second filter is needed as for many packages "transition" is not mentioned in the
+# first line of Description but rather in the detailed Description field which is not
+# available in `apt-cache search`.
+# The first filter is still kept as the second filter is slower
+driver_versions=$(lxc exec "$cname" -- sh -c "apt-cache search nvidia-driver | grep nvidia-driver | grep -v -i transition | cut -f1 -d' ' | \
+                                              while read -r v; do \
+                                                if [ \$(apt-cache depends \$v | grep -w Depends | wc -l) -gt 1 ]; then \
+                                                  echo \$v; \
+                                                fi; \
+                                              done")
 
 echo "-- release $release"
 echo "-- driver versions:"
