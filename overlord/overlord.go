e27c0a5abf27f8eb3c94a2fa3dd06c479473a146
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-01-07 13:02:33 +0100

    store, overlord/snapstate: improve handling of snap downloads cache (#16274)
    * cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    Split out cache policy. Make it more aggressive wrt. dropping items, by
    adding targets for max age, max total cached size and max total item
    count.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snap: tweak snap-downloads-cache debug to allow better view of the policy
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * overlord: change default snap downloads cache policy
    
    Change the default snap downloads cache policy.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! cmd/snap: tweak snap-downloads-cache debug to allow better view of the policy
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! cmd/snap: tweak snap-downloads-cache debug to allow better view of the policy
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! overlord: change default snap downloads cache policy
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! overlord: change default snap downloads cache policy
    
    * fixup! cmd/snap: tweak snap-downloads-cache debug to allow better view of the policy
    
    * fixup! cache: split out cache policy, make the policy more aggressive, add count, size and age targets
    
    * fixup! overlord: change default snap downloads cache policy
    
    ---------
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/overlord.go
+++ overlord/overlord.go
@@ -55,6 +55,7 @@ import (
 	"github.com/snapcore/snapd/overlord/snapshotstate"
 	"github.com/snapcore/snapd/overlord/snapstate"
 	_ "github.com/snapcore/snapd/overlord/snapstate/policy"
+	"github.com/snapcore/snapd/release"
 
 	// import to register linkNotify callback
 	_ "github.com/snapcore/snapd/overlord/snapstate/agentnotify"
@@ -76,8 +77,6 @@ var (
 	stateLockRetryInterval = 1 * time.Second
 
 	pruneMaxChanges = 500
-
-	defaultCachedDownloads = 5
 
 	configstateInit = configstate.Init
 	systemdSdNotify = systemd.SdNotify
@@ -362,7 +361,12 @@ func (o *Overlord) newStoreWithContext(storeCtx store.DeviceAndAuthContext) snap
 	cfg := store.DefaultConfig()
 	cfg.Proxy = o.proxyConf
 	sto := storeNew(cfg, storeCtx)
-	sto.SetCacheDownloads(defaultCachedDownloads)
+	// TODO add a way for overriding cache policy
+	if release.OnClassic {
+		sto.SetCachePolicy(store.DefaultCachePolicyClassic)
+	} else {
+		sto.SetCachePolicy(store.DefaultCachePolicyCore)
+	}
 	return sto
 }
 
--- overlord/overlord_test.go
+++ overlord/overlord_test.go
@@ -49,6 +49,7 @@ import (
 	"github.com/snapcore/snapd/overlord/snapstate"
 	"github.com/snapcore/snapd/overlord/snapstate/snapstatetest"
 	"github.com/snapcore/snapd/overlord/state"
+	"github.com/snapcore/snapd/release"
 	"github.com/snapcore/snapd/snap"
 	"github.com/snapcore/snapd/snapdenv"
 	"github.com/snapcore/snapd/snapdtool"
@@ -151,10 +152,12 @@ func (ovs *overlordSuite) TestNew(c *C) {
 	// store is setup
 	sto := snapstate.Store(s, nil)
 	c.Check(sto, FitsTypeOf, &store.Store{})
-	c.Check(sto.(*store.Store).CacheDownloads(), Equals, 5)
+	c.Check(sto.(*store.Store).CachePolicy(), Equals, store.DefaultCachePolicyClassic)
 }
 
-func (ovs *overlordSuite) TestNewStore(c *C) {
+func (ovs *overlordSuite) TestNewStoreClassic(c *C) {
+	restore := release.MockOnClassic(true)
+	defer restore()
 	// this is a shallow test, the deep testing happens in the
 	// remodeling tests in managers_test.go
 	o, err := overlord.New(nil)
@@ -164,7 +167,27 @@ func (ovs *overlordSuite) TestNewStore(c *C) {
 
 	sto := o.NewStore(devBE)
 	c.Check(sto, FitsTypeOf, &store.Store{})
-	c.Check(sto.(*store.Store).CacheDownloads(), Equals, 5)
+	// we're using a classic system specific policy
+	pol := sto.(*store.Store).CachePolicy()
+	c.Check(pol, Equals, store.DefaultCachePolicyClassic)
+	c.Check(pol.MaxSizeBytes, Equals, uint64(0))
+}
+
+func (ovs *overlordSuite) TestNewStoreCore(c *C) {
+	restore := release.MockOnClassic(false)
+	defer restore()
+	o, err := overlord.New(nil)
+	c.Assert(err, IsNil)
+
+	devBE := o.DeviceManager().StoreContextBackend()
+
+	sto := o.NewStore(devBE)
+	c.Check(sto, FitsTypeOf, &store.Store{})
+	// we're using a core system specific policy
+	pol := sto.(*store.Store).CachePolicy()
+	c.Check(pol, Equals, store.DefaultCachePolicyCore)
+	// the size limit is set to 1GB
+	c.Check(pol.MaxSizeBytes, Equals, uint64(1*1024*1024*1024))
 }
 
 func (ovs *overlordSuite) TestNewWithGoodState(c *C) {
