738d36359223abde1d39cbd0aedb48c9e11b021b
Zeyad Yasser <zeyad.gouda@canonical.com>
2026-01-08 17:25:25 +0200

    osutil/keyboard: add XKB configuration helpers (#16396)
    * osutil/keyboard: add XKB configuration helpers
    
    * osutil/keyboard: extend listener to detect kb config replacement
    
    This allows detecting configuration updates that happen atomically
    through replacement (i.e. rename) like what systemd does.
    
    * osutil/keyboard: only check variant/layout length mismatch if variant is set
    
    * fixup! osutil/keyboard: fix data race in unit test
    
    ---------
    
    Signed-off-by: Zeyad Gouda <zeyad.gouda@canonical.com>

---
+++ osutil/keyboard/xkb.go
@@ -0,0 +1,221 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package keyboard
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/godbus/dbus/v5"
+
+	"github.com/snapcore/snapd/dbusutil"
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/logger"
+	"github.com/snapcore/snapd/osutil/inotify"
+	"github.com/snapcore/snapd/strutil"
+)
+
+type XKBConfig struct {
+	Model    string
+	Layouts  []string
+	Variants []string
+	Options  []string
+}
+
+// Validates that this is a valid XKB config that can be parsed
+// by Plymouth.
+func (c *XKBConfig) validate() error {
+	if strings.Contains(c.Model, ",") {
+		return fmt.Errorf("model cannot contain ',': found %q", c.Model)
+	}
+	if len(c.Variants) != 0 && len(c.Variants) != len(c.Layouts) {
+		return fmt.Errorf("layouts and variants do not have the same length")
+	}
+	// XXX: Should we check that the model and layouts are always set?
+	return nil
+}
+
+// KernelCommandLineValue returns a simplified XKB configuration
+// value that can be parsed by plymouth-set-keymap.service.
+//
+// XKB config can each have multiple comma separated values
+// but for early boot this is simplified and only the first
+// value is considered to be able to compactly join the config
+// fields using a comma-separator except for XKBOPTIONS=
+// which is appended to the end as is.
+//
+// A typical XKB configuration looks something like this:
+//
+//	-------------------------
+//	XKBMODEL="pc105"
+//	XKBLAYOUT="us,cz,de"
+//	XKBVARIANT=",bksl,"
+//	XKBOPTIONS="grp:alt_shift_toggle,terminate:ctrl_alt_bksp"
+//	-------------------------
+//
+// This example would be simplified to the following kernel cmdline
+// argument:
+// snapd.xkb="us,pc105,,grp:alt_shift_toggle,terminate:ctrl_alt_bksp"
+//
+// Note that order is important and plymouth-set-keymap.service
+// must match this ordering when parsing: layout,model,variant,option(s).
+func (c *XKBConfig) KernelCommandLineValue() string {
+	var layout, variant string
+	if len(c.Layouts) > 0 {
+		layout = c.Layouts[0]
+	}
+	if len(c.Variants) > 0 {
+		variant = c.Variants[0]
+	}
+	opts := strings.Join(c.Options, ",")
+
+	return fmt.Sprintf("%s,%s,%s,%s", layout, c.Model, variant, opts)
+}
+
+func CurrentXKBConfig() (*XKBConfig, error) {
+	conn, err := dbusutil.SystemBus()
+	if err != nil {
+		return nil, err
+	}
+
+	obj := conn.Object("org.freedesktop.locale1", "/org/freedesktop/locale1")
+
+	dbusVals := make(map[string]dbus.Variant)
+	err = obj.Call("org.freedesktop.DBus.Properties.GetAll", 0, "org.freedesktop.locale1").Store(&dbusVals)
+	if err != nil {
+		return nil, err
+	}
+
+	properties := []string{"X11Layout", "X11Model", "X11Variant", "X11Options"}
+	vals := make(map[string]string, len(properties))
+	for _, property := range properties {
+		val, ok := dbusVals[property].Value().(string)
+		if !ok {
+			return nil, fmt.Errorf("internal error: expected type string for %q, found %T", property, dbusVals[property].Value())
+		}
+		vals[property] = val
+	}
+
+	// XXX: Fallback to parsing /etc/default/keyboard and /etc/vconsole.conf
+	// if we fail to obtain values over dbus?
+
+	config := &XKBConfig{
+		Model: vals["X11Model"], // Only one model can be specified.
+	}
+	if vals["X11Layout"] != "" {
+		config.Layouts = strings.Split(vals["X11Layout"], ",")
+	}
+	if vals["X11Variant"] != "" {
+		config.Variants = strings.Split(vals["X11Variant"], ",")
+	}
+	if vals["X11Options"] != "" {
+		config.Options = strings.Split(vals["X11Options"], ",")
+	}
+	if err := config.validate(); err != nil {
+		return nil, fmt.Errorf("cannot parse XKB configuration: %v", err)
+	}
+
+	return config, nil
+}
+
+type XKBConfigListener struct {
+	iw *inotify.Watcher
+
+	ctx  context.Context
+	done context.CancelFunc
+
+	cb func(config *XKBConfig)
+}
+
+func (w *XKBConfigListener) Close() {
+	w.done()
+	if err := w.iw.Close(); err != nil {
+		logger.Noticef("cannot close inotify watcher: %v", err)
+	}
+}
+
+// NewXKBConfigListener returns a XKBConfigListener that listens
+// for XKB configuration changes and calls cb(XKBConfig) if a
+// potential configuration change is detected.
+func NewXKBConfigListener(ctx context.Context, cb func(config *XKBConfig)) (*XKBConfigListener, error) {
+	iw, err := inotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	// We care about IN_MOVED_TO as well to detect configuration updates that
+	// happen atomically through replacement i.e. rename, renameat, renameat2.
+	etcDir := filepath.Join(dirs.GlobalRootDir, "/etc")
+	if err := iw.AddWatch(etcDir, inotify.InCloseWrite|inotify.InMovedTo); err != nil {
+		return nil, err
+	}
+	etcDefaultDir := filepath.Join(dirs.GlobalRootDir, "/etc/default")
+	if err := iw.AddWatch(etcDefaultDir, inotify.InCloseWrite|inotify.InMovedTo); err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	watcher := &XKBConfigListener{
+		iw:   iw,
+		ctx:  ctx,
+		done: cancel,
+		cb:   cb,
+	}
+	go watcher.loop()
+	return watcher, nil
+}
+
+func (w *XKBConfigListener) loop() {
+	var kbConfigFiles = []string{
+		filepath.Join(dirs.GlobalRootDir, "/etc/default/keyboard"),
+		filepath.Join(dirs.GlobalRootDir, "/etc/vconsole.conf"),
+	}
+	for {
+		select {
+		case e := <-w.iw.Event:
+			if !strutil.ListContains(kbConfigFiles, e.Name) {
+				continue
+			}
+			// Note: Reading the XKB configuration straight after
+			// the inotify event is fine because up until systemd
+			// v259 it always reads the properties from the relevant
+			// files, and since inotify is setup to only listen for
+			// IN_CLOSE_WRITE and IN_MOVED_TO events so the content
+			// should have been already written and the files closed.
+			//
+			// Keeping this in mind, There are no guarantees that
+			// systemd's behavior stays the same, so the listener
+			// callback notifications should always be considered
+			// as best-effort only with no guarantees of always up
+			// to date XKB configuration values.
+			config, err := CurrentXKBConfig()
+			if err != nil {
+				logger.Noticef("cannot obtain current XKB configuration: %v", err)
+				continue
+			}
+			w.cb(config)
+		case <-w.ctx.Done():
+			w.Close()
+			return
+		}
+	}
+}
---
+++ osutil/keyboard/xkb_test.go
@@ -0,0 +1,265 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package keyboard_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/godbus/dbus/v5"
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/dbusutil"
+	"github.com/snapcore/snapd/dbusutil/dbustest"
+	"github.com/snapcore/snapd/dirs"
+	"github.com/snapcore/snapd/osutil"
+	"github.com/snapcore/snapd/osutil/keyboard"
+	"github.com/snapcore/snapd/testutil"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type xkbTestSuite struct {
+	testutil.BaseTest
+
+	rootDir string
+
+	mockDBusProperties map[string]any
+}
+
+var _ = Suite(&xkbTestSuite{})
+
+func (s *xkbTestSuite) SetUpTest(c *C) {
+	s.BaseTest.SetUpTest(c)
+
+	s.rootDir = c.MkDir()
+	dirs.SetRootDir(s.rootDir)
+	s.AddCleanup(func() { dirs.SetRootDir("") })
+
+	s.mockDBusProperties = nil
+
+	systemBus, err := dbustest.Connection(func(msg *dbus.Message, n int) ([]*dbus.Message, error) {
+		c.Assert(msg.Type, Equals, dbus.TypeMethodCall)
+		c.Assert(msg.Flags, Equals, dbus.Flags(0))
+		c.Assert(msg.Headers, DeepEquals, map[dbus.HeaderField]dbus.Variant{
+			dbus.FieldPath:        dbus.MakeVariant(dbus.ObjectPath("/org/freedesktop/locale1")),
+			dbus.FieldInterface:   dbus.MakeVariant("org.freedesktop.DBus.Properties"),
+			dbus.FieldMember:      dbus.MakeVariant("GetAll"),
+			dbus.FieldDestination: dbus.MakeVariant("org.freedesktop.locale1"),
+			dbus.FieldSignature:   dbus.MakeVariant(dbus.SignatureOf("")),
+		})
+		c.Assert(msg.Body, DeepEquals, []any{"org.freedesktop.locale1"})
+
+		properties := make(map[string]dbus.Variant, len(s.mockDBusProperties))
+		for property, val := range s.mockDBusProperties {
+			properties[property] = dbus.MakeVariant(val)
+		}
+		resp := &dbus.Message{
+			Type: dbus.TypeMethodReply,
+			Headers: map[dbus.HeaderField]dbus.Variant{
+				dbus.FieldReplySerial: dbus.MakeVariant(msg.Serial()),
+				dbus.FieldPath:        dbus.MakeVariant(dbus.ObjectPath("/org/freedesktop/locale1")),
+				dbus.FieldSignature:   dbus.MakeVariant(dbus.SignatureOf(map[string]dbus.Variant{})),
+			},
+			Body: []any{properties},
+		}
+		return []*dbus.Message{resp}, nil
+	})
+	c.Assert(err, IsNil)
+
+	s.AddCleanup(dbusutil.MockOnlySystemBusAvailable(systemBus))
+}
+
+func (s *xkbTestSuite) TestXKBConfigKernelCommandLineValue(c *C) {
+	config := keyboard.XKBConfig{
+		Model:    "pc105",
+		Variants: []string{"", "bksl", ""},
+		Layouts:  []string{"us", "cz", "de"},
+		Options:  []string{"grp:alt_shift_toggle", "terminate:ctrl_alt_bksp"},
+	}
+	c.Assert(config.KernelCommandLineValue(), Equals, "us,pc105,,grp:alt_shift_toggle,terminate:ctrl_alt_bksp")
+}
+
+func (s *xkbTestSuite) TestCurrentXKBConfig(c *C) {
+	s.mockDBusProperties = map[string]any{
+		"X11Model":   "pc105",
+		"X11Layout":  "us,cz,de",
+		"X11Variant": ",bksl,",
+		"X11Options": "grp:alt_shift_toggle,terminate:ctrl_alt_bksp",
+	}
+
+	config, err := keyboard.CurrentXKBConfig()
+	c.Assert(err, IsNil)
+	c.Assert(config, DeepEquals, &keyboard.XKBConfig{
+		Model:    "pc105",
+		Variants: []string{"", "bksl", ""},
+		Layouts:  []string{"us", "cz", "de"},
+		Options:  []string{"grp:alt_shift_toggle", "terminate:ctrl_alt_bksp"},
+	})
+	c.Assert(config.KernelCommandLineValue(), Equals, "us,pc105,,grp:alt_shift_toggle,terminate:ctrl_alt_bksp")
+
+	// defaults
+	s.mockDBusProperties = map[string]any{
+		"X11Model":   "",
+		"X11Layout":  "",
+		"X11Variant": "",
+		"X11Options": "",
+	}
+
+	config, err = keyboard.CurrentXKBConfig()
+	c.Assert(err, IsNil)
+	c.Assert(config, DeepEquals, &keyboard.XKBConfig{
+		Model:    "",
+		Variants: nil,
+		Layouts:  nil,
+		Options:  nil,
+	})
+	c.Assert(config.KernelCommandLineValue(), Equals, ",,,")
+}
+
+func (s *xkbTestSuite) TestCurrentXKBConfigErrors(c *C) {
+	type testcase struct {
+		properties  map[string]any
+		expectedErr string
+	}
+
+	tcs := []testcase{
+		{
+			properties: map[string]any{
+				"X11Model":   "pc105,pc104",
+				"X11Layout":  "",
+				"X11Variant": "",
+				"X11Options": "",
+			},
+			expectedErr: `cannot parse XKB configuration: model cannot contain ',': found "pc105,pc104"`,
+		},
+		{
+			properties: map[string]any{
+				"X11Model":   "pc105",
+				"X11Layout":  "cz,us",
+				"X11Variant": "bksl",
+				"X11Options": "",
+			},
+			expectedErr: `cannot parse XKB configuration: layouts and variants do not have the same length`,
+		},
+		{
+			properties: map[string]any{
+				"X11Model":  "pc105",
+				"X11Layout": "us",
+				// If X11Variant is set, The length check should fail.
+				"X11Variant": ",bksl",
+				"X11Options": "",
+			},
+			expectedErr: `cannot parse XKB configuration: layouts and variants do not have the same length`,
+		},
+		{
+			properties: map[string]any{
+				"X11Model":  "pc105",
+				"X11Layout": "us,cz,de",
+				// If X11Variant is unset, the length check is ignored and
+				// the variant defaults to "basic".
+				"X11Variant": "",
+				"X11Options": "",
+			},
+		},
+		{
+			properties: map[string]any{
+				"X11Model":   1,
+				"X11Layout":  "",
+				"X11Variant": "",
+				"X11Options": "",
+			},
+			expectedErr: `internal error: expected type string for "X11Model", found int32`,
+		},
+	}
+
+	for i, tc := range tcs {
+		cmt := Commentf("tc[%d] failed", i)
+		s.mockDBusProperties = tc.properties
+		config, err := keyboard.CurrentXKBConfig()
+		if tc.expectedErr != "" {
+			c.Check(err, ErrorMatches, tc.expectedErr, cmt)
+			c.Check(config, IsNil, cmt)
+		} else {
+			c.Check(err, IsNil, cmt)
+		}
+	}
+}
+
+func (s *xkbTestSuite) TestXKBConfigListener(c *C) {
+	vconsoleConfPath := filepath.Join(s.rootDir, "/etc/vconsole.conf")
+	kbConfPath := filepath.Join(s.rootDir, "/etc/default/keyboard")
+	c.Assert(os.MkdirAll(filepath.Dir(kbConfPath), 0755), IsNil)
+	// mock /etc/default/keyboard as a symlink to /etc/vconsole.conf
+	c.Assert(os.WriteFile(vconsoleConfPath, nil, 0644), IsNil)
+	c.Assert(os.Symlink(vconsoleConfPath, kbConfPath), IsNil)
+
+	s.mockDBusProperties = map[string]any{
+		"X11Model":   "pc105",
+		"X11Layout":  "us,cz,de",
+		"X11Variant": ",bksl,",
+		"X11Options": "grp:alt_shift_toggle,terminate:ctrl_alt_bksp",
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	called := 0
+	cbChan := make(chan bool)
+	cb := func(config *keyboard.XKBConfig) {
+		called++
+		c.Assert(config, DeepEquals, &keyboard.XKBConfig{
+			Model:    "pc105",
+			Variants: []string{"", "bksl", ""},
+			Layouts:  []string{"us", "cz", "de"},
+			Options:  []string{"grp:alt_shift_toggle", "terminate:ctrl_alt_bksp"},
+		})
+		cbChan <- true
+	}
+	listener, err := keyboard.NewXKBConfigListener(ctx, cb)
+	c.Assert(err, IsNil)
+	c.Check(listener, NotNil)
+
+	c.Assert(os.WriteFile(kbConfPath, []byte("1"), 0644), IsNil)
+	<-cbChan
+	c.Assert(os.WriteFile(kbConfPath, []byte("2"), 0644), IsNil)
+	<-cbChan
+	c.Assert(os.WriteFile(vconsoleConfPath, []byte("3"), 0644), IsNil)
+	<-cbChan
+	// Simulate replacement i.e. rename
+	c.Assert(osutil.AtomicWriteFile(vconsoleConfPath, []byte("4"), 0644, 0), IsNil)
+	<-cbChan
+	c.Assert(os.WriteFile(vconsoleConfPath, []byte("5"), 0644), IsNil)
+	<-cbChan
+	c.Assert(os.WriteFile(vconsoleConfPath, []byte("6"), 0644), IsNil)
+	<-cbChan
+
+	c.Assert(called, Equals, 6)
+
+	cancel()
+	// Wait to make sure we don't accidentally check number of calls
+	// before inotify gets a chance to detect the event.
+	time.Sleep(20 * time.Millisecond)
+	c.Assert(os.WriteFile(vconsoleConfPath, []byte("4"), 0644), IsNil)
+	// Context cancellation closes the inotify watcher.
+	c.Assert(called, Equals, 6)
+}
