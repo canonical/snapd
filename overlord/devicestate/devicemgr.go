5bd21700ef0ed7b2dc9cf319468d6d74dc64e84c
Philip Meulengracht <philip.meulengracht@canonical.com>
2026-01-19 10:04:56 +0100

    overlord/devicestate: register the new ensure function

--- overlord/devicestate/devicemgr.go
+++ overlord/devicestate/devicemgr.go
@@ -98,6 +98,7 @@ func init() {
 	swfeats.RegisterEnsure("DeviceManager", "ensurePostFactoryReset")
 	swfeats.RegisterEnsure("DeviceManager", "ensureExpiredUsersRemoved")
 	swfeats.RegisterEnsure("DeviceManager", "ensureEarlyBootXKBConfigUpdated")
+	swfeats.RegisterEnsure("DeviceManager", "ensureCaCertificateDatabase")
 }
 
 // EarlyConfig is a hook set by configstate that can process early configuration
