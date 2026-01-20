df513bb64d204114ec929df2ae054efefc0fec76
Mohit Chachada <mohit.chachada@canonical.com>
2026-01-20 08:49:07 +0100

    daemon: fix unit test

--- daemon/api_confdb_test.go
+++ daemon/api_confdb_test.go
@@ -267,7 +267,7 @@ func (s *confdbSuite) TestGetTxError(c *C) {
 		{name: "no data", err: confdb.NewNoDataError(view, nil), status: 400, kind: client.ErrorKindConfigNoSuchOption},
 		{name: "no view", err: &confdbstate.NoViewError{}, status: 400, kind: client.ErrorKindOptionNotAvailable},
 		{name: "no match", err: confdb.NewNoMatchError(view, "", nil), status: 400, kind: client.ErrorKindOptionNotAvailable},
-		{name: "no placeholder", err: &confdb.UnmatchedConstraintsError{}, status: 400, kind: client.ErrorKindOptionNotAvailable},
+		{name: "unmatched constraints", err: &confdb.UnmatchedConstraintsError{}, status: 400},
 		{name: "internal", err: errors.New("internal"), status: 500},
 	} {
 		restore := daemon.MockConfdbstateLoadConfdbAsync(func(*state.State, *confdb.View, []string, map[string]any) (string, error) {
