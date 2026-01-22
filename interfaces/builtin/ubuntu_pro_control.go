a0c6fda59ac995950ba95913607a1fbc1ad5a306
Andrew Phelps <andrew.phelps@canonical.com>
2026-01-22 10:30:11 -0500

    i/builtin, tests: add ubuntu-pro-control interface to provide access to UbuntuAdvantage dbus API

---
+++ interfaces/builtin/ubuntu_pro_control.go
@@ -0,0 +1,131 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const ubuntuProControlSummary = `allows control of the Ubuntu Pro desktop daemon`
+
+const ubuntuProControlBaseDeclarationPlugs = `
+  ubuntu-pro-control:
+    allow-installation: false
+    deny-auto-connection: true
+`
+
+const ubuntuProControlBaseDeclarationSlots = `
+  ubuntu-pro-control:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const ubuntuProControlConnectedPlugAppArmor = `
+# Description: Allow access to the Ubuntu Pro desktop daemon.
+
+#include <abstractions/dbus-strict>
+
+# Allow use of ObjectManager APIs, used to enumerate services.
+# do not use peer=(label=unconfined) here since this is DBus activated
+dbus (send)
+    bus=system
+    path=/
+    interface=org.freedesktop.DBus.ObjectManager
+    member=GetManagedObjects
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (receive)
+    bus=system
+    path=/
+    interface=org.freedesktop.DBus.ObjectManager
+    member=Interfaces{Added,Removed}
+    peer=(label=unconfined),
+
+# Allow access to manager methods and properties.
+# do not use peer=(label=unconfined) here since this is DBus activated
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Manager
+    interface=com.canonical.UbuntuAdvantage.Manager
+    member={Attach,Detach}
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Manager
+    interface=org.freedesktop.DBus.Properties
+    member=Get{,All}
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (receive)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Manager
+    interface=org.freedesktop.DBus.Properties
+    member=PropertiesChanged
+    peer=(label=unconfined),
+
+# Allow access to service methods and properties.
+# do not use peer=(label=unconfined) here since this is DBus activated
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Services/*
+    interface=com.canonical.UbuntuAdvantage.Service
+    member={Enable,Disable}
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Services/*
+    interface=org.freedesktop.DBus.Properties
+    member=Get{,All}
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (receive)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Services/*
+    interface=org.freedesktop.DBus.Properties
+    member=PropertiesChanged
+    peer=(label=unconfined),
+
+# Allow clients to introspect the service.
+# do not use peer=(label=unconfined) here since this is DBus activated
+dbus (send)
+    bus=system
+    path=/
+    interface=org.freedesktop.DBus.Introspectable
+    member=Introspect
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Manager
+    interface=org.freedesktop.DBus.Introspectable
+    member=Introspect
+    peer=(name=com.canonical.UbuntuAdvantage),
+dbus (send)
+    bus=system
+    path=/com/canonical/UbuntuAdvantage/Services/*
+    interface=org.freedesktop.DBus.Introspectable
+    member=Introspect
+    peer=(name=com.canonical.UbuntuAdvantage),
+`
+
+func init() {
+	registerIface(&commonInterface{
+		name:                  "ubuntu-pro-control",
+		summary:               ubuntuProControlSummary,
+		implicitOnClassic:     true,
+		baseDeclarationPlugs:  ubuntuProControlBaseDeclarationPlugs,
+		baseDeclarationSlots:  ubuntuProControlBaseDeclarationSlots,
+		connectedPlugAppArmor: ubuntuProControlConnectedPlugAppArmor,
+	})
+}
---
+++ interfaces/builtin/ubuntu_pro_control_test.go
@@ -0,0 +1,94 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/interfaces"
+	"github.com/snapcore/snapd/interfaces/apparmor"
+	"github.com/snapcore/snapd/interfaces/builtin"
+	"github.com/snapcore/snapd/snap"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type UbuntuProControlInterfaceSuite struct {
+	iface    interfaces.Interface
+	slot     *interfaces.ConnectedSlot
+	slotInfo *snap.SlotInfo
+	plug     *interfaces.ConnectedPlug
+	plugInfo *snap.PlugInfo
+}
+
+var _ = Suite(&UbuntuProControlInterfaceSuite{
+	iface: builtin.MustInterface("ubuntu-pro-control"),
+})
+
+func (s *UbuntuProControlInterfaceSuite) SetUpTest(c *C) {
+	const coreYaml = `name: core
+version: 0
+type: os
+slots:
+ ubuntu-pro-control:
+  interface: ubuntu-pro-control
+`
+	s.slot, s.slotInfo = MockConnectedSlot(c, coreYaml, nil, "ubuntu-pro-control")
+
+	const consumerYaml = `name: consumer
+version: 0
+apps:
+ app:
+  plugs: [ubuntu-pro-control]
+`
+	s.plug, s.plugInfo = MockConnectedPlug(c, consumerYaml, nil, "ubuntu-pro-control")
+}
+
+func (s *UbuntuProControlInterfaceSuite) TestName(c *C) {
+	c.Assert(s.iface.Name(), Equals, "ubuntu-pro-control")
+}
+
+func (s *UbuntuProControlInterfaceSuite) TestSanitize(c *C) {
+	c.Assert(interfaces.BeforePreparePlug(s.iface, s.plugInfo), IsNil)
+	c.Assert(interfaces.BeforePrepareSlot(s.iface, s.slotInfo), IsNil)
+}
+
+func (s *UbuntuProControlInterfaceSuite) TestAppArmorConnectedPlug(c *C) {
+	appSet, err := interfaces.NewSnapAppSet(s.plug.Snap(), nil)
+	c.Assert(err, IsNil)
+	spec := apparmor.NewSpecification(appSet)
+	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
+	c.Assert(spec.SecurityTags(), HasLen, 1)
+	c.Check(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, `interface=org.freedesktop.DBus.ObjectManager`)
+	c.Check(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, `interface=com.canonical.UbuntuAdvantage.Manager`)
+	c.Check(spec.SnippetForTag("snap.consumer.app"), testutil.Contains, `interface=com.canonical.UbuntuAdvantage.Service`)
+}
+
+func (s *UbuntuProControlInterfaceSuite) TestStaticInfo(c *C) {
+	si := interfaces.StaticInfoOf(s.iface)
+	c.Check(si.ImplicitOnCore, Equals, false)
+	c.Check(si.ImplicitOnClassic, Equals, true)
+	c.Check(si.Summary, Equals, "allows control of the Ubuntu Pro desktop daemon")
+	c.Check(si.BaseDeclarationPlugs, testutil.Contains, "ubuntu-pro-control")
+	c.Check(si.BaseDeclarationSlots, testutil.Contains, "ubuntu-pro-control")
+}
+
+func (s *UbuntuProControlInterfaceSuite) TestInterfaces(c *C) {
+	c.Check(builtin.Interfaces(), testutil.DeepContains, s.iface)
+}
