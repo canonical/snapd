359e254eb26bbc920c5e27408dda0dea562a0123
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-01-30 14:51:19 +0100

    Reapply "many: update vendored apparmor parser to version 4.1.3 (#15969)" (#16426)
    This reverts commit 5c3292b40f96baf273fae6aece201a85e84617b4.

--- build-aux/snap/local/apparmor/af_names.h
+++ build-aux/snap/local/apparmor/af_names.h
@@ -1,8 +1,8 @@
 /*
   this file was generated on a Ubuntu noble install from the upstream
-  apparmor-4.0.2 release tarball as follows:
+  apparmor-4.1.3 release tarball as follows:
 
-  AA_VER=4.0.2
+  AA_VER=4.1.3
   TARBALL_NAME="apparmor-v${AA_VER}"
   wget \
   "https://gitlab.com/apparmor/apparmor/-/archive/v${AA_VER}/${TARBALL_NAME}.tar.gz"
