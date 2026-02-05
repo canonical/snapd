266772252ec1c59ce307416ae67d1968209f1b22
Kyuyrii <kyuyriibyakko@gmail.com>
2026-02-04 23:50:16 -0300

    Adding missing permissions

--- interfaces/builtin/umu_support_test.go
+++ interfaces/builtin/umu_support_test.go
@@ -79,41 +79,70 @@ func (s *UMUSupportInterfaceSuite) TestSanitizePlug(c *C) {
 func (s *UMUSupportInterfaceSuite) TestAppArmorSpec(c *C) {
 	restore := apparmor_sandbox.MockFeatures(nil, nil, []string{"userns"}, nil)
 	defer restore()
-	
+
 	appSet, err := interfaces.NewSnapAppSet(s.plug.Snap(), nil)
 	c.Assert(err, IsNil)
 	spec := apparmor.NewSpecification(appSet)
 	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
-	
+
 	c.Assert(spec.SecurityTags(), DeepEquals, []string{"snap.consumer.app"})
 	snippet := spec.SnippetForTag("snap.consumer.app")
-	
-	// Test basic capabilities
-	c.Check(snippet, testutil.Contains, "capability sys_admin,")
-	
-	// Test mounting operations
+
+	c.Check(snippet, testutil.Contains, "@{PROC}/sys/kernel/overflowuid r,")
+	c.Check(snippet, testutil.Contains, "@{PROC}/sys/kernel/overflowgid r,")
+	c.Check(snippet, testutil.Contains, "@{PROC}/sys/kernel/sched_autogroup_enabled r,")
+	c.Check(snippet, testutil.Contains, "owner @{PROC}/@{pid}/uid_map rw,")
+	c.Check(snippet, testutil.Contains, "owner @{PROC}/@{pid}/gid_map rw,")
+	c.Check(snippet, testutil.Contains, "owner @{PROC}/@{pid}/setgroups rw,")
+	c.Check(snippet, testutil.Contains, "owner @{PROC}/@{pid}/mounts r,")
+	c.Check(snippet, testutil.Contains, "owner @{PROC}/@{pid}/mountinfo r,")
+
 	c.Check(snippet, testutil.Contains, "mount,")
 	c.Check(snippet, testutil.Contains, "umount,")
 	c.Check(snippet, testutil.Contains, "pivot_root,")
-	
-	// Test user namespaces
+
 	c.Check(snippet, testutil.Contains, "userns,")
-	
-	// Test directory access
+
 	c.Check(snippet, testutil.Contains, "/newroot/** rwkl,")
-	
-	// Test host filesystem access
+
+	c.Check(snippet, testutil.Contains, "mount options=(rw, rbind) /oldroot/usr/ -> /newroot/run/host/usr/,")
+	c.Check(snippet, testutil.Contains, "mount options=(rw, rbind) /oldroot/etc/ -> /newroot/run/host/etc/,")
+	c.Check(snippet, testutil.Contains, "mount options=(rw, rbind) /oldroot/usr/lib/os-release -> /newroot/run/host/os-release,")
+
 	c.Check(snippet, testutil.Contains, "/oldroot/usr/** r,")
+	c.Check(snippet, testutil.Contains, "/oldroot/etc/** r,")
+
 	c.Check(snippet, testutil.Contains, "/run/host/ r,")
-	
-	// Test pressure-vessel executables
+	c.Check(snippet, testutil.Contains, "/run/host/usr/ r,")
+	c.Check(snippet, testutil.Contains, "/run/host/etc/ r,")
+	c.Check(snippet, testutil.Contains, "/run/host/lib{,32,64}/ r,")
+	c.Check(snippet, testutil.Contains, "/run/host/usr/lib{,32,64}/ r,")
+	c.Check(snippet, testutil.Contains, "/run/host/lib{,32,64}/** mr,")
+	c.Check(snippet, testutil.Contains, "/run/host/usr/lib{,32,64}/** mr,")
+
+	c.Check(snippet, testutil.Contains, "/bindfile* rw,")
+
+	c.Check(snippet, testutil.Contains, "mount options=(rw, rbind) /bindfile* -> /newroot/**,")
+
+	c.Check(snippet, testutil.Contains, "/media/ r,")
+	c.Check(snippet, testutil.Contains, "/mnt/ r,")
+	c.Check(snippet, testutil.Contains, "/run/media/ r,")
+
 	c.Check(snippet, testutil.Contains, "/usr/bin/steam-runtime-launcher-interface-* ixr,")
-	
-	// Test desktop integration
-	c.Check(snippet, testutil.Contains, "/home/*/.config/menus/{,**} rw,")
-	c.Check(snippet, testutil.Contains, "/home/*/.local/share/applications/{,**} rw,")
-	c.Check(snippet, testutil.Contains, "/home/*/.local/share/desktop-directories/{,**} rw,")
-	c.Check(snippet, testutil.Contains, "/home/*/.local/share/icons/{,**} rw,")
+	c.Check(snippet, testutil.Contains, "/usr/lib/pressure-vessel/from-host/libexec/steam-runtime-tools-*/* ixr,")
+
+	c.Check(snippet, testutil.Contains, "/*/pressure-vessel/** mrw,")
+
+	c.Check(snippet, testutil.Contains, "owner /home/*/.config/menus/{,**} rw,")
+	c.Check(snippet, testutil.Contains, "owner /home/*/.local/share/applications/{,**} rw,")
+	c.Check(snippet, testutil.Contains, "owner /home/*/.local/share/desktop-directories/{,**} rw,")
+	c.Check(snippet, testutil.Contains, "owner /home/*/.local/share/icons/{,**} rw,")
+
+	c.Check(snippet, testutil.Contains, "/usr/bin/zenity ixr,")
+	c.Check(snippet, testutil.Contains, "/run/host/usr/sbin/ldconfig* ixr,")
+	c.Check(snippet, testutil.Contains, "/usr/bin/df ixr,")
+
+	c.Check(snippet, testutil.Contains, "capability sys_admin,")
 }
 
 func (s *UMUSupportInterfaceSuite) TestSecCompSpec(c *C) {
@@ -121,7 +150,7 @@ func (s *UMUSupportInterfaceSuite) TestSecCompSpec(c *C) {
 	c.Assert(err, IsNil)
 	spec := seccomp.NewSpecification(appSet)
 	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
-	
+
 	snippet := spec.SnippetForTag("snap.consumer.app")
 	c.Check(snippet, Not(Equals), "")
 	c.Check(snippet, testutil.Contains, "@unrestricted")
@@ -136,7 +165,7 @@ func (s *UMUSupportInterfaceSuite) TestUDevSpec(c *C) {
 	c.Assert(err, IsNil)
 	spec := udev.NewSpecification(appSet)
 	c.Assert(spec.AddConnectedPlug(s.iface, s.plug, s.slot), IsNil)
-	
+
 	c.Assert(spec.Snippets(), HasLen, 1)
 	c.Assert(spec.Snippets()[0], testutil.Contains, `SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0660", TAG+="uaccess"`)
 }
