696e6c9de2f2387fea450200640c3bd5a452ad29
Lorenzo Medici <lorenzo.medici@canonical.com>
2026-02-12 16:42:17 +0200

    arch: move Mock functions to export_test.go
    Signed-off-by: Lorenzo Medici <lorenzo.medici@canonical.com>

--- arch/export_test.go
+++ arch/export_test.go
@@ -20,11 +20,69 @@
 package arch
 
 import (
+	"fmt"
+
 	"github.com/snapcore/snapd/testutil"
+	"golang.org/x/sys/unix"
 )
 
 func MockRuntimeGOARCH(arch string) (restore func()) {
 	restore = testutil.Backup(&runtimeGOARCH)
 	runtimeGOARCH = arch
 	return restore
+}
+
+var calledMockRISCVHWProbe = false
+
+// CalledMockRISCVHWProbe returns whether the mocked riscv_hwprobe syscall
+// was executed as part of a test plan.
+func CalledMockRISCVHWProbe() bool {
+	return calledMockRISCVHWProbe
+}
+
+// MockRISCVHWProbe mocks the return value of the riscv_hwprobe syscall
+// and returns a function to restore to the current value.
+func MockRISCVHWProbe(supportedExtensions []RISCVHWProbePairs, syscallError string) (restore func()) {
+	// Mock probe function that copies the test case's supportedExtensions over the input
+	var mockRISCVHWProbe = func(pairs []RISCVHWProbePairs, set *unix.CPUSet, flags uint) (err error) {
+		// Mark that we called the function for some tests
+		calledMockRISCVHWProbe = true
+
+		// Return an error if specified in the test case
+		if syscallError != "" {
+			return fmt.Errorf(syscallError)
+		}
+
+		if len(supportedExtensions) != 0 {
+			// Otherwise, write the requested value
+			pairs[0] = supportedExtensions[0]
+			pairs[1] = supportedExtensions[1]
+		}
+
+		return nil
+	}
+
+	// Replace the normal function with the mock one
+	normalRISCVHWProbe := RISCVHWProbe
+	RISCVHWProbe = mockRISCVHWProbe
+
+	// And restore the function and the "called" flag
+	return func() {
+		RISCVHWProbe = normalRISCVHWProbe
+		calledMockRISCVHWProbe = false
+	}
+}
+
+// MockKernelVersion mocks the running kernel version in the form of the
+// version string, and returns a function to restore to the current value.
+func MockKernelVersion(newKernelVersion string) (restore func()) {
+	// Do nothing if no kernel version specified
+	if newKernelVersion == "" {
+		return func() {}
+	}
+
+	originalKernelVersion := KernelVersion
+	KernelVersion = func() string { return newKernelVersion }
+
+	return func() { KernelVersion = originalKernelVersion }
 }
--- arch/isariscv64_generic.go
+++ arch/isariscv64_generic.go
@@ -8,9 +8,22 @@ package arch
 import (
 	"fmt"
 	"runtime"
+
+	"golang.org/x/sys/unix"
 )
 
 func IsRISCVISASupported(_ string) error {
 	// Shouldn't get here, error out just in case
 	return fmt.Errorf("cannot validate RiscV ISA support while running on: %s, %s", runtime.GOOS, runtime.GOARCH)
 }
+
+// Re-defined like this because Mock functions have been moved to export_test.go
+type RISCVHWProbePairs struct {
+	Key   int64
+	Value uint64
+}
+
+// Re-defined to allow mocking this
+var RISCVHWProbe = func(pairs []RISCVHWProbePairs, set *unix.CPUSet, flags uint) (err error) { return nil }
+
+var KernelVersion = func() string { return "0" }
