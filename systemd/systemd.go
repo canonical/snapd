c5a5aaea7592bd492877f948a768ddb0c3ef4f11
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-02-16 13:33:59 +0100

    wrappers/internal: Disable and re-enable daemons to refresh XXX.wants
    If the `WantedBy` entry in the `Install` section of a .service
    file changes from `default` to `graphical-session`, the soft
    link at `/etc/systemd/user/default.target.wants` must be deleted
    and a new one at `/etc/systemd/user/graphical-session.target.wants`
    must be created for those user daemons that have changed.
    But this must be done only to those daemons that are enabled,
    to avoid enabling daemons that the user manually disabled.
    
    This can happen in two cases:
    
    * snapd is updated from the old versions that didn't support
      `graphical-session` target to a new version that supports it,
      so all the currently user services already installed will have
      their .service files updated with the new WantedBy entry.
    * a snap with an user service is updated, and the new version
      have added or removed the `desktop` plug in any of the user
      services.
    
    In both cases, a `systemctl --user reenable SERVICE-NAME` must
    be run to update the symlinks at `/etc/systemd/user/default.target.wants`
    and `/etc/systemd/user/graphical-session.target.wants` folders,
    but only if it is globally enabled. That command removes the
    symlinks at the wrong places and re-creates them in the right
    places.
    
    This patch does that, but only for globally-enabled (preset)
    user daemons. Locally-enabled daemons (thus, with their symlinks
    placed at $HOME/.config/systemd/user/XXXXX.target.wants`) aren't
    modified. That is a task for another PR.

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -403,6 +403,8 @@ type Systemd interface {
 	Backend() Backend
 	// DaemonReload reloads systemd's configuration.
 	DaemonReload() error
+	// DaemonReEnable disables a daemon and re-enables it again
+	DaemonReEnable([]string) error
 	// DaemonRexec reexecutes systemd's system manager, should be
 	// only necessary to apply manager's configuration like
 	// watchdog.
@@ -411,6 +413,8 @@ type Systemd interface {
 	EnableNoReload(services []string) error
 	// DisableNoReload the given services, do not reload system.
 	DisableNoReload(services []string) error
+	// GetServicePath returns the path of the .service file
+	GetServicePath(service string) (string, error)
 	// Start the given service or services.
 	Start(service []string) error
 	// StartNoBlock starts the given service or services non-blocking.
@@ -695,6 +699,23 @@ func (s *systemd) DisableNoReload(serviceNames []string) error {
 		args = append(args, "--no-reload")
 	}
 	args = append(args, "disable")
+	args = append(args, serviceNames...)
+	_, err := s.systemctl(args...)
+	return err
+}
+
+func (s *systemd) DaemonReEnable(serviceNames []string) error {
+	if len(serviceNames) == 0 {
+		return nil
+	}
+	var args []string
+	if s.rootDir != "" {
+		// passing root already implies no reload
+		args = append(args, "--root", s.rootDir)
+	} else {
+		args = append(args, "--no-reload")
+	}
+	args = append(args, "reenable")
 	args = append(args, serviceNames...)
 	_, err := s.systemctl(args...)
 	return err
@@ -1690,6 +1711,10 @@ func extractOriginModule(systemdUnitPath string) (string, error) {
 		}
 	}
 	return originModule, nil
+}
+
+func (s *systemd) GetServicePath(service string) (string, error) {
+	return s.getPropertyStringValue(service, "FragmentPath")
 }
 
 func (s *systemd) ListMountUnits(snapName, origin string) ([]string, error) {
