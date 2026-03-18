095beb1bf09dd2cd668c71872f678e8eecdfb45d
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-03-18 17:36:57 +0100

    overlord/ifacestate: drop a log when no effects are scheduled (#16785)
    Drop a log which occasionally shows up in stderr.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/ifacestate/handlers.go
+++ overlord/ifacestate/handlers.go
@@ -2497,7 +2497,6 @@ func (m *InterfaceManager) doProcessDelayedSecurityBackendEffects(task *state.Ta
 	}
 
 	if len(delayed.TriggeringSnaps) == 0 {
-		task.Logf("no delayed backend effects")
 		return nil
 	}
 
