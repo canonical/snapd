3a2e3e8cd8ddc1c72c1c0517064f2e9e31116161
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2026-03-23 08:53:56 -0400

    interfaces/network-setup-*: allow running python in UC26
    In UC26 we are moving the python binary to a folder for which the default
    apparmor templace denies access, but we need it when running netplan, so we
    have to allow access for interfaces that allow using netplan.

--- interfaces/builtin/network_setup_control.go
+++ interfaces/builtin/network_setup_control.go
@@ -42,6 +42,9 @@ const networkSetupControlConnectedPlugAppArmor = `
 /usr/share/netplan/ r,
 /usr/share/netplan/** r,
 
+# For UC26+
+/usr/lib/python/python* ixr,
+
 # Netplan uses busctl internally, so allow using that as well
 /usr/bin/busctl ixr,
 
