99481d8d8e3496a13ab7a43fbe8e9815c57fa5ee
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-04-14 08:23:57 +0200

    fixup! release-tools/is-lp-fips-build: attempt to workaround LP FIPS build detection problem

--- release-tools/is-lp-fips-build.sh
+++ release-tools/is-lp-fips-build.sh
@@ -7,9 +7,7 @@ set -e
 # recipe https://launchpad.net/~fips-cc-stig/fips-cc-stig/+snap/snapd-fips
 #
 # - git origin https://git.launchpad.net/~snappy-dev/snapd/+git/snapd
-# - one of the following is true:
-#   - build path: /build/snapd-fips (but not always)
-#   - openssl-fips-module-3 package is available (meaning FIPS PPA is present)
+# - openssl-fips-module-3 package is available (meaning FIPS PPA is present)
 #
 # TODO: we really need knobs on LP/snapcraft side to make this explicit
 
@@ -18,18 +16,7 @@ if ! git remote get-url origin | grep "git.launchpad.net" >&2 ; then
     exit 0
 fi
 
-# LP has no means of injecting additional configuration to the snap build, so
-# try a couple of simple checks that let us identify whether we are building a
-# FIPS target
-if echo "$PWD" | grep '^/build/snapd-fips/' >&2 ; then
-    # when building from
-    # https://launchpad.net/~fips-cc-stig/fips-cc-stig/+snap/snapd-fips recipe, the
-    # code is cloned at /build/snapd-fips/. However if the build is published to the
-    # store using 'snapd' as the store package name, LP clones the source tree into
-    # directory named 'snapd', so the check is not always successful.
-    echo "true"
-    exit 0
-elif apt show openssl-fips-module-3 > /dev/null; then
+if apt show openssl-fips-module-3 > /dev/null; then
     echo ":: openssl-fips-module-3 package is available" >&2
     # when building with Pro FIPS Updates PPA
     # https://launchpad.net/~ubuntu-advantage/+archive/ubuntu/pro-fips-updates, the
