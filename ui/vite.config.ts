2d2d02f09cba37c4ad2c1f58ff6e727283190041
Andrew Phelps <andrewphelpsj@gmail.com>
2026-05-13 10:28:19 +0200

    ui: init ui

---
+++ ui/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
