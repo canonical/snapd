32d06603b7ec9a26257cc101ab278dd304c5c44f
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-05-19 15:35:17 +0200

    overlord/snapstate: add TODO about fixing component updates
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- overlord/snapstate/snapmgr.go
+++ overlord/snapstate/snapmgr.go
@@ -897,6 +897,9 @@ func Manager(st *state.State, runner *state.TaskRunner) (*SnapManager, error) {
 
 	// component tasks
 	runner.AddHandler("prepare-component", m.doPrepareComponent, nil)
+	// TODO: add undo handler for download-component similar to
+	// undoDownloadSnap to clean up potentially corrupted component
+	// blobs (SNAPDENG-36484)
 	runner.AddHandler("download-component", m.doDownloadComponent, nil)
 	runner.AddHandler("mount-component", m.doMountComponent, m.undoMountComponent)
 	runner.AddHandler("unlink-current-component", m.doUnlinkCurrentComponent, m.undoUnlinkCurrentComponent)
