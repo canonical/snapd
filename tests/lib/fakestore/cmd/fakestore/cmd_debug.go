9ae7916d84102f17668fced4e6454f062604019b
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-05-26 15:50:03 +0200

    tests/lib/fakestore: add fakestore debug-api command
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

---
+++ tests/lib/fakestore/cmd/fakestore/cmd_debug.go
@@ -0,0 +1,137 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+type cmdDebugAPI struct {
+	Addr string `long:"addr" default:"localhost:11028" description:"Store address"`
+}
+
+var shortDebugAPIHelp = "Interact with the fakestore debug API"
+
+var debugAPICmd *flags.Command
+
+func init() {
+	var err error
+	debugAPICmd, err = parser.AddCommand("debug-api", shortDebugAPIHelp, "", &cmdDebugAPI{})
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := debugAPICmd.AddCommand("reset", "Reset all debug state", "", &cmdDebugAPIReset{}); err != nil {
+		panic(err)
+	}
+	if _, err := debugAPICmd.AddCommand("kill-request", "Configure connection kill after N bytes", "", &cmdDebugAPIKillRequest{}); err != nil {
+		panic(err)
+	}
+	if _, err := debugAPICmd.AddCommand("stats", "Print debug stats as JSON", "", &cmdDebugAPIStats{}); err != nil {
+		panic(err)
+	}
+}
+
+func debugAPIURL() string {
+	// Find the parent command's --addr option value
+	opt := debugAPICmd.FindOptionByLongName("addr")
+	addr := opt.Value().(string)
+	return fmt.Sprintf("http://%s/debug", addr)
+}
+
+// reset
+
+type cmdDebugAPIReset struct{}
+
+func (x *cmdDebugAPIReset) Execute(args []string) error {
+	return doDebugPost(map[string]any{"action": "reset"})
+}
+
+// kill-request
+
+type cmdDebugAPIKillRequest struct {
+	Positional struct {
+		Path  string `positional-arg-name:"endpoint" description:"URL path to apply kill-after to"`
+		Bytes int64  `positional-arg-name:"bytes" description:"Number of bytes to serve before killing the connection"`
+	} `positional-args:"yes" required:"yes"`
+}
+
+func (x *cmdDebugAPIKillRequest) Execute(args []string) error {
+	return doDebugPost(map[string]any{
+		"action":     "kill-request",
+		"kill-path":  x.Positional.Path,
+		"kill-after": x.Positional.Bytes,
+	})
+}
+
+// stats
+
+type cmdDebugAPIStats struct{}
+
+func (x *cmdDebugAPIStats) Execute(args []string) error {
+	url := debugAPIURL()
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		msg, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("debug API returned %d: %s", resp.StatusCode, msg)
+	}
+	var result json.RawMessage
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := json.Indent(&buf, result, "", "    "); err != nil {
+		return err
+	}
+	buf.WriteByte('\n')
+	_, err = buf.WriteTo(os.Stdout)
+	return err
+}
+
+// helpers
+
+func doDebugPost(body map[string]any) error {
+	url := debugAPIURL()
+	data, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		msg, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("debug API returned %d: %s", resp.StatusCode, msg)
+	}
+	return nil
+}
