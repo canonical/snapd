cd3cbf13e32ca50fbbb78b65ee2e659470cb3f2a
Samuele Pedroni <pedronis@lucediurna.net>
2026-05-30 09:59:34 +0200

    asserts: make ExternalKeypairManager a thin wrapper around the implementation
    rename the backend to externalCmdKeypairMgrBackend and make it standalone

--- asserts/extkeypairmgr.go
+++ asserts/extkeypairmgr.go
@@ -38,98 +38,19 @@ type ExternalKeyInfo struct {
 // ExternalKeypairManager is key pair manager implemented via an external program interface.
 // TODO: points to interface docs
 type ExternalKeypairManager struct {
-	keyMgrPath string
-	impl       *extKeypairMgrImpl
+	impl *extKeypairMgrImpl
 }
 
 // NewExternalKeypairManager creates a new ExternalKeypairManager using the program at keyMgrPath.
 func NewExternalKeypairManager(keyMgrPath string) (*ExternalKeypairManager, error) {
-	em := &ExternalKeypairManager{keyMgrPath: keyMgrPath}
-	impl, err := newExtKeypairMgrImpl(&externalKeypairMgrBackend{manager: em}, extKeypairMgrConfig{
+	impl, err := newExtKeypairMgrImpl(&externalCmdKeypairMgrBackend{keyMgrPath: keyMgrPath}, extKeypairMgrConfig{
 		signingWith: fmt.Sprintf("external keypair manager %q", keyMgrPath),
 		keyStore:    "external keypair manager",
 	})
 	if err != nil {
 		return nil, err
 	}
-	em.impl = impl
-	return em, nil
-}
-
-func (em *ExternalKeypairManager) keyMgr(op string, args []string, in []byte, out any) error {
-	args = append([]string{op}, args...)
-	cmd := exec.Command(em.keyMgrPath, args...)
-	var outBuf bytes.Buffer
-	var errBuf bytes.Buffer
-
-	if len(in) != 0 {
-		cmd.Stdin = bytes.NewBuffer(in)
-	}
-	cmd.Stdout = &outBuf
-	cmd.Stderr = &errBuf
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("external keypair manager %q %v failed: %v (%q)", em.keyMgrPath, args, err, errBuf.Bytes())
-
-	}
-	switch o := out.(type) {
-	case *[]byte:
-		*o = outBuf.Bytes()
-	default:
-		if err := json.Unmarshal(outBuf.Bytes(), out); err != nil {
-			return fmt.Errorf("cannot decode external keypair manager %q %v output: %v", em.keyMgrPath, args, err)
-		}
-	}
-	return nil
-}
-
-func (em *ExternalKeypairManager) checkFeatures() (extKeypairMgrSigning, error) {
-	var feats struct {
-		Signing    []string `json:"signing"`
-		PublicKeys []string `json:"public-keys"`
-	}
-	if err := em.keyMgr("features", nil, nil, &feats); err != nil {
-		return "", err
-	}
-	var signing extKeypairMgrSigning
-	if strutil.ListContains(feats.Signing, "RSA-PKCS") {
-		signing = extKeypairMgrSigningRSAPKCS
-	} else if strutil.ListContains(feats.Signing, "OPENPGP") {
-		signing = extKeypairMgrSigningOpenPGP
-	} else {
-		return "", fmt.Errorf("external keypair manager %q missing support for RSA-PKCS or OPENPGP signing", em.keyMgrPath)
-	}
-	if !strutil.ListContains(feats.PublicKeys, "DER") {
-		return "", fmt.Errorf("external keypair manager %q missing support for public key DER output format", em.keyMgrPath)
-	}
-	return signing, nil
-}
-
-func (em *ExternalKeypairManager) keyNames() ([]string, error) {
-	var knames struct {
-		Names []string `json:"key-names"`
-	}
-	if err := em.keyMgr("key-names", nil, nil, &knames); err != nil {
-		return nil, fmt.Errorf("cannot get all external keypair manager key names: %v", err)
-	}
-	return knames.Names, nil
-}
-
-func (em *ExternalKeypairManager) findByName(name string) (PublicKey, error) {
-	var k []byte
-	err := em.keyMgr("get-public-key", []string{"-f", "DER", "-k", name}, nil, &k)
-	if err != nil {
-		return nil, &keyNotFoundError{msg: fmt.Sprintf("cannot find external key pair: %v", err)}
-	}
-	pubk, err := x509.ParsePKIXPublicKey(k)
-	if err != nil {
-		return nil, fmt.Errorf("cannot decode external key %q: %v", name, err)
-	}
-	rsaPub, ok := pubk.(*rsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("expected RSA public key, got instead: %T", pubk)
-	}
-	return RSAPublicKey(rsaPub), nil
+	return &ExternalKeypairManager{impl: impl}, nil
 }
 
 func (em *ExternalKeypairManager) Export(keyName string) ([]byte, error) {
@@ -174,26 +95,97 @@ func (em *ExternalKeypairManager) List() ([]ExternalKeyInfo, error) {
 	return em.impl.List()
 }
 
-// externalKeypairMgrBackend implements extKeypairMgrBackend and
-// extKeypairMgrByNameLookupBackend as a thin wrapper around
-// ExternalKeypairManager, allowing it to be used as the backend for an
-// extKeypairMgrImpl.
-type externalKeypairMgrBackend struct {
-	manager *ExternalKeypairManager
+// externalCmdKeypairMgrBackend implements extKeypairMgrBackend and
+// extKeypairMgrByNameLookupBackend by invoking the configured external
+// keypair manager command.
+type externalCmdKeypairMgrBackend struct {
+	keyMgrPath string
 }
 
 // expected interfaces are implemented
 var (
-	_ extKeypairMgrBackend             = (*externalKeypairMgrBackend)(nil)
-	_ extKeypairMgrByNameLookupBackend = (*externalKeypairMgrBackend)(nil)
+	_ extKeypairMgrBackend             = (*externalCmdKeypairMgrBackend)(nil)
+	_ extKeypairMgrByNameLookupBackend = (*externalCmdKeypairMgrBackend)(nil)
 )
 
-func (s *externalKeypairMgrBackend) CheckFeatures() (extKeypairMgrSigning, error) {
-	return s.manager.checkFeatures()
+func (s *externalCmdKeypairMgrBackend) keyMgr(op string, args []string, in []byte, out any) error {
+	args = append([]string{op}, args...)
+	cmd := exec.Command(s.keyMgrPath, args...)
+	var outBuf bytes.Buffer
+	var errBuf bytes.Buffer
+
+	if len(in) != 0 {
+		cmd.Stdin = bytes.NewBuffer(in)
+	}
+	cmd.Stdout = &outBuf
+	cmd.Stderr = &errBuf
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("external keypair manager %q %v failed: %v (%q)", s.keyMgrPath, args, err, errBuf.Bytes())
+
+	}
+	switch o := out.(type) {
+	case *[]byte:
+		*o = outBuf.Bytes()
+	default:
+		if err := json.Unmarshal(outBuf.Bytes(), out); err != nil {
+			return fmt.Errorf("cannot decode external keypair manager %q %v output: %v", s.keyMgrPath, args, err)
+		}
+	}
+	return nil
 }
 
-func (s *externalKeypairMgrBackend) LoadByName(name string) (*extKeypairMgrLoadedKey, error) {
-	pubKey, err := s.manager.findByName(name)
+func (s *externalCmdKeypairMgrBackend) CheckFeatures() (extKeypairMgrSigning, error) {
+	var feats struct {
+		Signing    []string `json:"signing"`
+		PublicKeys []string `json:"public-keys"`
+	}
+	if err := s.keyMgr("features", nil, nil, &feats); err != nil {
+		return "", err
+	}
+	var signing extKeypairMgrSigning
+	if strutil.ListContains(feats.Signing, "RSA-PKCS") {
+		signing = extKeypairMgrSigningRSAPKCS
+	} else if strutil.ListContains(feats.Signing, "OPENPGP") {
+		signing = extKeypairMgrSigningOpenPGP
+	} else {
+		return "", fmt.Errorf("external keypair manager %q missing support for RSA-PKCS or OPENPGP signing", s.keyMgrPath)
+	}
+	if !strutil.ListContains(feats.PublicKeys, "DER") {
+		return "", fmt.Errorf("external keypair manager %q missing support for public key DER output format", s.keyMgrPath)
+	}
+	return signing, nil
+}
+
+func (s *externalCmdKeypairMgrBackend) keyNames() ([]string, error) {
+	var knames struct {
+		Names []string `json:"key-names"`
+	}
+	if err := s.keyMgr("key-names", nil, nil, &knames); err != nil {
+		return nil, fmt.Errorf("cannot get all external keypair manager key names: %v", err)
+	}
+	return knames.Names, nil
+}
+
+func (s *externalCmdKeypairMgrBackend) findByName(name string) (PublicKey, error) {
+	var k []byte
+	err := s.keyMgr("get-public-key", []string{"-f", "DER", "-k", name}, nil, &k)
+	if err != nil {
+		return nil, &keyNotFoundError{msg: fmt.Sprintf("cannot find external key pair: %v", err)}
+	}
+	pubk, err := x509.ParsePKIXPublicKey(k)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode external key %q: %v", name, err)
+	}
+	rsaPub, ok := pubk.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("expected RSA public key, got instead: %T", pubk)
+	}
+	return RSAPublicKey(rsaPub), nil
+}
+
+func (s *externalCmdKeypairMgrBackend) LoadByName(name string) (*extKeypairMgrLoadedKey, error) {
+	pubKey, err := s.findByName(name)
 	if err != nil {
 		return nil, err
 	}
@@ -204,8 +196,8 @@ func (s *externalKeypairMgrBackend) LoadByName(name string) (*extKeypairMgrLoade
 	}, nil
 }
 
-func (s *externalKeypairMgrBackend) Visit(consider func(loaded *extKeypairMgrLoadedKey) error) error {
-	names, err := s.manager.keyNames()
+func (s *externalCmdKeypairMgrBackend) Visit(consider func(loaded *extKeypairMgrLoadedKey) error) error {
+	names, err := s.keyNames()
 	if err != nil {
 		return err
 	}
@@ -221,18 +213,18 @@ func (s *externalKeypairMgrBackend) Visit(consider func(loaded *extKeypairMgrLoa
 	return nil
 }
 
-func (s *externalKeypairMgrBackend) RSAPKCSSign(keyHandle string, prepared []byte) ([]byte, error) {
+func (s *externalCmdKeypairMgrBackend) RSAPKCSSign(keyHandle string, prepared []byte) ([]byte, error) {
 	var signature []byte
-	err := s.manager.keyMgr("sign", []string{"-m", "RSA-PKCS", "-k", keyHandle}, prepared, &signature)
+	err := s.keyMgr("sign", []string{"-m", "RSA-PKCS", "-k", keyHandle}, prepared, &signature)
 	if err != nil {
 		return nil, err
 	}
 	return signature, nil
 }
 
-func (s *externalKeypairMgrBackend) Sign(keyHandle string, content []byte) ([]byte, error) {
+func (s *externalCmdKeypairMgrBackend) Sign(keyHandle string, content []byte) ([]byte, error) {
 	var signature []byte
-	err := s.manager.keyMgr("sign", []string{"-m", "OPENPGP", "-k", keyHandle}, content, &signature)
+	err := s.keyMgr("sign", []string{"-m", "OPENPGP", "-k", keyHandle}, content, &signature)
 	if err != nil {
 		return nil, err
 	}
