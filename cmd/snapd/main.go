81a25be59f4b0b3553d45eeddcbb07a267a350bb
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-06-01 16:10:55 +0200

    cmd/snap, cmd/snapd: merge snap and snapd into a single binary
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- cmd/snapd/main.go
+++ cmd/snapd/main.go
@@ -1,7 +1,9 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 
+//go:build linux
+
 /*
- * Copyright (C) 2015-2020 Canonical Ltd
+ * Copyright (C) 2026 Canonical Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -20,9 +22,24 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
+	"github.com/snapcore/snapd/cmd/snapd/cli"
 	"github.com/snapcore/snapd/cmd/snapd/daemon"
 )
 
 func main() {
-	daemon.Main()
+	argv0 := filepath.Base(os.Args[0])
+
+	// dispatch the binary multi entry point
+	// TODO add snap-preseed
+	switch argv0 {
+	case "snapd":
+		daemon.Main()
+	default:
+		// "snap" needs to be handled last, as it's a special entrypoint for
+		// snap application execution through symlinks at /snap/bin/<name>
+		cli.Main()
+	}
 }
