3a7bb2c1cec065efff282ce03caac34267177a1c
Oliver Calder <oliver.calder@canonical.com>
2026-06-02 16:08:25 -0500

    tests/lib: ensure correct go is used during spread prepare
    Prioritizing the `best_golang` which was previously selected, then
    falling back to the most recent versioned go available, attempt to
    symlink the versioned `go` binary into `/usr/bin/go`.
    
    This avoids a scenario where e.g. the FIPS golang-1.23 package was
    installed but the `golang-go` package was also installed (let's say
    1.18), thus leaving the `go` 1.18 binary in the path to be used instead
    of the 1.23 FIPS binary we intended to use.
    
    This applies to any versioned `go` binary, as those are now installed as
    build dependencies instead of `golang-go`.
    
    It also ensures that a `go` binary does end up in the path, as versioned
    packages like `golang-1.23` do not automatically put a `go` symlink in
    `/usr/bin`.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- tests/lib/prepare-restore.sh
+++ tests/lib/prepare-restore.sh
@@ -534,7 +534,7 @@ prepare_project() {
     case "$SPREAD_SYSTEM" in
         debian-*|ubuntu-*)
             do_depinstall() {
-                best_golang=golang-1.18
+                best_golang=golang-1.23
                 case "$SPREAD_SYSTEM" in
                     ubuntu-fips-*)
                         # we are limited by the FIPS variants of go toolchain
@@ -546,14 +546,26 @@ prepare_project() {
                         quiet apt install -y golang-1.23
                         ;;
                 esac
-                # in 16.04: "apt build-dep -y ./" would also work but not on 14.04
-                gdebi --quiet --apt-line ./debian/control >deps.txt
-                quiet xargs -r eatmydata apt-get install -y < deps.txt
-                # The go 1.18 backport is not using alternatives or anything else so
-                # we need to get it on path somehow. This is not perfect but simple.
-                if [ -z "$(command -v go)" ]; then
-                    # the path filesystem path is: /usr/lib/go-1.18/bin
-                    ln -s "/usr/lib/${best_golang/lang/}/bin/go" /usr/bin/go
+                apt build-dep -y ./ # we don't run this for 14.04
+                # We need to ensure the correct version of golang is used.
+                go_path="$(command -v go)" || true
+                if [ -n "$go_path" ]; then
+                    # Ignore existing go so we ensure the desired one is used
+                    mv "$go_path" "$go_path"-orig
+                else
+                    go_path="/usr/bin/go"
+                fi
+                for real_golang in "$best_golang" golang-1.23 golang-1.22 golang-1.21 golang-1.20 golang-1.18 ; do
+                    real_golang_path="/usr/lib/${real_golang/lang/}/bin/go"
+                    if [ -e "$real_golang_path" ]; then
+                        ln -s "$real_golang_path" "$go_path"
+                        break
+                    fi
+                done
+                if [ ! -e "$go_path" ]; then
+                    # We didn't see any versioned go, so use the original go
+                    # from the path, or fail if it doesn't exist
+                    mv "$go_path"-orig "$go_path"
                 fi
             }
 
@@ -618,10 +630,8 @@ prepare_project() {
                 exit 1
                 ;;
             ubuntu-*|debian-*)
-                # in 16.04: "apt build-dep -y ./" would also work but not on 14.04
-                gdebi --quiet --apt-line ./debian/control >deps.txt
-                quiet xargs -r eatmydata apt-get install -y < deps.txt
-                
+                apt-get build-dep -y ./ # 14.04 is handled above
+
                 build_deb
                 ;;
             fedora-*|opensuse-*|amazon-*|centos-*)
