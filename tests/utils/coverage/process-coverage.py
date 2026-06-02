488cac94406ce82186b0b3b02c127c6da495e3f2
Katie May <katie.may@canonical.com>
2026-06-02 12:00:22 +0200

    working

--- tests/utils/coverage/process-coverage.py
+++ tests/utils/coverage/process-coverage.py
@@ -15,12 +15,12 @@ def test_passed(results_json: dict, backend: str, system: str, test_name: str) -
 def main():
     parser = argparse.ArgumentParser(description="Process spread coverage data")
     parser.add_argument("--coverage-dir", required=True, help="Directory containing per-test coverage.json files")
-    parser.add_argument("--results-path", required=True, help="Path to the spread results.json file")
+    # parser.add_argument("--results-path", required=True, help="Path to the spread results.json file")
     parser.add_argument("--output-dir", required=True, help="Directory to write execution.json and execution-w-init.json")
     args = parser.parse_args()
 
-    with open(args.results_path) as f:
-        results = json.load(f)
+    # with open(args.results_path) as f:
+    #     results = json.load(f)
 
     dirs = os.listdir(args.coverage_dir)
 
@@ -41,14 +41,14 @@ def main():
                 print(f"ERROR: could not generate {file}, skipping due to {result.stderr}", file=sys.stderr)
                 continue
         split = dir.split(":")
-        passed = test_passed(results, split[0], split[1], split[2].replace("--", "/"))
+        # passed = test_passed(results, split[0], split[1], split[2].replace("--", "/"))
         with open(file) as f:
             try:
                 data = json.load(f)
                 for entry in data["files"]:
                     if entry["path"] not in execution_dict:
                         execution_dict[entry["path"]] = []
-                    execution_dict[entry["path"]].append({"test": dir.replace("--", "/"), "passed": passed, "functions": entry["covered_functions"]})
+                    execution_dict[entry["path"]].append({"test": dir.replace("--", "/"), "functions": entry["covered_functions"]})
                     
             except:
                 print(f"ERROR: could not load {file}, skipping", file=sys.stderr)
