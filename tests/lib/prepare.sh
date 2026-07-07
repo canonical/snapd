8bfce57917d36b6f049fe979212989d8a35eddf5
Andrew Phelps <andrew.phelps@canonical.com>
2026-07-06 20:47:31 -0400

    tests: get hybrid remodel test working on 26.04

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1218,7 +1218,7 @@ uc24_build_initramfs_kernel_snap() {
 
     unsquashfs -d pc-kernel "$ORIG_SNAP"
     kernelver=$(find pc-kernel/modules/ -maxdepth 1 -mindepth 1 -printf "%f")
-    ubuntu-core-initramfs create-initrd --kernelver="$kernelver" --kerneldir pc-kernel/modules/"$kernelver" \
+    TMPDIR=/var/tmp ubuntu-core-initramfs create-initrd --kernelver="$kernelver" --kerneldir pc-kernel/modules/"$kernelver" \
                           --firmwaredir pc-kernel/firmware --output initrd.img
 
     # Check that manifest is generated
@@ -1249,7 +1249,7 @@ uc24_build_initramfs_kernel_snap() {
     # Build signed uki image - snakeoil keys shipped by ubuntu-core-initramfs
     # are used by default
     objcopy -O binary -j .linux pc-kernel/kernel.efi linux-"$kernelver"
-    ubuntu-core-initramfs create-efi --kernelver="$kernelver" --initrd initrd.img --kernel linux --output kernel.efi
+    TMPDIR=/var/tmp ubuntu-core-initramfs create-efi --kernelver="$kernelver" --initrd initrd.img --kernel linux --output kernel.efi
     cp kernel.efi-"$kernelver" pc-kernel/kernel.efi
 
     # copy any extra files that tests may need for the kernel
