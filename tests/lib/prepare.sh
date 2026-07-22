38ff9a3a46720469655aa319bc3c653708e1ccdd
Andrew Phelps <andrew.phelps@canonical.com>
2026-07-22 10:25:19 -0400

    fixup! tests: get hybrid remodel test working on 26.04

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1218,6 +1218,8 @@ uc24_build_initramfs_kernel_snap() {
 
     unsquashfs -d pc-kernel "$ORIG_SNAP"
     kernelver=$(find pc-kernel/modules/ -maxdepth 1 -mindepth 1 -printf "%f")
+
+    # TODO: update ubuntu-core-initramfs to use a disk-backed tmpdir by default
     TMPDIR=/var/tmp ubuntu-core-initramfs create-initrd --kernelver="$kernelver" --kerneldir pc-kernel/modules/"$kernelver" \
                           --firmwaredir pc-kernel/firmware --output initrd.img
 
@@ -1249,6 +1251,8 @@ uc24_build_initramfs_kernel_snap() {
     # Build signed uki image - snakeoil keys shipped by ubuntu-core-initramfs
     # are used by default
     objcopy -O binary -j .linux pc-kernel/kernel.efi linux-"$kernelver"
+
+    # TODO: update ubuntu-core-initramfs to use a disk-backed tmpdir by default
     TMPDIR=/var/tmp ubuntu-core-initramfs create-efi --kernelver="$kernelver" --initrd initrd.img --kernel linux --output kernel.efi
     cp kernel.efi-"$kernelver" pc-kernel/kernel.efi
 
