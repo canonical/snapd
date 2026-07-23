b6c5d685627036065413b38074d7b3f98a4c7f90
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2026-07-23 13:23:47 +0200

    Can be only a core

--- interfaces/policy/basedeclaration_test.go
+++ interfaces/policy/basedeclaration_test.go
@@ -854,7 +854,7 @@ var (
 		"nvme-control":              {"core"},
 		"ofono":                     {"app", "core"},
 		"online-accounts-service":   {"app"},
-		"orca-screen-reader":        {"app"},
+		"orca-screen-reader":        {"core"},
 		"power-control":             {"core"},
 		"ppp":                       {"core"},
 		"polkit-agent":              {"core"},
