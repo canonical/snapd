525ac4d2892b64dd57427835e3609728471ead1e
Andrew Phelps <andrew.phelps@canonical.com>
2026-08-12 10:29:34 -0400

    many: various fixes for hybrid-with-seed remodels (#17367)

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1241,7 +1241,9 @@ uc24_build_initramfs_kernel_snap() {
 
     unsquashfs -d pc-kernel "$ORIG_SNAP"
     kernelver=$(find pc-kernel/modules/ -maxdepth 1 -mindepth 1 -printf "%f")
-    ubuntu-core-initramfs create-initrd --kernelver="$kernelver" --kerneldir pc-kernel/modules/"$kernelver" \
+
+    # TODO: update ubuntu-core-initramfs to use a disk-backed tmpdir by default
+    TMPDIR=/var/tmp ubuntu-core-initramfs create-initrd --kernelver="$kernelver" --kerneldir pc-kernel/modules/"$kernelver" \
                           --firmwaredir pc-kernel/firmware --output initrd.img
 
     # Check that manifest is generated
@@ -1272,7 +1274,9 @@ uc24_build_initramfs_kernel_snap() {
     # Build signed uki image - snakeoil keys shipped by ubuntu-core-initramfs
     # are used by default
     objcopy -O binary -j .linux pc-kernel/kernel.efi linux-"$kernelver"
-    ubuntu-core-initramfs create-efi --kernelver="$kernelver" --initrd initrd.img --kernel linux --output kernel.efi
+
+    # TODO: update ubuntu-core-initramfs to use a disk-backed tmpdir by default
+    TMPDIR=/var/tmp ubuntu-core-initramfs create-efi --kernelver="$kernelver" --initrd initrd.img --kernel linux --output kernel.efi
     cp kernel.efi-"$kernelver" pc-kernel/kernel.efi
 
     # copy any extra files that tests may need for the kernel
