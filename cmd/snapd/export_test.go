6c07934e8adb4d146e7a282bc80c70235ed4c085
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-08-13 07:48:24 +0200

    cmd/snapd, packaging: dispatch to internal tools - snapd-apparmor, snap-gpio-helper, snap-preseed (#17357)
    * cmd/snapd-tool-wrap: add snapd tool entrypoint helper
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snapd: dispatch to internal tools
    
    Integrate dispatch to internal tools:
    - snapd-apparmor
    - snap-preseed
    - snap-gpio-helper
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * packaging, snapcraft: set up snapd-tool-wrap dispatch
    
    Set up dispatch through snapd-tools-wrap for the following tools:
    - snapd-apparmor
    - snap-preseed
    - snap-gpio-helper
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * snapdtool: expose mocking helpers
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * cmd/snapd: move reexec out of individual tools into snapd tool dispatch
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * tests/main/multi-entry: verify multi entry dispatch
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * ARCHITECTURE: update to mention internal tools.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * fixup! cmd/snapd: dispatch to internal tools
    
    * fixup! ARCHITECTURE: update to mention internal tools.
    
    * fixup! packaging, snapcraft: set up snapd-tool-wrap dispatch
    
    * fixup! cmd/snapd: dispatch to internal tools
    
    ---------
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

---
+++ cmd/snapd/export_test.go
@@ -0,0 +1,47 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+//go:build linux
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package main
+
+import "github.com/snapcore/snapd/testutil"
+
+// Main exposes the unexported main() for testing.
+var Main = main
+
+// MockToolMains replaces the toolMains dispatch map for the duration of a test.
+func MockToolMains(m map[string]func()) (restore func()) {
+	return testutil.Mock(&toolMains, m)
+}
+
+// MockReexecTools replaces the reexecTools list for the duration of a test.
+func MockReexecTools(r []string) (restore func()) {
+	return testutil.Mock(&reexecTools, r)
+}
+
+// MockDaemonMain replaces the daemon entry point for the duration of a test.
+func MockDaemonMain(f func()) (restore func()) {
+	return testutil.Mock(&daemonMain, f)
+}
+
+// MockCLIMain replaces the CLI entry point for the duration of a test.
+func MockCLIMain(f func()) (restore func()) {
+	return testutil.Mock(&cliMain, f)
+}
