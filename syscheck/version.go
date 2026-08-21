56d7f75ba6650d5d8a51d2aea56625362902695c
Andrew Phelps <andrew.phelps@canonical.com>
2026-08-21 04:48:02 -0700

    syscheck: fix static check error (#17498)

--- syscheck/version.go
+++ syscheck/version.go
@@ -64,7 +64,7 @@ func checkKernelVersion() error {
 			kver := osutil.KernelVersion()
 			// a kernel version looks like this: "4.4.0-112-generic" and
 			// we are only interested in the bits before the "-"
-			kver = strings.SplitN(kver, "-", 2)[0]
+			kver, _, _ = strings.Cut(kver, "-")
 			cmp, err := strutil.VersionCompare(kver, "3.13.0")
 			if err != nil {
 				logger.Noticef("cannot check kernel: %v", err)
