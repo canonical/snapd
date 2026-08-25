318d6fcb4358452c109275db7df104c46cb81d36
Katie May <katie.may@canonical.com>
2026-08-25 09:38:37 +0200

    naming: add TODO method to mark where string sound become type SnapName/InstanceName

--- snap/naming/snap.go
+++ snap/naming/snap.go
@@ -29,6 +29,12 @@ func (n SnapName) String() string {
 	return string(n)
 }
 
+// TODOInstanceName is the same as String, but marks all the places
+// that need to be updated to take SnapName type instance.
+func (n SnapName) TODOSnapName() string {
+	return string(n)
+}
+
 // InstanceName is the name of a snap decorated with an optional instance key.
 type InstanceName string
 
@@ -58,5 +64,11 @@ func (n InstanceName) InstanceKey() string {
 
 // String returns the instance name as a plain string.
 func (n InstanceName) String() string {
+	return string(n)
+}
+
+// TODOInstanceName is the same as String, but marks all the places
+// that need to be updated to take InstanceName type instance.
+func (n InstanceName) TODOInstanceName() string {
 	return string(n)
 }
