1d9854310e7f2f56a3e94513db7a48bf122d2c05
Andrew Phelps <andrew.phelps@canonical.com>
2026-08-31 13:09:47 -0400

    many: another round of fixes

--- cmd/snapd/cli/main_test.go
+++ cmd/snapd/cli/main_test.go
@@ -86,11 +86,7 @@ func (s *BaseSnapSuite) SetUpTest(c *C) {
 	snap.Stdout = s.stdout
 	snap.Stderr = s.stderr
 	snap.ReadPassword = s.readPassword
-	authDir := c.MkDir()
-	// client.readAuthData may run under the real sudo user, so the test
-	// directory must be traversable even when created by root.
-	c.Assert(os.Chmod(authDir, 0755), IsNil)
-	s.AuthFile = filepath.Join(authDir, "json")
+	s.AuthFile = filepath.Join(c.MkDir(), "json")
 	os.Setenv(TestAuthFileEnvKey, s.AuthFile)
 
 	s.AddCleanup(interfaces.MockSystemKey(`
