d33378325931c330afffbaa63be7c77a21ca3185
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-09-01 19:10:49 +0200

    tests/lib/prepare: configurable snapd memory limit, workaround for CentOS (#17544)
    * tests/lib/prepare: make snapd memory limit configurable, use MemoryMax or MemoryLimit
    
    Make the max memory setting for snapd configurable and use either
    MemoryLimit (now deprecated) or MemoryMax (added in systemd 231 back in
    2016).
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * tests/lib/prepare: override memory limit on CentOS
    
    Override the memory limit on CentoOS as a workaround for the xfs page
    cache weirdness.
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    * tests/lib/prepare: also set the soft memory limit on CentOS
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>
    
    ---------
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@canonical.com>

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -10,6 +10,9 @@ set -eux
 . "$TESTSLIB/state.sh"
 #shellcheck source=tests/lib/core-initrd.sh
 . "$TESTSLIB"/core-initrd.sh
+#shellcheck source=tests/lib/systems.sh
+. "$TESTSLIB"/systems.sh
+
 
 disable_kernel_rate_limiting() {
     # kernel rate limiting hinders debugging security policy so turn it off
@@ -333,6 +336,10 @@ prepare_memory_limit_override() {
     # or the system is known to be problematic
     local set_limit=1
 
+    memlimit="200M"
+    # soft limit which applies to v2 and controls when the kernel will apply
+    # throttling, normally unset
+    memsoftlimit=""
     case "$SPREAD_SYSTEM" in
         ubuntu-core-16-*|ubuntu-core-18-*|ubuntu-16.04-*|ubuntu-18.04-*)
             # the tests on UC16, UC18 and correspondingly 16.04 and 18.04 have
@@ -344,6 +351,15 @@ prepare_memory_limit_override() {
         amazon-linux-*)
             # similar issues have been observed on Amazon Linux 2
             set_limit=0
+            ;;
+        centos-*)
+            # try to workaround xfs doing weird things with the page cache
+            # which is counted towards the cgroup memory limits
+            memlimit="600M"
+            # we also need to apply soft limit to throttle snapd or its child
+            # processes within the same cgroup, such that a lot of cached,
+            # unflushed I/O will not exhaust the hard limit
+            memsoftlimit="300M"
             ;;
         *)
             if [ "$SNAPD_NO_MEMORY_LIMIT" = 1 ]; then
@@ -375,13 +391,20 @@ prepare_memory_limit_override() {
         # oom-killer which will be caught in restore_project_each in
         # prepare-restore.sh.
         #
-        # This ought to set MemoryMax, but on systems with older systemd we need to
-        # use MemoryLimit, which is deprecated and replaced by MemoryMax now, but
-        # systemd is backwards compatible so the limit is still set.
-        cat <<EOF > /etc/systemd/system/snapd.service.d/memory-max.conf
+        # MeoryMax was added in systemd 231 ~2016, so it's safe to assume all
+        # cgroup v2 systems should be using it.
+        if is_cgroupv2; then
+            cat <<EOF > /etc/systemd/system/snapd.service.d/memory-max.conf
 [Service]
-MemoryLimit=200M
+MemoryMax=${memlimit}
+MemoryHigh=${memsoftlimit}
 EOF
+        else
+            cat <<EOF > /etc/systemd/system/snapd.service.d/memory-max.conf
+[Service]
+MemoryLimit=${memlimit}
+EOF
+        fi
     fi
     # the service setting may have changed in the service so we need
     # to ensure snapd is reloaded
