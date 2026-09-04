67b5460ad19a0f3378d16cba68c9a4dcbbd2a498
Ardelean Călin <calin.ardelean@canonical.com>
2026-09-04 13:22:05 +0300

    asserts: use exported consistency checker internally

--- asserts/database.go
+++ asserts/database.go
@@ -834,26 +834,16 @@ func CheckTimestampVsSigningKeyValidity(assert Assertion, signingKey *AccountKey
 	return nil
 }
 
-// A consistencyChecker performs further checks based on the full
-// assertion database knowledge and its own signing key.
-type consistencyChecker interface {
-	checkConsistency(roDB RODatabase, signingKey *AccountKey) error
-}
-
-// ConsistencyChecker is implemented by externally defined assertions that
-// perform additional checks using the assertion database and their signing key.
+// A ConsistencyChecker performs further checks based on the full assertion
+// database knowledge and its own signing key.
 type ConsistencyChecker interface {
-	CheckConsistency(db RODatabase, signingKey *AccountKey) error
+	CheckConsistency(roDB RODatabase, signingKey *AccountKey) error
 }
 
 // CheckCrossConsistency verifies that the assertion is consistent with the other statements in the database.
 func CheckCrossConsistency(assert Assertion, signingKey *AccountKey, roDB RODatabase, checkTimeEarliest, checkTimeLatest time.Time) error {
 	if checker, ok := assert.(ConsistencyChecker); ok {
 		return checker.CheckConsistency(roDB, signingKey)
-	}
-	// see if the assertion requires further checks
-	if checker, ok := assert.(consistencyChecker); ok {
-		return checker.checkConsistency(roDB, signingKey)
 	}
 	return nil
 }
