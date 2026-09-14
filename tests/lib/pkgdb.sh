06a06866172abb4e20bbf27e1e12e189d87cae4c
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2026-09-14 08:43:58 -0300

    Adding comments explaining about the new dependencies

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -569,6 +569,8 @@ pkg_dependencies_ubuntu_nested(){
         echo "
             golang
         "
+        # These dependencies are used to build the initramfs deb package required for the
+        # kernel snap with initramfs when using Ubuntu 24.04 or later.
         if os.query is-ubuntu-ge 24.04; then
             echo "
                 dpkg-dev
@@ -578,6 +580,8 @@ pkg_dependencies_ubuntu_nested(){
                 linux-firmware
             "
         else
+            # Add the PPA which is required to install ubuntu-core-initramfs, needed to
+            # build the kernel snap with initramfs when using Ubuntu 20.04 or 22.04.
             add-apt-repository ppa:snappy-dev/image -y  > /dev/null 2>&1
             echo "
                 software-properties-common
@@ -585,7 +589,6 @@ pkg_dependencies_ubuntu_nested(){
                 linux-firmware
             "
         fi
-
     fi
 }
 
